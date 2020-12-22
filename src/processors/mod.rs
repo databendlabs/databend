bfdf90a4b3a0ba0baa82f86211e0d35c3a24dd48
BohuTANG <overred.shuttler@gmail.com>
2020-12-22 20:12:07 +0800

    [processor] remove through processor

--- src/processors/mod.rs
+++ src/processors/mod.rs
@@ -5,18 +5,15 @@
 mod pipeline_builder_test;
 mod processor_empty_test;
 mod processor_merge_test;
-mod processor_through_test;
 
 mod pipeline;
 mod pipeline_builder;
 mod processor;
 mod processor_empty;
 mod processor_merge;
-mod processor_through;
 
 pub use self::pipeline::Pipeline;
 pub use self::pipeline_builder::PipelineBuilder;
 pub use self::processor::{FormatterSettings, IProcessor};
 pub use self::processor_empty::EmptyProcessor;
 pub use self::processor_merge::MergeProcessor;
-pub use self::processor_through::ThroughProcessor;
