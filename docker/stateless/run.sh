5ed4d75e63de2dbb027455ebf916735018af31c3
sundy-li <543950155@qq.com>
2021-01-29 07:53:07 +0000

    [tests] modify script

--- docker/stateless/run.sh
+++ docker/stateless/run.sh
@@ -7,8 +7,8 @@ sleep 5
 
 function run_tests()
 {
-  mkdir -p test_output
   cd tests
+  mkdir -p test_output
   ./fuse-test --print-time  --jobs 4  2>&1 \
       | tee -a test_output/test_result.txt
 }
