2ac78d856e3f9fadf89ff1b09cb6e23e6b51d0c3
BohuTANG <overred.shuttler@gmail.com>
2021-03-24 19:23:28 +0800

    [query/clusters] fix test result

--- query/clusters/src/cluster_test.rs
+++ query/clusters/src/cluster_test.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0.
 
 #[test]
-fn test_cluster() -> crate::error::FuseQueryResult<()> {
+fn test_cluster() -> crate::error::ClusterResult<()> {
     use pretty_assertions::assert_eq;
 
     use crate::*;
