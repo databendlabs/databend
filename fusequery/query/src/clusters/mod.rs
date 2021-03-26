2e8f53507884751fe3882c9034df2580ab674caa
BohuTANG <overred.shuttler@gmail.com>
2021-03-26 19:26:31 +0800

    [fusequery/query] move configs/clusters to src

---
+++ fusequery/query/src/clusters/mod.rs
@@ -0,0 +1,11 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-License-Identifier: Apache-2.0.
+
+mod cluster_test;
+
+mod cluster;
+mod node;
+
+pub use cluster::{Cluster, ClusterRef};
+pub use node::Node;
