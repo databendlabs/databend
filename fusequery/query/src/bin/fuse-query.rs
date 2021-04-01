2ac4fcd15078d91b53ba84b1c9b87307936cb317
sundyli <543950155@qq.com>
2021-04-01 17:53:16 +0800

    [servers] add clickhouse server protocol support

--- fusequery/query/src/bin/fuse-query.rs
+++ fusequery/query/src/bin/fuse-query.rs
@@ -38,7 +38,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         );
     }
 
-    // MySQL handler.
+    // ClickHouse handler.
     {
         let handler =
             ClickHouseHandler::create(conf.clone(), cluster.clone(), session_manager.clone());
