hulunbier/fuse-query

WIP: table spec

fusestore/store/src/io/mod.rs
@@ -1,0 +1,7 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-Lise-Identifier: Apache-2.0.
+
+mod file_io;
+pub use file_io::FileIO;
+pub use file_io::AsyncSeekableRead;

fusestore/store/src/spec/mod.rs
@@ -1,0 +1,10 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-License-Identifier: Apache-2.0.
+
+pub use db_spec::DatabaseSpec;
+pub use tbl_spec::TblSpec;
+
+mod tbl_spec;
+mod db_spec;
+

databendlabs/databend

fusestore/store/src/io/mod.rs
@@ -1,0 +1,7 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-Lise-Identifier: Apache-2.0.
+
+mod storage;
+pub use storage::AsyncSeekableRead;
+pub use storage::FS;

fusestore/store/src/spec/mod.rs
@@ -1,0 +1,9 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-License-Identifier: Apache-2.0.
+
+pub use db_spec::DatabaseSpec;
+pub use tbl_spec::TableSpec;
+
+mod db_spec;
+mod tbl_spec;