c38f639e6299fb399f579760bcc65705747d679f
BohuTANG <overred.shuttler@gmail.com>
2021-04-19 11:25:52 +0800

    [common/streams] add progress stream

---
+++ common/streams/src/stream_progress.rs
@@ -0,0 +1,62 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-License-Identifier: Apache-2.0.
+
+use std::task::Context;
+use std::task::Poll;
+
+use anyhow::Result;
+use common_datablocks::DataBlock;
+use common_progress::Progress;
+use common_progress::ProgressCallback;
+use common_progress::ProgressRef;
+use futures::Stream;
+use pin_project_lite::pin_project;
+
+use crate::SendableDataBlockStream;
+
+pin_project! {
+    pub struct ProgressStream {
+        #[pin]
+        input: SendableDataBlockStream,
+        callback: ProgressCallback,
+        progress: ProgressRef,
+    }
+}
+
+impl ProgressStream {
+    pub fn try_create(input: SendableDataBlockStream, callback: ProgressCallback) -> Result<Self> {
+        Ok(Self {
+            input,
+            callback,
+            progress: Progress::create(),
+        })
+    }
+}
+
+impl Stream for ProgressStream {
+    type Item = Result<DataBlock>;
+
+    fn poll_next(
+        self: std::pin::Pin<&mut Self>,
+        ctx: &mut Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        let this = self.project();
+
+        match this.input.poll_next(ctx) {
+            Poll::Ready(x) => match x {
+                Some(result) => match result {
+                    Ok(block) => {
+                        this.progress.add_rows(block.num_rows());
+                        this.progress.add_bytes(block.memory_size());
+                        (this.callback)(&this.progress);
+                        Poll::Ready(Some(Ok(block)))
+                    }
+                    Err(e) => Poll::Ready(Some(Err(e))),
+                },
+                None => Poll::Ready(None),
+            },
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
