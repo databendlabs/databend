e0e2944502f7ba7b56713ded9525ec1291d7f059
BohuTANG <overred.shuttler@gmail.com>
2021-04-25 20:09:46 +0800

    [github] add stateless cluster test

--- scripts/ci/fusequery-cluster-3-nodes.sh
+++ scripts/ci/fusequery-cluster-3-nodes.sh
@@ -4,31 +4,27 @@ killall fuse-query
 sleep 1
 
 echo 'start cluster-1'
-nohup target/release/fuse-query --rpc-api-address=0.0.0.0:9091 --http-api-address=0.0.0.0:8081 --mysql-handler-port=3307 --metric-api-address=0.0.0.0:7071 --log-level ERROR &
+nohup target/debug/fuse-query --rpc-api-address=0.0.0.0:9091 --http-api-address=0.0.0.0:8081 --mysql-handler-port=3307 --metric-api-address=0.0.0.0:7071 --log-level ERROR &
 
 echo "Waiting on cluster-1..."
-while ! nc -z localhost 9091; do
-  sleep 0.1
-done
-
-while ! nc -z localhost 8081; do
-  sleep 0.1
+while ! nc -z -w 5 0.0.0.0 9091; do
+  sleep 1
 done
 
 echo 'start cluster-2'
-nohup target/release/fuse-query --rpc-api-address=0.0.0.0:9092 --http-api-address=0.0.0.0:8082 --mysql-handler-port=3308 --metric-api-address=0.0.0.0:7072 --log-level ERROR &
+nohup target/debug/fuse-query --rpc-api-address=0.0.0.0:9092 --http-api-address=0.0.0.0:8082 --mysql-handler-port=3308 --metric-api-address=0.0.0.0:7072 --log-level ERROR &
 
 echo "Waiting on cluster-2..."
-while ! nc -z localhost 9092; do
-  sleep 0.1
+while ! nc -z -w 5 0.0.0.0 9092; do
+  sleep 1
 done
 
 echo 'start cluster-3'
-nohup target/release/fuse-query --rpc-api-address=0.0.0.0:9093 --http-api-address=0.0.0.0:8083 --mysql-handler-port=3309 --metric-api-address=0.0.0.0:7073 --log-level ERROR &
+nohup target/debug/fuse-query --rpc-api-address=0.0.0.0:9093 --http-api-address=0.0.0.0:8083 --mysql-handler-port=3309 --metric-api-address=0.0.0.0:7073 --log-level ERROR &
 
 echo "Waiting on cluster-3..."
-while ! nc -z localhost 9093; do
-  sleep 0.1
+while ! nc -z -w 5 0.0.0.0 9093; do
+  sleep 1
 done
 
 curl http://127.0.0.1:8081/v1/cluster/add -X POST -H "Content-Type: application/json" -d '{"name":"cluster1","address":"0.0.0.0:9091", "priority":3, "cpus":8}'
--- scripts/ci/fusequery-standalone.sh
+++ scripts/ci/fusequery-standalone.sh
@@ -4,9 +4,9 @@ killall fuse-query
 sleep 1
 
 echo 'Start...'
-nohup target/release/fuse-query --rpc-api-address=0.0.0.0:9091 --http-api-address=0.0.0.0:8081 --mysql-handler-port=3307 --metric-api-address=0.0.0.0:7071 --log-level ERROR &
+nohup target/debug/fuse-query --rpc-api-address=0.0.0.0:9091 --http-api-address=0.0.0.0:8081 --mysql-handler-port=3307 --metric-api-address=0.0.0.0:7071 --log-level ERROR &
 
 echo "Waiting on..."
-while ! nc -z localhost 3307; do
-  sleep 0.1
+while ! nc -z -w 5 0.0.0.0 3307; do
+  sleep 1
 done
