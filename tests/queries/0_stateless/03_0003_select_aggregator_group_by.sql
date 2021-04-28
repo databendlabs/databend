a550a18163cc4303c5ed869b3950b22bbb52bac5
Yizheng Jiao <jyizheng@gmail.com>
2021-04-28 17:06:46 -0400

    fix stateless test

--- tests/queries/0_stateless/03_0003_select_aggregator_group_by.sql
+++ tests/queries/0_stateless/03_0003_select_aggregator_group_by.sql
@@ -3,5 +3,5 @@ SELECT max(number) FROM numbers_mt (10) where number > 99999999998 GROUP BY numb
 SELECT 'SELECT avg(number), max(number+1)+1 FROM numbers_mt(10000) where number > 2 GROUP BY 1';
 SELECT avg(number), max(number+1)+1 FROM numbers_mt(10000) where number > 2 GROUP BY 1;
 
-SELECT 'SELECT min(number%3) as c1, max(number%2) as c2 FROM numbers_mt(10000) where number > 2 group by c1, c2 order by c1,c2';
-SELECT min(number%3) as c1, max(number%2) as c2 FROM numbers_mt(10000) where number > 2 group by c1, c2 order by c1,c2;
+SELECT 'SELECT number%3 as c1, number%2 as c2 FROM numbers_mt(10000) where number > 2 group by c1, c2 order by c1,c2';
+SELECT number%3 as c1, number%2 as c2 FROM numbers_mt(10000) where number > 2 group by c1, c2 order by c1,c2;
--- tests/queries/0_stateless/05_0002_ddl_create_local_csv_table.sql
+++ tests/queries/0_stateless/05_0002_ddl_create_local_csv_table.sql
@@ -1,2 +1,2 @@
 create table default.test_csv (id int,name varchar(255),rank int) Engine = CSV location = 'tests/data/sample.csv';
-select avg(rank), max(id), min(name) as c1 from default.test_csv group by c1 order by c1 desc;
+select avg(rank), max(id), name from default.test_csv group by name order by name desc;
