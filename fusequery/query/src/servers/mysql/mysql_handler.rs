08049de8fa0d8b2e0e519948060f29a5a20e4253
zhang2014 <coswde@gmail.com>
2021-04-29 14:22:46 +0800

    ISSUES-364 clean code

--- fusequery/query/src/servers/mysql/mysql_handler.rs
+++ fusequery/query/src/servers/mysql/mysql_handler.rs
@@ -4,15 +4,13 @@
 
 use std::io;
 use std::net;
-use std::sync::Arc;
 use std::time::Instant;
 
 use common_datablocks::DataBlock;
 use common_exception::ErrorCodes;
 use common_exception::Result;
-use common_planners::Statistics;
-use futures::future::BoxFuture;
-use futures::FutureExt;
+use common_ext::ResultExt;
+use common_ext::ResultTupleExt;
 use log::debug;
 use metrics::histogram;
 use msql_srv::*;
@@ -22,8 +20,8 @@ use tokio_stream::StreamExt as OtherStreamExt;
 
 use crate::clusters::ClusterRef;
 use crate::configs::Config;
-use crate::interpreters::IInterpreter;
 use crate::interpreters::InterpreterFactory;
+use crate::interpreters::InterpreterPtr;
 use crate::sessions::FuseQueryContextRef;
 use crate::sessions::SessionRef;
 use crate::sql::PlanParser;
@@ -69,48 +67,34 @@ impl<W: io::Write> MysqlShim<W> for Session {
         self.ctx.reset().unwrap();
         let start = Instant::now();
 
-        fn build_runtime(max_threads: Result<u64>) -> Result<Runtime> {
-            max_threads
-                .map_err(|_| {
-                    ErrorCodes::UnknownSetting(String::from("Missing max_thread settings."))
-                })
-                .and_then(|v| {
-                    tokio::runtime::Builder::new_multi_thread()
-                        .enable_io()
-                        .worker_threads(v as usize)
-                        .build()
-                        .map_err(|tokio_error| ErrorCodes::TokioError(format!("{}", tokio_error)))
-                })
+        fn build_runtime(max_threads: u64) -> Result<Runtime> {
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_io()
+                .worker_threads(max_threads as usize)
+                .build()
+                .map_err(|tokio_error| ErrorCodes::TokioError(format!("{}", tokio_error)))
         }
 
-        type DataPuller<'a> = BoxFuture<'a, Result<Vec<DataBlock>>>;
-        fn data_puller(
-            interpreter: &'_ Arc<dyn IInterpreter>,
-            _statistics: Result<Statistics>
-        ) -> DataPuller<'_> {
-            return interpreter
-                .execute()
-                .then(|r_stream| match r_stream {
-                    Err(error_code) => futures::future::err(error_code).right_future(),
-                    Ok(stream) => stream.collect::<Result<Vec<DataBlock>>>().left_future()
-                })
-                .boxed();
+        type ResultSet = Result<Vec<DataBlock>>;
+        fn receive_data_set(runtime: Runtime, interpreter: InterpreterPtr) -> ResultSet {
+            use futures::future::TryFutureExt;
+            runtime.block_on(
+                interpreter
+                    .execute()
+                    .and_then(|stream| stream.collect::<Result<Vec<DataBlock>>>())
+            )
         }
 
         use crate::servers::mysql::endpoints::on_query_done as done;
         let output = PlanParser::create(self.ctx.clone())
             .build_from_sql(query)
             .and_then(|built_plan| InterpreterFactory::get(self.ctx.clone(), built_plan))
-            .and_then(|interpreter| {
-                build_runtime(self.ctx.get_max_threads()).map(|runtime| (runtime, interpreter))
-            })
-            .and_then(|(runtime, interpreter)| {
-                runtime.block_on(data_puller(&interpreter, self.ctx.try_get_statistics()))
-            })
-            .map(Some)
-            .transpose()
-            .map(done(writer))
-            .unwrap();
+            // Execute query and get result
+            .zip(self.ctx.get_max_threads())
+            .left_and_then(build_runtime)
+            .and_then_tuple(receive_data_set)
+            // Push result set to client
+            .and_match(done(writer));
 
         histogram!(
             super::mysql_metrics::METRIC_MYSQL_PROCESSOR_REQUEST_DURATION,
