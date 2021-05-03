12b11fd974c2b72ccd3cb5cdd2348e8045c8a7b6
Yizheng Jiao <jyizheng@gmail.com>
2021-05-03 11:43:13 -0400

    fix tests that failed

--- tests/queries/0_stateless/03_0007_select_alias.sql
+++ tests/queries/0_stateless/03_0007_select_alias.sql
@@ -1,2 +1,2 @@
 set max_threads=1;
-SELECT (number+1) as c1, max(number) as c2 FROM numbers_mt(10) where c2<10 group by c1%3 having c2>1 order by c1 desc, c2 asc;
+SELECT min(number+1) as c1, max(number) as c2 FROM numbers_mt(10) where c2<10 group by c1%3 having c2>1 order by c1 desc, c2 asc;
