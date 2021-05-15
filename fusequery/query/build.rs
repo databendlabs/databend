e39face0f23bb24e7f9ea6bafb6a5f866826b5bb
BohuTANG <overred.shuttler@gmail.com>
2021-05-15 10:08:56 +0800

    [github] format pr template

--- fusequery/query/build.rs
+++ fusequery/query/build.rs
@@ -1,8 +1,6 @@
 // Copyright 2020-2021 The Datafuse Authors.
 //
 // SPDX-License-Identifier: Apache-2.0.
-//
-// https://github.com/rust-lang/rustfmt/blob/e1ab878ccb24cda1b9e1c48865b375230385fede/build.rs
 
 use vergen::vergen;
 use vergen::Config;
