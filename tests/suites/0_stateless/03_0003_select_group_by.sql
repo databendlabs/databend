0695a49a7d8a46d2e67695ff3957b20e97be148e
leiysky <leiysky@outlook.com>
2021-05-19 13:56:48 +0800

    fix tests

--- tests/suites/0_stateless/03_0003_select_group_by.sql
+++ tests/suites/0_stateless/03_0003_select_group_by.sql
@@ -1,10 +1,10 @@
 SELECT max(number) from numbers_mt(0) group by number % 4;
 SELECT max(number) FROM numbers_mt (10) where number > 99999999998 GROUP BY number%3;
 SELECT avg(number), max(number+1)+1 FROM numbers_mt(10000) where number > 2 GROUP BY 1;
-SELECT number%3 as c1, number%2 as c2 FROM numbers_mt(10000) where number > 2 group by c1, c2 order by c1,c2;
+SELECT number%3 as c1, number%2 as c2 FROM numbers_mt(10000) where number > 2 group by number%3, number%2 order by c1,c2;
 
 SELECT number%3 as c1 FROM numbers_mt(10) where number > 2 group by number order by c1;
-SELECT number%3 as c1 FROM numbers_mt(10) where number > 2 group by c1 order by c1;
+SELECT number%3 as c1 FROM numbers_mt(10) where number > 2 group by number%3 order by c1;
 
 SELECT 'NOT in GROUP BY function check';
 SELECT number%3 as c1, number as c2 FROM numbers_mt(10) where number > 2 group by c1 order by c1;
