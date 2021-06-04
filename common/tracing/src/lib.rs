130d5fac84071ea96cb7d4c4b42e1562a3aaf52f
BohuTANG <overred.shuttler@gmail.com>
2021-06-04 16:53:39 +0800

    [common/tracing] add tokio::tracing to common/tracing

---
+++ common/tracing/src/lib.rs
@@ -0,0 +1,8 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-License-Identifier: Apache-2.0.
+
+mod logging;
+
+pub use logging::init_simple_tracing;
+pub use tracing;
--- fusequery/query/src/interpreters/interpreter_database_create_test.rs
+++ fusequery/query/src/interpreters/interpreter_database_create_test.rs
@@ -11,6 +11,8 @@ async fn test_create_database_interpreter() -> anyhow::Result<()> {
     use crate::interpreters::*;
     use crate::sql::*;
 
+    common_tracing::init_simple_tracing();
+
     let ctx = crate::tests::try_create_context()?;
 
     if let PlanNode::CreateDatabase(plan) =
