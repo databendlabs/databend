f78eed8aac1ed39552383e3c86c24f883c941dce
drdr xp <drdr.xp@gmail.com>
2021-06-04 15:39:00 +0800

    [store] dfs checks meta data first when reading.
    - [common/exceptions] add 2 store errors.
    - [store] `read_all()` returns ErrorCodes instead of anyhow::Error.
    - [store] `read_all()` check file meta before returning file content.

--- fusestore/store/src/dfs/distributed_fs_test.rs
+++ fusestore/store/src/dfs/distributed_fs_test.rs
@@ -14,7 +14,12 @@ use crate::meta_service::MetaServiceClient;
 use crate::tests::rand_local_addr;
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
-async fn test_distributed_fs() -> anyhow::Result<()> {
+async fn test_distributed_fs_single_node() -> anyhow::Result<()> {
+    // - Brings a single node dfs online.
+    // - Write a file.
+    // - Test read_all()
+    // - Test reading an absent file.
+
     let dir = tempdir()?;
     let root = dir.path();
 
@@ -30,6 +35,7 @@ async fn test_distributed_fs() -> anyhow::Result<()> {
     {
         let rst = dfs.add("foo".into(), "bar".as_bytes()).await;
         rst.unwrap();
+
         // check meta changes
 
         let mut client = MetaServiceClient::connect(format!("http://{}", meta_addr)).await?;
@@ -37,7 +43,19 @@ async fn test_distributed_fs() -> anyhow::Result<()> {
         let rst = client.get(req).await?.into_inner();
         assert_eq!("", rst.value);
 
-        // TODO read file data and check result.
+        // read file and check
+
+        let got = dfs.read_all("foo".into()).await?;
+        assert_eq!("bar".to_string().as_bytes(), got);
+
+        let got = dfs.read_all("absent".into()).await;
+        assert!(got.is_err());
+        assert_eq!(
+            "dfs/meta: key not found: \"absent\"",
+            got.unwrap_err().message()
+        );
+
+        // TODO: test a file presents in meta but not found on local fs.
     }
     Ok(())
 }
