7525636dd62308f785e2b4d7193be953c7738cc3
drdr xp <drdr.xp@gmail.com>
2021-06-04 22:28:22 +0800

    [store/meta] refine: the same test cases should be applied to both state-machine and the meta server
    The meta server is no more than a state-machine with log-management.
    Thus the command sequence applies to state-machine should also applies
    to the meta server, and both result in the same state.
    
    The common test cases are extracted and are shared by both sets of
    tests.

---
+++ fusestore/store/src/meta_service/state_machine_test.rs
@@ -0,0 +1,106 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-License-Identifier: Apache-2.0.
+use std::sync::Arc;
+
+use async_raft::RaftMetrics;
+use async_raft::State;
+use maplit::hashset;
+use pretty_assertions::assert_eq;
+use tokio::sync::watch::Receiver;
+use tokio::time::Duration;
+
+use crate::meta_service::ClientRequest;
+use crate::meta_service::ClientResponse;
+use crate::meta_service::Cmd;
+use crate::meta_service::GetReq;
+use crate::meta_service::MemStoreStateMachine;
+use crate::meta_service::MetaNode;
+use crate::meta_service::MetaServiceClient;
+use crate::meta_service::NodeId;
+use crate::meta_service::RaftTxId;
+use crate::tests::Seq;
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+async fn test_state_machine_apply_add_file() -> anyhow::Result<()> {
+    crate::tests::init_tracing();
+
+    let mut sm = MemStoreStateMachine::default();
+
+    let cases = crate::meta_service::raftmeta_test::cases_add_file();
+
+    for (name, txid, k, v, want_prev, want_result) in cases.iter() {
+        let resp = sm.apply(5, &ClientRequest {
+            txid: txid.clone(),
+            cmd: Cmd::AddFile {
+                key: k.to_string(),
+                value: v.to_string(),
+            },
+        });
+        assert_eq!(
+            ClientResponse::String {
+                prev: want_prev.clone(),
+                result: want_result.clone()
+            },
+            resp.unwrap(),
+            "{}",
+            name
+        );
+    }
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+async fn test_state_machine_apply_set_file() -> anyhow::Result<()> {
+    crate::tests::init_tracing();
+
+    let mut sm = MemStoreStateMachine::default();
+
+    let cases = crate::meta_service::raftmeta_test::cases_set_file();
+
+    for (name, txid, k, v, want_prev, want_result) in cases.iter() {
+        let resp = sm.apply(5, &ClientRequest {
+            txid: txid.clone(),
+            cmd: Cmd::SetFile {
+                key: k.to_string(),
+                value: v.to_string(),
+            },
+        });
+        assert_eq!(
+            ClientResponse::String {
+                prev: want_prev.clone(),
+                result: want_result.clone()
+            },
+            resp.unwrap(),
+            "{}",
+            name
+        );
+    }
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+async fn test_state_machine_apply_incr_seq() -> anyhow::Result<()> {
+    crate::tests::init_tracing();
+
+    let mut sm = MemStoreStateMachine::default();
+
+    let cases = crate::meta_service::raftmeta_test::cases_incr_seq();
+
+    for (name, txid, k, want) in cases.iter() {
+        let resp = sm.apply(5, &ClientRequest {
+            txid: txid.clone(),
+            cmd: Cmd::IncrSeq { key: k.to_string() },
+        });
+        assert_eq!(
+            ClientResponse::Seq { seq: *want },
+            resp.unwrap(),
+            "{}",
+            name
+        );
+    }
+
+    Ok(())
+}
