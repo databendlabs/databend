6dfe101d863f45aa2edd60c8d8550805d8991019
yizheng <yizheng@cs.unc.edu>
2021-06-09 20:05:47 -0400

    gc code

--- common/planners/src/plan_filter.rs
+++ common/planners/src/plan_filter.rs
@@ -8,7 +8,6 @@ use common_datavalues::DataSchemaRef;
 
 use crate::Expression;
 use crate::PlanNode;
-use crate::find_exists_exprs;
 
 #[derive(serde::Serialize, serde::Deserialize, Clone, PartialEq)]
 pub struct FilterPlan {
@@ -25,19 +24,5 @@ impl FilterPlan {
 
     pub fn set_input(&mut self, node: &PlanNode) {
         self.input = Arc::new(node.clone());
-    }
-
-    pub fn get_inputs_from_expr(&self) -> Option<Vec<Arc<PlanNode>>> {
-        let expr_vec = find_exists_exprs(&[self.predicate.clone()]);
-        if expr_vec.len() == 0 {
-            return None;
-        }
-        let mut vec = Vec::new();
-        for e in expr_vec {
-            if let Expression::Exists(p) = e {
-                vec.push(p.clone());
-            }
-        }
-        return Some(vec);
     }
 }
