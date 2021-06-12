ebc803664f2be2783328625485ead4017a50f8f6
BohuTANG <overred.shuttler@gmail.com>
2021-06-12 22:14:08 +0800

    [api/pprof] add http debug/pprof/profile api

--- common/profiling/src/profling.rs
+++ common/profiling/src/profling.rs
@@ -6,20 +6,23 @@ use std::time::Duration;
 
 use common_exception::ErrorCode;
 use common_exception::Result;
-//use pprof::protos::Message;
+use pprof::protos::Message;
 
 pub struct Profiling {
     duration: Duration,
+    frequency: i32,
 }
 
 impl Profiling {
-    pub fn create(duration: Duration) -> Self {
-        Self { duration }
+    pub fn create(duration: Duration, frequency: i32) -> Self {
+        Self {
+            duration,
+            frequency,
+        }
     }
 
     pub async fn report(&self) -> Result<pprof::Report> {
-        // 99 HZ
-        let guard = pprof::ProfilerGuard::new(99)
+        let guard = pprof::ProfilerGuard::new(self.frequency)
             .map_err(|e| ErrorCode::UnknownException(e.to_string()))?;
         tokio::time::sleep(self.duration).await;
         guard
@@ -28,7 +31,7 @@ impl Profiling {
             .map_err(|e| ErrorCode::UnknownException(e.to_string()))
     }
 
-    pub async fn dump(&self) -> Result<Vec<u8>> {
+    pub async fn dump_flamegraph(&self) -> Result<Vec<u8>> {
         let mut body: Vec<u8> = Vec::new();
 
         let report = self.report().await?;
@@ -44,6 +47,20 @@ impl Profiling {
             .map_err(|e| ErrorCode::UnknownException(e.to_string()))?;
 
          */
+
+        Ok(body)
+    }
+
+    pub async fn dump_proto(&self) -> Result<Vec<u8>> {
+        let mut body: Vec<u8> = Vec::new();
+
+        let report = self.report().await?;
+        let profile = report
+            .pprof()
+            .map_err(|e| ErrorCode::UnknownException(e.to_string()))?;
+        profile
+            .encode(&mut body)
+            .map_err(|e| ErrorCode::UnknownException(e.to_string()))?;
 
         Ok(body)
     }
--- fusequery/query/src/api/http/debug/pprof.rs
+++ fusequery/query/src/api/http/debug/pprof.rs
@@ -2,15 +2,36 @@
 //
 // SPDX-License-Identifier: Apache-2.0.
 
+use std::num::NonZeroI32;
+
 use warp::Filter;
 
 use crate::configs::Config;
 
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+pub struct PProfRequest {
+    #[serde(default = "PProfRequest::default_seconds")]
+    seconds: u64,
+    #[serde(default = "PProfRequest::default_frequency")]
+    frequency: NonZeroI32,
+}
+
+impl PProfRequest {
+    fn default_seconds() -> u64 {
+        30
+    }
+    fn default_frequency() -> NonZeroI32 {
+        NonZeroI32::new(99).unwrap()
+    }
+}
+
 pub fn pprof_handler(
     _cfg: Config,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path!("debug" / "pprof")
+    warp::path!("debug" / "pprof" / "profile")
         .and(warp::get())
+        .and(warp::header::<String>("Accept"))
+        .and(warp::query::<PProfRequest>())
         .and_then(handlers::pprof)
 }
 
@@ -19,10 +40,21 @@ mod handlers {
 
     use common_profling::Profiling;
 
-    pub async fn pprof() -> Result<impl warp::Reply, std::convert::Infallible> {
-        let duration = Duration::from_secs(10);
-        let profile = Profiling::create(duration);
-        let body = profile.dump().await.unwrap();
+    use crate::api::http::debug::pprof::PProfRequest;
+
+    pub async fn pprof(
+        header: String,
+        req: PProfRequest,
+    ) -> Result<impl warp::Reply, std::convert::Infallible> {
+        let body;
+        let duration = Duration::from_secs(req.seconds);
+        let profile = Profiling::create(duration, req.frequency.get());
+        // header:"text/html,..."
+        if header.contains("text/html") {
+            body = profile.dump_flamegraph().await.unwrap();
+        } else {
+            body = profile.dump_proto().await.unwrap();
+        }
         Ok(warp::reply::html(body))
     }
 }
