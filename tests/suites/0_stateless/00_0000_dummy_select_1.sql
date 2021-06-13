9e028ee21044de2eeca39e6c12031af433133c49
yizheng <yizheng@cs.unc.edu>
2021-06-12 20:38:38 -0400

    fix stateless unit test

--- tests/suites/0_stateless/00_0000_dummy_select_1.sql
+++ tests/suites/0_stateless/00_0000_dummy_select_1.sql
@@ -7,12 +7,13 @@ SELECT NOT(1=1) from numbers(3);
 SELECT TRUE;
 SELECT FALSE;
 SELECT NOT(TRUE);
+select number from numbers_mt(10) where number > 5  and exists (select name from system.settings);
+select number from numbers_mt(10) where number > 5  and exists (select name from system.settings) and exists (select number from numbers_mt(10));
+select number from numbers_mt(10) where number > 5  and exists (select name from system.settings where exists (select number from numbers_mt(10)));
 SELECT a.number FROM numbers(3) AS a order by a.number;
 SELECT a.number FROM (SELECT * FROM numbers(3) AS b ORDER BY b.number) AS a;
 SELECT b.number FROM numbers(3) AS a ORDER BY a.number;
 SELECT a.number FROM numbers(3) AS a ORDER BY b.number;
 SELECT b.number FROM (SELECT * FROM numbers(3) AS b ORDER BY a.number) AS a;
 SELECT b.number FROM (SELECT * FROM numbers(3) AS b) AS a ORDER BY b.number;
-select number from numbers_mt(10) where number > 5  and exists (select name from system.settings);
-select number from numbers_mt(10) where number > 5  and exists (select name from system.settings) and exists (select number from numbers_mt(10));
-select number from numbers_mt(10) where number > 5  and exists (select name from system.settings where exists (select number from numbers_mt(10)));
+
