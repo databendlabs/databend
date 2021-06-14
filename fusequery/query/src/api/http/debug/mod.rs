04d58a5d939a539ae40763cddec997557debae60
BohuTANG <overred.shuttler@gmail.com>
2021-06-14 17:45:32 +0800

    [api/debug] add debug home

--- fusequery/query/src/api/http/debug/mod.rs
+++ fusequery/query/src/api/http/debug/mod.rs
@@ -2,4 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0.
 
+pub mod home;
 pub mod pprof;
+
+pub use home::PProfRequest;
--- fusequery/query/src/api/http/router.rs
+++ fusequery/query/src/api/http/router.rs
@@ -23,8 +23,8 @@ impl Router {
     ) -> Result<impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone> {
         let v1 = super::v1::hello::hello_handler(self.cfg.clone())
             .or(super::v1::config::config_handler(self.cfg.clone()))
-            .or(super::debug::pprof::pprof_handler(self.cfg.clone()))
-            .or(super::v1::cluster::cluster_handler(self.cluster.clone()));
+            .or(super::v1::cluster::cluster_handler(self.cluster.clone()))
+            .or(super::debug::home::debug_handler(self.cfg.clone()));
         let routes = v1.with(warp::log("v1"));
         Ok(routes)
     }
