6b004b179068fd4e8f5cb40ee5f16e1aa55ddf8b
BohuTANG <overred.shuttler@gmail.com>
2021-06-30 18:30:45 +0800

    ISSUE-938: add os version to --version

--- fusecli/cli/src/cmds/versions/version.rs
+++ fusecli/cli/src/cmds/versions/version.rs
@@ -5,6 +5,7 @@
 use sha2::Digest;
 use sha2::Sha256;
 use structopt::StructOpt;
+use sysinfo::SystemExt;
 
 use crate::error::Result;
 
@@ -21,17 +22,21 @@ impl VersionCommand {
             _ => ("", "", ""),
         };
 
-        self.print_with_width("Datafuse CLI", ver, 20);
-        if let Some(sha) = self.cli_sha() {
-            self.print_with_width("Datafuse CLI SHA256", &sha, 20);
+        self.print("Datafuse CLI", ver);
+        if let Some(sha) = self.cli_sha_info() {
+            self.print("Datafuse CLI SHA256", &sha);
         }
-        self.print_with_width("Git commit", git, 20);
-        self.print_with_width("Build date", ts, 20);
+        self.print("Git commit", git);
+        self.print("Build date", ts);
+
+        if let Some(os) = self.os_info() {
+            self.print("OS version", &os);
+        }
 
         Ok(())
     }
 
-    fn cli_sha(&self) -> Option<String> {
+    fn cli_sha_info(&self) -> Option<String> {
         let path = std::env::current_exe().ok()?;
         let cli_bin = std::fs::read(path).ok()?;
         let mut hasher = Sha256::default();
@@ -40,7 +45,21 @@ impl VersionCommand {
         Some(format!("{:x}", &bin_sha256))
     }
 
-    fn print_with_width(&self, name: &str, version: &str, width: usize) {
+    fn os_info(&self) -> Option<String> {
+        let sys = sysinfo::System::new_all();
+
+        let info = format!(
+            "{} {} (kernel {})",
+            sys.host_name()?,
+            sys.os_version()?,
+            sys.kernel_version()?,
+        );
+
+        Some(info)
+    }
+
+    fn print(&self, name: &str, version: &str) {
+        let width = 20;
         println!("{:width$} : {}", name, version, width = width);
     }
 }
