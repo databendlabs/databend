454a3a7c426a80cd60a8833e72d1ca4e958f6072
BohuTANG <overred.shuttler@gmail.com>
2021-07-02 19:11:35 +0800

    Try to fix cargo clippy error

--- common/allocators/src/lib.rs
+++ common/allocators/src/lib.rs
@@ -2,30 +2,33 @@
 //
 // SPDX-License-Identifier: Apache-2.0.
 
-#[cfg(feature = "jemalloc-alloc")]
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-#[cfg(feature = "tcmalloc-alloc")]
-#[global_allocator]
-static ALLOC: tcmalloc::TCMalloc = tcmalloc::TCMalloc;
-
-#[cfg(feature = "snmalloc-alloc")]
-#[global_allocator]
-static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "jemalloc-alloc")] {
+        #[global_allocator]
+        static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+    }
+    else if #[cfg(feature = "tcmalloc-alloc")] {
+        #[global_allocator]
+        static ALLOC: tcmalloc::TCMalloc = tcmalloc::TCMalloc;
+    }
+    else if #[cfg(feature = "snmalloc-alloc")] {
+        #[global_allocator]
+        static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+    }
+}
 
 // To use the global alloc
 // put let _ = common_allocators::init() in your main method.
 pub fn init() -> String {
     cfg_if::cfg_if! {
         if #[cfg(feature = "jemalloc-alloc")] {
-            return "jemalloc".to_string();
+            "jemalloc".to_string()
         }
         else if #[cfg(feature = "tcmalloc-alloc")] {
-            return "tcmalloc".to_string();
+            "tcmalloc".to_string()
         }
         else if #[cfg(feature = "snmalloc-alloc")] {
-            return "snmalloc".to_string();
+            "snmalloc".to_string()
         }
         else {
             "default".to_string()
