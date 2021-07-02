be201985050944716a956f1d709642e6c766978b
BohuTANG <overred.shuttler@gmail.com>
2021-07-02 18:35:19 +0800

    Fix unit test

--- common/allocators/src/lib.rs
+++ common/allocators/src/lib.rs
@@ -17,12 +17,18 @@ static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 // To use the global alloc
 // put let _ = common_allocators::init() in your main method.
 pub fn init() -> String {
-    #[cfg(feature = "jemalloc-alloc")]
-    return "jemalloc".to_string();
-
-    #[cfg(feature = "tcmalloc-alloc")]
-    return "tcmalloc".to_string();
-
-    #[cfg(feature = "snmalloc-alloc")]
-    return "snmalloc".to_string();
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "jemalloc-alloc")] {
+            return "jemalloc".to_string();
+        }
+        else if #[cfg(feature = "tcmalloc-alloc")] {
+            return "tcmalloc".to_string();
+        }
+        else if #[cfg(feature = "snmalloc-alloc")] {
+            return "snmalloc".to_string();
+        }
+        else {
+            "default".to_string()
+        }
+    }
 }
