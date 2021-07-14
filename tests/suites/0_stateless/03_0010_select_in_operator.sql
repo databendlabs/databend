a7528c381a0625d03e349153d9414274978db08f
yizheng <yizheng@cs.unc.edu>
2021-07-13 23:29:28 -0400

    add not in test

--- tests/suites/0_stateless/03_0010_select_in_operator.sql
+++ tests/suites/0_stateless/03_0010_select_in_operator.sql
@@ -1,2 +1,3 @@
-select number from numbers_mt(10) where number+1 in (2, 3 ,20);
-select number from numbers_mt(10000) where number+1 in (2, 3 ,20, 5000);
+select number from numbers_mt(10) where number+1 in (2, 3 ,20) order by number;
+select number from numbers_mt(10000) where number+1 in (2, 3 ,20, 5000) order by number;
+select number from numbers_mt(10) where number+1 not in (2, 3 ,20, 5000) order by number;
