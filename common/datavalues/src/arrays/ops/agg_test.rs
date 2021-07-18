a33eae6ce03aba7acdca42aea6948a609c63e9f3
yizheng <yizheng@cs.unc.edu>
2021-07-17 22:31:51 -0400

    add unit test for boolean array and utf8 array

--- common/datavalues/src/arrays/ops/agg_test.rs
+++ common/datavalues/src/arrays/ops/agg_test.rs
@@ -6,32 +6,82 @@ use common_exception::Result;
 
 use crate::prelude::*;
 use crate::DFUInt16Array;
+use crate::DFBooleanArray;
+use crate::DFUtf8Array;
 use crate::arrays::ops::agg::ArrayAgg;
 
 #[test]
 fn test_array_agg() -> Result<()> {
     let array = DFUInt16Array::new_from_iter(1u16..4u16);
     let value = [
-        array.sum()?.to_series_with_size(1)?,
-        array.max()?.to_series_with_size(1)?,
-        array.min()?.to_series_with_size(1)?,
-        array.arg_min()?.to_series_with_size(1)?,
-        array.arg_max()?.to_series_with_size(1)?,
+        array.sum()?,
+        array.max()?,
+        array.min()?,
+        array.arg_min()?,
+        array.arg_max()?,
     ];
 
     let expected = [
-        DataValue::UInt16(Some(6)).to_series_with_size(1)?,
-        DataValue::UInt16(Some(3)).to_series_with_size(1)?;
-        DataValue::UInt16(Some(1)).to_series_with_size(1)?;
-        DataValue::UInt16(Some(0)).to_series_with_size(1)?;
-        DataValue::UInt16(Some(2)).to_series_with_size(1)?;
+        DataValue::UInt16(Some(6)),
+        DataValue::UInt16(Some(3)),
+        DataValue::UInt16(Some(1)),
+        DataValue::Struct(vec![DataValue::UInt64(Some(0)), DataValue::UInt16(Some(1))]),
+        DataValue::Struct(vec![DataValue::UInt64(Some(2)), DataValue::UInt16(Some(3))]),
     ];
 
     let len = value.len();
     for i in 0..len {
-        v = value[i];
-        e = expected[i];
-        assert!(v.series_equal(&e));   
+        assert_eq!(value[i], expected[i]);   
+    }
+    Ok(())
+}
+
+#[test]
+fn test_boolean_array_agg() -> Result<()> {
+    let array = DFBooleanArray::new_from_slice(&vec![true, false, true]);
+
+    let value = [
+        array.sum()?,
+        array.max()?,
+        array.min()?,
+        array.arg_min()?,
+        array.arg_max()?,
+    ];
+
+    let expected = [
+        DataValue::UInt32(Some(2)),
+        DataValue::UInt32(Some(1)),
+        DataValue::UInt32(Some(0)),
+        DataValue::Struct(vec![DataValue::UInt64(Some(1)), DataValue::Boolean(Some(false))]),
+        DataValue::Struct(vec![DataValue::UInt64(Some(0)), DataValue::Boolean(Some(true))]),
+    ];
+    let len = value.len();
+    for i in 0..len {
+        assert_eq!(value[i], expected[i]);   
+    }
+    Ok(())
+}
+
+#[test]
+fn test_utf8_array_agg() -> Result<()> {
+    let array = DFUtf8Array::new_from_slice(&vec!["h", "e", "l", "o"]);
+
+    let value = [
+        array.max()?,
+        array.min()?,
+        array.arg_min()?,
+        array.arg_max()?,
+    ];
+
+    let expected = [
+        DataValue::Utf8(Some("o".to_string())),
+        DataValue::Utf8(Some("e".to_string())),
+        DataValue::Struct(vec![DataValue::UInt64(Some(1)), DataValue::Utf8(Some("e".to_string()))]),
+        DataValue::Struct(vec![DataValue::UInt64(Some(3)), DataValue::Utf8(Some("o".to_string()))]),
+    ];
+    let len = value.len();
+    for i in 0..len {
+        assert_eq!(value[i], expected[i]);   
     }
     Ok(())
 }
