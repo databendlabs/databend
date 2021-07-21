ee02c5eb6cf385610aeaf6c1a198db11a4bcb835
yizheng <yizheng@cs.unc.edu>
2021-07-21 12:04:24 -0400

    add as_ref test for listarray and binaryarray

--- common/datavalues/src/arrays/ops/downcast_test.rs
+++ common/datavalues/src/arrays/ops/downcast_test.rs
@@ -2,21 +2,22 @@
 //
 // SPDX-License-Identifier: Apache-2.0.
 
-use common_exception::Result;
-use common_arrow::arrow::array::PrimitiveArray;
 use common_arrow::arrow::array::BooleanArray;
+use common_arrow::arrow::array::PrimitiveArray;
+use common_arrow::arrow::buffer::Buffer;
+use common_exception::Result;
+
 use crate::prelude::*;
-use crate::DFUInt16Array;
 use crate::DFBooleanArray;
+use crate::DFUInt16Array;
 use crate::UInt16Type;
 use crate::UInt8Type;
-use common_arrow::arrow::buffer::Buffer;
 
 #[test]
 fn test_array_as_ref() -> Result<()> {
     // Test DFUint16Array
     let df_uint16_array = DFUInt16Array::new_from_iter(1u16..4u16);
-    let arrow_uint16_array : &PrimitiveArray<UInt16Type> = df_uint16_array.as_ref();
+    let arrow_uint16_array: &PrimitiveArray<UInt16Type> = df_uint16_array.as_ref();
     assert_eq!(&[1u16, 2, 3], &arrow_uint16_array.values());
 
     // Test DFBooleanArray
@@ -25,7 +26,7 @@ fn test_array_as_ref() -> Result<()> {
     boolean_builder.append_value(false);
     boolean_builder.append_value(true);
     let df_boolean_array = boolean_builder.finish();
-    let arrow_boolean_array : &BooleanArray = df_boolean_array.as_ref();
+    let arrow_boolean_array: &BooleanArray = df_boolean_array.as_ref();
     // 5 means 0b_101
     assert_eq!(&[5], &arrow_boolean_array.values().as_slice());
 
@@ -36,7 +37,27 @@ fn test_array_as_ref() -> Result<()> {
     utf8_builder.append_value("3c");
     let df_utf8_array = utf8_builder.finish();
     let arrow_string_array = df_utf8_array.as_ref();
-    assert_eq!(&"1a2b3c".as_bytes(), &arrow_string_array.value_data().as_slice());
+    assert_eq!(
+        &"1a2b3c".as_bytes(),
+        &arrow_string_array.value_data().as_slice()
+    );
+
+    // Test DFListArray
+    let mut list_value_builder = ArrowPrimitiveArrayBuilder::<UInt16Type>::new(3);
+    list_value_builder.append_slice(&[1u16, 2u16, 3u16]);
+    let mut list_builder = ListPrimitiveArrayBuilder::<UInt16Type>::new(list_value_builder, 3);
+    let df_list = list_builder.finish();
+    let arrow_list = df_list.as_ref();
+    println!("value-{:?}", arrow_list.values());
+
+    // Test DFBinaryArray
+    let mut binary_builder = BinaryArrayBuilder::new(8);
+    binary_builder.append_value(&[1u8, 2u8, 3u8]);
+    let mut df_binary_array = binary_builder.finish();
+    let arrow_binary_array = df_binary_array.as_ref();
+    assert_eq!(&[5], &arrow_binary_array.values().as_slice());
+
+    // TODO: Test DFStructArray
 
     Ok(())
 }
