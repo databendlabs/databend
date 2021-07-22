0a4f6bd6fc42199eedc048ea440ce9e2ac8b8b2f
jyizheng <jyizheng@gmail.com>
2021-07-22 00:55:37 -0400

    add test_array_fill test

---
+++ common/datavalues/src/arrays/ops/fill_test.rs
@@ -0,0 +1,23 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-License-Identifier: Apache-2.0.
+
+use crate::arrays::builders::*;
+use crate::prelude::*;
+use crate::DFBinaryArray;
+use crate::DFBooleanArray;
+use crate::DFListArray;
+use crate::DFStructArray;
+use crate::DFUInt16Array;
+use crate::DFUtf8Array;
+use crate::Int32Type;
+use crate::UInt16Type;
+
+#[test]
+fn test_array_fill() -> Result<()> {
+    let df_uint16_array = DFUInt16Array::fill(5u16, 3);
+    let arrow_uint16_array: &PrimitiveArray<UInt16Type> = df_uint16_array.as_ref();
+    assert_eq!(&[5u16, 5u16, 5u16], &arrow_uint16_array.values());
+
+    Ok(())
+}
