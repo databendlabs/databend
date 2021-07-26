5894e3b0e0da44ef96691b9fe0a6330a5e4ba16f
Yizheng Jiao <jyizheng@gmail.com>
2021-07-26 04:25:25 +0100

    add group_hash_test

---
+++ common/datavalues/src/arrays/ops/group_hash_test.rs
@@ -0,0 +1,26 @@
+// Copyright 2020-2021 The Datafuse Authors.
+//
+// SPDX-License-Identifier: Apache-2.0.
+
+use crate::DFUInt16Array;
+use common_exception::Result;
+use crate::arrays::builders::*;
+use crate::prelude::*;
+use crate::arrays::ops::group_hash::GroupHash;
+
+#[test]
+fn test_group_hash() -> Result<()> {
+    // Create DFUint16Array
+    let df_uint16_array = DFUInt16Array::new_from_iter(1u16..4u16);
+    // Create a buffer 
+    let buffer = Box::new([0u16, 0, 0]);
+    let ptr: *const u16 = &*buffer;
+
+    df_unit16_array.group_hash(ptr as usize, u16::BITS >> 3);
+
+    assert_eq!(buffer[0], 1);
+    assert_eq!(buffer[1], 2);
+    assert_eq!(buffer[2], 3);
+
+    return Ok(())
+}
--- common/datavalues/src/arrays/ops/mod.rs
+++ common/datavalues/src/arrays/ops/mod.rs
@@ -26,6 +26,9 @@ mod cast_test;
 mod downcast_test;
 #[cfg(test)]
 mod fill_test;
+#[cfg(test)]
+mod group_hash_test;
+
 
 pub use agg::*;
 pub use apply::*;
