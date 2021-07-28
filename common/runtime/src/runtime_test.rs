eb1be55c22e63cd4e3cfc78e760935c6dcaed889
BohuTANG <overred.shuttler@gmail.com>
2021-07-28 09:48:23 +0800

    Fix cargo lint

--- common/runtime/src/runtime_test.rs
+++ common/runtime/src/runtime_test.rs
@@ -2,10 +2,13 @@
 //
 // SPDX-License-Identifier: Apache-2.0.
 
-use common_exception::{Result, ErrorCode};
-use std::rc::Rc;
 use std::cell::RefCell;
-use std::sync::{Mutex, Arc};
+use std::rc::Rc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+use common_exception::ErrorCode;
+use common_exception::Result;
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
 async fn test_runtime() -> Result<()> {
@@ -16,15 +19,12 @@ async fn test_runtime() -> Result<()> {
     let runtime = Runtime::with_default_worker_threads()?;
     let runtime_counter = Arc::clone(&counter);
     let runtime_header = runtime.spawn(async move {
-
         let rt1 = Runtime::with_default_worker_threads().unwrap();
         let rt1_counter = Arc::clone(&runtime_counter);
         let rt1_header = rt1.spawn(async move {
-
             let rt2 = Runtime::with_worker_threads(1).unwrap();
             let rt2_counter = Arc::clone(&rt1_counter);
             let rt2_header = rt2.spawn(async move {
-
                 let rt3 = Runtime::with_default_worker_threads().unwrap();
                 let rt3_counter = Arc::clone(&rt2_counter);
                 let rt3_header = rt3.spawn(async move {
