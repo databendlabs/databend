52f3655933e289a29bf2f7f0fd6628e643eef924
zhyass <34016424+zhyass@users.noreply.github.com>
2021-07-28 10:54:58 +0800

    bug fix: clean data parts while dropping database

--- fusequery/query/src/datasources/remote/remote_database.rs
+++ fusequery/query/src/datasources/remote/remote_database.rs
@@ -53,7 +53,10 @@ impl Database for RemoteDatabase {
     fn get_table(&self, _table_name: &str) -> Result<Arc<TableMeta>> {
         match self.tables.read().name2meta.get(_table_name) {
             Some(tbl) => Ok(tbl.clone()),
-            None => Err(ErrorCode::UnknownTable(_table_name)),
+            None => Err(ErrorCode::UnknownTable(format!(
+                "Unknown table: '{}'",
+                _table_name
+            ))),
         }
     }
 
