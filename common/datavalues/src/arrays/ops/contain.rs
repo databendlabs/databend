b514ccc5f48c80925a7cae58c0f390c6e416adb8
yizheng <yizheng@cs.unc.edu>
2021-08-11 13:33:21 -0400

    duplicate list inside contain function

--- common/datavalues/src/arrays/ops/contain.rs
+++ common/datavalues/src/arrays/ops/contain.rs
@@ -10,6 +10,10 @@ use std::hash::Hasher;
 use common_exception::ErrorCode;
 use common_exception::Result;
 
+use crate::series::IntoSeries;
+use crate::series::Series;
+use common_arrow::arrow::array::ArrayRef;
+
 use crate::arrays::get_list_builder;
 use crate::arrays::BinaryArrayBuilder;
 use crate::arrays::BooleanArrayBuilder;
@@ -45,7 +49,22 @@ where T: DFNumericType
     where Self: std::marker::Sized,
     {
         let arrow_array = self.downcast_ref();
-        let arrow_list = list.downcast_ref();
+
+        // List's length must be 1. Need to create
+        assert_eq!(list.len(), 1);
+
+        // a new ListArray whose length is equal to self.len()
+        // Create a new ListArray to Arrow API
+        let mut builder = get_list_builder(&list.sub_data_type(), 3, 3);
+        let arr_ref: ArrayRef = Arc::from(list.downcast_ref().value(0));
+        let series = arr_ref.into_series();
+        for i in 0..self.len() {
+            builder.append_series(&series);
+        }
+        let df_list = builder.finish();
+        let arrow_list = df_list.downcast_ref();
+
+        // Call arrow2 API
         let arrow_res = contains(arrow_list, arrow_array)?;
         Ok(DFBooleanArray::from_arrow_array(arrow_res)) 
     }
--- common/datavalues/src/arrays/ops/contain_test.rs
+++ common/datavalues/src/arrays/ops/contain_test.rs
@@ -4,6 +4,7 @@
 
 use std::borrow::Cow;
 
+use common_arrow::arrow::array::ArrayRef;
 use common_arrow::arrow::array::Array;
 use common_arrow::arrow::compute::arithmetics::basic::add;
 use common_arrow::arrow::compute::boolean::and;
@@ -13,19 +14,19 @@ use common_exception::Result;
 use crate::arrays::ops::contain::ArrayContain;
 use crate::arrays::DataArray;
 use crate::prelude::*;
+use crate::series::IntoSeries;
+use crate::series::Series;
 
 #[test]
 fn test_contain() -> Result<()> {
     // Create DFUint16Array
     let df_uint16_array = &DFUInt16Array::new_from_iter(1u16..4u16);
     // Create ListArray
-    let mut builder = get_list_builder(&DataType::UInt16, 12, 3);
+    let mut builder = get_list_builder(&DataType::UInt16, 3, 1);
     builder.append_series(&Series::new(vec![1_u16, 2, 5]));
-    builder.append_series(&Series::new(vec![1_u16, 2, 5]));
-    builder.append_series(&Series::new(vec![1_u16, 2, 5]));
-    let df_list = &builder.finish();
-    let boolean = df_uint16_array.contain(df_list);
+    let df_list = builder.finish();
 
+    let boolean = df_uint16_array.contain(&df_list);
     let values = boolean?.collect_values();
     assert_eq!(&[Some(true), Some(true), Some(false)], values.as_slice());
 
