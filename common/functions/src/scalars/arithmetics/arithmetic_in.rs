10f2034571fb52dd5c39e7521b2a0e0fbd8fb0ca
jyizheng <jyizheng@gmail.com>
2021-08-15 18:19:38 -0400

    implement in function

---
+++ common/functions/src/scalars/arithmetics/arithmetic_in.rs
@@ -0,0 +1,27 @@
+// Copyright 2020 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use common_datavalues::DataValueArithmeticOperator;
+use common_exception::Result;
+
+use crate::scalars::ArithmeticFunction;
+use crate::scalars::Function;
+
+pub struct ArithmeticInFunction;
+
+impl ArithmeticInFunction {
+    pub fn try_create_func(_display_name: &str) -> Result<Box<dyn Function>> {
+        ArithmeticFunction::try_create_func(DataValueArithmeticOperator::In)
+    }
+}
