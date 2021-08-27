1c27de92c394071640fb5f3465ed09cd76d20022
Yijun Zhao <ariesdevil77@gmail.com>
2021-08-28 00:46:38 +0800

    fix range_remove index in log compaction

--- store/src/meta_service/raftmeta.rs
+++ store/src/meta_service/raftmeta.rs
@@ -454,7 +454,19 @@ impl RaftStorage<LogEntry, AppliedState> for MetaStore {
             .insert(&Entry::new_snapshot_pointer(&snapshot.meta))
             .await?;
 
-        self.log.range_remove(0..last_applied_log.index).await?;
+        let current_snapshot_last_log_index;
+
+        {
+            let current_snapshot = self.current_snapshot.read().await;
+            current_snapshot_last_log_index = match current_snapshot.as_ref() {
+                Some(snap) => snap.meta.last_log_id.index + 1,
+                None => 0,
+            };
+        }
+
+        self.log
+            .range_remove(current_snapshot_last_log_index..last_applied_log.index)
+            .await?;
 
         tracing::debug!("log range_remove complete");
 
