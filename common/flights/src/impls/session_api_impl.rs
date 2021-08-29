009f5aaec5de8dc4af7a13e36925090837e31625
jyz0309 <a1kaid@foxmail.com>
2021-08-29 17:32:26 +0800

    remove session api
    Signed-off-by: jyz0309 <a1kaid@foxmail.com>

--- common/flights/src/impls/session_api_impl.rs
+++ common/flights/src/impls/session_api_impl.rs
@@ -12,25 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use common_exception::Result;
-pub use common_store_api::SessionApi;
-use common_tracing::tracing;
-
 use crate::RequestFor;
-use crate::StoreClient;
 use crate::StoreDoAction;
 
 #[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
 pub struct KillQueryReq {
     pub query_id: String,
-}
-
-#[async_trait::async_trait]
-impl SessionApi for StoreClient {
-    #[tracing::instrument(level = "debug", skip(self))]
-    async fn kill_query(&mut self, query_id: String) -> Result<()> {
-        self.do_action(KillQueryReq { query_id }).await
-    }
 }
 
 impl RequestFor for KillQueryReq {
