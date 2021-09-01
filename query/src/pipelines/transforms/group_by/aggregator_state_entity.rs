9e4aa5f91390c67dfd943071e28c867e4228ff17
zhang2014 <coswde@gmail.com>
2021-09-01 09:41:23 +0800

    Add license and try fix test failure

--- query/src/pipelines/transforms/group_by/aggregator_state_entity.rs
+++ query/src/pipelines/transforms/group_by/aggregator_state_entity.rs
@@ -1,3 +1,17 @@
+// Copyright 2020 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use crate::common::HashTableEntity;
 use crate::common::HashTableKeyable;
 use crate::common::KeyValueEntity;
@@ -20,7 +34,6 @@ pub struct ShortFixedKeysStateEntity<Key: ShortFixedKeyable> {
 }
 
 impl<Key: ShortFixedKeyable> StateEntity<Key> for ShortFixedKeysStateEntity<Key> {
-
     #[inline(always)]
     fn get_state_key<'a>(self: *mut Self) -> &'a Key {
         unsafe { &(*self).key }
--- query/src/pipelines/transforms/group_by/aggregator_state_iterator.rs
+++ query/src/pipelines/transforms/group_by/aggregator_state_iterator.rs
@@ -1,8 +1,19 @@
-use std::intrinsics::unlikely;
+// Copyright 2020 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use crate::pipelines::transforms::group_by::aggregator_state_entity::ShortFixedKeyable;
 use crate::pipelines::transforms::group_by::aggregator_state_entity::ShortFixedKeysStateEntity;
-use crate::pipelines::transforms::group_by::aggregator_state_entity::StateEntity;
 
 type Entities<Key> = *mut ShortFixedKeysStateEntity<Key>;
 
