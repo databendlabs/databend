f23cd55f7373c9ae052e903bb463b17254138011
dantengsky <dantengsky@gmail.com>
2021-09-21 12:27:58 +0800

    fix: incorrect err-code returned for create db
    - EmbededMetaBackend should return DatabaseAlreadyExists instead
      of UnknownDatabase if database already exist
    
    - RemoteMetabackend should evaluate the inner result returned from
      `do_block`
    
    - `create_database` of EmbeddedMetaBackend should acquire a lock
       for both checking and inserting, not a read lock for checking and a
       write lock for inserting

--- tests/suites/0_stateless/05_0001_ddl_create_database.sql
+++ tests/suites/0_stateless/05_0001_ddl_create_database.sql
@@ -5,7 +5,7 @@ CREATE TABLE db.t(c1 int) ENGINE = Null;
 SELECT COUNT(1) from system.tables where name = 't' and database = 'db';
 
 CREATE DATABASE IF NOT EXISTS db ENGINE = default;
-CREATE DATABASE db ENGINE = default; -- {ErrorCode 3}
+CREATE DATABASE db ENGINE = default; -- {ErrorCode 4001}
 
 DROP DATABASE IF EXISTS db;
 
