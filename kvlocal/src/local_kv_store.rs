4944ac60a66bfb04fdbd471285a31a1ae04e2adf
drdr xp <drdr.xp@gmail.com>
2021-09-24 18:18:06 +0800

    [kvlocal] refactor: kv-local no longer depends on metasrv. Oh yeah~
    `query` does not indirectly depend on `metasrv` any more.
    Before this patch in order to use `kvlocal`,  one indirectly depends on
    `metasrv`, which is ugly and dirty.
    
    By extracting `raft-store` into a standalone crate, `kvlocal` is no
    longer depending on `metasrv`.
    
    ```
    cargo tree -i metasrv
    metasrv v0.1.0
    └── databend-store v0.1.0
        └── databend-cli v0.1.0
    ```

--- kvlocal/src/local_kv_store.rs
+++ kvlocal/src/local_kv_store.rs
@@ -22,6 +22,7 @@ use common_metatypes::Cmd;
 use common_metatypes::KVMeta;
 use common_metatypes::MatchSeq;
 use common_metatypes::Operation;
+use common_raft_store::config::RaftConfig;
 use common_raft_store::state_machine::AppliedState;
 use common_raft_store::state_machine::StateMachine;
 use common_runtime::tokio::sync::Mutex;
@@ -33,7 +34,6 @@ use common_store_api::KVApi;
 use common_store_api::PrefixListReply;
 use common_store_api::UpsertKVActionResult;
 use common_tracing::tracing;
-use metasrv::configs;
 
 /// Local storage that provides the API defined by `KVApi`.
 /// It is just a wrapped `StateMachine`, which is the same one used by raft driven meta-store service.
@@ -60,20 +60,18 @@ impl LocalKVStore {
     /// - `databend_store::meta_service::raft_db::init_temp_sled_db`
     #[allow(dead_code)]
     pub async fn new(name: &str) -> common_exception::Result<LocalKVStore> {
-        let mut config = configs::Config::empty();
+        let mut config = RaftConfig::empty();
 
-        config.raft_config.sled_tree_prefix = format!("{}-local-kv-store", name);
+        config.sled_tree_prefix = format!("{}-local-kv-store", name);
 
         if cfg!(target_os = "macos") {
             tracing::warn!("Disabled fsync for meta data tests. fsync on mac is quite slow");
-            config.raft_config.no_sync = true;
+            config.no_sync = true;
         }
 
         Ok(LocalKVStore {
             // StateMachine does not need to be replaced, thus we always use id=0
-            inner: Arc::new(Mutex::new(
-                StateMachine::open(&config.raft_config, 0).await?,
-            )),
+            inner: Arc::new(Mutex::new(StateMachine::open(&config, 0).await?)),
         })
     }
 
