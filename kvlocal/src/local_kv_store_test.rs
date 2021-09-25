32ee263a883e74da5b952cc3af537a68e8d58ee1
zhang2014 <coswde@gmail.com>
2021-09-25 10:22:37 +0800

    Uncomment cluster test

--- kvlocal/src/local_kv_store_test.rs
+++ kvlocal/src/local_kv_store_test.rs
@@ -12,331 +12,331 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// use std::time::SystemTime;
-// use std::time::UNIX_EPOCH;
-//
-// use common_exception::Result;
-// use common_kv_api::KVApi;
-// use common_kv_api::SyncKVApi;
-// use common_kv_api_vo::GetKVActionResult;
-// use common_kv_api_vo::MGetKVActionResult;
-// use common_kv_api_vo::UpsertKVActionResult;
-// use common_metatypes::KVMeta;
-// use common_metatypes::KVValue;
-// use common_metatypes::MatchSeq;
-// use common_runtime::tokio;
-// use common_sled_store::init_temp_sled_db;
-// use common_tracing::tracing;
-//
-// use crate::local_kv_store::LocalKVStore;
-//
-// #[tokio::test]
-// async fn test_local_kv_store() -> Result<()> {
-//     init_testing_sled_db();
-//
-//     let now = SystemTime::now()
-//         .duration_since(UNIX_EPOCH)
-//         .unwrap()
-//         .as_secs();
-//
-//     let api = LocalKVStore::new_temp().await?;
-//
-//     tracing::info!("--- upsert");
-//
-//     let res = api
-//         .upsert_kv(
-//             "upsert-key",
-//             MatchSeq::Any,
-//             Some(b"upsert-value".to_vec()),
-//             None,
-//         )
-//         .await?;
-//
-//     assert_eq!(
-//         UpsertKVActionResult {
-//             prev: None,
-//             result: Some((1, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value".to_vec(),
-//             }))
-//         },
-//         res
-//     );
-//
-//     tracing::info!("--- update meta with mismatching seq");
-//
-//     let res = api
-//         .update_kv_meta(
-//             "upsert-key",
-//             MatchSeq::Exact(10),
-//             Some(KVMeta {
-//                 expire_at: Some(now + 20),
-//             }),
-//         )
-//         .await?;
-//
-//     assert_eq!(
-//         UpsertKVActionResult {
-//             prev: Some((1, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value".to_vec(),
-//             })),
-//             result: Some((1, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value".to_vec(),
-//             }))
-//         },
-//         res,
-//         "unchanged with mismatching seq"
-//     );
-//
-//     tracing::info!("--- update meta with matching seq");
-//
-//     let res = api
-//         .update_kv_meta(
-//             "upsert-key",
-//             MatchSeq::Exact(1),
-//             Some(KVMeta {
-//                 expire_at: Some(now + 20),
-//             }),
-//         )
-//         .await?;
-//
-//     assert_eq!(
-//         UpsertKVActionResult {
-//             prev: Some((1, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value".to_vec(),
-//             })),
-//             result: Some((2, KVValue {
-//                 meta: Some(KVMeta {
-//                     expire_at: Some(now + 20)
-//                 }),
-//                 value: b"upsert-value".to_vec(),
-//             })),
-//         },
-//         res
-//     );
-//
-//     tracing::info!("--- get_kv");
-//
-//     let res = api.get_kv("upsert-key").await?;
-//     assert_eq!(
-//         GetKVActionResult {
-//             result: Some((2, KVValue {
-//                 meta: Some(KVMeta {
-//                     expire_at: Some(now + 20)
-//                 }),
-//                 value: b"upsert-value".to_vec(),
-//             })),
-//         },
-//         res
-//     );
-//
-//     tracing::info!("--- mget_kv");
-//
-//     let _res = api
-//         .upsert_kv(
-//             "upsert-key-2",
-//             MatchSeq::Any,
-//             Some(b"upsert-value-2".to_vec()),
-//             None,
-//         )
-//         .await?;
-//
-//     let res = api
-//         .mget_kv(&[
-//             "upsert-key".to_string(),
-//             "upsert-key-2".to_string(),
-//             "nonexistent".to_string(),
-//         ])
-//         .await?;
-//
-//     assert_eq!(
-//         MGetKVActionResult {
-//             result: vec![
-//                 Some((2, KVValue {
-//                     meta: Some(KVMeta {
-//                         expire_at: Some(now + 20)
-//                     }),
-//                     value: b"upsert-value".to_vec(),
-//                 })),
-//                 Some((3, KVValue {
-//                     meta: None,
-//                     value: b"upsert-value-2".to_vec(),
-//                 })),
-//                 None
-//             ]
-//         },
-//         res
-//     );
-//
-//     tracing::info!("--- prefix_list_kv");
-//
-//     let res = api.prefix_list_kv("upsert-key-").await?;
-//     assert_eq!(
-//         vec![(
-//             "upsert-key-2".to_string(),
-//             (3, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value-2".to_vec(),
-//             })
-//         )],
-//         res
-//     );
-//
-//     Ok(())
-// }
-//
-// #[test]
-// fn sync_test_local_kv_store() -> Result<()> {
-//     init_testing_sled_db();
-//
-//     let now = SystemTime::now()
-//         .duration_since(UNIX_EPOCH)
-//         .unwrap()
-//         .as_secs();
-//
-//     let api = LocalKVStore::sync_new_temp()?;
-//
-//     tracing::info!("--- upsert");
-//
-//     let res = api.sync_upsert_kv(
-//         "upsert-key",
-//         MatchSeq::Any,
-//         Some(b"upsert-value".to_vec()),
-//         None,
-//     )?;
-//
-//     assert_eq!(
-//         UpsertKVActionResult {
-//             prev: None,
-//             result: Some((1, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value".to_vec(),
-//             }))
-//         },
-//         res
-//     );
-//
-//     tracing::info!("--- update meta with mismatching seq");
-//
-//     let res = api.sync_update_kv_meta(
-//         "upsert-key",
-//         MatchSeq::Exact(10),
-//         Some(KVMeta {
-//             expire_at: Some(now + 20),
-//         }),
-//     )?;
-//
-//     assert_eq!(
-//         UpsertKVActionResult {
-//             prev: Some((1, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value".to_vec(),
-//             })),
-//             result: Some((1, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value".to_vec(),
-//             }))
-//         },
-//         res,
-//         "unchanged with mismatching seq"
-//     );
-//
-//     tracing::info!("--- update meta with matching seq");
-//
-//     let res = api.sync_update_kv_meta(
-//         "upsert-key",
-//         MatchSeq::Exact(1),
-//         Some(KVMeta {
-//             expire_at: Some(now + 20),
-//         }),
-//     )?;
-//
-//     assert_eq!(
-//         UpsertKVActionResult {
-//             prev: Some((1, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value".to_vec(),
-//             })),
-//             result: Some((2, KVValue {
-//                 meta: Some(KVMeta {
-//                     expire_at: Some(now + 20)
-//                 }),
-//                 value: b"upsert-value".to_vec(),
-//             })),
-//         },
-//         res
-//     );
-//
-//     tracing::info!("--- get_kv");
-//
-//     let res = api.sync_get_kv("upsert-key")?;
-//     assert_eq!(
-//         GetKVActionResult {
-//             result: Some((2, KVValue {
-//                 meta: Some(KVMeta {
-//                     expire_at: Some(now + 20)
-//                 }),
-//                 value: b"upsert-value".to_vec(),
-//             })),
-//         },
-//         res
-//     );
-//
-//     tracing::info!("--- mget_kv");
-//
-//     let _res = api.sync_upsert_kv(
-//         "upsert-key-2",
-//         MatchSeq::Any,
-//         Some(b"upsert-value-2".to_vec()),
-//         None,
-//     )?;
-//
-//     let res = api.sync_mget_kv(&[
-//         "upsert-key".to_string(),
-//         "upsert-key-2".to_string(),
-//         "nonexistent".to_string(),
-//     ])?;
-//
-//     assert_eq!(
-//         MGetKVActionResult {
-//             result: vec![
-//                 Some((2, KVValue {
-//                     meta: Some(KVMeta {
-//                         expire_at: Some(now + 20)
-//                     }),
-//                     value: b"upsert-value".to_vec(),
-//                 })),
-//                 Some((3, KVValue {
-//                     meta: None,
-//                     value: b"upsert-value-2".to_vec(),
-//                 })),
-//                 None
-//             ]
-//         },
-//         res
-//     );
-//
-//     tracing::info!("--- prefix_list_kv");
-//
-//     let res = api.sync_prefix_list_kv("upsert-key-")?;
-//     assert_eq!(
-//         vec![(
-//             "upsert-key-2".to_string(),
-//             (3, KVValue {
-//                 meta: None,
-//                 value: b"upsert-value-2".to_vec(),
-//             })
-//         )],
-//         res
-//     );
-//
-//     Ok(())
-// }
-//
-// fn init_testing_sled_db() {
-//     let t = tempfile::tempdir().expect("create temp dir to sled db");
-//     init_temp_sled_db(t);
-// }
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+
+use common_exception::Result;
+use common_kv_api::KVApi;
+use common_kv_api::SyncKVApi;
+use common_kv_api_vo::GetKVActionResult;
+use common_kv_api_vo::MGetKVActionResult;
+use common_kv_api_vo::UpsertKVActionResult;
+use common_metatypes::KVMeta;
+use common_metatypes::KVValue;
+use common_metatypes::MatchSeq;
+use common_runtime::tokio;
+use common_sled_store::init_temp_sled_db;
+use common_tracing::tracing;
+
+use crate::local_kv_store::LocalKVStore;
+
+#[tokio::test]
+async fn test_local_kv_store() -> Result<()> {
+    init_testing_sled_db();
+
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let api = LocalKVStore::new_temp().await?;
+
+    tracing::info!("--- upsert");
+
+    let res = api
+        .upsert_kv(
+            "upsert-key",
+            MatchSeq::Any,
+            Some(b"upsert-value".to_vec()),
+            None,
+        )
+        .await?;
+
+    assert_eq!(
+        UpsertKVActionResult {
+            prev: None,
+            result: Some((1, KVValue {
+                meta: None,
+                value: b"upsert-value".to_vec(),
+            }))
+        },
+        res
+    );
+
+    tracing::info!("--- update meta with mismatching seq");
+
+    let res = api
+        .update_kv_meta(
+            "upsert-key",
+            MatchSeq::Exact(10),
+            Some(KVMeta {
+                expire_at: Some(now + 20),
+            }),
+        )
+        .await?;
+
+    assert_eq!(
+        UpsertKVActionResult {
+            prev: Some((1, KVValue {
+                meta: None,
+                value: b"upsert-value".to_vec(),
+            })),
+            result: Some((1, KVValue {
+                meta: None,
+                value: b"upsert-value".to_vec(),
+            }))
+        },
+        res,
+        "unchanged with mismatching seq"
+    );
+
+    tracing::info!("--- update meta with matching seq");
+
+    let res = api
+        .update_kv_meta(
+            "upsert-key",
+            MatchSeq::Exact(1),
+            Some(KVMeta {
+                expire_at: Some(now + 20),
+            }),
+        )
+        .await?;
+
+    assert_eq!(
+        UpsertKVActionResult {
+            prev: Some((1, KVValue {
+                meta: None,
+                value: b"upsert-value".to_vec(),
+            })),
+            result: Some((2, KVValue {
+                meta: Some(KVMeta {
+                    expire_at: Some(now + 20)
+                }),
+                value: b"upsert-value".to_vec(),
+            })),
+        },
+        res
+    );
+
+    tracing::info!("--- get_kv");
+
+    let res = api.get_kv("upsert-key").await?;
+    assert_eq!(
+        GetKVActionResult {
+            result: Some((2, KVValue {
+                meta: Some(KVMeta {
+                    expire_at: Some(now + 20)
+                }),
+                value: b"upsert-value".to_vec(),
+            })),
+        },
+        res
+    );
+
+    tracing::info!("--- mget_kv");
+
+    let _res = api
+        .upsert_kv(
+            "upsert-key-2",
+            MatchSeq::Any,
+            Some(b"upsert-value-2".to_vec()),
+            None,
+        )
+        .await?;
+
+    let res = api
+        .mget_kv(&[
+            "upsert-key".to_string(),
+            "upsert-key-2".to_string(),
+            "nonexistent".to_string(),
+        ])
+        .await?;
+
+    assert_eq!(
+        MGetKVActionResult {
+            result: vec![
+                Some((2, KVValue {
+                    meta: Some(KVMeta {
+                        expire_at: Some(now + 20)
+                    }),
+                    value: b"upsert-value".to_vec(),
+                })),
+                Some((3, KVValue {
+                    meta: None,
+                    value: b"upsert-value-2".to_vec(),
+                })),
+                None
+            ]
+        },
+        res
+    );
+
+    tracing::info!("--- prefix_list_kv");
+
+    let res = api.prefix_list_kv("upsert-key-").await?;
+    assert_eq!(
+        vec![(
+            "upsert-key-2".to_string(),
+            (3, KVValue {
+                meta: None,
+                value: b"upsert-value-2".to_vec(),
+            })
+        )],
+        res
+    );
+
+    Ok(())
+}
+
+#[test]
+fn sync_test_local_kv_store() -> Result<()> {
+    init_testing_sled_db();
+
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let api = LocalKVStore::sync_new_temp()?;
+
+    tracing::info!("--- upsert");
+
+    let res = api.sync_upsert_kv(
+        "upsert-key",
+        MatchSeq::Any,
+        Some(b"upsert-value".to_vec()),
+        None,
+    )?;
+
+    assert_eq!(
+        UpsertKVActionResult {
+            prev: None,
+            result: Some((1, KVValue {
+                meta: None,
+                value: b"upsert-value".to_vec(),
+            }))
+        },
+        res
+    );
+
+    tracing::info!("--- update meta with mismatching seq");
+
+    let res = api.sync_update_kv_meta(
+        "upsert-key",
+        MatchSeq::Exact(10),
+        Some(KVMeta {
+            expire_at: Some(now + 20),
+        }),
+    )?;
+
+    assert_eq!(
+        UpsertKVActionResult {
+            prev: Some((1, KVValue {
+                meta: None,
+                value: b"upsert-value".to_vec(),
+            })),
+            result: Some((1, KVValue {
+                meta: None,
+                value: b"upsert-value".to_vec(),
+            }))
+        },
+        res,
+        "unchanged with mismatching seq"
+    );
+
+    tracing::info!("--- update meta with matching seq");
+
+    let res = api.sync_update_kv_meta(
+        "upsert-key",
+        MatchSeq::Exact(1),
+        Some(KVMeta {
+            expire_at: Some(now + 20),
+        }),
+    )?;
+
+    assert_eq!(
+        UpsertKVActionResult {
+            prev: Some((1, KVValue {
+                meta: None,
+                value: b"upsert-value".to_vec(),
+            })),
+            result: Some((2, KVValue {
+                meta: Some(KVMeta {
+                    expire_at: Some(now + 20)
+                }),
+                value: b"upsert-value".to_vec(),
+            })),
+        },
+        res
+    );
+
+    tracing::info!("--- get_kv");
+
+    let res = api.sync_get_kv("upsert-key")?;
+    assert_eq!(
+        GetKVActionResult {
+            result: Some((2, KVValue {
+                meta: Some(KVMeta {
+                    expire_at: Some(now + 20)
+                }),
+                value: b"upsert-value".to_vec(),
+            })),
+        },
+        res
+    );
+
+    tracing::info!("--- mget_kv");
+
+    let _res = api.sync_upsert_kv(
+        "upsert-key-2",
+        MatchSeq::Any,
+        Some(b"upsert-value-2".to_vec()),
+        None,
+    )?;
+
+    let res = api.sync_mget_kv(&[
+        "upsert-key".to_string(),
+        "upsert-key-2".to_string(),
+        "nonexistent".to_string(),
+    ])?;
+
+    assert_eq!(
+        MGetKVActionResult {
+            result: vec![
+                Some((2, KVValue {
+                    meta: Some(KVMeta {
+                        expire_at: Some(now + 20)
+                    }),
+                    value: b"upsert-value".to_vec(),
+                })),
+                Some((3, KVValue {
+                    meta: None,
+                    value: b"upsert-value-2".to_vec(),
+                })),
+                None
+            ]
+        },
+        res
+    );
+
+    tracing::info!("--- prefix_list_kv");
+
+    let res = api.sync_prefix_list_kv("upsert-key-")?;
+    assert_eq!(
+        vec![(
+            "upsert-key-2".to_string(),
+            (3, KVValue {
+                meta: None,
+                value: b"upsert-value-2".to_vec(),
+            })
+        )],
+        res
+    );
+
+    Ok(())
+}
+
+fn init_testing_sled_db() {
+    let t = tempfile::tempdir().expect("create temp dir to sled db");
+    init_temp_sled_db(t);
+}
