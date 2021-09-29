d05c9789fd2a11bd5b321bd5f3a64186fa4740c5
BohuTANG <overred.shuttler@gmail.com>
2021-09-29 12:21:28 +0800

    DFS-1997: change the query meta servcie to databend-kv

--- scripts/deploy/databend-query-cluster-3-nodes.sh
+++ scripts/deploy/databend-query-cluster-3-nodes.sh
@@ -6,7 +6,13 @@ SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 cd "$SCRIPT_PATH/../.." || exit
 
 killall databend-query
+killall databend-kvsvr
 sleep 1
+
+echo 'Start one databend-kvsvr...'
+nohup target/debug/databend-kv  --log-level=ERROR &
+echo "Waiting on databend-kv 10 seconds..."
+python scripts/ci/wait_tcp.py --timeout 5 --port 28003
 
 echo 'Start DatabendQuery node-1'
 nohup target/debug/databend-query -c scripts/deploy/config/databend-query-node-1.toml &
