a9fd28176f4d431c9713e1e7d744ff58a11a2d1e
drdr xp <drdr.xp@gmail.com>
2021-10-18 01:04:50 +0800

    [query] refactor: remove ExampleDatabase

--- query/src/datasources/database/example/mod.rs
+++ query/src/datasources/database/example/mod.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod example_database;
 mod example_table;
 
-pub use example_database::ExampleDatabase;
 pub use example_table::ExampleTable;
