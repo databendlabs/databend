0a8a9566118d9e0617e28cab42dc5ebdfb5da8bf
Chojan Shang <psiace@outlook.com>
2021-11-05 11:19:48 +0800

    [test] fix import
    Signed-off-by: Chojan Shang <psiace@outlook.com>

--- common/cache/tests/it/cache/lru.rs
+++ common/cache/tests/it/cache/lru.rs
@@ -211,7 +211,7 @@ fn test_metered_cache_oversize() {
 #[cfg(not(target_os = "macos"))]
 #[test]
 fn test_heapsize_cache() {
-    use crate::HeapSize;
+    use common_cache::HeapSize;
 
     let mut cache = LruCache::<&str, (u8, u8, u8), _, _>::with_meter(8, HeapSize);
     cache.put("foo1", (1, 2, 3));
