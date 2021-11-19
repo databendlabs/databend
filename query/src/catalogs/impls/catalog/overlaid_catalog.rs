89a382c5da1b029d667a6b17e25207eb6471e894
Li Yazhou <me.ssword@gmail.com>
2021-11-19 23:34:26 +0800

    refactor: move the same exists_database code to Catalog trait

--- query/src/catalogs/impls/catalog/overlaid_catalog.rs
+++ query/src/catalogs/impls/catalog/overlaid_catalog.rs
@@ -220,17 +220,4 @@ impl Catalog for OverlaidCatalog {
         }
         self.bottom.drop_database(plan).await
     }
-
-    async fn exists_database(&self, db_name: &str) -> common_exception::Result<bool> {
-        match self.get_database(db_name).await {
-            Ok(_) => Ok(true),
-            Err(err) => {
-                if err.code() == ErrorCode::UnknownDatabaseCode() {
-                    Ok(false)
-                } else {
-                    Err(err)
-                }
-            }
-        }
-    }
 }
