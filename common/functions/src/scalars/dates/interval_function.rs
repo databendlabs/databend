f36b0a348c5c3c9ad1ca8cd9de800c5ae732d9ba
sundyli <543950155@qq.com>
2021-11-26 12:27:26 +0800

    Uniform data_type functions

--- common/functions/src/scalars/dates/interval_function.rs
+++ common/functions/src/scalars/dates/interval_function.rs
@@ -73,7 +73,7 @@ where T: IntegerTypedArithmetic + Clone + Sync + Send + 'static
     }
 
     fn return_type(&self, args: &[DataType]) -> Result<DataType> {
-        if is_date_or_date_time(&args[0]) {
+        if args[0].is_date_or_date_time() {
             Ok(args[0].clone())
         } else {
             Ok(args[1].clone())
@@ -100,10 +100,11 @@ where T: IntegerTypedArithmetic + Clone + Sync + Send + 'static
 
         let date_col: &DataColumnWithField;
         let integer_col: &DataColumnWithField;
-        if is_date_or_date_time(columns[0].data_type()) && is_integer(columns[1].data_type()) {
+        if columns[0].data_type().is_date_or_date_time() && columns[1].data_type().is_integer() {
             date_col = &columns[0];
             integer_col = &columns[1];
-        } else if is_date_or_date_time(columns[1].data_type()) && is_integer(columns[0].data_type())
+        } else if columns[1].data_type().is_date_or_date_time()
+            && columns[0].data_type().is_integer()
         {
             date_col = &columns[1];
             integer_col = &columns[0];
@@ -528,8 +529,7 @@ impl IntervalFunctionFactory {
     ) -> Result<(&'a DataColumnWithField, &'a DataColumnWithField)> {
         match op {
             DataValueArithmeticOperator::Plus | DataValueArithmeticOperator::Minus => {
-                if is_integer(col0.data_type()) || matches!(col0.data_type(), DataType::Interval(_))
-                {
+                if col0.data_type().is_integer() || col0.data_type().is_interval() {
                     Ok((col0, col1))
                 } else {
                     Ok((col1, col0))
