f948612f1dc1a156eec98ee8ea395ffd5450834d
sundyli <543950155@qq.com>
2021-11-30 22:00:27 +0800

    Add ignore function which can be used in performance tests

---
+++ common/functions/src/scalars/others/ignore.rs
@@ -0,0 +1,76 @@
+// Copyright 2020 Datafuse Lfloor.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::fmt;
+use std::str;
+
+use common_datavalues::prelude::DataColumn;
+use common_datavalues::prelude::DataColumnsWithField;
+use common_datavalues::DataSchema;
+use common_datavalues::DataType;
+use common_datavalues::DataValue;
+use common_exception::Result;
+
+use crate::scalars::function_factory::FunctionDescription;
+use crate::scalars::function_factory::FunctionFeatures;
+use crate::scalars::Function;
+
+// ignore(...) is a function that takes any arguments, and always returns 0.
+// it can be used in performance tests
+// eg: SELECT count() FROM numbers(1000000000) WHERE NOT ignore( toString(number) );
+#[derive(Clone)]
+pub struct IgnoreFunction {
+    display_name: String,
+}
+
+impl IgnoreFunction {
+    pub fn try_create(display_name: &str) -> Result<Box<dyn Function>> {
+        Ok(Box::new(IgnoreFunction {
+            display_name: display_name.to_string(),
+        }))
+    }
+
+    pub fn desc() -> FunctionDescription {
+        FunctionDescription::creator(Box::new(Self::try_create))
+            .features(FunctionFeatures::default().deterministic())
+    }
+}
+
+impl Function for IgnoreFunction {
+    fn name(&self) -> &str {
+        &*self.display_name
+    }
+
+    fn variadic_arguments(&self) -> Option<(usize, usize)> {
+        Some((0, usize::MAX))
+    }
+
+    fn return_type(&self, _args: &[DataType]) -> Result<DataType> {
+        Ok(DataType::UInt8)
+    }
+
+    fn nullable(&self, _input_schema: &DataSchema) -> Result<bool> {
+        Ok(false)
+    }
+
+    fn eval(&self, _columns: &DataColumnsWithField, input_rows: usize) -> Result<DataColumn> {
+        Ok(DataColumn::Constant(DataValue::UInt8(Some(0)), input_rows))
+    }
+}
+
+impl fmt::Display for IgnoreFunction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.display_name.to_uppercase())
+    }
+}
