7b7172ee89623dec67e659dab9164c65cfb472e2
Xuanwo <github@xuanwo.io>
2021-12-02 19:08:28 +0800

    Update index
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- website/databend/src/pages/index.tsx
+++ website/databend/src/pages/index.tsx
@@ -4,37 +4,36 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/overview/architecture">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <Link
+                    className="button button--secondary button--lg"
+                    to="/overview/building-and-running/">
+                    Quick start
+                </Link>
+                <Link
+                    className="button button--secondary button--lg"
+                    to="/overview/performance/">
+                    Performance
+                </Link>
+            </div>
+        </header>
+    );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`Hello from ${siteConfig.title}`}
+            description={`${siteConfig.title} - ${siteConfig.tagline}`}>
+            <HomepageHeader/>
+        </Layout>
+    );
 }
