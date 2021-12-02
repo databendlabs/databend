67283b53e1ef6eda386d3f1fb25bfd29ea9c8b2d
Mike Shauneu <m.shauneu@gmail.com>
2021-12-02 17:50:50 -0500

    fix tests

--- tests/suites/0_stateless/02_0029_function_strings_field.sql
+++ tests/suites/0_stateless/02_0029_function_strings_field.sql
@@ -1,5 +1,12 @@
-SELECT FIELD(3, 77, 3) from numbers(5);
-SELECT FIELD(3, 77, number+1) from numbers(5);
-SELECT FIELD(number+1, 77, 3) from numbers(5);
-SELECT FIELD(number+1, 77, 3) from numbers(5);
-SELECT FIELD(number, 77, 4-number) from numbers(5);
+-- 2 2 2 2 2
+SELECT '===1===';
+SELECT FIELD(3, 77, 3) FROM numbers(5) ORDER BY number;
+-- 0 0 2 0 0
+SELECT '===2===';
+SELECT FIELD(3, 77, number+1) FROM numbers(5) ORDER BY number;
+-- 0 0 2 0 0
+SELECT '===3===';
+SELECT FIELD(number+1, 77, 3) FROM numbers(5) ORDER BY number;
+-- 0 0 2 0 0
+SELECT '===4===';
+SELECT FIELD(number, 77, 4-number) FROM numbers(5) ORDER BY number;
