e4647dab7645bf8f70a20296d815cba7efc45121
Xuanwo <github@xuanwo.io>
2021-12-02 17:22:28 +0800

    Trigger build
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- website/databend/docusaurus.config.js
+++ website/databend/docusaurus.config.js
@@ -4,6 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'Databend',
