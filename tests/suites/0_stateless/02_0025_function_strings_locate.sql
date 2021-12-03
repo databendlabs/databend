f92ca3e151777303615d4e6ab4f9b5914f205c12
Mike Shauneu <m.shauneu@gmail.com>
2021-12-02 20:48:25 -0500

    test for instr()

--- tests/suites/0_stateless/02_0025_function_strings_locate.sql
+++ tests/suites/0_stateless/02_0025_function_strings_locate.sql
@@ -5,4 +5,7 @@ SELECT LOCATE('bar', 'foobarbar', 5);
 SELECT '===POSITION===';
 SELECT POSITION('bar' IN 'foobarbar');
 SELECT POSITION('xbar' IN 'foobar');
+SELECT '===INSTR===';
+SELECT INSTR('foobarbar', 'bar');
+SELECT INSTR('foobar', 'xbar');
 
