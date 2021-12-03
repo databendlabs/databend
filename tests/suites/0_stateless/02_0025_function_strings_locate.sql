c6dd92c68030b54a6ec76a0b766de8d9b6a255ca
Mike Shauneu <m.shauneu@gmail.com>
2021-12-02 20:21:18 -0500

    merge LOCATE() and POSITION() tests

--- tests/suites/0_stateless/02_0025_function_strings_locate.sql
+++ tests/suites/0_stateless/02_0025_function_strings_locate.sql
@@ -1,3 +1,8 @@
+SELECT '===LOCATE===';
 SELECT LOCATE('bar', 'foobarbar');
 SELECT LOCATE('xbar', 'foobar');
 SELECT LOCATE('bar', 'foobarbar', 5);
+SELECT '===POSITION===';
+SELECT POSITION('bar' IN 'foobarbar');
+SELECT POSITION('xbar' IN 'foobar');
+
