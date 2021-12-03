1a51209ba6b1d654c8a39be5af52279297dfdbb1
unconsolable <chenzhipeng2012@gmail.com>
2021-12-03 17:36:03 +0800

    add octet_length function
    Signed-off-by: unconsolable <chenzhipeng2012@gmail.com>

--- common/functions/src/scalars/strings/string.rs
+++ common/functions/src/scalars/strings/string.rs
@@ -23,6 +23,7 @@ use crate::scalars::InstrFunction;
 use crate::scalars::LTrimFunction;
 use crate::scalars::LocateFunction;
 use crate::scalars::OctFunction;
+use crate::scalars::OctetLengthFunction;
 use crate::scalars::PositionFunction;
 use crate::scalars::QuoteFunction;
 use crate::scalars::RTrimFunction;
@@ -53,5 +54,6 @@ impl StringFunction {
         factory.register("instr", InstrFunction::desc());
         factory.register("insert", InsertFunction::desc());
         factory.register("field", FieldFunction::desc());
+        factory.register("octet_length", OctetLengthFunction::desc());
     }
 }
