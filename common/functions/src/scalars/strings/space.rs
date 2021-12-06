aebe5c16494e52f630358dd578d5f78532245184
Mike Shauneu <m.shauneu@gmail.com>
2021-12-06 00:02:55 -0500

    reuse buffer

--- common/functions/src/scalars/strings/space.rs
+++ common/functions/src/scalars/strings/space.rs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 use std::fmt;
+use std::marker::PhantomData;
 
 use common_datavalues::prelude::*;
 use common_exception::ErrorCode;
@@ -22,15 +23,36 @@ use crate::scalars::function_factory::FunctionDescription;
 use crate::scalars::function_factory::FunctionFeatures;
 use crate::scalars::Function;
 
-#[derive(Clone)]
-pub struct SpaceFunction {
-    display_name: String,
+pub type SpaceFunction = SpaceGenFunction<SpaceGen>;
+
+pub trait SpaceGenOperator: Send + Sync + Clone + Default + 'static {
+    fn apply<'a>(&'a mut self, c: &u64) -> &'a [u8];
 }
 
-impl SpaceFunction {
+#[derive(Clone, Default)]
+pub struct SpaceGen {
+    buf: Vec<u8>,
+}
+
+impl SpaceGenOperator for SpaceGen {
+    #[inline]
+    fn apply<'a>(&'a mut self, c: &u64) -> &'a [u8] {
+        self.buf.resize(*c as usize, 32);
+        &self.buf[..]
+    }
+}
+
+#[derive(Clone)]
+pub struct SpaceGenFunction<T> {
+    display_name: String,
+    _marker: PhantomData<T>,
+}
+
+impl<T: SpaceGenOperator> SpaceGenFunction<T> {
     pub fn try_create(display_name: &str) -> Result<Box<dyn Function>> {
-        Ok(Box::new(SpaceFunction {
+        Ok(Box::new(Self {
             display_name: display_name.to_string(),
+            _marker: PhantomData,
         }))
     }
 
@@ -40,7 +62,7 @@ impl SpaceFunction {
     }
 }
 
-impl Function for SpaceFunction {
+impl<T: SpaceGenOperator> Function for SpaceGenFunction<T> {
     fn name(&self) -> &str {
         &*self.display_name
     }
@@ -67,10 +89,15 @@ impl Function for SpaceFunction {
     }
 
     fn eval(&self, columns: &DataColumnsWithField, input_rows: usize) -> Result<DataColumn> {
+        let mut op = T::default();
+
         let r_column: DataColumn = match columns[0].column().cast_with_type(&DataType::UInt64)? {
             DataColumn::Constant(DataValue::UInt64(c), _) => {
                 if let Some(c) = c {
-                    DataColumn::Constant(DataValue::String(Some(space(&c))), input_rows)
+                    DataColumn::Constant(
+                        DataValue::String(Some(op.apply(&c).to_owned())),
+                        input_rows,
+                    )
                 } else {
                     DataColumn::Constant(DataValue::Null, input_rows)
                 }
@@ -78,7 +105,7 @@ impl Function for SpaceFunction {
             DataColumn::Array(c_series) => {
                 let mut r_array = StringArrayBuilder::with_capacity(input_rows);
                 for oc in c_series.u64()? {
-                    r_array.append_option(oc.map(space));
+                    r_array.append_option(oc.map(|c| op.apply(c)));
                 }
                 r_array.finish().into()
             }
@@ -88,13 +115,8 @@ impl Function for SpaceFunction {
     }
 }
 
-impl fmt::Display for SpaceFunction {
+impl<F> fmt::Display for SpaceGenFunction<F> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.display_name)
     }
-}
-
-#[inline]
-fn space(count: &u64) -> Vec<u8> {
-    vec![32; *count as usize]
 }
