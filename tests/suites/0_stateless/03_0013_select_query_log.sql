b55984276af300d58dd9b1873da463775a830418
BohuTANG <overred.shuttler@gmail.com>
2021-12-09 18:55:55 +0800

    Fix stateless test for query log

--- tests/suites/0_stateless/03_0013_select_query_log.sql
+++ tests/suites/0_stateless/03_0013_select_query_log.sql
@@ -1,3 +1,4 @@
+set max_threads=16;
 truncate table system.query_log;
 select * from numbers(100) where number > 95;
 select log_type, handler_type, cpu_usage, memory_usage, read_rows, read_bytes, written_rows, written_bytes, result_rows, result_bytes, query_kind, query_text, sql_user, sql_user_quota, sql_user_privileges from system.query_log;
