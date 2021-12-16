0e08fae7a1cf428d2725afe93f1cf999ac960381
dantengsky <dantengsky@gmail.com>
2021-12-16 19:45:04 +0800

    fix typo and license format

--- query/src/storages/fuse/operations/truncate_history.rs
+++ query/src/storages/fuse/operations/truncate_history.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 //  Copyright 2021 Datafuse Labs.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +12,8 @@ use std::collections::HashSet;
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
+
+use std::collections::HashSet;
 use std::sync::Arc;
 
 use common_dal::DataAccessor;
