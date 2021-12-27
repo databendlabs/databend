b2f67070a488145677959ff566139cd94379aae4
Hanzhen Liang <handrenliang@gmail.com>
2021-12-27 12:32:46 +0100

    Make lint

--- common/functions/src/udfs/udf_parser.rs
+++ common/functions/src/udfs/udf_parser.rs
@@ -14,10 +14,10 @@
 
 use std::sync::Arc;
 
-use common_exception::ErrorCode;
-use common_exception::Result;
 use common_ast::expr::ExprTraverser;
 use common_ast::expr::ExprVisitor;
+use common_exception::ErrorCode;
+use common_exception::Result;
 use once_cell::sync::Lazy;
 use regex::Regex;
 use sqlparser::ast::Expr;
