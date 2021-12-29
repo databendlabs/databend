d8fa67fb824a0ae7e9edea24e18a02b3ba1da820
Yijun Zhao <ariesdevil77@gmail.com>
2021-12-30 04:01:33 +0800

    add grpc client tests

--- common/meta/raft-store/tests/it/grpc/mod.rs
+++ common/meta/raft-store/tests/it/grpc/mod.rs
@@ -11,3 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+mod grpc_client;
+mod grpc_server;
+
+pub use grpc_server::start_grpc_server;
