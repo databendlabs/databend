44160b6292657dcfb53cb87a1bce36ad1eeb95f5
BohuTANG <overred.shuttler@gmail.com>
2022-01-02 19:47:08 +0800

    Rename common/codegen result_type.rs -> arithmetics_type.rs

--- common/codegen/src/bin/codegen.rs
+++ common/codegen/src/bin/codegen.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use codegen::writes::write_arithmetic_result_type;
+use codegen::writes::codegen_arithmetic_type;
 
 fn main() {
-    write_arithmetic_result_type();
+    codegen_arithmetic_type();
 }
