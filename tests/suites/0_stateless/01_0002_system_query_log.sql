5d9f54ee75c258ea3f67d73ebbb95938a8a15d57
dantengsky <dantengsky@gmail.com>
2022-01-05 16:49:17 +0800

    refine statless test

--- tests/suites/0_stateless/01_0002_system_query_log.sql
+++ tests/suites/0_stateless/01_0002_system_query_log.sql
@@ -4,12 +4,12 @@ create table tbl_01_0002(a int);
 
 -- insert one row, and one partition
 insert into  tbl_01_0002 values(1);
-select written_rows from system.query_log where query_text='insert into  tbl_01_0002 values(1)' order by written_rows;
+select written_rows from system.query_log where query_text='insert into  tbl_01_0002 values(1)' and written_rows != 0;
 
 -- insert another row and partition
 insert into  tbl_01_0002 values(2);
 -- two rows and two partitions will be scanned
 select count(*) from tbl_01_0002;
-select scan_rows, scan_partitions from system.query_log where query_text='select count(*) from tbl_01_0002' order by scan_partitions;
+select scan_rows, scan_partitions from system.query_log where query_text='select count(*) from tbl_01_0002' and scan_partitions != 0;
 
 drop table tbl_01_0002;
