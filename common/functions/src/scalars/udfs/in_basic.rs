056ca25c942f0a652e93d434245f6cb318571a85
谭巍 <931418134@qq.com>
2022-01-10 00:35:35 +0800

    save

--- common/functions/src/scalars/udfs/in_basic.rs
+++ common/functions/src/scalars/udfs/in_basic.rs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 use std::fmt;
+use std::collections::HashSet;
 
 use common_datavalues::columns::DataColumn;
 use common_datavalues::prelude::DataColumnsWithField;
@@ -21,6 +22,7 @@ use common_datavalues::DataTypeAndNullable;
 use common_datavalues::DataValue;
 use common_exception::ErrorCode;
 use common_exception::Result;
+use common_datavalues::prelude::Series;
 
 use crate::scalars::function_factory::FunctionDescription;
 use crate::scalars::function_factory::FunctionFeatures;
@@ -36,7 +38,7 @@ impl<const NEGATED: bool> InFunction<NEGATED> {
 
     pub fn desc() -> FunctionDescription {
         FunctionDescription::creator(Box::new(Self::try_create))
-            .features(FunctionFeatures::default().bool_function().num_arguments(1))
+            .features(FunctionFeatures::default().bool_function().variadic_arguments(2, usize::MAX))
     }
 }
 
@@ -51,6 +53,31 @@ impl<const NEGATED: bool> Function for InFunction<NEGATED> {
     }
 
     fn eval(&self, columns: &DataColumnsWithField, _input_rows: usize) -> Result<DataColumn> {
+        let input_column = columns[0].column();
+        let input_array = match input_column {
+            DataColumn::Array(array) => array.to_owned(),
+            DataColumn::Constant(scalar, _) => scalar.to_array()?,
+        };
+
+        let input_dt = input_array.data_type();
+        common_tracing::tracing::debug!("input_dt: {:#?}", input_dt);
+        let check_arrays = &columns[1..];
+        let mut check_column_set = Vec::new();
+        for column in check_arrays {
+            let array = column.column();
+            let array = match array {
+                DataColumn::Array(_) => {
+                    return Err(ErrorCode::UnexpectedError("logical bug"));
+                },
+                DataColumn::Constant(scalar, _) => {
+                    // cast to DataType::Struct(for tuple struct) will fail, maybe we can consider get into seperate ColumnSet to compare
+                    common_tracing::tracing::debug!("input_dt: {:#?}", scalar.data_type());
+                    scalar.to_array()?.cast_with_type(input_dt)?
+                }
+            };
+            check_column_set.push(ColumnSet::new(&array)?);
+        }
+        
         unimplemented!()
     }
 }
@@ -62,5 +89,23 @@ impl<const NEGATED: bool> fmt::Display for InFunction<NEGATED> {
         }else {
             write!(f, "IN")
         }
+    }
+}
+
+struct ColumnSet {
+    inner: HashSet<DataValue>,
+}
+
+impl ColumnSet {
+    fn new(array: &Series) -> Result<Self> {
+        let mut inner = HashSet::new();
+        for idx in 0..array.len() {
+            let val = array.try_get(idx)?;
+            // f32 and f32 can not impl `Eq` and `Hash`
+            // inner.insert(val);
+        }
+        Ok(Self {
+            inner
+        })
     }
 }
