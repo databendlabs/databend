997678623d0043c78573f118593a9ebc0d9386e8
zhihanz <zzh243402448@gmail.com>
2022-01-14 12:20:56 +0800

    revise based on comment(https://github.com/datafuselabs/databend/pull/3853#discussion_r784489794)

--- scripts/setup/run_docker.sh
+++ scripts/setup/run_docker.sh
@@ -18,7 +18,7 @@ START_COMMAND=("/bin/bash" "-lc" "groupadd --gid $(id -g) -f databendgroup \
   && chown -R databendbuild:databendgroup /source \
   && chown -R databendbuild:databendgroup /opt/rust/cargo \
   && chown -R databendbuild:databendgroup /tmp \
-  && sudo -EHs -u databendbuild bash -c 'cd /source && export LC_NUMERIC='de_DE.UTF-8' && export LC_ALL=C && DATABEND_DEV_CONTAINER=TRUE RUST_BACKTRACE=full env CARGO_HOME=/opt/rust/cargo PATH=/home/rust/.cargo/bin:/opt/rust/cargo/bin:/usr/local/musl/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $*'")
+  && sudo -EHs -u databendbuild bash -c 'cd /source && export LC_NUMERIC='en_US.UTF-8' && export LC_ALL=en_US.UTF-8 && DATABEND_DEV_CONTAINER=TRUE RUST_BACKTRACE=full env CARGO_HOME=/opt/rust/cargo PATH=/home/rust/.cargo/bin:/opt/rust/cargo/bin:/usr/local/musl/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $*'")
 
 [[ -f .git ]] && [[ ! -d .git ]] && DOCKER_OPTIONS+=(-v "$(git rev-parse --git-common-dir):$(git rev-parse --git-common-dir)")
 [[ -n "${SSH_AUTH_SOCK}" ]] && DOCKER_OPTIONS+=(-v "${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK}" -e SSH_AUTH_SOCK)
