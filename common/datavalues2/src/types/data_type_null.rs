283c3a217c7d0fcbf056d81feb50c03e545b6410
sundyli <543950155@qq.com>
2022-01-14 20:01:06 +0800

    Fill TODO

---
+++ common/datavalues2/src/types/data_type_null.rs
@@ -0,0 +1,63 @@
+// Copyright 2021 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::sync::Arc;
+
+use common_arrow::arrow::datatypes::DataType as ArrowType;
+
+use super::data_type::IDataType;
+use crate::prelude::*;
+
+#[derive(Debug, Default, Clone, serde::Deserialize, serde::Serialize)]
+pub struct DataTypeNull {}
+
+#[typetag::serde]
+impl IDataType for DataTypeNull {
+    fn data_type_id(&self) -> TypeID {
+        TypeID::Null
+    }
+
+    #[inline]
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    // it's nothing, so we can't create any default value
+    fn default_value(&self) -> DataValue {
+        DataValue::Null
+    }
+
+    fn create_constant_column(
+        &self,
+        _data: &DataValue,
+        size: usize,
+    ) -> common_exception::Result<ColumnRef> {
+        Ok(Arc::new(NullColumn::new(size)))
+    }
+
+    // DataTypeNull must inside nullable
+    fn arrow_type(&self) -> ArrowType {
+        ArrowType::Null
+    }
+
+    fn create_serializer(&self) -> Box<dyn TypeSerializer> {
+        Box::new(NullSerializer::default())
+    }
+
+    fn create_deserializer(&self, _capacity: usize) -> Box<dyn TypeDeserializer> {
+        Box::new(NullDeserializer {
+            builder: MutableNullColumn::default(),
+        })
+    }
+}
