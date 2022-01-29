81b830d1b189885467d3903bc605921c8458be23
Andy Lok <andylokandy@hotmail.com>
2022-01-30 03:37:45 +0800

    Add utils for sql parser

---
+++ common/ast/src/parser/rule/util.rs
@@ -0,0 +1,83 @@
+// Copyright 2022 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use nom::branch::alt;
+use nom::combinator::map;
+use nom::error::ErrorKind;
+use nom::error::ParseError;
+use nom::IResult;
+
+use crate::parser::ast::Identifier;
+use crate::parser::token::Token;
+use crate::parser::token::TokenKind;
+
+pub type Input<'a> = &'a [Token<'a>];
+
+pub fn satisfy<'a, F, Error>(cond: F) -> impl Fn(Input<'a>) -> IResult<Input<'a>, &'a Token, Error>
+where
+    F: Fn(&Token) -> bool,
+    Error: ParseError<Input<'a>>,
+{
+    move |i| match i.get(0).map(|t| {
+        let b = cond(&t);
+        (t, b)
+    }) {
+        Some((t, true)) => Ok((&i[1..], t)),
+        _ => Err(nom::Err::Error(Error::from_error_kind(
+            i,
+            ErrorKind::Satisfy,
+        ))),
+    }
+}
+
+pub fn match_text<'a, Error>(
+    text: &'static str,
+) -> impl FnMut(Input<'a>) -> IResult<Input<'a>, &'a Token, Error>
+where Error: ParseError<Input<'a>> {
+    move |i| satisfy(|token: &Token| token.text == text)(i)
+}
+
+pub fn match_token<'a, Error>(
+    kind: TokenKind,
+) -> impl FnMut(Input<'a>) -> IResult<Input<'a>, &'a Token, Error>
+where Error: ParseError<Input<'a>> {
+    move |i| satisfy(|token: &Token| token.kind == kind)(i)
+}
+
+pub fn ident<'a, Error>(i: Input<'a>) -> IResult<Input<'a>, Identifier, Error>
+where Error: ParseError<Input<'a>> {
+    alt((
+        map(satisfy(|token| token.kind == TokenKind::Ident), |token| {
+            Identifier {
+                name: token.text.to_string(),
+                quote: None,
+            }
+        }),
+        map(
+            satisfy(|token| token.kind == TokenKind::QuotedIdent),
+            |token| Identifier {
+                name: token.text[1..token.text.len() - 1].to_string(),
+                quote: Some('"'),
+            },
+        ),
+    ))(i)
+}
+
+macro_rules! rule {
+    ($($tt:tt)*) => { nom_rule::rule!(
+        (crate::parser::rule::util::match_text),
+        (crate::parser::rule::util::match_token),
+        $($tt)*)
+    }
+}
---
+++ common/ast/tests/it/rule.rs
@@ -0,0 +1,58 @@
+// Copyright 2022 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use common_ast::parser::rule::statement::*;
+use common_ast::parser::rule::util::Input;
+use common_ast::parser::token::*;
+use nom::Parser;
+use nom_supreme::error::ErrorTree;
+
+#[test]
+fn test_truncate_table() {
+    assert_parse(
+        truncate_table,
+        &tokenise("truncate table a;").unwrap(),
+        "TRUNCATE TABLE a",
+    );
+    assert_parse(
+        truncate_table,
+        &tokenise(r#"truncate table "a".b;"#).unwrap(),
+        r#"TRUNCATE TABLE "a".b"#,
+    );
+}
+
+#[test]
+fn test_drop_table() {
+    assert_parse(
+        drop_table,
+        &tokenise("drop table a;").unwrap(),
+        "DROP TABLE a",
+    );
+    assert_parse(
+        drop_table,
+        &tokenise(r#"drop table if exists a."b";"#).unwrap(),
+        r#"DROP TABLE IF EXISTS a."b""#,
+    );
+}
+
+fn assert_parse<'a, P, Output>(mut parser: P, source: Input<'a>, expected: &str)
+where
+    P: Parser<Input<'a>, Output, ErrorTree<Input<'a>>>,
+    Output: PartialEq + std::fmt::Debug + std::fmt::Display,
+{
+    let (i, output) = parser.parse(source).unwrap();
+
+    assert_eq!(&format!("{}", output), expected);
+    assert_eq!(i, &[]);
+}
