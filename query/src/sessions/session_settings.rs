c23b3fbd212ed04384af5d16f4834e680f1e240f
BohuTANG <overred.shuttler@gmail.com>
2022-02-04 16:50:01 +0800

    Improve the error code and code style

--- query/src/sessions/session_settings.rs
+++ query/src/sessions/session_settings.rs
@@ -77,7 +77,7 @@ impl Settings {
             SettingValue {
                 default_value:DataValue::UInt64(Some(10000)),
                 user_setting: UserSetting::create("max_block_size", DataValue::UInt64(Some(10000))),
-                level:ScopeLevel::Session,
+                level: ScopeLevel::Session,
                 desc: "Maximum block size for reading",
             },
 
@@ -85,7 +85,7 @@ impl Settings {
             SettingValue {
                 default_value:DataValue::UInt64(Some(16)),
                 user_setting: UserSetting::create("max_threads", DataValue::UInt64(Some(16))),
-                level:ScopeLevel::Session,
+                level: ScopeLevel::Session,
                 desc: "The maximum number of threads to execute the request. By default, it is determined automatically.",
             },
 
@@ -93,7 +93,7 @@ impl Settings {
             SettingValue {
                 default_value:DataValue::UInt64(Some(60)),
                 user_setting: UserSetting::create("flight_client_timeout", DataValue::UInt64(Some(60))),
-                level:ScopeLevel::Session,
+                level: ScopeLevel::Session,
                 desc:"Max duration the flight client request is allowed to take in seconds. By default, it is 60 seconds",
             },
 
@@ -101,7 +101,7 @@ impl Settings {
             SettingValue {
                 default_value: DataValue::UInt64(Some(1)),
                 user_setting: UserSetting::create("parallel_read_threads", DataValue::UInt64(Some(1))),
-                level:ScopeLevel::Session,
+                level: ScopeLevel::Session,
                 desc:"The maximum number of parallelism for reading data. By default, it is 1.",
             },
 
@@ -109,7 +109,7 @@ impl Settings {
             SettingValue {
                 default_value: DataValue::UInt64(Some(1024*1024)),
                 user_setting: UserSetting::create("storage_read_buffer_size", DataValue::UInt64(Some(1024*1024))),
-                level:ScopeLevel::Session,
+                level: ScopeLevel::Session,
                 desc:"The size of buffer in bytes for buffered reader of dal. By default, it is 1MB.",
             },
 
@@ -117,7 +117,7 @@ impl Settings {
             SettingValue {
                 default_value: DataValue::UInt64(Some(5)),
                 user_setting: UserSetting::create("storage_occ_backoff_init_delay_ms", DataValue::UInt64(Some(5))),
-                level:ScopeLevel::Session,
+                level: ScopeLevel::Session,
                 desc:"The initial retry delay in millisecond. By default, it is 5 ms.",
             },
 
@@ -125,7 +125,7 @@ impl Settings {
             SettingValue {
                 default_value:DataValue::UInt64(Some(20*1000)),
                 user_setting: UserSetting::create("storage_occ_backoff_max_delay_ms", DataValue::UInt64(Some(20*1000))),
-                level:ScopeLevel::Session,
+                level: ScopeLevel::Session,
                 desc:"The maximum  back off delay in millisecond, once the retry interval reaches this value, it stops increasing. By default, it is 20 seconds.",
             },
 
@@ -133,7 +133,7 @@ impl Settings {
             SettingValue {
                 default_value:DataValue::UInt64(Some(120*1000)),
                 user_setting: UserSetting::create("storage_occ_backoff_max_elapsed_ms", DataValue::UInt64(Some(120*1000))),
-                level:ScopeLevel::Session,
+                level: ScopeLevel::Session,
                 desc:"The maximum elapsed time after the occ starts, beyond which there will be no more retries. By default, it is 2 minutes.",
             },
         ];
