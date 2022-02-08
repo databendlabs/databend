6421055f8c005173fd78642414e6c720f9892098
youngsofun <yangxiufeng.c@gmail.com>
2022-02-08 21:20:58 +0800

    kill query if timeout.

--- query/src/servers/http/v1/query/http_query_manager.rs
+++ query/src/servers/http/v1/query/http_query_manager.rs
@@ -86,6 +86,8 @@ impl HttpQueryManager {
                 }
                 if self_clone.remove_query(&query_id_clone).await.is_none() {
                     tracing::warn!("http query {} timeout", &query_id_clone);
+                } else {
+                    query.kill().await;
                 }
             });
         };
