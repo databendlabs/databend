6ab070fd0b6c629de9ce84898a5066569d9cd3ad
Xuanwo <github@xuanwo.io>
2022-02-09 17:55:19 +0800

    Format code
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- query/src/configs/config.rs
+++ query/src/configs/config.rs
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-
-
 use clap::Parser;
 use common_exception::ErrorCode;
 use common_exception::Result;
