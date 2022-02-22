b1de20babe8bdb750f7d235f6120687b8ca54860
everpcpc <git@everpcpc.com>
2022-02-22 16:25:04 +0800

    z

--- common/datavalues/src/data_value.rs
+++ common/datavalues/src/data_value.rs
@@ -193,7 +193,7 @@ impl DataValue {
     pub fn as_bool(&self) -> Result<bool> {
         match self {
             DataValue::Boolean(v) => Ok(*v),
-            DataValue::UInt64(v) => Ok(*v == 1),
+            DataValue::UInt64(v) => Ok(*v != 0),
             other => Result::Err(ErrorCode::BadDataValueType(format!(
                 "Unexpected type:{:?} to get boolean",
                 other.value_type()
