4287e96acebe13b89f1e69e9e3e59d8910660904
BohuTANG <overred.shuttler@gmail.com>
2022-02-23 15:13:43 +0800

    Add ontime 200lines test dataset

--- tests/suites/1_stateful/00_copy/00_0000_copy_s3.sh
+++ tests/suites/1_stateful/00_copy/00_0000_copy_s3.sh
@@ -4,13 +4,13 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../../../shell_env.sh
 
 ## Create table
-cat $CURDIR/../ontime/create_table.sql | sed 's/ontime/ontime_s3_copy/g' | $MYSQL_CLIENT_CONNECT
+cat $CURDIR/../ontime/create_table.sql | sed 's/ontime/ontime200/g' | $MYSQL_CLIENT_CONNECT
 
 ## Copy from s3.
-echo "copy into ontime_s3_copy from 's3://repo.databend.rs/dataset/stateful/ontime.csv' FILE_FORMAT = (type = 'CSV' field_delimiter = ','  record_delimiter = '\n' skip_header = 1)" | $MYSQL_CLIENT_CONNECT
+echo "copy into ontime200 from 's3://testbucket/tests/data/ontime_200.csv' FILE_FORMAT = (type = 'CSV' field_delimiter = ','  record_delimiter = '\n' skip_header = 1)" | $MYSQL_CLIENT_CONNECT
 
 ## Result.
-echo "select count(1) ,avg(Year), sum(DayOfWeek)  from ontime_s3_copy" | $MYSQL_CLIENT_CONNECT
+echo "select count(1) ,avg(Year), sum(DayOfWeek)  from ontime200" | $MYSQL_CLIENT_CONNECT
 
 ## Drop table.
-echo "drop table ontime_s3_copy" | $MYSQL_CLIENT_CONNECT
+echo "drop table ontime200" | $MYSQL_CLIENT_CONNECT
