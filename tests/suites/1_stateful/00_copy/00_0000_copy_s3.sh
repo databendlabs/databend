6082071e20cbd572e6f90111fe2f4275592e23a4
BohuTANG <overred.shuttler@gmail.com>
2022-02-23 15:50:41 +0800

    Add credential to copy statful test

--- tests/suites/1_stateful/00_copy/00_0000_copy_s3.sh
+++ tests/suites/1_stateful/00_copy/00_0000_copy_s3.sh
@@ -7,7 +7,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cat $CURDIR/../ontime/create_table.sql | sed 's/ontime/ontime200/g' | $MYSQL_CLIENT_CONNECT
 
 ## Copy from s3.
-echo "copy into ontime200 from 's3://testbucket/tests/data/ontime_200.csv' FILE_FORMAT = (type = 'CSV' field_delimiter = ','  record_delimiter = '\n' skip_header = 1)" | $MYSQL_CLIENT_CONNECT
+echo "copy into ontime200 from 's3://testbucket/tests/data/ontime_200.csv' credentials=(aws_key_id='minioadmin' aws_secret_key='minioadmin') FILE_FORMAT = (type = 'CSV' field_delimiter = ','  record_delimiter = '\n' skip_header = 1)" | $MYSQL_CLIENT_CONNECT
 
 ## Result.
 echo "select count(1) ,avg(Year), sum(DayOfWeek)  from ontime200" | $MYSQL_CLIENT_CONNECT
