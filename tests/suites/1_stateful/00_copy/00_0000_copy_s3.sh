6689b0f40e5e7ef267f4610492888365e72922e9
BohuTANG <overred.shuttler@gmail.com>
2022-02-26 09:23:19 +0800

    Update 00_0000_copy_s3.sh

--- tests/suites/1_stateful/00_copy/00_0000_copy_s3.sh
+++ tests/suites/1_stateful/00_copy/00_0000_copy_s3.sh
@@ -7,7 +7,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cat $CURDIR/../ontime/create_table.sql | sed 's/ontime/ontime200/g' | $MYSQL_CLIENT_CONNECT
 
 ## Copy from s3.
-echo "copy into ontime200 from 's3://testbucket/tests/data/ontime_200.csv' credentials=(aws_key_id='minioadmin' aws_secret_key='minioadmin') FILE_FORMAT = (type = 'CSV' field_delimiter = ','  record_delimiter = '\n' skip_header = 1)" | $MYSQL_CLIENT_CONNECT
+echo "copy into ontime200 from 's3://testbucket/admin/tests/data/ontime_200.csv' credentials=(aws_key_id='minioadmin' aws_secret_key='minioadmin') FILE_FORMAT = (type = 'CSV' field_delimiter = ','  record_delimiter = '\n' skip_header = 1)" | $MYSQL_CLIENT_CONNECT
 
 ## Result.
 echo "select count(1) ,avg(Year), sum(DayOfWeek)  from ontime200" | $MYSQL_CLIENT_CONNECT
@@ -17,7 +17,7 @@ echo "truncate table ontime200" | $MYSQL_CLIENT_CONNECT
 
 
 ## Copy from s3 with files.
-echo "copy into ontime200 from 's3://testbucket/tests/data/' credentials=(aws_key_id='minioadmin' aws_secret_key='minioadmin') FILES = ('ontime_200.csv', 'ontime_200_v1.csv') FILE_FORMAT = (type = 'CSV' field_delimiter = ','  record_delimiter = '\n' skip_header = 1)" | $MYSQL_CLIENT_CONNECT
+echo "copy into ontime200 from 's3://testbucket/admin/tests/data/' credentials=(aws_key_id='minioadmin' aws_secret_key='minioadmin') FILES = ('ontime_200.csv', 'ontime_200_v1.csv') FILE_FORMAT = (type = 'CSV' field_delimiter = ','  record_delimiter = '\n' skip_header = 1)" | $MYSQL_CLIENT_CONNECT
 
 ## Result.
 echo "select count(1) ,avg(Year), sum(DayOfWeek)  from ontime200" | $MYSQL_CLIENT_CONNECT
