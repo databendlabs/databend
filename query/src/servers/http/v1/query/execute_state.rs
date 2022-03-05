d71cf62489bad8e68e9398e16c86986294244136
youngsofun <yangxiufeng.c@gmail.com>
2022-03-05 18:51:59 +0800

    http handler time int to float

--- query/src/servers/http/v1/query/execute_state.rs
+++ query/src/servers/http/v1/query/execute_state.rs
@@ -82,10 +82,10 @@ impl Executor {
             Stopped(f) => f.progress.clone(),
         }
     }
-    pub(crate) fn elapsed(&self) -> Duration {
+    pub(crate) fn elapsed(&self) -> Option<Duration> {
         match &self.state {
-            Running(_) => Instant::now() - self.start_time,
-            Stopped(f) => f.stop_time - self.start_time,
+            Running(_) => None,
+            Stopped(f) => Some(f.stop_time - self.start_time),
         }
     }
     pub(crate) async fn stop(this: &Arc<RwLock<Executor>>, reason: Result<()>, kill: bool) {
