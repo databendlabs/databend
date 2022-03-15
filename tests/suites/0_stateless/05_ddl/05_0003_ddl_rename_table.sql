8d4f68a8663e33327c9df68b60781ad89194177a
wuzhiguo <changye@startdt.com>
2022-03-15 11:20:35 +0800

    modify rename stateless tests

--- tests/suites/0_stateless/05_ddl/05_0003_ddl_rename_table.sql
+++ tests/suites/0_stateless/05_ddl/05_0003_ddl_rename_table.sql
@@ -1,10 +1,41 @@
-DROP TABLE IF EXISTS t;
+-- No data when Engine is Null
+DROP TABLE IF EXISTS t0;
 
-CREATE TABLE t(c1 int) ENGINE = Null;
+CREATE TABLE t0(a int) ENGINE = Null;
+INSERT INTO TABLE t0 values(1);
+SELECT * FROM t0;
 
-RENAME TABLE t TO t0;
-DROP TABLE t; -- {ErrorCode 1025}
-DROP TABLE t0;
+RENAME TABLE t0 TO t1;
 DROP TABLE t0; -- {ErrorCode 1025}
+SELECT * FROM t1;
 
-DROP TABLE system.null; -- {ErrorCode 1002}
+RENAME TABLE t1 to system.t1; -- {ErrorCode 1002}
+DROP TABLE IF EXISTS t1;
+
+-- No data after rename when Engine is Memory
+DROP TABLE IF EXISTS t0;
+
+CREATE TABLE t0(a int) ENGINE = Memory;
+INSERT INTO TABLE t0 values(1);
+SELECT * FROM t0;
+
+RENAME TABLE t0 TO t1;
+DROP TABLE t0; -- {ErrorCode 1025}
+SELECT * FROM t1;
+
+RENAME TABLE t1 to system.t1; -- {ErrorCode 1002}
+DROP TABLE IF EXISTS t1;
+
+-- Data exists before and after rename
+DROP TABLE IF EXISTS t0;
+
+CREATE TABLE t0(a int);
+INSERT INTO TABLE t0 values(1);
+SELECT * FROM t0;
+
+RENAME TABLE t0 TO t1;
+DROP TABLE t0; -- {ErrorCode 1025}
+SELECT * FROM t1;
+
+RENAME TABLE t1 to system.t1; -- {ErrorCode 1002}
+DROP TABLE IF EXISTS t1;
