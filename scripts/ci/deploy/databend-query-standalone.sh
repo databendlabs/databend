f4fb8f480bd0342184330074e2d0af3bb0322ef8
sundyli <543950155@qq.com>
2022-03-16 21:57:57 +0800

    feat(list): Add grep logs

--- scripts/ci/deploy/databend-query-standalone.sh
+++ scripts/ci/deploy/databend-query-standalone.sh
@@ -25,5 +25,11 @@ python3 scripts/ci/wait_tcp.py --timeout 5 --port 9191
 
 echo 'Start databend-query...'
 nohup target/${BIN}/databend-query -c scripts/ci/deploy/config/databend-query-node-1.toml &
+
+sleep 10
+echo "Grep Storage"
+grep 'Storage' nohup.log
+grep 'Storage' ./_logs/databend-query*.log
+
 echo "Waiting on databend-query 10 seconds..."
 python3 scripts/ci/wait_tcp.py --timeout 5 --port 3307
