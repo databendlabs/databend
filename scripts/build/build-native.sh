5e8c059b101c91ce3e9cf7eab3b86f21f1953ac7
Chojan Shang <psiace@outlook.com>
2022-03-17 17:29:11 +0800

    chore: remove databend-benchamrk and old perf tools

--- scripts/build/build-native.sh
+++ scripts/build/build-native.sh
@@ -6,5 +6,5 @@ SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 cd "$SCRIPT_PATH/../.." || exit
 
 echo "Build(NATIVE) start..."
-RUSTFLAGS="-C target-cpu=native" cargo build --bin=databend-query --bin=databend-benchmark --bin=databend-meta --bin=databend-metactl --release
+RUSTFLAGS="-C target-cpu=native" cargo build --bin=databend-query --bin=databend-meta --bin=databend-metactl --release
 echo "All done..."
