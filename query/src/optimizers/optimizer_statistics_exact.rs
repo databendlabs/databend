7877a05ebfb65ef8435c48d4c09a187e259b0c91
dantengsky <dantengsky@gmail.com>
2022-03-20 14:47:15 +0800

    disable case insensitive checking of op name
    case-insensitive checking of operation name "count" has been disabled
    temporarily. clickhouse-result-writer reports errors while write
    the result of 'select count() from t'. converting form DataBlock to CH
    specific data block failed.

--- query/src/optimizers/optimizer_statistics_exact.rs
+++ query/src/optimizers/optimizer_statistics_exact.rs
@@ -53,10 +53,7 @@ impl PlanRewriter for StatisticsExactImpl<'_> {
                     ..
                 }],
                 PlanNode::ReadSource(read_source_plan),
-            ) if op.to_lowercase().as_str() == "count"
-                && args.is_empty()
-                && read_source_plan.statistics.is_exact =>
-            {
+            ) if op == "count" && args.is_empty() && read_source_plan.statistics.is_exact => {
                 let db_name = "system";
                 let table_name = "one";
 
