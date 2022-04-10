3b95770476b3f0b5817119a2cdd339ca3edb172b
BohuTANG <overred.shuttler@gmail.com>
2022-04-10 11:54:19 +0800

    chore(mysql): add 8.0.26 prefix to mysql select version()

--- tests/suites/0_stateless/10_drivers/10_0001_python_sqlalchemy_driver.py
+++ tests/suites/0_stateless/10_drivers/10_0001_python_sqlalchemy_driver.py
@@ -6,7 +6,9 @@ engine = sqlalchemy.create_engine("mysql+pymysql://root:root@localhost:3307/")
 conn = engine.connect()
 conn.execute("create database if not exists book_db")
 conn.execute("use book_db")
-conn.execute("create table if not exists books(title varchar(255), author varchar(255), date varchar(255))")
+conn.execute(
+    "create table if not exists books(title varchar(255), author varchar(255), date varchar(255))"
+)
 conn.execute("insert into books values('mybook', 'author', '2022')")
 results = conn.execute('select * from books').fetchall()
 for result in results:
