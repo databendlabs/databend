3ba809fe7552699fcfdf62b3ff2750ebca7484dd
taichong <tai_chong@foxmail.com>
2022-04-14 16:19:42 +0800

    Select expr support use backquoted
    More info link to : https://github.com/datafuse-extras/sqlparser-rs/issues/28

--- tests/suites/0_stateless/03_dml/03_0007_select_alias.sql
+++ tests/suites/0_stateless/03_dml/03_0007_select_alias.sql
@@ -1,2 +1,10 @@
 set max_threads=1;
 SELECT (number+1) as c1, max(number) as c2 FROM numbers_mt(10) group by number+1 having c2>1 order by c1 desc, c2 asc;
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+USE test;
+CREATE TABLE test.t (id int);
+INSERT INTO test.t VALUES(1);
+SELECT t.`id` AS C, `t`.id AS C1, `id` AS C2, `t`.`id` AS C3  FROM test.t;
+DROP DATABASE test;
+SELECT `T`.`ID` AS C -- {ErrorCode 1058}
