6865b4e64d4ce698c9ef191ed50152aa7a9aabb3
BohuTANG <overred.shuttler@gmail.com>
2022-04-25 11:29:06 +0800

    Move meta/types schema type to catalog

---
+++ query/src/catalogs/types/database.rs
@@ -0,0 +1,142 @@
+//  Copyright 2022 Datafuse Labs.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+use std::collections::BTreeMap;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::ops::Deref;
+
+use common_datavalues::chrono::DateTime;
+use common_datavalues::chrono::Utc;
+
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct DatabaseNameIdent {
+    pub tenant: String,
+    pub db_name: String,
+}
+
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct DatabaseInfo {
+    pub database_id: u64,
+    pub db: String,
+    pub meta: DatabaseMeta,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct DatabaseMeta {
+    pub engine: String,
+    pub engine_options: BTreeMap<String, String>,
+    pub options: BTreeMap<String, String>,
+    pub created_on: DateTime<Utc>,
+}
+
+impl Default for DatabaseMeta {
+    fn default() -> Self {
+        DatabaseMeta {
+            engine: "".to_string(),
+            engine_options: BTreeMap::new(),
+            options: BTreeMap::new(),
+            created_on: Utc::now(),
+        }
+    }
+}
+
+impl Display for DatabaseMeta {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Engine: {}={:?}, Options: {:?}, CreatedOn: {:?}",
+            self.engine, self.engine_options, self.options, self.created_on
+        )
+    }
+}
+
+impl DatabaseInfo {
+    pub fn engine(&self) -> &str {
+        &self.meta.engine
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct CreateDatabaseReq {
+    pub if_not_exists: bool,
+    pub tenant: String,
+    pub db_name: String,
+    pub meta: DatabaseMeta,
+}
+
+impl Display for CreateDatabaseReq {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "create_db(if_not_exists={}):{}/{}={:?}",
+            self.if_not_exists, self.tenant, self.db_name, self.meta
+        )
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct CreateDatabaseReply {
+    pub database_id: u64,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct DropDatabaseReq {
+    pub if_exists: bool,
+    pub tenant: String,
+    pub db_name: String,
+}
+
+impl Display for DropDatabaseReq {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "drop_db(if_exists={}):{}/{}",
+            self.if_exists, self.tenant, self.db_name
+        )
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct DropDatabaseReply {}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct GetDatabaseReq {
+    pub inner: DatabaseNameIdent,
+}
+
+impl Deref for GetDatabaseReq {
+    type Target = DatabaseNameIdent;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl GetDatabaseReq {
+    pub fn new(tenant: impl Into<String>, db_name: impl Into<String>) -> GetDatabaseReq {
+        GetDatabaseReq {
+            inner: DatabaseNameIdent {
+                tenant: tenant.into(),
+                db_name: db_name.into(),
+            },
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ListDatabaseReq {
+    pub tenant: String,
+}
--- query/src/catalogs/types/mod.rs
+++ query/src/catalogs/types/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 Datafuse Labs.
+// Copyright 2022 Datafuse Labs.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,21 +11,4 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-mod catalog;
-mod catalog_context;
-mod table_id_ranges;
-mod table_memory_meta;
-
-mod backends;
-mod impls;
-mod types;
-
-pub use backends::MetaBackend;
-pub use catalog::Catalog;
-pub use catalog_context::CatalogContext;
-pub use impls::DatabaseCatalog;
-pub use impls::ImmutableCatalog;
-pub use impls::MutableCatalog;
-pub use table_id_ranges::*;
-pub use table_memory_meta::InMemoryMetas;
+mod database;
