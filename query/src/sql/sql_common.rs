7d9e22730964603601706cf317e2accc5817100a
sundyli <543950155@qq.com>
2022-04-25 16:33:19 +0800

    feat(query): attach short sql in context

--- query/src/sql/sql_common.rs
+++ query/src/sql/sql_common.rs
@@ -87,8 +87,8 @@ impl SQLCommon {
     }
 
     pub fn short_sql(query: &str) -> String {
-        if query.len() > 64 && query[0..=6].eq_ignore_ascii_case("INSERT") {
-            format!("{}...", query[0..64].to_string())
+        if query.len() >= 64 && query[..=6].eq_ignore_ascii_case("INSERT") {
+            format!("{}...", &query[..64])
         } else {
             query.to_string()
         }
