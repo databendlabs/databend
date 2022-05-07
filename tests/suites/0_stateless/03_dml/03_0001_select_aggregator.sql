abae845ffff1da6d1e478093c8e82fbb92b8e488
xudong963 <wxd963996380@gmail.com>
2022-05-07 15:28:41 +0800

    copy test

--- tests/suites/0_stateless/03_dml/03_0001_select_aggregator.sql
+++ tests/suites/0_stateless/03_dml/03_0001_select_aggregator.sql
@@ -1,7 +1,5 @@
-set enable_planner_v2 = 1;
 select 1, sum(number) from numbers_mt(1000000);
 select count(*) = count(1) from numbers(1000);
 select count(1) from numbers(1000);
 select sum(3) from numbers(1000);
 select count(null) from numbers(1000);
-set enable_planner_v2 = 0;
--- tests/suites/0_stateless/03_dml/03_0002_select_aggregator_with_filter.sql
+++ tests/suites/0_stateless/03_dml/03_0002_select_aggregator_with_filter.sql
@@ -1,4 +1,2 @@
-set enable_planner_v2 = 1;
 SELECT max(number) FROM numbers_mt (10) where number > 99999999998;
 SELECT max(number) FROM numbers_mt (10) where number > 2;
-set enable_planner_v2 = 0;
