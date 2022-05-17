75d93f8bcfd055629c5bf02ff25183b05200382b
xudong963 <wxd963996380@gmail.com>
2022-05-17 19:46:42 +0800

    fix annotation

--- query/src/sql/planner/binder/bind_context.rs
+++ query/src/sql/planner/binder/bind_context.rs
@@ -47,7 +47,7 @@ pub struct ColumnBinding {
 
     /// Consider the sql: `select * from t join t1 using(a)`.
     /// The result should only contain one `a` column.
-    /// So we need mark `t.a` or `t1.a` duplicated.
+    /// So we need make `t.a` or `t1.a` invisible in unqualified wildcard.
     pub visible_in_unqualified_wildcard: bool,
 }
 
