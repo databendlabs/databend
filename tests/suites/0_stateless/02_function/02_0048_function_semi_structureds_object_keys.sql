b097a3421c28c3c682d01d9b6894b3dbdc007ec2
fkuner <784819644@qq.com>
2022-05-19 18:00:00 +0800

    fix test about out-of-order in cluster mode

--- tests/suites/0_stateless/02_function/02_0048_function_semi_structureds_object_keys.sql
+++ tests/suites/0_stateless/02_function/02_0048_function_semi_structureds_object_keys.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS objects_test1(id TINYINT, obj OBJECT, var VARIANT) Engine=Memory;
 
 insert into objects_test1 values (1, parse_json('{"a": 1, "b": [1,2,3]}'), parse_json('{"1": 2}'));
-insert into objects_test1 values (2, parse_json('{"b": [2,3,4]}'), parse_json('{"c": "d"}'));
 
 select id, object_keys(obj), object_keys(var) from objects_test1;
 
