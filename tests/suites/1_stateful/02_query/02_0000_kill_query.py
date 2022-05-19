33f31adfd38b87a414d9f3846d90fa3964332bf4
taichong <tai_chong@foxmail.com>
2022-05-19 20:54:42 +0800

    fix flaky test

--- tests/suites/1_stateful/02_query/02_0000_kill_query.py
+++ tests/suites/1_stateful/02_query/02_0000_kill_query.py
@@ -14,34 +14,27 @@ from native_client import prompt
 
 log = None
 
+# client1 send long query, client mydb fetch the long query and kill it.
+# Use to mock in MySQL Client press Ctrl C to intercept a long query.
+
+mydb = mysql.connector.connect(host="127.0.0.1",
+                               user="root",
+                               passwd="root",
+                               port="3307")
+
 with NativeClient(name='client1>') as client1:
     client1.expect(prompt)
 
-    client1.send('SET enable_new_processor_framework=0;')
     client1.expect('')
-    client1.send('SELECT * FROM numbers(999999999);')
-    client1.expect(prompt)
+    client1.send('SELECT SLEEP(2.9);')
+    time.sleep(0.1)
 
-with NativeClient(name='client2>') as client2:
-    client2.expect(prompt)
-
-    # client1 send long query, client select the long query and kill it
-    # Use to mock in MySQL Client press Ctrl C to intercept a long query.
-    mydb = mysql.connector.connect(host="127.0.0.1",
-                                   user="root",
-                                   passwd="root",
-                                   port="3307")
     mycursor = mydb.cursor()
-    mycursor.execute(
-        "SELECT mysql_connection_id FROM system.processes WHERE extra_info LIKE '%select * from numbers(999999999)%'"
-    )
+    mycursor.execute("SELECT mysql_connection_id FROM system.processes WHERE extra_info LIKE '%SELECT SLEEP(2.9)%' AND extra_info NOT LIKE '%system.processes%';")
     res = mycursor.fetchone()
     kill_query = 'kill query ' + str(res[0]) + ';'
-    client2.send(kill_query)
-    client2.expect(prompt)
-    time.sleep(5)
-    mycursor.execute(
-        "SELECT * FROM system.processes WHERE extra_info LIKE '%select * from numbers(999999999)%' AND extra_info NOT LIKE '%system.processes%'"
-    )
+    mycursor.execute(kill_query)
+    time.sleep(0.1)
+    mycursor.execute("SELECT * FROM system.processes WHERE extra_info LIKE '%SELECT SLEEP(2.9)%' AND extra_info NOT LIKE '%system.processes%';")
     res = mycursor.fetchone()
     assert res is None
