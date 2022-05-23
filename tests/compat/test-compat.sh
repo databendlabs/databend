b60e81d713c5919822c8531f270ea10463d957a4
张炎泼 <drdr.xp@gmail.com>
2022-05-23 14:52:52 +0800

    [ci] add compatibility test with old metasrv and latest query

---
+++ tests/compat/test-compat.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -o errexit
+
+usage()
+{
+    echo "test query being compatible with old metasrv"
+    echo "Expect current release binaries are in ./current/."
+    echo "Expect     old release binaries are in ./old/."
+    echo "Usage: $0"
+}
+
+SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+
+echo "SCRIPT_PATH: $SCRIPT_PATH"
+
+# go to work tree root
+cd "$SCRIPT_PATH/../../"
+
+pwd
+
+mkdir -p ./target/debug/
+chmod +x ./current/*
+chmod +x ./old/*
+
+cp ./current/databend-query ./target/debug/
+cp ./old/databend-meta      ./target/debug/
+
+./target/debug/databend-meta --version
+./target/debug/databend-query --version
+
+./scripts/ci/ci-run-stateless-tests-standalone.sh
