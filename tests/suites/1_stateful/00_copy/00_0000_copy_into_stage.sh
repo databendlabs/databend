1dc2a2ea9bcaa267811be7377212f8a5ddc3db83
sundyli <543950155@qq.com>
2022-05-26 11:42:28 +0800

    Fix tests

--- tests/suites/1_stateful/00_copy/00_0000_copy_into_stage.sh
+++ tests/suites/1_stateful/00_copy/00_0000_copy_into_stage.sh
@@ -19,7 +19,7 @@ done
 
 echo "copy into @s2 from test_table FILE_FORMAT = (type = 'CSV');" | $MYSQL_CLIENT_CONNECT
 echo "copy into @s2 from (select name, age, id from test_table limit 100) FILE_FORMAT = (type = 'PARQUET');" | $MYSQL_CLIENT_CONNECT
-echo "list @s2;" | $MYSQL_CLIENT_CONNECT | wc -l
+echo "list @s2;" | $MYSQL_CLIENT_CONNECT | wc -l | sed 's/ //g'
 echo "drop STAGE s2;" | $MYSQL_CLIENT_CONNECT 
 echo "drop table test_table;" | $MYSQL_CLIENT_CONNECT
 
