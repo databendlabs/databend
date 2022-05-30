81328e8b7b150fcf0439cfde35d04dc835cc5be1
Chasen-Zhang <787025321@qq.com>
2022-05-30 13:53:05 +0800

    fix: twitter card

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -193,7 +193,7 @@ const config = {
                 indexName: 'databend-rs-docs',
                 contextualSearch: true,
             },
-            image: 'img/logo/databend-logo-horizontal.png',
+            image: 'img/logo/logo-no-text.png',
             metadata: [
               {name: 'twitter:card', content: 'summary_large_image'},
               {name: 'twitter:site', content: '@databend.rs'}
