581632e48b0d12252c4694ab66260800ef43617d
Yang Xiufeng <yangxiufeng.c@gmail.com>
2022-06-01 17:00:13 +0800

    add result_table_sink

--- query/src/storages/result/mod.rs
+++ query/src/storages/result/mod.rs
@@ -16,8 +16,10 @@ pub mod block_buffer;
 mod download;
 mod result_locations;
 mod result_table;
+mod result_table_sink;
 mod writer;
 
 pub use result_table::ResultQueryInfo;
 pub use result_table::ResultTable;
+pub use result_table_sink::ResultTableSink;
 pub use writer::ResultTableWriter;
