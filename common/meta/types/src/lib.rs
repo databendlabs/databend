52457858b8d8c10a0895b76f3a11aca8c08a714d
张炎泼 <drdr.xp@gmail.com>
2022-06-02 14:45:28 +0800

    [common/exception] refactor: add ErrorCode::set_backtrace() for keeping the original bt when converting MetaError to ErrorCode

--- common/meta/types/src/lib.rs
+++ common/meta/types/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![feature(backtrace)]
+
 //! This crate defines data types used in meta data storage service.
 
 mod applied_state;
