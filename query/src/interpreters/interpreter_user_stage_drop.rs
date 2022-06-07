c35f58b7fe6fb56dce09fe17b9ff862a028943a1
fkuner <784819644@qq.com>
2022-06-07 10:34:54 +0800

    fix unknown stage bug

--- query/src/interpreters/interpreter_user_stage_drop.rs
+++ query/src/interpreters/interpreter_user_stage_drop.rs
@@ -58,23 +58,22 @@ impl Interpreter for DropUserStageInterpreter {
         let tenant = self.ctx.get_tenant();
         let user_mgr = self.ctx.get_user_manager();
 
-        let stage = user_mgr.get_stage(&tenant, plan.name.as_str()).await?;
-
-        if matches!(&stage.stage_type, StageType::Internal) {
-            let op = StageSource::get_op(&self.ctx, &stage).await?;
-            let absolute_path = format!("/stage/{}/", stage.stage_name);
-            let objects = op.object(&absolute_path).list().await?;
-            remove_recursive_objects(objects, op.clone()).await?;
-            info!(
-                "drop stage {:?} with all objects removed in stage",
-                stage.stage_name
-            );
+        if let Ok(stage) = user_mgr.get_stage(&tenant, plan.name.as_str()).await {
+            if matches!(&stage.stage_type, StageType::Internal) {
+                let op = StageSource::get_op(&self.ctx, &stage).await?;
+                let absolute_path = format!("/stage/{}/", stage.stage_name);
+                let objects = op.object(&absolute_path).list().await?;
+                remove_recursive_objects(objects, op.clone()).await?;
+                info!(
+                    "drop stage {:?} with all objects removed in stage",
+                    stage.stage_name
+                );
+            }
         }
 
         user_mgr
-            .drop_stage(&tenant, &stage.stage_name, plan.if_exists)
+            .drop_stage(&tenant, plan.name.as_str(), plan.if_exists)
             .await?;
-
         Ok(Box::pin(DataBlockStream::create(
             self.plan.schema(),
             None,
