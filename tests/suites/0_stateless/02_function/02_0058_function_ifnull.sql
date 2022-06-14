a60aca41739d772bd1a9d1e15a61153698edfcc6
yuuch <ywchcs@gmail.com>
2022-06-14 23:28:47 +0800

    add test result

--- tests/suites/0_stateless/02_function/02_0058_function_ifnull.sql
+++ tests/suites/0_stateless/02_function/02_0058_function_ifnull.sql
@@ -1,40 +1,28 @@
--- SET enable_planner_v2 = 1;
+SET enable_planner_v2 = 1;
 
--- SELECT '==ifnull_const_values==';
--- SELECT IFNULL(1, 1);
--- SELECT IFNULL(2, 1);
--- SELECT IFNULL(1, 2);
--- SELECT IFNULL(1, NULL);
--- SELECT IFNULL(NULL, 1);
--- SELECT IFNULL('a', 'a');
--- SELECT IFNULL('a', 'b');
--- SELECT IFNULL('b', 'a');
--- SELECT IFNULL('a', NULL);
--- SELECT IFNULL(NULL, 'a');
--- SELECT IFNULL(NULL, NULL);
+SELECT '==ifnull_const_values==';
+SELECT IFNULL(1, 1);
+SELECT IFNULL(2, 1);
+SELECT IFNULL(1, 2);
+SELECT IFNULL(1, NULL);
+SELECT IFNULL(NULL, 1);
+SELECT IFNULL('a', 'a');
+SELECT IFNULL('a', 'b');
+SELECT IFNULL('b', 'a');
+SELECT IFNULL('a', NULL);
+SELECT IFNULL(NULL, 'a');
+SELECT IFNULL(NULL, NULL);
 
--- SELECT '==ifnull_non_nullable_columns=';
--- CREATE TABLE IF NOT EXISTS t(a INT, b INT) ENGINE=Memory;
--- INSERT INTO t VALUES(0, 0), (0, 1), (1, 0), (1, 1);
--- SELECT a, b, IFNULL(a, b) FROM t;
--- DROP TABLE t;
+SELECT '==ifnull_non_nullable_columns=';
+CREATE TABLE IF NOT EXISTS t(a INT, b INT) ENGINE=Memory;
+INSERT INTO t VALUES(0, 0), (0, 1), (1, 0), (1, 1);
+SELECT a, b, IFNULL(a, b) FROM t;
+DROP TABLE t;
 
--- SELECT '==ifnull_nullable_columns==';
--- CREATE TABLE IF NOT EXISTS t(a INT NULL, b INT NULL) ENGINE=Memory;
--- INSERT INTO t VALUES(0, 0), (0, 1), (0, NULL), (1, 0), (1, 1), (1, NULL), (NULL, 0), (NULL, 1), (NULL, NULL);
--- SELECT a, b, IFNULL(a, b) FROM t;
--- DROP TABLE t;
+SELECT '==ifnull_nullable_columns==';
+CREATE TABLE IF NOT EXISTS t(a INT NULL, b INT NULL) ENGINE=Memory;
+INSERT INTO t VALUES (0, NULL), (1, NULL), (NULL, 0), (NULL, 1), (NULL, NULL);
+SELECT a, b, IFNULL(a, b) FROM t;
+DROP TABLE t;
 
--- SELECT '==ifnull_lhs_all_null==';
--- CREATE TABLE IF NOT EXISTS t(a INT NULL, b INT NULL) ENGINE=Memory;
--- INSERT INTO t VALUES(NULL, 0), (NULL, 1), (NULL, NULL), (NULL, 0), (NULL, 1);
--- SELECT a, b, IFNULL(a, b) FROM t;
--- DROP TABLE t;
-
--- SELECT '==ifnull_rhs_all_null==';
--- CREATE TABLE IF NOT EXISTS t(a INT NULL, b INT NULL) ENGINE=Memory;
--- INSERT INTO t VALUES(0, NULL), (1, NULL), (0, NULL), (1, NULL), (NULL, NULL);
--- SELECT a, b, IFNULL(a, b) FROM t;
--- DROP TABLE t;
-
--- SET enable_planner_v2 = 0;
+SET enable_planner_v2 = 0;
