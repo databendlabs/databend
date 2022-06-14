c8545729c4e913bf5ce8bfe728990ee6626fb3db
fkuner <784819644@qq.com>
2022-06-14 16:51:41 +0800

    fix test

--- tests/suites/0_stateless/03_dml/03_0025_insert_async.sh
+++ tests/suites/0_stateless/03_dml/03_0025_insert_async.sh
@@ -9,14 +9,14 @@ echo "create table async_insert_test(a tinyint, b bigint, c double);" | $MYSQL_C
 
 # parallel insert
 count=0
-while(($count < 100))
+while(($count < 10))
 do curl -s -u root: -XPOST "http://localhost:${QUERY_HTTP_HANDLER_PORT}/v1/query" \
     --header 'Content-Type: application/json' \
     --data-raw '{"sql": "insert into async_insert_test values(1, 2, 3.4);", "session": {"settings": {"enable_async_insert": "1"}}}' > /dev/null 2>&1 &
     let "count++"
 done
 
-sleep 1
+sleep 2
 echo "select count() from async_insert_test;" | $MYSQL_CLIENT_CONNECT
 
 # drop table
