229b7d2dac1a77681f35e511d1fefb97bd80126e
cadl <ctrlaltdeleteliu@gmail.com>
2022-06-20 22:45:55 +0800

    test: make last snapshot.timestamp + 1μs as travel timepoint
    because the time precision of snapshot is higher than timestamp(6)

--- tests/suites/0_stateless/12_time_travel/12_0004_time_travel_select_at.sh
+++ tests/suites/0_stateless/12_time_travel/12_0004_time_travel_select_at.sh
@@ -8,6 +8,10 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 echo "create table t12_0004(c int)" | $MYSQL_CLIENT_CONNECT
 echo "two insertions"
 echo "insert into t12_0004 values(1),(2)" | $MYSQL_CLIENT_CONNECT
+
+# Ensure there is a small interval between two insertions, for time travel
+sleep 0.001
+
 echo "insert into t12_0004 values(3)" | $MYSQL_CLIENT_CONNECT
 echo "latest snapshot should contain 3 rows"
 echo "select count(*)  from t12_0004" | $MYSQL_CLIENT_CONNECT
@@ -23,9 +27,13 @@ echo "planner_v2: counting the data set of first insertion, which should contain
 echo "set enable_planner_v2 = 1;select count(t.c) from t12_0004 at (snapshot => '$SNAPSHOT_ID') as t" | $MYSQL_CLIENT_CONNECT
 
 
-TIMEPOINT=$(echo "select timestamp from fuse_snapshot('default', 't12_0004') where row_count=2" | $MYSQL_CLIENT_CONNECT)
+# Get a time point at/after the first insertion.
+# Cannot directly use the timestamp of first insertion, because the time precision of snapshot is higher than timestamp(6). So +1μs.
+
+TIMEPOINT=$(echo "select cast(timestamp as BIGINT) + 1 from fuse_snapshot('default', 't12_0004') where row_count=2" | $MYSQL_CLIENT_CONNECT)
+
 echo "planner_v2: counting the data set of fisrt insertion by timestamp, which should contains 2 rows"
-echo "set enable_planner_v2 = 1;select count(t.c) from t12_0004 at (timestamp => cast('$TIMEPOINT' as timestamp)) as t" | $MYSQL_CLIENT_CONNECT
+echo "set enable_planner_v2 = 1;select count(t.c) from t12_0004 at (TIMESTAMP => $TIMEPOINT::TIMESTAMP) as t" | $MYSQL_CLIENT_CONNECT
 
 ## Drop table.
-echo "drop table  t12_0004" | $MYSQL_CLIENT_CONNECT
+echo "drop table t12_0004" | $MYSQL_CLIENT_CONNECT
