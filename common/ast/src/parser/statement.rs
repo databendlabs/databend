37da1b1e0663804fcc6dca4d08cbef0d6e68db65
Xuanwo <github@xuanwo.io>
2022-06-21 19:54:19 +0800

    Return u64_string directly
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- common/ast/src/parser/statement.rs
+++ common/ast/src/parser/statement.rs
@@ -1193,7 +1193,7 @@ pub fn options(i: Input) -> IResult<BTreeMap<String, String>> {
         })(i)
     };
 
-    let u64_to_string = |i| map_res(literal_u64, |v| Ok(v.to_string()))(i);
+    let u64_to_string = |i| map(literal_u64, |v| v.to_string())(i);
 
     let ident_with_format = alt((
         ident_to_string,
