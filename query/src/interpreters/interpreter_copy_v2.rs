001861871a3152c4110e161ac26a36a532bdfc55
Xuanwo <github@xuanwo.io>
2022-06-21 18:51:38 +0800

    Fix format
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- query/src/interpreters/interpreter_copy_v2.rs
+++ query/src/interpreters/interpreter_copy_v2.rs
@@ -1,4 +1,3 @@
-use std::path::Path;
 // Copyright 2022 Datafuse Labs.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,7 @@ use std::path::Path;
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+use std::path::Path;
 use std::sync::Arc;
 
 use common_datablocks::DataBlock;
