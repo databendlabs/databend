9967e38ed1be1aee02d89f5b6b1aaaa688c908ae
dantengsky <dantengsky@gmail.com>
2022-06-23 14:17:48 +0800

    fix stateless test

--- tests/suites/0_stateless/09_fuse_engine/09_0017_func_fuse_snapshot_history_limit.sh
+++ tests/suites/0_stateless/09_fuse_engine/09_0017_func_fuse_snapshot_history_limit.sh
@@ -15,14 +15,16 @@ echo "set enable_planner_v2 = 1; select * from fuse_snapshot('default', 't09_001
 echo "limit 0 using new planner"
 echo "set enable_planner_v2 = 1; select * from fuse_snapshot('default', 't09_0017') limit 0" | $MYSQL_CLIENT_CONNECT | wc -l
 
-echo "limit 1 using new planner"
+echo "limit 1 using old planner"
 echo "set enable_planner_v2 = 0; select * from fuse_snapshot('default', 't09_0017') limit 1" | $MYSQL_CLIENT_CONNECT | wc -l
-echo "limit 0 using new planner"
+echo "limit 0 using old planner"
 echo "set enable_planner_v2 = 0; select * from fuse_snapshot('default', 't09_0017') limit 0" | $MYSQL_CLIENT_CONNECT | wc -l
 
-echo "limit 1 using new planner and stored procedure"
+echo "limit 1 using stored procedure"
 echo "call system\$fuse_snapshot('default', 't09_0017', 1)" | $MYSQL_CLIENT_CONNECT | wc -l
-echo "limit 1 using new planner and stored procedure"
+echo "limit 0 using stored procedure"
 echo "call system\$fuse_snapshot('default', 't09_0017', 0)" | $MYSQL_CLIENT_CONNECT | wc -l
+echo "no limits using stored procedure, expects 2"
+echo "call system\$fuse_snapshot('default', 't09_0017')" | $MYSQL_CLIENT_CONNECT | wc -l
 ## Drop table.
 echo "drop table  t09_0017" | $MYSQL_CLIENT_CONNECT
