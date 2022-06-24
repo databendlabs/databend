98f3ea05680f5b299497103393cf5b828ef41bdd
sundyli <543950155@qq.com>
2022-06-24 20:34:02 +0800

    query(functions): fix aggregate count incorrect state place

--- common/functions/src/aggregates/aggregate_count.rs
+++ common/functions/src/aggregates/aggregate_count.rs
@@ -120,7 +120,7 @@ impl AggregateFunction for AggregateCountFunction {
             }
             None => {
                 for place in places {
-                    let state = place.get::<AggregateCountState>();
+                    let state = place.next(offset).get::<AggregateCountState>();
                     state.count += 1;
                 }
             }
@@ -143,7 +143,6 @@ impl AggregateFunction for AggregateCountFunction {
     fn deserialize(&self, place: StateAddr, reader: &mut &[u8]) -> Result<()> {
         let state = place.get::<AggregateCountState>();
         state.count = deserialize_from_slice(reader)?;
-
         Ok(())
     }
 
