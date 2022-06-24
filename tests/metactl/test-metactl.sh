6bbf7749454219179aba87579df49c2a37ce1cb4
everpcpc <git@everpcpc.com>
2022-06-24 20:15:06 +0800

    fix(ci): add env BUILD_PROFILE for production test

--- tests/metactl/test-metactl.sh
+++ tests/metactl/test-metactl.sh
@@ -3,31 +3,32 @@
 set -o errexit
 
 SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+BUILD_PROFILE="${BUILD_PROFILE:-debug}"
 
 meta_dir="$SCRIPT_PATH/_meta_dir"
 meta_json="$SCRIPT_PATH/meta.json"
 exported="$SCRIPT_PATH/exported"
 grpc_exported="$SCRIPT_PATH/exported"
 
-chmod +x ./target/debug/databend-metactl
+chmod +x ./target/${BUILD_PROFILE}/databend-metactl
 
-cat $meta_json \
-    | ./target/debug/databend-metactl --import --raft-dir "$meta_dir"
+cat $meta_json |
+    ./target/${BUILD_PROFILE}/databend-metactl --import --raft-dir "$meta_dir"
 
 sleep 1
 
-./target/debug/databend-metactl --export --raft-dir "$meta_dir" > $exported
+./target/${BUILD_PROFILE}/databend-metactl --export --raft-dir "$meta_dir" >$exported
 
 diff $meta_json $exported
 
 # test export from grpc
-chmod +x ./target/debug/databend-meta
-./target/debug/databend-meta --single &
+chmod +x ./target/${BUILD_PROFILE}/databend-meta
+./target/${BUILD_PROFILE}/databend-meta --single &
 METASRV_PID=$!
 echo $METASRV_PID
 sleep 0.5
 
-./target/debug/databend-metactl --export --grpc-api-address "127.0.0.1:9191"  > $grpc_exported
+./target/${BUILD_PROFILE}/databend-metactl --export --grpc-api-address "127.0.0.1:9191" >$grpc_exported
 grep -Fxq '["raft_state",{"RaftStateKV":{"key":"Id","value":{"NodeId":0}}}]' $grpc_exported
 
 kill $METASRV_PID
