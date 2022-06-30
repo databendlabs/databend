528b5c0ab17fa7cd74aca982aaea85c17e3017e4
zhyass <34016424+zhyass@users.noreply.github.com>
2022-07-01 00:57:28 +0800

    remove unused code

--- query/src/storages/storage_table_read_plan.rs
+++ query/src/storages/storage_table_read_plan.rs
@@ -87,7 +87,7 @@ impl ToReadDataSourcePlan for dyn Table {
     }
 }
 
-pub fn get_description(table_info: &TableInfo, statistics: &Statistics) -> String {
+fn get_description(table_info: &TableInfo, statistics: &Statistics) -> String {
     if statistics.read_rows > 0 {
         format!(
             "(Read from {} table, {} Read Rows:{}, Read Bytes:{}, Partitions Scanned:{}, Partitions Total:{})",
