bc991217d194a243b52737f69bcf37c383fc41d0
Xuanwo <github@xuanwo.io>
2022-07-07 13:55:55 +0800

    Format code
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- query/src/sql/planner/binder/presign.rs
+++ query/src/sql/planner/binder/presign.rs
@@ -11,36 +11,18 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-use std::collections::BTreeMap;
-use std::str::FromStr;
 
-use common_ast::ast::CopyStmt;
-use common_ast::ast::CopyUnit;
 use common_ast::ast::PresignAction as AstPresignAction;
 use common_ast::ast::PresignLocation;
 use common_ast::ast::PresignStmt;
-use common_ast::ast::Query;
-use common_ast::ast::Statement;
-use common_ast::parser::parse_sql;
-use common_ast::parser::tokenize_sql;
-use common_ast::Backtrace;
-use common_exception::ErrorCode;
 use common_exception::Result;
-use common_meta_types::UserStageInfo;
-use common_planners::ReadDataSourcePlan;
-use common_planners::SourceInfo;
-use common_planners::StageTableInfo;
 use time::Duration;
 
 use crate::sql::binder::Binder;
-use crate::sql::plans::CopyPlanV2;
 use crate::sql::plans::Plan;
 use crate::sql::plans::PresignAction;
 use crate::sql::plans::PresignPlan;
-use crate::sql::plans::ValidationMode;
-use crate::sql::statements::parse_copy_file_format_options;
 use crate::sql::statements::parse_stage_location_v2;
-use crate::sql::statements::parse_uri_location_v2;
 use crate::sql::BindContext;
 
 impl<'a> Binder {
--- query/src/sql/planner/plans/presign.rs
+++ query/src/sql/planner/plans/presign.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::fmt::Display;
-
 use common_datavalues::DataField;
 use common_datavalues::DataSchemaRef;
 use common_datavalues::DataSchemaRefExt;
