c3c6cd46c5b01062f45bd784a7ab86f9353ec7b1
baishen <baishen2009@gmail.com>
2022-07-07 10:50:13 +0800

    fix datetime test

--- tests/suites/0_stateless/03_dml/03_0016_insert_into_values.sql
+++ tests/suites/0_stateless/03_dml/03_0016_insert_into_values.sql
@@ -7,17 +7,16 @@ CREATE TABLE IF NOT EXISTS t1(a Int8 null, b UInt32 null, c DateTime null, d Str
 INSERT INTO t1 (a,b,c,d,e) VALUES(-1, 33, '2021-08-15 10:00:00', 'string1234', 1.4e5),
                                        (101, 67, '2021-11-15 10:00:00', 'string5678', 9.9e-3);
 
-INSERT INTO t1 (a,b,c,d,e) VALUES(1, 10, '0000-00-00 00:00:00', 'test)', 12.34);  -- {ErrorCode 1105}
 
 select * from t1;
 select sum(a),sum(b) from t1;
 
+CREATE TABLE IF NOT EXISTS t2(a Int8, b DateTime, c String) Engine = Fuse;
+INSERT INTO t2 (a,b,c) VALUES(1, '0000-00-00 00:00:00', 'test)');  -- {ErrorCode 1010}
 
 CREATE TABLE IF NOT EXISTS t_str(a Varchar);
 INSERT INTO t_str(a) values( 'a"b\"c\'d');
 INSERT INTO t_str(a) values( 'a"b\"c\\\'d');
 select * from t_str order by a;
-
-drop table t_str;
 
 DROP DATABASE db1;
