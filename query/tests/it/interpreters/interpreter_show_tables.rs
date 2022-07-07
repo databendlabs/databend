b4d060ba35c3b9c12cc82cb84a2fec8eba83f5fc
sundyli <543950155@qq.com>
2022-07-07 09:47:32 +0800

    fix tests

--- query/tests/it/interpreters/interpreter_show_tables.rs
+++ query/tests/it/interpreters/interpreter_show_tables.rs
@@ -70,24 +70,6 @@ async fn test_show_tables_interpreter() -> Result<()> {
         common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
     }
 
-    // show full tables, hard to compare because of the create_time
-    // {
-    //     let plan = PlanParser::parse(ctx.clone(), "show full tables").await?;
-    //     let executor = InterpreterFactory::get(ctx.clone(), plan.clone())?;
-    //     assert_eq!(executor.name(), "ShowTablesInterpreter");
-    //     let stream = executor.execute(None).await?;
-    //     let result = stream.try_collect::<Vec<_>>().await?;
-    //     let expected = vec![
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| Tables_in_db1 | Table_type | table_catalog | engine | create_time                   | num_rows | data_size | data_compressed_size | index_size |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| bend          | BASE TABLE | db1           | FUSE   | 1970-01-01 00:00:00.000 +0000 | 0        | 0         | 0                    | NULL       |",
-    //         "| data          | BASE TABLE | db1           | FUSE   | 1970-01-01 00:00:00.000 +0000 | 0        | 0         | 0                    | NULL       |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //     ];
-    //     common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
-    // }
-
     // show tables like '%da%'.
     {
         let plan = PlanParser::parse(ctx.clone(), "show tables like '%da%'").await?;
@@ -104,23 +86,6 @@ async fn test_show_tables_interpreter() -> Result<()> {
         ];
         common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
     }
-
-    // show full tables like '%da%'.
-    // {
-    //     let plan = PlanParser::parse(ctx.clone(), "show full tables like '%da%'").await?;
-    //     let executor = InterpreterFactory::get(ctx.clone(), plan.clone())?;
-    //     assert_eq!(executor.name(), "ShowTablesInterpreter");
-    //     let stream = executor.execute(None).await?;
-    //     let result = stream.try_collect::<Vec<_>>().await?;
-    //     let expected = vec![
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| Tables_in_db1 | Table_type | table_catalog | engine | create_time                   | num_rows | data_size | data_compressed_size | index_size |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| data          | BASE TABLE | db1           | FUSE   | 1970-01-01 00:00:00.000 +0000 | 0        | 0         | 0                    | NULL       |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //     ];
-    //     common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
-    // }
 
     // show tables != 'data'.
     {
@@ -140,27 +105,6 @@ async fn test_show_tables_interpreter() -> Result<()> {
         common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
     }
 
-    // show full tables != 'data'.
-    // {
-    //     let plan = PlanParser::parse(
-    //         ctx.clone(),
-    //         "show full tables where Tables_in_db1 != 'data'",
-    //     )
-    //     .await?;
-    //     let executor = InterpreterFactory::get(ctx.clone(), plan.clone())?;
-    //     assert_eq!(executor.name(), "ShowTablesInterpreter");
-    //     let stream = executor.execute(None).await?;
-    //     let result = stream.try_collect::<Vec<_>>().await?;
-    //     let expected = vec![
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| Tables_in_db1 | Table_type | table_catalog | engine | create_time                   | num_rows | data_size | data_compressed_size | index_size |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| bend          | BASE TABLE | db1           | FUSE   | 1970-01-01 00:00:00.000 +0000 | 0        | 0         | 0                    | NULL       |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //     ];
-    //     common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
-    // }
-
     // show tables from db1.
     {
         let plan = PlanParser::parse(ctx.clone(), "show tables from db1").await?;
@@ -178,24 +122,6 @@ async fn test_show_tables_interpreter() -> Result<()> {
         ];
         common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
     }
-
-    // show full tables from db1.
-    // {
-    //     let plan = PlanParser::parse(ctx.clone(), "show full tables from db1").await?;
-    //     let executor = InterpreterFactory::get(ctx.clone(), plan.clone())?;
-    //     assert_eq!(executor.name(), "ShowTablesInterpreter");
-    //     let stream = executor.execute(None).await?;
-    //     let result = stream.try_collect::<Vec<_>>().await?;
-    //     let expected = vec![
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| Tables_in_db1 | Table_type | table_catalog | engine | create_time                   | num_rows | data_size | data_compressed_size | index_size |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| bend          | BASE TABLE | db1           | FUSE   | 1970-01-01 00:00:00.000 +0000 | 0        | 0         | 0                    | NULL       |",
-    //         "| data          | BASE TABLE | db1           | FUSE   | 1970-01-01 00:00:00.000 +0000 | 0        | 0         | 0                    | NULL       |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //     ];
-    //     common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
-    // }
 
     // show tables in db1.
     {
@@ -215,25 +141,6 @@ async fn test_show_tables_interpreter() -> Result<()> {
         ];
         common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
     }
-
-    // show full tables in db1.
-    // {
-    //     let plan = PlanParser::parse(ctx.clone(), "show full tables in db1").await?;
-    //     let executor = InterpreterFactory::get(ctx.clone(), plan.clone())?;
-    //     assert_eq!(executor.name(), "ShowTablesInterpreter");
-    //     let stream = executor.execute(None).await?;
-    //     let result = stream.try_collect::<Vec<_>>().await?;
-
-    //     let expected = vec![
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| Tables_in_db1 | Table_type | table_catalog | engine | create_time                   | num_rows | data_size | data_compressed_size | index_size |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //         "| bend          | BASE TABLE | db1           | FUSE   | 1970-01-01 00:00:00.000 +0000 | 0        | 0         | 0                    | NULL       |",
-    //         "| data          | BASE TABLE | db1           | FUSE   | 1970-01-01 00:00:00.000 +0000 | 0        | 0         | 0                    | NULL       |",
-    //         "+---------------+------------+---------------+--------+-------------------------------+----------+-----------+----------------------+------------+",
-    //     ];
-    //     common_datablocks::assert_blocks_sorted_eq(expected, result.as_slice());
-    // }
 
     // Teardown.
     {
