f9c68cd4b042eeaf913297e0629572740b1b69ee
zhang2014 <coswde@gmail.com>
2022-07-13 12:26:10 +0800

    refactor(processor): implement hive source in new processor

---
+++ query/src/catalogs/hive/hive_table_source.rs
@@ -0,0 +1,142 @@
+// Copyright 2022 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::any::Any;
+use std::sync::Arc;
+use common_base::base::{Progress, ProgressValues};
+use common_datablocks::DataBlock;
+use common_planners::PartInfoPtr;
+use crate::pipelines::new::processors::port::OutputPort;
+use crate::pipelines::new::processors::processor::{Event, ProcessorPtr};
+use crate::sessions::QueryContext;
+use crate::storages::hive::HiveParquetBlockReader;
+use common_exception::{ErrorCode, Result};
+use crate::catalogs::hive::hive_table_source::State::Generated;
+use crate::pipelines::new::processors::Processor;
+
+enum State {
+    ReadData(PartInfoPtr),
+    Deserialize(usize, Vec<Vec<u8>>),
+    Generated(Option<PartInfoPtr>, DataBlock),
+    Finish,
+}
+
+pub struct HiveTableSource {
+    state: State,
+    ctx: Arc<QueryContext>,
+    scan_progress: Arc<Progress>,
+    block_reader: Arc<HiveParquetBlockReader>,
+    output: Arc<OutputPort>,
+}
+
+impl HiveTableSource {
+    pub fn create(ctx: Arc<QueryContext>, output: Arc<OutputPort>, block_reader: Arc<HiveParquetBlockReader>) -> Result<ProcessorPtr> {
+        let scan_progress = ctx.get_scan_progress();
+        let mut partitions = ctx.try_get_partitions(1)?;
+        match partitions.is_empty() {
+            true => Ok(ProcessorPtr::create(Box::new(HiveTableSource {
+                ctx,
+                output,
+                block_reader,
+                scan_progress,
+                state: State::Finish,
+            }))),
+            false => Ok(ProcessorPtr::create(Box::new(HiveTableSource {
+                ctx,
+                output,
+                block_reader,
+                scan_progress,
+                state: State::ReadData(partitions.remove(0)),
+            }))),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl Processor for HiveTableSource {
+    fn name(&self) -> &'static str {
+        "HiveEngineSource"
+    }
+
+    fn as_any(&mut self) -> &mut dyn Any {
+        self
+    }
+
+    fn event(&mut self) -> Result<Event> {
+        if matches!(self.state, State::Finish) {
+            self.output.finish();
+            return Ok(Event::Finished);
+        }
+
+        if self.output.is_finished() {
+            return Ok(Event::Finished);
+        }
+
+        if !self.output.can_push() {
+            return Ok(Event::NeedConsume);
+        }
+
+        if matches!(self.state, State::Generated(_, _)) {
+            if let Generated(part, data_block) = std::mem::replace(&mut self.state, State::Finish) {
+                self.state = match part {
+                    None => State::Finish,
+                    Some(part) => State::ReadData(part),
+                };
+
+                self.output.push_data(Ok(data_block));
+                return Ok(Event::NeedConsume);
+            }
+        }
+
+        match self.state {
+            State::Finish => Ok(Event::Finished),
+            State::ReadData(_) => Ok(Event::Async),
+            State::Deserialize(_, _) => Ok(Event::Sync),
+            State::Generated(_, _) => Err(ErrorCode::LogicalError("It's a bug.")),
+        }
+    }
+
+    fn process(&mut self) -> Result<()> {
+        match std::mem::replace(&mut self.state, State::Finish) {
+            State::Deserialize(num_rows, chunks) => {
+                let data_block = self.block_reader.deserialize(chunks, num_rows)?;
+                let mut partitions = self.ctx.try_get_partitions(1)?;
+
+                let progress_values = ProgressValues {
+                    rows: data_block.num_rows(),
+                    bytes: data_block.memory_size(),
+                };
+                self.scan_progress.incr(&progress_values);
+
+                self.state = match partitions.is_empty() {
+                    true => State::Generated(None, data_block),
+                    false => State::Generated(Some(partitions.remove(0)), data_block),
+                };
+                Ok(())
+            }
+            _ => Err(ErrorCode::LogicalError("It's a bug.")),
+        }
+    }
+
+    async fn async_process(&mut self) -> Result<()> {
+        match std::mem::replace(&mut self.state, State::Finish) {
+            State::ReadData(part) => {
+                let (num_rows, chunks) = self.block_reader.read_columns_data(part.clone()).await?;
+                self.state = State::Deserialize(num_rows, chunks);
+                Ok(())
+            }
+            _ => Err(ErrorCode::LogicalError("It's a bug.")),
+        }
+    }
+}
