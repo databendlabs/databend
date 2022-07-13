e5b621c6e7586df7fdbf04db37dac70c650d9e34
GL.Ruan <ruanguiliang_34@whu.edu.cn>
2022-07-13 11:17:06 +0800

    show all pip install log

--- scripts/setup/dev_setup.sh
+++ scripts/setup/dev_setup.sh
@@ -546,11 +546,11 @@ if [[ "$INSTALL_DEV_TOOLS" == "true" ]]; then
 		install_pkg py3-pip "$PACKAGE_MANAGER"
 		install_pkg libffi-dev "$PACKAGE_MANAGER"
 	fi
-	python3 -m pip install --quiet boto3 "moto[all]" yapf shfmt-py toml yamllint
+	python3 -m pip install boto3 "moto[all]" yapf shfmt-py toml yamllint
 	# drivers
-	python3 -m pip install --quiet mysql-connector-python pymysql sqlalchemy clickhouse_driver
+	python3 -m pip install mysql-connector-python pymysql sqlalchemy clickhouse_driver
 	# sqllogic dependencies
-	python3 -m pip install --quiet mysql-connector six PyHamcrest requests environs fire
+	python3 -m pip install mysql-connector six PyHamcrest requests environs fire
 	# fuzz dependencies
 	python3 -m pip install fuzzingbook
 
