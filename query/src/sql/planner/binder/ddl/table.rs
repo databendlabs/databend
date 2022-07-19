8cbb921ce182f6ed211e6d5ceafb4ab8a2991484
sundyli <543950155@qq.com>
2022-07-19 10:52:19 +0800

    remove copy v1

--- query/src/sql/planner/binder/ddl/table.rs
+++ query/src/sql/planner/binder/ddl/table.rs
@@ -34,6 +34,7 @@ use common_planners::OptimizeTableAction;
 use common_planners::*;
 use common_tracing::tracing;
 
+use crate::catalogs::DatabaseCatalog;
 use crate::sessions::TableContext;
 use crate::sql::binder::scalar::ScalarBinder;
 use crate::sql::binder::Binder;
