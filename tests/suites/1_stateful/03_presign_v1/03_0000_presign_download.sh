b308b809a79ec0b9135dcd3383f4678b50687d14
Xuanwo <github@xuanwo.io>
2022-07-20 22:12:04 +0800

    Add presign test for old planner
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ tests/suites/1_stateful/03_presign_v1/03_0000_presign_download.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. "$CURDIR"/../../../shell_env.sh
+
+echo "drop stage if exists presign_stage" | $MYSQL_CLIENT_CONNECT
+
+aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/admin/data/ontime_200.csv s3://testbucket/admin/stage/presign_stage/ontime_200.csv >/dev/null 2>&1
+
+echo "CREATE STAGE presign_stage;" | $MYSQL_CLIENT_CONNECT
+
+# Here is the a magic of curl.
+# -s: make curl silent to avoid pollute our result.
+# -w "%{http_code}": only print http code so that we can use this for test.
+# -o /dev/null: redirect content to /dev/null to avoid pollute our result.
+# cut -f 3: get the third value of output - the url.
+curl -s -w "%{http_code}\n" -o /dev/null "`echo "PRESIGN @presign_stage/ontime_200.csv" | $MYSQL_CLIENT_CONNECT | cut -f 3`"
+
+## Drop table.
+echo "drop stage if exists presign_stage" | $MYSQL_CLIENT_CONNECT
---
+++ tests/suites/1_stateful/03_presign_v1/03_0001_presign_upload.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. "$CURDIR"/../../../shell_env.sh
+
+echo "drop stage if exists presign_stage" | $MYSQL_CLIENT_CONNECT
+
+echo "CREATE STAGE presign_stage;" | $MYSQL_CLIENT_CONNECT
+
+# Most arguments is the same with previous, except:
+# -X PUT: Specify the http method
+curl -s -w "%{http_code}\n" -X PUT -o /dev/null "`echo "PRESIGN UPLOAD @presign_stage/hello_world.txt" | $MYSQL_CLIENT_CONNECT | cut -f 3`" -d "Hello, World!"
+
+# LIST will output file's updated time, so we only take first three of output:
+# file_name, file_size, file_md5
+echo "LIST @presign_stage/" | $MYSQL_CLIENT_CONNECT | awk '{print $1,$2,$3}';
+
+## Drop table.
+echo "drop stage if exists presign_stage" | $MYSQL_CLIENT_CONNECT
