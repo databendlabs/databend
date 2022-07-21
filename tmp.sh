d8733afa595a8714af933d017f08c41371ae8afb
edPanda <y1433011488@outlook.com>
2022-07-22 01:21:31 +0800

    delete tmp.sh

--- tmp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for t in customer lineitem nation orders partsupp part region supplier
-do
-    echo "$t"
-    curl -XPUT 'http://root:@127.0.0.1:8000/v1/streaming_load' -H 'insert_sql: insert into tpch.'$t' format CSV' -H 'skip_header: 0' -H 'field_delimiter:|' -H 'record_delimiter: \n' -F 'upload=@"data/'$t'.tbl"'
-done
