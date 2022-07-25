74fd6df1f074349f6ebe50510aa56fae5444416c
GL.Ruan <ruanguiliang_34@whu.edu.cn>
2022-07-25 16:47:37 +0800

    migration stateless test 05_ddl and 06_show

--- tests/suites/0_stateless/05_ddl/05_0011_ddl_drop_udf.sql
+++
@@ -1,8 +0,0 @@
-DROP FUNCTION IF EXISTS isnotempty;
-
-CREATE FUNCTION isnotempty AS (p) -> not(is_null(p));
-DROP FUNCTION isnotempty;
-
-DROP FUNCTION IF EXISTS isnotempty;
-
-DROP FUNCTION isnotempty; -- {ErrorCode 2602}
