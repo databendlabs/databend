1afa48bb48eb02b91c4ffa3d524ee27654c1a25b
taichong <tai_chong@foxmail.com>
2022-07-27 01:37:04 +0800

    fix(planner): err return

--- common/ast/src/parser/token.rs
+++ common/ast/src/parser/token.rs
@@ -880,7 +880,7 @@ impl TokenKind {
             | TokenKind::WHEN => true,
             | TokenKind::ARRAY
             | TokenKind::AS
-            | TokenKind::CHAR => false,
+            // | TokenKind::CHAR
             // | TokenKind::CHARACTER
             | TokenKind::CREATE
             | TokenKind::EXCEPT
