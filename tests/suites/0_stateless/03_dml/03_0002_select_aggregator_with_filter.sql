dc25f07aea3e05484c029fe5aba723cdbc706659
GL.Ruan <ruanguiliang_34@whu.edu.cn>
2022-07-27 10:26:45 +0800

    migration stateless test 03_dml and 04_explain
    explain has some test about cluster, keep it in stateless, did not remove now.

--- tests/suites/0_stateless/03_dml/03_0002_select_aggregator_with_filter.sql
+++
@@ -1,2 +0,0 @@
-SELECT max(number) FROM numbers_mt (10) where number > 99999999998;
-SELECT max(number) FROM numbers_mt (10) where number > 2;
--- tests/suites/0_stateless/03_dml/03_0009_select_exists.sql
+++
@@ -1,4 +0,0 @@
-select number from numbers_mt(10) where number > 5  and exists (select name from system.settings) order by number asc;
-select number from numbers_mt(10) where number > 5  and exists (select name from system.settings) and exists (select number from numbers_mt(10)) order by number asc;
-select number from numbers_mt(10) where number > 5  and exists (select name from system.settings where exists (select number from numbers_mt(10))) order by number asc;
-select number from numbers_mt(20) where number > 15  and not exists (select number from numbers_mt(5) where number > 10) order by number asc;
