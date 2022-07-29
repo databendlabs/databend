50ac2edd037cf95c7ede8955d12fe6dabef574d8
Xuanwo <github@xuanwo.io>
2022-07-29 11:52:39 +0800

    Remove not used statement
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- query/src/sql/sql_parser.rs
+++ query/src/sql/sql_parser.rs
@@ -15,13 +15,11 @@
 // Borrow from apache/arrow/rust/datafusion/src/sql/sql_parser
 // See notice.md
 
-use std::collections::BTreeMap;
 use std::time::Instant;
 
 use common_exception::ErrorCode;
 use metrics::histogram;
 use sqlparser::ast::Expr;
-use sqlparser::ast::Value;
 use sqlparser::dialect::keywords::Keyword;
 use sqlparser::dialect::Dialect;
 use sqlparser::dialect::GenericDialect;
@@ -32,13 +30,7 @@ use sqlparser::tokenizer::Token;
 use sqlparser::tokenizer::Tokenizer;
 use sqlparser::tokenizer::Whitespace;
 
-use super::statements::DfShowRoles;
 use crate::sessions::SessionType;
-use crate::sql::statements::DfShowEngines;
-use crate::sql::statements::DfShowMetrics;
-use crate::sql::statements::DfShowProcessList;
-use crate::sql::statements::DfShowSettings;
-use crate::sql::statements::DfShowUsers;
 use crate::sql::DfHint;
 use crate::sql::DfStatement;
 
@@ -174,125 +166,6 @@ impl<'a> DfParser<'a> {
             },
             Token::LParen => self.parse_query(),
             unexpected => self.expected("an SQL statement", unexpected),
-        }
-    }
-
-    /// This is a copy from sqlparser
-    /// Parse a literal value (numbers, strings, date/time, booleans)
-    #[allow(dead_code)]
-    fn parse_value(&mut self) -> Result<Value, ParserError> {
-        match self.parser.next_token() {
-            Token::Word(w) => match w.keyword {
-                Keyword::TRUE => Ok(Value::Boolean(true)),
-                Keyword::FALSE => Ok(Value::Boolean(false)),
-                Keyword::NULL => Ok(Value::Null),
-                Keyword::NoKeyword if w.quote_style.is_some() => match w.quote_style {
-                    Some('"') => Ok(Value::DoubleQuotedString(w.value)),
-                    Some('\'') => Ok(Value::SingleQuotedString(w.value)),
-                    _ => self.expected("A value?", Token::Word(w))?,
-                },
-                _ => self.expected("a concrete value", Token::Word(w)),
-            },
-            // The call to n.parse() returns a bigdecimal when the
-            // bigdecimal feature is enabled, and is otherwise a no-op
-            // (i.e., it returns the input string).
-            Token::Number(ref n, l) => match n.parse() {
-                Ok(n) => Ok(Value::Number(n, l)),
-                Err(e) => parser_err!(format!("Could not parse '{}' as number: {}", n, e)),
-            },
-            Token::SingleQuotedString(ref s) => Ok(Value::SingleQuotedString(s.to_string())),
-            Token::NationalStringLiteral(ref s) => Ok(Value::NationalStringLiteral(s.to_string())),
-            Token::HexStringLiteral(ref s) => Ok(Value::HexStringLiteral(s.to_string())),
-            unexpected => self.expected("a value", unexpected),
-        }
-    }
-
-    pub(crate) fn parse_value_or_ident(&mut self) -> Result<String, ParserError> {
-        match self.parser.next_token() {
-            Token::Word(w) => match w.keyword {
-                Keyword::TRUE => Ok("true".to_string()),
-                Keyword::FALSE => Ok("false".to_string()),
-                Keyword::NULL => Ok("null".to_string()),
-                _ => Ok(w.value),
-            },
-            // The call to n.parse() returns a bigdecimal when the
-            // bigdecimal feature is enabled, and is otherwise a no-op
-            // (i.e., it returns the input string).
-            Token::Number(n, _) => Ok(n),
-            Token::SingleQuotedString(s) => Ok(s),
-            Token::NationalStringLiteral(s) => Ok(s),
-            Token::HexStringLiteral(s) => Ok(s),
-            unexpected => self.expected("a value", unexpected),
-        }
-    }
-
-    pub(crate) fn parse_options(&mut self) -> Result<BTreeMap<String, String>, ParserError> {
-        let mut options = BTreeMap::new();
-        loop {
-            let name = self.parser.parse_identifier();
-            if name.is_err() {
-                self.parser.prev_token();
-                break;
-            }
-            let name = name.unwrap();
-            if !self.parser.consume_token(&Token::Eq) {
-                // only paired values are considered as options
-                self.parser.prev_token();
-                break;
-            }
-            let value = self.parse_value_or_ident()?;
-
-            options.insert(name.to_string().to_lowercase(), value);
-        }
-        Ok(options)
-    }
-
-    pub(crate) fn parse_list(&mut self, token: &Token) -> Result<Vec<String>, ParserError> {
-        let mut list: Vec<String> = vec![];
-        loop {
-            let value = self.parse_value_or_ident()?;
-            list.push(value.to_string());
-
-            if !self.parser.consume_token(token) {
-                break;
-            }
-        }
-        Ok(list)
-    }
-
-    pub(crate) fn consume_token(&mut self, expected: &str) -> bool {
-        if self.parser.peek_token().to_string().to_uppercase() == *expected.to_uppercase() {
-            self.parser.next_token();
-            true
-        } else {
-            false
-        }
-    }
-
-    pub(crate) fn consume_token_until_or_end(&mut self, until_tokens: Vec<&str>) -> Vec<String> {
-        let mut tokens = vec![];
-
-        loop {
-            let next_token = self.parser.peek_token();
-
-            if next_token == Token::EOF
-                || next_token == Token::SemiColon
-                || until_tokens.contains(&next_token.to_string().to_uppercase().as_str())
-            {
-                break;
-            }
-
-            tokens.push(self.parser.next_token().to_string());
-        }
-
-        tokens
-    }
-
-    pub(crate) fn expect_token(&mut self, expected: &str) -> Result<(), ParserError> {
-        if self.consume_token(expected) {
-            Ok(())
-        } else {
-            self.expected(expected, self.parser.peek_token())
         }
     }
 
