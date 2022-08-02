0e6f61eba65a8e46ccb1afeb7d1886ef3ab91048
dantengsky <dantengsky@gmail.com>
2022-08-02 16:22:36 +0800

    try fix sqllogic test

--- scripts/ci/ci-run-sqllogic-tests.sh
+++ scripts/ci/ci-run-sqllogic-tests.sh
@@ -9,4 +9,4 @@ SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 cd "$SCRIPT_PATH/../../tests/logictest" || exit
 
 echo "Starting databend-sqllogic tests"
-python3 main.py
+python3 main.py $@
