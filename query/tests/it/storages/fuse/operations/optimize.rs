c3415d7100bbc78d3b16ac8c26a79f6d692ce93e
dantengsky <dantengsky@gmail.com>
2022-08-03 14:07:30 +0800

    adjust test cases

--- query/tests/it/storages/fuse/operations/optimize.rs
+++ query/tests/it/storages/fuse/operations/optimize.rs
@@ -33,7 +33,6 @@ async fn test_fuse_snapshot_optimize() -> Result<()> {
 
 #[tokio::test]
 async fn test_fuse_snapshot_optimize_purge() -> Result<()> {
-    // TODO check index
     do_purge_test("explicit pure", "purge").await
 }
 
@@ -48,7 +47,7 @@ async fn do_purge_test(case_name: &str, operation: &str) -> Result<()> {
     let tbl = fixture.default_table_name();
     let qry = format!("optimize table {}.{} {}", db, tbl, operation);
 
-    // insert, and then insert overwrite (1 snapshot, 1 segment, 1 block for each insertion);
+    // insert, and then insert overwrite (1 snapshot, 1 segment, 1 data block, 1 index block for each insertion);
     do_insertions(&fixture).await?;
 
     // execute the query
@@ -56,7 +55,7 @@ async fn do_purge_test(case_name: &str, operation: &str) -> Result<()> {
     execute_command(ctx, &qry).await?;
 
     // there should be only 1 snapshot, 1 segment, 1 block left, and 0 index left
-    check_data_dir(&fixture, case_name, 1, 1, 1, 0).await;
+    check_data_dir(&fixture, case_name, 1, 1, 1, 1).await;
     history_should_have_only_one_item(&fixture, case_name).await
 }
 
