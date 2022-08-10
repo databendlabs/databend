94536e9dc2549171c580cd8b5b76ef05e7f38937
Chojan Shang <psiace@outlook.com>
2022-08-10 17:39:50 +0800

    fix: edit test path

--- src/query/tests/it/tests/tls_constants.rs
+++ src/query/tests/it/tests/tls_constants.rs
@@ -12,15 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub const TEST_CA_CERT: &str = "../tests/certs/ca.pem";
-pub const TEST_SERVER_CERT: &str = "../tests/certs/server.pem";
-pub const TEST_SERVER_KEY: &str = "../tests/certs/server.key";
+pub const TEST_CA_CERT: &str = "../../tests/certs/ca.pem";
+pub const TEST_SERVER_CERT: &str = "../../tests/certs/server.pem";
+pub const TEST_SERVER_KEY: &str = "../../tests/certs/server.key";
 pub const TEST_CN_NAME: &str = "localhost";
 
-pub const TEST_TLS_CA_CERT: &str = "../tests/certs/tls/cfssl/ca/ca.pem";
-pub const TEST_TLS_SERVER_CERT: &str = "../tests/certs/tls/cfssl/server/server.pem";
-pub const TEST_TLS_SERVER_KEY: &str = "../tests/certs/tls/cfssl/server/pkcs8-server-key.pem";
+pub const TEST_TLS_CA_CERT: &str = "../../tests/certs/tls/cfssl/ca/ca.pem";
+pub const TEST_TLS_SERVER_CERT: &str = "../../tests/certs/tls/cfssl/server/server.pem";
+pub const TEST_TLS_SERVER_KEY: &str = "../../tests/certs/tls/cfssl/server/pkcs8-server-key.pem";
 // pub const TEST_TLS_CLIENT_CERT: &'static str = "../tests/certs/tls/cfssl/client/client.pem";
 // pub const TEST_TLS_CLIENT_KEY: &'static str = "../tests/certs/tls/cfssl/client/pkcs8-client-key.pem";
-pub const TEST_TLS_CLIENT_IDENTITY: &str = "../tests/certs/tls/cfssl/client/client-identity.pfx";
+pub const TEST_TLS_CLIENT_IDENTITY: &str = "../../tests/certs/tls/cfssl/client/client-identity.pfx";
 pub const TEST_TLS_CLIENT_PASSWORD: &str = "databend";
