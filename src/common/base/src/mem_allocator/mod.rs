cbe51869964ee8839a51e96119d03150de07fa06
sundyli <543950155@qq.com>
2022-08-19 22:44:11 +0800

    feat(allocator): add stackful allocator

--- src/common/base/src/mem_allocator/mod.rs
+++ src/common/base/src/mem_allocator/mod.rs
@@ -12,13 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod allocators;
-mod allocators_mmap;
+mod mmap_allocator;
+mod stackful_allocator;
+mod je_allocator;
 
-pub use allocators::Allocator;
+use std::alloc::Layout;
+
+pub use je_allocator::JEAllocator;
 
 #[cfg(feature = "memory-profiling")]
 mod profiling;
 
 #[cfg(feature = "memory-profiling")]
 pub use profiling::dump_profile;
+
+// A new Allocator trait suits databend
+pub unsafe trait Allocator {
+    unsafe fn allocx(&mut self, layout: Layout, clear_mem: bool) -> *mut u8;
+    unsafe fn deallocx(&mut self, ptr: *mut u8, layout: Layout);
+    unsafe fn reallocx(
+        &mut self,
+        ptr: *mut u8,
+        layout: Layout,
+        new_size: usize,
+        clear_mem: bool,
+    ) -> *mut u8 {
+        // SAFETY: the caller must ensure that the `new_size` does not overflow.
+        // `layout.align()` comes from a `Layout` and is thus guaranteed to be valid.
+        let new_layout = Layout::from_size_align_unchecked(new_size, layout.align());
+        // SAFETY: the caller must ensure that `new_layout` is greater than zero.
+        let new_ptr = self.allocx(new_layout, clear_mem);
+        if !new_ptr.is_null() {
+            // SAFETY: the previously allocated block cannot overlap the newly allocated block.
+            // The safety contract for `dealloc` must be upheld by the caller.
+            std::ptr::copy_nonoverlapping(ptr, new_ptr, std::cmp::min(layout.size(), new_size));
+            self.deallocx(ptr, layout);
+        }
+        new_ptr
+    }
+}
