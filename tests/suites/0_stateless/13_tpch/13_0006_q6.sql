97206ba72885edeb8d9ed75044353b92fecb847a
edPanda <y1433011488@outlook.com>
2022-08-21 21:12:07 +0800

    use curl instead of wget

--- tests/suites/0_stateless/13_tpch/13_0006_q6.sql
+++ tests/suites/0_stateless/13_tpch/13_0006_q6.sql
@@ -1,1 +1,9 @@
-
+select
+   truncate(sum(l_extendedprice * l_discount),3) as revenue 
+from
+   lineitem 
+where
+   l_shipdate >= '1994-01-01' 
+   and l_shipdate < date_add(to_date('1994-01-01'), 1, year) 
+   and l_discount between 0.05 and 0.07 
+   and l_quantity < 24;
