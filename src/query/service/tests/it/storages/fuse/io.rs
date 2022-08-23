1f95fcbdbcc4a6158ca12320eda8059a2c9253a1
zhyass <34016424+zhyass@users.noreply.github.com>
2022-08-23 22:55:27 +0800

    fix lint

--- src/query/service/tests/it/storages/fuse/io.rs
+++ src/query/service/tests/it/storages/fuse/io.rs
@@ -17,7 +17,6 @@ use std::sync::Arc;
 
 use common_base::base::tokio;
 use common_datablocks::DataBlock;
-use common_datavalues::prelude::*;
 use common_exception::ErrorCode;
 use common_exception::Result;
 use common_fuse_meta::meta::TableSnapshot;
