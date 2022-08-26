2d5d46f0ca6050fb374a27661985368b8a4ef3c3
leiysky <leiysky@outlook.com>
2022-08-26 17:46:04 +0800

    fix clippy

--- src/query/service/src/sql/optimizer/heuristic/prune_columns.rs
+++ src/query/service/src/sql/optimizer/heuristic/prune_columns.rs
@@ -89,7 +89,7 @@ impl ColumnPruner {
                     push_down_predicates: p.push_down_predicates.clone(),
                     limit: p.limit,
                     order_by: p.order_by.clone(),
-                    statistics: p.statistics.clone(),
+                    statistics: p.statistics,
                 })))
             }
             RelOperator::LogicalInnerJoin(p) => {
