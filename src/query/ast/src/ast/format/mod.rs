f346e2a9ebbdbf83717a5106c5d4992aed6b5a04
leiysky <leiysky@outlook.com>
2022-08-29 17:18:14 +0800

    resolve conflict

--- src/query/ast/src/ast/format/mod.rs
+++ src/query/ast/src/ast/format/mod.rs
@@ -24,13 +24,14 @@ pub use indent_format::*;
 pub use pretty_format::*;
 pub use syntax::pretty_statement;
 
-pub struct FormatTreeNode<T: Display> {
+#[derive(Clone)]
+pub struct FormatTreeNode<T: Display + Clone> {
     payload: T,
     children: Vec<Self>,
 }
 
 impl<T> FormatTreeNode<T>
-where T: Display
+where T: Display + Clone
 {
     pub fn new(payload: T) -> Self {
         Self {
