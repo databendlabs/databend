66e7a962f5e4569821dfd194fde877c71607ff4a
Chasen-Zhang <787025321@qq.com>
2022-09-05 02:01:09 +0800

    fix: add os type

--- website/src/components/Releases/index.tsx
+++ website/src/components/Releases/index.tsx
@@ -11,13 +11,14 @@ import Link from '@docusaurus/Link';
 interface IRow {
   name: string;
   tagName: string;
+  osType: string;
 }
 const Releases: FC = (): ReactElement=> {
   const [releaseData, setReleaseData] = useState<IRow[]>([]);
   const DOWN_LINK = 'https://github.com/datafuselabs/databend/releases/download';
   const columns = [
     {
-      title: 'Assets',
+      title: 'Arch',
       dataIndex: 'name',
       key: 'name',
       render(name:string, record: IRow){
@@ -25,18 +26,23 @@ const Releases: FC = (): ReactElement=> {
         return <a download href={`${DOWN_LINK}/${tagName}/${name}`}>{name}</a>
       }
     },
+    {
+      title: 'Os type',
+      dataIndex: 'osType',
+      key: 'osType'
+    },
   ];
   return (
     <>
       {
       getLatest((tagName: string)=> {
         const data = [
-          { name: `databend-${tagName}-aarch64-apple-darwin.tar.gz`, tagName},
-          { name: `databend-${tagName}-aarch64-unknown-linux-gnu.tar.gz`, tagName},
-          { name: `databend-${tagName}-aarch64-unknown-linux-musl.tar.gz`, tagName},
-          { name: `databend-${tagName}-x86_64-apple-darwin.tar.gz`, tagName},
-          { name: `databend-${tagName}-x86_64-unknown-linux-gnu.tar.gz`, tagName},
-          { name: `databend-${tagName}-x86_64-unknown-linux-musl.tar.gz`, tagName},
+          { name: `databend-${tagName}-aarch64-apple-darwin.tar.gz`, tagName, osType: 'arm macos'},
+          { name: `databend-${tagName}-aarch64-unknown-linux-gnu.tar.gz`, tagName, osType:'arm ubuntu'},
+          { name: `databend-${tagName}-aarch64-unknown-linux-musl.tar.gz`, tagName, osType: 'arm general_linux'},
+          { name: `databend-${tagName}-x86_64-apple-darwin.tar.gz`, tagName, osType: 'x86_64 macos'},
+          { name: `databend-${tagName}-x86_64-unknown-linux-gnu.tar.gz`, tagName, osType: 'x86_64 ubuntu'},
+          { name: `databend-${tagName}-x86_64-unknown-linux-musl.tar.gz`, tagName, osType: 'x86_64 general_linux'},
         ];
         setReleaseData(data);
         })
@@ -44,7 +50,7 @@ const Releases: FC = (): ReactElement=> {
      <Tooltip
       showArrow={false}
       overlayClassName={styles.toolTip}
-      placement="right" 
+      placement="bottom" 
       trigger={['hover']} 
       overlay={<Table rowKey="name" columns={columns} data={releaseData} />}>
       <Link className={clsx("button button--secondary button--lg", styles.link)}>Releases</Link>
