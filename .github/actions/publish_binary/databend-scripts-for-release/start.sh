b12cb07b43854409d34c8185edf59bc80eff2ae5
Chojan Shang <psiace@outlook.com>
2022-09-05 15:38:02 +0800

    chore(scripts): cat log to print logging

--- .github/actions/publish_binary/databend-scripts-for-release/start.sh
+++ .github/actions/publish_binary/databend-scripts-for-release/start.sh
@@ -5,8 +5,8 @@ echo "Deploy new Databend(standalone)"
 ulimit  -n 65535
 nohup bin/databend-meta --config-file=configs/databend-meta.toml 2>&1 >meta.log &
 sleep 3
+cat meta.log
 # export STORAGE_S3_ENABLE_VIRTUAL_HOST_STYLE=true
 nohup bin/databend-query --config-file=configs/databend-query.toml 2>&1 >query.log &
 sleep 3
-echo "Usage: mysql -h127.0.0.1 -P3307 -uroot"
-echo "or: curl -u root: --request POST '127.0.0.1:8001/v1/query/' --data-raw '{\"sql\": \"<your-query>\"}' -H 'Content-Type: application/json'"
+cat query.log
