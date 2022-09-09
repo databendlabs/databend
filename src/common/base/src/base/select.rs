05abf2eb62259b6626db9f7f9ff7ddc4412581ef
zhang2014 <coswde@gmail.com>
2022-09-09 11:29:37 +0800

    fix(cluster): add license head

--- src/common/base/src/base/select.rs
+++ src/common/base/src/base/select.rs
@@ -1,3 +1,17 @@
+// Copyright 2022 Datafuse Labs.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::future::Future;
 use std::pin::Pin;
 use std::task::Context;
@@ -6,10 +20,10 @@ use std::task::Poll;
 use futures::FutureExt;
 
 pub fn select3<A, B, C>(fut1: A, fut2: B, fut3: C) -> Select3<A, B, C>
-where
-    A: Future + Unpin,
-    B: Future + Unpin,
-    C: Future + Unpin,
+    where
+        A: Future + Unpin,
+        B: Future + Unpin,
+        C: Future + Unpin,
 {
     Select3 {
         inner: Some((fut1, fut2, fut3)),
@@ -23,10 +37,10 @@ pub struct Select3<A, B, C> {
 impl<A: Unpin, B: Unpin, C: Unpin> Unpin for Select3<A, B, C> {}
 
 impl<A, B, C> Future for Select3<A, B, C>
-where
-    A: Future + Unpin,
-    B: Future + Unpin,
-    C: Future + Unpin,
+    where
+        A: Future + Unpin,
+        B: Future + Unpin,
+        C: Future + Unpin,
 {
     type Output = Select3Output<(A::Output, B, C), (B::Output, A, C), (C::Output, A, B)>;
 
