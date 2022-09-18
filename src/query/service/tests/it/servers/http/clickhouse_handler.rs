f036b639c72d3a09992763da7d0561905bebc80a
sundy-li <543950155@qq.com>
2022-09-18 12:48:51 +0800

    chore(query): fix tests

--- src/query/service/tests/it/servers/http/clickhouse_handler.rs
+++ src/query/service/tests/it/servers/http/clickhouse_handler.rs
@@ -270,7 +270,7 @@ async fn test_insert_format_ndjson() -> PoemResult<()> {
         let (status, body) = server
             .post("insert into table t1 format JSONEachRow", &body)
             .await;
-        assert_eq!(status, StatusCode::BAD_REQUEST);
+        assert_eq!(status, StatusCode::INTERNAL_SERVER_ERROR);
         assert_error!(body, "column a");
     }
     Ok(())
