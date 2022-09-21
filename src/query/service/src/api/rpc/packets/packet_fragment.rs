89da5106d3b2e3c9e9d1f5552b1aa700407bb141
Xuanwo <github@xuanwo.io>
2022-09-21 10:07:40 +0800

    Remove old interpreters
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/query/service/src/api/rpc/packets/packet_fragment.rs
+++ src/query/service/src/api/rpc/packets/packet_fragment.rs
@@ -17,7 +17,6 @@ use std::fmt::Formatter;
 
 use common_datavalues::DataSchemaRef;
 use common_exception::Result;
-use common_legacy_planners::PlanNode;
 
 use crate::api::DataExchange;
 use crate::sql::executor::PhysicalPlan;
@@ -27,14 +26,12 @@ use crate::sql::executor::PhysicalPlan;
 #[allow(clippy::large_enum_variant)]
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
 pub enum FragmentPayload {
-    PlanV1(PlanNode),
     PlanV2(PhysicalPlan),
 }
 
 impl Debug for FragmentPayload {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         match self {
-            FragmentPayload::PlanV1(plan) => write!(f, "PlanNode({})", plan.name()),
             FragmentPayload::PlanV2(plan) => write!(f, "PhysicalPlan({:?})", plan),
         }
     }
@@ -43,7 +40,6 @@ impl Debug for FragmentPayload {
 impl FragmentPayload {
     pub fn schema(&self) -> Result<DataSchemaRef> {
         match self {
-            FragmentPayload::PlanV1(node) => Ok(node.schema()),
             FragmentPayload::PlanV2(plan) => plan.output_schema(),
         }
     }
