adb1e613285577450d2b9b2d65c0b120e118566a
ClSlaid <cailue@bupt.edu.cn>
2022-09-21 16:37:46 +0800

    fix: correct stateful test
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>

--- tests/suites/1_stateful/00_copy/00_0000_copy_from_ipfs_location.sh
+++ tests/suites/1_stateful/00_copy/00_0000_copy_from_ipfs_location.sh
@@ -18,8 +18,6 @@ copy_from_location_cases=(
   "copy into ontime_200 from 'ipfs://$QMHASH/ontime.csv.zst' CONNECTION = (ENDPOINT_URL='https://ipfs.filebase.io') FILE_FORMAT = (type = 'CSV' field_delimiter = ',' compression = 'zstd'  record_delimiter = '\n' skip_header = 1)"
   # copy bz2 csv
   "copy into ontime_200 from 'ipfs://$QMHASH/ontime.csv.bz2' CONNECTION = (ENDPOINT_URL='https://ipfs.filebase.io') FILE_FORMAT = (type = 'CSV' field_delimiter = ',' compression = 'bz2'  record_delimiter = '\n' skip_header = 1)"
-  # copy xz csv
-  "copy into ontime_200 from 'ipfs://$QMHASH/ontime.csv.xz' CONNECTION = (ENDPOINT_URL='https://ipfs.filebase.io') FILE_FORMAT = (type = 'CSV' field_delimiter = ',' compression = 'xz'  record_delimiter = '\n' skip_header = 1)"
 )
 
 for i in "${copy_from_location_cases[@]}"; do
