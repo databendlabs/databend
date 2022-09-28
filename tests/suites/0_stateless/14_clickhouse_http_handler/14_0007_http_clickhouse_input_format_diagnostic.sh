514bfbbd3a5e659b3cd7b003e4b7c4b032398e0e
Yang Xiufeng <yangxiufeng.c@gmail.com>
2022-09-28 09:19:52 +0800

    test(format): TSV should not use quote.

--- tests/suites/0_stateless/14_clickhouse_http_handler/14_0007_http_clickhouse_input_format_diagnostic.sh
+++ tests/suites/0_stateless/14_clickhouse_http_handler/14_0007_http_clickhouse_input_format_diagnostic.sh
@@ -35,8 +35,8 @@ curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT
 echo -e '\ntsv 1'
 cat << EOF > /tmp/databend_test_tsv_error1.txt
 insert into a(a,b,c) format TSV
-"2023-04-08 01:01:01"	"Hello"	12345678
-1989-023-03 15:23:23	"World"	123456
+2023-04-08 01:01:01	Hello	12345678
+1989-023-03 15:23:23	World	123456
 EOF
 curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/?enable_planner_v2=1" --data-binary @/tmp/databend_test_tsv_error1.txt | grep -c "Date"
 
@@ -44,8 +44,8 @@ curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT
 echo -e '\ntsv 2'
 cat << EOF > /tmp/databend_test_tsv_error2.txt
 insert into a(a,b,c) format TSV
-2023-04-08 01:01:01	"Hello"	12345678
-1989-02-03 15:23:23	"World"	123456 1
+2023-04-08 01:01:01	Hello	12345678
+1989-02-03 15:23:23	World	123456 1
 EOF
 curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/?enable_planner_v2=1" --data-binary @/tmp/databend_test_tsv_error2.txt | grep -c "column 2 (c int32): bad field end"
 
