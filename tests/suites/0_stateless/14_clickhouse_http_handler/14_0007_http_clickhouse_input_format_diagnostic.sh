0c27657e362190420019f745b271a1bf08f11c27
Yang Xiufeng <yangxiufeng.c@gmail.com>
2022-09-28 09:21:43 +0800

    test(format): CSV should not have space after delimiter by default.

--- tests/suites/0_stateless/14_clickhouse_http_handler/14_0007_http_clickhouse_input_format_diagnostic.sh
+++ tests/suites/0_stateless/14_clickhouse_http_handler/14_0007_http_clickhouse_input_format_diagnostic.sh
@@ -18,7 +18,7 @@ curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT
 # 2 one more column
 echo -e '\ncsv 2'
 cat << EOF > /tmp/databend_test_csv_error2.txt
-insert into a(a,b,c) format CSV "2023-04-08 01:01:01", "Hello",12345678,1
+insert into a(a,b,c) format CSV "2023-04-08 01:01:01","Hello",12345678,1
 
 EOF
 curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/?enable_planner_v2=1" --data-binary @/tmp/databend_test_csv_error2.txt | grep -c "allow ending"
