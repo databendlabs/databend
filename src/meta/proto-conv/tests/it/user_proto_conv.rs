30cb75c5ebb37ef5d812e58188d45d0927fab01d
ClSlaid <cailue@bupt.edu.cn>
2022-10-09 19:57:15 +0800

    feat: oss stage support
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>

--- src/meta/proto-conv/tests/it/user_proto_conv.rs
+++ src/meta/proto-conv/tests/it/user_proto_conv.rs
@@ -28,6 +28,7 @@ use common_proto_conv::VER;
 use common_protos::pb;
 use common_storage::StorageFsConfig;
 use common_storage::StorageGcsConfig;
+use common_storage::StorageOssConfig;
 use common_storage::StorageParams;
 use common_storage::StorageS3Config;
 use enumflags2::make_bitflags;
@@ -139,6 +140,42 @@ pub(crate) fn test_gcs_stage_info() -> mt::UserStageInfo {
                 bucket: "my_bucket".to_string(),
                 root: "/data/files".to_string(),
                 credential: "my_credential".to_string(),
+            }),
+        },
+        file_format_options: mt::FileFormatOptions {
+            format: mt::StageFileFormatType::Json,
+            skip_header: 1024,
+            field_delimiter: "|".to_string(),
+            record_delimiter: "//".to_string(),
+            compression: mt::StageFileCompression::Bz2,
+        },
+        copy_options: mt::CopyOptions {
+            on_error: mt::OnErrorMode::SkipFileNum(666),
+            size_limit: 1038,
+            purge: true,
+            single: false,
+            max_file_size: 0,
+        },
+        comment: "test".to_string(),
+        ..Default::default()
+    }
+}
+
+// Version 13: add OSS as a stage backend
+pub(crate) fn test_oss_stage_info() -> mt::UserStageInfo {
+    mt::UserStageInfo {
+        stage_name: "oss://my_bucket/data/files".to_string(),
+        stage_type: mt::StageType::External,
+        stage_params: mt::StageParams {
+            storage: StorageParams::Oss(StorageOssConfig {
+                endpoint_url: "https://oss-cn-litang.example.com".to_string(),
+                bucket: "my_bucket".to_string(),
+                root: "/data/files".to_string(),
+
+                access_key_id: "access_key_id".to_string(),
+                access_key_secret: "access_key_secret".to_string(),
+                oidc_token: "oidc_token".to_string(),
+                role_arn: "role_arn".to_string(),
             }),
         },
         file_format_options: mt::FileFormatOptions {
@@ -293,6 +330,25 @@ fn test_user_incompatible() -> anyhow::Result<()> {
         );
     }
 
+    {
+        let oss_stage_info = test_oss_stage_info();
+        let mut p = oss_stage_info.to_pb()?;
+        p.ver = VER + 1;
+        p.min_compatible = VER + 1;
+
+        let res = mt::UserStageInfo::from_pb(p);
+        assert_eq!(
+            Incompatible {
+                reason: format!(
+                    "executable ver={} is smaller than the message min compatible ver: {}",
+                    VER,
+                    VER + 1
+                )
+            },
+            res.unwrap_err()
+        )
+    }
+
     Ok(())
 }
 
@@ -348,6 +404,15 @@ fn test_build_user_pb_buf() -> anyhow::Result<()> {
         let mut buf = vec![];
         common_protos::prost::Message::encode(&p, &mut buf)?;
         println!("gcs_stage_info: {:?}", buf);
+    }
+
+    // Stage on OSS, supported in version >= 13.
+    {
+        let oss_stage_info = test_oss_stage_info();
+        let p = oss_stage_info.to_pb()?;
+        let mut buf = vec![];
+        common_protos::prost::Message::encode(&p, &mut buf)?;
+        println!("oss_stage_info: {:?}", buf);
     }
 
     Ok(())
