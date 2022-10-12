8e5edec1758ab5a0e37613820daeaa3bd748abd5
Yang Xiufeng <yangxiufeng.c@gmail.com>
2022-10-12 23:44:15 +0800

    feat(input format): add BeyondEndReader.

--- src/query/pipeline/sources/src/processors/sources/input_formats/mod.rs
+++ src/query/pipeline/sources/src/processors/sources/input_formats/mod.rs
@@ -12,6 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+mod beyond_end_reader;
 mod delimiter;
 mod impls;
 mod input_context;
