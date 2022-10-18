c20db0480c3027aea215509a4cce24ecf3991d61
zhyass <34016424+zhyass@users.noreply.github.com>
2022-10-18 23:20:21 +0800

    update tests

--- src/query/service/tests/it/storages/fuse/operations/mutation/compact_mutator.rs
+++ src/query/service/tests/it/storages/fuse/operations/mutation/compact_mutator.rs
@@ -16,111 +16,79 @@ use std::sync::Arc;
 
 use common_base::base::tokio;
 use common_catalog::table::Table;
-use common_datablocks::DataBlock;
+use common_catalog::table_mutator::TableMutator;
 use common_exception::Result;
 use common_storages_fuse::FuseTable;
-use common_streams::SendableDataBlockStream;
+use databend_query::interpreters::CreateTableInterpreterV2;
+use databend_query::interpreters::Interpreter;
 use databend_query::pipelines::executor::ExecutorSettings;
 use databend_query::pipelines::executor::PipelineCompleteExecutor;
+use databend_query::sessions::QueryContext;
 use databend_query::sessions::TableContext;
-use futures_util::TryStreamExt;
 
 use crate::storages::fuse::table_test_fixture::execute_command;
 use crate::storages::fuse::table_test_fixture::execute_query;
+use crate::storages::fuse::table_test_fixture::expects_ok;
 use crate::storages::fuse::table_test_fixture::TestFixture;
 
 #[tokio::test]
-async fn test_compact_normal_case() -> Result<()> {
+async fn test_compact() -> Result<()> {
     let fixture = TestFixture::new().await;
     let ctx = fixture.ctx();
+    let create_table_plan = fixture.create_normal_table_plan();
 
-    // setup
-    let qry = "create table t(c int)  row_per_block=5";
-    execute_command(ctx.clone(), qry).await?;
-
-    let catalog = ctx.get_catalog("default")?;
-
-    // insert
-    let qry = "insert into t values(1)";
-    for _ in 0..9 {
-        execute_command(ctx.clone(), qry).await?;
-    }
-
-    // check count
-    let count_qry = "select count(*) from t";
-    let stream = execute_query(fixture.ctx(), count_qry).await?;
-    assert_eq!(9, check_count(stream).await?);
-
-    
-    // compact
-    let settings = ctx.get_settings();
-    settings.set_max_threads(1)?;
-    let table = catalog
-        .get_table(ctx.get_tenant().as_str(), "default", "t")
-        .await?;
-    let fuse_table = FuseTable::try_from_table(table.as_ref())?;
-    let mut pipeline = common_pipeline_core::Pipeline::create();
-    let mutator = fuse_table
-        .compact(ctx.clone(), &mut pipeline)
-        .await?;
-    assert!(mutator.is_some());
-    let mutator = mutator.unwrap();
-    pipeline.set_max_threads(1);
-    let executor_settings = ExecutorSettings::try_create(&settings)?;
-    let executor = PipelineCompleteExecutor::try_create(pipeline, executor_settings)?;
-    ctx.set_executor(Arc::downgrade(&executor.get_inner()));
-    executor.execute()?;
-    drop(executor);
-    mutator.try_commit(table).await?;
-
-    // check segment count
-    let qry = "select segment_count as count from fuse_snapshot('default', 't') limit 1";
-    let stream = execute_query(fixture.ctx(), qry).await?;
-    // after compact, in our case, there should be only 1 segment left
-    assert_eq!(1, check_count(stream).await?);
-
-    // check block count
-    let qry = "select block_count as count from fuse_snapshot('default', 't') limit 1";
-    let stream = execute_query(fixture.ctx(), qry).await?;
-    assert_eq!(2, check_count(stream).await?);
-    Ok(())
-}
-
-
-#[tokio::test]
-async fn test_compact_conflict() -> Result<()> {
-    let fixture = TestFixture::new().await;
-    let ctx = fixture.ctx();
-
-    // setup
-    let qry = "create table t(c int)  row_per_block=5";
-    execute_command(ctx.clone(), qry).await?;
-
-    let catalog = ctx.get_catalog("default")?;
+    // create test table
+    let tbl_name = create_table_plan.table.clone();
+    let db_name = create_table_plan.database.clone();
+    let interpreter = CreateTableInterpreterV2::try_create(ctx.clone(), create_table_plan)?;
+    interpreter.execute(ctx.clone()).await?;
 
     // insert
     for i in 0..9 {
-        let qry =  format!("insert into t values({})", i) ;
+        let qry = format!("insert into {}.{}(id) values({})", db_name, tbl_name, i);
         execute_command(ctx.clone(), qry.as_str()).await?;
     }
 
-    // check count
-    let count_qry = "select count(*) from t";
-    let stream = execute_query(fixture.ctx(), count_qry).await?;
-    assert_eq!(9, check_count(stream).await?);
-
-    
     // compact
+    let catalog = ctx.get_catalog(fixture.default_catalog_name().as_str())?;
+    let table = catalog
+        .get_table(ctx.get_tenant().as_str(), &db_name, &tbl_name)
+        .await?;
+    let mutator = build_mutator(ctx.clone(), table.clone()).await?;
+
+    // compact commit
+    mutator.try_commit(table).await?;
+
+    // check count
+    let expected = vec![
+        "+---------------+-------+",
+        "| segment_count | count |",
+        "+---------------+-------+",
+        "| 1             | 1     |",
+        "+---------------+-------+",
+    ];
+    let qry = format!(
+        "select segment_count, block_count as count from fuse_snapshot('{}', '{}') limit 1",
+        db_name, tbl_name
+    );
+    expects_ok(
+        "check segment and block count",
+        execute_query(fixture.ctx(), qry.as_str()).await,
+        expected,
+    )
+    .await?;
+    Ok(())
+}
+
+async fn build_mutator(
+    ctx: Arc<QueryContext>,
+    table: Arc<dyn Table>,
+) -> Result<Arc<dyn TableMutator>> {
+    let fuse_table = FuseTable::try_from_table(table.as_ref())?;
     let settings = ctx.get_settings();
     settings.set_max_threads(1)?;
-    let table = catalog
-        .get_table(ctx.get_tenant().as_str(), "default", "t")
-        .await?;
-    let fuse_table = FuseTable::try_from_table(table.as_ref())?;
     let mut pipeline = common_pipeline_core::Pipeline::create();
-    let mutator = fuse_table
-        .compact(ctx.clone(), &mut pipeline)
-        .await?;
+    let mutator = fuse_table.compact(ctx.clone(), &mut pipeline).await?;
     assert!(mutator.is_some());
     let mutator = mutator.unwrap();
     pipeline.set_max_threads(1);
@@ -129,19 +97,5 @@ async fn test_compact_conflict() -> Result<()> {
     ctx.set_executor(Arc::downgrade(&executor.get_inner()));
     executor.execute()?;
     drop(executor);
-
-    execute_command(ctx.clone(), "delete from t where c = 1").await?;
-    
-    let res = mutator.try_commit(table).await;
-    assert!(res.is_err());
-    assert_eq!(
-        "Code: 1051, displayText = missing field `subject` in jwt.",
-        res.err().unwrap().to_string()
-    );
-    Ok(())
-}
-
-async fn check_count(result_stream: SendableDataBlockStream) -> Result<u64> {
-    let blocks: Vec<DataBlock> = result_stream.try_collect().await?;
-    blocks[0].column(0).get_u64(0)
+    Ok(mutator)
 }
