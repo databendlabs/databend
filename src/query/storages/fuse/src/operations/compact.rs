6e665123e0bf48b53a23839dd272805e0101672f
dantengsky <dantengsky@gmail.com>
2022-10-18 21:18:45 +0800

    minor code gc

--- src/query/storages/fuse/src/operations/compact.rs
+++ src/query/storages/fuse/src/operations/compact.rs
@@ -141,6 +141,9 @@ impl FuseTable {
         ctx.try_set_partitions(plan.parts.clone())?;
         self.do_read_data(ctx.clone(), &plan, pipeline)?;
 
+        let max_threads = ctx.get_settings().get_max_threads()? as usize;
+        pipeline.resize(max_threads)?;
+
         pipeline.add_transform(|transform_input_port, transform_output_port| {
             TransformCompact::try_create(
                 transform_input_port,
