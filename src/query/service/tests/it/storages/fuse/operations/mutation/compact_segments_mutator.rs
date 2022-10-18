8aa847b7a46d60e9fb63db1688d553a3aab1d488
dantengsky <dantengsky@gmail.com>
2022-10-18 15:55:31 +0800

    make lint

--- src/query/service/tests/it/storages/fuse/operations/mutation/compact_segments_mutator.rs
+++ src/query/service/tests/it/storages/fuse/operations/mutation/compact_segments_mutator.rs
@@ -12,40 +12,21 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-use std::collections::BTreeMap;
-use std::collections::HashMap;
 use std::sync::Arc;
 
 use common_base::base::tokio;
 use common_catalog::table::Table;
-use common_catalog::table_mutator::TableMutator;
-use common_datablocks::pretty_format_blocks;
 use common_datablocks::DataBlock;
-use common_datavalues::DataSchema;
-use common_datavalues::DataSchemaRef;
-use common_datavalues::DataValue;
 use common_exception::ErrorCode;
 use common_exception::Result;
-use common_fuse_meta::caches::CacheManager;
-use common_fuse_meta::meta::BlockMeta;
-use common_fuse_meta::meta::ClusterStatistics;
-use common_fuse_meta::meta::SegmentInfo;
-use common_fuse_meta::meta::Statistics;
-use common_fuse_meta::meta::TableSnapshot;
-use common_fuse_meta::meta::Versioned;
 use common_storages_fuse::FuseTable;
 use common_streams::SendableDataBlockStream;
 use databend_query::sessions::QueryContext;
 use databend_query::sessions::TableContext;
-use databend_query::storages::fuse::io::SegmentWriter;
-use databend_query::storages::fuse::io::TableMetaLocationGenerator;
-use databend_query::storages::fuse::operations::CompactSegmentMutator;
 use futures_util::TryStreamExt;
-use uuid::Uuid;
 
 use crate::storages::fuse::table_test_fixture::execute_command;
 use crate::storages::fuse::table_test_fixture::execute_query;
-use crate::storages::fuse::table_test_fixture::expects_ok;
 use crate::storages::fuse::table_test_fixture::TestFixture;
 
 #[tokio::test]
@@ -54,8 +35,8 @@ async fn test_compact_segment_normal_case() -> Result<()> {
     let ctx = fixture.ctx();
 
     // setup
-    let qry = format!("create table t(c int)  block_per_segment=10");
-    execute_command(ctx.clone(), qry.as_str()).await?;
+    let qry = "create table t(c int)  block_per_segment=10";
+    execute_command(ctx.clone(), qry).await?;
 
     let catalog = ctx.get_catalog("default")?;
 
@@ -78,18 +59,18 @@ async fn test_compact_segment_normal_case() -> Result<()> {
         .compact(ctx.clone(), segment_only, &mut pipeline)
         .await?;
     assert!(mutator.is_some());
-    let mut mutator = mutator.unwrap();
+    let mutator = mutator.unwrap();
     mutator.try_commit(table.get_table_info()).await?;
 
     // check segment count
-    let qry = format!("select segment_count as count from fuse_snapshot('default', 't') limit 1",);
-    let stream = execute_query(fixture.ctx(), qry.as_str()).await?;
+    let qry = "select segment_count as count from fuse_snapshot('default', 't') limit 1";
+    let stream = execute_query(fixture.ctx(), qry).await?;
     // after compact, in our case, there should be only 1 segment left
     assert_eq!(1, check_count(stream).await?);
 
     // check block count
-    let qry = format!("select block_count as count from fuse_snapshot('default', 't') limit 1",);
-    let stream = execute_query(fixture.ctx(), qry.as_str()).await?;
+    let qry = "select block_count as count from fuse_snapshot('default', 't') limit 1";
+    let stream = execute_query(fixture.ctx(), qry).await?;
     assert_eq!(num_inserts as u64, check_count(stream).await?);
     Ok(())
 }
@@ -100,8 +81,8 @@ async fn test_compact_segment_resolvable_conflict() -> Result<()> {
     let ctx = fixture.ctx();
 
     // setup
-    let create_tbl_command = format!("create table t(c int)  block_per_segment=10");
-    execute_command(ctx.clone(), create_tbl_command.as_str()).await?;
+    let create_tbl_command = "create table t(c int)  block_per_segment=10";
+    execute_command(ctx.clone(), create_tbl_command).await?;
 
     let catalog = ctx.get_catalog("default")?;
 
@@ -124,7 +105,7 @@ async fn test_compact_segment_resolvable_conflict() -> Result<()> {
         .compact(ctx.clone(), segment_only, &mut pipeline)
         .await?;
     assert!(mutator.is_some());
-    let mut mutator = mutator.unwrap();
+    let mutator = mutator.unwrap();
 
     // before commit compact segments, gives 9 append commits
     let num_inserts = 9;
@@ -133,17 +114,15 @@ async fn test_compact_segment_resolvable_conflict() -> Result<()> {
     mutator.try_commit(table.get_table_info()).await?;
 
     // check segment count
-    let count_seg =
-        format!("select segment_count as count from fuse_snapshot('default', 't') limit 1",);
-    let stream = execute_query(fixture.ctx(), count_seg.as_str()).await?;
+    let count_seg = "select segment_count as count from fuse_snapshot('default', 't') limit 1";
+    let stream = execute_query(fixture.ctx(), count_seg).await?;
     // after compact, in our case, there should be only 1 + num_inserts segments left
     // during compact retry, newly appended segments will NOT be compacted again
     assert_eq!(1 + num_inserts as u64, check_count(stream).await?);
 
     // check block count
-    let count_block =
-        format!("select block_count as count from fuse_snapshot('default', 't') limit 1",);
-    let stream = execute_query(fixture.ctx(), count_block.as_str()).await?;
+    let count_block = "select block_count as count from fuse_snapshot('default', 't') limit 1";
+    let stream = execute_query(fixture.ctx(), count_block).await?;
     assert_eq!(num_inserts as u64 * 2, check_count(stream).await?);
     Ok(())
 }
@@ -154,8 +133,8 @@ async fn test_compact_segment_unresolvable_conflict() -> Result<()> {
     let ctx = fixture.ctx();
 
     // setup
-    let create_tbl_command = format!("create table t(c int)  block_per_segment=10");
-    execute_command(ctx.clone(), create_tbl_command.as_str()).await?;
+    let create_tbl_command = "create table t(c int)  block_per_segment=10";
+    execute_command(ctx.clone(), create_tbl_command).await?;
 
     let catalog = ctx.get_catalog("default")?;
 
@@ -178,7 +157,7 @@ async fn test_compact_segment_unresolvable_conflict() -> Result<()> {
         .compact(ctx.clone(), segment_only, &mut pipeline)
         .await?;
     assert!(mutator.is_some());
-    let mut mutator = mutator.unwrap();
+    let mutator = mutator.unwrap();
 
     {
         // inject a unresolvable commit
@@ -194,16 +173,15 @@ async fn test_compact_segment_unresolvable_conflict() -> Result<()> {
 }
 
 async fn append_rows(ctx: Arc<QueryContext>, n: usize) -> Result<()> {
-    let qry = format!("insert into t values(1)");
+    let qry = "insert into t values(1)";
     for _ in 0..n {
-        execute_command(ctx.clone(), qry.as_str()).await?;
+        execute_command(ctx.clone(), qry).await?;
     }
     Ok(())
 }
 
 async fn check_count(result_stream: SendableDataBlockStream) -> Result<u64> {
     let blocks: Vec<DataBlock> = result_stream.try_collect().await?;
-    let formatted = pretty_format_blocks(&blocks).unwrap();
     blocks[0].column(0).get_u64(0)
 }
 
@@ -211,7 +189,7 @@ async fn compact_segment(ctx: Arc<QueryContext>, table: &dyn Table) -> Result<()
     let fuse_table = FuseTable::try_from_table(table)?;
     let segment_only = true;
     let mut pipeline = common_pipeline_core::Pipeline::create();
-    let mut mutator = fuse_table
+    let mutator = fuse_table
         .compact(ctx, segment_only, &mut pipeline)
         .await?
         .unwrap();
