ec17ab0f7d38738937d6f9a2186b2b3dc220fef8
Yang Xiufeng <yangxiufeng.c@gmail.com>
2022-10-19 15:35:54 +0800

    fix(logic test): fix error display when "statement query" return error.

--- tests/logictest/http_connector.py
+++ tests/logictest/http_connector.py
@@ -71,13 +71,18 @@ def get_result(response):
 
 
 def get_error(response):
-    # Get mysql-connector Error
     if response['error'] is None:
         return None
 
     # Wrap errno into msg, for result check
     return Error(msg=response['error']['message'],
                  errno=response['error']['code'])
+
+
+def check_error(response):
+    error = get_error(response)
+    if error:
+        raise error
 
 
 class HttpConnector(object):
@@ -149,23 +154,23 @@ class HttpConnector(object):
         response_list.append(response)
         start_time = time.time()
         time_limit = 12
+        session = response['session']
+        if session:
+            self._session = session
         while response['next_uri'] is not None:
-            try:
-                resp = self.next_page(response['next_uri'])
-                response = json.loads(resp.content)
-                log.debug(
-                    f"Sql in progress, fetch next_uri content: {response}")
-                response_list.append(response)
-            except Exception as err:
-                log.warning(
-                    f"Fetch next_uri response with error: {str(err)}")
+            resp = self.next_page(response['next_uri'])
+            response = json.loads(resp.content)
+            log.debug(
+                f"Sql in progress, fetch next_uri content: {response}")
+            check_error(response)
+            session = response['session']
+            if session:
+                self._session = session
+            response_list.append(response)
             if time.time() - start_time > time_limit:
                 log.warning(
                     f"after waited for {time_limit} secs, query still not finished (next uri not none)!"
                 )
-        session = response['session']
-        if session:
-            self._session = session
         return response_list
 
     def fetch_all(self, statement):
