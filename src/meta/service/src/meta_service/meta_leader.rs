4340b682aa64d421b5abe70528d81a43345803d9
张炎泼 <drdr.xp@gmail.com>
2022-10-19 13:02:37 +0800

    refactor(meta/service): try best to leave a cluster
    When leaving a node from a meta-service cluster, no matter the node is a
    voter and non-voter(learner), it should try the best removing it with
    the following step:
    
    - Remove it from membership list.
    - Stop replication to it.
    - Remove its node info record from meta store.
    
    Before this commit, if the node is not in membership list it won't do
    anything, hense there is no way to clean up a dirty state, e.g., to
    remove a non-voter(learner).

--- src/meta/service/src/meta_service/meta_leader.rs
+++ src/meta/service/src/meta_service/meta_leader.rs
@@ -143,8 +143,9 @@ impl<'a> MetaLeader<'a> {
 
     /// A node leave the cluster.
     ///
-    /// - Remove the node from cluster.
     /// - Remove the node from membership.
+    /// - Stop replication.
+    /// - Remove the node from cluster.
     ///
     /// If the node is not in cluster membership, it still returns Ok.
     #[tracing::instrument(level = "debug", skip(self))]
@@ -153,33 +154,23 @@ impl<'a> MetaLeader<'a> {
         let metrics = self.meta_node.raft.metrics().borrow().clone();
         let membership = metrics.membership_config.membership.clone();
 
-        if !membership.contains(&node_id) {
-            return Ok(());
-        }
-
         // safe unwrap: if the first config is None, panic is the expected behavior here.
         let mut membership = membership.get_ith_config(0).unwrap().clone();
-        membership.remove(&node_id);
 
-        self.meta_node
-            .raft
-            .change_membership(membership, true)
-            .await?;
+        // 1. Remove it from membership if needed.
+        if membership.contains(&node_id) {
+            membership.remove(&node_id);
 
-        let ent = LogEntry {
-            txid: None,
-            time_ms: None,
-            cmd: Cmd::RemoveNode { node_id },
-        };
-        self.write(ent).await?;
+            self.meta_node
+                .raft
+                .change_membership(membership, true)
+                .await?;
+        }
 
-        // When write(RemoveNode{}) returns, the replication to that node should be dropped.
-        // But there is chance `add_configured_non_voters()` adds it back because it runs in another task.
-        // Thus we need to ensure the replication is removed here.
+        // 2. Stop replication
         let res = self.meta_node.raft.remove_learner(node_id).await;
-
         if let Err(e) = res {
-            return match e {
+            let res = match e {
                 RemoveLearnerError::ForwardToLeader(e) => {
                     Err(RaftChangeMembershipError::ForwardToLeader(e))
                 }
@@ -193,7 +184,16 @@ impl<'a> MetaLeader<'a> {
                 }
                 RemoveLearnerError::Fatal(e) => Err(RaftChangeMembershipError::Fatal(e)),
             };
+            res?;
         }
+
+        // 3. Remove node info
+        let ent = LogEntry {
+            txid: None,
+            time_ms: None,
+            cmd: Cmd::RemoveNode { node_id },
+        };
+        self.write(ent).await?;
 
         Ok(())
     }
