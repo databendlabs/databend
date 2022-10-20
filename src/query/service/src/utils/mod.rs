5fec30b7d3525f01e50b7136d7e39a6c5ca22fc9
BohuTANG <overred.shuttler@gmail.com>
2022-10-20 17:27:45 +0800

    download.rs to http/v1/

--- src/query/service/src/utils/mod.rs
+++ src/query/service/src/utils/mod.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod download;
 mod storage_table_read_wrap;
 
-pub use download::Dowloader;
 pub use storage_table_read_wrap::DataBlockStream;
