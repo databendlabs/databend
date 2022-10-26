1f8bbbefb1a275d6fe9fcc6d027fb06dc7016534
Xuanwo <github@xuanwo.io>
2022-10-26 19:11:54 +0800

    Fix test
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/query/service/tests/it/storages/system.rs
+++ src/query/service/tests/it/storages/system.rs
@@ -385,7 +385,7 @@ async fn test_tracing_table() -> Result<()> {
     let stream = table.read_data_block_stream(ctx, &source_plan).await?;
     let result = stream.try_collect::<Vec<_>>().await?;
     let block = &result[0];
-    assert_eq!(block.num_columns(), 4);
+    assert_eq!(block.num_columns(), 1);
     assert!(block.num_rows() > 0);
 
     Ok(())
