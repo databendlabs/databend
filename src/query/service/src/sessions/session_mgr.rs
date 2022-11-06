06cb89e3f9602d8a41f68a1abbe6be81bee2347f
蔡略 <cailue@bupt.edu.cn>
2022-11-06 21:24:18 +0800

    sometimes Arc<RwLock> is good enough
    Signed-off-by: 蔡略 <cailue@bupt.edu.cn>

--- src/query/service/src/sessions/session_mgr.rs
+++ src/query/service/src/sessions/session_mgr.rs
@@ -12,9 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
 use std::future::Future;
+use std::ops::DerefMut;
 use std::sync::atomic::AtomicU32;
-use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use std::sync::Weak;
@@ -29,8 +30,6 @@ use common_exception::Result;
 use common_metrics::label_counter;
 use common_settings::Settings;
 use common_users::UserApiProvider;
-use dashmap::mapref::entry::Entry;
-use dashmap::DashMap;
 use futures::future::Either;
 use futures::StreamExt;
 use once_cell::sync::OnceCell;
@@ -47,16 +46,12 @@ use crate::sessions::SessionType;
 pub struct SessionManager {
     pub(in crate::sessions) conf: Config,
     pub(in crate::sessions) max_sessions: usize,
-    pub(in crate::sessions) active_sessions: Arc<DashMap<String, Weak<Session>>>,
-    pub(in crate::sessions) active_session_counter: AtomicUsize,
-
+    pub(in crate::sessions) active_sessions: Arc<RwLock<HashMap<String, Weak<Session>>>>,
     pub status: Arc<RwLock<SessionManagerStatus>>,
 
     // When typ is MySQL, insert into this map, key is id, val is MySQL connection id.
-    pub(crate) mysql_conn_map: Arc<DashMap<Option<u32>, String>>,
+    pub(crate) mysql_conn_map: Arc<RwLock<HashMap<Option<u32>, String>>>,
     pub(in crate::sessions) mysql_basic_conn_id: AtomicU32,
-    // present mysql connections
-    pub(in crate::sessions) mysql_conn_counter: AtomicUsize,
 }
 
 static SESSION_MANAGER: OnceCell<Singleton<Arc<SessionManager>>> = OnceCell::new();
@@ -74,12 +69,10 @@ impl SessionManager {
         Arc::new(SessionManager {
             conf,
             max_sessions,
-            active_session_counter: AtomicUsize::new(0),
             mysql_basic_conn_id: AtomicU32::new(9_u32.to_le()),
-            mysql_conn_counter: AtomicUsize::new(0),
             status: Arc::new(RwLock::new(SessionManagerStatus::default())),
-            mysql_conn_map: Arc::new(DashMap::with_capacity(max_sessions)),
-            active_sessions: Arc::new(DashMap::with_capacity(max_sessions)),
+            mysql_conn_map: Arc::new(RwLock::new(HashMap::with_capacity(max_sessions))),
+            active_sessions: Arc::new(RwLock::new(HashMap::with_capacity(max_sessions))),
         })
     }
 
@@ -98,7 +91,8 @@ impl SessionManager {
         // TODO: maybe deadlock
         let config = self.get_conf();
         {
-            if self.active_sessions.len() == self.max_sessions {
+            let sessions = self.active_sessions.read();
+            if sessions.len() == self.max_sessions {
                 return Err(ErrorCode::TooManyUserConnections(
                     "The current accept connection has exceeded max_active_sessions config",
                 ));
@@ -109,7 +103,15 @@ impl SessionManager {
         let mut mysql_conn_id = None;
         match session_typ {
             SessionType::MySQL => {
-                mysql_conn_id = self.add_mysql_session(id.clone())?;
+                let mut conn_id_session_id = self.mysql_conn_map.write();
+                mysql_conn_id = Some(self.mysql_basic_conn_id.fetch_add(1, Ordering::Relaxed));
+                if conn_id_session_id.len() < self.max_sessions {
+                    conn_id_session_id.insert(mysql_conn_id, id.clone());
+                } else {
+                    return Err(ErrorCode::TooManyUserConnections(
+                        "The current accept connection has exceeded max_active_sessions config",
+                    ));
+                }
             }
             _ => {
                 debug!(
@@ -125,19 +127,34 @@ impl SessionManager {
         let session_ctx = SessionContext::try_create(config.clone(), session_settings)?;
         let session = Session::try_create(id, typ.clone(), session_ctx, mysql_conn_id)?;
 
-        self.add_active_session(config, session, typ)
+        let mut sessions = self.active_sessions.write();
+        if sessions.len() < self.max_sessions {
+            label_counter(
+                super::metrics::METRIC_SESSION_CONNECT_NUMBERS,
+                &config.query.tenant_id,
+                &config.query.cluster_id,
+            );
+
+            if !matches!(typ, SessionType::FlightRPC) {
+                sessions.insert(session.get_id(), Arc::downgrade(&session));
+            }
+
+            Ok(session)
+        } else {
+            Err(ErrorCode::TooManyUserConnections(
+                "The current accept connection has exceeded max_active_sessions config",
+            ))
+        }
     }
 
     pub fn get_session_by_id(&self, id: &str) -> Option<Arc<Session>> {
-        self.active_sessions
-            .get(id)
-            .and_then(|weak_ptr| weak_ptr.upgrade())
+        let sessions = self.active_sessions.read();
+        sessions.get(id).and_then(|weak_ptr| weak_ptr.upgrade())
     }
 
     pub fn get_id_by_mysql_conn_id(&self, mysql_conn_id: &Option<u32>) -> Option<String> {
-        self.mysql_conn_map
-            .get(mysql_conn_id)
-            .map(|e| e.value().clone())
+        let sessions = self.mysql_conn_map.read();
+        sessions.get(mysql_conn_id).cloned()
     }
 
     pub fn destroy_session(&self, session_id: &String) {
@@ -149,19 +166,20 @@ impl SessionManager {
         );
 
         // stop tracking session
-        self.remove_active_session(session_id);
+        {
+            let mut sessions = self.active_sessions.write();
+            sessions.remove(session_id);
+        }
 
         // also need remove mysql_conn_map
-        self.mysql_conn_map.retain(|_, v| v != session_id);
-        // todo: maybe incorrect
-        self.mysql_conn_counter
-            .store(self.mysql_conn_map.len(), Ordering::Relaxed);
+        let mut mysql_conns_map = self.mysql_conn_map.write();
+        for (k, v) in mysql_conns_map.deref_mut().clone() {
+            if &v == session_id {
+                mysql_conns_map.remove(&k);
+            }
+        }
     }
 
-    /// Gentlely destroy all idle sessions
-    ///
-    /// Note:
-    /// since this is the end, atomic counters will not be updated
     pub fn graceful_shutdown(
         &self,
         mut signal: SignalStream,
@@ -192,10 +210,7 @@ impl SessionManager {
 
             // During the destroy session, we need to get active_sessions write locks,
             // so we can only get active_sessions snapshots.
-            let active_sessions = active_sessions
-                .iter()
-                .map(|e| e.value().clone())
-                .collect::<Vec<_>>();
+            let active_sessions = active_sessions.read().values().cloned().collect::<Vec<_>>();
             for weak_ptr in &active_sessions {
                 if let Some(active_session) = weak_ptr.upgrade() {
                     active_session.force_kill_session();
@@ -205,9 +220,10 @@ impl SessionManager {
     }
 
     pub fn processes_info(&self) -> Vec<ProcessInfo> {
-        let mut processes_info = Vec::with_capacity(self.active_sessions.len());
-        for entry in self.active_sessions.iter() {
-            let weak_ptr = entry.value();
+        let sessions = self.active_sessions.read();
+
+        let mut processes_info = Vec::with_capacity(sessions.len());
+        for weak_ptr in sessions.values() {
             if let Some(active_session) = weak_ptr.upgrade() {
                 processes_info.push(active_session.process_info());
             }
@@ -216,12 +232,13 @@ impl SessionManager {
         processes_info
     }
 
-    /// Gentlely destroy all idle sessions
-    ///
-    /// Note:
-    /// since this is the end, atomic counters will not be updated
-    fn destroy_idle_sessions(sessions: &Arc<DashMap<String, Weak<Session>>>) -> bool {
-        sessions.retain(|_id, weak_ptr| -> bool {
+    fn destroy_idle_sessions(sessions: &Arc<RwLock<HashMap<String, Weak<Session>>>>) -> bool {
+        // Read lock does not support reentrant
+        // https://github.com/Amanieu/parking_lot::/blob/lock_api-0.4.4/lock_api/src/rwlock.rs#L422
+        let mut active_sessions_read_guard = sessions.write();
+
+        // First try to kill the idle session
+        active_sessions_read_guard.retain(|_id, weak_ptr| -> bool {
             match weak_ptr.upgrade() {
                 None => false,
                 Some(session) => {
@@ -231,7 +248,8 @@ impl SessionManager {
             }
         });
 
-        let active_sessions = sessions.len();
+        // active_sessions_read_guard.values().for_each(Session::kill);
+        let active_sessions = active_sessions_read_guard.len();
 
         match active_sessions {
             0 => true,
@@ -239,103 +257,6 @@ impl SessionManager {
                 info!("Waiting for {} connections to close.", active_sessions);
                 false
             }
-        }
-    }
-
-    /// record an session in `Self::active_sessions` and increase `Self::active_session_counter` by 1
-    ///
-    /// no changes will happen if failed
-    fn add_active_session(
-        &self,
-        config: Config,
-        session: Arc<Session>,
-        typ: SessionType,
-    ) -> Result<Arc<Session>> {
-        // acquire write lock by calling `DashMap::entry`
-        let entry = self.active_sessions.entry(session.get_id());
-        // Note:
-        // # Why not `self.active_sessions.len()`
-        //
-        // `self.active_sessions.entry()` had acquired write lock on the DashMap shard
-        // but the `len()` requires read locks on all shards
-        // this will cause a deadlock
-        //
-        // # Why must record with an Atomic typed value
-        //
-        // `entry()` will only acquire write lock on one shard
-        // if concurrent write happens on different shards
-        // multiple threads may go inside this critial zone
-        if self.active_session_counter.fetch_add(1, Ordering::AcqRel) < self.max_sessions {
-            label_counter(
-                super::metrics::METRIC_SESSION_CONNECT_NUMBERS,
-                &config.query.tenant_id,
-                &config.query.cluster_id,
-            );
-
-            if !matches!(typ, SessionType::FlightRPC) {
-                let s = Arc::downgrade(&session);
-                match entry {
-                    Entry::Occupied(mut o) => {
-                        o.insert(s);
-                    }
-                    Entry::Vacant(v) => {
-                        v.insert(s);
-                    }
-                }
-            }
-
-            Ok(session)
-        } else {
-            // go back one, since nothing inserted
-            self.active_session_counter.fetch_sub(1, Ordering::AcqRel);
-            Err(ErrorCode::TooManyUserConnections(
-                "The current accept connection has exceeded max_active_sessions config",
-            ))
-        }
-    }
-
-    /// record a mysql connection in `Self::mysql_conn_map`, will increase `Self::mysql_conn_counter` and `Self::mysql_basic_conn_id` by 1
-    ///
-    /// if fail, `Self::mysql_basic_conn_id` will still increase by 1
-    fn add_mysql_session(&self, uuid: String) -> Result<Option<u32>> {
-        let mysql_conn_id = Some(self.mysql_basic_conn_id.fetch_add(1, Ordering::Relaxed));
-
-        // acquire write lock by calling `DashMap::entry`
-        let conn_entry = self.mysql_conn_map.entry(mysql_conn_id);
-        // Note:
-        // # Why not `self.mysql_conn_map.len()`
-        //
-        // `self.mysql_conn_map.entry()` had acquired write lock on the DashMap shard
-        // but the `len()` requires read locks on all shards
-        // this will cause a deadlock
-        //
-        // # Why must record with an Atomic typed value
-        //
-        // `entry()` will only acquire write lock on one shard
-        // if concurrent write happens on different shards
-        // multiple threads may go inside this critial zone
-        if self.mysql_conn_counter.fetch_add(1, Ordering::AcqRel) < self.max_sessions {
-            match conn_entry {
-                Entry::Occupied(mut o) => {
-                    o.insert(uuid);
-                }
-                Entry::Vacant(v) => {
-                    v.insert(uuid);
-                }
-            }
-        } else {
-            // go back one, since nothing inserted
-            self.mysql_conn_counter.fetch_sub(1, Ordering::AcqRel);
-            return Err(ErrorCode::TooManyUserConnections(
-                "The current accept connection has exceeded max_active_sessions config",
-            ));
-        }
-        Ok(mysql_conn_id)
-    }
-
-    fn remove_active_session(&self, session_id: &str) {
-        if self.active_sessions.remove(session_id).is_some() {
-            self.active_session_counter.fetch_sub(1, Ordering::AcqRel);
         }
     }
 }
