beb0c45122f04df3fab2960cfe6dcf37de506065
BohuTANG <overred.shuttler@gmail.com>
2022-11-08 11:24:13 +0800

    add purge files to copy

--- src/query/service/src/interpreters/interpreter_copy_v2.rs
+++ src/query/service/src/interpreters/interpreter_copy_v2.rs
@@ -108,8 +108,25 @@ impl CopyInterpreterV2 {
         Ok(build_res)
     }
 
+    async fn try_purge_files(
+        ctx: Arc<QueryContext>,
+        stage_info: &UserStageInfo,
+        stage_file_infos: &[StageFilePartition],
+    ) -> Result<()> {
+        if stage_info.copy_options.purge {
+            let table_ctx: Arc<dyn TableContext> = ctx.clone();
+            let op = StageTable::get_op(&table_ctx, stage_info)?;
+            for file in stage_file_infos {
+                if let Err(e) = op.object(&file.path).delete().await {
+                    tracing::error!("Failed to delete file: {}, error: {}", file.path, e);
+                }
+            }
+        }
+        Ok(())
+    }
+
     #[allow(clippy::too_many_arguments)]
-    async fn execute_copy_into_table(
+    async fn build_copy_into_table_pipeline(
         &self,
         catalog_name: &str,
         database_name: &str,
@@ -120,31 +137,33 @@ impl CopyInterpreterV2 {
         force: bool,
         stage_table_info: &StageTableInfo,
     ) -> Result<PipelineBuildResult> {
-        let stage_table = StageTable::try_create(stage_table_info.clone())?;
-        let copy_info = CopyInfo {
-            force,
-            files: files.to_vec(),
-            path: path.to_string(),
-            pattern: pattern.to_string(),
-            stage_info: stage_table_info.stage_info.clone(),
-            into_table_catalog_name: catalog_name.to_string(),
-            into_table_database_name: database_name.to_string(),
-            into_table_name: table_name.to_string(),
-        };
-        let pushdown = PushDownInfo {
-            projection: None,
-            filters: vec![],
-            prewhere: None,
-            limit: None,
-            order_by: vec![],
-            copy: Some(copy_info),
-        };
         let ctx = self.ctx.clone();
+        let stage_table = StageTable::try_create(stage_table_info.clone())?;
 
-        // Get the source plan with all files.
-        let read_source_plan = stage_table
-            .read_plan_with_catalog(ctx.clone(), catalog_name.to_string(), Some(pushdown))
-            .await?;
+        // Get the source plan with all files from stage_table.read_partitions.
+        let read_source_plan = {
+            let copy_info = CopyInfo {
+                force,
+                files: files.to_vec(),
+                path: path.to_string(),
+                pattern: pattern.to_string(),
+                stage_info: stage_table_info.stage_info.clone(),
+                into_table_catalog_name: catalog_name.to_string(),
+                into_table_database_name: database_name.to_string(),
+                into_table_name: table_name.to_string(),
+            };
+            let pushdown = PushDownInfo {
+                projection: None,
+                filters: vec![],
+                prewhere: None,
+                limit: None,
+                order_by: vec![],
+                copy: Some(copy_info),
+            };
+            stage_table
+                .read_plan_with_catalog(ctx.clone(), catalog_name.to_string(), Some(pushdown))
+                .await?
+        };
 
         // Remove the AlreadyCopied files from the source plan.
         let mut need_copy_source_plan = read_source_plan.clone();
@@ -170,7 +189,7 @@ impl CopyInterpreterV2 {
         // Build pipeline.
         let mut build_res = PipelineBuildResult::create();
         {
-            // InputContext.
+            //  Build copy pipeline.
             let files = copied_file_infos
                 .iter()
                 .map(|v| v.path.clone())
@@ -198,7 +217,7 @@ impl CopyInterpreterV2 {
                 .format
                 .exec_copy(input_ctx.clone(), &mut build_res.main_pipeline)?;
 
-            // Limit.
+            // Build Limit pipeline.
             let limit = stage_table_info.stage_info.copy_options.size_limit;
             if limit > 0 {
                 build_res.main_pipeline.resize(1)?;
@@ -214,7 +233,7 @@ impl CopyInterpreterV2 {
                 )?;
             }
 
-            // Append data.
+            // Build append data pipeline.
             to_table.append_data(
                 ctx.clone(),
                 &mut build_res.main_pipeline,
@@ -223,11 +242,14 @@ impl CopyInterpreterV2 {
             )?;
         }
 
+        let stage_table_info_clone = stage_table_info.clone();
         build_res.main_pipeline.set_on_finished(move |may_error| {
             if may_error.is_none() {
                 // capture out variable
                 let ctx = ctx.clone();
                 let to_table = to_table.clone();
+                let stage_info = stage_table_info_clone.stage_info.clone();
+                let files = copied_file_infos.clone();
 
                 return GlobalIORuntime::instance().block_on(async move {
                     // Commit
@@ -236,7 +258,8 @@ impl CopyInterpreterV2 {
                         .commit_insertion(ctx.clone(), operations, false)
                         .await?;
 
-                    // TODO(bohu): Purge copied files
+                    // Try purge copied files, if error will skip.
+                    CopyInterpreterV2::try_purge_files(ctx.clone(), &stage_info, &files).await?;
                     // TODO(bohu): Upsert copied files
                     Ok(())
                 });
@@ -269,7 +292,7 @@ impl Interpreter for CopyInterpreterV2 {
             } => match &from.source_info {
                 DataSourceInfo::StageSource(table_info) => {
                     let path = &table_info.path;
-                    self.execute_copy_into_table(
+                    self.build_copy_into_table_pipeline(
                         catalog_name,
                         database_name,
                         table_name,
