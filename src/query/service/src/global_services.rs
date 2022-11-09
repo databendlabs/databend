0c4b6b440950c7c99118a9fa101506cd6528b6f2
zhihanz <zzh243402448@gmail.com>
2022-11-09 15:24:11 +0800

    f

--- src/query/service/src/global_services.rs
+++ src/query/service/src/global_services.rs
@@ -15,8 +15,6 @@
 use std::cell::UnsafeCell;
 use std::sync::Arc;
 
-use base64::encode_config;
-use base64::URL_SAFE;
 use common_base::base::GlobalIORuntime;
 use common_base::base::Runtime;
 use common_base::base::SingletonImpl;
@@ -86,7 +84,7 @@ impl GlobalServices {
         ShareTableConfig::init(
             &config.query.share_endpoint_address,
             &config.query.share_endpoint_auth_token_file,
-            encode_config(config.query.tenant_id.clone(), URL_SAFE),
+            config.query.tenant_id.clone(),
             global_services.clone(),
         )?;
 
