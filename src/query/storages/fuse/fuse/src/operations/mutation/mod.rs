7486095209122c00d366d4bd6c631e93944c6371
zhyass <mytesla@live.com>
2022-11-18 00:08:15 +0800

    update dir name

--- src/query/storages/fuse/fuse/src/operations/mutation/mod.rs
+++ src/query/storages/fuse/fuse/src/operations/mutation/mod.rs
@@ -15,7 +15,7 @@
 pub mod abort_operation;
 pub mod base_mutator;
 pub mod block_filter;
-mod compact_mutator;
+mod compact_mutation;
 pub mod deletion_mutator;
 pub mod recluster_mutator;
 
@@ -23,12 +23,12 @@ pub use abort_operation::AbortOperation;
 pub use base_mutator::BaseMutator;
 pub use block_filter::all_the_columns_ids;
 pub use block_filter::delete_from_block;
-pub use compact_mutator::BlockCompactMutator;
-pub use compact_mutator::CompactSink;
-pub use compact_mutator::CompactSource;
-pub use compact_mutator::CompactTransform;
-pub use compact_mutator::SegmentCompactMutator;
-pub use compact_mutator::SegmentCompactionState;
-pub use compact_mutator::SegmentCompactor;
+pub use compact_mutation::BlockCompactMutator;
+pub use compact_mutation::CompactSink;
+pub use compact_mutation::CompactSource;
+pub use compact_mutation::CompactTransform;
+pub use compact_mutation::SegmentCompactMutator;
+pub use compact_mutation::SegmentCompactionState;
+pub use compact_mutation::SegmentCompactor;
 pub use deletion_mutator::DeletionMutator;
 pub use recluster_mutator::ReclusterMutator;
