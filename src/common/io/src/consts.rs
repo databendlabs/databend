c53bbd8fc0f4df5a36c4a40de5c527efb1d46847
Yijun Zhao <ariesdevil77@gmail.com>
2022-11-23 16:53:47 +0800

    output float NaN instead of nan

--- src/common/io/src/consts.rs
+++ src/common/io/src/consts.rs
@@ -19,5 +19,5 @@ pub const FALSE_BYTES_NUM: &str = "0";
 pub const NULL_BYTES_UPPER: &str = "NULL";
 pub const NULL_BYTES_LOWER: &str = "null";
 pub const NULL_BYTES_ESCAPE: &str = "\\N";
-pub const NAN_BYTES_LOWER: &str = "nan";
+pub const NAN_BYTES_LOWER: &str = "NaN";
 pub const INF_BYTES_LOWER: &str = "inf";
