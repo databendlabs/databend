82b502937fafbf5785337dcb4c8573b06aa25680
everpcpc <git@everpcpc.com>
2022-11-23 19:01:43 +0800

    docs: update deploy document for kubernetes

---
+++ scripts/build/build-website.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2020-2021 The Databend Authors.
+# SPDX-License-Identifier: Apache-2.0.
+
+set -e
+
+SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+cd "$SCRIPT_PATH/../.." || exit
+
+echo "Building docs"
+cd website
+
+# NOET: for aarch64 macos
+# arch -x86_64 zsh
+
+mkdir -p ~/.nvm
+export NVM_DIR="$HOME/.nvm"
+# This loads nvm
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+
+nvm install
+nvm use
+
+npm install
+npm start
