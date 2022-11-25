76044e852af5784876ee5e3b1a6d2e08f30c1a15
张炎泼 <drdr.xp@gmail.com>
2022-11-25 22:36:42 +0800

    refactor(mem-tracker): add global tracker and allow panicking to allocate memory
    - Feature: add `GLOBAL_TRACKER` to track all memory.
    
    - Refactor: Remove `on_thread_stop()`, use Drop to flush the buffered state.
    
    - Refactor: Change thread local tracker from `Option<T>` to T: a thread
      always has a tracker(`ThreadTracker`). A ThreadTracker reports the
      allocation stat to a MemoryTracker, or the global tracker:
      `GLOBAL_TRACKER`.
    
    - Refactor: Reduce the scope in which it allows to allocated memory for `panic!`
      Set a panicking flag to allow `panic!()` to allocate memory.
      Use a **guard** to reset the thread-local panicking flag
    
    Other changes:
    
    - Refactor: Generalize `AsyncThreadTracker`: the inner future can be any Future impl.
    
    - Test `AsyncThreadTracker`
    
    - Refactor: remove decr(), use incr(-size)

--- src/common/base/src/mem_allocator/system_allocator.rs
+++ src/common/base/src/mem_allocator/system_allocator.rs
@@ -26,24 +26,20 @@ pub struct SystemAllocator;
 unsafe impl Allocator for SystemAllocator {
     #[inline(always)]
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let p = System.allocate(layout)?;
-        ThreadTracker::alloc_memory(layout.size() as i64, &p);
-        Ok(p)
+        ThreadTracker::alloc_memory(layout.size() as i64);
+        System.allocate(layout)
     }
 
     #[inline(always)]
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
-        ThreadTracker::dealloc_memory(layout.size() as i64, &ptr);
+        ThreadTracker::dealloc_memory(layout.size() as i64);
         System.deallocate(ptr, layout)
     }
 
     #[inline(always)]
     fn allocate_zeroed(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let p = System.allocate_zeroed(layout)?;
-
-        ThreadTracker::alloc_memory(layout.size() as i64, &p);
-
-        Ok(p)
+        ThreadTracker::alloc_memory(layout.size() as i64);
+        System.allocate_zeroed(layout)
     }
 
     #[inline(always)]
@@ -53,12 +49,10 @@ unsafe impl Allocator for SystemAllocator {
         old_layout: Layout,
         new_layout: Layout,
     ) -> Result<NonNull<[u8]>, AllocError> {
-        ThreadTracker::dealloc_memory(old_layout.size() as i64, &ptr);
+        ThreadTracker::dealloc_memory(old_layout.size() as i64);
+        ThreadTracker::alloc_memory(new_layout.size() as i64);
 
-        let new_ptr = System.grow(ptr, old_layout, new_layout)?;
-
-        ThreadTracker::alloc_memory(new_layout.size() as i64, &new_ptr);
-        Ok(new_ptr)
+        System.grow(ptr, old_layout, new_layout)
     }
 
     #[inline(always)]
@@ -68,12 +62,10 @@ unsafe impl Allocator for SystemAllocator {
         old_layout: Layout,
         new_layout: Layout,
     ) -> Result<NonNull<[u8]>, AllocError> {
-        ThreadTracker::dealloc_memory(old_layout.size() as i64, &ptr);
+        ThreadTracker::dealloc_memory(old_layout.size() as i64);
+        ThreadTracker::alloc_memory(new_layout.size() as i64);
 
-        let new_ptr = System.grow_zeroed(ptr, old_layout, new_layout)?;
-
-        ThreadTracker::alloc_memory(new_layout.size() as i64, &new_ptr);
-        Ok(new_ptr)
+        System.grow_zeroed(ptr, old_layout, new_layout)
     }
 
     #[inline(always)]
@@ -83,11 +75,9 @@ unsafe impl Allocator for SystemAllocator {
         old_layout: Layout,
         new_layout: Layout,
     ) -> Result<NonNull<[u8]>, AllocError> {
-        ThreadTracker::dealloc_memory(old_layout.size() as i64, &ptr);
+        ThreadTracker::dealloc_memory(old_layout.size() as i64);
+        ThreadTracker::alloc_memory(new_layout.size() as i64);
 
-        let new_ptr = System.shrink(ptr, old_layout, new_layout)?;
-
-        ThreadTracker::alloc_memory(new_layout.size() as i64, &new_ptr);
-        Ok(new_ptr)
+        System.shrink(ptr, old_layout, new_layout)
     }
 }
