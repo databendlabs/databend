ea4bd78182aff1592ce875429219bd658ffeb215
Yang Xiufeng <yangxiufeng.c@gmail.com>
2022-12-01 21:33:52 +0800

    refactor(stage): bind stage table in select.

--- src/query/sql/src/planner/binder/table.rs
+++ src/query/sql/src/planner/binder/table.rs
@@ -13,6 +13,8 @@
 // limitations under the License.
 
 use std::collections::HashMap;
+use std::default::Default;
+use std::path::Path;
 use std::sync::Arc;
 
 use common_ast::ast::Indirection;
@@ -28,6 +30,7 @@ use common_ast::Backtrace;
 use common_ast::Dialect;
 use common_ast::DisplayError;
 use common_catalog::catalog_kind::CATALOG_DEFAULT;
+use common_catalog::plan::StageTableInfo;
 use common_catalog::table::ColumnId;
 use common_catalog::table::ColumnStatistics;
 use common_catalog::table::NavigationPoint;
@@ -36,8 +39,14 @@ use common_catalog::table_function::TableFunction;
 use common_datavalues::prelude::*;
 use common_exception::ErrorCode;
 use common_exception::Result;
+use common_meta_types::StageFileFormatType;
+use common_pipeline_sources::processors::sources::input_formats::InputContext;
+use common_storages_stage::get_first_file;
+use common_storages_stage::StageTable;
 use common_storages_view::view_table::QUERY;
+use opendal::layers::SubdirLayer;
 
+use crate::binder::copy::parse_stage_location_v2;
 use crate::binder::scalar::ScalarBinder;
 use crate::binder::Binder;
 use crate::binder::ColumnBinding;
@@ -273,8 +282,88 @@ impl<'a> Binder {
                 }
                 Ok((s_expr, new_bind_context))
             }
-            TableReference::Stage { .. } => {
-                unimplemented!()
+            TableReference::Stage {
+                span: _,
+                location,
+                files,
+                alias,
+            } => {
+                let table_alias_name = if let Some(table_alias) = alias {
+                    Some(normalize_identifier(&table_alias.name, &self.name_resolution_ctx).name)
+                } else {
+                    None
+                };
+
+                let (user_stage_info, path) =
+                    parse_stage_location_v2(&self.ctx, &location.name, &location.path).await?;
+                let op = self.ctx.get_data_operator()?.operator();
+                let op = op.layer(SubdirLayer::new(&user_stage_info.stage_prefix()));
+
+                let first_file = if files.is_empty() {
+                    let file = get_first_file(&op, &path).await?;
+                    match file {
+                        None => {
+                            return Err(ErrorCode::BadArguments(format!(
+                                "no file in {}",
+                                location
+                            )));
+                        }
+                        Some(f) => f.path().to_string(),
+                    }
+                } else {
+                    Path::new(&path)
+                        .join(&files[0])
+                        .to_string_lossy()
+                        .to_string()
+                };
+
+                let format_type = StageFileFormatType::Parquet;
+                let input_format = InputContext::get_input_format(&format_type)?;
+                let schema = input_format.infer_schema(&first_file, &op).await?;
+
+                let stage_table_info = StageTableInfo {
+                    schema,
+                    user_stage_info,
+                    path: path.to_string(),
+                    files: vec![],
+                };
+
+                let stage_table = StageTable::try_create(stage_table_info)?;
+
+                let table_index = self.metadata.write().add_table(
+                    "stage_catalog".to_string(),
+                    "stage_db".to_string(),
+                    stage_table,
+                    table_alias_name,
+                );
+                let mut bind_context = BindContext::with_parent(Box::new(bind_context.clone()));
+                if let Some(alias) = alias {
+                    bind_context.apply_table_alias(alias, &self.name_resolution_ctx)?;
+                }
+                let columns = self.metadata.read().columns_by_table_index(table_index);
+                let s_expr = SExpr::create_leaf(
+                    LogicalGet {
+                        table_index,
+                        columns: columns
+                            .into_iter()
+                            .map(|col| match col {
+                                ColumnEntry::BaseTableColumn { column_index, .. } => column_index,
+                                ColumnEntry::DerivedColumn { column_index, .. } => column_index,
+                            })
+                            .collect(),
+                        push_down_predicates: None,
+                        limit: None,
+                        order_by: None,
+                        statistics: Statistics {
+                            statistics: None,
+                            col_stats: Default::default(),
+                            is_accurate: true,
+                        },
+                        prewhere: None,
+                    }
+                    .into(),
+                );
+                Ok((s_expr, bind_context))
             }
         }
     }
