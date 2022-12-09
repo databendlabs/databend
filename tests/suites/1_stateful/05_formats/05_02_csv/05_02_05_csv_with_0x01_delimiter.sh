2110b0468422fa9041ed0ff905783e636df43705
BohuTANG <overred.shuttler@gmail.com>
2022-12-09 21:08:11 +0800

    add stateful test for x01 delimiter

---
+++ tests/suites/1_stateful/05_formats/05_02_csv/05_02_05_csv_with_0x01_delimiter.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. "$CURDIR"/../../../../shell_env.sh
+
+ echo "drop table if exists test_x01_csv" | $MYSQL_CLIENT_CONNECT
+
+ echo "CREATE TABLE test_x01_csv
+ (
+     a VARCHAR,
+     b Int,
+     c VARCHAR
+ );" | $MYSQL_CLIENT_CONNECT
+
+ curl -sH "insert_sql:insert into test_x01_csv format CSV" -H "format_field_delimiter:\x01" -H "format_skip_header:1" -H "format_record_delimiter:\r\n" -F "upload=@${CURDIR}/x01_field_delimiter.csv" \
+ -u root: -XPUT "http://localhost:${QUERY_HTTP_HANDLER_PORT}/v1/streaming_load" | grep -c "SUCCESS"
+ echo "select count() from test_x01_csv" | $MYSQL_CLIENT_CONNECT
