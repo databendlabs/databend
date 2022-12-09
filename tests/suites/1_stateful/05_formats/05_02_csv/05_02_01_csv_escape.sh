dd3590b1e8086ebabe1f21f45afbc4e8d810991a
Yang Xiufeng <yangxiufeng.c@gmail.com>
2022-12-09 23:12:49 +0800

    fix test for format_escape in copy.

--- tests/suites/1_stateful/05_formats/05_02_csv/05_02_01_csv_escape.sh
+++ tests/suites/1_stateful/05_formats/05_02_csv/05_02_01_csv_escape.sh
@@ -46,11 +46,11 @@ echo "select * from test_csv" | $MYSQL_CLIENT_CONNECT
 echo "truncate table test_csv" | $MYSQL_CLIENT_CONNECT
 
 aws --endpoint-url http://127.0.0.1:9900/ s3 cp /tmp/escape_slash2.csv s3://testbucket/admin/data/csv/escape_slash2.csv > /dev/null 2>&1
-echo "copy into test_csv from 's3://testbucket/admin/data/csv/escape_slash2.csv' connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='http://127.0.0.1:9900/') FILE_FORMAT = (type = 'CSV' escape='\\\')" | $MYSQL_CLIENT_CONNECT
+echo "copy into test_csv from 's3://testbucket/admin/data/csv/escape_slash2.csv' connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='http://127.0.0.1:9900/') FILE_FORMAT = (type = 'CSV' escape='\\\\\\\\')" | $MYSQL_CLIENT_CONNECT
 echo "select * from test_csv" | $MYSQL_CLIENT_CONNECT
 
 aws --endpoint-url http://127.0.0.1:9900/ s3 cp /tmp/escape_slash3.csv s3://testbucket/admin/data/csv/escape_slash3.csv > /dev/null 2>&1
-echo "copy into test_csv2 from 's3://testbucket/admin/data/csv/escape_slash3.csv' connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='http://127.0.0.1:9900/') FILE_FORMAT = (type = 'CSV' escape='\\\' skip_header=1)" | $MYSQL_CLIENT_CONNECT
+echo "copy into test_csv2 from 's3://testbucket/admin/data/csv/escape_slash3.csv' connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='http://127.0.0.1:9900/') FILE_FORMAT = (type = 'CSV' escape='\\\\\\\\' skip_header=1)" | $MYSQL_CLIENT_CONNECT
 echo "select * from test_csv2" | $MYSQL_CLIENT_CONNECT
 
 echo "drop table if exists test_csv" | $MYSQL_CLIENT_CONNECT
