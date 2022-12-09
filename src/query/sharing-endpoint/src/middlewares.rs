fd787ff69a876baca6866758f71a677be7dfd52b
zhihanz <zzh243402448@gmail.com>
2022-12-09 20:52:01 +0800

    chore: lint

--- src/query/sharing-endpoint/src/middlewares.rs
+++ src/query/sharing-endpoint/src/middlewares.rs
@@ -41,7 +41,6 @@ impl<E: Endpoint> Endpoint for SharingAuthImpl<E> {
     // TODO(zhihanz) current implementation only used for stateless test
     // for production usage, we need to implement a middleware with JWT authentication
     async fn call(&self, mut req: Request) -> Result<Self::Output> {
-
         // decode auth header from bearer base64
         let auth_header = req
             .headers()
