a9d6dd31834d4a7ba7793cecfac3b27fb10c2d86
ClSlaid <cailue@bupt.edu.cn>
2022-12-12 20:56:43 +0800

    refactor: change column name
    Signed-off-by: ClSlaid <cailue@bupt.edu.cn>

--- src/query/sql/src/planner/binder/ddl/database.rs
+++ src/query/sql/src/planner/binder/ddl/database.rs
@@ -71,7 +71,7 @@ impl<'a> Binder {
         if *full {
             select_builder.with_column("catalog AS Catalog");
         }
-        select_builder.with_column(format!("name AS database_in_{ctl}"));
+        select_builder.with_column(format!("name AS databases_in_{ctl}"));
         select_builder.with_order_by("catalog");
         select_builder.with_order_by("name");
         match limit {
