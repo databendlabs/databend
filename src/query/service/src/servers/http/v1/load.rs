c3c103a157a2c4cc9c55546fe2d8e5c638ff6781
Yijun Zhao <ariesdevil77@gmail.com>
2022-12-12 09:53:30 +0800

    improve with reviewers suggestion

--- src/query/service/src/servers/http/v1/load.rs
+++ src/query/service/src/servers/http/v1/load.rs
@@ -100,15 +100,6 @@ pub async fn streaming_load(
         .unwrap_or("");
 
     let settings = context.get_settings();
-    for (key, value) in req.headers().iter() {
-        if settings.has_setting(key.as_str()) {
-            let value = value.to_str().map_err(InternalServerError)?;
-            let value = parse_escape_string(remove_quote(value.as_bytes()));
-            settings
-                .set_settings(key.to_string(), value, false)
-                .map_err(InternalServerError)?
-        }
-    }
 
     for (key, value) in req.headers().iter() {
         if settings.has_setting(key.as_str()) {
