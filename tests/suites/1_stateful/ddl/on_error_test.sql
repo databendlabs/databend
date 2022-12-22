029394c958f81f4928fdb9b22ef70365725ed4ff
Yijun Zhao <ariesdevil77@gmail.com>
2022-12-22 14:14:56 +0800

    add ndjson,tsv,xml tests

--- tests/suites/1_stateful/ddl/on_error_test.sql
+++ tests/suites/1_stateful/ddl/on_error_test.sql
@@ -4,3 +4,30 @@ CREATE TABLE wrong_csv
     City   VARCHAR,
     Score  INT
 );
+
+CREATE TABLE wrong_ndjson
+(
+    a Boolean,
+    b Int,
+    c Float,
+    d String,
+    e Date,
+    f Timestamp,
+    g Array(Int),
+    h Tuple(Int, String),
+    i Variant
+);
+
+CREATE TABLE wrong_tsv
+(
+    a string,
+    b int
+);
+
+CREATE TABLE wrong_xml (
+  id          INT,
+  name        VARCHAR,
+  data        VARCHAR,
+  create_time TIMESTAMP,
+  empty       VARCHAR NULL
+);
