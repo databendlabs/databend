8a7c57f2ebdc2d35e08ece0060b7960225c1a3b7
Xuanwo <github@xuanwo.io>
2022-12-26 18:03:55 +0800

    Fix ssl test
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/query/service/tests/it/servers/http/http_query_handlers.rs
+++ src/query/service/tests/it/servers/http/http_query_handlers.rs
@@ -63,14 +63,7 @@ use wiremock::Mock;
 use wiremock::MockServer;
 use wiremock::ResponseTemplate;
 
-use crate::tests::tls_constants::TEST_CA_CERT;
-use crate::tests::tls_constants::TEST_CN_NAME;
-use crate::tests::tls_constants::TEST_SERVER_CERT;
-use crate::tests::tls_constants::TEST_SERVER_KEY;
-use crate::tests::tls_constants::TEST_TLS_CA_CERT;
-use crate::tests::tls_constants::TEST_TLS_CLIENT_IDENTITY;
-use crate::tests::tls_constants::TEST_TLS_SERVER_CERT;
-use crate::tests::tls_constants::TEST_TLS_SERVER_KEY;
+use crate::tests::tls_constants::*;
 use crate::tests::ConfigBuilder;
 use crate::tests::TestGlobalServices;
 
@@ -1039,7 +1032,8 @@ async fn test_http_service_tls_server_mutual_tls() -> Result<()> {
 
     // get identity
     let mut buf = Vec::new();
-    File::open(TEST_TLS_CLIENT_IDENTITY)?.read_to_end(&mut buf)?;
+    File::open(TEST_TLS_CLIENT_KEY)?.read_to_end(&mut buf)?;
+    File::open(TEST_TLS_CLIENT_CERT)?.read_to_end(&mut buf)?;
     let pkcs12 = reqwest::Identity::from_pem(&buf).unwrap();
     let mut buf = Vec::new();
     File::open(TEST_TLS_CA_CERT)?.read_to_end(&mut buf)?;
