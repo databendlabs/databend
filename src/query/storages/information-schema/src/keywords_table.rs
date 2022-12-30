1d2dd9f7511534fd22d01490c8be2c27f418a299
hantmc <hantmac@outlook.com>
2022-12-30 14:58:22 +0800

    fix

--- src/query/storages/information-schema/src/keywords_table.rs
+++ src/query/storages/information-schema/src/keywords_table.rs
@@ -28,7 +28,7 @@ pub struct KeywordsTable {}
 impl KeywordsTable {
     pub fn create(table_id: u64) -> Arc<dyn Table> {
         let all_keywords = all_reserved_keywords();
-        let query = "SELECT ".to_owned() + &all_keywords + " AS WORD, 1 AS RESERVED";
+        let query = "SELECT '".to_owned() + &all_keywords + "' AS WORD, 1 AS RESERVED";
 
         let mut options = BTreeMap::new();
         options.insert(QUERY.to_string(), query);
