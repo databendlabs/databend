d309f1eac0116dbe6d00ef7736d2318d945a218c
sundy-li <543950155@qq.com>
2022-12-31 11:13:51 +0800

    chore(expression): fix cross join bug

--- src/query/service/src/pipelines/processors/transforms/hash_join/probe_join/cross_join.rs
+++ src/query/service/src/pipelines/processors/transforms/hash_join/probe_join/cross_join.rs
@@ -13,9 +13,11 @@
 // limitations under the License.
 
 use common_exception::Result;
+use common_expression::BlockEntry;
 use common_expression::DataBlock;
 use common_expression::DataField;
 use common_expression::DataSchemaRefExt;
+use common_expression::Value;
 
 use crate::pipelines::processors::transforms::hash_join::ProbeState;
 use crate::pipelines::processors::JoinHashTable;
@@ -45,8 +47,7 @@ impl JoinHashTable {
         let build_block = DataBlock::concat(&build_blocks)?;
         let mut results = Vec::with_capacity(input.num_rows());
         for i in 0..input.num_rows() {
-            let probe_block = DataBlock::take(input, &[i as u32])?;
-            results.push(self.merge_with_constant_block(&build_block, &probe_block)?);
+            results.push(self.merge_with_constant_block(&build_block, input, i)?);
         }
         Ok(results)
     }
@@ -56,11 +57,18 @@ impl JoinHashTable {
         &self,
         build_block: &DataBlock,
         probe_block: &DataBlock,
+        take_index: usize,
     ) -> Result<DataBlock> {
         let columns = Vec::with_capacity(build_block.num_columns() + probe_block.num_columns());
         let mut replicated_probe_block = DataBlock::new(columns, build_block.num_rows());
+
         for col in probe_block.columns() {
-            replicated_probe_block.add_column(col.clone());
+            let value_ref = col.value.as_ref();
+            let scalar = unsafe { value_ref.index_unchecked(take_index) };
+            replicated_probe_block.add_column(BlockEntry {
+                data_type: col.data_type.clone(),
+                value: Value::Scalar(scalar.to_owned()),
+            });
         }
         for col in build_block.columns() {
             replicated_probe_block.add_column(col.clone());
