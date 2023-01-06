9f7956db087ce43eb7c559f95de28ae90c020449
sundy-li <543950155@qq.com>
2023-01-06 21:57:24 +0800

    chore(query): disable return result

--- src/query/functions/src/scalars/comparison.rs
+++ src/query/functions/src/scalars/comparison.rs
@@ -550,27 +550,26 @@ fn register_like(registry: &mut FunctionRegistry) {
         |_, _| FunctionDomain::Full,
         vectorize_like(|str, pat, _, pattern_type| {
             match pattern_type {
-                PatternType::OrdinalStr => Ok(str == pat),
+                PatternType::OrdinalStr => str == pat,
                 PatternType::EndOfPercent => {
                     // fast path, can use starts_with
                     let starts_with = &pat[..pat.len() - 1];
-                    Ok(str.starts_with(starts_with))
+                    str.starts_with(starts_with)
                 }
                 PatternType::StartOfPercent => {
                     // fast path, can use ends_with
-                    let ends_with = &pat[1..];
-                    Ok(str.ends_with(ends_with))
+                    str.ends_with(&pat[1..])
                 }
 
                 PatternType::SurroundByPercent => {
                     if pat.len() > 2 {
-                        Ok(memmem::find(str, &pat[1..pat.len() - 1]).is_some())
+                        memmem::find(str, &pat[1..pat.len() - 1]).is_some()
                     } else {
-                        Ok(str.is_empty())
+                        str.is_empty()
                     }
                 }
 
-                PatternType::PatternStr => Ok(like(str, pat)),
+                PatternType::PatternStr => like(str, pat),
             }
         }),
     );
@@ -593,7 +592,7 @@ fn register_like(registry: &mut FunctionRegistry) {
 }
 
 fn vectorize_like(
-    func: impl Fn(&[u8], &[u8], EvalContext, PatternType) -> Result<bool, String> + Copy,
+    func: impl Fn(&[u8], &[u8], EvalContext, PatternType) -> bool + Copy,
 ) -> impl Fn(
     ValueRef<StringType>,
     ValueRef<StringType>,
@@ -603,14 +602,14 @@ fn vectorize_like(
     move |arg1, arg2, ctx| match (arg1, arg2) {
         (ValueRef::Scalar(arg1), ValueRef::Scalar(arg2)) => {
             let pattern_type = check_pattern_type(arg2, false);
-            Ok(Value::Scalar(func(arg1, arg2, ctx, pattern_type)?))
+            Ok(Value::Scalar(func(arg1, arg2, ctx, pattern_type)))
         }
         (ValueRef::Column(arg1), ValueRef::Scalar(arg2)) => {
             let arg1_iter = StringType::iter_column(&arg1);
             let mut builder = MutableBitmap::with_capacity(arg1.len());
             let pattern_type = check_pattern_type(arg2, false);
             for arg1 in arg1_iter {
-                builder.push(func(arg1, arg2, ctx, pattern_type)?);
+                builder.push(func(arg1, arg2, ctx, pattern_type));
             }
             Ok(Value::Column(builder.into()))
         }
@@ -619,7 +618,7 @@ fn vectorize_like(
             let mut builder = MutableBitmap::with_capacity(arg2.len());
             for arg2 in arg2_iter {
                 let pattern_type = check_pattern_type(arg2, false);
-                builder.push(func(arg1, arg2, ctx, pattern_type)?);
+                builder.push(func(arg1, arg2, ctx, pattern_type));
             }
             Ok(Value::Column(builder.into()))
         }
@@ -629,7 +628,7 @@ fn vectorize_like(
             let mut builder = MutableBitmap::with_capacity(arg2.len());
             for (arg1, arg2) in arg1_iter.zip(arg2_iter) {
                 let pattern_type = check_pattern_type(arg2, false);
-                builder.push(func(arg1, arg2, ctx, pattern_type)?);
+                builder.push(func(arg1, arg2, ctx, pattern_type));
             }
             Ok(Value::Column(builder.into()))
         }
