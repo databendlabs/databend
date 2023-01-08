069574b697ddc0efdf63b36c7b74ada3cc33d581
sundy-li <543950155@qq.com>
2023-01-08 11:23:18 +0800

    refactor(query): fix codegen

--- src/query/codegen/src/writes/mod.rs
+++ src/query/codegen/src/writes/mod.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// mod arithmetics_type_v2;
+mod arithmetics_type_v2;
 mod register;
 
-// pub use arithmetics_type_v2::*;
+pub use arithmetics_type_v2::*;
 pub use register::*;
