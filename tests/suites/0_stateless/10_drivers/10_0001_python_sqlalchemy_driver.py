1427148fb4b38d8b691d57b5057358bb0f3cd80e
蔡略 <cailue@bupt.edu.cn>
2023-01-10 22:41:24 +0800

    ci: fix sqlalchemy test
    engine.connect() is deprecated in sqlalchemy2.0
    use `engine.begin()` instead
    
    Signed-off-by: 蔡略 <cailue@bupt.edu.cn>

--- tests/suites/0_stateless/10_drivers/10_0001_python_sqlalchemy_driver.py
+++ tests/suites/0_stateless/10_drivers/10_0001_python_sqlalchemy_driver.py
@@ -9,16 +9,18 @@ if tcp_port is None:
 else:
     port = tcp_port
 
+results = []
 uri = "mysql+pymysql://root:root@localhost:" + port + "/"
 engine = sqlalchemy.create_engine(uri)
-conn = engine.connect()
-conn.execute("create database if not exists book_db")
-conn.execute("use book_db")
-conn.execute(
-    "create table if not exists books(title varchar(255), author varchar(255), date varchar(255))"
-)
-conn.execute("insert into books values('mybook', 'author', '2022')")
-results = conn.execute("select * from books").fetchall()
+with engine.begin() as conn:
+    conn.execute("create database if not exists book_db")
+    conn.execute("use book_db")
+    conn.execute(
+        "create table if not exists books(title varchar(255), author varchar(255), date varchar(255))"
+    )
+    conn.execute("insert into books values('mybook', 'author', '2022')")
+    results = conn.execute("select * from books").fetchall()
+    conn.execute("drop database book_db")
+
 for result in results:
     print(result)
-conn.execute("drop database book_db")
