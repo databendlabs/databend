7c2ace477189cfc1e65be95972387bed75e05652
sundy-li <543950155@qq.com>
2023-01-15 16:45:10 +0800

    chore(query): fix lint

--- src/query/sql/src/planner/semantic/distinct_to_groupby.rs
+++ src/query/sql/src/planner/semantic/distinct_to_groupby.rs
@@ -35,70 +35,70 @@ impl VisitorMut for DistinctToGroupBy {
         } = stmt;
 
         if group_by.is_empty() && select_list.len() == 1 && from.len() == 1 {
-            match &select_list[0] {
-                common_ast::ast::SelectTarget::AliasedExpr { expr, alias } => match expr.as_ref() {
-                    Expr::FunctionCall {
+            if let common_ast::ast::SelectTarget::AliasedExpr {
+                expr:
+                    box Expr::FunctionCall {
                         span,
                         distinct,
                         name,
                         args,
                         ..
-                    } if (name.name.to_ascii_lowercase() == "count" && *distinct)
-                        || name.name.to_ascii_lowercase() == "count_distinct" =>
-                    {
-                        let tmp_token = span[0].clone();
-                        let subquery = Query {
-                            span: &[],
-                            with: None,
-                            body: SetExpr::Select(Box::new(SelectStmt {
-                                span: &[],
-                                distinct: false,
-                                select_list: vec![],
-                                from: from.clone(),
-                                selection: None,
-                                group_by: args.clone(),
-                                having: None,
-                            })),
-                            order_by: vec![],
-                            limit: vec![],
-                            offset: None,
-                            ignore_result: false,
-                        };
-
-                        let new_stmt = SelectStmt {
+                    },
+                alias,
+            } = &select_list[0]
+            {
+                if (name.name.to_ascii_lowercase() == "count" && *distinct)
+                    || name.name.to_ascii_lowercase() == "count_distinct"
+                {
+                    let tmp_token = span[0].clone();
+                    let subquery = Query {
+                        span: &[],
+                        with: None,
+                        body: SetExpr::Select(Box::new(SelectStmt {
                             span: &[],
                             distinct: false,
-                            select_list: vec![common_ast::ast::SelectTarget::AliasedExpr {
-                                expr: Box::new(Expr::FunctionCall {
-                                    span: &[],
-                                    distinct: false,
-                                    name: Identifier {
-                                        name: "count".to_string(),
-                                        quote: None,
-                                        span: tmp_token.clone(),
-                                    },
-                                    args: vec![],
-                                    params: vec![],
-                                }),
-                                alias: alias.clone(),
-                            }],
-                            from: vec![TableReference::Subquery {
-                                span: &[],
-                                subquery: Box::new(subquery),
-                                alias: None,
-                            }],
-                            selection: selection.clone(),
-                            group_by: vec![],
-                            having: having.clone(),
-                        };
+                            select_list: vec![],
+                            from: from.clone(),
+                            selection: None,
+                            group_by: args.clone(),
+                            having: None,
+                        })),
+                        order_by: vec![],
+                        limit: vec![],
+                        offset: None,
+                        ignore_result: false,
+                    };
 
-                        *stmt = new_stmt;
-                    }
-                    _ => {}
-                },
-                _ => {}
+                    let new_stmt = SelectStmt {
+                        span: &[],
+                        distinct: false,
+                        select_list: vec![common_ast::ast::SelectTarget::AliasedExpr {
+                            expr: Box::new(Expr::FunctionCall {
+                                span: &[],
+                                distinct: false,
+                                name: Identifier {
+                                    name: "count".to_string(),
+                                    quote: None,
+                                    span: tmp_token.clone(),
+                                },
+                                args: vec![],
+                                params: vec![],
+                            }),
+                            alias: alias.clone(),
+                        }],
+                        from: vec![TableReference::Subquery {
+                            span: &[],
+                            subquery: Box::new(subquery),
+                            alias: None,
+                        }],
+                        selection: selection.clone(),
+                        group_by: vec![],
+                        having: having.clone(),
+                    };
+
+                    *stmt = new_stmt;
+                }
             }
-            return;
         }
     }
 }
