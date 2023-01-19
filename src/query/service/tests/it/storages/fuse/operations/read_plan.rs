8aff9e41de1ced6e6143d35f001f50e939015630
sundy-li <543950155@qq.com>
2023-01-19 15:30:47 +0800

    feat(query): enable empty projection

--- src/query/service/tests/it/storages/fuse/operations/read_plan.rs
+++ src/query/service/tests/it/storages/fuse/operations/read_plan.rs
@@ -182,8 +182,7 @@ async fn test_fuse_table_exact_statistic() -> Result<()> {
         };
         let (stats, parts) = table.read_partitions(ctx.clone(), Some(push_downs)).await?;
         assert_eq!(stats.read_rows, num_blocks * rows_per_block);
-        assert!(parts.is_empty());
+        assert!(parts.len() > 0);
     }
-
     Ok(())
 }
