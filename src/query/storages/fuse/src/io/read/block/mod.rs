6650451a93f1392438cbed71002a47c50d0e4320
BohuTANG <overred.shuttler@gmail.com>
2023-01-23 19:24:45 +0800

    read/block_* -> read/block/*

--- src/query/storages/fuse/src/io/read/block/mod.rs
+++ src/query/storages/fuse/src/io/read/block/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 Datafuse Labs.
+// Copyright 2023 Datafuse Labs.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,20 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+mod block_reader;
+mod block_reader_native;
+mod block_reader_parquet;
+mod decompressor;
 
-mod block;
-mod bloom;
-mod meta;
-mod read_settings;
-mod snapshot_history_reader;
-
-pub use block::BlockReader;
-pub use block::MergeIOReadResult;
-pub use block::NativeReaderExt;
-pub use block::UncompressedBuffer;
-pub use bloom::BloomFilterReader;
-pub use meta::MetaReaders;
-pub use meta::SegmentInfoReader;
-pub use meta::TableSnapshotReader;
-pub use read_settings::ReadSettings;
-pub use snapshot_history_reader::SnapshotHistoryReader;
+pub use block_reader::BlockReader;
+pub use block_reader::MergeIOReadResult;
+pub use block_reader_native::NativeReaderExt;
+pub use decompressor::UncompressedBuffer;
