9f7f6becf2f85dab516d939d79fd4dfb630b8337
RinChanNOWWW <hzy427@gmail.com>
2023-01-29 22:10:20 +0800

    List and match files manually.

--- src/query/storages/parquet/src/table_function/table.rs
+++ src/query/storages/parquet/src/table_function/table.rs
@@ -22,6 +22,7 @@ use common_arrow::arrow::datatypes::DataType as ArrowDataType;
 use common_arrow::arrow::datatypes::Field as ArrowField;
 use common_arrow::arrow::datatypes::Schema as ArrowSchema;
 use common_arrow::arrow::io::parquet::read as pread;
+use common_base::runtime::GlobalIORuntime;
 use common_catalog::plan::DataSourcePlan;
 use common_catalog::plan::PartStatistics;
 use common_catalog::plan::Partitions;
@@ -38,7 +39,12 @@ use common_meta_app::schema::TableIdent;
 use common_meta_app::schema::TableInfo;
 use common_meta_app::schema::TableMeta;
 use common_pipeline_core::Pipeline;
+use futures::TryStreamExt;
+use opendal::raw::get_basename;
+use opendal::raw::get_parent;
+use opendal::ObjectMode;
 use opendal::Operator;
+use regex::Regex;
 
 use super::TableContext;
 use crate::ReadOptions;
@@ -54,7 +60,14 @@ pub struct ParquetTable {
 }
 
 impl ParquetTable {
-    pub fn create(
+    /// Create the table function `read_parquet`.
+    ///
+    /// Syntax:
+    ///
+    /// ```sql
+    /// select * from read_parquet('path1', 'path2', ..., prune_pages=>true, refresh_meta_cache=>true, ...);
+    /// ```
+    pub fn create_table_function(
         database_name: &str,
         table_func_name: &str,
         table_id: u64,
@@ -65,10 +78,31 @@ impl ParquetTable {
                 "Should enable `allow_insecure` to use table function `read_parquet`",
             ));
         }
+        let mut builder = opendal::services::fs::Builder::default();
+        builder.root("/");
+        let operator = Operator::new(builder.build()?);
 
-        // Syntax:
-        // read_parquet('path1', 'path2', ..., prune_pages=>true, refresh_meta_cache=>true, ...)
-        // The options should be behind the file paths.
+        Self::create(
+            database_name,
+            table_func_name,
+            table_id,
+            table_args,
+            operator,
+        )
+    }
+
+    pub fn create(
+        database_name: &str,
+        table_func_name: &str,
+        table_id: u64,
+        table_args: TableArgs,
+        operator: Operator,
+    ) -> Result<Arc<dyn TableFunction>> {
+        if !GlobalConfig::instance().storage.allow_insecure {
+            return Err(ErrorCode::StorageInsecure(
+                "Should enable `allow_insecure` to use table function `read_parquet`",
+            ));
+        }
 
         if table_args.is_none() {
             return Err(ErrorCode::BadArguments(
@@ -87,28 +121,12 @@ impl ParquetTable {
             ));
         }
 
-        let mut builder = opendal::services::fs::Builder::default();
-        builder.root("/");
-        let operator = Operator::new(builder.build()?);
-
-        let mut file_locations = Vec::with_capacity(args.len());
+        let mut paths = Vec::with_capacity(path_num);
         for arg in args.iter().take(path_num) {
             match arg {
                 Scalar::String(path) => {
-                    let maybe_glob_path = std::str::from_utf8(path).unwrap();
-                    // TODO: use OpenDAL.
-                    let paths = glob::glob(maybe_glob_path)
-                        .map_err(|e| ErrorCode::Internal(format!("glob error: {}", e)))?;
-                    for entry in paths {
-                        match entry {
-                            Ok(path) => {
-                                file_locations.push(path.to_string_lossy().to_string());
-                            }
-                            Err(e) => {
-                                return Err(ErrorCode::Internal(format!("glob error: {}", e)));
-                            }
-                        }
-                    }
+                    let path = std::str::from_utf8(path).unwrap();
+                    paths.push(path.to_string());
                 }
                 _ => {
                     return Err(ErrorCode::BadArguments(
@@ -118,26 +136,10 @@ impl ParquetTable {
             }
         }
 
-        if file_locations.is_empty() {
-            return Err(ErrorCode::BadArguments(
-                "No matched files found for read_parquet",
-            ));
-        }
+        let (file_locations, arrow_schema) = Self::prepare_metas(paths, operator.clone())?;
 
         // Now, `read_options` is hard-coded.
         let read_options = ReadOptions::try_from(&args[path_num..])?;
-
-        // Infer schema from the first parquet file.
-        // Assume all parquet files have the same schema.
-        // If not, throw error during reading.
-        let mut reader = operator.object(&file_locations[0]).blocking_reader()?;
-        let first_meta = pread::read_metadata(&mut reader).map_err(|e| {
-            ErrorCode::Internal(format!(
-                "Read parquet file '{}''s meta error: {}",
-                &file_locations[0], e
-            ))
-        })?;
-        let arrow_schema = pread::infer_schema(&first_meta)?;
 
         let table_info = TableInfo {
             ident: TableIdent::new(table_id, 0),
@@ -163,6 +165,74 @@ impl ParquetTable {
             operator,
             read_options,
         }))
+    }
+
+    fn prepare_metas(paths: Vec<String>, operator: Operator) -> Result<(Vec<String>, ArrowSchema)> {
+        let (file_locations, meta) = GlobalIORuntime::instance().block_on(async move {
+            let mut file_locations = Vec::with_capacity(paths.len());
+            for maybe_glob_path in paths.iter() {
+                let files = Self::list_files(maybe_glob_path, &operator).await?;
+                file_locations.extend(files);
+            }
+
+            if file_locations.is_empty() {
+                return Err(ErrorCode::BadArguments(
+                    "No matched files found for read_parquet",
+                ));
+            }
+
+            // Infer schema from the first parquet file.
+            // Assume all parquet files have the same schema.
+            // If not, throw error during reading.
+            let mut reader = operator.object(&file_locations[0]).reader().await?;
+            let first_meta = pread::read_metadata_async(&mut reader).await.map_err(|e| {
+                ErrorCode::Internal(format!(
+                    "Read parquet file '{}''s meta error: {}",
+                    &file_locations[0], e
+                ))
+            })?;
+
+            Ok((file_locations, first_meta))
+        })?;
+
+        let arrow_schema = pread::infer_schema(&meta)?;
+
+        Ok((file_locations, arrow_schema))
+    }
+
+    /// List files from the given path with pattern.
+    ///
+    /// Only support simple patterns (one level): `path/to/dir/*.parquet`.
+    async fn list_files(maybe_glob_path: &str, operator: &Operator) -> Result<Vec<String>> {
+        let basename = get_basename(maybe_glob_path);
+        let regex = match Regex::new(basename) {
+            Ok(regex) => regex,
+            Err(_) => {
+                // not a regex format, push the path directly.
+                return Ok(vec![maybe_glob_path.to_string()]);
+            }
+        };
+
+        let obj = operator.object(get_parent(maybe_glob_path));
+        let mut files = Vec::new();
+        let mut list = obj.list().await?;
+        while let Some(de) = list.try_next().await? {
+            match de.mode().await? {
+                ObjectMode::FILE => {
+                    if regex.is_match(de.name()) {
+                        files.push(de.path().to_string());
+                    }
+                }
+                ObjectMode::DIR => {
+                    return Err(ErrorCode::BadArguments(
+                        "read_parquet only support simple patterns (example: `path/to/dir/*.parquet`)",
+                    ));
+                }
+                ObjectMode::Unknown => continue,
+            }
+        }
+
+        Ok(files)
     }
 }
 
