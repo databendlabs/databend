1f06dd90a42ba6a352c74951eac1c0d869f15189
dantengsky <dantengsky@gmail.com>
2023-02-08 17:28:18 +0800

    tweak doc

--- src/query/storages/common/cache/src/providers/disk_cache.rs
+++ src/query/storages/common/cache/src/providers/disk_cache.rs
@@ -109,9 +109,8 @@ impl CacheAccessor<String, Vec<u8>, common_cache::DefaultHashBuilder, Count> for
     }
 }
 
-/// Assuming that the crc32 is at the end of `bytes` and encoded as le u32.
+/// The crc32 checksum is stored at the end of `bytes` and encoded as le u32.
 // Although parquet page has built-in crc, but it is optional (and not generated in parquet2)
-// Later, if cache data is put into redis, we can reuse the checksum logic
 fn validate_checksum(bytes: &[u8]) -> Result<()> {
     let total_len = bytes.len();
     if total_len <= 4 {
