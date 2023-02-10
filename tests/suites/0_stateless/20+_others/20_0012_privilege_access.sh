2864c20863dab386b02544439397fd792c4ea351
yufan <wangyufan022@gmail.com>
2023-02-10 19:15:43 +0800

    add privilege check for procedure

--- tests/suites/0_stateless/20+_others/20_0012_privilege_access.sh
+++ tests/suites/0_stateless/20+_others/20_0012_privilege_access.sh
@@ -48,10 +48,12 @@ echo "GRANT SELECT ON system.fuse_snapshot TO 'test-user'@'$QUERY_MYSQL_HANDLER_
 echo "optimize table t20_0012 all" | $TEST_USER_CONNECT
 ## verify
 echo "select count(*)=1  from fuse_snapshot('default', 't20_0012')" | $TEST_USER_CONNECT
+## revoke privilege
+echo "REVOKE SELECT ON system.fuse_snapshot FROM 'test-user'@'$QUERY_MYSQL_HANDLER_HOST'" | $MYSQL_CLIENT_CONNECT
 
 ## select data
 ## Init tables
-echo "CREATE TABLE default.t20_0012_a(c int)" | $MYSQL_CLIENT_CONNECT
+echo "CREATE TABLE default.t20_0012_a(c int) CLUSTER BY(c)" | $MYSQL_CLIENT_CONNECT
 echo "GRANT INSERT ON default.t20_0012_a TO 'test-user'@'$QUERY_MYSQL_HANDLER_HOST'" | $MYSQL_CLIENT_CONNECT
 echo "INSERT INTO default.t20_0012_a values(1)" | $TEST_USER_CONNECT
 echo "CREATE TABLE default.t20_0012_b(c int)" | $MYSQL_CLIENT_CONNECT
@@ -68,14 +70,32 @@ echo "GRANT SELECT ON default.t20_0012_b TO 'test-user'@'$QUERY_MYSQL_HANDLER_HO
 echo "select * from default.t20_0012_b order by c" | $TEST_USER_CONNECT
 ## Create view table
 echo "create database default2" | $MYSQL_CLIENT_CONNECT
-echo "create view default2.v_t20_0012 as select * from default.t20_0012_a;" | $MYSQL_CLIENT_CONNECT
+echo "create view default2.v_t20_0012 as select * from default.t20_0012_a" | $MYSQL_CLIENT_CONNECT
 ## Verify view table privilege
-echo "select * from default2.v_t20_0012;" | $TEST_USER_CONNECT
+echo "select * from default2.v_t20_0012" | $TEST_USER_CONNECT
 ## Only grant privilege for view table
 echo "GRANT SELECT ON default2.v_t20_0012 TO 'test-user'@'$QUERY_MYSQL_HANDLER_HOST'" | $MYSQL_CLIENT_CONNECT
 echo "REVOKE SELECT ON default.t20_0012_a FROM 'test-user'@'$QUERY_MYSQL_HANDLER_HOST'" | $MYSQL_CLIENT_CONNECT
 echo "REVOKE SELECT ON default.t20_0012_b FROM 'test-user'@'$QUERY_MYSQL_HANDLER_HOST'" | $MYSQL_CLIENT_CONNECT
-echo "select * from default2.v_t20_0012;" | $TEST_USER_CONNECT
+echo "select * from default2.v_t20_0012" | $TEST_USER_CONNECT
+
+## select procedure
+## clustering_information
+echo "select count(*)=1 from clustering_information('default', 't20_0012_a')" | $TEST_USER_CONNECT
+echo "GRANT SELECT ON system.clustering_information TO 'test-user'@'$QUERY_MYSQL_HANDLER_HOST'" | $MYSQL_CLIENT_CONNECT
+echo "select count(*)=1 from clustering_information('default', 't20_0012_a')" | $TEST_USER_CONNECT
+## fuse_snapshot
+echo "select count(*)=1 from fuse_snapshot('default', 't20_0012_a')" | $TEST_USER_CONNECT
+echo "GRANT SELECT ON system.fuse_snapshot TO 'test-user'@'$QUERY_MYSQL_HANDLER_HOST'" | $MYSQL_CLIENT_CONNECT
+echo "select count(*)=1 from fuse_snapshot('default', 't20_0012_a')" | $TEST_USER_CONNECT
+## fuse_segment
+echo "select count(*)=0 from fuse_segment('default', 't20_0012_a', '')" | $TEST_USER_CONNECT
+echo "GRANT SELECT ON system.fuse_segment TO 'test-user'@'$QUERY_MYSQL_HANDLER_HOST'" | $MYSQL_CLIENT_CONNECT
+echo "select count(*)=0 from fuse_segment('default', 't20_0012_a', '')" | $TEST_USER_CONNECT
+## fuse_block
+echo "select count(*)=1 from fuse_block('default', 't20_0012_a')" | $TEST_USER_CONNECT
+echo "GRANT SELECT ON system.fuse_block TO 'test-user'@'$QUERY_MYSQL_HANDLER_HOST'" | $MYSQL_CLIENT_CONNECT
+echo "select count(*)=1 from fuse_block('default', 't20_0012_a')" | $TEST_USER_CONNECT
 
 ## Drop table.
 echo "drop table default.t20_0012 all" | $MYSQL_CLIENT_CONNECT
