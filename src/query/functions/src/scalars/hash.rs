8f0b8b66673210b9559e5938910c811710070703
Andy Lok <andylokandy@hotmail.com>
2023-02-19 03:10:42 +0800

    fix

--- src/query/functions/src/scalars/hash.rs
+++ src/query/functions/src/scalars/hash.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(clippy::unnecessary-cast)]
+#![allow(clippy::unnecessary_cast)]
 
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hash;
