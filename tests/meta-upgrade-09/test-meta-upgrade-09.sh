3f6b9a32eb735619475c9eb46bbb12076888fde9
baishen <baishen2009@gmail.com>
2023-02-23 22:52:48 +0800

    update meta-upgrade-09

--- tests/meta-upgrade-09/test-meta-upgrade-09.sh
+++ tests/meta-upgrade-09/test-meta-upgrade-09.sh
@@ -38,10 +38,10 @@ fi
 
 echo " === check ver"
 ./target/${BUILD_PROFILE}/databend-meta-upgrade-09 --cmd print --raft-dir "$meta_dir"
-count_of_v28=$(./target/${BUILD_PROFILE}/databend-meta-upgrade-09 --cmd print --raft-dir "$meta_dir" | grep ' ver: 28' | wc -l)
-if [ "$count_of_table_meta" == "$count_of_v28" ]; then
-    echo " === count of ver=28: $count_of_v28; OK"
+count_of_v29=$(./target/${BUILD_PROFILE}/databend-meta-upgrade-09 --cmd print --raft-dir "$meta_dir" | grep ' ver: 29' | wc -l)
+if [ "$count_of_table_meta" == "$count_of_v29" ]; then
+    echo " === count of ver=29: $count_of_v29; OK"
 else
-    echo " === mismatching lines of ver=28: expect: $count_of_table_meta; got: $count_of_v28"
+    echo " === mismatching lines of ver=29: expect: $count_of_table_meta; got: $count_of_v29"
     exit 1
 fi
