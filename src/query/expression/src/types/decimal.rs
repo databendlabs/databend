1941669fc04472b508c5bb1f98d1a943099b01d7
Yang Xiufeng <yangxiufeng.c@gmail.com>
2023-03-04 22:43:15 +0800

    fix: to_variant(decimal) should be number.

--- src/query/expression/src/types/decimal.rs
+++ src/query/expression/src/types/decimal.rs
@@ -196,6 +196,15 @@ pub enum DecimalScalar {
     Decimal256(i256, DecimalSize),
 }
 
+impl DecimalScalar {
+    pub fn to_float64(&self) -> f64 {
+        match self {
+            DecimalScalar::Decimal128(v, size) => i128::to_float64(*v, size.scale),
+            DecimalScalar::Decimal256(v, size) => i256::to_float64(*v, size.scale),
+        }
+    }
+}
+
 #[derive(Clone, PartialEq, EnumAsInner)]
 pub enum DecimalColumn {
     Decimal128(Buffer<i128>, DecimalSize),
@@ -260,6 +269,8 @@ pub trait Decimal:
     fn from_u64(value: u64) -> Self;
     fn from_i64(value: i64) -> Self;
     fn de_binary(bytes: &mut &[u8]) -> Self;
+
+    fn to_float64(self, scale: u8) -> f64;
 
     fn try_downcast_column(column: &Column) -> Option<(Buffer<Self>, DecimalSize)>;
     fn try_downcast_builder<'a>(builder: &'a mut ColumnBuilder) -> Option<&'a mut Vec<Self>>;
@@ -374,6 +385,11 @@ impl Decimal for i128 {
         *bytes = &bytes[std::mem::size_of::<Self>()..];
 
         i128::from_le_bytes(bs)
+    }
+
+    fn to_float64(self, scale: u8) -> f64 {
+        let div = 10_f64.powi(scale as i32);
+        self as f64 / div
     }
 
     fn try_downcast_column(column: &Column) -> Option<(Buffer<Self>, DecimalSize)> {
@@ -506,6 +522,11 @@ impl Decimal for i256 {
         *bytes = &bytes[std::mem::size_of::<Self>()..];
 
         i256::from_le_bytes(bs)
+    }
+
+    fn to_float64(self, scale: u8) -> f64 {
+        let div = 10_f64.powi(scale as i32);
+        self.as_f64() / div
     }
 
     fn try_downcast_column(column: &Column) -> Option<(Buffer<Self>, DecimalSize)> {
