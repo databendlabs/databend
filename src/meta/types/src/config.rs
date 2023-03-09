adb5382b240fa747762496850653fe21d2e7e883
everpcpc <git@everpcpc.com>
2023-03-09 09:17:04 +0800

    fix: wrong env for VERGEN_GIT_SEMVER

--- src/meta/types/src/config.rs
+++ src/meta/types/src/config.rs
@@ -15,7 +15,7 @@
 use once_cell::sync::Lazy;
 
 pub static DATABEND_COMMIT_VERSION: Lazy<String> = Lazy::new(|| {
-    let build_semver = option_env!("VERGEN_BUILD_SEMVER");
+    let build_semver = option_env!("VERGEN_GIT_SEMVER");
     let git_sha = option_env!("VERGEN_GIT_SHA");
     let rustc_semver = option_env!("VERGEN_RUSTC_SEMVER");
     let timestamp = option_env!("VERGEN_BUILD_TIMESTAMP");
