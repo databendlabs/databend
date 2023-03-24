bdf27e5b3e7950f9c7c916ce0ec322481d97816b
sky <geekvpt@gmail.com>
2023-03-24 21:30:36 +0800

    feat: FuseBlockTable use the limit push down info

--- src/query/storages/fuse/src/table_functions/fuse_blocks/fuse_block_table.rs
+++ src/query/storages/fuse/src/table_functions/fuse_blocks/fuse_block_table.rs
@@ -115,7 +115,7 @@ impl Table for FuseBlockTable {
     fn read_data(
         &self,
         ctx: Arc<dyn TableContext>,
-        _: &DataSourcePlan,
+        plan: &DataSourcePlan,
         pipeline: &mut Pipeline,
     ) -> Result<()> {
         pipeline.add_source(
@@ -126,6 +126,7 @@ impl Table for FuseBlockTable {
                     self.arg_database_name.to_owned(),
                     self.arg_table_name.to_owned(),
                     self.arg_snapshot_id.to_owned(),
+                    plan.push_downs.as_ref().and_then(|x| x.limit),
                 )
             },
             1,
@@ -141,6 +142,7 @@ struct FuseBlockSource {
     arg_database_name: String,
     arg_table_name: String,
     arg_snapshot_id: Option<String>,
+    limit: Option<usize>,
 }
 
 impl FuseBlockSource {
@@ -150,6 +152,7 @@ impl FuseBlockSource {
         arg_database_name: String,
         arg_table_name: String,
         arg_snapshot_id: Option<String>,
+        limit: Option<usize>,
     ) -> Result<ProcessorPtr> {
         AsyncSourcer::create(ctx.clone(), output, FuseBlockSource {
             ctx,
@@ -157,6 +160,7 @@ impl FuseBlockSource {
             arg_table_name,
             arg_database_name,
             arg_snapshot_id,
+            limit,
         })
     }
 }
@@ -182,12 +186,16 @@ impl AsyncSource for FuseBlockSource {
                 self.arg_table_name.as_str(),
             )
             .await?;
-
         let tbl = FuseTable::try_from_table(tbl.as_ref())?;
         Ok(Some(
-            FuseBlock::new(self.ctx.clone(), tbl, self.arg_snapshot_id.clone())
-                .get_blocks()
-                .await?,
+            FuseBlock::new(
+                self.ctx.clone(),
+                tbl,
+                self.arg_snapshot_id.clone(),
+                self.limit,
+            )
+            .get_blocks()
+            .await?,
         ))
     }
 }
