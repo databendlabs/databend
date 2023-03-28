6693c2987527a19ad89f978db31952cacf08b767
everpcpc <git@everpcpc.com>
2023-03-28 22:45:58 +0800

    z

--- scripts/ci/ci-run-sqllogic-tests-management-mode.sh
+++ scripts/ci/ci-run-sqllogic-tests-management-mode.sh
@@ -19,4 +19,4 @@ fi
 echo "Run suites using argument: $RUN_DIR"
 
 echo "Starting databend-sqllogic tests"
-target/${BUILD_PROFILE}/databend-sqllogictests --handlers ${TEST_HANDLERS} ${RUN_DIR} --enable_sandbox --debug
+target/${BUILD_PROFILE}/databend-sqllogictests --handlers ${TEST_HANDLERS} ${RUN_DIR} --enable_sandbox
