2d3bb518de20447bcbafe993ea15ae45d2d3a39d
sundy-li <543950155@qq.com>
2023-03-28 18:30:32 +0800

    fix(query): fix range/generate_series schema

--- src/query/service/src/table_functions/srf/mod.rs
+++ src/query/service/src/table_functions/srf/mod.rs
@@ -12,6 +12,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-mod generate_series;
+mod range;
 
-pub use generate_series::GenerateSeriesTable;
+pub use range::RangeTable;
