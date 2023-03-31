892c1cfa83ca182d0da6ba925547fd1454720e19
taichong <tai_chong@foxmail.com>
2023-03-31 09:20:06 +0800

    ProjectSet precise_cardinality set None

--- src/query/sql/src/planner/optimizer/rule/rewrite/rule_fold_count_aggregate.rs
+++ src/query/sql/src/planner/optimizer/rule/rewrite/rule_fold_count_aggregate.rs
@@ -67,9 +67,6 @@ impl Rule for RuleFoldCountAggregate {
     }
 
     fn apply(&self, s_expr: &SExpr, state: &mut TransformResult) -> Result<()> {
-        if contaions_project_set(s_expr) {
-            return Ok(());
-        }
         let agg: Aggregate = s_expr.plan().clone().try_into()?;
 
         if agg.mode == AggregateMode::Final || agg.mode == AggregateMode::Partial {
