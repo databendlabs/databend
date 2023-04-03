2c8190a5bcd5e51e30ef6e2aeac7114398d017eb
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-03 12:53:02 -0400

    spelling: announcing
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/query/storages/parquet/src/pruning.rs
+++ src/query/storages/parquet/src/pruning.rs
@@ -550,7 +550,7 @@ mod tests {
     fn unzip_option<T: NativeType>(
         array: &[Option<T>],
     ) -> common_arrow::parquet::error::Result<(Vec<u8>, Vec<u8>)> {
-        // leave the first 4 bytes anouncing the length of the def level
+        // leave the first 4 bytes announcing the length of the def level
         // this will be overwritten at the end, once the length is known.
         // This is unknown at this point because of the uleb128 encoding,
         // whose length is variable.
