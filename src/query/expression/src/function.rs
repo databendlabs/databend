d58020a7773d747d665808ffe00a56c6fe058da5
sundyli <543950155@qq.com>
2023-04-05 09:14:33 -0700

    feat(query): common eval scalar  Elimination (#10927)
    * feat(query): cse
    
    * feat(query): cse
    
    * feat(query): cse
    
    * feat(query): add cse tests
    
    * feat(query): update function display
    
    * feat(query): support hash for expr
    
    * feat(query): update traits
    
    * feat(query): update traits
    
    * feat(query): update traits
    
    * feat(query): update progress
    
    * feat(query): add comments

--- src/query/expression/src/function.rs
+++ src/query/expression/src/function.rs
@@ -102,7 +102,7 @@ pub struct EvalContext<'a> {
 
 /// `FunctionID` is a unique identifier for a function in the registry. It's used to
 /// construct the exactly same function in the remote execution nodes.
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Hash, Eq, Serialize, Deserialize)]
 pub enum FunctionID {
     Builtin {
         name: String,
@@ -403,6 +403,13 @@ impl FunctionID {
         match self {
             FunctionID::Builtin { name, .. } => name,
             FunctionID::Factory { name, .. } => name,
+        }
+    }
+
+    pub fn params(&self) -> &[usize] {
+        match self {
+            FunctionID::Builtin { .. } => &[],
+            FunctionID::Factory { params, .. } => params.as_slice(),
         }
     }
 }
