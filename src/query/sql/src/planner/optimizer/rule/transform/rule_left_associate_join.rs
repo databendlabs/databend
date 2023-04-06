7280b2e3906e97bf205368b56fdaff34fee4b270
xudong.w <wxd963996380@gmail.com>
2023-04-06 17:32:36 +0800

    fix: non-equi condition is mistaken as equi condition (#10956)

--- src/query/sql/src/planner/optimizer/rule/transform/rule_left_associate_join.rs
+++ src/query/sql/src/planner/optimizer/rule/transform/rule_left_associate_join.rs
@@ -146,9 +146,13 @@ impl Rule for RuleLeftAssociateJoin {
                 JoinPredicate::Right(pred) => {
                     join_4_preds.push(pred.clone());
                 }
-                JoinPredicate::Both { left, right, .. } => {
-                    join_3.left_conditions.push(left.clone());
-                    join_3.right_conditions.push(right.clone());
+                JoinPredicate::Both { left, right, equal } => {
+                    if !equal {
+                        join_3.non_equi_conditions.push(predicate.clone());
+                    } else {
+                        join_3.left_conditions.push(left.clone());
+                        join_3.right_conditions.push(right.clone());
+                    }
                 }
                 JoinPredicate::Other(pred) => {
                     join_3.non_equi_conditions.push(pred.clone());
@@ -168,9 +172,13 @@ impl Rule for RuleLeftAssociateJoin {
                     // TODO(leiysky): push down the predicate
                     join_4.non_equi_conditions.push(predicate.clone());
                 }
-                JoinPredicate::Both { left, right, .. } => {
-                    join_4.left_conditions.push(left.clone());
-                    join_4.right_conditions.push(right.clone());
+                JoinPredicate::Both { left, right, equal } => {
+                    if equal {
+                        join_4.left_conditions.push(left.clone());
+                        join_4.right_conditions.push(right.clone());
+                    } else {
+                        join_4.non_equi_conditions.push(predicate.clone());
+                    }
                 }
             }
         }
