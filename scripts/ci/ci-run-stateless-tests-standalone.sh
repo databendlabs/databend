5768e52ac49a8e1bad497448c692c3d357c0d747
RinChanNOWWW <hzy427@gmail.com>
2023-04-06 22:43:28 +0800

    Fix tests scripts.

--- scripts/ci/ci-run-stateless-tests-standalone.sh
+++ scripts/ci/ci-run-stateless-tests-standalone.sh
@@ -11,4 +11,4 @@ SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 cd "$SCRIPT_PATH/../../tests" || exit
 
 echo "Starting databend-test"
-./databend-test --mode 'standalone' --run-dir 0_stateless --skip '18_0010_q10' --skip '18_0035_q35' --skip '18_0069_q69'
+./databend-test --mode 'standalone' --run-dir 0_stateless --skip '18_0010_q10' + '18_0035_q35' + '18_0069_q69'
