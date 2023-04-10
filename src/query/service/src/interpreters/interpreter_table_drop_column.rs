4c8b83e3b0dc2d086624af883835b788dfa9f450
taichong <tai_chong@foxmail.com>
2023-04-10 14:03:51 +0800

    fix(query): drop table unknown table should return error

--- src/query/service/src/interpreters/interpreter_table_drop_column.rs
+++ src/query/service/src/interpreters/interpreter_table_drop_column.rs
@@ -49,44 +49,41 @@ impl Interpreter for DropTableColumnInterpreter {
         let catalog_name = self.plan.catalog.as_str();
         let db_name = self.plan.database.as_str();
         let tbl_name = self.plan.table.as_str();
-        let tbl = self
+        let table = self
             .ctx
             .get_catalog(catalog_name)?
             .get_table(self.ctx.get_tenant().as_str(), db_name, tbl_name)
-            .await
-            .ok();
+            .await?;
 
-        if let Some(table) = &tbl {
-            let table_info = table.get_table_info();
-            if table_info.engine() == VIEW_ENGINE {
-                return Err(ErrorCode::TableEngineNotSupported(format!(
-                    "{}.{} engine is VIEW that doesn't support alter",
-                    &self.plan.database, &self.plan.table
-                )));
-            }
-            if table_info.db_type != DatabaseType::NormalDB {
-                return Err(ErrorCode::TableEngineNotSupported(format!(
-                    "{}.{} doesn't support alter",
-                    &self.plan.database, &self.plan.table
-                )));
-            }
+        let table_info = table.get_table_info();
+        if table_info.engine() == VIEW_ENGINE {
+            return Err(ErrorCode::TableEngineNotSupported(format!(
+                "{}.{} engine is VIEW that doesn't support alter",
+                &self.plan.database, &self.plan.table
+            )));
+        }
+        if table_info.db_type != DatabaseType::NormalDB {
+            return Err(ErrorCode::TableEngineNotSupported(format!(
+                "{}.{} doesn't support alter",
+                &self.plan.database, &self.plan.table
+            )));
+        }
 
-            let catalog = self.ctx.get_catalog(catalog_name)?;
-            let mut new_table_meta = table.get_table_info().meta.clone();
-            new_table_meta.drop_column(&self.plan.column)?;
+        let catalog = self.ctx.get_catalog(catalog_name)?;
+        let mut new_table_meta = table.get_table_info().meta.clone();
+        new_table_meta.drop_column(&self.plan.column)?;
 
-            let table_id = table_info.ident.table_id;
-            let table_version = table_info.ident.seq;
+        let table_id = table_info.ident.table_id;
+        let table_version = table_info.ident.seq;
 
-            let req = UpdateTableMetaReq {
-                table_id,
-                seq: MatchSeq::Exact(table_version),
-                new_table_meta,
-                copied_files: None,
-            };
-
-            catalog.update_table_meta(table_info, req).await?;
+        let req = UpdateTableMetaReq {
+            table_id,
+            seq: MatchSeq::Exact(table_version),
+            new_table_meta,
+            copied_files: None,
         };
+
+        catalog.update_table_meta(table_info, req).await?;
 
         Ok(PipelineBuildResult::create())
     }
