c5cb4970c6ef153d7d9bf9dd7368f33348011f0c
RinChanNOW <rin_chan_now@outlook.com>
2023-04-11 21:47:38 +0800

    feat: support nullable order by column in range frame. (#11032)

--- src/query/service/src/pipelines/pipeline_builder.rs
+++ src/query/service/src/pipelines/pipeline_builder.rs
@@ -771,6 +771,7 @@ impl PipelineBuilder {
                     offset,
                     asc: o.asc,
                     nulls_first: o.nulls_first,
+                    is_nullable: input_schema.field(offset).is_nullable(), // Used for check null frame.
                 })
             })
             .collect::<Result<Vec<_>>>()?;
@@ -784,6 +785,7 @@ impl PipelineBuilder {
                     offset: *offset,
                     asc: true,
                     nulls_first: true,
+                    is_nullable: input_schema.field(*offset).is_nullable(),  // This information is not needed here.
                 })
             }
 
@@ -807,13 +809,15 @@ impl PipelineBuilder {
                     func.clone(),
                     partition_by.clone(),
                     order_by.clone(),
-                    window.window_frame.units.clone(),
                     (start_bound, end_bound),
                 )?) as Box<dyn Processor>
             } else {
                 if order_by.len() == 1 {
                     // If the length of order_by is 1, there may be a RANGE frame.
-                    let data_type = input_schema.field(order_by[0].offset).data_type();
+                    let data_type = input_schema
+                        .field(order_by[0].offset)
+                        .data_type()
+                        .remove_nullable();
                     with_number_mapped_type!(|NUM_TYPE| match data_type {
                         DataType::Number(NumberDataType::NUM_TYPE) => {
                             let start_bound =
@@ -826,7 +830,6 @@ impl PipelineBuilder {
                                     func.clone(),
                                     partition_by.clone(),
                                     order_by.clone(),
-                                    window.window_frame.units.clone(),
                                     (start_bound, end_bound),
                                 )?,
                             )
@@ -846,7 +849,6 @@ impl PipelineBuilder {
                     func.clone(),
                     partition_by.clone(),
                     order_by.clone(),
-                    window.window_frame.units.clone(),
                     (start_bound, end_bound),
                 )?) as Box<dyn Processor>
             };
@@ -870,6 +872,7 @@ impl PipelineBuilder {
                     offset,
                     asc: desc.asc,
                     nulls_first: desc.nulls_first,
+                    is_nullable: input_schema.field(offset).is_nullable(),  // This information is not needed here.
                 })
             })
             .collect::<Result<Vec<_>>>()?;
