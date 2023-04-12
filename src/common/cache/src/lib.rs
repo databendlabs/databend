8fda7ea0b2d69e7b3627e6647ea7acc1ab94c6eb
sky <geekvpt@gmail.com>
2023-04-12 18:31:29 +0800

    delete useless modification

--- src/common/cache/src/lib.rs
+++ src/common/cache/src/lib.rs
@@ -26,7 +26,6 @@ pub use cache::Cache;
 pub use meter::bytes_meter::BytesMeter;
 pub use meter::count_meter::Count;
 pub use meter::count_meter::CountableMeter;
-pub use meter::count_meter::CountableMeterWithMeasure;
 pub use meter::file_meter::FileSize;
 #[cfg(feature = "heapsize")]
 #[cfg(not(target_os = "macos"))]
