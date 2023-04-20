bd971b4fc4b0711930b6244396b9ffc36a4faf89
zhyass <mytesla@live.com>
2023-04-20 13:19:37 +0800

    remove excess double slashes

--- src/query/storages/common/index/src/filters/xor8/mod.rs
+++ src/query/storages/common/index/src/filters/xor8/mod.rs
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 
 mod block_filter;
 mod block_filter_versions;
