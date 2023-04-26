217c250ca0feff4494a821cb797cece3a63795a9
everpcpc <everpcpc@users.noreply.github.com>
2023-04-26 17:06:23 +0800

    fix(cli): append space for appending mutiline query (#97)
    * fix(cli): append space for appending mutiline query
    
    * fix(cli): use full host for prompt

--- cli/src/session.rs
+++ cli/src/session.rs
@@ -80,14 +80,8 @@ impl Session {
             "> ".to_owned()
         } else {
             let info = self.conn.info();
-            let tokens = info.host.split('.').collect::<Vec<_>>();
-            let host = if tokens.len() >= 2 {
-                tokens[..2].join(".")
-            } else {
-                info.host
-            };
             let mut prompt = self.settings.prompt.clone();
-            prompt = prompt.replace("{host}", &host);
+            prompt = prompt.replace("{host}", &info.host);
             prompt = prompt.replace("{user}", &info.user);
             prompt = prompt.replace("{port}", &info.port.to_string());
             format!("{} ", prompt.trim_end())
@@ -185,6 +179,7 @@ impl Session {
             return vec![line.to_owned()];
         }
 
+        self.query.push(' ');
         self.query.push_str(line);
 
         let mut queries = Vec::new();
