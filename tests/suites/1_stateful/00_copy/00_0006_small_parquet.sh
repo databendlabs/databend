627df301bdecdb65a3ac9b1fb455dd2fc1af7479
Yang Xiufeng <yangxiufeng.c@gmail.com>
2023-04-27 15:55:32 +0800

    fix copy small parquet on s3.

--- tests/suites/1_stateful/00_copy/00_0006_small_parquet.sh
+++ tests/suites/1_stateful/00_copy/00_0006_small_parquet.sh
@@ -3,28 +3,29 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../../../shell_env.sh
 
+DATADIR="fs://${CURDIR}/../../../data/"
 
-DATADIR_PATH="/tmp/small_parquets"
-rm -rf ${DATADIR_PATH}
-mkdir ${DATADIR_PATH}
-DATADIR="fs://$DATADIR_PATH/"
+echo "drop stage if exists data_fs;" | $MYSQL_CLIENT_CONNECT
+echo "drop stage if exists data_s3;" | $MYSQL_CLIENT_CONNECT
 
-cp "$CURDIR"/../../../data/tuple.parquet ${DATADIR_PATH}/
-
-echo "drop stage if exists small_parquets;" | $MYSQL_CLIENT_CONNECT
-echo "drop table if exists small_parquets;" | $MYSQL_CLIENT_CONNECT
-
-echo "create stage small_parquets url = '${DATADIR}' FILE_FORMAT = (type = PARQUET);"  | $MYSQL_CLIENT_CONNECT
+echo "create stage data_s3 url='s3://testbucket/admin/data/' connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='http://127.0.0.1:9900') FILE_FORMAT = (type = PARQUET);" | $MYSQL_CLIENT_CONNECT
+echo "create stage data_fs url = '${DATADIR}' FILE_FORMAT = (type = PARQUET);"  | $MYSQL_CLIENT_CONNECT
 echo "create table small_parquets (id int, t tuple(a int, b string));" | $MYSQL_CLIENT_CONNECT
 
-echo "--- copy"
-echo "set parquet_read_whole_file_threshold=4096; copy into small_parquets from @small_parquets;" | $MYSQL_CLIENT_CONNECT
-echo "select * from small_parquets" | $MYSQL_CLIENT_CONNECT
-echo "truncate table small_parquets" | $MYSQL_CLIENT_CONNECT
+for threshold in "0" "4096"; do
+	for stage in "data_fs" "data_s3"; do
+		echo "--- copy, threshold=${threshold} stage=${stage}"
+		echo "set parquet_read_whole_file_threshold=${threshold}; copy into small_parquets from @${stage} PATTERN='tuple.parquet' force=true;" | $MYSQL_CLIENT_CONNECT
+		echo "select * from small_parquets" | $MYSQL_CLIENT_CONNECT
+		echo "truncate table small_parquets" | $MYSQL_CLIENT_CONNECT
 
-echo "--- copy from select"
-echo "set parquet_read_whole_file_threshold=4096; copy into small_parquets from (select * from @small_parquets t) force = true;" | $MYSQL_CLIENT_CONNECT
-echo "select * from small_parquets" | $MYSQL_CLIENT_CONNECT
+		echo "--- copy from select, threshold=${threshold} stage=${stage}"
+		echo "set parquet_read_whole_file_threshold=${threshold}; copy into small_parquets from (select * from @${stage} t) PATTERN='tuple.parquet' force = true;" | $MYSQL_CLIENT_CONNECT
+		echo "select * from small_parquets" | $MYSQL_CLIENT_CONNECT
+		echo "truncate table small_parquets" | $MYSQL_CLIENT_CONNECT
+	done
+done
 
-echo "drop stage small_parquets;"  | $MYSQL_CLIENT_CONNECT
+echo "drop stage if exists data_fs;" | $MYSQL_CLIENT_CONNECT
+echo "drop stage if exists data_s3;" | $MYSQL_CLIENT_CONNECT
 echo "drop table small_parquets;" | $MYSQL_CLIENT_CONNECT
