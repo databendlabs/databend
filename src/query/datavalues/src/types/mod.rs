5229bc1e1b0bb4cafb19fac3189a35643cbc0fcc
tison <wander4096@gmail.com>
2023-04-30 10:02:37 +0800

    chore: apply license header for all files
    Signed-off-by: tison <wander4096@gmail.com>

--- src/query/datavalues/src/types/mod.rs
+++ src/query/datavalues/src/types/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 Datafuse Labs.
+// Copyright 2021 Datafuse Labs
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.pub use data_type::*;
+// limitations under the License.
 
 pub mod data_type;
 pub mod type_array;
