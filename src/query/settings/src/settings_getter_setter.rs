41efac496bd5a4366957989f21ca71fc8e5af4e9
souravghosh01 <samghosh878@gmail.com>
2023-05-08 22:36:36 +0530

    validate/max_storage_io_requests

--- src/query/settings/src/settings_getter_setter.rs
+++ src/query/settings/src/settings_getter_setter.rs
@@ -124,7 +124,13 @@ impl Settings {
     }
 
     pub fn set_max_storage_io_requests(&self, val: u64) -> Result<()> {
-        self.try_set_u64("max_storage_io_requests", val)
+        if val > 0 {
+            self.try_set_u64("max_storage_io_requests", val)?;
+            Ok(())
+        } else {
+            Err(Error::new(ErrorKind::InvalidInput, "Value must be greater than 0"))
+        }
+    }
     }
 
     pub fn get_storage_io_min_bytes_for_seek(&self) -> Result<u64> {
