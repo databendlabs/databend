cb85de601a0d09e9880adb00375f0b7ff4c033ef
sundy-li <543950155@qq.com>
2023-05-10 23:27:09 +0800

    chore: z3-features2

--- src/query/sql/src/planner/optimizer/property/mod.rs
+++ src/query/sql/src/planner/optimizer/property/mod.rs
@@ -14,7 +14,10 @@
 
 mod builder;
 mod column_stat;
+
+#[cfg(feature = "z3-prove")]
 mod constraint;
+
 mod datum;
 mod enforcer;
 mod histogram;
@@ -26,6 +29,7 @@ pub use builder::RelExpr;
 pub use column_stat::ColumnStat;
 pub use column_stat::ColumnStatSet;
 pub use column_stat::NewStatistic;
+#[cfg(feature = "z3-prove")]
 pub use constraint::ConstraintSet;
 pub use datum::Datum;
 pub use enforcer::require_property;
