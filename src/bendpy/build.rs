55d16d1934fe031e7c91b953db13046e53821e13
sundy-li <543950155@qq.com>
2023-05-11 11:06:44 +0800

    chore: fix build

--- src/bendpy/build.rs
+++ src/bendpy/build.rs
@@ -12,28 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use common_expression::DataBlock;
-use pyo3::prelude::*;
-
-#[pyclass]
-
-pub struct Block(pub DataBlock);
-
-#[pymethods]
-impl Block {
-    fn __repr__(&self, _py: Python) -> PyResult<String> {
-        Ok(self.0.to_string())
-    }
-
-    fn show(&self) {
-        println!("{}", self.0.to_string());
-    }
-
-    fn num_rows(&self) -> usize {
-        self.0.num_rows()
-    }
-
-    fn num_columns(&self) -> usize {
-        self.0.num_columns()
-    }
+fn main() {
+    pyo3_build_config::add_extension_module_link_args();
 }
