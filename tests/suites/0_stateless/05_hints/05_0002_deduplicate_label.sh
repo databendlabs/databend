2c9e67f22b78894cea4c756969a1f7ef3611bd59
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2023-05-30 17:32:46 +0800

    test: add

---
+++ tests/suites/0_stateless/05_hints/05_0002_deduplicate_label.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. "$CURDIR"/../../../shell_env.sh
+
+# mariadb mysql client has some bug, please use mysql official client
+# mysql --version
+# mysql  Ver 8.0.32-0ubuntu0.20.04.2 for Linux on x86_64 ((Ubuntu))
+echo "CREATE TABLE t1(a Int, b bool) Engine = Fuse;" |  $MYSQL_CLIENT_CONNECT
+
+echo "INSERT /*+ SET_VAR(duplicate_label='insert-test') */ INTO t1 (a, b) VALUES(1, false)" | $MYSQL_CLIENT_CONNECT
+echo "INSERT /*+ SET_VAR(duplicate_label='insert-test') */ INTO t1 (a, b) VALUES(1, false)" | $MYSQL_CLIENT_CONNECT
+echo "select * from t1" | $MYSQL_CLIENT_CONNECT
+
+echo "UPDATE /*+ SET_VAR(deduplicate_label='update-test') */ t SET a = 20 WHERE b = false;" | $MYSQL_CLIENT_CONNECT
+echo "UPDATE /*+ SET_VAR(deduplicate_label='update-test') */ t SET a = 30 WHERE b = false;" | $MYSQL_CLIENT_CONNECT
+echo "select * from t1" | $MYSQL_CLIENT_CONNECT
+
+echo "replace /*+ SET_VAR(deduplicate_label='replace-test') */ into t on(a,b) values(40,false);" | $MYSQL_CLIENT_CONNECT
+echo "replace /*+ SET_VAR(deduplicate_label='replace-test') */ into t on(a,b) values(50,false);" | $MYSQL_CLIENT_CONNECT
+echo "select * from t1" | $MYSQL_CLIENT_CONNECT
