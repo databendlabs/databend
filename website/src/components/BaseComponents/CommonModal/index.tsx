84aecbc47f1e610cc39c2795714bfce807415ed6
q <787025321@qq.com>
2023-05-31 20:00:30 +0800

    fix: hidden

--- website/src/components/BaseComponents/CommonModal/index.tsx
+++ website/src/components/BaseComponents/CommonModal/index.tsx
@@ -12,7 +12,10 @@ interface IProps {
 const CommonModal = ({ visible, width, children, className, onClose }: IProps) => {
   useEffect(() => {
     if (visible) {
-      document.body.style.overflow = 'hidden';
+      const id = setTimeout(()=> {
+        document.body.style.overflow = 'hidden';
+        clearTimeout(id);
+      });
     } else {
       document.body.style.overflow = 'visible';
     }
