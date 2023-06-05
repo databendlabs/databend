2c4bdb8c78208447b183bbce29d862f8c43b9eba
dantengsky <dantengsky@gmail.com>
2023-06-05 14:53:31 +0800

    add meta version compatibility test

--- src/meta/app/src/schema/table.rs
+++ src/meta/app/src/schema/table.rs
@@ -202,11 +202,11 @@ pub struct TableStatistics {
     pub index_data_bytes: u64,
 
     /// number of segments
-    /// for old version of table meta, this field is None
+    /// for meta versions before 43, this field is None
     pub number_of_segments: Option<u64>,
 
     /// number of blocks
-    /// for old version of table meta, this field is None
+    /// for meta versions before 43, this field is None
     pub number_of_blocks: Option<u64>,
 }
 
