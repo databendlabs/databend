571cc33f32f62633bd1c99890b2a4773ba269700
RinChanNOW <rin_chan_now@outlook.com>
2023-06-06 10:36:23 +0800

    feat: support distributed Top-N. (#11662)
    * Enable topn on cluster nodes.
    
    * Fix: only push down topn with pattern topn-exchange.
    
    * Fix: limit should not be None.
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/query/sql/src/planner/optimizer/distributed/distributed.rs
+++ src/query/sql/src/planner/optimizer/distributed/distributed.rs
@@ -17,21 +17,23 @@ use std::sync::Arc;
 use common_catalog::table_context::TableContext;
 use common_exception::Result;
 
-use crate::optimizer::distributed::topk::TopK;
+use super::topn::TopNPushDownOptimizer;
 use crate::optimizer::property::require_property;
 use crate::optimizer::Distribution;
 use crate::optimizer::RelExpr;
 use crate::optimizer::RequiredProperty;
 use crate::optimizer::SExpr;
 use crate::plans::Exchange;
-use crate::plans::RelOperator;
 
 pub fn optimize_distributed_query(ctx: Arc<dyn TableContext>, s_expr: &SExpr) -> Result<SExpr> {
     let required = RequiredProperty {
         distribution: Distribution::Any,
     };
-    let mut result = require_property(ctx, &required, s_expr)?;
-    result = push_down_topk_to_merge(&result, None)?;
+    let result = require_property(ctx, &required, s_expr)?;
+
+    let topn_optimizer = TopNPushDownOptimizer::create();
+    let mut result = topn_optimizer.optimize(&result)?;
+
     let rel_expr = RelExpr::with_s_expr(&result);
     let physical_prop = rel_expr.derive_physical_prop()?;
     let root_required = RequiredProperty {
@@ -43,44 +45,4 @@ pub fn optimize_distributed_query(ctx: Arc<dyn TableContext>, s_expr: &SExpr) ->
     }
 
     Ok(result)
-}
-
-// Traverse the SExpr tree to find top_k, if find, push down it to Exchange::Merge
-fn push_down_topk_to_merge(s_expr: &SExpr, mut top_k: Option<TopK>) -> Result<SExpr> {
-    if let RelOperator::Exchange(Exchange::Merge) = *s_expr.plan {
-        // A quick fix for Merge child is aggregate.
-        // Todo: consider to push down topk to the above of aggregate.
-        if let RelOperator::Aggregate(_) = *s_expr.child(0)?.plan {
-            return Ok(s_expr.clone());
-        }
-        if let Some(top_k) = top_k {
-            let mut child = s_expr.children[0].clone();
-            child = Arc::new(SExpr::create_unary(Arc::new(top_k.sort.into()), child));
-            let children = if s_expr.children.len() == 2 {
-                vec![child, s_expr.children[1].clone()]
-            } else {
-                vec![child]
-            };
-            return Ok(s_expr.replace_children(children.into_iter()));
-        }
-    }
-
-    let mut s_expr_children = vec![];
-    for child in s_expr.children.iter() {
-        top_k = None;
-        if let RelOperator::Sort(sort) = (*child.plan).clone() {
-            if sort.limit.is_some() {
-                top_k = Some(TopK { sort: sort.clone() });
-            }
-        }
-        let mut new_children = vec![];
-        for child_child in child.children.iter() {
-            new_children.push(Arc::new(push_down_topk_to_merge(
-                child_child,
-                top_k.clone(),
-            )?));
-        }
-        s_expr_children.push(Arc::new(child.replace_children(new_children)));
-    }
-    Ok(s_expr.replace_children(s_expr_children))
 }
