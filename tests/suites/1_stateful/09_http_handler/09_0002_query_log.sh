db5732c1864eb926c8d59625cc99c9534600e4e0
JackTan25 <jacktby@gmail.com>
2023-06-12 20:18:14 +0800

    clean useless output

--- tests/suites/1_stateful/09_http_handler/09_0002_query_log.sh
+++ tests/suites/1_stateful/09_http_handler/09_0002_query_log.sh
@@ -5,7 +5,8 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 echo "drop stage if exists s1;" | $MYSQL_CLIENT_CONNECT
 echo "CREATE STAGE s1 FILE_FORMAT = (TYPE = CSV);" | $MYSQL_CLIENT_CONNECT
+echo "create table products (id int, name string, description string);" | $MYSQL_CLIENT_CONNECT
 
-curl -u root: -H "stage_name:s1" -F "upload=@${CURDIR}/../../../data/ttt.csv" -XPUT "http://localhost:8000/v1/upload_to_stage" -u root: | jq ".data"
+curl -s -u root: -H "stage_name:s1" -F "upload=@${CURDIR}/../../../data/ttt.csv" -XPUT "http://localhost:8000/v1/upload_to_stage" -u root: | jq ".data"
 curl -s -u root: -XPOST "http://localhost:8000/v1/query" --header 'Content-Type: application/json' -d '{"sql": "insert into products (id, name, description) VALUES(?,?,?)", "stage_attachment": {"location": "@s1/ttt.csv", "copy_options": {"purge": "false"}}}' -u root: | jq ".data"
 echo "select query_kind from system.query_log where query_text =  'INSERT INTO products (id, name, description) VALUES (?,?,?)' limit 1;" | $MYSQL_CLIENT_CONNECT
