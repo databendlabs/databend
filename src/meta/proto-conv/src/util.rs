2ad34051ca9a8f4d75f3cc94b70c75a26d5cdbb8
zhihanz <zzh243402448@gmail.com>
2023-06-14 23:31:01 +0800

    fix comment

--- src/meta/proto-conv/src/util.rs
+++ src/meta/proto-conv/src/util.rs
@@ -71,8 +71,6 @@ const META_CHANGE_LOG: &[(u64, &str)] = &[
     (39, "2023-05-22: Add: data_mask.proto", ),
     (40, "2023-05-26: Add: TableMeta add column_mask_policy field", ),
     (41, "2023-05-29: Add: virtual_column.proto", ),
-    (42,"2023-06-03: Add allow_anonymous in S3 Config",),
-    (43,"2023-06-05: Add fields `number_of_segments` and `number_of_blocks` to TableStatistics",),
     (42, "2023-06-03: Add allow_anonymous in S3 Config", ),
     (43, "2023-06-05: Add fields `number_of_segments` and `number_of_blocks` to TableStatistics", ),
     (44, "2023-06-07: Add: metadata.proto/ComputedExpr", ),
