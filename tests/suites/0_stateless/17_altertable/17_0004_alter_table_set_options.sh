941b8c8357dbc21b4bb55c1a6283373b755acce5
JackTan25 <jacktby@gmail.com>
2023-06-14 12:24:00 +0800

    add more tests

---
+++ tests/suites/0_stateless/17_altertable/17_0004_alter_table_set_options.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. "$CURDIR"/../../../shell_env.sh
+
+echo "create table t(a int)" | $MYSQL_CLIENT_CONNECT
+echo "insert into t values(1)" | $MYSQL_CLIENT_CONNECT
+
+# get snapshot location
+SNAPSHOT_LOCATION=$(echo "select _snapshot_name from t;" | $MYSQL_CLIENT_CONNECT)
+
+echo "create table t2(a int)" | $MYSQL_CLIENT_CONNECT
+echo "alter table t2 set options(snapshot_location = '$SNAPSHOT_LOCATION',block_per_segment = 500)" | $MYSQL_CLIENT_CONNECT
+echo "select * from t2;" | $MYSQL_CLIENT_CONNECT
