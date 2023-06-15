08e386347838f1321e499b597be63069422cafb2
xudong963 <wxd963996380@gmail.com>
2023-06-15 11:41:08 +0800

    feat: support subquery in update statement

--- src/query/storages/fuse/src/operations/mutation/mutation_source.rs
+++ src/query/storages/fuse/src/operations/mutation/mutation_source.rs
@@ -261,6 +261,8 @@ impl Processor for MutationSource {
                                 }
                             }
                             MutationAction::Update => {
+                                // Pop the row_id column
+                                let mut data_block = data_block.pop_columns(1)?;
                                 if self.remain_reader.is_none() {
                                     data_block.add_column(BlockEntry::new(
                                         DataType::Boolean,
