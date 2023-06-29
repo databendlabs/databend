87922f67d71bc4e0ccb5702941bdd7f2883c23b4
Dousir9 <736191200@qq.com>
2023-06-29 21:48:12 +0800

    improve selectivity

--- src/query/sql/src/planner/plans/scan.rs
+++ src/query/sql/src/planner/plans/scan.rs
@@ -223,7 +223,7 @@ impl Operator for Scan {
                 let mut selectivity = MAX_SELECTIVITY;
                 for pred in prewhere.predicates.iter() {
                     // Compute selectivity for each conjunction
-                    selectivity *= sb.compute_selectivity(pred, true)?;
+                    selectivity = selectivity.min(sb.compute_selectivity(pred, true)?);
                 }
                 // Update other columns's statistic according to selectivity.
                 sb.update_other_statistic_by_selectivity(selectivity);
