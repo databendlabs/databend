84e807444b7b7ce9d7b01a6fc12a346ea0a5107f
baishen <baishen2009@gmail.com>
2023-06-30 17:19:34 +0800

    chore: show user add configured users (#11929)

--- src/query/sql/src/planner/binder/binder.rs
+++ src/query/sql/src/planner/binder/binder.rs
@@ -282,7 +282,7 @@ impl<'a> Binder {
                 if_exists: *if_exists,
                 user: user.clone(),
             })),
-            Statement::ShowUsers => self.bind_rewrite_to_query(bind_context, "SELECT name, hostname, auth_type, auth_string FROM system.users ORDER BY name", RewriteKind::ShowUsers).await?,
+            Statement::ShowUsers => self.bind_rewrite_to_query(bind_context, "SELECT name, hostname, auth_type, auth_string, is_configured FROM system.users ORDER BY name", RewriteKind::ShowUsers).await?,
             Statement::AlterUser(stmt) => self.bind_alter_user(stmt).await?,
 
             // Roles
