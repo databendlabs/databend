80700b71f16bf9179c32abdf765706566e79cbec
sky <3374614481@qq.com>
2023-07-04 09:25:52 +0800

    fix table lock

--- src/query/service/src/interpreters/interpreter_delete.rs
+++ src/query/service/src/interpreters/interpreter_delete.rs
@@ -16,6 +16,7 @@ use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::sync::Arc;
 
+use common_base::runtime::GlobalIORuntime;
 use common_catalog::plan::Partitions;
 use common_catalog::table::DeletionFilters;
 use common_exception::ErrorCode;
@@ -53,6 +54,7 @@ use common_storages_factory::Table;
 use common_storages_fuse::FuseTable;
 use futures_util::TryStreamExt;
 use storages_common_table_meta::meta::TableSnapshot;
+use table_lock::TableLockHandlerWrapper;
 
 use crate::interpreters::Interpreter;
 use crate::interpreters::SelectInterpreter;
@@ -94,6 +96,17 @@ impl Interpreter for DeleteInterpreter {
         let catalog_name = self.plan.catalog_name.as_str();
         let db_name = self.plan.database_name.as_str();
         let tbl_name = self.plan.table_name.as_str();
+
+        let tbl = self.ctx.get_table(catalog_name, db_name, tbl_name).await?;
+        let table_info = tbl.get_table_info().clone();
+
+        // Add table lock heartbeat.
+        let handler = TableLockHandlerWrapper::instance(self.ctx.clone());
+        let mut heartbeat = handler
+            .try_lock(self.ctx.clone(), table_info.clone())
+            .await?;
+
+        // refresh table.
         let tbl = self
             .ctx
             .get_catalog(catalog_name)?
@@ -202,6 +215,7 @@ impl Interpreter for DeleteInterpreter {
                     tbl.name(),
                     tbl.get_table_info().engine(),
                 )))?;
+
         let mut build_res = PipelineBuildResult::create();
         let query_row_id_col = !self.plan.subquery_desc.is_empty();
         if let Some((partitions, snapshot)) = fuse_table
@@ -231,6 +245,19 @@ impl Interpreter for DeleteInterpreter {
                 build_res = build_local_pipeline(&self.ctx, &physical_plan, false).await?
             }
         }
+        if build_res.main_pipeline.is_empty() {
+            heartbeat.shutdown().await?;
+        } else {
+            build_res.main_pipeline.set_on_finished(move |may_error| {
+                // shutdown table lock heartbeat.
+                GlobalIORuntime::instance().block_on(async move { heartbeat.shutdown().await })?;
+                match may_error {
+                    None => Ok(()),
+                    Some(error_code) => Err(error_code.clone()),
+                }
+            });
+        }
+
         Ok(build_res)
     }
 }
