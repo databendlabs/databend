60179bf29ca467555525d4bfe1c7306051679e3e
sky <3374614481@qq.com>
2023-07-05 15:06:43 +0800

    fix test

--- tests/suites/1_stateful/02_query/02_0004_attach_table.sh
+++ tests/suites/1_stateful/02_query/02_0004_attach_table.sh
@@ -21,7 +21,7 @@ done
 
 storage_prefix=$(mysql -uroot -h127.0.0.1 -P3307  -e "set global hide_options_in_show_create_table=0;show create table table_from" | grep -i snapshot_location | awk -F'SNAPSHOT_LOCATION='"'"'|_ss' '{print $2}')
 
-echo "attach table table_to 's3://testbucket/admin/data/'$storage_prefix connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $MYSQL_CLIENT_CONNECT
+echo "attach table table_to 's3://testbucket/admin/data/$storage_prefix' connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $MYSQL_CLIENT_CONNECT
 
 
 ## Select table
