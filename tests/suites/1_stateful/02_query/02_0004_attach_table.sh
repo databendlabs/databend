c47bfd3fab417f02636df3c61be884ff40f365e6
sky <3374614481@qq.com>
2023-07-05 14:21:53 +0800

    fix test

--- tests/suites/1_stateful/02_query/02_0004_attach_table.sh
+++ tests/suites/1_stateful/02_query/02_0004_attach_table.sh
@@ -9,7 +9,10 @@ echo "drop table if exists table_to;" | $MYSQL_CLIENT_CONNECT
 ## Create table
 echo "create table table_from(a int) select * from numbers(3) 's3://testbucket/admin/data/' connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $MYSQL_CLIENT_CONNECT
 
-echo "attach table_to 's3://testbucket/admin/data/' connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $MYSQL_CLIENT_CONNECT
+storage_prefix=$(mysql -uroot -h127.0.0.1 -P3307  -e "set global hide_options_in_show_create_table=0;show create table table_from" | grep -i snapshot_location | awk -F'SNAPSHOT_LOCATION='"'"'|_ss' '{print $2}')
+
+echo "attach table table_to 's3://testbucket/admin/data/'$storage_prefix connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $MYSQL_CLIENT_CONNECT
+
 
 ## Select table
 echo "select * from table_to;" | $MYSQL_CLIENT_CONNECT
