ffa88f99eaf3669ed0de809bf5e23b9c65da9fb1
baishen <baishen2009@gmail.com>
2023-07-10 21:31:17 +0800

    fix

--- src/query/sql/src/planner/semantic/type_check.rs
+++ src/query/sql/src/planner/semantic/type_check.rs
@@ -2681,15 +2681,10 @@ impl<'a> TypeChecker<'a> {
         column: ColumnBinding,
         paths: &mut VecDeque<(Span, Literal)>,
     ) -> Option<Result<Box<(ScalarExpr, DataType)>>> {
-        let table_index = match self.metadata.read().get_table_index(
+        let table_index = self.metadata.read().get_table_index(
             column.database_name.as_deref(),
             column.table_name.as_deref().unwrap(),
-        ) {
-            Some(table_index) => table_index,
-            None => {
-                return None;
-            }
-        };
+        )?;
 
         if !self
             .metadata
