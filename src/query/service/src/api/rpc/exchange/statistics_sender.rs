4d95f05229add251fb172b1c320c81eeffbca480
JackTan25 <jacktby@gmail.com>
2023-07-12 23:52:16 +0800

    fix warn

--- src/query/service/src/api/rpc/exchange/statistics_sender.rs
+++ src/query/service/src/api/rpc/exchange/statistics_sender.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use std::time::Duration;
 
