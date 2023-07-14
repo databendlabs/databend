68d4032006ac42e55510958fc351621aca5442ce
q <787025321@qq.com>
2023-07-14 23:34:08 +0800

    feat: add load-img

---
+++ website/src/components/BaseComponents/ImgLazyLoad/index.tsx
@@ -0,0 +1,31 @@
+// Copyright 2023 DatabendLabs.
+import React, { FC, ReactElement, useState } from 'react';
+// refs: https://databendcloud.github.io/databend-logos/
+import { LightDatabendSingleSvg } from 'databend-logos';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import clsx from 'clsx';
+interface IProps {
+  src: string;
+  width?: number;
+  className?: string;
+}
+const LoadLazyImg: FC<IProps> = ({src, width, className}): ReactElement=> {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
+  return (
+    <>
+    {!isLoaded && <LightDatabendSingleSvg width={width}></LightDatabendSingleSvg>}
+    <LazyLoadImage
+      onLoad={()=> handleImageLoad()}
+      className={clsx('g-w100', className, isLoaded ? 'g-db' :'g-dn')}
+      src={src} />
+    </>
+    
+  );
+};
+LoadLazyImg.defaultProps = {
+  width: 200
+}
+export default LoadLazyImg;
