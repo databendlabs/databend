9275e402e230b62bd08aeda8fd931d888b31980f
everpcpc <everpcpc@users.noreply.github.com>
2023-07-15 21:30:41 +0800

    ci: add license check (#167)

--- bindings/python/tests/steps/binding.py
+++ bindings/python/tests/steps/binding.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Datafuse Labs.
+# Copyright 2021 Datafuse Labs
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import os
 
 from behave import given, when, then
