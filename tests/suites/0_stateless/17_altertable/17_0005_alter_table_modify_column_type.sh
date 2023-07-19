7ac96004b3906f6295a30136201f3cf38645490a
lichuang <lichuang1982@gmail.com>
2023-07-19 19:48:38 +0800

    fix test fail

--- tests/suites/0_stateless/17_altertable/17_0005_alter_table_modify_column_type.sh
+++ tests/suites/0_stateless/17_altertable/17_0005_alter_table_modify_column_type.sh
@@ -6,20 +6,17 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 echo "DROP DATABASE IF EXISTS test_modify_column_type" | $MYSQL_CLIENT_CONNECT
 echo "CREATE DATABASE test_modify_column_type" | $MYSQL_CLIENT_CONNECT
 
-echo "CREATE table test_modify_column_type.a(a String)"  | $MYSQL_CLIENT_CONNECT
-echo "INSERT INTO test_modify_column_type.a values('1')"  | $MYSQL_CLIENT_CONNECT
-echo "SELECT * from test_modify_column_type.a"  | $MYSQL_CLIENT_CONNECT
+echo "CREATE table test_modify_column_type.a(a String, b int)"  | $MYSQL_CLIENT_CONNECT
+echo "INSERT INTO test_modify_column_type.a values('1', 2)"  | $MYSQL_CLIENT_CONNECT
+echo "SELECT a,b from test_modify_column_type.a"  | $MYSQL_CLIENT_CONNECT
 echo "DESC test_modify_column_type.a"  | $MYSQL_CLIENT_CONNECT
 
 echo "alter table test_modify_column_type.a modify column a set data type float"  | $MYSQL_CLIENT_CONNECT
-echo "SELECT * from test_modify_column_type.a"  | $MYSQL_CLIENT_CONNECT
+echo "SELECT a,b from test_modify_column_type.a"  | $MYSQL_CLIENT_CONNECT
 echo "DESC test_modify_column_type.a"  | $MYSQL_CLIENT_CONNECT
 
 echo "CREATE table test_modify_column_type.b(a String)"  | $MYSQL_CLIENT_CONNECT
 echo "INSERT INTO test_modify_column_type.b values('a')"  | $MYSQL_CLIENT_CONNECT
 echo "alter table test_modify_column_type.b modify column a set data type float"  | $MYSQL_CLIENT_CONNECT
-
-echo "create table test_modify_column_type.t1(a string, b string as (concat(a, '-')) stored)"   | $MYSQL_CLIENT_CONNECT
-echo "alter table test_modify_column_type.t1 modify column a set data type float"  | $MYSQL_CLIENT_CONNECT
 
 echo "DROP DATABASE IF EXISTS test_modify_column_type" | $MYSQL_CLIENT_CONNECT
