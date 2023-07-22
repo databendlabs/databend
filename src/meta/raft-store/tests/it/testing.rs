a59fc4c18de2c71018bdba2dadc0b8a147ae4897
Andy Lok <andylokandy@hotmail.com>
2023-07-23 05:40:17 +0800

    fix

--- src/meta/raft-store/tests/it/testing.rs
+++ src/meta/raft-store/tests/it/testing.rs
@@ -12,10 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::sync::Arc;
+use std::sync::Mutex;
+
 use common_base::base::GlobalSequence;
 use common_meta_raft_store::config::RaftConfig;
 use common_meta_sled_store::get_sled_db;
 use common_meta_sled_store::sled;
+use common_tracing::init_logging;
+use common_tracing::Config;
+use log::info;
+use once_cell::sync::Lazy;
 
 pub struct RaftTestContext {
     pub raft_config: RaftConfig,
@@ -34,4 +41,30 @@ pub fn new_raft_test_context() -> RaftTestContext {
         raft_config: config,
         db: get_sled_db(),
     }
+}
+
+#[allow(dyn_drop)]
+struct MetaLogGuard {
+    #[allow(dead_code)]
+    log_guards: Vec<Box<dyn Drop + Send + Sync + 'static>>,
+}
+
+static META_LOG_GUARD: Lazy<Arc<Mutex<Option<MetaLogGuard>>>> =
+    Lazy::new(|| Arc::new(Mutex::new(None)));
+
+/// Initialize unit test tracing for metasrv
+#[ctor::ctor]
+fn init_meta_ut_tracing() {
+    let guards = init_logging("meta_unittests", &Config::new_testing());
+    info!("start");
+
+    *META_LOG_GUARD.as_ref().lock().unwrap() = Some(MetaLogGuard { log_guards: guards });
+
+    let t = tempfile::tempdir().expect("create temp dir to sled db");
+    common_meta_sled_store::init_temp_sled_db(t);
+}
+
+#[ctor::dtor]
+fn cleanup_meta_ut_tracing() {
+    META_LOG_GUARD.as_ref().lock().unwrap().take();
 }
--- src/meta/service/tests/it/testing.rs
+++ src/meta/service/tests/it/testing.rs
@@ -12,35 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod fake_key_spaces;
-pub mod fake_state_machine_meta;
-
 use std::sync::Arc;
 use std::sync::Mutex;
 
-use common_base::base::GlobalSequence;
-use common_meta_sled_store::get_sled_db;
 use common_tracing::init_logging;
 use common_tracing::Config;
 use log::info;
 use once_cell::sync::Lazy;
-
-pub struct SledTestContext {
-    pub tree_name: String,
-    pub db: sled::Db,
-}
-
-/// Create a new context for testing sled
-pub fn new_sled_test_context() -> SledTestContext {
-    SledTestContext {
-        tree_name: format!("test-{}-", next_seq()),
-        db: get_sled_db(),
-    }
-}
-
-pub fn next_seq() -> u32 {
-    29000u32 + (GlobalSequence::next() as u32)
-}
 
 #[allow(dyn_drop)]
 struct MetaLogGuard {
