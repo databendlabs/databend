2666df9953edebae5ea1cb79b248026716c05e0b
q <787025321@qq.com>
2023-07-31 16:25:34 +0800

    fix: styles

--- website/src/components/TryCloudCard/index.tsx
+++ website/src/components/TryCloudCard/index.tsx
@@ -3,6 +3,7 @@ import React, { FC, ReactElement } from 'react';
 import styles from './styles.module.scss';
 import { Close } from '../Icons';
 import { useSessionStorageState } from 'ahooks';
+import CheckIcon from './Check';
 const TryCloudCard: FC = (): ReactElement=> {
   const [hidden, setHiddenFlag] = useSessionStorageState('DATABEND_TOC_CARD', {
     defaultValue: ''
@@ -10,17 +11,32 @@ const TryCloudCard: FC = (): ReactElement=> {
   const closeCard = ()=> {
     setHiddenFlag('closed');
   }
+  const features = [
+    'Low-cost',
+    'Fast Analytics',
+    'Easy Data Ingestion',
+    'Elastic Scaling'
+  ]
   return (
     <>
       {
         !hidden &&
         <div className={styles.card}>
           <div className={styles.header}>
-            <h6>Try Databend Cloud for FREE</h6>
+            <h6>Explore Databend Cloud for FREE</h6>
             <span onClick={closeCard} className={styles.close}><Close size={20}/></span>
           </div>
           <div className={styles.desc}>
-            Low-cost, fast analytics, easy data ingestion, and elastic scaling. 
+            {
+              features?.map((item)=> {
+                return (
+                  <div className={styles.descItem} key={item}>
+                    <span> <CheckIcon /></span>
+                    <span>{item}</span>
+                  </div>
+                )
+              })
+            }
           </div>
           <a href="https://www.databend.com/apply/?r=doc-card" className={styles.button}>Try it today</a>
         </div>
