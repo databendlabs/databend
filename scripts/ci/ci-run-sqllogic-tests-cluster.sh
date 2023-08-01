871e87bbf785937ca8eb11bf6452b79b39d45574
张炎泼 <drdr.xp@gmail.com>
2023-08-02 00:44:56 +0800

    ci: customize sqllogictest parallel level with "--parallel <n=8>"

--- scripts/ci/ci-run-sqllogic-tests-cluster.sh
+++ scripts/ci/ci-run-sqllogic-tests-cluster.sh
@@ -4,6 +4,17 @@
 
 set -e
 
+usage()
+{
+    cat <<-END
+	$0 [--parallel <n=8>] [<dir1> <dir2>]
+	Run sqllogictests with <n> threads in parallel.
+	If <dir>s are provided, only run tests in these dir.
+	END
+
+    exit 0
+}
+
 export STORAGE_ALLOW_INSECURE=true
 
 echo "Starting Cluster databend-query"
@@ -14,6 +25,22 @@ export RUST_BACKTRACE=1
 TEST_HANDLERS=${TEST_HANDLERS:-"mysql,http,clickhouse"}
 BUILD_PROFILE=${BUILD_PROFILE:-debug}
 
+PARALLEL=8
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --parallel)
+            shift
+            PARALLEL="$1"
+            shift
+            ;;
+
+        *)
+            break
+            ;;
+    esac
+done
+echo "Run suites in parallel: $PARALLEL"
+
 RUN_DIR=""
 if [ $# -gt 0 ]; then
 	RUN_DIR="--run_dir $*"
@@ -21,4 +48,10 @@ fi
 echo "Run suites using argument: $RUN_DIR"
 
 echo "Starting databend-sqllogic tests"
-target/${BUILD_PROFILE}/databend-sqllogictests --debug --handlers ${TEST_HANDLERS} ${RUN_DIR} --enable_sandbox --parallel 1 --skip_file tpcds_q64.test
+target/${BUILD_PROFILE}/databend-sqllogictests \
+    --debug \
+    --handlers ${TEST_HANDLERS} \
+    ${RUN_DIR} \
+    --enable_sandbox \
+    --parallel "$PARALLEL" \
+    --skip_file tpcds_q64.test
