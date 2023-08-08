62e99e0601d438cad05b1242d942cc2f8cc7c7c2
sky <3374614481@qq.com>
2023-08-08 22:07:09 +0800

    default 0

--- src/query/settings/src/settings_default.rs
+++ src/query/settings/src/settings_default.rs
@@ -329,7 +329,7 @@ impl DefaultSettings {
                     display_in_show_settings: true,
                 }),
                 ("enable_distributed_replace_into", DefaultSettingValue {
-                    value: UserSettingValue::UInt64(1),
+                    value: UserSettingValue::UInt64(0),
                     desc: "Enable distributed execution of replace into.",
                     possible_values: None,
                     display_in_show_settings: true,
