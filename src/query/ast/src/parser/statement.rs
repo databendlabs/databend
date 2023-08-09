bf1a61b42fa134dd3114d168192665bb81609502
taichong <tai_chong@foxmail.com>
2023-08-09 18:13:01 +0800

    fix conversation && Compatible with previous parse

--- src/query/ast/src/parser/statement.rs
+++ src/query/ast/src/parser/statement.rs
@@ -236,11 +236,11 @@ pub fn statement(i: Input) -> IResult<StatementMsg> {
 
     let set_role = map(
         rule! {
-            SET ~ (DEFAULT)? ~ ROLE ~ #ident
+            SET ~ (DEFAULT)? ~ ROLE ~ #role_name
         },
         |(_, opt_is_default, _, role_name)| Statement::SetRole {
             is_default: opt_is_default.is_some(),
-            role_name: role_name.name,
+            role_name,
         },
     );
 
@@ -879,20 +879,20 @@ pub fn statement(i: Input) -> IResult<StatementMsg> {
     let show_roles = value(Statement::ShowRoles, rule! { SHOW ~ ROLES });
     let create_role = map(
         rule! {
-            CREATE ~ ROLE ~ ( IF ~ NOT ~ EXISTS )? ~ #ident
+            CREATE ~ ROLE ~ ( IF ~ NOT ~ EXISTS )? ~ #role_name
         },
         |(_, _, opt_if_not_exists, role_name)| Statement::CreateRole {
             if_not_exists: opt_if_not_exists.is_some(),
-            role_name: role_name.name,
+            role_name,
         },
     );
     let drop_role = map(
         rule! {
-            DROP ~ ROLE ~ ( IF ~ EXISTS )? ~ #ident
+            DROP ~ ROLE ~ ( IF ~ EXISTS )? ~ #role_name
         },
         |(_, _, opt_if_exists, role_name)| Statement::DropRole {
             if_exists: opt_if_exists.is_some(),
-            role_name: role_name.name,
+            role_name,
         },
     );
     let grant = map(
@@ -1504,7 +1504,7 @@ pub fn statement(i: Input) -> IResult<StatementMsg> {
             | #alter_user : "`ALTER USER ('<username>'@'hostname' | USER()) [IDENTIFIED [WITH <auth_type>] [BY <password>]] [WITH <user_option>, ...]`"
             | #drop_user : "`DROP USER [IF EXISTS] '<username>'@'hostname'`"
             | #show_roles : "`SHOW ROLES`"
-            | #create_role : "`CREATE ROLE [IF NOT EXISTS] <role_name>]`"
+            | #create_role : "`CREATE ROLE [IF NOT EXISTS] <role_name>`"
             | #drop_role : "`DROP ROLE [IF EXISTS] <role_name>`"
             | #create_udf : "`CREATE FUNCTION [IF NOT EXISTS] <udf_name> (<parameter>, ...) -> <definition expr> [DESC = <description>]`"
             | #drop_udf : "`DROP FUNCTION [IF EXISTS] <udf_name>`"
@@ -1766,14 +1766,32 @@ pub fn column_def(i: Input) -> IResult<ColumnDefinition> {
     )(i)
 }
 
+pub fn role_name(i: Input) -> IResult<String> {
+    let role_ident = map(
+        rule! {
+            #ident
+        },
+        |role_name| role_name.name,
+    );
+    let role_lit = map(
+        rule! {
+            #literal_string
+        },
+        |role_name| role_name,
+    );
+
+    rule!(
+        #role_ident : "<role_name>"
+        | #role_lit : "'<role_name>'"
+    )(i)
+}
+
 pub fn grant_source(i: Input) -> IResult<AccountMgrSource> {
     let role = map(
         rule! {
-            ROLE ~ #ident
+            ROLE ~ #role_name
         },
-        |(_, role_name)| AccountMgrSource::Role {
-            role: role_name.name,
-        },
+        |(_, role_name)| AccountMgrSource::Role { role: role_name },
     );
     let privs = map(
         rule! {
@@ -1917,9 +1935,9 @@ pub fn show_grant_option(i: Input) -> IResult<ShowGrantOption> {
 pub fn grant_option(i: Input) -> IResult<PrincipalIdentity> {
     let role = map(
         rule! {
-            ROLE ~ #ident
+            ROLE ~ #role_name
         },
-        |(_, role_name)| PrincipalIdentity::Role(role_name.name),
+        |(_, role_name)| PrincipalIdentity::Role(role_name),
     );
 
     let user = map(
@@ -2351,9 +2369,9 @@ pub fn catalog_type(i: Input) -> IResult<CatalogType> {
 pub fn user_option(i: Input) -> IResult<UserOptionItem> {
     let default_role_option = map(
         rule! {
-            "DEFAULT_ROLE" ~ "=" ~ #ident
+            "DEFAULT_ROLE" ~ "=" ~ #role_name
         },
-        |(_, _, role)| UserOptionItem::DefaultRole(role.name),
+        |(_, _, role)| UserOptionItem::DefaultRole(role),
     );
     let set_network_policy = map(
         rule! {
