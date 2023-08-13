b33ca0d7ac86c028d1a2b09cf3c1934d32d0254c
JackTan25 <60096118+JackTan25@users.noreply.github.com>
2023-08-13 08:57:27 +0800

    fix: Fix copy rows (#12449)
    * fix copy into stage rows error
    
    * fix copy into stage rows error
    
    * fix clippy
    
    * fix clippy
    
    * Update src/query/storages/stage/src/row_based_file/serialize_processor.rs
    
    Co-authored-by: BohuTANG <overred.shuttler@gmail.com>
    
    * fix ctx parameter
    
    * fix clippy
    
    ---------
    
    Co-authored-by: BohuTANG <overred.shuttler@gmail.com>

--- src/query/storages/stage/src/row_based_file/serialize_processor.rs
+++ src/query/storages/stage/src/row_based_file/serialize_processor.rs
@@ -14,6 +14,8 @@
 
 use std::sync::Arc;
 
+use common_base::base::ProgressValues;
+use common_catalog::table_context::TableContext;
 use common_exception::Result;
 use common_expression::DataBlock;
 use common_formats::output_format::OutputFormat;
@@ -26,11 +28,13 @@ use common_pipeline_transforms::processors::transforms::Transformer;
 use crate::row_based_file::buffers::FileOutputBuffers;
 
 pub(super) struct SerializeProcessor {
+    ctx: Arc<dyn TableContext>,
     output_format: Box<dyn OutputFormat>,
 }
 
 impl SerializeProcessor {
     pub(super) fn try_create(
+        ctx: Arc<dyn TableContext>,
         input: Arc<InputPort>,
         output: Arc<OutputPort>,
         output_format: Box<dyn OutputFormat>,
@@ -38,7 +42,7 @@ impl SerializeProcessor {
         Ok(ProcessorPtr::create(Transformer::create(
             input,
             output,
-            SerializeProcessor { output_format },
+            SerializeProcessor { output_format, ctx },
         )))
     }
 }
@@ -50,12 +54,19 @@ impl Transform for SerializeProcessor {
         let mut buffers = vec![];
         let step = 1024;
         let num_rows = block.num_rows();
+        let mut bytes = 0;
         for i in (0..num_rows).step_by(step) {
             let end = (i + step).min(num_rows);
             let small_block = block.slice(i..end);
             let bs = self.output_format.serialize_block(&small_block)?;
+            bytes += bs.len();
             buffers.push(bs);
         }
+        let progress_values = ProgressValues {
+            rows: num_rows,
+            bytes,
+        };
+        self.ctx.get_write_progress().incr(&progress_values);
         Ok(FileOutputBuffers::create_block(buffers))
     }
 }
