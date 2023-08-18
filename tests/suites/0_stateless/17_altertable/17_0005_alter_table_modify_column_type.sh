1cf6ff82aeeed98fd0ed6b8996c855d7ada43a08
lichuang <lichuang1982@gmail.com>
2023-08-18 15:00:21 +0800

    add default column expr test case

--- tests/suites/0_stateless/17_altertable/17_0005_alter_table_modify_column_type.sh
+++ tests/suites/0_stateless/17_altertable/17_0005_alter_table_modify_column_type.sh
@@ -29,4 +29,15 @@ echo "SELECT a,b from test_modify_column_type.c"  | $MYSQL_CLIENT_CONNECT
 echo "INSERT INTO test_modify_column_type.c (b) values(2)"  | $MYSQL_CLIENT_CONNECT
 echo "SELECT a,b from test_modify_column_type.c"  | $MYSQL_CLIENT_CONNECT
 
+echo "CREATE table test_modify_column_type.d(a int, b int default 10)"  | $MYSQL_CLIENT_CONNECT
+echo "INSERT INTO test_modify_column_type.d (a) values(1)"  | $MYSQL_CLIENT_CONNECT
+echo "SELECT a,b from test_modify_column_type.d"  | $MYSQL_CLIENT_CONNECT
+echo "alter table test_modify_column_type.d modify column b int default 2"  | $MYSQL_CLIENT_CONNECT
+echo "SELECT a,b from test_modify_column_type.d"  | $MYSQL_CLIENT_CONNECT
+echo "alter table test_modify_column_type.d add column c float default 1.01" | $MYSQL_CLIENT_CONNECT
+echo "SELECT a,b,c from test_modify_column_type.d"  | $MYSQL_CLIENT_CONNECT
+echo "alter table test_modify_column_type.d modify column c float default 2.2"  | $MYSQL_CLIENT_CONNECT
+echo "INSERT INTO test_modify_column_type.d (a) values(10)"  | $MYSQL_CLIENT_CONNECT
+echo "SELECT a,b,c from test_modify_column_type.d"  | $MYSQL_CLIENT_CONNECT
+
 echo "DROP DATABASE IF EXISTS test_modify_column_type" | $MYSQL_CLIENT_CONNECT
