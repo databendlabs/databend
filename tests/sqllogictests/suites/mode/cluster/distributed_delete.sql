c4d899934b823639d0c8ce15a297290da1643c1e
sky <3374614481@qq.com>
2023-08-24 13:19:40 +0800

    add more slt to cover more situation

--- tests/sqllogictests/suites/mode/cluster/distributed_delete.sql
+++ tests/sqllogictests/suites/mode/cluster/distributed_delete.sql
@@ -16,15 +16,26 @@ statement ok
 insert into t select number, number * 10, number * 5 from numbers(1000) where number > 499;
 
 statement ok
-insert into t select number, number * 10, number * 5 from numbers(1500) where number > 1499;
+insert into t select number, number * 10, number * 5 from numbers(1500) where number > 999;
+
+query II
+select segment_count, block_count from fuse_snapshot('default','t') limit 1;
+----
+3 150
 
 # "backup" t
 statement ok
 create table t_origin as select * from t;
 
 # do the deletion (in distributed settings)
+# two segments are totally rewritten, one segment is reserved
 statement ok
 delete from t where id % 3 = 0 and id > 500;
+
+query II
+select segment_count, block_count from fuse_snapshot('default','t') limit 1;
+----
+3 150
 
 # check the sum of columns
 query I
@@ -41,6 +52,50 @@ query I
 select (select sum(c2) from t_origin where id % 3 != 0 or id <= 500) = (select sum(c2) from t);
 ----
 1
+
+# backup t again
+statement ok
+create table t_after_delete as select * from t;
+
+# one segment is totally deleted, two segments are reserved
+statement ok
+delete from t where id <= 499;
+
+query II
+select segment_count, block_count from fuse_snapshot('default','t') limit 1;
+----
+2 100
+
+# check the sum of columns
+query I
+select (select sum(id) from t_after_delete where id > 499) = (select sum(id) from t);
+
+query I
+select (select sum(c1) from t_after_delete where id > 499) = (select sum(c1) from t);
+
+query I
+select (select sum(c2) from t_after_delete where id > 499) = (select sum(c2) from t);
+
+# backup t again
+statement ok
+create table t_after_delete_2 as select * from t;
+
+# some block is totally deleted, some block is reserved, some block is partially reserved
+statement ok
+delete from t where id > 600 and id < 700;
+
+query II
+select segment_count, block_count from fuse_snapshot('default','t') limit 1;
+
+# check the sum of columns
+query I
+select (select sum(id) from t_after_delete_2 where id <= 600 or id >= 700) = (select sum(id) from t);
+
+query I
+select (select sum(c1) from t_after_delete_2 where id <= 600 or id >= 700) = (select sum(c1) from t);
+
+query I
+select (select sum(c2) from t_after_delete_2 where id <= 600 or id >= 700) = (select sum(c2) from t);
 
 # for issue #11784 https://github.com/datafuselabs/databend/issues/11784
 
