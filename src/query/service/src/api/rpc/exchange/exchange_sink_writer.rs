6ef13d44728deb2bd32c75653d91efcbc3f5be7d
zhang2014 <coswde@gmail.com>
2023-08-24 15:59:31 +0800

    chore(cluster): push ignore_result into cluster exchange

--- src/query/service/src/api/rpc/exchange/exchange_sink_writer.rs
+++ src/query/service/src/api/rpc/exchange/exchange_sink_writer.rs
@@ -46,7 +46,9 @@ impl AsyncSink for ExchangeWriterSink {
 
     #[async_backtrace::framed]
     async fn on_finish(&mut self) -> Result<()> {
-        self.flight_sender.close();
+        let (tx, _) = async_channel::bounded(1);
+        let mut empty = FlightSender::create(tx);
+        std::mem::swap(&mut self.flight_sender, &mut empty);
         Ok(())
     }
 
