93382781ed37e4a5cfc5eaaad515ba3646066f40
JackTan25 <jacktby@gmail.com>
2023-08-26 23:29:06 +0800

    fix test

--- src/query/storages/fuse/src/operations/merge_into/mutator/mod.rs
+++ src/query/storages/fuse/src/operations/merge_into/mutator/mod.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 mod matched_mutator;
 mod merge_into_split_mutator;
 mod split_by_expr_mutator;
