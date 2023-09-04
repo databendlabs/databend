412aa22507960eba67ef28387ac6be7479f1c900
Li Yazhou <me.ssword@gmail.com>
2023-09-04 20:13:23 +0800

    use prometheus-metrics in fuse

--- src/query/storages/fuse/src/metrics/fuse_metrics.rs
+++ src/query/storages/fuse/src/metrics/fuse_metrics.rs
@@ -28,9 +28,9 @@ macro_rules! key {
 
 lazy_static! {
     static ref COMMIT_MUTATION_UNRESOLVABLE_CONFLICT: Counter =
-        register_counter(key!(key!("commit_mutation_unresolvable_conflict")));
+        register_counter(key!("commit_mutation_unresolvable_conflict"));
     static ref COMMIT_MUTATION_LATEST_SNAPSHOT_APPEND_ONLY: Counter =
-        register_counter(key!(key!("commit_mutation_latest_snapshot_append_only")));
+        register_counter(key!("commit_mutation_latest_snapshot_append_only"));
     static ref COMMIT_MUTATION_MODIFIED_SEGMENT_EXISTS_IN_LATEST: Counter =
         register_counter(key!("commit_mutation_modified_segment_exists_in_latest"));
     static ref COMMIT_MUTATION_RETRY: Counter = register_counter(key!("commit_mutation_retry"));
@@ -48,15 +48,15 @@ lazy_static! {
     static ref REMOTE_IO_READ_MILLISECONDS: Histogram =
         register_histogram(key!("remote_io_read_milliseconds"));
     static ref REMOTE_IO_DESERIALIZE_MILLISECONDS: Histogram =
-        register_histogram(key!("remote_io_deserialize_milliseconds"));
+        register_histogram(key!("remote_io_deserialize_seconds"));
     static ref BLOCK_WRITE_NUMS: Counter = register_counter(key!("block_write_nums"));
     static ref BLOCK_WRITE_BYTES: Counter = register_counter(key!("block_write_bytes"));
     static ref BLOCK_WRITE_MILLISECONDS: Histogram =
-        register_histogram(key!("block_write_milliseconds"));
+        register_histogram(key!("block_write_seconds"));
     static ref BLOCK_INDEX_WRITE_NUMS: Counter = register_counter(key!("block_index_write_nums"));
     static ref BLOCK_INDEX_WRITE_BYTES: Counter = register_counter(key!("block_index_write_bytes"));
     static ref BLOCK_INDEX_WRITE_MILLISECONDS: Histogram =
-        register_histogram(key!("block_index_write_milliseconds"));
+        register_histogram(key!("block_index_write_seconds"));
     static ref BLOCK_INDEX_READ_BYTES: Counter = register_counter(key!("block_index_read_bytes"));
     static ref COMPACT_BLOCK_READ_NUMS: Counter = register_counter(key!("compact_block_read_nums"));
     static ref COMPACT_BLOCK_READ_BYTES: Counter =
@@ -99,10 +99,14 @@ lazy_static! {
         register_counter(key!("replace_into_block_number_after_pruning"));
     static ref REPLACE_INTO_SEGMENT_NUMBER_AFTER_PRUNING: Counter =
         register_counter(key!("replace_into_segment_number_after_pruning"));
+    static ref REPLACE_INTO_ROW_NUMBER_AFTER_PRUNING: Counter =
+        register_counter(key!("replace_into_row_number_after_pruning"));
     static ref REPLACE_INTO_BLOCK_NUMBER_TOTALLY_LOADED: Counter =
         register_counter(key!("replace_into_block_number_totally_loaded"));
     static ref REPLACE_INTO_ROW_NUMBER_WRITE: Counter =
         register_counter(key!("replace_into_row_number_write"));
+    static ref REPLACE_INTO_BLOCK_NUMBER_WRITE: Counter =
+        register_counter(key!("replace_into_block_number_write"));
     static ref REPLACE_INTO_ROW_NUMBER_TOTALLY_LOADED: Counter =
         register_counter(key!("replace_into_row_number_totally_loaded"));
     static ref REPLACE_INTO_BLOCK_NUMBER_WHOLE_BLOCK_DELETION: Counter =
@@ -117,6 +121,8 @@ lazy_static! {
         register_counter(key!("replace_into_partition_number"));
     static ref REPLACE_INTO_TIME_PROCESS_INPUT_BLOCK_MS: Histogram =
         register_histogram(key!("replace_into_time_process_input_block_ms"));
+    static ref REPLACE_INTO_NUMBER_ACCUMULATED_MERGE_ACTION: Counter =
+        register_counter(key!("replace_into_number_accumulated_merge_action"));
     static ref REPLACE_INTO_NUMBER_APPLY_DELETION: Counter =
         register_counter(key!("replace_into_number_apply_deletion"));
     static ref REPLACE_INTO_TIME_ACCUMULATED_MERGE_ACTION_MS: Histogram =
@@ -156,150 +162,180 @@ pub fn metrics_inc_commit_mutation_success() {
 
 pub fn metrics_inc_commit_copied_files(n: u64) {
     counter!(key!("commit_copied_files"), n);
-    COMMIT_COPIED_FILES.inc_by(n as f64);
+    COMMIT_COPIED_FILES.inc_by(n);
 }
 
 pub fn metrics_inc_commit_milliseconds(c: u128) {
     increment_gauge!(key!("commit_milliseconds"), c as f64);
-    COMMIT_MILLISECONDS.inc_by(c as f64);
+    COMMIT_MILLISECONDS.inc_by(c as u64);
 }
 
 pub fn metrics_inc_commit_aborts() {
     counter!(key!("commit_aborts"), 1);
-    COMMIT_ABORTS.inc()
+    COMMIT_ABORTS.inc();
 }
 
 pub fn metrics_inc_remote_io_seeks(c: u64) {
     increment_gauge!(key!("remote_io_seeks"), c as f64);
-    REMOTE_IO_SEEKS.inc_by(c as f64);
+    REMOTE_IO_SEEKS.inc_by(c);
 }
 
 pub fn metrics_inc_remote_io_seeks_after_merged(c: u64) {
     increment_gauge!(key!("remote_io_seeks_after_merged"), c as f64);
-    REMOTE_IO_SEEKS_AFTER_MERGED.inc_by(c as f64);
+    REMOTE_IO_SEEKS_AFTER_MERGED.inc_by(c);
 }
 
 pub fn metrics_inc_remote_io_read_bytes(c: u64) {
     increment_gauge!(key!("remote_io_read_bytes"), c as f64);
+    REMOTE_IO_READ_BYTES.inc_by(c);
 }
 
 pub fn metrics_inc_remote_io_read_bytes_after_merged(c: u64) {
     increment_gauge!(key!("remote_io_read_bytes_after_merged"), c as f64);
+    REMOTE_IO_READ_BYTES_AFTER_MERGED.inc_by(c);
 }
 
 pub fn metrics_inc_remote_io_read_parts(c: u64) {
     increment_gauge!(key!("remote_io_read_parts"), c as f64);
+    REMOTE_IO_READ_PARTS.inc_by(c);
 }
 
 pub fn metrics_inc_remote_io_read_milliseconds(c: u64) {
     increment_gauge!(key!("remote_io_read_milliseconds"), c as f64);
+    REMOTE_IO_READ_MILLISECONDS.observe(c as f64);
 }
 
 pub fn metrics_inc_remote_io_deserialize_milliseconds(c: u64) {
     increment_gauge!(key!("remote_io_deserialize_milliseconds"), c as f64);
+    REMOTE_IO_DESERIALIZE_MILLISECONDS.observe(c as f64);
 }
 
 /// Block metrics.
 pub fn metrics_inc_block_write_nums(c: u64) {
     increment_gauge!(key!("block_write_nums"), c as f64);
+    BLOCK_WRITE_NUMS.inc_by(c);
 }
 
 pub fn metrics_inc_block_write_bytes(c: u64) {
     increment_gauge!(key!("block_write_bytes"), c as f64);
+    BLOCK_WRITE_BYTES.inc_by(c);
 }
 
 pub fn metrics_inc_block_write_milliseconds(c: u64) {
     increment_gauge!(key!("block_write_milliseconds"), c as f64);
+    BLOCK_WRITE_MILLISECONDS.observe(c as f64);
 }
 
 pub fn metrics_inc_block_index_write_nums(c: u64) {
     increment_gauge!(key!("block_index_write_nums"), c as f64);
+    BLOCK_INDEX_WRITE_NUMS.inc_by(c);
 }
 
 pub fn metrics_inc_block_index_write_bytes(c: u64) {
     increment_gauge!(key!("block_index_write_bytes"), c as f64);
+    BLOCK_INDEX_WRITE_BYTES.inc_by(c);
 }
 
 pub fn metrics_inc_block_index_write_milliseconds(c: u64) {
     increment_gauge!(key!("block_index_write_milliseconds"), c as f64);
+    BLOCK_INDEX_WRITE_MILLISECONDS.observe(c as f64);
 }
 
 pub fn metrics_inc_block_index_read_bytes(c: u64) {
     increment_gauge!(key!("block_index_read_bytes"), c as f64);
+    BLOCK_INDEX_READ_BYTES.inc_by(c);
 }
 
 /// Compact metrics.
 pub fn metrics_inc_compact_block_read_nums(c: u64) {
     increment_gauge!(key!("compact_block_read_nums"), c as f64);
+    COMPACT_BLOCK_READ_NUMS.inc_by(c);
 }
 
 pub fn metrics_inc_compact_block_read_bytes(c: u64) {
     increment_gauge!(key!("compact_block_read_bytes"), c as f64);
+    COMPACT_BLOCK_READ_BYTES.inc_by(c);
 }
 
 pub fn metrics_inc_compact_block_read_milliseconds(c: u64) {
     increment_gauge!(key!("compact_block_read_milliseconds"), c as f64);
+    COMPACT_BLOCK_READ_MILLISECONDS.observe(c as f64);
 }
 
 /// Pruning metrics.
 pub fn metrics_inc_segments_range_pruning_before(c: u64) {
     increment_gauge!(key!("segments_range_pruning_before"), c as f64);
+    SEGMENTS_RANGE_PRUNING_BEFORE.inc_by(c);
 }
 
 pub fn metrics_inc_segments_range_pruning_after(c: u64) {
     increment_gauge!(key!("segments_range_pruning_after"), c as f64);
+    SEGMENTS_RANGE_PRUNING_AFTER.inc_by(c);
 }
 
 pub fn metrics_inc_bytes_segment_range_pruning_before(c: u64) {
     increment_gauge!(key!("bytes_segment_range_pruning_before"), c as f64);
+    BYTES_SEGMENT_RANGE_PRUNING_BEFORE.inc_by(c);
 }
 
 pub fn metrics_inc_bytes_segment_range_pruning_after(c: u64) {
     increment_gauge!(key!("bytes_segment_range_pruning_after"), c as f64);
+    BYTES_SEGMENT_RANGE_PRUNING_AFTER.inc_by(c);
 }
 
 pub fn metrics_inc_blocks_range_pruning_before(c: u64) {
     increment_gauge!(key!("blocks_range_pruning_before"), c as f64);
+    BLOCKS_RANGE_PRUNING_BEFORE.inc_by(c);
 }
 
 pub fn metrics_inc_blocks_range_pruning_after(c: u64) {
     increment_gauge!(key!("blocks_range_pruning_after"), c as f64);
+    BLOCKS_RANGE_PRUNING_AFTER.inc_by(c);
 }
 
 pub fn metrics_inc_bytes_block_range_pruning_before(c: u64) {
     increment_gauge!(key!("bytes_block_range_pruning_before"), c as f64);
+    BYTES_BLOCK_BLOOM_PRUNING_BEFORE.inc_by(c);
 }
 
 pub fn metrics_inc_bytes_block_range_pruning_after(c: u64) {
     increment_gauge!(key!("bytes_block_range_pruning_after"), c as f64);
+    BYTES_BLOCK_BLOOM_PRUNING_AFTER.inc_by(c);
 }
 
 pub fn metrics_inc_blocks_bloom_pruning_before(c: u64) {
     increment_gauge!(key!("blocks_bloom_pruning_before"), c as f64);
+    BLOCKS_BLOOM_PRUNING_BEFORE.inc_by(c);
 }
 
 pub fn metrics_inc_blocks_bloom_pruning_after(c: u64) {
     increment_gauge!(key!("blocks_bloom_pruning_after"), c as f64);
+    BLOCKS_BLOOM_PRUNING_AFTER.inc_by(c);
 }
 
 pub fn metrics_inc_bytes_block_bloom_pruning_before(c: u64) {
     increment_gauge!(key!("bytes_block_bloom_pruning_before"), c as f64);
+    BYTES_BLOCK_BLOOM_PRUNING_BEFORE.inc_by(c);
 }
 
 pub fn metrics_inc_bytes_block_bloom_pruning_after(c: u64) {
     increment_gauge!(key!("bytes_block_bloom_pruning_after"), c as f64);
+    BYTES_BLOCK_BLOOM_PRUNING_AFTER.inc_by(c);
 }
 
 pub fn metrics_inc_pruning_prewhere_nums(c: u64) {
     increment_gauge!(key!("pruning_prewhere_nums"), c as f64);
+    PRUNING_PREWHERE_NUMS.inc_by(c);
 }
 
 pub fn metrics_inc_pruning_milliseconds(c: u64) {
     increment_gauge!(key!("pruning_milliseconds"), c as f64);
+    PRUNING_MILLISECONDS.observe(c as f64);
 }
 
 pub fn metrics_inc_deletion_block_range_pruned_nums(c: u64) {
     increment_gauge!(key!("deletion_block_range_pruned_nums"), c as f64);
+    DELETION_BLOCK_RANGE_PRUNED_NUMS.inc_by(c);
 }
 
 pub fn metrics_inc_deletion_segment_range_purned_whole_segment_nums(c: u64) {
@@ -307,6 +343,7 @@ pub fn metrics_inc_deletion_segment_range_purned_whole_segment_nums(c: u64) {
         key!("deletion_segment_range_pruned_whole_segment_nums"),
         c as f64
     );
+    DELETION_SEGMENT_RANGE_PRUNED_WHOLE_SEGMENT_NUMS.inc_by(c);
 }
 
 pub fn metrics_inc_deletion_block_range_pruned_whole_block_nums(c: u64) {
@@ -314,33 +351,42 @@ pub fn metrics_inc_deletion_block_range_pruned_whole_block_nums(c: u64) {
         key!("deletion_block_range_pruned_whole_block_nums"),
         c as f64
     );
+    DELETION_BLOCK_RANGE_PRUNED_WHOLE_BLOCK_NUMS.inc_by(c);
 }
 
 pub fn metrics_inc_replace_block_number_after_pruning(c: u64) {
     increment_gauge!(key!("replace_into_block_number_after_pruning"), c as f64);
+    REPLACE_INTO_BLOCK_NUMBER_AFTER_PRUNING.inc_by(c);
 }
 
 pub fn metrics_inc_replace_segment_number_after_pruning(c: u64) {
     increment_gauge!(key!("replace_into_segment_number_after_pruning"), c as f64);
+    REPLACE_INTO_SEGMENT_NUMBER_AFTER_PRUNING.inc_by(c);
 }
 
 pub fn metrics_inc_replace_row_number_after_pruning(c: u64) {
     increment_gauge!(key!("replace_into_row_number_after_pruning"), c as f64);
+    REPLACE_INTO_ROW_NUMBER_AFTER_PRUNING.inc_by(c);
 }
 
 pub fn metrics_inc_replace_block_number_totally_loaded(c: u64) {
     increment_gauge!(key!("replace_into_block_number_totally_loaded"), c as f64);
+    REPLACE_INTO_BLOCK_NUMBER_TOTALLY_LOADED.inc_by(c);
 }
 
 pub fn metrics_inc_replace_row_number_write(c: u64) {
     increment_gauge!(key!("replace_into_row_number_write"), c as f64);
+    REPLACE_INTO_ROW_NUMBER_WRITE.inc_by(c);
 }
+
 pub fn metrics_inc_replace_block_number_write(c: u64) {
     increment_gauge!(key!("replace_into_block_number_write"), c as f64);
+    REPLACE_INTO_BLOCK_NUMBER_WRITE.inc_by(c);
 }
 
 pub fn metrics_inc_replace_row_number_totally_loaded(c: u64) {
     increment_gauge!(key!("replace_into_row_number_totally_loaded"), c as f64);
+    REPLACE_INTO_ROW_NUMBER_TOTALLY_LOADED.inc_by(c);
 }
 
 pub fn metrics_inc_replace_whole_block_deletion(c: u64) {
@@ -348,14 +394,17 @@ pub fn metrics_inc_replace_whole_block_deletion(c: u64) {
         key!("replace_into_block_number_whole_block_deletion"),
         c as f64
     );
+    REPLACE_INTO_BLOCK_NUMBER_WHOLE_BLOCK_DELETION.inc_by(c);
 }
 
 pub fn metrics_inc_replace_block_of_zero_row_deleted(c: u64) {
     increment_gauge!(key!("replace_into_block_number_zero_row_deleted"), c as f64);
+    REPLACE_INTO_BLOCK_NUMBER_ZERO_ROW_DELETED.inc_by(c);
 }
 
 pub fn metrics_inc_replace_original_row_number(c: u64) {
     increment_gauge!(key!("replace_into_row_number_source_block"), c as f64);
+    REPLACE_INTO_ROW_NUMBER_SOURCE_BLOCK.inc_by(c);
 }
 
 pub fn metrics_inc_replace_row_number_after_table_level_pruning(c: u64) {
@@ -363,25 +412,30 @@ pub fn metrics_inc_replace_row_number_after_table_level_pruning(c: u64) {
         key!("replace_into_row_number_after_table_level_pruning"),
         c as f64
     );
+    REPLACE_INTO_ROW_NUMBER_AFTER_TABLE_LEVEL_PRUNING.inc_by(c);
 }
 
 pub fn metrics_inc_replace_partition_number(c: u64) {
     increment_gauge!(key!("replace_into_partition_number"), c as f64);
+    REPLACE_INTO_PARTITION_NUMBER.inc_by(c);
 }
 
 // time used in processing the input block
 pub fn metrics_inc_replace_process_input_block_time_ms(c: u64) {
     increment_gauge!(key!("replace_into_time_process_input_block_ms"), c as f64);
+    REPLACE_INTO_TIME_PROCESS_INPUT_BLOCK_MS.observe(c as f64);
 }
 
 // the number of accumulate_merge_action operation invoked
 pub fn metrics_inc_replace_number_accumulated_merge_action() {
     increment_gauge!(key!("replace_into_number_accumulate_merge_action"), 1_f64);
+    REPLACE_INTO_NUMBER_ACCUMULATED_MERGE_ACTION.inc();
 }
 
 // the number of apply_deletion operation applied
 pub fn metrics_inc_replace_number_apply_deletion() {
     increment_gauge!(key!("replace_into_number_apply_deletion"), 1_f64);
+    REPLACE_INTO_NUMBER_APPLY_DELETION.inc();
 }
 
 // time used in executing the accumulated_merge_action operation
@@ -390,18 +444,22 @@ pub fn metrics_inc_replace_accumulated_merge_action_time_ms(c: u64) {
         key!("replace_into_time_accumulated_merge_action_ms"),
         c as f64
     );
+    REPLACE_INTO_TIME_ACCUMULATED_MERGE_ACTION_MS.observe(c as f64)
 }
 
 // time used in executing the apply_deletion operation
 pub fn metrics_inc_replace_apply_deletion_time_ms(c: u64) {
     increment_gauge!(key!("replace_into_time_apply_deletion_ms"), c as f64);
+    REPLACE_INTO_TIME_APPLY_DELETION_MS.observe(c as f64);
 }
 
 // number of blocks that pruned by bloom filter
 pub fn metrics_inc_replace_block_number_bloom_pruned(c: u64) {
     increment_gauge!(key!("replace_into_block_number_bloom_pruned"), c as f64);
+    REPLACE_INTO_BLOCK_NUMBER_BLOOM_PRUNED.inc_by(c);
 }
 // number of blocks from upstream  source
 pub fn metrics_inc_replace_block_number_input(c: u64) {
     increment_gauge!(key!("replace_into_block_number_source"), c as f64);
+    REPLACE_INTO_BLOCK_NUMBER_SOURCE.inc_by(c);
 }
