e930bd1f5ff969269ea220a3e432fd7fe5efdece
TCeason <33082201+TCeason@users.noreply.github.com>
2023-09-08 14:29:44 +0800

    feat(sqlsmith): add groupby (#12739)
    * feat(sqlsmith): add groupby
    
    * sqlsmith: recording group by generate select list
    
    * fix conversation && add gen order_by
    
    ---------
    
    Co-authored-by: baishen <baishen2009@gmail.com>

--- src/tests/sqlsmith/src/sql_gen/query.rs
+++ src/tests/sqlsmith/src/sql_gen/query.rs
@@ -13,8 +13,10 @@
 // limitations under the License.
 
 use common_ast::ast::Expr;
+use common_ast::ast::GroupBy;
 use common_ast::ast::Identifier;
 use common_ast::ast::Literal;
+use common_ast::ast::OrderByExpr;
 use common_ast::ast::Query;
 use common_ast::ast::SelectStmt;
 use common_ast::ast::SelectTarget;
@@ -32,13 +34,14 @@ impl<'a, R: Rng> SqlGenerator<'a, R> {
         let body = self.gen_set_expr();
         let limit = self.gen_limit();
         let offset = self.gen_offset(limit.len());
+        let order_by = self.gen_order_by();
         Query {
             span: None,
             // TODO
             with: None,
             body,
             // TODO
-            order_by: vec![],
+            order_by,
             limit,
             offset,
             ignore_result: false,
@@ -58,6 +61,32 @@ impl<'a, R: Rng> SqlGenerator<'a, R> {
 
     fn flip_coin(&mut self) -> bool {
         self.rng.gen_bool(0.5)
+    }
+
+    fn gen_order_by(&mut self) -> Vec<OrderByExpr> {
+        let order_nums = self.rng.gen_range(1..5);
+        let mut orders = Vec::with_capacity(order_nums);
+        if self.flip_coin() {
+            for _ in 0..order_nums {
+                let ty = self.gen_data_type();
+                let expr = self.gen_expr(&ty);
+                let order_by_expr = if self.rng.gen_bool(0.2) {
+                    OrderByExpr {
+                        expr,
+                        asc: None,
+                        nulls_first: None,
+                    }
+                } else {
+                    OrderByExpr {
+                        expr,
+                        asc: Some(self.flip_coin()),
+                        nulls_first: Some(self.flip_coin()),
+                    }
+                };
+                orders.push(order_by_expr);
+            }
+        }
+        orders
     }
 
     fn gen_limit(&mut self) -> Vec<Expr> {
@@ -91,7 +120,8 @@ impl<'a, R: Rng> SqlGenerator<'a, R> {
 
     fn gen_select(&mut self) -> SelectStmt {
         let from = self.gen_from();
-        let select_list = self.gen_select_list();
+        let group_by = self.gen_group_by();
+        let select_list = self.gen_select_list(&group_by);
         let selection = self.gen_selection();
         SelectStmt {
             span: None,
@@ -102,8 +132,7 @@ impl<'a, R: Rng> SqlGenerator<'a, R> {
             select_list,
             from,
             selection,
-            // TODO
-            group_by: None,
+            group_by,
             // TODO
             having: None,
             // TODO
@@ -111,25 +140,71 @@ impl<'a, R: Rng> SqlGenerator<'a, R> {
         }
     }
 
-    fn gen_select_list(&mut self) -> Vec<SelectTarget> {
-        let select_num = self.rng.gen_range(1..=5);
-        let mut targets = Vec::with_capacity(select_num);
-        for _ in 0..select_num {
-            let target = match self.rng.gen_range(0..=9) {
-                0..=9 => {
+    fn gen_group_by(&mut self) -> Option<GroupBy> {
+        let mode = self.rng.gen_range(0..=25);
+        let group_cap = self.rng.gen_range(1..=5);
+        let mut groupby_items = Vec::with_capacity(group_cap);
+
+        for _ in 0..group_cap {
+            let ty = self.gen_data_type();
+            let groupby_item = self.gen_expr(&ty);
+            groupby_items.push(groupby_item);
+        }
+
+        match mode {
+            0..=20 => None,
+            21 => Some(GroupBy::Normal(groupby_items)),
+            22 => Some(GroupBy::All),
+            23 => Some(GroupBy::GroupingSets(vec![groupby_items])),
+            24 => Some(GroupBy::Cube(groupby_items)),
+            25 => Some(GroupBy::Rollup(groupby_items)),
+            _ => unreachable!(),
+        }
+    }
+
+    fn gen_select_list(&mut self, group_by: &Option<GroupBy>) -> Vec<SelectTarget> {
+        let mut targets = Vec::with_capacity(5);
+
+        let generate_target = |expr: Expr| SelectTarget::AliasedExpr {
+            expr: Box::new(expr),
+            alias: None,
+        };
+
+        match group_by {
+            Some(GroupBy::Normal(group_by))
+            | Some(GroupBy::Cube(group_by))
+            | Some(GroupBy::Rollup(group_by)) => {
+                targets.extend(group_by.iter().map(|expr| SelectTarget::AliasedExpr {
+                    expr: Box::new(expr.clone()),
+                    alias: None,
+                }));
+            }
+            Some(GroupBy::All) => {
+                let select_num = self.rng.gen_range(1..=5);
+                for _ in 0..select_num {
                     let ty = self.gen_data_type();
                     let expr = self.gen_expr(&ty);
-                    SelectTarget::AliasedExpr {
-                        expr: Box::new(expr),
-                        // TODO
-                        alias: None,
-                    }
+                    let target = generate_target(expr);
+                    targets.push(target);
                 }
-                // TODO
-                _ => unreachable!(),
-            };
-            targets.push(target)
+            }
+            Some(GroupBy::GroupingSets(group_by)) => {
+                targets.extend(group_by[0].iter().map(|expr| SelectTarget::AliasedExpr {
+                    expr: Box::new(expr.clone()),
+                    alias: None,
+                }));
+            }
+            None => {
+                let select_num = self.rng.gen_range(1..=5);
+                for _ in 0..select_num {
+                    let ty = self.gen_data_type();
+                    let expr = self.gen_expr(&ty);
+                    let target = generate_target(expr);
+                    targets.push(target);
+                }
+            }
         }
+
         targets
     }
 
