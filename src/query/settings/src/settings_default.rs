4cae407775c6172d6c55cbfa4effe3d5fffbac4b
xudong.w <wxd963996380@gmail.com>
2023-09-10 21:52:33 +0800

    feat: support hash join build spill (#12608)
    * define structures and APIs for hash join build spill
    
    * try to integrate with transform
    
    * delete some useless apis
    
    * finish hash join build spill event
    
    * finish partition
    
    * refactor
    
    * implement simple spill to storage
    
    * add spill with location method
    
    * fix await
    
    * add simple check_need_spill method
    
    * add spill step for hash join probe processor
    
    * one partition multiple files
    
    * add spill read support
    
    * add it
    
    * add spill read test
    
    * cargo fix
    
    * save
    
    * equally split partitions
    
    * make settings work
    
    * fix probe state
    
    * fix hash
    
    * seems not read data
    
    * fix column layout
    
    * fix some states
    
    * make it run: select * from t3 inner join numbers(100000000) on t3.a = number;
    
    * refactor
    
    * fix block and add simple test
    
    * reduce test size
    
    * fix column_layout and cluster block
    
    * address comments

--- src/query/settings/src/settings_default.rs
+++ src/query/settings/src/settings_default.rs
@@ -158,7 +158,18 @@ impl DefaultSettings {
                     value: UserSettingValue::UInt64(0),
                     desc: "Disable join reorder optimization.",
                     possible_values: None,
-                    display_in_show_settings: false,
+                    display_in_show_settings: false,}),
+                ("enable_join_spill", DefaultSettingValue {
+                    value: UserSettingValue::UInt64(0),
+                    desc: "Enables hash join spill.",
+                    possible_values: None,
+                    display_in_show_settings: true,
+                }),
+                ("join_spilling_threshold", DefaultSettingValue {
+                    value: UserSettingValue::UInt64(0),
+                    desc: "Maximum amount of memory can use for hash join build or probe, 0 is unlimited.",
+                    possible_values: None,
+                    display_in_show_settings: true,
                 }),
                 ("enable_runtime_filter", DefaultSettingValue {
                     value: UserSettingValue::UInt64(0),
