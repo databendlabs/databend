a9139495366b82a4fba0b20c53fb7af9e3ce6e1f
zhihanz <zzh243402448@gmail.com>
2023-09-11 15:41:03 +0800

    chore: resolve session leak in background service (#12776)

--- tests/suites/6_background/00_target_tables/00_0000_target_tables.sh
+++ tests/suites/6_background/00_target_tables/00_0000_target_tables.sh
@@ -12,6 +12,8 @@ echo "drop table if exists target2" | $BENDSQL_CLIENT_CONNECT
 echo "create table target1(i int);" | $BENDSQL_CLIENT_CONNECT
 
 current_time=$(date -u +"%Y-%m-%d %H:%M:%S.%N")
+echo "select * from system.processes where type != 'HTTPQuery';" | $BENDSQL_CLIENT_CONNECT
+
 echo "select st.name bt,type, bt.trigger from system.background_tasks AS bt JOIN system.tables st ON bt.table_id = st.table_id where bt.trigger is not null and bt.created_on > TO_TIMESTAMP('$current_time') order by st.name;" | $BENDSQL_CLIENT_CONNECT
 echo "call  system\$execute_background_job('test_tenant-compactor-job');"
 echo "call  system\$execute_background_job('test_tenant-compactor-job');" | $BENDSQL_CLIENT_CONNECT
@@ -23,6 +25,7 @@ echo "call  system\$execute_background_job('test_tenant-compactor-job');"
 echo "call  system\$execute_background_job('test_tenant-compactor-job');" | $BENDSQL_CLIENT_CONNECT
 sleep 1
 echo "select st.name bt,type, bt.trigger from system.background_tasks AS bt JOIN system.tables st ON bt.table_id = st.table_id where bt.trigger is not null and bt.created_on > TO_TIMESTAMP('$current_time') order by st.name;" | $BENDSQL_CLIENT_CONNECT
+echo "select * from system.processes where type != 'HTTPQuery';" | $BENDSQL_CLIENT_CONNECT
 
 ## Drop table
 echo "drop table if exists target1;" | $BENDSQL_CLIENT_CONNECT
