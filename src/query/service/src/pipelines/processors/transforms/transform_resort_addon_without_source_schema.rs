2d65f9065dc074672539fd2cd28abc8fed146939
JackTan25 <jacktby@gmail.com>
2023-09-12 16:14:19 +0800

    reduce duplicate codes

--- src/query/service/src/pipelines/processors/transforms/transform_resort_addon_without_source_schema.rs
+++ src/query/service/src/pipelines/processors/transforms/transform_resort_addon_without_source_schema.rs
@@ -39,56 +39,56 @@ pub struct TransformResortAddOnWithoutSourceSchema {
     table: Arc<dyn Table>,
 }
 
-impl TransformResortAddOnWithoutSourceSchema {
-    fn build_expression_transform(
-        &self,
-        input_schema: DataSchemaRef,
-    ) -> Result<CompoundBlockOperator> {
-        let mut exprs = Vec::with_capacity(self.output_schema.fields().len());
-        for f in self.output_schema.fields().iter() {
-            let expr = if !input_schema.has_field(f.name()) {
-                if let Some(default_expr) = f.default_expr() {
-                    let mut expr = parse_exprs(self.ctx.clone(), self.table.clone(), default_expr)?;
-                    let mut expr = expr.remove(0);
-                    if expr.data_type() != f.data_type() {
-                        expr = Expr::Cast {
-                            span: None,
-                            is_try: f.data_type().is_nullable(),
-                            expr: Box::new(expr),
-                            dest_type: f.data_type().clone(),
-                        };
-                    }
-                    expr
-                } else {
-                    let default_value = Scalar::default_value(f.data_type());
-                    Expr::Constant {
+pub fn build_expression_transform(
+    input_schema: DataSchemaRef,
+    output_schema: DataSchemaRef,
+    table: Arc<dyn Table>,
+    ctx: Arc<QueryContext>,
+) -> Result<CompoundBlockOperator> {
+    let mut exprs = Vec::with_capacity(output_schema.fields().len());
+    for f in output_schema.fields().iter() {
+        let expr = if !input_schema.has_field(f.name()) {
+            if let Some(default_expr) = f.default_expr() {
+                let mut expr = parse_exprs(ctx.clone(), table.clone(), default_expr)?;
+                let mut expr = expr.remove(0);
+                if expr.data_type() != f.data_type() {
+                    expr = Expr::Cast {
                         span: None,
-                        scalar: default_value,
-                        data_type: f.data_type().clone(),
-                    }
+                        is_try: f.data_type().is_nullable(),
+                        expr: Box::new(expr),
+                        dest_type: f.data_type().clone(),
+                    };
                 }
+                expr
             } else {
-                let field = input_schema.field_with_name(f.name()).unwrap();
-                let id = input_schema.index_of(f.name()).unwrap();
-                Expr::ColumnRef {
+                let default_value = Scalar::default_value(f.data_type());
+                Expr::Constant {
                     span: None,
-                    id,
-                    data_type: field.data_type().clone(),
-                    display_name: field.name().clone(),
+                    scalar: default_value,
+                    data_type: f.data_type().clone(),
                 }
-            };
-            exprs.push(expr);
-        }
-
-        let func_ctx = self.ctx.get_function_context()?;
-        Ok(CompoundBlockOperator {
-            ctx: func_ctx,
-            operators: vec![BlockOperator::Map {
-                exprs,
-                projections: None,
-            }],
-        })
+            }
+        } else {
+            let field = input_schema.field_with_name(f.name()).unwrap();
+            let id = input_schema.index_of(f.name()).unwrap();
+            Expr::ColumnRef {
+                span: None,
+                id,
+                data_type: field.data_type().clone(),
+                display_name: field.name().clone(),
+            }
+        };
+        exprs.push(expr);
     }
+
+    let func_ctx = ctx.get_function_context()?;
+    Ok(CompoundBlockOperator {
+        ctx: func_ctx,
+        operators: vec![BlockOperator::Map {
+            exprs,
+            projections: None,
+        }],
+    })
 }
 
 impl TransformResortAddOnWithoutSourceSchema
@@ -119,9 +119,13 @@ impl Transform for TransformResortAddOnWithoutSourceSchema {
     fn transform(&mut self, mut block: DataBlock) -> Result<DataBlock> {
         let input_schema =
             DataSchemaRef::downcast_from(block.clone().get_owned_meta().unwrap()).unwrap();
-        block = self
-            .build_expression_transform(input_schema.clone())?
-            .transform(block)?;
+        block = build_expression_transform(
+            input_schema.clone(),
+            self.output_schema.clone(),
+            self.table.clone(),
+            self.ctx.clone(),
+        )?
+        .transform(block)?;
         let columns = block.columns()[input_schema.num_fields()..].to_owned();
         Ok(DataBlock::new(columns, block.num_rows()))
     }
