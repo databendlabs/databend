5261704fd570b384a026234533aadb2cbe20d80c
RinChanNOW <rin_chan_now@outlook.com>
2023-09-13 20:41:50 +0800

    feat: support column stats provider for PaquetTable (parquet_rs) (#12809)
    * parquet_rs: read meta when create parquet table and collect stats.
    
    * Store parquet meta in memory after creating table.
    
    * Lazy read parquet metas.
    
    * Skip create stats provider if is copy.
    
    * Fix deadlock.
    
    * Fix comment.
    
    * Fix leaf fields.
    
    * Satisfy comment.
    
    * Fix clippy.
    
    * Fix unit tests for leaf fields.
    
    ---------
    
    Co-authored-by: sundyli <543950155@qq.com>

--- src/query/sql/src/planner/binder/table.rs
+++ src/query/sql/src/planner/binder/table.rs
@@ -617,6 +617,7 @@ impl Binder {
                     .await?
                 } else {
                     ParquetRSTable::create(
+                        table_ctx.clone(),
                         stage_info.clone(),
                         files_info,
                         read_options,
@@ -958,7 +959,7 @@ impl Binder {
                         if let Some(col_id) = *leaf_index {
                             let col_stat =
                                 statistics_provider.column_statistics(col_id as ColumnId);
-                            col_stats.insert(*column_index, col_stat);
+                            col_stats.insert(*column_index, col_stat.cloned());
                         }
                     }
                 }
