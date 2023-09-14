e47afa1535c85f0774f801fbb27ea7b23a1147cb
JackTan25 <jacktby@gmail.com>
2023-09-14 22:01:22 +0800

    fix

--- src/query/sql/src/planner/plans/update.rs
+++ src/query/sql/src/planner/plans/update.rs
@@ -68,7 +68,7 @@ impl UpdatePlan {
     ) -> Result<Vec<(FieldIndex, RemoteExpr<String>)>> {
         let column = ColumnBindingBuilder::new(
             PREDICATE_COLUMN_NAME.to_string(),
-            use_column_name_index.unwrap_or_else(schema.num_fields()),
+            use_column_name_index.unwrap_or_else(|| schema.num_fields()),
             Box::new(DataType::Boolean),
             Visibility::Visible,
         )
