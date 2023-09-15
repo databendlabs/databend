0c85c7e9b96cf9b74e2c7449077929f64e84972d
Yang Xiufeng <yangxiufeng.c@gmail.com>
2023-09-15 17:01:46 +0800

    refactor: improve error message of copy.

--- tests/suites/1_stateful/00_stage/00_0011_distributed_copy_into_table_exection_test.sh
+++ tests/suites/1_stateful/00_stage/00_0011_distributed_copy_into_table_exection_test.sh
@@ -20,7 +20,7 @@ echo "select * from products order by id,name,description;" | $MYSQL_CLIENT_CONN
 echo "select count(*) from products;" | $MYSQL_CLIENT_CONNECT
 ## error test!!!
 curl -s -u root: -H "x-databend-stage-name:s0011" -F "upload=@${CURDIR}/../../../data/csv/sample_2_columns.csv" -XPUT "http://localhost:8000/v1/upload_to_stage" -u root: | jq ".data"
-echo "set enable_distributed_copy_into = 1;copy into products from @s0011 pattern = '.*[.]csv' purge = true;"  | $MYSQL_CLIENT_CONNECT 2>&1 | grep -o 'Text = expect 3 fields, only found 2'
+echo "set enable_distributed_copy_into = 1;copy into products from @s0011 pattern = '.*[.]csv' purge = true;"  | $MYSQL_CLIENT_CONNECT 2>&1 | grep -o 'wrong number of columns'
 echo "select count(*) from products;" | $MYSQL_CLIENT_CONNECT
 echo "list @s0011;" | $MYSQL_CLIENT_CONNECT | grep -o 'csv' | wc -l
 echo "set enable_distributed_copy_into = 1;copy into products from (select \$1,\$2,\$3 from @s0011 as t2) force = true purge = true;"  | $MYSQL_CLIENT_CONNECT
