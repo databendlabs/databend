b5bf91702d3d54b25a788ffe8ec3ca5340471ad8
Andy Lok <andylokandy@hotmail.com>
2023-09-25 16:26:26 +0800

    chore(planner): suggest string literal when quoted column doesn't exist

--- src/query/sql/src/planner/binder/bind_context.rs
+++ src/query/sql/src/planner/binder/bind_context.rs
@@ -17,6 +17,7 @@ use std::collections::BTreeMap;
 use std::hash::Hash;
 use std::sync::Arc;
 
+use common_ast::ast::Identifier;
 use common_ast::ast::Query;
 use common_ast::ast::TableAlias;
 use common_ast::ast::WindowSpec;
@@ -246,7 +247,8 @@ impl BindContext {
                 "table has {} columns available but {} columns specified",
                 self.columns.len(),
                 alias.columns.len()
-            )));
+            ))
+            .set_span(alias.name.span));
         }
         for (index, column_name) in alias
             .columns
@@ -265,16 +267,16 @@ impl BindContext {
         &self,
         database: Option<&str>,
         table: Option<&str>,
-        column: &str,
-        span: Span,
+        column: &Identifier,
         available_aliases: &[(String, ScalarExpr)],
     ) -> Result<NameResolutionResult> {
+        let name = &column.name;
         let mut result = vec![];
         // Lookup parent context to resolve outer reference.
         let mut alias_match_count = 0;
         if self.expr_context.prefer_resolve_alias() {
             for (alias, scalar) in available_aliases {
-                if database.is_none() && table.is_none() && column == alias {
+                if database.is_none() && table.is_none() && name == alias {
                     result.push(NameResolutionResult::Alias {
                         alias: alias.clone(),
                         scalar: scalar.clone(),
@@ -285,14 +287,14 @@ impl BindContext {
             }
 
             if alias_match_count == 0 {
-                self.search_bound_columns_recursively(database, table, column, &mut result);
+                self.search_bound_columns_recursively(database, table, name, &mut result);
             }
         } else {
-            self.search_bound_columns_recursively(database, table, column, &mut result);
+            self.search_bound_columns_recursively(database, table, name, &mut result);
 
             if result.is_empty() {
                 for (alias, scalar) in available_aliases {
-                    if database.is_none() && table.is_none() && column == alias {
+                    if database.is_none() && table.is_none() && name == alias {
                         result.push(NameResolutionResult::Alias {
                             alias: alias.clone(),
                             scalar: scalar.clone(),
@@ -305,12 +307,23 @@ impl BindContext {
 
         if result.len() > 1 && !result.iter().all_equal() {
             return Err(ErrorCode::SemanticError(format!(
-                "column {column} reference or alias is ambiguous, please use another alias name",
-            )));
+                "column {name} reference or alias is ambiguous, please use another alias name",
+            ))
+            .set_span(column.span));
         }
 
         if result.is_empty() {
-            Err(ErrorCode::SemanticError(format!("column {column} doesn't exist")).set_span(span))
+            if column.is_quoted() {
+                Err(ErrorCode::SemanticError(format!(
+                    "column {name} doesn't exist, do you mean '{name}'?"
+                ))
+                .set_span(column.span))
+            } else {
+                Err(
+                    ErrorCode::SemanticError(format!("column {name} doesn't exist"))
+                        .set_span(column.span),
+                )
+            }
         } else {
             Ok(result.remove(0))
         }
--- src/query/sql/src/planner/semantic/type_check.rs
+++ src/query/sql/src/planner/semantic/type_check.rs
@@ -221,12 +221,11 @@ impl<'a> TypeChecker<'a> {
                     .map(|ident| normalize_identifier(ident, self.name_resolution_ctx).name);
                 let result = match ident {
                     ColumnID::Name(ident) => {
-                        let column = normalize_identifier(ident, self.name_resolution_ctx).name;
+                        let column = normalize_identifier(ident, self.name_resolution_ctx);
                         self.bind_context.resolve_name(
                             database.as_deref(),
                             table.as_deref(),
-                            column.as_str(),
-                            ident.span,
+                            &column,
                             self.aliases,
                         )?
                     }
@@ -3075,12 +3074,15 @@ impl<'a> TypeChecker<'a> {
             };
         }
 
-        let inner_column_name = names.join(":");
+        let inner_column_ident = Identifier {
+            name: names.join(":"),
+            quote: None,
+            span,
+        };
         match self.bind_context.resolve_name(
             column.database_name.as_deref(),
             column.table_name.as_deref(),
-            inner_column_name.as_str(),
-            span,
+            &inner_column_ident,
             self.aliases,
         ) {
             Ok(result) => {
