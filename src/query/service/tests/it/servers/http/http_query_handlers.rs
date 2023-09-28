8487def317284f6e17ea5d5d71049cda3e36b7d7
Li Yazhou <me.ssword@gmail.com>
2023-09-28 21:50:53 +0800

    fix typo

--- src/query/service/tests/it/servers/http/http_query_handlers.rs
+++ src/query/service/tests/it/servers/http/http_query_handlers.rs
@@ -1196,7 +1196,7 @@ async fn test_affect() -> Result<()> {
             serde_json::json!({"sql": "set max_threads=1", "session": {"settings": {"max_threads": "6", "timezone": "Asia/Shanghai"}}}),
             Some(QueryAffect::ChangeSettings {
                 keys: vec!["max_threads".to_string()],
-                values: vec!["24".to_string()],
+                values: vec!["1".to_string()],
                 is_globals: vec![false],
             }),
             Some(HttpSessionConf {
