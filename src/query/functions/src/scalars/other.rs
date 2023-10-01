04eb3a0f4ba2e1f23eee7bf5ec19e13426e63ec8
zenus <625305505@qq.com>
2023-10-01 21:38:38 +0800

    optimized

--- src/query/functions/src/scalars/other.rs
+++ src/query/functions/src/scalars/other.rs
@@ -250,7 +250,7 @@ pub fn register(registry: &mut FunctionRegistry) {
         let f = Function {
             signature: FunctionSignature {
                 name,
-                args_type: vec![arg_type.clone(); args_type.len()],
+                args_type: args_type.to_vec(),
                 return_type: arg_type.clone(),
             },
             eval: FunctionEval::Scalar {
