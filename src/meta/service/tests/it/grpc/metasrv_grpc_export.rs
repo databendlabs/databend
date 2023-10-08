1b83bdb011ebebea1fc05da7ce802cef16565ee7
dantengsky <dantengsky@gmail.com>
2023-10-08 15:08:37 +0800

    Revert "tweak unit tests, adapts to nextest"
    This reverts commit 732cf93a21ce7ae497dda63cdc703a724c21054e.

--- src/meta/service/tests/it/grpc/metasrv_grpc_export.rs
+++ src/meta/service/tests/it/grpc/metasrv_grpc_export.rs
@@ -36,8 +36,6 @@ async fn test_export() -> anyhow::Result<()> {
     // Thus in this test we'll gonna trigger a snapshot manually.
 
     let (tc, _addr) = crate::tests::start_metasrv().await?;
-    let config_id = tc.config.raft_config.config_id.clone();
-    let grpc_api_advertise_addr = tc.config.grpc_api_advertise_address().unwrap();
 
     let client = tc.grpc_client().await?;
 
@@ -94,34 +92,20 @@ async fn test_export() -> anyhow::Result<()> {
 
     // The addresses are built from random number.
     // Wash them.
-    let want = want
+    let lines = lines
         .iter()
         .map(|x| {
             Regex::new(r"29\d\d\d")
                 .unwrap()
-                .replace_all(x, config_id.clone())
+                .replace_all(x, "29000")
                 .to_string()
         })
         .map(|x| {
             Regex::new(r"test-29\d\d\d")
                 .unwrap()
-                .replace_all(&x, format!("test-{}", config_id))
+                .replace_all(&x, "test-29000")
                 .to_string()
         })
-        .map(|x| {
-            Regex::new(r#""grpc_api_advertise_address":"127\.0\.0\.1:\d+""#)
-                .unwrap()
-                .replace_all(
-                    &x,
-                    format!(r#""grpc_api_advertise_address":"{}""#, grpc_api_advertise_addr),
-                )
-                .to_string()
-        })
-        .collect::<Vec<_>>();
-
-    // Wash the time_ms fields
-    let lines = lines
-        .iter()
         .map(|x| {
             Regex::new(r"\d{13}")
                 .unwrap()
