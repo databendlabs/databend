3f42b0ea64432a204ca162568b8a29f3c00501c3
Andy Lok <andylokandy@hotmail.com>
2023-10-13 03:20:30 +0800

    fix

--- src/query/sql/src/executor/physical_plans/physical_table_scan.rs
+++ src/query/sql/src/executor/physical_plans/physical_table_scan.rs
@@ -32,7 +32,6 @@ use common_expression::type_check::check_function;
 use common_expression::types::DataType;
 use common_expression::ConstantFolder;
 use common_expression::DataField;
-use common_expression::DataSchema;
 use common_expression::DataSchemaRef;
 use common_expression::DataSchemaRefExt;
 use common_expression::FieldIndex;
