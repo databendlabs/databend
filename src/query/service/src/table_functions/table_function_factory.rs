13606f30e9f723c3e422253e909b7df55ab6ddbe
zenus <625305505@qq.com>
2023-10-13 21:58:07 +0800

    add new feature

--- src/query/service/src/table_functions/table_function_factory.rs
+++ src/query/service/src/table_functions/table_function_factory.rs
@@ -36,6 +36,7 @@ use crate::storages::fuse::table_functions::FuseSnapshotTable;
 use crate::storages::fuse::table_functions::FuseStatisticTable;
 use crate::table_functions::async_crash_me::AsyncCrashMeTable;
 use crate::table_functions::infer_schema::InferSchemaTable;
+use crate::table_functions::inspect_parquet::InspectParquetTable;
 use crate::table_functions::list_stage::ListStageTable;
 use crate::table_functions::numbers::NumbersTable;
 use crate::table_functions::srf::RangeTable;
@@ -147,6 +148,10 @@ impl TableFunctionFactory {
         creators.insert(
             "infer_schema".to_string(),
             (next_id(), Arc::new(InferSchemaTable::create)),
+        );
+        creators.insert(
+            "inspect_parquet".to_string(),
+            (next_id(), Arc::new(InspectParquetTable::create)),
         );
 
         creators.insert(
