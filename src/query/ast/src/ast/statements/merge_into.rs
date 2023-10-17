7754ce77734d0fc0cf927e96690cc6b958c956ed
JackTan25 <jacktby@gmail.com>
2023-10-17 15:41:09 +0800

    refactor source as alias

--- src/query/ast/src/ast/statements/merge_into.rs
+++ src/query/ast/src/ast/statements/merge_into.rs
@@ -92,7 +92,6 @@ pub struct MergeIntoStmt {
     pub database: Option<Identifier>,
     pub table_ident: Identifier,
     pub source: MergeSource,
-    pub source_alias: Option<TableAlias>,
     // target_alias is belong to target
     pub target_alias: Option<TableAlias>,
     pub join_expr: Expr,
@@ -221,6 +220,7 @@ pub enum MergeSource {
 
     Select {
         query: Box<Query>,
+        source_alias: TableAlias,
     },
 }
 
@@ -232,7 +232,7 @@ pub struct StreamingSource {
 }
 
 impl MergeSource {
-    pub fn transform_table_reference(&self, alias: Option<TableAlias>) -> TableReference {
+    pub fn transform_table_reference(&self) -> TableReference {
         match self {
             Self::StreamingV2 {
                 settings: _,
@@ -240,10 +240,13 @@ impl MergeSource {
                 start: _,
             } => unimplemented!(),
 
-            Self::Select { query } => TableReference::Subquery {
+            Self::Select {
+                query,
+                source_alias,
+            } => TableReference::Subquery {
                 span: None,
                 subquery: query.clone(),
-                alias,
+                alias: Some(source_alias.clone()),
             },
         }
     }
@@ -267,7 +270,10 @@ impl Display for MergeSource {
                 )
             }
 
-            MergeSource::Select { query } => write!(f, "{query}"),
+            MergeSource::Select {
+                query,
+                source_alias,
+            } => write!(f, "{query} as {source_alias}"),
         }
     }
 }
