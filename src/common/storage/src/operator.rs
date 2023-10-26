b5678a30afe5306dfcd145615f8082ae4f21bf87
Andy Lok <andylokandy@hotmail.com>
2023-10-26 23:19:45 +0800

    fix

--- src/common/storage/src/operator.rs
+++ src/common/storage/src/operator.rs
@@ -41,6 +41,7 @@ use common_meta_app::storage::StorageWebhdfsConfig;
 use log::warn;
 use opendal::layers::ImmutableIndexLayer;
 use opendal::layers::LoggingLayer;
+use opendal::layers::MinitraceLayer;
 use opendal::layers::RetryLayer;
 use opendal::layers::TimeoutLayer;
 use opendal::raw::HttpClient;
