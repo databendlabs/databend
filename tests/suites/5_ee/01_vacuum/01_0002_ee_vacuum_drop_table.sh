2768cb37e382a39c0a99ace27f530f7742b08e07
BohuTANG <overred.shuttler@gmail.com>
2023-10-27 19:31:32 +0800

    chore: aws_* to access_key_id/secret_access_key (#13473)
    * chore: aws_* to access_key_id/secret_access_key
    
    * fix show

--- tests/suites/5_ee/01_vacuum/01_0002_ee_vacuum_drop_table.sh
+++ tests/suites/5_ee/01_vacuum/01_0002_ee_vacuum_drop_table.sh
@@ -61,7 +61,7 @@ echo "drop database if exists test_vacuum_drop_4" | $MYSQL_CLIENT_CONNECT
 echo "drop table if exists table_drop_external_location;" | $MYSQL_CLIENT_CONNECT
 
 ## Create table
-echo "create table table_drop_external_location(a int) 's3://testbucket/admin/data/' connection=(aws_key_id='minioadmin' aws_secret_key='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $MYSQL_CLIENT_CONNECT
+echo "create table table_drop_external_location(a int) 's3://testbucket/admin/data/' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $MYSQL_CLIENT_CONNECT
 
 table_inserts=(
   "insert into table_drop_external_location(a) values(888)"
