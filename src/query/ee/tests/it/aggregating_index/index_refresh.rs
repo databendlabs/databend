4c19497aefb63b10acc70519b7a7c69121aa3b77
JackTan25 <jacktby@gmail.com>
2023-11-02 23:48:48 +0800

    fix tests

--- src/query/ee/tests/it/aggregating_index/index_refresh.rs
+++ src/query/ee/tests/it/aggregating_index/index_refresh.rs
@@ -38,6 +38,9 @@ use databend_query::test_kits::TestFixture;
 use enterprise_query::test_kits::context::create_ee_query_context;
 use futures_util::TryStreamExt;
 
+use crate::aggregating_index::CATALOG;
+use crate::aggregating_index::DATABASE;
+
 async fn plan_sql(ctx: Arc<QueryContext>, sql: &str) -> Result<Plan> {
     let mut planner = Planner::new(ctx);
     let (plan, _) = planner.plan_sql(sql).await?;
@@ -110,18 +113,23 @@ async fn refresh_index(
 #[tokio::test(flavor = "multi_thread")]
 async fn test_refresh_agg_index() -> Result<()> {
     let (_guard, ctx, root) = create_ee_query_context(None).await.unwrap();
-    let fixture = TestFixture::new_with_ctx(_guard, ctx.clone()).await;
+    let fixture = TestFixture::new_with_ctx(_guard, ctx).await;
 
     // Create table
-    ctx.clean_cache();
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
     execute_sql(
         fixture.ctx(),
         "CREATE TABLE t0 (a int, b int, c int) storage_format = 'parquet'",
     )
     .await?;
 
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
+
     // Insert data
-    ctx.clean_cache();
     execute_sql(
         fixture.ctx(),
         "INSERT INTO t0 VALUES (1,1,4), (1,2,1), (1,2,4), (2,2,5)",
@@ -131,7 +139,10 @@ async fn test_refresh_agg_index() -> Result<()> {
     // Create index
     let index_name = "index0";
 
-    ctx.clean_cache();
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
+
     let index_id = create_index(
         fixture.ctx(),
         index_name,
@@ -141,7 +152,9 @@ async fn test_refresh_agg_index() -> Result<()> {
     .await?;
 
     // Refresh Index
-    ctx.clean_cache();
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, &index_name)?;
     refresh_index(fixture.ctx(), index_name, None).await?;
 
     let block_path = find_block_path(&root)?.unwrap();
@@ -160,7 +173,9 @@ async fn test_refresh_agg_index() -> Result<()> {
 
     // Check aggregating index is correct.
     {
-        ctx.clean_cache();
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         let res = execute_sql(
             fixture.ctx(),
             "SELECT b, SUM_STATE(a) from t0 WHERE c > 1 GROUP BY b",
@@ -168,7 +183,9 @@ async fn test_refresh_agg_index() -> Result<()> {
         .await?;
         let data_blocks: Vec<DataBlock> = res.try_collect().await?;
 
-        ctx.clean_cache();
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         let agg_res = execute_sql(
             fixture.ctx(),
             &format!(
@@ -183,7 +200,9 @@ async fn test_refresh_agg_index() -> Result<()> {
     }
 
     // Insert more data
-    ctx.clean_cache();
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
     execute_sql(
         fixture.ctx(),
         "INSERT INTO t0 VALUES (1,1,4), (1,2,1), (1,2,4), (2,2,5)",
@@ -195,7 +214,9 @@ async fn test_refresh_agg_index() -> Result<()> {
     assert!(blocks.len() > indexes.len());
 
     // Refresh Index again
-    ctx.clean_cache();
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
     refresh_index(fixture.ctx(), index_name, None).await?;
 
     // check the new added index is correct.
@@ -214,7 +235,9 @@ async fn test_refresh_agg_index() -> Result<()> {
             indexes[0].clone()
         };
 
-        ctx.clean_cache();
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         let data_blocks: Vec<DataBlock> = execute_sql(
             fixture.ctx(),
             &format!(
@@ -226,7 +249,9 @@ async fn test_refresh_agg_index() -> Result<()> {
         .try_collect()
         .await?;
 
-        ctx.clean_cache();
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         let agg_data_blocks: Vec<DataBlock> = execute_sql(
             fixture.ctx(),
             &format!(
@@ -238,7 +263,9 @@ async fn test_refresh_agg_index() -> Result<()> {
         .try_collect()
         .await?;
 
-        ctx.clean_cache();
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         assert_two_blocks_sorted_eq_with_name(
             "refresh index again",
             &data_blocks,
@@ -270,7 +297,9 @@ async fn test_refresh_agg_index_with_limit() -> Result<()> {
 
     // Create index
     let index_name = "index1";
-
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t1")?;
     let index_id = create_index(
         fixture.ctx(),
         index_name,
@@ -280,12 +309,18 @@ async fn test_refresh_agg_index_with_limit() -> Result<()> {
     .await?;
 
     // Insert more data
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t1")?;
     execute_sql(
         fixture.ctx(),
         "INSERT INTO t1 VALUES (1,1,4), (1,2,1), (1,2,4), (2,2,5)",
     )
     .await?;
 
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t1")?;
     execute_sql(
         fixture.ctx(),
         "INSERT INTO t1 VALUES (1,1,4), (1,2,1), (1,2,4), (2,2,5)",
@@ -293,6 +328,9 @@ async fn test_refresh_agg_index_with_limit() -> Result<()> {
     .await?;
 
     // Refresh index with limit 1
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t1")?;
     refresh_index(fixture.ctx(), index_name, Some(1)).await?;
 
     let block_path = find_block_path(&root)?.unwrap();
@@ -350,6 +388,9 @@ async fn test_sync_agg_index_after_update() -> Result<()> {
     .await?;
 
     // Insert data
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
     execute_sql(
         ctx.clone(),
         "INSERT INTO t0 VALUES (1,1,4), (1,2,1), (1,2,4), (2,2,5)",
@@ -367,6 +408,9 @@ async fn test_sync_agg_index_after_update() -> Result<()> {
 
     // Check aggregating index_0 is correct.
     {
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         let res = execute_sql(
             ctx.clone(),
             "SELECT b, SUM_STATE(a) from t0 WHERE c > 1 GROUP BY b",
@@ -392,6 +436,9 @@ async fn test_sync_agg_index_after_update() -> Result<()> {
     }
 
     // Update
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
     execute_sql(ctx.clone(), "UPDATE t0 SET c = 2 WHERE b = 2").await?;
 
     let first_block = blocks[0].clone();
@@ -405,6 +452,9 @@ async fn test_sync_agg_index_after_update() -> Result<()> {
 
     // Check aggregating index_0 is correct after update.
     {
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         let updated_block = blocks
             .iter()
             .find(|b| !b.eq_ignore_ascii_case(&first_block))
@@ -479,6 +529,9 @@ async fn test_sync_agg_index_after_insert() -> Result<()> {
     .await?;
 
     // Insert data
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
     execute_sql(
         ctx.clone(),
         "INSERT INTO t0 VALUES (1,1,4), (1,2,1), (1,2,4), (2,2,5)",
@@ -500,6 +553,9 @@ async fn test_sync_agg_index_after_insert() -> Result<()> {
 
     // Check aggregating index_0 is correct.
     {
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         let res = execute_sql(
             ctx.clone(),
             "SELECT b, SUM_STATE(a) from t0 WHERE c > 1 GROUP BY b",
@@ -526,6 +582,9 @@ async fn test_sync_agg_index_after_insert() -> Result<()> {
 
     // Check aggregating index_1 is correct.
     {
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         let res = execute_sql(
             ctx.clone(),
             "SELECT a, SUM_STATE(b) from t0 WHERE c > 1 GROUP BY a",
@@ -551,6 +610,9 @@ async fn test_sync_agg_index_after_insert() -> Result<()> {
     }
 
     // Insert more data with insert into ... select ...
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
     execute_sql(ctx.clone(), "INSERT INTO t0 SELECT * FROM t0").await?;
 
     let blocks = collect_file_names(&block_path)?;
@@ -592,6 +654,9 @@ async fn test_sync_agg_index_after_copy_into() -> Result<()> {
     .await?;
 
     // Copy into data
+    fixture
+        .ctx()
+        .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
     execute_sql(
         ctx.clone(),
         "COPY INTO books FROM 'https://datafuse-1253727613.cos.ap-hongkong.myqcloud.com/data/books.csv' FILE_FORMAT = (TYPE = CSV);",
@@ -609,6 +674,9 @@ async fn test_sync_agg_index_after_copy_into() -> Result<()> {
 
     // Check aggregating index_0 is correct.
     {
+        fixture
+            .ctx()
+            .evict_table_from_cache(CATALOG, DATABASE, "t0")?;
         let res = execute_sql(ctx.clone(), "SELECT MAX_STATE(title) from books").await?;
         let data_blocks: Vec<DataBlock> = res.try_collect().await?;
 
--- src/query/ee/tests/it/storages/fuse/operations/vacuum.rs
+++ src/query/ee/tests/it/storages/fuse/operations/vacuum.rs
@@ -14,129 +14,14 @@
 
 use std::sync::Arc;
 
-use chrono::Duration;
-use chrono::Utc;
 use common_base::base::tokio;
 use common_catalog::table_context::TableContext;
 use common_exception::Result;
-use common_storages_fuse::FuseTable;
 use databend_query::test_kits::table_test_fixture::append_sample_data;
 use databend_query::test_kits::table_test_fixture::check_data_dir;
 use databend_query::test_kits::table_test_fixture::execute_command;
-use databend_query::test_kits::table_test_fixture::execute_query;
 use databend_query::test_kits::table_test_fixture::TestFixture;
-use databend_query::test_kits::utils::generate_orphan_files;
-use databend_query::test_kits::utils::generate_snapshot_with_segments;
-use databend_query::test_kits::utils::query_count;
-use enterprise_query::storages::fuse::do_vacuum;
 use enterprise_query::storages::fuse::do_vacuum_drop_tables;
-use enterprise_query::storages::fuse::operations::vacuum_table::get_snapshot_referenced_files;
-use enterprise_query::storages::fuse::operations::vacuum_table::SnapshotReferencedFiles;
-
-async fn check_files_existence(
-    fixture: &TestFixture,
-    exist_files: Vec<&Vec<String>>,
-    not_exist_files: Vec<&Vec<String>>,
-) -> Result<()> {
-    let table = fixture.latest_default_table().await?;
-    let fuse_table = FuseTable::try_from_table(table.as_ref())?;
-    let dal = fuse_table.get_operator_ref();
-    for file_vec in &not_exist_files {
-        for file in file_vec.iter() {
-            assert!(!dal.is_exist(file).await?);
-        }
-    }
-
-    for file_vec in &exist_files {
-        for file in file_vec.iter() {
-            assert!(dal.is_exist(file).await?);
-        }
-    }
-
-    Ok(())
-}
-
-async fn check_dry_run_files(fixture: &TestFixture, files: Vec<&Vec<String>>) -> Result<()> {
-    let ctx = fixture.ctx();
-    let table_ctx: Arc<dyn TableContext> = ctx.clone();
-    let table = fixture.latest_default_table().await?;
-    let fuse_table = FuseTable::try_from_table(table.as_ref())?;
-    let retention_time = chrono::Utc::now() - chrono::Duration::seconds(2);
-    let files_opt = do_vacuum(fuse_table, table_ctx, retention_time, true).await?;
-
-    assert!(files_opt.is_some());
-    let purge_files = files_opt.unwrap();
-    for file_vec in &files {
-        for file in file_vec.iter() {
-            assert!(purge_files.contains(file));
-        }
-    }
-    let mut count = 0;
-    files.iter().for_each(|f| count += f.len());
-    assert_eq!(purge_files.len(), count);
-    Ok(())
-}
-
-// return all the snapshot files and referenced files.
-async fn get_snapshots_and_referenced_files(
-    fixture: &TestFixture,
-) -> Result<(SnapshotReferencedFiles, Vec<String>)> {
-    let ctx = fixture.ctx();
-    let table_ctx: Arc<dyn TableContext> = ctx.clone();
-    let table = fixture.latest_default_table().await?;
-    let fuse_table = FuseTable::try_from_table(table.as_ref())?;
-    let referenced_files = get_snapshot_referenced_files(fuse_table, &table_ctx)
-        .await?
-        .unwrap();
-    let snapshot_files = fuse_table.list_snapshot_files().await?;
-
-    Ok((referenced_files, snapshot_files))
-}
-
-// return generate orphan files and snapshot file(optional).
-async fn generate_test_orphan_files(
-    fixture: &TestFixture,
-    genetate_snapshot: bool,
-    orphan_segment_file_num: usize,
-    orphan_block_per_segment_num: usize,
-) -> Result<(Vec<String>, Option<String>)> {
-    let table = fixture.latest_default_table().await?;
-    let fuse_table = FuseTable::try_from_table(table.as_ref())?;
-    let files = generate_orphan_files(
-        fuse_table,
-        orphan_segment_file_num,
-        orphan_block_per_segment_num,
-    )
-    .await?;
-
-    let snapshot_opt = if genetate_snapshot {
-        let mut segments = Vec::with_capacity(files.len());
-        files.iter().for_each(|(location, _)| {
-            segments.push(location.clone());
-        });
-
-        let new_timestamp = Utc::now() - Duration::minutes(1);
-        let snapshot_location =
-            generate_snapshot_with_segments(fuse_table, segments, Some(new_timestamp)).await?;
-
-        Some(snapshot_location)
-    } else {
-        None
-    };
-
-    let mut orphan_files = vec![];
-    for (location, segment) in files {
-        orphan_files.push(location.0);
-        for block_meta in segment.blocks {
-            orphan_files.push(block_meta.location.0.clone());
-            if let Some(block_index) = &block_meta.bloom_filter_index_location {
-                orphan_files.push(block_index.0.clone());
-            }
-        }
-    }
-
-    Ok((orphan_files, snapshot_opt))
-}
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_fuse_do_vacuum_drop_table() -> Result<()> {
