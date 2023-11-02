f2475f06c39c482e7ba16ba9f705d716c2bbc5d5
Yang Xiufeng <yangxiufeng.c@gmail.com>
2023-11-02 03:00:23 -0500

    Update tests/suites/1_stateful/09_http_handler/09_0003_error_detail.sh

--- tests/suites/1_stateful/09_http_handler/09_0003_error_detail.sh
+++ tests/suites/1_stateful/09_http_handler/09_0003_error_detail.sh
@@ -10,4 +10,4 @@ echo "remove @s1;" | $BENDSQL_CLIENT_CONNECT
 echo "create table products (id int, name string, description string);" | $BENDSQL_CLIENT_CONNECT
 
 curl -s -u root: -H "x-databend-stage-name:s1" -F "upload=@${CURDIR}/../../../data/csv/select.csv" -XPUT "http://localhost:8000/v1/upload_to_stage" -u root: | jq ".data"
-curl -s -u root: -XPOST "http://localhost:8000/v1/query" --header 'Content-Type: application/json' -d '{"sql": "set enable_distributed_copy_into = 0;copy into products (id, name, description) from @s1/", "pagination": { "wait_time_secs": 6}}' -u root: | jq ".error"
+curl -s -u root: -XPOST "http://localhost:8000/v1/query" --header 'Content-Type: application/json' -d '{"sql": "copy /*+ set_var(enable_distributed_copy_into = 0) */ into products (id, name, description) from @s1/", "pagination": { "wait_time_secs": 6}}' -u root: | jq ".error"
