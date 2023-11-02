bccc589c58deb03d323b2d6abf90510ed462ff3d
baishen <baishen2009@gmail.com>
2023-11-02 18:13:38 +0800

    feat(query): Support flatten table function (#13520)
    * feat(query): Support flatten table function
    
    * fix

--- src/query/service/src/table_functions/srf/mod.rs
+++ src/query/service/src/table_functions/srf/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod flatten;
 mod range;
 
+pub use flatten::FlattenTable;
 pub use range::RangeTable;
