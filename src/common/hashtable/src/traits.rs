487ca743475dae5b7a5a6c07f90a43a010ebf8ac
Dousir9 <736191200@qq.com>
2023-11-03 13:02:00 +0800

    make lint

--- src/common/hashtable/src/traits.rs
+++ src/common/hashtable/src/traits.rs
@@ -20,8 +20,8 @@ use std::iter::TrustedLen;
 use std::mem::MaybeUninit;
 use std::num::NonZeroU64;
 
-use ethnum::i256;
 use common_arrow::arrow::bitmap::Bitmap;
+use ethnum::i256;
 use ethnum::U256;
 use ordered_float::OrderedFloat;
 
