9767331d7da9a6c3ce3bdbb62db67e059fbd5fd1
flaneur <me.ssword@gmail.com>
2023-11-04 13:09:41 +0800

    feat: make http_handler_result_timeout_secs a setting (#13565)
    * add http_handler_result_timeout_secs in settings
    
    * add http_handler_result_timeout_secs as settings
    
    * remove http query config
    
    * fix lint
    
    * laod the settings from conf
    
    * fix typo
    
    * add comment
    
    * fix test
    
    * fix test
    
    * set default as 60

--- src/query/service/src/servers/http/v1/query/http_query.rs
+++ src/query/service/src/servers/http/v1/query/http_query.rs
@@ -40,7 +40,6 @@ use crate::servers::http::v1::query::execute_state::ExecutorSessionState;
 use crate::servers::http::v1::query::execute_state::Progresses;
 use crate::servers::http::v1::query::expirable::Expirable;
 use crate::servers::http::v1::query::expirable::ExpiringState;
-use crate::servers::http::v1::query::http_query_manager::HttpQueryConfig;
 use crate::servers::http::v1::query::sized_spsc::sized_spsc;
 use crate::servers::http::v1::query::ExecuteState;
 use crate::servers::http::v1::query::ExecuteStateKind;
@@ -187,8 +186,11 @@ pub struct HttpQuery {
     request: HttpQueryRequest,
     state: Arc<RwLock<Executor>>,
     page_manager: Arc<TokioMutex<PageManager>>,
-    config: HttpQueryConfig,
     expire_state: Arc<TokioMutex<ExpireState>>,
+    /// The timeout for the query result polling. In the normal case, the client driver
+    /// should fetch the paginated result in a timely manner, and the interval should not
+    /// exceed this result_timeout_secs.
+    pub(crate) result_timeout_secs: u64,
 }
 
 impl HttpQuery {
@@ -197,7 +199,6 @@ impl HttpQuery {
     pub(crate) async fn try_create(
         ctx: &HttpQueryContext,
         request: HttpQueryRequest,
-        config: HttpQueryConfig,
     ) -> Result<Arc<HttpQuery>> {
         let http_query_manager = HttpQueryManager::instance();
 
@@ -234,7 +235,7 @@ impl HttpQuery {
         // the session variables includes:
         // - the current database
         // - the current role
-        // - the session-level settings, like max_threads
+        // - the session-level settings, like max_threads, http_handler_result_timeout_secs, etc.
         if let Some(session_conf) = &request.session {
             if let Some(db) = &session_conf.database {
                 session.set_current_database(db.clone());
@@ -269,6 +270,8 @@ impl HttpQuery {
             }
         };
 
+        let settings = session.get_settings();
+        let result_timeout_secs = settings.get_http_handler_result_timeout_secs()?;
         let deduplicate_label = &ctx.deduplicate_label;
         let user_agent = &ctx.user_agent;
         let query_id = ctx.query_id.clone();
@@ -372,7 +375,7 @@ impl HttpQuery {
             request,
             state,
             page_manager: data,
-            config,
+            result_timeout_secs,
             expire_state: Arc::new(TokioMutex::new(ExpireState::Working)),
         };
 
@@ -476,7 +479,7 @@ impl HttpQuery {
 
     #[async_backtrace::framed]
     pub async fn update_expire_time(&self, before_wait: bool) {
-        let duration = Duration::from_secs(self.config.result_timeout_secs)
+        let duration = Duration::from_secs(self.result_timeout_secs)
             + if before_wait {
                 Duration::from_secs(self.request.pagination.wait_time_secs as u64)
             } else {
@@ -508,7 +511,7 @@ impl HttpQuery {
             }
             ExpireState::Removed => ExpireResult::Removed,
             ExpireState::Working => {
-                ExpireResult::Sleep(Duration::from_secs(self.config.result_timeout_secs))
+                ExpireResult::Sleep(Duration::from_secs(self.result_timeout_secs))
             }
         }
     }
