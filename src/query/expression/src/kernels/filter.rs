584625077b6c3d49906f688c34e9bb167e041a71
Dousir9 <736191200@qq.com>
2023-11-05 23:21:58 +0800

    revert

--- src/query/expression/src/kernels/filter.rs
+++ src/query/expression/src/kernels/filter.rs
@@ -320,9 +320,7 @@ impl Column {
         // [`StringColumn`] consists of [`data`] and [`offset`], we build [`data`] and [`offset`] respectively,
         // and then call `StringColumn::new(data.into(), offsets.into())` to create [`StringColumn`].
         let values_offset = values.offsets().as_slice();
-        let values_data_ptr = &(values.data().as_slice())
-            [values_offset[0] as usize..values_offset[values_offset.len() - 1] as usize]
-            .as_ptr();
+        let values_data_ptr = values.data().as_slice();
         let mut offsets: Vec<u64> = Vec::with_capacity(num_rows + 1);
         let mut offsets_ptr = offsets.as_mut_ptr();
         let mut items_ptr = items.as_mut_ptr();
--- src/query/expression/src/kernels/take.rs
+++ src/query/expression/src/kernels/take.rs
@@ -227,9 +227,7 @@ impl Column {
         // [`StringColumn`] consists of [`data`] and [`offset`], we build [`data`] and [`offset`] respectively,
         // and then call `StringColumn::new(data.into(), offsets.into())` to create [`StringColumn`].
         let col_offset = col.offsets().as_slice();
-        let col_data_ptr = &(col.data().as_slice())
-            [col_offset[0] as usize..col_offset[col_offset.len() - 1] as usize]
-            .as_ptr();
+        let col_data_ptr = col.data().as_slice();
         let mut offsets: Vec<u64> = Vec::with_capacity(num_rows + 1);
         let mut data_size = 0;
 
