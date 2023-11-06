a7f8003b261376080096acc87759a957a681891b
Winter Zhang <coswde@gmail.com>
2023-11-06 20:57:01 +0800

    fix(query): fix memory leak for http kill query (#13599)

--- src/query/service/src/servers/http/v1/http_query_handlers.rs
+++ src/query/service/src/servers/http/v1/http_query_handlers.rs
@@ -264,6 +264,7 @@ async fn query_cancel_handler(
         let http_query_manager = HttpQueryManager::instance();
         match http_query_manager.get_query(&query_id).await {
             Some(query) => {
+                query.detach().await;
                 query.kill().await;
                 http_query_manager.remove_query(&query_id).await;
                 StatusCode::OK
