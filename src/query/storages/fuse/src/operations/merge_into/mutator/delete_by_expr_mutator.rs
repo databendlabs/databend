c0da15a0217b08cdaf85c3b1a69e94f2f92ac45c
JackTan25 <jacktby@gmail.com>
2023-11-08 14:42:17 +0800

    add header

--- src/query/storages/fuse/src/operations/merge_into/mutator/delete_by_expr_mutator.rs
+++ src/query/storages/fuse/src/operations/merge_into/mutator/delete_by_expr_mutator.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use common_exception::Result;
 use common_expression::eval_function;
 use common_expression::types::BooleanType;
