5274b8260f16a8f5973405ea167870ba04f40d6b
Andy Lok <andylokandy@hotmail.com>
2023-11-10 21:17:55 +0800

    improve

--- src/query/sql/src/planner/binder/delete.rs
+++ src/query/sql/src/planner/binder/delete.rs
@@ -27,16 +27,20 @@ use crate::binder::ScalarBinder;
 use crate::binder::INTERNAL_COLUMN_FACTORY;
 use crate::optimizer::SExpr;
 use crate::optimizer::SubqueryRewriter;
+use crate::plans::AggregateFunction;
 use crate::plans::DeletePlan;
 use crate::plans::Filter;
+use crate::plans::LambdaFunc;
 use crate::plans::Operator;
 use crate::plans::Plan;
 use crate::plans::RelOp;
 use crate::plans::RelOperator::Scan;
 use crate::plans::SubqueryDesc;
 use crate::plans::SubqueryExpr;
+use crate::plans::Visitor;
 use crate::BindContext;
 use crate::ScalarExpr;
+use crate::plans::WindowFunc;
 
 impl<'a> Binder {
     pub(in crate::planner::binder) async fn process_selection(
@@ -184,35 +188,39 @@ impl Binder {
         table_expr: SExpr,
         subquery_desc: &mut Vec<SubqueryDesc>,
     ) -> Result<()> {
-        match scalar {
-            ScalarExpr::FunctionCall(scalar) => {
-                for arg in scalar.arguments.iter() {
-                    self.subquery_desc(arg, table_expr.clone(), subquery_desc)
-                        .await?;
-                }
-            }
-            ScalarExpr::CastExpr(scalar) => {
-                self.subquery_desc(scalar.argument.as_ref(), table_expr, subquery_desc)
-                    .await?;
-            }
-            ScalarExpr::SubqueryExpr(subquery) => {
-                let desc = self
-                    .process_subquery(scalar, subquery, table_expr.clone())
-                    .await?;
-                subquery_desc.push(desc);
-            }
-            ScalarExpr::UDFServerCall(scalar) => {
-                for arg in scalar.arguments.iter() {
-                    self.subquery_desc(arg, table_expr.clone(), subquery_desc)
-                        .await?;
-                }
-            }
-            ScalarExpr::BoundColumnRef(_)
-            | ScalarExpr::ConstantExpr(_)
-            | ScalarExpr::WindowFunction(_)
-            | ScalarExpr::AggregateFunction(_)
-            | ScalarExpr::LambdaFunction(_) => {}
+        struct FindSubquery<'a> {
+            subqueries: Vec<&'a SubqueryExpr>,
         }
+
+        impl<'a> Visitor<'a> for FindSubquery<'a> {
+            fn visit_subquery(&mut self, subquery: &'a SubqueryExpr) -> Result<()> {
+                self.subqueries.push(subquery);
+                Ok(())
+            }
+            fn visit_window_function(&mut self, _: &'a WindowFunc) -> Result<()> {
+                // Stop finding subquery in window function.
+                Ok(())
+            }
+            fn visit_aggregate_function(&mut self, _: &'a AggregateFunction) -> Result<()> {
+                // Stop finding subquery in aggregate function.
+                Ok(())
+            }
+            fn visit_lambda_function(&mut self, _: &'a LambdaFunc) -> Result<()> {
+                // Stop finding subquery in lambda function.
+                Ok(())
+            }
+        }
+
+        let mut find_subquery = FindSubquery { subqueries: vec![] };
+        find_subquery.visit(scalar)?;
+
+        for subquery in find_subquery.subqueries {
+            let desc = self
+                .process_subquery(scalar, subquery, table_expr.clone())
+                .await?;
+            subquery_desc.push(desc);
+        }
+
         Ok(())
     }
 }
