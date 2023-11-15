51b4faac575a1258f4403966c6eeafcefcf95d9c
Li Yazhou <me.ssword@gmail.com>
2023-11-15 20:58:07 +0800

    fix test

--- tests/suites/0_stateless/18_rbac/20_0015_set_role.sh
+++ tests/suites/0_stateless/18_rbac/20_0015_set_role.sh
@@ -24,6 +24,25 @@ echo '-- grant privilege to roles'
 echo 'GRANT SELECT, INSERT ON default.t20_0015_table1 TO ROLE testrole1' | $BENDSQL_CLIENT_CONNECT
 echo 'GRANT SELECT, INSERT ON default.t20_0015_table2 TO ROLE testrole2' | $BENDSQL_CLIENT_CONNECT
 
-echo '-- test 1: set role as role1, secondary roles as NONE, can access table1, can not access table2'
+echo '-- test 1: set role as testrole1, then SELECT current_role()'
+echo "SET ROLE testrole1; SELECT current_role();" | $TEST_USER_CONNECT
+
+echo '-- test 2: set role as testrole1, secondary roles as NONE, can access table1, can not access table2'
 echo "SET ROLE testrole1; SET SECONDARY ROLES NONE; INSERT INTO t20_0015_table1 VALUES (1);" | $TEST_USER_CONNECT
-echo "SET ROLE testrole1; SET SECONDARY ROLES NONE; INSERT INTO t20_0015_table2 VALUES (1);" | $TEST_USER_CONNECT
+echo "SET ROLE testrole1; SET SECONDARY ROLES NONE; INSERT INTO t20_0015_table2 VALUES (1);" | $TEST_USER_CONNECT || true
+
+echo '-- test 3: set role as testrole2, secondary roles as NONE, can access table2, can not access table1'
+echo "SET ROLE testrole2; SET SECONDARY ROLES NONE; INSERT INTO t20_0015_table1 VALUES (1);" | $TEST_USER_CONNECT || true
+echo "SET ROLE testrole2; SET SECONDARY ROLES NONE; INSERT INTO t20_0015_table2 VALUES (1);" | $TEST_USER_CONNECT
+
+echo '-- test 4: set role as testrole1, secondary roles as ALL, can access both table1 and table2'
+echo "SET ROLE testrole1; SET SECONDARY ROLES ALL; INSERT INTO t20_0015_table1 VALUES (1);" | $TEST_USER_CONNECT
+echo "SET ROLE testrole1; SET SECONDARY ROLES ALL; INSERT INTO t20_0015_table2 VALUES (1);" | $TEST_USER_CONNECT
+
+echo '-- test 5: set role as testrole1, secondary roles defaults as ALL, can both table1 and table2'
+echo "SET ROLE testrole1; INSERT INTO t20_0015_table1 VALUES (1);" | $TEST_USER_CONNECT
+echo "SET ROLE testrole1; INSERT INTO t20_0015_table2 VALUES (1);" | $TEST_USER_CONNECT
+
+echo '-- test 6: not change role, secondary roles defaults as ALL, can both table1 and table2'
+echo "INSERT INTO t20_0015_table1 VALUES (1);" | $TEST_USER_CONNECT
+echo "INSERT INTO t20_0015_table2 VALUES (1);" | $TEST_USER_CONNECT
