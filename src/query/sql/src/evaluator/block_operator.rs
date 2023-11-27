a38cdf1b9873a3b232ed48bd4115cc7914a0dd4d
hezheyu <rinchannow@bupt.edu.cn>
2023-11-27 21:07:18 +0800

    Add a new transform `TransformSRF` to process SRFs.
    Output blocks by determined batch size.

--- src/query/sql/src/evaluator/block_operator.rs
+++ src/query/sql/src/evaluator/block_operator.rs
@@ -18,21 +18,17 @@ use common_catalog::plan::AggIndexMeta;
 use common_exception::Result;
 use common_expression::types::array::ArrayColumn;
 use common_expression::types::nullable::NullableColumn;
-use common_expression::types::nullable::NullableColumnBuilder;
 use common_expression::types::BooleanType;
 use common_expression::types::DataType;
-use common_expression::types::VariantType;
 use common_expression::BlockEntry;
 use common_expression::BlockMetaInfoDowncast;
 use common_expression::Column;
-use common_expression::ColumnBuilder;
 use common_expression::DataBlock;
 use common_expression::Evaluator;
 use common_expression::Expr;
 use common_expression::FieldIndex;
 use common_expression::FunctionContext;
 use common_expression::Scalar;
-use common_expression::ScalarRef;
 use common_expression::Value;
 use common_functions::BUILTIN_FUNCTIONS;
 use common_pipeline_core::processors::InputPort;
@@ -59,12 +55,6 @@ pub enum BlockOperator {
 
     /// Reorganize the input [`DataBlock`] with `projection`.
     Project { projection: Vec<FieldIndex> },
-
-    /// Expand the input [`DataBlock`] with set-returning functions.
-    FlatMap {
-        projections: ColumnSet,
-        srf_exprs: Vec<Expr>,
-    },
 
     /// Execute lambda function on input [`DataBlock`].
     LambdaMap { funcs: Vec<LambdaFunctionDesc> },
@@ -127,187 +117,6 @@ impl BlockOperator {
                 let mut result = DataBlock::new(vec![], input.num_rows());
                 for index in projection {
                     result.add_column(input.get_by_offset(*index).clone());
-                }
-                Ok(result)
-            }
-
-            BlockOperator::FlatMap {
-                projections,
-                srf_exprs,
-            } => {
-                let eval = Evaluator::new(&input, func_ctx, &BUILTIN_FUNCTIONS);
-
-                // [
-                //   srf1: [
-                //     result_set1: [
-                //       col1, col2, ...
-                //     ],
-                //     ...
-                //   ],
-                //   ...
-                // ]
-                let input_num_rows = input.num_rows();
-                let mut max_nums_per_row = vec![0; input_num_rows];
-                let srf_results = srf_exprs
-                    .iter()
-                    .map(|srf_expr| eval.run_srf(srf_expr, &mut max_nums_per_row))
-                    .collect::<Result<Vec<_>>>()?;
-                let mut total_num_rows = 0;
-                for max_nums in max_nums_per_row.iter().take(input_num_rows) {
-                    total_num_rows += *max_nums;
-                }
-
-                let input_num_columns = input.num_columns();
-                let mut result = DataBlock::empty();
-                let mut block_is_empty = true;
-                for index in 0..input_num_columns {
-                    if !projections.contains(&index) {
-                        continue;
-                    }
-                    let column = input.get_by_offset(index);
-                    let mut builder =
-                        ColumnBuilder::with_capacity(&column.data_type, total_num_rows);
-                    for (i, max_nums) in max_nums_per_row.iter().take(input_num_rows).enumerate() {
-                        let scalar_ref = unsafe { column.value.index_unchecked(i) };
-                        for _ in 0..*max_nums {
-                            builder.push(scalar_ref.clone());
-                        }
-                    }
-                    let block_entry =
-                        BlockEntry::new(column.data_type.clone(), Value::Column(builder.build()));
-                    if block_is_empty {
-                        result = DataBlock::new(vec![block_entry], total_num_rows);
-                        block_is_empty = false;
-                    } else {
-                        result.add_column(block_entry);
-                    }
-                }
-
-                for (srf_expr, srf_results) in srf_exprs.iter().zip(srf_results) {
-                    if let Expr::FunctionCall { function, .. } = srf_expr {
-                        match function.signature.name.as_str() {
-                            "json_path_query" => {
-                                let mut builder: NullableColumnBuilder<VariantType> =
-                                    NullableColumnBuilder::with_capacity(total_num_rows, &[]);
-                                for (i, (row_result, repeat_times)) in
-                                    srf_results.into_iter().enumerate()
-                                {
-                                    if let Value::Column(Column::Tuple(fields)) = row_result {
-                                        debug_assert!(fields.len() == 1);
-                                        match &fields[0] {
-                                            Column::Nullable(box nullable_column) => {
-                                                match &nullable_column.column {
-                                                    Column::Variant(string_column) => {
-                                                        for idx in 0..repeat_times {
-                                                            builder.push(unsafe {
-                                                                string_column.index_unchecked(idx)
-                                                            });
-                                                        }
-                                                        for _ in
-                                                            0..(max_nums_per_row[i] - repeat_times)
-                                                        {
-                                                            builder.push_null();
-                                                        }
-                                                    }
-                                                    _ => unreachable!(
-                                                        "json_path_query's return type is: `DataType::Tuple(vec![DataType::Nullable(Box::new(DataType::Variant))])`"
-                                                    ),
-                                                }
-                                            }
-                                            _ => unreachable!(
-                                                "json_path_query's return type is: `DataType::Tuple(vec![DataType::Nullable(Box::new(DataType::Variant))])`"
-                                            ),
-                                        };
-                                    }
-                                }
-                                let column = builder.build().upcast();
-                                let block_entry = BlockEntry::new(
-                                    DataType::Tuple(vec![DataType::Nullable(Box::new(
-                                        DataType::Variant,
-                                    ))]),
-                                    Value::Column(Column::Tuple(vec![Column::Nullable(Box::new(
-                                        column,
-                                    ))])),
-                                );
-                                if block_is_empty {
-                                    result = DataBlock::new(vec![block_entry], total_num_rows);
-                                    block_is_empty = false;
-                                } else {
-                                    result.add_column(block_entry);
-                                }
-                            }
-                            _ => {
-                                let mut result_data_blocks = Vec::with_capacity(input.num_rows());
-                                for (i, (mut row_result, repeat_times)) in
-                                    srf_results.into_iter().enumerate()
-                                {
-                                    if let Value::Column(Column::Tuple(fields)) = &mut row_result {
-                                        // If the current result set has less rows than the max number of rows,
-                                        // we need to pad the result set with null values.
-                                        // TODO(leiysky): this can be optimized by using a `zip` array function
-                                        if repeat_times < max_nums_per_row[i] {
-                                            for field in fields {
-                                                match field {
-                                                    Column::Null { .. } => {
-                                                        *field = ColumnBuilder::repeat(
-                                                            &ScalarRef::Null,
-                                                            max_nums_per_row[i],
-                                                            &DataType::Null,
-                                                        )
-                                                        .build();
-                                                    }
-                                                    Column::Nullable(box nullable_column) => {
-                                                        let mut column_builder =
-                                                            NullableColumnBuilder::from_column(
-                                                                (*nullable_column).clone(),
-                                                            );
-                                                        (0..(max_nums_per_row[i] - repeat_times))
-                                                            .for_each(|_| {
-                                                                column_builder.push_null();
-                                                            });
-                                                        *field = Column::Nullable(Box::new(
-                                                            column_builder.build(),
-                                                        ));
-                                                    }
-                                                    _ => unreachable!(),
-                                                }
-                                            }
-                                        }
-                                    } else {
-                                        row_result = Value::Column(
-                                            ColumnBuilder::repeat(
-                                                &ScalarRef::Tuple(vec![ScalarRef::Null]),
-                                                max_nums_per_row[i],
-                                                srf_expr.data_type(),
-                                            )
-                                            .build(),
-                                        );
-                                    }
-
-                                    let block_entry =
-                                        BlockEntry::new(srf_expr.data_type().clone(), row_result);
-                                    result_data_blocks.push(DataBlock::new(
-                                        vec![block_entry],
-                                        max_nums_per_row[i],
-                                    ))
-                                }
-                                let data_block = DataBlock::concat(&result_data_blocks)?;
-                                debug_assert!(data_block.num_rows() == total_num_rows);
-                                let block_entry = BlockEntry::new(
-                                    data_block.get_by_offset(0).data_type.clone(),
-                                    data_block.get_by_offset(0).value.clone(),
-                                );
-                                if block_is_empty {
-                                    result = DataBlock::new(vec![block_entry], total_num_rows);
-                                    block_is_empty = false;
-                                } else {
-                                    result.add_column(block_entry);
-                                }
-                            }
-                        }
-                    } else {
-                        unreachable!("expr is not a set returning function: {srf_expr}");
-                    }
                 }
                 Ok(result)
             }
@@ -522,7 +331,6 @@ impl Transform for CompoundBlockOperator {
                         BlockOperator::Map { .. } => "Map",
                         BlockOperator::Filter { .. } => "Filter",
                         BlockOperator::Project { .. } => "Project",
-                        BlockOperator::FlatMap { .. } => "FlatMap",
                         BlockOperator::LambdaMap { .. } => "LambdaMap",
                     }
                     .to_string()
