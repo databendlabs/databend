775d2b4dbb5b901d50d8c4f39a57afe8b1874eaa
JackTan25 <jacktby@gmail.com>
2023-12-01 00:42:02 +0800

    revert copy

--- src/query/ast/src/visitors/visitor.rs
+++ src/query/ast/src/visitors/visitor.rs
@@ -378,12 +378,12 @@ pub trait Visitor<'ast>: Sized {
 
     fn visit_explain(&mut self, _kind: &'ast ExplainKind, _query: &'ast Statement) {}
 
-    fn visit_copy_into_table(&mut self, _copy: &'ast CopyIntoTableStmt) {
+    fn visit_copy_into_table(&mut self, copy: &'ast CopyIntoTableStmt) {
         if let CopyIntoTableSource::Query(query) = &copy.src {
             self.visit_query(query)
         }
     }
-    fn visit_copy_into_location(&mut self, _copy: &'ast CopyIntoLocationStmt) {
+    fn visit_copy_into_location(&mut self, copy: &'ast CopyIntoLocationStmt) {
         if let CopyIntoLocationSource::Query(query) = &copy.src {
             self.visit_query(query)
         }
