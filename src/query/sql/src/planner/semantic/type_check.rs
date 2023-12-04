d65b0fcefd030c461dc849aace56aaf3b5e6fb94
baishen <baishen2009@gmail.com>
2023-12-04 22:37:59 +0800

    fix

--- src/query/sql/src/planner/semantic/type_check.rs
+++ src/query/sql/src/planner/semantic/type_check.rs
@@ -2988,24 +2988,22 @@ impl<'a> TypeChecker<'a> {
         let mut table_data_type = infer_schema_type(&data_type)?;
         // If it is a tuple column, convert it to the internal column specified by the paths.
         // For other types of columns, convert it to get functions.
-        if let TableDataType::Tuple { .. } = table_data_type.remove_nullable() {
-            if let ScalarExpr::BoundColumnRef(BoundColumnRef { ref column, .. }) = scalar {
-                let column_entry = self.metadata.read().column(column.index).clone();
-                if let ColumnEntry::BaseTableColumn(BaseTableColumn { ref data_type, .. }) =
-                    column_entry
-                {
-                    table_data_type = data_type.clone();
-                    if let TableDataType::Tuple { .. } = table_data_type.remove_nullable() {
-                        let box (inner_scalar, _inner_data_type) = self
-                            .resolve_tuple_map_access_pushdown(
-                                expr.span(),
-                                column.clone(),
-                                &mut table_data_type,
-                                &mut paths,
-                            )
-                            .await?;
-                        scalar = inner_scalar;
-                    }
+        if let ScalarExpr::BoundColumnRef(BoundColumnRef { ref column, .. }) = scalar {
+            let column_entry = self.metadata.read().column(column.index).clone();
+            if let ColumnEntry::BaseTableColumn(BaseTableColumn { ref data_type, .. }) =
+                column_entry
+            {
+                table_data_type = data_type.clone();
+                if let TableDataType::Tuple { .. } = table_data_type.remove_nullable() {
+                    let box (inner_scalar, _inner_data_type) = self
+                        .resolve_tuple_map_access_pushdown(
+                            expr.span(),
+                            column.clone(),
+                            &mut table_data_type,
+                            &mut paths,
+                        )
+                        .await?;
+                    scalar = inner_scalar;
                 }
             }
         }
