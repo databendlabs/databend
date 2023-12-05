f9bffb4e6b76b8be058c3535b54a7a34ecf3b037
guojidan <1948535941@qq.com>
2023-12-05 10:48:24 +0000

    fix

--- src/meta/app/src/principal/connection.rs
+++ src/meta/app/src/principal/connection.rs
@@ -43,12 +43,7 @@ impl UserDefinedConnection {
     pub fn storage_params_display(&self) -> String {
         self.storage_params
             .iter()
-            .map(|(k, v)| {
-                if k.to_lowercase() == "access_key_id" || k.to_lowercase() == "secret_access_key" {
-                    return format!("{}={}", k, mask_string(v, 3));
-                }
-                format!("{}={}", k, v)
-            })
+            .map(|(k, v)| format!("{}={}", k, v))
             .join(" ")
     }
 }
