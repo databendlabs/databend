409baaba84d9d11300f76cfb7cfcad4b2a9140e3
JackTan25 <jacktby@gmail.com>
2023-12-07 01:51:42 +0800

    fix test

--- tests/suites/0_stateless/14_clickhouse_http_handler/14_0007_http_clickhouse_input_format_diagnostic.sh
+++ tests/suites/0_stateless/14_clickhouse_http_handler/14_0007_http_clickhouse_input_format_diagnostic.sh
@@ -5,10 +5,24 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/" -d "drop table if exists a"
 curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/" -d "create table a ( a int not null, b string not null, c int not null)"
+curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/" -d "create table b ( a int not null, b string not null, c int)"
+
 
 cat << EOF > /tmp/databend_test_csv_error.txt
 insert into a(a,b) format CSV 1,"Hello",1
 
 EOF
+
 curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/" --data-binary @/tmp/databend_test_csv_error.txt
+
+cat << EOF > /tmp/databend_test_csv_error2.txt
+insert into b(a,b) format CSV 1,"Hello",1
+
+EOF
+
+echo ""
+curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/" --data-binary @/tmp/databend_test_csv_error2.txt
+
 curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/" -d "drop table if exists a"
+curl -s -u 'root:' -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/" -d "drop table if exists b"
+echo ""
