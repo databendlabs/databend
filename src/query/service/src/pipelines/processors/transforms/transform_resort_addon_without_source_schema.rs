5af69ad2e4fe6c187e45a0d7356bf739fa4b00b3
JackTan25 <jacktby@gmail.com>
2023-12-07 16:04:08 +0800

    fix check

--- src/query/service/src/pipelines/processors/transforms/transform_resort_addon_without_source_schema.rs
+++ src/query/service/src/pipelines/processors/transforms/transform_resort_addon_without_source_schema.rs
@@ -17,7 +17,6 @@ use std::sync::Arc;
 use common_catalog::table_context::TableContext;
 use common_exception::ErrorCode;
 use common_exception::Result;
-use common_expression::types::DataType;
 use common_expression::BlockMetaInfoDowncast;
 use common_expression::DataBlock;
 use common_expression::DataSchemaRef;
