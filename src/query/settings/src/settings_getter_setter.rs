0dc4fa6c7f60a62b6584a0dd544807788f1d25aa
zhang2014 <coswde@gmail.com>
2023-12-08 10:02:45 +0800

    fix(query): ignore ascii case for flight compression setting

--- src/query/settings/src/settings_getter_setter.rs
+++ src/query/settings/src/settings_getter_setter.rs
@@ -503,8 +503,12 @@ impl Settings {
     }
 
     pub fn get_query_flight_compression(&self) -> Result<Option<FlightCompression>> {
-        match self.try_get_string("query_flight_compression")?.as_str() {
-            "None" => Ok(None),
+        match self
+            .try_get_string("query_flight_compression")?
+            .to_uppercase()
+            .as_str()
+        {
+            "NONE" => Ok(None),
             "LZ4" => Ok(Some(FlightCompression::Lz4)),
             "ZSTD" => Ok(Some(FlightCompression::Zstd)),
             _ => unreachable!("check possible_values in set variable"),
