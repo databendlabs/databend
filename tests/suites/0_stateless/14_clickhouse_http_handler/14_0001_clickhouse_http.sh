ee6c715d83f2664a96e5b116abbba5807fac9c7f
guojidan <1948535941@qq.com>
2023-12-12 02:25:15 +0000

    fix test case

--- tests/suites/0_stateless/14_clickhouse_http_handler/14_0001_clickhouse_http.sh
+++ tests/suites/0_stateless/14_clickhouse_http_handler/14_0001_clickhouse_http.sh
@@ -3,11 +3,11 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../../../shell_env.sh
 
-curl -s -u root: -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}" -d 'select 1'
+curl -s -u root: -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}?enable_clickhouse_handler=1" -d 'select 1'
 
-curl -s -u root: -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/" -d 'select 1'
+curl -s -u root: -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/?enable_clickhouse_handler=1" -d 'select 1'
 
-curl -s -u root: -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}//" -d 'select 1'
+curl -s -u root: -XPOST "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}//?enable_clickhouse_handler=1" -d 'select 1'
 
 curl -s -u root: "http://localhost:${QUERY_CLICKHOUSE_HTTP_HANDLER_PORT}/ping"
 
