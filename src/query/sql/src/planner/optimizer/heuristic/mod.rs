568f83d108ea8f9a16d60b4927915412aa0cdb86
leiysky <leiysky@outlook.com>
2023-12-14 17:26:51 +0800

    refactor heuristic optimizer

--- src/query/sql/src/planner/optimizer/heuristic/mod.rs
+++ src/query/sql/src/planner/optimizer/heuristic/mod.rs
@@ -17,6 +17,7 @@ mod decorrelate;
 mod heuristic;
 mod subquery_rewriter;
 
+pub use decorrelate::decorrelate_subquery;
 pub use heuristic::HeuristicOptimizer;
 pub use heuristic::DEFAULT_REWRITE_RULES;
 pub use heuristic::RESIDUAL_RULES;
