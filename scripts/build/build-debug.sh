83e729e522e2b1727eb07bb7e124bbf63ef38574
everpcpc <git@everpcpc.com>
2023-12-15 09:38:28 +0800

    fix: add test for proto change

--- scripts/build/build-debug.sh
+++ scripts/build/build-debug.sh
@@ -10,10 +10,10 @@ cd "$SCRIPT_PATH/../.." || exit
 echo "Build(DEBUG) start..."
 
 if [[ $(uname -a) =~ Darwin ]]; then
-    export CMAKE_CC_COMPILER=/opt/homebrew/opt/llvm/bin/clang
-    export CMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++
-    export JEMALLOC_SYS_WITH_LG_PAGE=14
-    export JEMALLOC_SYS_WITH_MALLOC_CONF=oversize_threshold:0,dirty_decay_ms:5000,muzzy_decay_ms:5000
+	export CMAKE_CC_COMPILER=/opt/homebrew/opt/llvm/bin/clang
+	export CMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++
+	export JEMALLOC_SYS_WITH_LG_PAGE=14
+	export JEMALLOC_SYS_WITH_MALLOC_CONF=oversize_threshold:0,dirty_decay_ms:5000,muzzy_decay_ms:5000
 fi
 
 cargo build --bin=databend-query --bin=databend-meta --bin=databend-metactl --bin=databend-sqllogictests --bin=databend-sqlsmith
