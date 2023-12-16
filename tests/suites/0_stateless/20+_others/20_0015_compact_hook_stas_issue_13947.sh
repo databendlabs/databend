ef2ad5462a5f25c31a0c29d4100f7c93dc9207c3
dantengsky <dantengsky@gmail.com>
2023-12-16 08:43:06 +0800

    fix:  incorrect stats when auto-compaction is hooked (#14042)
    fix: reset context progress value after compact hook

---
+++ tests/suites/0_stateless/20+_others/20_0015_compact_hook_stas_issue_13947.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. "$CURDIR"/../../../shell_env.sh
+
+# set up
+cat <<EOF |  $BENDSQL_CLIENT_CONNECT
+create database i13947;
+use i13947;
+create stage test_stage;
+create table tmp(id int);
+insert into tmp values(1);
+insert into tmp values(2);
+copy into @test_stage from (select * from tmp);
+EOF
+
+
+# It is not convenient to extract the .stats.write_progress.rows from the output of bendsql,
+# thus, curl is used. To prevent the immature result returned by curl(which will not poll the result until the query is finished),
+# pagination.wait_time_secs is set to 6 seconds.
+
+# since 2 rows will be copied into tmp table from stage
+echo "expects .stats.write_progress.rows be 2"
+echo "expects .error be null"
+curl -s -u root: -XPOST "http://localhost:8000/v1/query" \
+  --header 'Content-Type: application/json'              \
+  -d '{"sql": "copy into i13947.tmp from (select * from @test_stage)", "pagination": { "wait_time_secs": 6}}'  \
+  | jq -r '.stats.write_progress.rows, .error'
+
+echo "DROP database i13947;" | $BENDSQL_CLIENT_CONNECT
