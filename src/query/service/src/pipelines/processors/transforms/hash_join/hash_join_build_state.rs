70bc9e03e1de542d47dd590cf1fe6f7069b0f559
xudong963 <wxd963996380@gmail.com>
2023-12-22 00:26:37 +0800

    fix lint

--- src/query/service/src/pipelines/processors/transforms/hash_join/hash_join_build_state.rs
+++ src/query/service/src/pipelines/processors/transforms/hash_join/hash_join_build_state.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
