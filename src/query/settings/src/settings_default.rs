da8a4190d32732046313ba03e7dacf5f39f60ce7
BohuTANG <overred.shuttler@gmail.com>
2023-12-21 10:39:47 +0800

    chore: add more comments for auto compact and remove the settings table goldenfile test (#14101)

--- src/query/settings/src/settings_default.rs
+++ src/query/settings/src/settings_default.rs
@@ -492,7 +492,7 @@ impl DefaultSettings {
                 }),
                 ("auto_compaction_threshold", DefaultSettingValue {
                     value: UserSettingValue::UInt64(1000),
-                    desc: "Threshold for triggering auto compaction after write(copy/insert).",
+                    desc: "Threshold for triggering auto compaction. This occurs when the number of imperfect blocks in a snapshot exceeds this value after write (copy/insert) operations.",
                     mode: SettingMode::Both,
                     range: None,
                 }),
