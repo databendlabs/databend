a5f7cad24d575b982f17a3ca5ee4e645d1b4ccf3
Li Yazhou <me.ssword@gmail.com>
2023-12-27 17:24:05 +0800

    add stateless tests

--- tests/suites/0_stateless/18_rbac/20_0016_rewrite_statements.sh
+++ tests/suites/0_stateless/18_rbac/20_0016_rewrite_statements.sh
@@ -11,13 +11,13 @@ echo "DROP USER IF EXISTS 'testuser1'" | $BENDSQL_CLIENT_CONNECT
 
 echo '-- prepare user and tables for tests'
 echo "CREATE USER 'testuser1' IDENTIFIED BY '$TEST_USER_PASSWORD'" | $BENDSQL_CLIENT_CONNECT
-echo "CREATE TABLE t20_0016_table1(c int not null)" | $BENDSQL_CLIENT_CONNECT
+echo "CREATE TABLE IF NOT EXISTS t20_0016_table1(c int not null)" | $BENDSQL_CLIENT_CONNECT
 echo "GRANT SELECT ON default.t20_0016_table1 TO testuser1" | $BENDSQL_CLIENT_CONNECT
 
 echo '-- ensure the statements not break with PUBLIC role'
-echo "SHOW TABLES;" | $TEST_USER_CONNECT
-echo "SHOW DATABASES;" | $TEST_USER_CONNECT
-echo "SHOW USERS;" | $TEST_USER_CONNECT
-echo "SHOW ROLES;" | $TEST_USER_CONNECT
-echo "SHOW STAGES;" | $TEST_USER_CONNECT
-echo "SHOW PROCESSLIST;" | $TEST_USER_CONNECT
+echo "SHOW TABLES;" | $TEST_USER_CONNECT > /dev/null
+echo "SHOW DATABASES;" | $TEST_USER_CONNECT > /dev/null
+echo "SHOW USERS;" | $TEST_USER_CONNECT > /dev/null
+echo "SHOW ROLES;" | $TEST_USER_CONNECT > /dev/null
+echo "SHOW STAGES;" | $TEST_USER_CONNECT > /dev/null
+echo "SHOW PROCESSLIST;" | $TEST_USER_CONNECT > /dev/null
