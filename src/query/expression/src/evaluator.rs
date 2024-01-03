807c64bb616d3485684b0448a6f6db4a16610954
Dousir9 <736191200@qq.com>
2024-01-03 21:56:25 +0800

    merge

--- src/query/expression/src/evaluator.rs
+++ src/query/expression/src/evaluator.rs
@@ -1046,7 +1046,7 @@ impl<'a> Evaluator<'a> {
             let evaluator = Evaluator::new(&block, self.func_ctx, self.fn_registry);
             let result = evaluator.run(expr)?;
             arg0 = self
-                .run_cast(None, expr.data_type(), &col_type, result, None)?
+                .run_cast(None, expr.data_type(), &col_type, result, None, None)?
                 .as_scalar()
                 .unwrap()
                 .clone();
