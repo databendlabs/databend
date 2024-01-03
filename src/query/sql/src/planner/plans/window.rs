b436baffe939730c01c125101188f561445ecbee
lichuang <lichuang1982@gmail.com>
2024-01-03 17:26:33 +0800

    feat: push down filter in window function, fix by review comment

--- src/query/sql/src/planner/plans/window.rs
+++ src/query/sql/src/planner/plans/window.rs
@@ -91,7 +91,7 @@ impl Window {
         Ok(used_columns)
     }
 
-    // `Window.group_columns` used in `RulePushDownFilterWindow` only consider `partition_by` field,
+    // `Window.partition_by_columns` used in `RulePushDownFilterWindow` only consider `partition_by` field,
     // like `Aggregate.group_columns` only consider `group_items` field.
     pub fn partition_by_columns(&self) -> Result<ColumnSet> {
         let mut col_set = ColumnSet::new();
