3440cea2472f1a9258283a855cbcb0344df26844
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-01-16 13:26:14 +0800

    refactor file format options.
    breaking changes for newly added feature, for stricter type checking: databend will by default report error when trying to load csv empty or json null to not nullable columns.
    
    1. change default of CSV EMPTY_FIELD_AS from FIELD_DEFAULT to NULL.
    2. change default of CSV NULL_FIELD_AS from FIELD_DEFAULT to NULL.
    
    other refinement:
    
    1. prefer ident to string literal for values of EMPTY_FIELD_AS | NULL_FIELD_AS | MISSING_FIELD_AS.
    2. Fix Display of FileFormatParams.
    3. test compatibility for CsvFileFormatParams instead of FileFormatParam.
    3. refine tests and error messages.

--- tests/suites/1_stateful/00_stage/00_0014_parquet_missing_field.sh
+++ tests/suites/1_stateful/00_stage/00_0014_parquet_missing_field.sh
@@ -15,8 +15,10 @@ stmt "ALTER TABLE ontime ADD COLUMN new_col int NOT NULL DEFAULT 3;"
 comment "create stage data..."
 echo "create stage data url='fs://$TESTS_DATA_DIR/';" |  $BENDSQL_CLIENT_CONNECT
 
-query "copy into ontime from @data/ontime_200.parquet FILE_FORMAT = (type = parquet  missing_field_as = 'field_default');"
+query "copy into ontime from @data/ontime_200.parquet FILE_FORMAT = (type = parquet  missing_field_as = field_default);"
 query " select tail_number, new_col from ontime where dayofmonth=1 order by new_col"
+
+query "copy into ontime from @data/ontime_200.parquet FILE_FORMAT = (type = parquet  missing_field_as = 'field_default') force=true;"
 
 stmt "DROP TABLE IF EXISTS ontime"
 stmt "DROP STAGE IF EXISTS data"
