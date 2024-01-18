401bc6db1b4e533b2f6c48a8cd008be55c7047c2
sundy-li <543950155@qq.com>
2024-01-18 19:02:06 +0800

    chore(query): remove h2 crate

--- src/query/service/src/api/rpc/flight_client.rs
+++ src/query/service/src/api/rpc/flight_client.rs
@@ -283,28 +283,3 @@ impl FlightExchange {
         }
     }
 }
-
-#[allow(dead_code)]
-fn match_for_io_error(err_status: &Status) -> Option<&std::io::Error> {
-    let mut err: &(dyn Error + 'static) = err_status;
-
-    loop {
-        if let Some(io_err) = err.downcast_ref::<std::io::Error>() {
-            return Some(io_err);
-        }
-
-        // h2::Error do not expose std::io::Error with `source()`
-        // https://github.com/hyperium/h2/pull/462
-        use h2::Error as h2Error;
-        if let Some(h2_err) = err.downcast_ref::<h2Error>() {
-            if let Some(io_err) = h2_err.get_io() {
-                return Some(io_err);
-            }
-        }
-
-        err = match err.source() {
-            Some(err) => err,
-            None => return None,
-        };
-    }
-}
