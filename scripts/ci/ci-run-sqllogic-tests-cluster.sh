4ddb85782544e2716c5d817cd394a8d0cf05fbfc
sky <3374614481@qq.com>
2024-01-18 09:27:15 +0800

    fix explain

--- scripts/ci/ci-run-sqllogic-tests-cluster.sh
+++ scripts/ci/ci-run-sqllogic-tests-cluster.sh
@@ -21,4 +21,4 @@ fi
 echo "Run suites using argument: $RUN_DIR"
 
 echo "Starting databend-sqllogic tests"
-target/${BUILD_PROFILE}/databend-sqllogictests --handlers ${TEST_HANDLERS} ${RUN_DIR} --enable_sandbox --parallel 8 --skip_file tpcds_q64.test,tpcds_join_order.test
+target/${BUILD_PROFILE}/databend-sqllogictests --handlers ${TEST_HANDLERS} ${RUN_DIR} --enable_sandbox --parallel 8 --skip_file tpcds_q64.test,tpcds_join_order.test --no-fail-fast
