8e0dd58df02642992d9962209d5c4ecf5c86ef34
zhang2014 <coswde@gmail.com>
2024-01-19 10:12:00 +0800

    refactor(executor): refactor processor profiling

--- src/query/pipeline/core/src/lib.rs
+++ src/query/pipeline/core/src/lib.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #![feature(once_cell_try)]
 #![feature(variant_count)]
 #![allow(clippy::arc_with_non_send_sync)]
