0162b1b437ff2a70f24076d71779e6153cbf16ac
Chojan Shang <psiace@apache.org>
2024-01-23 00:10:52 +0800

    chore: remove wrong keywords
    Signed-off-by: Chojan Shang <psiace@apache.org>

--- src/query/ast/src/parser/token.rs
+++ src/query/ast/src/parser/token.rs
@@ -1093,10 +1093,6 @@ pub enum TokenKind {
     RANDOM,
     #[token("IFNULL", ignore(ascii_case))]
     IFNULL,
-    #[token("NVL", ignore(ascii_case))]
-    NVL,
-    #[token("NVL2", ignore(ascii_case))]
-    NVL2,
     #[token("NULLS", ignore(ascii_case))]
     NULLS,
     #[token("FIRST", ignore(ascii_case))]
