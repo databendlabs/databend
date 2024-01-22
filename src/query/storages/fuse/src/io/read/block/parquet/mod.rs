42027b04cee1adf059febac553ba76276c063fb6
sky <3374614481@qq.com>
2024-01-22 23:22:54 +0800

    respect data cache

--- src/query/storages/fuse/src/io/read/block/parquet/mod.rs
+++ src/query/storages/fuse/src/io/read/block/parquet/mod.rs
@@ -13,13 +13,20 @@
 // limitations under the License.
 
 use std::collections::HashMap;
+use std::sync::Arc;
 
+use arrow_array::ArrayRef;
+use arrow_array::RecordBatch;
 use arrow_array::StructArray;
+use databend_common_exception::ErrorCode;
 use databend_common_expression::BlockEntry;
 use databend_common_expression::Column;
 use databend_common_expression::ColumnId;
 use databend_common_expression::DataBlock;
 use databend_common_expression::Value;
+use databend_storages_common_cache::CacheAccessor;
+use databend_storages_common_cache::TableDataCacheKey;
+use databend_storages_common_cache_manager::CacheManager;
 use databend_storages_common_table_meta::meta::ColumnMeta;
 use databend_storages_common_table_meta::meta::Compression;
 
@@ -33,10 +40,10 @@ impl BlockReader {
     pub(crate) fn deserialize_column_chunks_1(
         &self,
         num_rows: usize,
-        _column_metas: &HashMap<ColumnId, ColumnMeta>,
+        column_metas: &HashMap<ColumnId, ColumnMeta>,
         column_chunks: HashMap<ColumnId, DataItem>,
         compression: &Compression,
-        _block_path: &str,
+        block_path: &str,
     ) -> databend_common_exception::Result<DataBlock> {
         if column_chunks.is_empty() {
             return self.build_default_values_block(num_rows);
@@ -50,23 +57,37 @@ impl BlockReader {
         let mut columns = Vec::with_capacity(self.projected_schema.fields.len());
         for (i, field) in self.projected_schema.fields.iter().enumerate() {
             let data_type = field.data_type().into();
+            let leaf_column_ids = field.leaf_column_ids();
+            if leaf_column_ids.len() > 1 {
+                if leaf_column_ids
+                    .iter()
+                    .any(|id| matches!(column_chunks.get(id), Some(DataItem::ColumnArray(_))))
+                {
+                    return Err(ErrorCode::StorageOther(
+                        "unexpected nested field: nested leaf field hits cached",
+                    ));
+                }
+                let arrow_array = column_by_name(&record_batch, &field.name);
+                let arrow2_array: Box<dyn databend_common_arrow::arrow::array::Array> =
+                    arrow_array.into();
+                let value = Value::Column(Column::from_arrow(arrow2_array.as_ref(), &data_type)?);
+                columns.push(BlockEntry::new(data_type, value));
+                continue;
+            }
             let value = match column_chunks.get(&field.column_id) {
-                Some(DataItem::RawData(_)) => {
-                    let arrow_array = record_batch
-                        .column_by_name(&field.name)
-                        .cloned()
-                        .unwrap_or_else(|| {
-                            let names = field.name.split(':').collect::<Vec<_>>();
-                            let mut array = record_batch.column_by_name(names[0]).unwrap().clone();
-                            for name in &names[1..] {
-                                let struct_array =
-                                    array.as_any().downcast_ref::<StructArray>().unwrap();
-                                array = struct_array.column_by_name(name).unwrap().clone();
-                            }
-                            array
-                        });
+                Some(DataItem::RawData(data)) => {
+                    let arrow_array = column_by_name(&record_batch, &field.name);
                     let arrow2_array: Box<dyn databend_common_arrow::arrow::array::Array> =
                         arrow_array.into();
+                    if self.put_cache {
+                        if let Some(cache) = CacheManager::instance().get_table_data_array_cache() {
+                            let meta = column_metas.get(&field.column_id).unwrap();
+                            let (offset, len) = meta.offset_length();
+                            let key =
+                                TableDataCacheKey::new(block_path, field.column_id, offset, len);
+                            cache.put(key.into(), Arc::new((arrow2_array.clone(), data.len())))
+                        }
+                    }
                     Value::Column(Column::from_arrow(arrow2_array.as_ref(), &data_type)?)
                 }
                 Some(DataItem::ColumnArray(cached)) => {
@@ -78,4 +99,20 @@ impl BlockReader {
         }
         Ok(DataBlock::new(columns, num_rows))
     }
+}
+
+fn column_by_name(record_batch: &RecordBatch, name: &str) -> ArrayRef {
+    let arrow_array = record_batch
+        .column_by_name(name)
+        .cloned()
+        .unwrap_or_else(|| {
+            let names = name.split(':').collect::<Vec<_>>();
+            let mut array = record_batch.column_by_name(names[0]).unwrap().clone();
+            for name in &names[1..] {
+                let struct_array = array.as_any().downcast_ref::<StructArray>().unwrap();
+                array = struct_array.column_by_name(name).unwrap().clone();
+            }
+            array
+        });
+    arrow_array
 }
