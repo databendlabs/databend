aed1113b16a4f346e56a9c0b3110de9933563c87
Chojan Shang <psiace@apache.org>
2024-01-25 16:58:15 +0800

    refactor: remove udf from system functions table
    Signed-off-by: Chojan Shang <psiace@apache.org>

--- src/query/storages/system/src/functions_table.rs
+++ src/query/storages/system/src/functions_table.rs
@@ -69,86 +69,28 @@ impl AsyncSystemTable for FunctionsTable {
         scalar_func_names.sort();
         let aggregate_function_factory = AggregateFunctionFactory::instance();
         let aggr_func_names = aggregate_function_factory.registered_names();
-        let enable_experimental_rbac_check =
-            ctx.get_settings().get_enable_experimental_rbac_check()?;
-        let udfs = if enable_experimental_rbac_check {
-            let visibility_checker = ctx.get_visibility_checker().await?;
-            let udfs = FunctionsTable::get_udfs(ctx).await?;
-            udfs.into_iter()
-                .filter(|udf| visibility_checker.check_udf_visibility(&udf.name))
-                .collect::<Vec<_>>()
-        } else {
-            FunctionsTable::get_udfs(ctx).await?
-        };
 
         let names: Vec<&str> = scalar_func_names
             .iter()
             .chain(&aggr_func_names)
-            .chain(udfs.iter().map(|udf| &udf.name))
             .map(|x| x.as_str())
             .collect();
 
-        let builtin_func_len = scalar_func_names.len() + aggr_func_names.len();
-
-        let is_builtin = (0..names.len())
-            .map(|i| i < builtin_func_len)
-            .collect::<Vec<bool>>();
-
         let is_aggregate = (0..names.len())
-            .map(|i| i >= scalar_func_names.len() && i < builtin_func_len)
+            .map(|i| i >= scalar_func_names.len())
             .collect::<Vec<bool>>();
 
-        let definitions = (0..names.len())
-            .map(|i| {
-                if i < builtin_func_len {
-                    "".to_string()
-                } else {
-                    udfs.get(i - builtin_func_len)
-                        .map_or("".to_string(), |udf| udf.definition.to_string())
-                }
-            })
-            .collect::<Vec<String>>();
+        let descriptions = (0..names.len()).map(|_| "").collect::<Vec<&str>>();
 
-        let categories = (0..names.len())
-            .map(|i| if i < builtin_func_len { "" } else { "UDF" })
-            .collect::<Vec<&str>>();
-
-        let descriptions = (0..names.len())
-            .map(|i| {
-                if i < builtin_func_len {
-                    ""
-                } else {
-                    udfs.get(i - builtin_func_len)
-                        .map_or("", |udf| udf.description.as_str())
-                }
-            })
-            .collect::<Vec<&str>>();
-
-        let syntaxes = (0..names.len())
-            .map(|i| {
-                if i < builtin_func_len {
-                    "".to_string()
-                } else {
-                    udfs.get(i - builtin_func_len)
-                        .map_or("".to_string(), |udf| udf.definition.to_string())
-                }
-            })
-            .collect::<Vec<String>>();
+        let syntaxes = (0..names.len()).map(|_| "").collect::<Vec<&str>>();
 
         let examples = (0..names.len()).map(|_| "").collect::<Vec<&str>>();
+
         Ok(DataBlock::new_from_columns(vec![
             StringType::from_data(names),
-            BooleanType::from_data(is_builtin),
             BooleanType::from_data(is_aggregate),
-            StringType::from_data(
-                definitions
-                    .iter()
-                    .map(String::as_str)
-                    .collect::<Vec<&str>>(),
-            ),
-            StringType::from_data(categories),
             StringType::from_data(descriptions),
-            StringType::from_data(syntaxes.iter().map(String::as_str).collect::<Vec<&str>>()),
+            StringType::from_data(syntaxes),
             StringType::from_data(examples),
         ]))
     }
@@ -158,10 +100,7 @@ impl FunctionsTable {
     pub fn create(table_id: u64) -> Arc<dyn Table> {
         let schema = TableSchemaRefExt::create(vec![
             TableField::new("name", TableDataType::String),
-            TableField::new("is_builtin", TableDataType::Boolean),
             TableField::new("is_aggregate", TableDataType::Boolean),
-            TableField::new("definition", TableDataType::String),
-            TableField::new("category", TableDataType::String),
             TableField::new("description", TableDataType::String),
             TableField::new("syntax", TableDataType::String),
             TableField::new("example", TableDataType::String),
