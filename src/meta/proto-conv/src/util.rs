7456abe049e02ec0184defd71e41c2e16440dd6f
Chojan Shang <psiace@apache.org>
2024-02-01 09:45:07 +0800

    feat(meta): add created_on field to UserDefinedFunction
    Signed-off-by: Chojan Shang <psiace@apache.org>

--- src/meta/proto-conv/src/util.rs
+++ src/meta/proto-conv/src/util.rs
@@ -108,6 +108,7 @@ const META_CHANGE_LOG: &[(u64, &str)] = &[
     (76, "2024-01-18: ADD: ownership.proto and role.proto", ),
     (77, "2024-01-22: Remove: allow_anonymous in S3 Config", ),
     (78, "2024-01-29: Refactor: GrantEntry::UserPrivilegeType and ShareGrantEntry::ShareGrantObjectPrivilege use from_bits_truncate deserialize", ),
+    (79, "2024-01-31: Add: udf.proto/UserDefinedFunction add created_on field", ),
     // Dear developer:
     //      If you're gonna add a new metadata version, you'll have to add a test for it.
     //      You could just copy an existing test file(e.g., `../tests/it/v024_table_meta.rs`)
