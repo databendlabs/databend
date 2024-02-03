baa021ec6c5f2f61023c6ec3fee1e8e8841b675a
jw <freejw@gmail.com>
2024-02-03 11:41:26 +0800

    test

--- src/query/service/src/pipelines/processors/transforms/aggregator/transform_single_key.rs
+++ src/query/service/src/pipelines/processors/transforms/aggregator/transform_single_key.rs
@@ -92,6 +92,8 @@ impl AccumulatingTransform for PartialSingleStateAggregator {
 
     fn transform(&mut self, block: DataBlock) -> Result<Vec<DataBlock>> {
         if !block.is_empty() {
+            return Ok(vec![])
+        }
             let is_agg_index_block = block
                 .get_meta()
                 .and_then(AggIndexMeta::downcast_ref_from)
@@ -123,7 +125,6 @@ impl AccumulatingTransform for PartialSingleStateAggregator {
                     func.accumulate(place, &arg_columns, None, block.num_rows())?;
                 }
             }
-        }
 
         Ok(vec![])
     }
