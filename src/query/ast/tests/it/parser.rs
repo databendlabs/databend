ad86b046747d45c9d9e9617568ae317fd3b44b6d
codedump <lichuang1982@gmail.com>
2024-02-05 10:02:06 +0800

    feat: add create or replace view support (#14599)

--- src/query/ast/tests/it/parser.rs
+++ src/query/ast/tests/it/parser.rs
@@ -132,6 +132,7 @@ fn test_statement() {
         r#"alter view v as select number % 3 as a from numbers(1000);"#,
         r#"drop view v;"#,
         r#"create view v1(c1) as select number % 3 as a from numbers(1000);"#,
+        r#"create or replace view v1(c1) as select number % 3 as a from numbers(1000);"#,
         r#"alter view v1(c2) as select number % 3 as a from numbers(1000);"#,
         r#"create stream test2.s1 on table test.t append_only = false;"#,
         r#"create stream if not exists test2.s2 on table test.t at (stream => test1.s1) comment = 'this is a stream';"#,
