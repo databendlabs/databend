ce4b56945123edc448dd54c5962facc4cb43eb11
张炎泼 <drdr.xp@gmail.com>
2024-02-06 13:26:09 +0800

    refactor: Add `KVPbApi::list_pb()`, simplify `UdfMgr::get_udfs()`
    `KVPbApi::list_pb()` lists key value pairs by a prefix `DirName<K>`, and
    decodes key back to `kvapi::Key` and values back to `kvapi::Key::ValueType`.
    
    `UdfMgr::get_udfs()` is simplified by using `list_pb()`.

--- src/meta/api/src/kv_pb_api.rs
+++ src/meta/api/src/kv_pb_api.rs
@@ -17,7 +17,11 @@
 use std::future::Future;
 
 use databend_common_meta_kvapi::kvapi;
+use databend_common_meta_kvapi::kvapi::DirName;
 use databend_common_meta_kvapi::kvapi::KVApi;
+use databend_common_meta_kvapi::kvapi::Key;
+use databend_common_meta_kvapi::kvapi::NonEmptyItem;
+use databend_common_meta_types::protobuf::StreamItem;
 use databend_common_meta_types::InvalidReply;
 use databend_common_meta_types::MetaError;
 use databend_common_meta_types::MetaNetworkError;
@@ -25,6 +29,9 @@ use databend_common_meta_types::SeqV;
 use databend_common_proto_conv::FromToProto;
 use databend_common_proto_conv::Incompatible;
 use futures::future::FutureExt;
+use futures::stream::BoxStream;
+use futures::stream::StreamExt;
+use futures::TryStreamExt;
 use PbApiReadError::KvApiError;
 
 /// An error occurs when decoding protobuf encoded value.
@@ -35,12 +42,30 @@ pub enum PbDecodeError {
     Incompatible(#[from] Incompatible),
 }
 
+/// An error occurs when found an unexpected None value.
+#[derive(Clone, Debug, PartialEq, thiserror::Error)]
+#[error("NoneValue: unexpected None value of key: '{key}'")]
+pub struct NoneValue {
+    key: String,
+}
+
+impl NoneValue {
+    pub fn new(key: impl ToString) -> Self {
+        NoneValue {
+            key: key.to_string(),
+        }
+    }
+}
+
 /// An error occurs when reading protobuf encoded value from kv store.
 #[derive(Clone, Debug, PartialEq, thiserror::Error)]
 #[error("PbApiReadError: {0}")]
 pub enum PbApiReadError<E> {
     DecodeError(#[from] prost::DecodeError),
     Incompatible(#[from] Incompatible),
+    KeyError(#[from] kvapi::KeyError),
+    NoneValue(#[from] NoneValue),
+    /// Error returned from KVApi.
     KvApiError(E),
 }
 
@@ -66,6 +91,16 @@ impl From<PbApiReadError<MetaError>> for MetaError {
                 MetaError::NetworkError(net_err)
             }
             PbApiReadError::Incompatible(e) => {
+                let inv = InvalidReply::new("", &e);
+                let net_err = MetaNetworkError::InvalidReply(inv);
+                MetaError::NetworkError(net_err)
+            }
+            PbApiReadError::KeyError(e) => {
+                let inv = InvalidReply::new("", &e);
+                let net_err = MetaNetworkError::InvalidReply(inv);
+                MetaError::NetworkError(net_err)
+            }
+            PbApiReadError::NoneValue(e) => {
                 let inv = InvalidReply::new("", &e);
                 let net_err = MetaNetworkError::InvalidReply(inv);
                 MetaError::NetworkError(net_err)
@@ -110,10 +145,76 @@ pub trait KVPbApi: KVApi {
             Ok(v)
         }
     }
-    // TODO: add list
+
+    /// List protobuf encoded values by prefix and returns a stream.
+    ///
+    /// The returned value is decoded by `FromToProto`.
+    /// It returns the same error as `KVApi::Error`,
+    /// thus it requires KVApi::Error can describe a decoding error, i.e., `impl From<PbApiReadError>`.
+    fn list_pb<K>(
+        &self,
+        prefix: &DirName<K>,
+    ) -> impl Future<
+        Output = Result<BoxStream<'static, Result<NonEmptyItem<K>, Self::Error>>, Self::Error>,
+    > + Send
+    where
+        K: kvapi::Key + 'static,
+        K::ValueType: FromToProto,
+        Self::Error: From<PbApiReadError<Self::Error>>,
+    {
+        self.list_pb_low(prefix).map(|r| match r {
+            Ok(strm) => Ok(strm.map_err(Self::Error::from).boxed()),
+            Err(e) => Err(Self::Error::from(e)),
+        })
+    }
+
+    /// Same as `list_pb` but returns [`PbApiReadError`]. No require of `From<PbApiReadError>` for `Self::Error`.
+    fn list_pb_low<K>(
+        &self,
+        prefix: &DirName<K>,
+    ) -> impl Future<
+        Output = Result<
+            BoxStream<'static, Result<NonEmptyItem<K>, PbApiReadError<Self::Error>>>,
+            PbApiReadError<Self::Error>,
+        >,
+    > + Send
+    where
+        K: kvapi::Key + 'static,
+        K::ValueType: FromToProto,
+    {
+        let prefix = prefix.to_string_key();
+        async move {
+            let strm = self.list_kv(&prefix).await.map_err(KvApiError)?;
+            let strm = strm.map(decode_non_empty_item::<K, Self::Error>);
+            Ok(strm.boxed())
+        }
+    }
 }
 
 impl<T> KVPbApi for T where T: KVApi + ?Sized {}
+
+/// Decode key and protobuf encoded value from `StreamItem`.
+///
+/// It requires K to be static because it is used in a static stream map()
+fn decode_non_empty_item<K, E>(
+    r: Result<StreamItem, E>,
+) -> Result<NonEmptyItem<K>, PbApiReadError<E>>
+where
+    K: kvapi::Key + 'static,
+    K::ValueType: FromToProto,
+{
+    match r {
+        Ok(item) => {
+            let k = K::from_str_key(&item.key)?;
+
+            let raw = item.value.ok_or_else(|| NoneValue::new(item.key))?;
+            let v = decode_seqv::<K::ValueType>(SeqV::from(raw))?;
+
+            Ok(NonEmptyItem::new(k, v))
+        }
+        Err(e) => Err(KvApiError(e)),
+    }
+}
 
 /// Deserialize SeqV<Vec<u8>> into SeqV<T>, with FromToProto.
 fn decode_seqv<T>(seqv: SeqV) -> Result<SeqV<T>, PbDecodeError>
--- src/meta/kvapi/src/kvapi/key.rs
+++ src/meta/kvapi/src/kvapi/key.rs
@@ -74,3 +74,143 @@ impl kvapi::Key for String {
         Ok(s.to_string())
     }
 }
+
+/// The dir name of a key.
+///
+/// For example, the dir name of a key `a/b/c` is `a/b`.
+///
+/// Note that the dir name of `a` is still `a`.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct DirName<K> {
+    key: K,
+    level: usize,
+}
+
+impl<K> DirName<K> {
+    pub fn new(key: K) -> Self {
+        DirName { key, level: 1 }
+    }
+
+    pub fn new_with_level(key: K, level: usize) -> Self {
+        DirName { key, level }
+    }
+
+    pub fn with_level(&mut self, level: usize) -> &mut Self {
+        self.level = level;
+        self
+    }
+
+    pub fn key(&self) -> &K {
+        &self.key
+    }
+
+    pub fn into_key(self) -> K {
+        self.key
+    }
+}
+
+impl<K: Key> Key for DirName<K> {
+    const PREFIX: &'static str = K::PREFIX;
+    type ValueType = K::ValueType;
+
+    fn to_string_key(&self) -> String {
+        let k = self.key.to_string_key();
+        k.rsplitn(self.level + 1, '/').last().unwrap().to_string()
+    }
+
+    fn from_str_key(s: &str) -> Result<Self, KeyError> {
+        let d = DirName::new_with_level(K::from_str_key(s)?, 0);
+        Ok(d)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::DirName;
+    use crate::kvapi::Key;
+    use crate::kvapi::KeyError;
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    struct FooKey {
+        a: u64,
+        b: String,
+        c: u64,
+    }
+
+    impl Key for FooKey {
+        const PREFIX: &'static str = "pref";
+        type ValueType = ();
+
+        fn to_string_key(&self) -> String {
+            format!("{}/{}/{}/{}", Self::PREFIX, self.a, self.b, self.c)
+        }
+
+        fn from_str_key(s: &str) -> Result<Self, KeyError> {
+            // dummy impl
+            let k = FooKey {
+                a: 9,
+                b: "x".to_string(),
+                c: 8,
+            };
+            Ok(k)
+        }
+    }
+
+    #[test]
+    fn test_dir_name_from_key() {
+        let d = DirName::<FooKey>::from_str_key("").unwrap();
+        assert_eq!(
+            FooKey {
+                a: 9,
+                b: "x".to_string(),
+                c: 8,
+            },
+            d.into_key()
+        );
+    }
+
+    #[test]
+    fn test_dir_name() {
+        let k = FooKey {
+            a: 1,
+            b: "b".to_string(),
+            c: 2,
+        };
+
+        let dir = DirName::new(k);
+        assert_eq!("pref/1/b", dir.to_string_key());
+
+        let dir = DirName::new(dir);
+        assert_eq!("pref/1", dir.to_string_key());
+
+        let dir = DirName::new(dir);
+        assert_eq!("pref", dir.to_string_key());
+
+        let dir = DirName::new(dir);
+        assert_eq!("pref", dir.to_string_key(), "root dir should be the same");
+    }
+
+    #[test]
+    fn test_dir_name_with_level() {
+        let k = FooKey {
+            a: 1,
+            b: "b".to_string(),
+            c: 2,
+        };
+
+        let mut dir = DirName::new(k);
+        assert_eq!("pref/1/b", dir.to_string_key());
+
+        dir.with_level(0);
+        assert_eq!("pref/1/b/2", dir.to_string_key());
+
+        dir.with_level(2);
+        assert_eq!("pref/1", dir.to_string_key());
+
+        dir.with_level(3);
+        assert_eq!("pref", dir.to_string_key());
+
+        dir.with_level(4);
+        assert_eq!("pref", dir.to_string_key(), "root dir should be the same");
+    }
+}
