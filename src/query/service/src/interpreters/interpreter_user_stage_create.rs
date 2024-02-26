900f4d41164088091a181e60be76891f8672cd76
codedump <lichuang1982@gmail.com>
2024-02-26 20:51:09 +0800

    fix: fix create or replace stage bug

--- src/query/service/src/interpreters/interpreter_user_stage_create.rs
+++ src/query/service/src/interpreters/interpreter_user_stage_create.rs
@@ -20,11 +20,14 @@ use databend_common_exception::Result;
 use databend_common_management::RoleApi;
 use databend_common_meta_app::principal::OwnershipObject;
 use databend_common_meta_app::principal::StageType;
+use databend_common_meta_app::schema::CreateOption;
 use databend_common_meta_types::MatchSeq;
 use databend_common_sql::plans::CreateStagePlan;
+use databend_common_storages_stage::StageTable;
 use databend_common_users::RoleCacheManager;
 use databend_common_users::UserApiProvider;
 use log::debug;
+use log::info;
 
 use crate::interpreters::Interpreter;
 use crate::pipelines::PipelineBuildResult;
@@ -79,17 +82,25 @@ impl Interpreter for CreateUserStageInterpreter {
             )));
         };
 
-        if user_stage.stage_type != StageType::External {
-            let op = self.ctx.get_data_operator()?.operator();
-            op.create_dir(&user_stage.stage_prefix()).await?
-        }
-
         let mut user_stage = user_stage;
         user_stage.creator = Some(self.ctx.get_current_user()?.identity());
         user_stage.created_on = Utc::now();
         let _ = user_mgr
-            .add_stage(&plan.tenant, user_stage, &plan.create_option)
+            .add_stage(&plan.tenant, user_stage.clone(), &plan.create_option)
             .await?;
+
+        if user_stage.stage_type != StageType::External {
+            if let CreateOption::CreateOrReplace = plan.create_option {
+                let op = StageTable::get_op(&user_stage)?;
+                op.remove_all("/").await?;
+                info!(
+                    "create or replace stage {:?} with all objects removed in stage",
+                    user_stage.stage_name
+                );
+            }
+            let op = self.ctx.get_data_operator()?.operator();
+            op.create_dir(&user_stage.stage_prefix()).await?
+        }
 
         // Grant ownership as the current role
         let tenant = self.ctx.get_tenant();
