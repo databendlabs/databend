2a17ec158b14758b7a9311814bf56a42de5b6196
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-03-01 13:28:04 +0800

    feat: http handler txn support. (#14802)
    * feature: http handler txn support.
    
    * add more tests.

--- tests/sqllogictests/suites/stage/copy_into_txn.sql
+++ tests/sqllogictests/suites/stage/copy_into_txn.sql
@@ -11,7 +11,6 @@ use test_txn_copy;
 statement ok
 create table t1(c int);
 
-onlyif mysql
 statement ok
 begin;
 
@@ -29,13 +28,11 @@ query
 copy into t1 from @data/csv/numbers.csv file_format = (type = CSV);
 ----
 
-
 query I
 select count(*) from t1;
 ----
 18
 
-onlyif mysql
 statement ok
 commit;
 
@@ -47,7 +44,6 @@ select count(*) from t1;
 query
 copy into t1 from @data/csv/numbers.csv file_format = (type = CSV);
 ----
-
 
 query I
 select count(*) from t1;
@@ -67,47 +63,38 @@ use test_txn_copy_1;
 statement ok
 create table t1(c int);
 
-onlyif mysql
 statement ok
 begin;
 
-onlyif mysql
+
 query
 copy into t1 from @data/csv/numbers.csv file_format = (type = CSV) purge = true;
 ----
 csv/numbers.csv 18 0 NULL NULL
 
-onlyif mysql
 statement error 1025
 select * from t100;
 
-onlyif mysql
 statement error 4002
 select * from t1;
 
-onlyif mysql
 statement ok
 commit;
 
-onlyif mysql
 query I
 select count(*) from t1;
 ----
 0
 
-onlyif mysql
 statement ok
 create table t2(c int);
 
-onlyif mysql
 query
 copy into t2 from @data/csv/numbers.csv file_format = (type = CSV);
 ----
 csv/numbers.csv 18 0 NULL NULL
 
-onlyif mysql
 query I
 select count(*) from t2;
 ----
 18
-
