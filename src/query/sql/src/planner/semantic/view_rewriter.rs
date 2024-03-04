4fbd8cfb1b0ea1b83a521aaf5962d5ffa5b3b894
Andy Lok <andylokandy@hotmail.com>
2024-03-04 19:20:14 +0800

    fix(ast): add missing #[drive(skip)] (#14823)
    * refactor(ast): migrate to derive-visitor
    
    * fix
    
    * fix

--- src/query/sql/src/planner/semantic/view_rewriter.rs
+++ src/query/sql/src/planner/semantic/view_rewriter.rs
@@ -12,51 +12,49 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use databend_common_ast::ast::walk_table_reference_mut;
 use databend_common_ast::ast::Identifier;
 use databend_common_ast::ast::TableReference;
-use databend_common_ast::ast::VisitorMut;
+use derive_visitor::VisitorMut;
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, VisitorMut)]
+#[visitor(TableReference(enter))]
 pub struct ViewRewriter {
     pub current_database: String,
 }
 
-impl VisitorMut for ViewRewriter {
-    fn visit_table_reference(&mut self, table_ref: &mut TableReference) {
-        match table_ref {
-            TableReference::Table {
-                span,
-                catalog,
-                database,
-                table,
-                alias,
-                travel_point,
-                since_point,
-                pivot,
-                unpivot,
-            } => {
-                // Must rewrite view query when table_ref::database is none. If not:
-                // e.g.
-                // create view default.v_t as select * from t;
-                // use db1; -- db1 does not contain table `t`
-                // select * from default.v_t; => select * from (select * from t); -- will return err that unknown table db1.t
-                if database.is_none() {
-                    let database = Some(Identifier::from_name(self.current_database.clone()));
-                    *table_ref = TableReference::Table {
-                        span: *span,
-                        catalog: catalog.clone(),
-                        database,
-                        table: table.clone(),
-                        alias: alias.clone(),
-                        travel_point: travel_point.clone(),
-                        since_point: since_point.clone(),
-                        pivot: pivot.clone(),
-                        unpivot: unpivot.clone(),
-                    }
+impl ViewRewriter {
+    fn enter_table_reference(&mut self, table_ref: &mut TableReference) {
+        if let TableReference::Table {
+            span,
+            catalog,
+            database,
+            table,
+            alias,
+            travel_point,
+            since_point,
+            pivot,
+            unpivot,
+        } = table_ref
+        {
+            // Must rewrite view query when table_ref::database is none. If not:
+            // e.g.
+            // create view default.v_t as select * from t;
+            // use db1; -- db1 does not contain table `t`
+            // select * from default.v_t; => select * from (select * from t); -- will return err that unknown table db1.t
+            if database.is_none() {
+                let database = Some(Identifier::from_name(self.current_database.clone()));
+                *table_ref = TableReference::Table {
+                    span: *span,
+                    catalog: catalog.clone(),
+                    database,
+                    table: table.clone(),
+                    alias: alias.clone(),
+                    travel_point: travel_point.clone(),
+                    since_point: since_point.clone(),
+                    pivot: pivot.clone(),
+                    unpivot: unpivot.clone(),
                 }
             }
-            _ => walk_table_reference_mut(self, table_ref),
         }
     }
 }
