8009f74dab3cdb260beba61bf8502f5df5cb3d1d
baishen <baishen2009@gmail.com>
2024-03-04 21:52:54 +0800

    fix check

--- src/query/storages/fuse/src/io/read/inverted_index/cache_directory.rs
+++ src/query/storages/fuse/src/io/read/inverted_index/cache_directory.rs
@@ -11,7 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
+
 // Copyright (c) 2018 by the tantivy project authors
 // (https://github.com/quickwit-oss/tantivy), as listed in the AUTHORS file.
 //
