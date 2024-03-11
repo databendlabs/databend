07def2c64cd1ecd2c6f1556d44cb4c0f6e8c6296
张炎泼 <drdr.xp@gmail.com>
2024-03-11 09:43:58 +0800

    fix: `check_and_upgrade_to_pb()` should return MetaError, add quota
    When writing back upgraded json-to-pb data to meta-service,
    it should return `MetaError`, instead of business error such as
    `ErrorCode::UnknownRole`.
    
    Add quota limit to `check_and_upgrade_to_pb`: for each API calls, it
    upgrades upto 100 records. So that an API call won't be delay a lot.
    
    After upgrading, it should returns the `seq` number after upgrading.

--- src/query/management/src/serde/pb_serde.rs
+++ src/query/management/src/serde/pb_serde.rs
@@ -20,11 +20,16 @@ use databend_common_exception::Result;
 use databend_common_exception::ToErrorCode;
 use databend_common_meta_kvapi::kvapi;
 use databend_common_meta_kvapi::kvapi::UpsertKVReq;
+use databend_common_meta_types::InvalidReply;
 use databend_common_meta_types::MatchSeq;
 use databend_common_meta_types::MetaError;
+use databend_common_meta_types::MetaNetworkError;
 use databend_common_meta_types::Operation;
 use databend_common_meta_types::SeqV;
+use databend_common_meta_types::SeqValue;
 use databend_common_proto_conv::FromToProto;
+
+use crate::serde::Quota;
 
 pub fn serialize_struct<T, ErrFn, CtxFn, D>(
     value: &T,
@@ -60,21 +65,21 @@ where
     Ok(v)
 }
 
-pub async fn check_and_upgrade_to_pb<T, ErrFn, CtxFn, D>(
+/// Check if the data is in protobuf format, if not, upgrade it to protobuf format.
+///
+/// Because upgrading relies on RPC, it takes a long time if it upgrades too many records.
+/// Thus if the upgrading quota is used up,
+/// it will just convert the data but do not write to backend storage.
+pub async fn check_and_upgrade_to_pb<T>(
+    quota: &mut Quota,
     key: String,
     seq_value: &SeqV,
-    kv_api: Arc<dyn kvapi::KVApi<Error = MetaError> + Send + Sync>,
-    err_code_fn: ErrFn,
-    context_fn: CtxFn,
-) -> std::result::Result<SeqV<T>, ErrorCode>
+    kv_api: &(impl kvapi::KVApi<Error = MetaError> + ?Sized),
+) -> std::result::Result<SeqV<T>, MetaError>
 where
     T: FromToProto + serde::de::DeserializeOwned + 'static,
-    ErrFn: FnOnce(String) -> ErrorCode + Copy,
-    D: Display,
-    CtxFn: FnOnce() -> D + Copy,
 {
-    let deserialize_result =
-        deserialize_struct(&seq_value.data, ErrorCode::IllegalUserInfoFormat, || "");
+    let deserialize_result = databend_common_meta_api::deserialize_struct(&seq_value.data);
 
     let err = match deserialize_result {
         Ok(data) => return Ok(SeqV::new(seq_value.seq, data)),
@@ -84,30 +89,33 @@ where
     log::debug!("deserialize as pb err: {}, rollback to use serde json", err);
 
     // If there's an error, try to deserialize as JSON.
-    let data = serde_json::from_slice::<T>(&seq_value.data)?;
+    let data = serde_json::from_slice::<T>(&seq_value.data).map_err(|e| {
+        let inv_reply = InvalidReply::new("non json non pb data", &e);
+        MetaNetworkError::from(inv_reply)
+    })?;
+
+    if quota.is_used_up() {
+        return Ok(SeqV::new(seq_value.seq, data));
+    }
+    quota.decrement();
 
     // If we reached here, it means JSON deserialization was successful but we need to serialize to protobuf format.
-    let value = serialize_struct(&data, ErrorCode::IllegalUserInfoFormat, || "")?;
-    match kv_api
+    let value = databend_common_meta_api::serialize_struct(&data)?;
+
+    let res = kv_api
         .upsert_kv(UpsertKVReq::new(
             &key,
             MatchSeq::Exact(seq_value.seq),
             Operation::Update(value),
             None,
         ))
-        .await
-        .map_err_to_code(err_code_fn, context_fn)
-    {
-        Ok(res) => {
-            if let Some(seq) = res.result {
-                Ok(SeqV::new(seq.seq, data))
-            } else {
-                Ok(SeqV::new(seq_value.seq, data))
-            }
-        }
-        Err(err) => {
-            log::error!("json to pb upsert_kv failed, error: {}", err);
-            Err(err)
-        }
-    }
+        .await?;
+
+    let seq = if res.is_changed() {
+        res.result.seq()
+    } else {
+        seq_value.seq
+    };
+
+    Ok(SeqV::new(seq, data))
 }
