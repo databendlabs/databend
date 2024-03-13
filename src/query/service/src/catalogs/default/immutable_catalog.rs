7f78af86295d6b6d5d6be358731d602a3a68bfd0
taichong <tai_chong@foxmail.com>
2024-03-13 15:12:26 +0800

    feat: show grants not display droped table/database

--- src/query/service/src/catalogs/default/immutable_catalog.rs
+++ src/query/service/src/catalogs/default/immutable_catalog.rs
@@ -205,11 +205,11 @@ impl Catalog for ImmutableCatalog {
     async fn mget_table_names_by_ids(
         &self,
         table_ids: &[MetaId],
-    ) -> databend_common_exception::Result<Vec<String>> {
+    ) -> databend_common_exception::Result<Vec<Option<String>>> {
         let mut table_name = Vec::with_capacity(table_ids.len());
         for id in table_ids {
             if let Some(table) = self.sys_db_meta.get_by_id(id) {
-                table_name.push(table.name().to_string());
+                table_name.push(Some(table.name().to_string()));
             }
         }
         Ok(table_name)
@@ -228,13 +228,13 @@ impl Catalog for ImmutableCatalog {
         }
     }
 
-    async fn mget_database_names_by_ids(&self, db_ids: &[MetaId]) -> Result<Vec<String>> {
+    async fn mget_database_names_by_ids(&self, db_ids: &[MetaId]) -> Result<Vec<Option<String>>> {
         let mut res = Vec::new();
         for id in db_ids {
             if self.sys_db.get_db_info().ident.db_id == *id {
-                res.push("system".to_string());
+                res.push(Some("system".to_string()));
             } else if self.info_schema_db.get_db_info().ident.db_id == *id {
-                res.push("information_schema".to_string());
+                res.push(Some("information_schema".to_string()));
             }
         }
         Ok(res)
