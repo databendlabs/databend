10d47ac978ec559935829124dc02aa234a4a571a
Dousir9 <736191200@qq.com>
2024-03-14 21:38:32 +0800

    chore: fix distributed query plan

--- src/query/sql/src/planner/optimizer/optimizer.rs
+++ src/query/sql/src/planner/optimizer/optimizer.rs
@@ -264,30 +264,41 @@ pub fn optimize_query(opt_ctx: OptimizerContext, mut s_expr: SExpr) -> Result<SE
         opt_ctx.table_ctx.clone(),
         opt_ctx.metadata.clone(),
         dphyp_optimized,
-        false,
+        enable_distributed_query,
     )?;
 
-    // Cascades optimizer may fail due to timeout.
+    if opt_ctx.enable_join_reorder {
+        s_expr =
+            RecursiveOptimizer::new([RuleID::CommuteJoin].as_slice(), &opt_ctx).run(&s_expr)?;
+    }
+
+    // Cascades optimizer may fail due to timeout, fallback to heuristic optimizer in this case.
     s_expr = match cascades.optimize(s_expr.clone()) {
-        Ok(s_expr) => s_expr,
+        Ok(mut s_expr) => {
+            // Push down sort and limit
+            // TODO(leiysky): do this optimization in cascades optimizer
+            if enable_distributed_query {
+                let sort_and_limit_optimizer = SortAndLimitPushDownOptimizer::create();
+                s_expr = sort_and_limit_optimizer.optimize(&s_expr)?;
+            }
+            s_expr
+        }
+
         Err(e) => {
-            info!("CascadesOptimizer failed: {}", e);
+            info!(
+                "CascadesOptimizer failed, fallback to heuristic optimizer: {}",
+                e
+            );
+            if enable_distributed_query {
+                s_expr = optimize_distributed_query(opt_ctx.table_ctx.clone(), &s_expr)?;
+            }
+
             s_expr
         }
     };
 
-    let rules = if opt_ctx.enable_join_reorder {
-        [RuleID::EliminateEvalScalar, RuleID::CommuteJoin].as_slice()
-    } else {
-        [RuleID::EliminateEvalScalar].as_slice()
-    };
-    s_expr = RecursiveOptimizer::new(rules, &opt_ctx).run(&s_expr)?;
-
-    if enable_distributed_query {
-        let sort_and_limit_optimizer = SortAndLimitPushDownOptimizer::create();
-        s_expr = sort_and_limit_optimizer.optimize(&s_expr)?;
-        s_expr = optimize_distributed_query(opt_ctx.table_ctx.clone(), &s_expr)?;
-    }
+    s_expr =
+        RecursiveOptimizer::new([RuleID::EliminateEvalScalar].as_slice(), &opt_ctx).run(&s_expr)?;
 
     Ok(s_expr)
 }
