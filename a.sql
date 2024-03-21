946e994d21a7e0bd703962a3e5528b1815301f21
JackTan25 <jacktby@gmail.com>
2024-03-21 22:53:33 +0800

    remove a.sql

--- a.sql
+++
@@ -1,2 +0,0 @@
-
-explain merge into t1 using (SELECT * EXCLUDE (rn) FROM (SELECT *, row_number() OVER (PARTITION BY id ORDER BY t DESC) AS rn FROM tbcc WHERE a in(1,2,3))) as t2 on t1.a = t2.a when matched then update * when not matched then insert *;
