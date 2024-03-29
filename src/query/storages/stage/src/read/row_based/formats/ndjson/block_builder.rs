7452ffcd0e45b3ffebda1207a798489ebf7bd22a
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-03-29 23:05:18 +0800

    feat: refactor ndjson input format. (#14943)
    * feat: refactor ndjson input format.
    
    * feat: refactor ndjson input format.

--- src/query/storages/stage/src/read/row_based/formats/ndjson/block_builder.rs
+++ src/query/storages/stage/src/read/row_based/formats/ndjson/block_builder.rs
@@ -17,38 +17,37 @@ use std::sync::Arc;
 use bstr::ByteSlice;
 use databend_common_exception::Result;
 use databend_common_expression::ColumnBuilder;
-use databend_common_expression::Scalar;
-use databend_common_expression::TableSchemaRef;
-use databend_common_formats::FieldDecoder;
+use databend_common_expression::DataBlock;
 use databend_common_formats::FieldJsonAstDecoder;
-use databend_common_formats::FileFormatOptionsExt;
-use databend_common_meta_app::principal::FileFormatParams;
 use databend_common_meta_app::principal::NullAs;
-use databend_common_meta_app::principal::StageFileFormatType;
+use databend_common_pipeline_sources::input_formats::error_utils::truncate_column_data;
 use databend_common_storage::FileParseError;
 
-use crate::input_formats::error_utils::truncate_column_data;
-use crate::input_formats::AligningStateRowDelimiter;
-use crate::input_formats::BlockBuilder;
-use crate::input_formats::InputContext;
-use crate::input_formats::InputFormatTextBase;
-use crate::input_formats::RowBatch;
-use crate::input_formats::SplitInfo;
+use crate::read::load_context::LoadContext;
+use crate::read::row_based::batch::RowBatchWithPosition;
+use crate::read::row_based::format::RowDecoder;
+use crate::read::row_based::formats::ndjson::format::NdJsonInputFormat;
+use crate::read::row_based::processors::BlockBuilderState;
 
-pub struct InputFormatNDJson {}
+pub struct NdJsonDecoder {
+    pub load_context: Arc<LoadContext>,
+    pub fmt: NdJsonInputFormat,
+    pub field_decoder: FieldJsonAstDecoder,
+}
 
-impl InputFormatNDJson {
-    pub fn create() -> Self {
-        Self {}
+impl NdJsonDecoder {
+    pub fn create(fmt: NdJsonInputFormat, load_context: Arc<LoadContext>) -> Self {
+        let field_decoder = FieldJsonAstDecoder::create(&load_context.file_format_options_ext);
+        Self {
+            load_context,
+            fmt,
+            field_decoder,
+        }
     }
     fn read_row(
-        field_decoder: &FieldJsonAstDecoder,
+        &self,
         buf: &[u8],
         columns: &mut [ColumnBuilder],
-        schema: &TableSchemaRef,
-        default_values: &Option<Vec<Scalar>>,
-        null_field_as: &NullAs,
-        missing_field_as: &NullAs,
         null_if: &[&str],
     ) -> std::result::Result<(), FileParseError> {
         let mut json: serde_json::Value =
@@ -56,32 +55,37 @@ impl InputFormatNDJson {
                 message: e.to_string(),
             })?;
         // todo: this is temporary
-        if field_decoder.is_select {
-            field_decoder
+        if self.field_decoder.is_select {
+            self.field_decoder
                 .read_field(&mut columns[0], &json)
                 .map_err(|e| FileParseError::InvalidNDJsonRow {
                     message: e.to_string(),
                 })?;
         } else {
             // if it's not case_sensitive, we convert to lowercase
-            if !field_decoder.ident_case_sensitive {
+            if !self.field_decoder.ident_case_sensitive {
                 if let serde_json::Value::Object(x) = json {
                     let y = x.into_iter().map(|(k, v)| (k.to_lowercase(), v)).collect();
                     json = serde_json::Value::Object(y);
                 }
             }
 
-            for ((column_index, field), column) in
-                schema.fields().iter().enumerate().zip(columns.iter_mut())
+            for ((column_index, field), column) in self
+                .load_context
+                .schema
+                .fields()
+                .iter()
+                .enumerate()
+                .zip(columns.iter_mut())
             {
-                let field_name = if field_decoder.ident_case_sensitive {
+                let field_name = if self.field_decoder.ident_case_sensitive {
                     field.name().to_owned()
                 } else {
                     field.name().to_lowercase()
                 };
                 let value = json.get(field_name);
                 match value {
-                    None => match missing_field_as {
+                    None => match self.fmt.params.missing_field_as {
                         NullAs::Error => {
                             return Err(FileParseError::ColumnMissingError {
                                 column_index,
@@ -101,30 +105,30 @@ impl InputFormatNDJson {
                             }
                         }
                         NullAs::FieldDefault => {
-                            if let Some(values) = default_values {
+                            if let Some(values) = &self.load_context.default_values {
                                 column.push(values[column_index].as_ref());
                             } else {
                                 column.push_default();
                             }
                         }
                     },
-                    Some(serde_json::Value::Null) => match null_field_as {
+                    Some(serde_json::Value::Null) => match self.fmt.params.null_field_as {
                         NullAs::Error => unreachable!("null_field_as should be error"),
                         NullAs::Null => {
                             if field.is_nullable_or_null() {
                                 column.push_default();
                             } else {
                                 return Err(FileParseError::ColumnDecodeError {
-                                        column_index,
-                                        column_name: field.name().to_owned(),
-                                        column_type: field.data_type.to_string(),
-                                        decode_error: "null value is not allowed for non-nullable field, when NULL_FIELDS_AS=NULL".to_owned(),
-                                        column_data: "null".to_owned(),
-                                    });
+                                    column_index,
+                                    column_name: field.name().to_owned(),
+                                    column_type: field.data_type.to_string(),
+                                    decode_error: "null value is not allowed for non-nullable field, when NULL_FIELDS_AS=NULL".to_owned(),
+                                    column_data: "null".to_owned(),
+                                });
                             }
                         }
                         NullAs::FieldDefault => {
-                            if let Some(values) = default_values {
+                            if let Some(values) = &self.load_context.default_values {
                                 column.push(values[column_index].as_ref());
                             } else {
                                 column.push_default();
@@ -139,7 +143,7 @@ impl InputFormatNDJson {
                         {
                             column.push_default();
                         } else {
-                            field_decoder.read_field(column, value).map_err(|e| {
+                            self.field_decoder.read_field(column, value).map_err(|e| {
                                 FileParseError::ColumnDecodeError {
                                     column_index,
                                     column_name: field.name().to_owned(),
@@ -157,78 +161,39 @@ impl InputFormatNDJson {
     }
 }
 
-impl InputFormatTextBase for InputFormatNDJson {
-    type AligningState = AligningStateRowDelimiter;
-
-    fn format_type() -> StageFileFormatType {
-        StageFileFormatType::NdJson
-    }
-
-    fn is_splittable() -> bool {
-        true
-    }
-
-    fn try_create_align_state(
-        ctx: &Arc<InputContext>,
-        split_info: &Arc<SplitInfo>,
-    ) -> Result<Self::AligningState> {
-        AligningStateRowDelimiter::try_create(ctx, split_info, b'\n', true, 0)
-    }
-
-    fn create_field_decoder(
-        _params: &FileFormatParams,
-        options: &FileFormatOptionsExt,
-    ) -> Arc<dyn FieldDecoder> {
-        Arc::new(FieldJsonAstDecoder::create(options))
-    }
-
-    fn deserialize(builder: &mut BlockBuilder<Self>, batch: RowBatch) -> Result<()> {
-        let field_decoder = builder
-            .field_decoder
-            .as_any()
-            .downcast_ref::<FieldJsonAstDecoder>()
-            .expect("must success");
-
-        let columns = &mut builder.mutable_columns;
-        let mut start = 0usize;
-        let format_params = match builder.ctx.file_format_params {
-            FileFormatParams::NdJson(ref p) => p,
-            _ => unreachable!(),
-        };
-
-        let null_if = format_params
+impl RowDecoder for NdJsonDecoder {
+    fn add(
+        &self,
+        state: &mut BlockBuilderState,
+        batch: RowBatchWithPosition,
+    ) -> Result<Vec<DataBlock>> {
+        let columns = &mut state.mutable_columns;
+        let data = batch.data.into_nd_json().unwrap();
+        let null_if = self
+            .fmt
+            .params
             .null_if
             .iter()
             .map(|x| x.as_str())
             .collect::<Vec<_>>();
-        for (i, end) in batch.row_ends.iter().enumerate() {
-            let buf = &batch.data[start..*end];
-            let buf = buf.trim();
-            if !buf.is_empty() {
-                if let Err(e) = Self::read_row(
-                    field_decoder,
-                    buf,
-                    columns,
-                    &builder.ctx.schema,
-                    &builder.ctx.default_values,
-                    &format_params.null_field_as,
-                    &format_params.missing_field_as,
-                    &null_if,
-                ) {
-                    builder.ctx.on_error(
+
+        for (row_id, row) in data.iter().enumerate() {
+            let row = row.trim();
+            if !row.is_empty() {
+                if let Err(e) = self.read_row(row, columns, &null_if) {
+                    self.load_context.error_handler.on_error(
                         e,
-                        Some((columns, builder.num_rows)),
-                        &mut builder.file_status,
-                        &batch.split_info.file.path,
-                        batch.start_row_in_split + i,
+                        Some((columns, state.num_rows)),
+                        &mut state.file_status,
+                        &batch.start_pos.path,
+                        batch.start_pos.rows + row_id,
                     )?
                 } else {
-                    builder.num_rows += 1;
-                    builder.file_status.num_rows_loaded += 1;
+                    state.num_rows += 1;
+                    state.file_status.num_rows_loaded += 1;
                 }
             }
-            start = *end;
         }
-        Ok(())
+        Ok(vec![])
     }
 }
