75a0bb8e2499498e4fafea172c7b82cf0d8b113e
sky <3374614481@qq.com>
2024-04-02 22:09:01 +0800

    fix missing header

--- src/query/service/src/test_kits/block_writer.rs
+++ src/query/service/src/test_kits/block_writer.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use chrono::Utc;
 use databend_common_exception::Result;
 use databend_common_expression::DataBlock;
