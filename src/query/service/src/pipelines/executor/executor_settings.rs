5606d5b03efb36327fa7e282e0f5fbe077fb69e3
Liuqing Yue <dqhl76@gmail.com>
2024-04-08 13:56:22 +0800

    chore(executor): add time limit daemon for queries executor
    Signed-off-by: Liuqing Yue <dqhl76@gmail.com>

--- src/query/service/src/pipelines/executor/executor_settings.rs
+++ src/query/service/src/pipelines/executor/executor_settings.rs
@@ -35,7 +35,8 @@ impl ExecutorSettings {
         let max_execute_time_in_seconds = settings.get_max_execute_time_in_seconds()?;
 
         Ok(ExecutorSettings {
-            enable_queries_executor: settings.get_enable_experimental_queries_executor()?,
+            // enable_queries_executor: settings.get_enable_experimental_queries_executor()?,
+            enable_queries_executor: true,
             query_id: Arc::new(query_id),
             max_execute_time_in_seconds: Duration::from_secs(max_execute_time_in_seconds),
             max_threads,
