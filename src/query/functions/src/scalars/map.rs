13afda707ea2567908d43bcdd3ed10dacbcff6f6
Yijun Zhao <ariesdevil77@gmail.com>
2024-04-14 08:57:50 +0800

    fix: fix map get return double nested nullable (#15230)
    fix map get return type

--- src/query/functions/src/scalars/map.rs
+++ src/query/functions/src/scalars/map.rs
@@ -97,6 +97,32 @@ pub fn register(registry: &mut FunctionRegistry) {
         |_, _, _| Value::Scalar(()),
     );
 
+    // NOTE: order is matter, we first catch <K, Nullable<V>> here.
+    registry.register_combine_nullable_2_arg::<MapType<GenericType<0>, NullableType<GenericType<1>>>, GenericType<0>, GenericType<1>, _, _>(
+        "get",
+        |_, domain, _| {
+            FunctionDomain::Domain(NullableDomain {
+                has_null: true,
+                value: domain.as_ref().and_then(|(_, val_domain)| val_domain.value.clone())
+            })
+        },
+        vectorize_with_builder_2_arg::<MapType<GenericType<0>, NullableType<GenericType<1>>>, GenericType<0>, NullableType<GenericType<1>>>(
+            |map, key, output, _| {
+                for (k, v) in map.iter() {
+                    if k == key {
+                        match v {
+                            Some(v) => output.push(v),
+                            None => output.push_null()
+                        }
+                        return
+                    }
+                }
+                output.push_null()
+            }
+        ),
+    );
+
+    // NOTE: order is matter, we catch <K, V> here after <K, Nullable<V>>
     registry.register_combine_nullable_2_arg::<MapType<GenericType<0>, GenericType<1>>, GenericType<0>, GenericType<1>, _, _>(
         "get",
         |_, domain, _| {
