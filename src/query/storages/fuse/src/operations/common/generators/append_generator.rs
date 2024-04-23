8018bb1f2ad86438b8bea378342ac41f28d8bd7d
dantengsky <dantengsky@gmail.com>
2024-04-23 19:59:54 +0800

    chore: tweak code comments

--- src/query/storages/fuse/src/operations/common/generators/append_generator.rs
+++ src/query/storages/fuse/src/operations/common/generators/append_generator.rs
@@ -212,10 +212,13 @@ impl SnapshotGenerator for AppendGenerator {
             .get_auto_compaction_imperfect_blocks_threshold()?;
 
         if imperfect_count >= auto_compaction_imperfect_blocks_threshold {
-            // slightly increase the number of blocks eligible for auto-compaction,
+            // if imperfect_count is larger, slightly increase the number of blocks
+            // eligible for auto-compaction.
             // this adjustment is intended to help reduce fragmentation over time.
-            let compact_num_block_hint =
-                (auto_compaction_imperfect_blocks_threshold as f64 * 1.5).ceil() as u64;
+            let compact_num_block_hint = std::cmp::min(
+                imperfect_count,
+                (auto_compaction_imperfect_blocks_threshold as f64 * 1.5).ceil() as u64,
+            );
             info!("set compact_num_block_hint to {compact_num_block_hint }");
             self.ctx
                 .set_compaction_num_block_hint(compact_num_block_hint);
