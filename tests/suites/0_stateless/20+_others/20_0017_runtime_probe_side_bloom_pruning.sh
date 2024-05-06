e4f3f31d87b160d833492a225ff2193a10580f30
dantengsky <dantengsky@gmail.com>
2024-05-06 10:28:50 +0800

    add sql logic test for runtime bloom pruning

---
+++ tests/suites/0_stateless/20+_others/20_0017_runtime_probe_side_bloom_pruning.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. "$CURDIR"/../../../shell_env.sh
+
+echo "create or replace database rt_bloom" | $BENDSQL_CLIENT_CONNECT
+
+echo "create table rt_bloom.probe(c uint64) as select * from numbers(100000)" |  $BENDSQL_CLIENT_CONNECT
+echo "create table rt_bloom.build(c uint64)" |  $BENDSQL_CLIENT_CONNECT
+
+echo "The probe table should consist of 2 blocks"
+echo "select block_count from fuse_snapshot('rt_bloom','probe')" |  $BENDSQL_CLIENT_CONNECT
+
+
+echo "insert into rt_bloom.build values(50)" |  $BENDSQL_CLIENT_CONNECT
+
+
+echo "runtime range filter should work, one of the blocks should be pruned by range filter"
+echo "explain analyze select * from rt_bloom.probe  inner join rt_bloom.build on probe.c = build.c " \
+  |  $BENDSQL_CLIENT_CONNECT | grep "parts pruned by"
+
+
+
+echo "delete from rt_bloom.probe where c = 50" |  $BENDSQL_CLIENT_CONNECT;
+echo "runtime bloom filter should work, another block should be pruned by bloom filter"
+echo "explain analyze select * from rt_bloom.probe  inner join rt_bloom.build on probe.c = build.c " \
+  |  $BENDSQL_CLIENT_CONNECT | grep "parts pruned by"
+
+echo "DROP TABLE rt_bloom.probe" | $BENDSQL_CLIENT_CONNECT
+echo "DROP TABLE rt_bloom.build" | $BENDSQL_CLIENT_CONNECT
+echo "drop database rt_bloom" | $BENDSQL_CLIENT_CONNECT
