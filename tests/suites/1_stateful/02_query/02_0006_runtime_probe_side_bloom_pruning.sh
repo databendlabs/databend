d956b8c4d322eb647a2a34fd2b87eebd70d36509
dantengsky <dantengsky@gmail.com>
2024-05-06 13:45:08 +0800

    tweak stateful test

--- tests/suites/1_stateful/02_query/02_0006_runtime_probe_side_bloom_pruning.sh
+++ tests/suites/1_stateful/02_query/02_0006_runtime_probe_side_bloom_pruning.sh
@@ -9,26 +9,29 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 echo "create or replace database rt_bloom" | $BENDSQL_CLIENT_CONNECT
 
-echo "create table rt_bloom.probe(c uint64) as select * from numbers(100000)" |  $BENDSQL_CLIENT_CONNECT
+echo "create table rt_bloom.probe(c uint64)" |  $BENDSQL_CLIENT_CONNECT
 echo "create table rt_bloom.build(c uint64)" |  $BENDSQL_CLIENT_CONNECT
 
+echo "insert into rt_bloom.probe values (1),(2),(3)" |  $BENDSQL_CLIENT_CONNECT
+echo "insert into rt_bloom.probe values (4),(5),(6)" |  $BENDSQL_CLIENT_CONNECT
+
 echo "The probe table should consist of 2 blocks"
-echo "select block_count from fuse_snapshot('rt_bloom','probe')" |  $BENDSQL_CLIENT_CONNECT
+echo "select block_count from fuse_snapshot('rt_bloom','probe') limit 1" |  $BENDSQL_CLIENT_CONNECT
 
 
-echo "insert into rt_bloom.build values(50)" |  $BENDSQL_CLIENT_CONNECT
-
+echo "insert into rt_bloom.build values(5)" |  $BENDSQL_CLIENT_CONNECT
 
 echo "runtime range filter should work, one of the blocks should be pruned by range filter"
+# leading spaces are trimmed, as distributed plan has different indentation
 echo "explain analyze select * from rt_bloom.probe  inner join rt_bloom.build on probe.c = build.c " \
-  |  $BENDSQL_CLIENT_CONNECT | grep "parts pruned by"
+  |  $BENDSQL_CLIENT_CONNECT | grep "parts pruned by" | awk '{$1=$1}1'
 
 
-
-echo "delete from rt_bloom.probe where c = 50" |  $BENDSQL_CLIENT_CONNECT;
+echo "delete from rt_bloom.probe where c = 5" |  $BENDSQL_CLIENT_CONNECT;
 echo "runtime bloom filter should work, another block should be pruned by bloom filter"
+# leading spaces are trimmed, as distributed plan has different indentation
 echo "explain analyze select * from rt_bloom.probe  inner join rt_bloom.build on probe.c = build.c " \
-  |  $BENDSQL_CLIENT_CONNECT | grep "parts pruned by"
+  |  $BENDSQL_CLIENT_CONNECT | grep "parts pruned by" | awk '{$1=$1}1'
 
 echo "DROP TABLE rt_bloom.probe" | $BENDSQL_CLIENT_CONNECT
 echo "DROP TABLE rt_bloom.build" | $BENDSQL_CLIENT_CONNECT
