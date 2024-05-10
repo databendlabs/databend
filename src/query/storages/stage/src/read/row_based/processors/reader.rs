03ecd95e1070e9b9e785c3e18e6650ac48bea23e
Xuanwo <github@xuanwo.io>
2024-05-10 23:31:43 +0800

    reduce to 2 concurrent
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/query/storages/stage/src/read/row_based/processors/reader.rs
+++ src/query/storages/stage/src/read/row_based/processors/reader.rs
@@ -134,8 +134,8 @@ impl PrefetchAsyncSource for BytesReader {
                 .op
                 .reader_with(&file.path)
                 .chunk(self.io_size)
-                // TODO: Use 4 concurrent for test.
-                .concurrent(4)
+                // TODO: Use 2 concurrent for test, let's extract as a new setting.
+                .concurrent(2)
                 .await?
                 .into_futures_async_read(0..file.size as u64);
             self.file_state = Some(FileState {
