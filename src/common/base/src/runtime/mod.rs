1ff87bb7c7ed50002e45dbef9def38c1380647e7
Winter Zhang <coswde@gmail.com>
2024-05-13 16:43:48 +0800

    chore(meta): tracking query id for meta log (#15497)

--- src/common/base/src/runtime/mod.rs
+++ src/common/base/src/runtime/mod.rs
@@ -49,6 +49,7 @@ pub use runtime::TrySpawn;
 pub use runtime::GLOBAL_TASK;
 pub use runtime_tracker::LimitMemGuard;
 pub use runtime_tracker::ThreadTracker;
+pub use runtime_tracker::TrackingGuard;
 pub use runtime_tracker::TrackingPayload;
 pub use runtime_tracker::UnlimitedFuture;
 pub use thread::Thread;
