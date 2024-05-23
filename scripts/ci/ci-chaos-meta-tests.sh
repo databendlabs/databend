7325a2f94b3a3f4d0ebd703f3373da62050aee2f
codedump <lichuang1982@gmail.com>
2024-05-23 22:26:31 +0800

    feat: add meta chaos action scripts

--- scripts/ci/ci-chaos-meta-tests.sh
+++ scripts/ci/ci-chaos-meta-tests.sh
@@ -6,6 +6,8 @@ set -e
 
 echo "install k3d"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.6.0 bash
+echo "k3d create cluster"
+k3d cluster create mycluster
 
 echo "install chaos mesh on k3d"
 curl -sSL https://mirrors.chaos-mesh.org/v2.0.2/install.sh | bash -s -- --k3s
