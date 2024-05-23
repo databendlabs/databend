ddf4099d3f1d3175b436c22d555a35e573704419
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-05-23 08:04:40 +0800

    refactor: polish memory usage when copy into parquet. (#15596)
    * refactor: polish memory usage when copy into parquet.
    
    1. serialize early to reduce memory usage.
    2. limit num of threads by memory and max_file_size.
    3. adjust default max_file_size.
    
    * fix

--- src/query/storages/stage/src/append/row_based_file/pipeline.rs
+++ src/query/storages/stage/src/append/row_based_file/pipeline.rs
@@ -18,6 +18,7 @@ use databend_common_catalog::plan::StageTableInfo;
 use databend_common_catalog::table_context::TableContext;
 use databend_common_exception::Result;
 use databend_common_formats::FileFormatOptionsExt;
+use databend_common_meta_app::principal::StageFileCompression;
 use databend_common_pipeline_core::Pipeline;
 use databend_common_pipeline_sources::input_formats::InputContext;
 use opendal::Operator;
@@ -26,20 +27,39 @@ use super::limit_file_size_processor::LimitFileSizeProcessor;
 use super::serialize_processor::SerializeProcessor;
 use super::writer_processor::RowBasedFileWriter;
 
-// SerializeProcessor * N: serialize each data block to many small byte buffers.
-// LimitFileSizeProcessor * 1:  group small byte buffers to batches (as a block meta) that are large enough as a file.
-// RowBasedFileSink * N: simply concat small byte buffers to a whole and write out.
+/// SerializeProcessor * N: serialize each data block to many small byte buffers.
+/// LimitFileSizeProcessor * 1:  group small byte buffers to batches (as a block meta) that are large enough as a file.
+/// RowBasedFileSink * N: simply concat small byte buffers to a whole and write out.
 #[allow(clippy::too_many_arguments)]
 pub(crate) fn append_data_to_row_based_files(
     pipeline: &mut Pipeline,
     ctx: Arc<dyn TableContext>,
     table_info: StageTableInfo,
     op: Operator,
-    max_file_size: usize,
-    max_threads: usize,
     uuid: String,
     group_id: &std::sync::atomic::AtomicUsize,
+    mem_limit: usize,
+    max_threads: usize,
 ) -> Result<()> {
+    let is_single = table_info.stage_info.copy_options.single;
+    let max_file_size = table_info.stage_info.copy_options.max_file_size;
+    let compression = table_info.stage_info.file_format_params.compression();
+    // when serializing block to parquet, the memory may be doubled
+    let mem_limit = mem_limit / 2;
+    let max_file_size = if is_single {
+        usize::MAX
+    } else if max_file_size == 0 {
+        if compression == StageFileCompression::None {
+            16 * 1024 * 1024
+        } else {
+            64 * 1024 * 1024
+        }
+    } else {
+        max_file_size.min(mem_limit)
+    };
+
+    let max_threads = max_threads.min(mem_limit / max_file_size).max(1);
+
     let mut options_ext = FileFormatOptionsExt::create_from_settings(&ctx.get_settings(), false)?;
     let output_format = options_ext.get_output_format(
         table_info.schema(),
