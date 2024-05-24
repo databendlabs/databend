cd31fcd3439853efbf02ef4dff2540459e0edf02
codedump <lichuang1982@gmail.com>
2024-05-24 16:16:17 +0800

    feat: add meta chaos action scripts

--- scripts/ci/ci-setup-chaos-meta.sh
+++ scripts/ci/ci-setup-chaos-meta.sh
@@ -19,6 +19,7 @@ curl -sSL https://mirrors.chaos-mesh.org/v2.6.3/install.sh | bash -s -- --k3s
 
 echo "kubectl get pods"
 kubectl get pods -o wide
+kubectl delete pvc --namespace databend  data-my-release-databend-meta-0 data-my-release-databend-meta-1 data-my-release-databend-meta-2 2>/dev/null
 
 echo "install helm"
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
@@ -26,7 +27,28 @@ helm version
 
 echo "heml start meta cluster"
 helm repo add databend https://charts.databend.rs
-helm install my-release databend/databend-meta --namespace databend --create-namespace --values scripts/ci/meta-chaos/meta-ha.yaml
+helm install my-release databend/databend-meta --namespace databend --create-namespace --values scripts/ci/meta-chaos/meta-ha.yaml --wait
 
-echo "get meta pods"
-kubectl get pods -o wide --namespace databend
+echo "check meta Running pods"
+MAX_WAIT_TIME=15
+countdown=$MAX_WAIT_TIME
+
+while [ $countdown -gt 0 ]; do
+    kubectl logs --namespace databend my-release-databend-meta-0 | tail -n 50
+    output=$(kubectl get pods -o wide --namespace databend | grep Running)
+
+    LINE_COUNT=$(echo "$output" | wc -l)
+
+    if [ $LINE_COUNT -eq 3 ]; then
+        echo "Success: The output has exactly 3 Running pods."
+        exit 0
+    fi
+
+    echo "try to check meta Running pods again.."
+    ((countdown--))
+
+    sleep 1
+done
+
+echo "Error: Timeout after $MAX_WAIT_TIME seconds."
+exit 1
