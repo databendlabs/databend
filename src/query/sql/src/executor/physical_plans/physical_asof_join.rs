b91b30c1e7d307c280788009396e686bac1a5e79
zenus <625305505@qq.com>
2024-05-25 21:00:53 +0800

    fixed

--- src/query/sql/src/executor/physical_plans/physical_asof_join.rs
+++ src/query/sql/src/executor/physical_plans/physical_asof_join.rs
@@ -342,9 +342,9 @@ impl PhysicalPlanBuilder {
         }
 
         let index = self.metadata.write().add_derived_column(
-            window.display_name.clone(),
-        window.func.return_type(),
-             None,
+	    window.display_name.clone(),
+            window.func.return_type(),
+            None,
         );
 
         let window_plan = Window {
