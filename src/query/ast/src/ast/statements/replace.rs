2951f38bebbad90c65d08ba9f9a8a00576ee1697
sundy-li <543950155@qq.com>
2024-05-28 17:00:13 +0800

    fix(query): add assert on explain parser

--- src/query/ast/src/ast/statements/replace.rs
+++ src/query/ast/src/ast/statements/replace.rs
@@ -51,13 +51,15 @@ impl Display for ReplaceStmt {
                 .chain(&self.database)
                 .chain(Some(&self.table)),
         )?;
+
         if !self.columns.is_empty() {
             write!(f, " (")?;
             write_comma_separated_list(f, &self.columns)?;
             write!(f, ")")?;
         }
-        write!(f, " ON CONFLICT")?;
+
         if !self.on_conflict_columns.is_empty() {
+            write!(f, " ON CONFLICT")?;
             write!(f, " (")?;
             write_comma_separated_list(f, &self.on_conflict_columns)?;
             write!(f, ")")?;
