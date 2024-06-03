6a65c785b47def8991b609b34dd487b98a8affbc
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-06-03 20:58:17 +0800

    feat: support input file format ORC.

---
+++ src/query/storages/orc/src/source.rs
@@ -0,0 +1,91 @@
+// Copyright 2021 Datafuse Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::sync::Arc;
+
+use databend_common_catalog::table_context::TableContext;
+use databend_common_exception::ErrorCode;
+use databend_common_exception::Result;
+use databend_common_expression::DataBlock;
+use databend_common_expression::DataSchema;
+use databend_common_pipeline_core::processors::OutputPort;
+use databend_common_pipeline_core::processors::ProcessorPtr;
+use databend_common_pipeline_sources::SyncSource;
+use databend_common_pipeline_sources::SyncSourcer;
+use opendal::Operator;
+use orc_rust::ArrowReader;
+use orc_rust::ArrowReaderBuilder;
+
+use super::file_reader::OrcFileReader;
+use crate::one_file_partition::OneFilePartition;
+
+pub struct ORCSource {
+    table_ctx: Arc<dyn TableContext>,
+    op: Operator,
+    reader: Option<ArrowReader<OrcFileReader>>,
+    data_schema: Arc<DataSchema>,
+}
+
+impl ORCSource {
+    pub fn try_create(
+        output: Arc<OutputPort>,
+        table_ctx: Arc<dyn TableContext>,
+        op: Operator,
+        data_schema: Arc<DataSchema>,
+    ) -> Result<ProcessorPtr> {
+        SyncSourcer::create(table_ctx.clone(), output, ORCSource {
+            table_ctx,
+            op,
+            data_schema,
+            reader: None,
+        })
+    }
+}
+
+impl SyncSource for ORCSource {
+    const NAME: &'static str = "ORCSource";
+
+    fn generate(&mut self) -> Result<Option<DataBlock>> {
+        if self.reader.is_none() {
+            let part = match self.table_ctx.get_partition() {
+                Some(part) => part,
+                None => return Ok(None),
+            };
+            let file = OneFilePartition::from_part(&part)?.clone();
+
+            let file = OrcFileReader {
+                operator: self.op.clone(),
+                size: file.size as u64,
+                path: file.path,
+            };
+            let reader = ArrowReaderBuilder::try_new(file).unwrap().build();
+
+            self.reader = Some(reader)
+        }
+        if let Some(reader) = &mut self.reader {
+            return match reader.next() {
+                None => Ok(None),
+                Some(batch) => {
+                    let (block, _) =
+                        DataBlock::from_record_batch(&self.data_schema.as_ref(), &batch?)?;
+                    Ok(Some(block))
+                }
+            };
+        } else {
+            Err(ErrorCode::Internal(
+                "Bug: ORCSource: should not be called with reader != None.",
+            ))
+        }
+    }
+}
---
+++ src/query/storages/orc/src/table.rs
@@ -0,0 +1,190 @@
+// Copyright 2021 Datafuse Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::any::Any;
+use std::sync::Arc;
+
+use arrow_array::RecordBatchReader;
+use arrow_schema::Schema as ArrowSchema;
+use chrono::DateTime;
+use databend_common_catalog::plan::DataSourceInfo;
+use databend_common_catalog::plan::DataSourcePlan;
+use databend_common_catalog::plan::PartStatistics;
+use databend_common_catalog::plan::Partitions;
+use databend_common_catalog::plan::PushDownInfo;
+use databend_common_catalog::plan::StageTableInfo;
+use databend_common_catalog::table::Table;
+use databend_common_catalog::table::TableStatistics;
+use databend_common_catalog::table_context::TableContext;
+use databend_common_exception::ErrorCode;
+use databend_common_exception::Result;
+use databend_common_expression::TableSchema;
+use databend_common_meta_app::principal::StageInfo;
+use databend_common_meta_app::schema::TableIdent;
+use databend_common_meta_app::schema::TableInfo;
+use databend_common_meta_app::schema::TableMeta;
+use databend_common_pipeline_core::Pipeline;
+use databend_common_storage::init_stage_operator;
+use databend_common_storage::StageFileInfo;
+use databend_storages_common_table_meta::table::ChangeType;
+use opendal::Operator;
+use orc_rust::ArrowReaderBuilder;
+
+use crate::file_reader::OrcFileReader;
+use crate::read_partition::read_partitions_simple;
+
+pub struct OrcTable {
+    pub(super) stage_table_info: StageTableInfo,
+    pub(super) table_info: TableInfo,
+}
+
+impl OrcTable {
+    pub fn from_info(stage_table_info: &StageTableInfo) -> Result<Arc<dyn Table>> {
+        let table_info = create_orc_table_info(
+            stage_table_info.schema.clone(),
+            &stage_table_info.stage_info,
+        )?;
+        Ok(Arc::new(OrcTable {
+            stage_table_info: stage_table_info.clone(),
+            table_info,
+        }))
+    }
+
+    #[async_backtrace::framed]
+    pub async fn try_create(mut stage_table_info: StageTableInfo) -> Result<Arc<dyn Table>> {
+        let stage_info = &stage_table_info.stage_info;
+        let files_to_read = &stage_table_info.files_to_copy;
+        let operator = init_stage_operator(&stage_info)?;
+        let first_file = match &files_to_read {
+            Some(files) => files[0].clone(),
+            None => stage_table_info
+                .files_info
+                .first_file(&operator)
+                .await?
+                .clone(),
+        };
+
+        let arrow_schema = Self::prepare_metas(first_file, operator.clone()).await?;
+
+        let table_schema = Arc::new(
+            TableSchema::try_from(arrow_schema.as_ref()).map_err(ErrorCode::from_std_error)?,
+        );
+
+        let table_info = create_orc_table_info(table_schema.clone(), &stage_info)?;
+
+        stage_table_info.schema = table_schema;
+
+        Ok(Arc::new(OrcTable {
+            table_info,
+            stage_table_info,
+        }))
+    }
+
+    #[async_backtrace::framed]
+    async fn prepare_metas(
+        file_info: StageFileInfo,
+        operator: Operator,
+    ) -> Result<Arc<ArrowSchema>> {
+        let file = OrcFileReader {
+            operator,
+            size: file_info.size,
+            path: file_info.path,
+        };
+        let arrow_schema = ArrowReaderBuilder::try_new(file).unwrap().build().schema();
+
+        // TableSchema::try_from(&arrow_schema).map_err(ErrorCode::from_std_error)
+        Ok(arrow_schema)
+    }
+}
+
+#[async_trait::async_trait]
+impl Table for OrcTable {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn is_local(&self) -> bool {
+        false
+    }
+
+    fn get_table_info(&self) -> &TableInfo {
+        &self.table_info
+    }
+
+    fn support_column_projection(&self) -> bool {
+        true
+    }
+
+    fn support_prewhere(&self) -> bool {
+        false
+    }
+
+    fn has_exact_total_row_count(&self) -> bool {
+        false
+    }
+
+    fn get_data_source_info(&self) -> DataSourceInfo {
+        DataSourceInfo::ORCSource(self.stage_table_info.clone())
+    }
+
+    /// The returned partitions only record the locations of files to read.
+    /// So they don't have any real statistics.
+    #[async_backtrace::framed]
+    async fn read_partitions(
+        &self,
+        ctx: Arc<dyn TableContext>,
+        _push_downs: Option<PushDownInfo>,
+        _dry_run: bool,
+    ) -> Result<(PartStatistics, Partitions)> {
+        read_partitions_simple(ctx, &self.stage_table_info).await
+    }
+
+    fn read_data(
+        &self,
+        ctx: Arc<dyn TableContext>,
+        plan: &DataSourcePlan,
+        pipeline: &mut Pipeline,
+        _put_cache: bool,
+    ) -> Result<()> {
+        self.do_read_data(ctx, plan, pipeline)
+    }
+
+    fn is_stage_table(&self) -> bool {
+        true
+    }
+
+    async fn table_statistics(
+        &self,
+        _ctx: Arc<dyn TableContext>,
+        _change_type: Option<ChangeType>,
+    ) -> Result<Option<TableStatistics>> {
+        Ok(None)
+    }
+}
+
+fn create_orc_table_info(schema: Arc<TableSchema>, stage_info: &StageInfo) -> Result<TableInfo> {
+    Ok(TableInfo {
+        ident: TableIdent::new(0, 0),
+        desc: "''.'orc_table'".to_string(),
+        name: format!("orc_table({})", stage_info.stage_name),
+        meta: TableMeta {
+            schema,
+            engine: "ORCTable".to_string(),
+            created_on: DateTime::from_timestamp(0, 0).unwrap(),
+            updated_on: DateTime::from_timestamp(0, 0).unwrap(),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+}
