767855cbd5935bfe8219081f055647f37c0d7907
everpcpc <git@everpcpc.com>
2024-06-04 17:25:27 +0800

    z

--- tests/logging/test-logs.sh
+++ tests/logging/test-logs.sh
@@ -3,5 +3,10 @@
 set -e
 
 BUILD_PROFILE="${BUILD_PROFILE:-debug}"
+SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+cd "$SCRIPT_PATH/../../tests" || exit
 
 curl --proto '=https' --tlsv1.2 -sSfL https://sh.vector.dev | sudo bash -s -- -y --prefix /usr/local
+
+echo "Starting standalone Databend Query with logging"
+./scripts/ci/deploy/databend-query-standalone-logging.sh
