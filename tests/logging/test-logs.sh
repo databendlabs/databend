1df8a244b1b6022cdd3a33c94fd90d293b9b9ec5
everpcpc <git@everpcpc.com>
2024-06-13 14:25:52 +0800

    z

--- tests/logging/test-logs.sh
+++ tests/logging/test-logs.sh
@@ -4,9 +4,19 @@ set -e
 
 BUILD_PROFILE="${BUILD_PROFILE:-debug}"
 SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
-cd "$SCRIPT_PATH/../../tests" || exit
+cd "$SCRIPT_PATH/../../" || exit
 
-curl --proto '=https' --tlsv1.2 -sSfL https://sh.vector.dev | sudo bash -s -- -y --prefix /usr/local
+if vector -V; then
+  echo "vector is already installed"
+else
+  echo "Installing vector" curl --proto '=https' --tlsv1.2 -sSfL https://sh.vector.dev | sudo bash -s -- -y --prefix /usr/local
+fi
 
 echo "Starting standalone Databend Query with logging"
 ./scripts/ci/deploy/databend-query-standalone-logging.sh
+
+echo "Starting Vector"
+killall vector || true
+sleep 1
+nohup vector --config-yaml ./tests/logging/vector/config.yaml &
+python3 scripts/ci/wait_tcp.py --timeout 10 --port 4317
