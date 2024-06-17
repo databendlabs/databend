bcc7f30eff5c0c8844f7897200b7d46569ddc4e1
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-06-17 16:45:31 +0800

    ci: test load parquet unloaded by databend.

--- tests/suites/1_stateful/05_formats/05_05_parquet/05_05_01_parquet_load_unload.sh
+++ tests/suites/1_stateful/05_formats/05_05_parquet/05_05_01_parquet_load_unload.sh
@@ -1,67 +1,72 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-# . "$CURDIR"/../../../../shell_env.sh
+ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+ . "$CURDIR"/../../../../shell_env.sh
 
-# echo "drop table if exists test_load_unload" | $BENDSQL_CLIENT_CONNECT
+ stmt "drop table if exists test_load_unload"
 
-# echo "CREATE TABLE test_load_unload
-# (
-#     a VARCHAR NULL,
-#     b float,
-#     c array(string not null),
-#     d Variant,
-#     e timestamp,
-#     f decimal(4, 2),
-#     h tuple(string not null,int not null)
-# );" | $BENDSQL_CLIENT_CONNECT
+ stmt "CREATE TABLE test_load_unload
+ (
+     a VARCHAR NULL,
+     b float,
+     c array(string not null),
+     d Variant,
+     e timestamp,
+     f decimal(4, 2),
+     g tuple(string not null,int not null),
+     h array(variant),
+     i array(tuple(string, int))
+ );"
 
-# insert_data() {
-# 	echo "insert into test_load_unload values
-# 	('a\"b', 1, ['a\"b'], parse_json('{\"k\":\"v\"}'), '2044-05-06T03:25:02.868894-07:00', 010.011, ('a', 5)),
-# 	(null, 2, ['a\'b'], parse_json('[1]'), '2044-05-06T03:25:02.868894-07:00', -010.011, ('b',10))
-# 	" | $BENDSQL_CLIENT_CONNECT
-# }
+ insert_data() {
+ 	stmt "insert into test_load_unload values
+ 	('a\"b', 1, ['a\"b'], parse_json('{\"k\":\"v\"}'), '2044-05-06T03:25:02.868894-07:00', 010.011, ('a', 5),
+ 		[parse_json('{\"k\":\"v\"}')],
+ 		[('a',5)]
+ 	),
+ 	(null, 2, ['a\'b'], parse_json('[1]'), '2044-05-06T03:25:02.868894-07:00', -010.011, ('b',10),
+ 		[parse_json('[1]')],
+ 		[('b',10)]
+ 	);"
+ }
 
-# test_format() {
-# 	rm -rf /tmp/test_load_unload
-# 	mkdir /tmp/test_load_unload
-# 	echo "drop stage if exists s1" | $BENDSQL_CLIENT_CONNECT
-# 	echo "create stage s1 url='fs:///tmp/test_load_unload/'" | $BENDSQL_CLIENT_CONNECT
-# 	echo "truncate table test_load_unload" | $BENDSQL_CLIENT_CONNECT
+ test_format() {
+ 	rm -rf /tmp/test_load_unload
+ 	mkdir /tmp/test_load_unload
+ 	stmt "drop stage if exists s1"
+ 	stmt "create stage s1 url='fs:///tmp/test_load_unload/'"
+ 	stmt "truncate table test_load_unload"
 
-# 	# insert data
-# 	insert_data
+ 	# insert data
+ 	insert_data
 
-# 	# unload1
-# 	echo "copy into @s1/unload1/ from test_load_unload" | $BENDSQL_CLIENT_CONNECT
-# 	mv `ls /tmp/test_load_unload/unload1/*` /tmp/test_load_unload/unload1.parquet
+ 	# unload1
+ 	query "select * from test_load_unload"
+ 	query "select * from test_load_unload" > /tmp/test_load_unload/select1.txt
+ 	stmt "copy into @s1/unload1/ from test_load_unload"
+ 	mv `ls /tmp/test_load_unload/unload1/*` /tmp/test_load_unload/unload1.parquet
 
-# 	echo "truncate table test_load_unload" | $BENDSQL_CLIENT_CONNECT
+ 	# reload with copy into table
+ 	stmt "truncate table test_load_unload"
+ 	stmt "copy into test_load_unload from @s1/unload1.parquet force=true;"
 
-# 	# load streaming
-# 	curl -sH "insert_sql:insert into test_load_unload file_format = (type = ${1})" \
-# 	-F "upload=@/tmp/test_load_unload/unload1.parquet" \
-# 	-u root: -XPUT "http://localhost:${QUERY_HTTP_HANDLER_PORT}/v1/streaming_load" | grep -c "SUCCESS"
+ 	# unload2
+ 	query "select * from test_load_unload" > /tmp/test_load_unload/select2.txt
 
-# 	# unload2
-# 	echo "copy into @s1/unload2/ from test_load_unload" | $BENDSQL_CLIENT_CONNECT
-# 	mv `ls /tmp/test_load_unload/unload2/*` /tmp/test_load_unload/unload2.parquet
+	echo "begin diff select"
+ 	diff /tmp/test_load_unload/select1.txt /tmp/test_load_unload/select2.txt
+	echo "end diff"
 
-# 	echo "truncate table test_load_unload" | $BENDSQL_CLIENT_CONNECT
+ 	stmt "copy into @s1/unload2/ from test_load_unload"
+ 	mv `ls /tmp/test_load_unload/unload2/*` /tmp/test_load_unload/unload2.parquet
 
-# 	# load with copy into table
-# 	echo "copy into test_load_unload from @s1/unload1.parquet force=true;" | $BENDSQL_CLIENT_CONNECT
 
-# 	# unload3
-# 	echo "copy into @s1/unload3/ from test_load_unload" | $BENDSQL_CLIENT_CONNECT
-# 	mv `ls /tmp/test_load_unload/unload3/*` /tmp/test_load_unload/unload3.parquet
+	echo "begin diff parquet"
+ 	diff /tmp/test_load_unload/unload2.parquet /tmp/test_load_unload/unload1.parquet
+	echo "end diff"
+ 	stmt "truncate table test_load_unload"
+ }
 
-# 	diff /tmp/test_load_unload/unload2.parquet /tmp/test_load_unload/unload1.parquet
-# 	diff /tmp/test_load_unload/unload3.parquet /tmp/test_load_unload/unload1.parquet
-# 	echo "truncate table test_load_unload" | $BENDSQL_CLIENT_CONNECT
-# }
+ test_format "PARQUET"
 
-# test_format "PARQUET"
-
-# echo "drop table if exists test_load_unload" | $BENDSQL_CLIENT_CONNECT
+ echo "drop table if exists test_load_unload" | $BENDSQL_CLIENT_CONNECT
