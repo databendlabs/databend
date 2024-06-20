62f28153b0ae50b7b79dd4280b262790406c4671
TCeason <33082201+TCeason@users.noreply.github.com>
2024-06-20 16:40:32 +0800

    chore(query): show view just display view name (#15841)
    * chore(query): show view just display view name
    
    * add test

--- tests/suites/0_stateless/12_time_travel/12_0002_time_travel_show_tables_hist.sql
+++ tests/suites/0_stateless/12_time_travel/12_0002_time_travel_show_tables_hist.sql
@@ -3,8 +3,9 @@ CREATE DATABASE db12_0002;
 USE db12_0002;
 
 CREATE TABLE t(c1 int);
-
+create view v_t as select * from t;
 show full tables from db12_0002;
+show full views from db12_0002;
 
 DROP TABLE t;
 
