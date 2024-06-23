234a4f9cab75ae43bb30ff8da2d4da8b97131eaf
Dousir9 <736191200@qq.com>
2024-06-23 16:17:19 +0800

    chore: row fetch warp nullable for right outer join

--- src/query/sql/src/executor/physical_plans/physical_merge_into.rs
+++ src/query/sql/src/executor/physical_plans/physical_merge_into.rs
@@ -46,6 +46,7 @@ use crate::executor::physical_plans::RowFetch;
 use crate::executor::PhysicalPlanBuilder;
 use crate::optimizer::ColumnSet;
 use crate::optimizer::SExpr;
+use crate::plans::JoinType;
 use crate::plans;
 use crate::ColumnEntry;
 use crate::IndexType;
@@ -135,6 +136,7 @@ impl PhysicalPlanBuilder {
 
         let mut builder = PhysicalPlanBuilder::new(meta_data.clone(), self.ctx.clone(), false);
         let mut plan = builder.build(s_expr.child(0)?, columns_set.clone()).await?;
+        let join_type = plan.into_hash_join()?.join_type;
 
         let join_output_schema = plan.output_schema()?;
         let is_insert_only = matches!(merge_type, MergeIntoType::InsertOnly);
@@ -217,6 +219,7 @@ impl PhysicalPlanBuilder {
                 row_id_col_offset: row_id_offset,
                 cols_to_fetch,
                 fetched_fields,
+                need_wrap_nullable: matches!(join_type, JoinType::Right),
                 stat_info: None,
             });
         }
