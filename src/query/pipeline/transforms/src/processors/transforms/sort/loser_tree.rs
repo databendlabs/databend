b948302275e581f058d79ab12894e4823ffbf351
coldWater <forsaken628@gmail.com>
2024-06-27 10:54:14 +0800

    fix
    Signed-off-by: coldWater <forsaken628@gmail.com>

--- src/query/pipeline/transforms/src/processors/transforms/sort/loser_tree.rs
+++ src/query/pipeline/transforms/src/processors/transforms/sort/loser_tree.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::cmp::Ordering;
-
 pub struct LoserTree<T: Ord> {
     ready: bool,
     tree: Vec<Option<usize>>,
