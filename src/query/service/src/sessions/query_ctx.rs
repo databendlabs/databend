54958db31eafd2404cabd8cf8789f9cfa8491d8e
Xuanwo <github@xuanwo.io>
2024-06-27 11:54:59 +0800

    refactor: Make CatalogCreator Accept Arc<CatalogInfo> (#15906)
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/query/service/src/sessions/query_ctx.rs
+++ src/query/service/src/sessions/query_ctx.rs
@@ -177,7 +177,7 @@ impl QueryContext {
         let catalog = self
             .shared
             .catalog_manager
-            .build_catalog(&table_info.catalog_info, self.txn_mgr())?;
+            .build_catalog(table_info.catalog_info.clone(), self.txn_mgr())?;
         match table_args {
             None => {
                 let table = catalog.get_table_by_info(table_info);
