1c786295b352d4fbb741d6a9324bfc83731268cb
zhang2014 <coswde@gmail.com>
2024-06-28 14:04:56 +0800

    fix(query): fix expr stack overflow

--- tests/suites/0_stateless/20+_others/20_0017_stackoverflow.sh
+++ tests/suites/0_stateless/20+_others/20_0017_stackoverflow.sh
@@ -10,3 +10,18 @@ for i in `seq 1 1000`;do
 done
 
 echo "SELECT * FROM ($SQL) LIMIT 1000" |$BENDSQL_CLIENT_CONNECT |wc -l
+
+echo "DROP TABLE IF EXISTS t1"|$BENDSQL_CLIENT_CONNECT
+echo "CREATE TABLE t1(id VARCHAR NULL, timestamp TIMESTAMP NULL, type VARCHAR NULL)" |$BENDSQL_CLIENT_CONNECT
+
+SQL="SELECT * FROM t1 WHERE 1 = 1 AND((timestamp = '2024-05-05 18:05:20' AND type = '1' AND id = 'xx')"
+
+for i in `seq 1 300`;do
+  SQL="$SQL OR (timestamp = '2024-05-05 18:05:20' AND type = '1' AND id = 'xx')"
+done
+
+SQL="$SQL)"
+
+echo "$SQL"|$BENDSQL_CLIENT_CONNECT
+
+echo "DROP TABLE IF EXISTS t1"|$BENDSQL_CLIENT_CONNECT
