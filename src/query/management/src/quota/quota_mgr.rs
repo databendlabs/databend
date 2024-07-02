34c28d00e5b0b84e58286a48bde2fb43f2c302bd
coldWater <forsaken628@gmail.com>
2024-07-02 11:15:31 +0800

    fix
    Signed-off-by: coldWater <forsaken628@gmail.com>

--- src/query/management/src/quota/quota_mgr.rs
+++ src/query/management/src/quota/quota_mgr.rs
@@ -35,14 +35,12 @@ use super::quota_api::QuotaApi;
 use crate::serde::check_and_upgrade_to_pb;
 use crate::serde::Quota;
 
-pub struct QuotaMgr<const W: bool = false> {
+pub struct QuotaMgr<const WRITE_PB: bool = false> {
     kv_api: Arc<dyn kvapi::KVApi<Error = MetaError>>,
     ident: TenantQuotaIdent,
 }
 
-impl<const W: bool> QuotaMgr<W> {
-    const WRITE_PB: bool = W;
-
+impl<const WRITE_PB: bool> QuotaMgr<WRITE_PB> {
     pub fn create(kv_api: Arc<dyn kvapi::KVApi<Error = MetaError>>, tenant: &Tenant) -> Self {
         QuotaMgr {
             kv_api,
@@ -56,7 +54,7 @@ impl<const W: bool> QuotaMgr<W> {
 }
 
 #[async_trait::async_trait]
-impl<const W: bool> QuotaApi for QuotaMgr<W> {
+impl<const WRITE_PB: bool> QuotaApi for QuotaMgr<WRITE_PB> {
     #[async_backtrace::framed]
     async fn get_quota(&self, seq: MatchSeq) -> Result<SeqV<TenantQuota>> {
         let res = self.kv_api.get_kv(&self.key()).await?;
@@ -65,7 +63,7 @@ impl<const W: bool> QuotaApi for QuotaMgr<W> {
             Some(seq_value) => match seq.match_seq(&seq_value) {
                 Err(_) => Err(ErrorCode::TenantQuotaUnknown("Tenant does not exist.")),
                 Ok(_) => {
-                    let mut quota = if Self::WRITE_PB {
+                    let mut quota = if WRITE_PB {
                         Quota::new(func_name!())
                     } else {
                         // Do not serialize to protobuf format
@@ -89,7 +87,7 @@ impl<const W: bool> QuotaApi for QuotaMgr<W> {
 
     #[async_backtrace::framed]
     async fn set_quota(&self, quota: &TenantQuota, seq: MatchSeq) -> Result<u64> {
-        if Self::WRITE_PB {
+        if WRITE_PB {
             let res = self
                 .kv_api
                 .upsert_pb(&UpsertPB::update(self.ident.clone(), quota.clone()).with(seq))
