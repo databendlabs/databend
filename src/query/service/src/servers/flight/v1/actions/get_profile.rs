657b827dd74294acbfde0abf7e819d5c9e47713c
Liuqing Yue <dqhl76@gmail.com>
2024-07-03 19:03:12 +0800

    feat: support real-time retrieval of profiles from admin API (part 1) (#15958)
    * feat: support get profile from admin API(part 1)
    
    Signed-off-by: Liuqing Yue <dqhl76@gmail.com>
    
    * fix typo
    
    Signed-off-by: Liuqing Yue <dqhl76@gmail.com>
    
    * apply review suggestion and remove unnecessary Arc
    
    Signed-off-by: Liuqing Yue <dqhl76@gmail.com>
    
    * fix build
    
    Signed-off-by: Liuqing Yue <dqhl76@gmail.com>
    
    ---------
    
    Signed-off-by: Liuqing Yue <dqhl76@gmail.com>

--- src/query/service/src/servers/flight/v1/actions/get_profile.rs
+++ src/query/service/src/servers/flight/v1/actions/get_profile.rs
@@ -12,13 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod background_tasks;
-pub mod cluster;
-pub mod config;
-pub mod instance_status;
-pub mod processes;
-pub mod query_profiling;
-pub mod settings;
-pub mod stream_status;
-pub mod system;
-pub mod tenant_tables;
+use databend_common_exception::Result;
+use databend_common_pipeline_core::PlanProfile;
+
+use crate::servers::flight::v1::actions::create_session;
+
+pub static GET_PROFILE: &str = "/actions/get_profile";
+
+pub async fn get_profile(query_id: String) -> Result<Vec<PlanProfile>> {
+    let session = create_session()?;
+    let query_context = session.create_query_context().await?;
+    match query_context.get_session_by_id(&query_id) {
+        Some(session) => Ok(session.get_profile().unwrap_or_default()),
+        None => Ok(vec![]),
+    }
+}
