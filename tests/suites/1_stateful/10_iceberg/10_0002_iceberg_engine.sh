458b66574f99d51227006c19b5dce13bc2aba6b0
Xuanwo <github@xuanwo.io>
2024-07-03 21:29:31 +0800

    iceberg engine will support later
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- tests/suites/1_stateful/10_iceberg/10_0002_iceberg_engine.sh
+++ tests/suites/1_stateful/10_iceberg/10_0002_iceberg_engine.sh
@@ -7,20 +7,18 @@ ROOT=$(realpath "$CURDIR"/../../../data/iceberg/iceberg_ctl/iceberg_db/iceberg_t
 
 stmt "drop table if exists test_iceberg;"
 
-echo ">>>> create table test_iceberg engine = iceberg location = 'fs://\${ROOT}/';"
-echo "create table test_iceberg engine = iceberg location = 'fs://${ROOT}/';" | $BENDSQL_CLIENT_CONNECT
-
-query "select * from test_iceberg order by id, data;"
-
-stmt "drop table test_iceberg;"
-
-stmt "drop connection if exists iceberg_conn;"
-stmt "create connection iceberg_conn storage_type = 's3' access_key_id ='minioadmin' secret_access_key ='minioadmin' ENDPOINT_URL='http://127.0.0.1:9900';"
-
-echo ">>>> create table test_iceberg engine = iceberg location = 's3://testbucket/iceberg_ctl/iceberg_db/iceberg_tbl/'  connection_name = 'iceberg_conn' ;"
-echo "create table test_iceberg engine = iceberg location = 's3://testbucket/iceberg_ctl/iceberg_db/iceberg_tbl/' connection_name = 'iceberg_conn';" | $BENDSQL_CLIENT_CONNECT
-query "select * from test_iceberg order by id, data;"
-query "show create table test_iceberg;"
-stmt "drop table test_iceberg;"
-
-
+#echo ">>>> create table test_iceberg engine = iceberg location = 'fs://\${ROOT}/';"
+#echo "create table test_iceberg engine = iceberg location = 'fs://${ROOT}/';" | $BENDSQL_CLIENT_CONNECT
+#
+#query "select * from test_iceberg order by id, data;"
+#
+#stmt "drop table test_iceberg;"
+#
+#stmt "drop connection if exists iceberg_conn;"
+#stmt "create connection iceberg_conn storage_type = 's3' access_key_id ='minioadmin' secret_access_key ='minioadmin' ENDPOINT_URL='http://127.0.0.1:9900';"
+#
+#echo ">>>> create table test_iceberg engine = iceberg location = 's3://testbucket/iceberg_ctl/iceberg_db/iceberg_tbl/'  connection_name = 'iceberg_conn' ;"
+#echo "create table test_iceberg engine = iceberg location = 's3://testbucket/iceberg_ctl/iceberg_db/iceberg_tbl/' connection_name = 'iceberg_conn';" | $BENDSQL_CLIENT_CONNECT
+#query "select * from test_iceberg order by id, data;"
+#query "show create table test_iceberg;"
+#stmt "drop table test_iceberg;"
