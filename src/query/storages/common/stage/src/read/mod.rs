3452b9e350c1d573af4e4698d0f9ce74b711f2a4
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-07-08 21:25:06 +0800

    chore: orc use option missing_field_as.

--- src/query/storages/common/stage/src/read/mod.rs
+++ src/query/storages/common/stage/src/read/mod.rs
@@ -11,27 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+mod columnar;
+mod single_file_partition;
 
-#![allow(internal_features)]
-#![allow(clippy::uninlined_format_args)]
-#![allow(clippy::useless_asref)]
-#![allow(clippy::diverging_sub_expression)]
-#![feature(try_blocks)]
-#![feature(impl_trait_in_assoc_type)]
-#![feature(let_chains)]
-#![feature(core_intrinsics)]
-#![feature(int_roundings)]
-#![feature(box_patterns)]
-
-mod chunk_reader_impl;
-mod copy_into_table;
-mod hashable_schema;
-mod processors;
-mod read_partition;
-mod read_pipeline;
-mod strip;
-mod table;
-mod utils;
-
-pub use copy_into_table::OrcTableForCopy;
-pub use table::OrcTable;
+pub use columnar::*;
+pub use single_file_partition::SingleFilePartition;
