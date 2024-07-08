d5736a406e2c0fa81b06a2b123ae005e1e0c0cb1
everpcpc <everpcpc@users.noreply.github.com>
2024-07-08 16:53:48 +0800

    chore(ci): add license for integration test (#449)

--- cli/tests/http/05-stream.sh
+++ cli/tests/http/05-stream.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-## uncomment these when QUERY_DATABEND_ENTERPRISE_LICENSE env is set 
 
-# cat <<SQL | ${BENDSQL}
-# CREATE DATABASE IF NOT EXISTS stream_test;
+cat <<SQL | ${BENDSQL}
+CREATE DATABASE IF NOT EXISTS stream_test;
 
-# CREATE OR REPLACE TABLE stream_test.abc
-# (
-#     title VARCHAR,
-#     author VARCHAR,
-#     date VARCHAR
-# );
+CREATE OR REPLACE TABLE stream_test.abc
+(
+    title VARCHAR,
+    author VARCHAR,
+    date VARCHAR
+);
 
-# CREATE OR REPLACE STREAM stream_test.s on table stream_test.abc;
-# SQL
+CREATE OR REPLACE STREAM stream_test.s on table stream_test.abc;
+SQL
 
-# cat <<SQL | ${BENDSQL} -D stream_test
-# DROP TABLE abc
-# SQL
+cat <<SQL | ${BENDSQL} -D stream_test
+DROP TABLE abc
+SQL
 
 cat <<SQL | ${BENDSQL} -D stream_test
 select 1;
