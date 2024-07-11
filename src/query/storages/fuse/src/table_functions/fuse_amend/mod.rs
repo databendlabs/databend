ce41040c78de17eca227f138cc50038228b8d8cf
dantengsky <dantengsky@gmail.com>
2024-07-11 13:26:47 +0800

    cargo fmt

--- src/query/storages/fuse/src/table_functions/fuse_amend/mod.rs
+++ src/query/storages/fuse/src/table_functions/fuse_amend/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//mod fuse_amend_table;
+// mod fuse_amend_table;
 mod fuse_amend_table;
 
 pub use fuse_amend_table::FuseAmendTable;
