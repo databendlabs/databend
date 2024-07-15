ae0a2bbe9606bd49b947327f2cc8154a6844ab48
zhang2014 <coswde@gmail.com>
2024-07-15 15:59:11 +0800

    refactor(query): add crash handler

--- src/common/tracing/src/crash_hook.rs
+++ src/common/tracing/src/crash_hook.rs
@@ -1,10 +1,10 @@
-// Copyright 2023 Databend Cloud
+// Copyright 2021 Datafuse Labs
 //
-// Licensed under the Elastic License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     https://www.elastic.co/licensing/elastic-license
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
