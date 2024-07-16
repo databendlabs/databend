9116da2e6cd2f29f365c92e6f1c26f6f1869bf84
codedump <lichuang1982@gmail.com>
2024-07-16 18:23:39 +0800

    feat: add share integration test

--- scripts/ci/ci-run-share-tests.sh
+++ scripts/ci/ci-run-share-tests.sh
@@ -4,20 +4,6 @@
 
 set -e
 
-echo "*************************************"
-echo "* Setting STORAGE_TYPE to S3.       *"
-echo "*                                   *"
-echo "* Please make sure that S3 backend  *"
-echo "* is ready, and configured properly.*"
-echo "*************************************"
-export STORAGE_TYPE=s3
-export STORAGE_S3_BUCKET=testbucket
-export STORAGE_S3_ROOT=
-export STORAGE_S3_ENDPOINT_URL=http://127.0.0.1:9900
-export STORAGE_S3_ACCESS_KEY_ID=minioadmin
-export STORAGE_S3_SECRET_ACCESS_KEY=minioadmin
-export STORAGE_ALLOW_INSECURE=true
-
 echo "Install dependence"
 python3 -m pip install --quiet mysql-connector-python
 SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
