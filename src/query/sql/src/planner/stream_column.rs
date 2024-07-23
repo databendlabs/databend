9d97b89a6b6d02bb759b1998ea637dc0aae6e659
Dousir9 <736191200@qq.com>
2024-07-23 22:10:14 +0800

    chore(query): refine steam column code

--- src/query/sql/src/planner/stream_column.rs
+++ src/query/sql/src/planner/stream_column.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use databend_common_catalog::plan::StreamColumn;
@@ -51,160 +52,98 @@ impl StreamContext {
         is_delete: bool,
     ) -> Result<Self> {
         let input_schema = schema.remove_virtual_computed_fields();
-        let fields_num = input_schema.fields().len();
-        let mut exprs = Vec::with_capacity(3);
+        let num_fields = input_schema.fields().len();
 
-        let origin_version_col =
-            StreamColumn::new(ORIGIN_VERSION_COL_NAME, StreamColumnType::OriginVersion);
-        let version_type = Box::new(origin_version_col.data_type());
-        let origin_version_index = input_schema
-            .index_of(origin_version_col.column_name())
-            .unwrap();
-        let origin_version_scalar = ScalarExpr::BoundColumnRef(BoundColumnRef {
-            span: None,
-            column: ColumnBindingBuilder::new(
-                origin_version_col.column_name().to_string(),
-                origin_version_index,
-                version_type.clone(),
-                Visibility::Visible,
-            )
-            .build(),
-        });
-        let current_version_scalar = ScalarExpr::ConstantExpr(ConstantExpr {
-            span: None,
-            value: table_version.into(),
-        });
-        let version_predicate = ScalarExpr::FunctionCall(FunctionCall {
-            span: None,
-            func_name: "is_not_null".to_string(),
-            params: vec![],
-            arguments: vec![origin_version_scalar.clone()],
-        });
-        // if(is_not_null(_origin_version), _origin_version, _current_version)
-        let version_scalar = ScalarExpr::FunctionCall(FunctionCall {
-            span: None,
-            func_name: "if".to_string(),
-            params: vec![],
-            arguments: vec![
-                version_predicate,
-                origin_version_scalar,
-                current_version_scalar,
-            ],
-        });
-        exprs.push(
-            version_scalar
-                .as_expr()?
-                .project_column_ref(|col| col.index),
-        );
+        let stream_columns = [
+            StreamColumn::new(ORIGIN_VERSION_COL_NAME, StreamColumnType::OriginVersion),
+            StreamColumn::new(ORIGIN_BLOCK_ID_COL_NAME, StreamColumnType::OriginBlockId),
+            StreamColumn::new(
+                ORIGIN_BLOCK_ROW_NUM_COL_NAME,
+                StreamColumnType::OriginRowNum,
+            ),
+        ];
 
-        let origin_block_id_col =
-            StreamColumn::new(ORIGIN_BLOCK_ID_COL_NAME, StreamColumnType::OriginBlockId);
-        let block_id_type = Box::new(origin_block_id_col.data_type());
-        let origin_block_id_index = input_schema
-            .index_of(origin_block_id_col.column_name())
-            .unwrap();
-        let origin_block_id_scalar = ScalarExpr::BoundColumnRef(BoundColumnRef {
-            span: None,
-            column: ColumnBindingBuilder::new(
-                origin_block_id_col.column_name().to_string(),
-                origin_block_id_index,
-                block_id_type.clone(),
-                Visibility::Visible,
-            )
-            .build(),
-        });
-        let current_block_id_scalar = ScalarExpr::BoundColumnRef(BoundColumnRef {
-            span: None,
-            column: ColumnBindingBuilder::new(
-                CURRENT_BLOCK_ID_COL_NAME.to_string(),
-                fields_num + 1,
-                block_id_type,
-                Visibility::Visible,
-            )
-            .build(),
-        });
-        let block_id_predicate = ScalarExpr::FunctionCall(FunctionCall {
-            span: None,
-            func_name: "is_not_null".to_string(),
-            params: vec![],
-            arguments: vec![origin_block_id_scalar.clone()],
-        });
-        // if(is_not_null(_origin_block_id), _origin_block_id, _current_block_id)
-        let block_id_scalar = ScalarExpr::FunctionCall(FunctionCall {
-            span: None,
-            func_name: "if".to_string(),
-            params: vec![],
-            arguments: vec![
-                block_id_predicate,
-                origin_block_id_scalar,
-                current_block_id_scalar,
-            ],
-        });
-        exprs.push(
-            block_id_scalar
-                .as_expr()?
-                .project_column_ref(|col| col.index),
-        );
+        let mut new_schema_index = HashMap::with_capacity(stream_columns.len());
+        let mut exprs = Vec::with_capacity(stream_columns.len());
+        for stream_column in stream_columns.iter() {
+            let schema_index = input_schema.index_of(stream_column.column_name()).unwrap();
 
-        let origin_row_num_col = StreamColumn::new(
-            ORIGIN_BLOCK_ROW_NUM_COL_NAME,
-            StreamColumnType::OriginRowNum,
-        );
-        let row_num_type = Box::new(origin_row_num_col.data_type());
-        let origin_row_num_index = input_schema
-            .index_of(origin_row_num_col.column_name())
-            .unwrap();
-        let origin_row_num_scalar = ScalarExpr::BoundColumnRef(BoundColumnRef {
-            span: None,
-            column: ColumnBindingBuilder::new(
-                origin_row_num_col.column_name().to_string(),
-                origin_row_num_index,
-                row_num_type.clone(),
-                Visibility::Visible,
-            )
-            .build(),
-        });
-        let current_row_num_scalar = ScalarExpr::BoundColumnRef(BoundColumnRef {
-            span: None,
-            column: ColumnBindingBuilder::new(
-                CURRENT_BLOCK_ROW_NUM_COL_NAME.to_string(),
-                fields_num,
-                row_num_type,
-                Visibility::Visible,
-            )
-            .build(),
-        });
-        let row_num_predicate = ScalarExpr::FunctionCall(FunctionCall {
-            span: None,
-            func_name: "is_not_null".to_string(),
-            params: vec![],
-            arguments: vec![origin_row_num_scalar.clone()],
-        });
-        // if(is_not_null(_origin_block_row_num), _origin_block_row_num, _current_block_row_num)
-        let row_num_scalar = ScalarExpr::FunctionCall(FunctionCall {
-            span: None,
-            func_name: "if".to_string(),
-            params: vec![],
-            arguments: vec![
-                row_num_predicate,
-                origin_row_num_scalar,
-                current_row_num_scalar,
-            ],
-        });
-        exprs.push(
-            row_num_scalar
-                .as_expr()?
-                .project_column_ref(|col| col.index),
-        );
+            let origin_stream_column_scalar_expr = ScalarExpr::BoundColumnRef(BoundColumnRef {
+                span: None,
+                column: ColumnBindingBuilder::new(
+                    stream_column.column_name().to_string(),
+                    schema_index,
+                    Box::new(stream_column.data_type()),
+                    Visibility::Visible,
+                )
+                .build(),
+            });
 
-        let mut projections = Vec::with_capacity(fields_num);
-        for i in 0..fields_num {
-            if i == origin_version_index {
-                projections.push(fields_num + 2);
-            } else if i == origin_block_id_index {
-                projections.push(fields_num + 3);
-            } else if i == origin_row_num_index {
-                projections.push(fields_num + 4);
+            let current_stream_column_scalar_expr = match stream_column.column_type() {
+                StreamColumnType::OriginVersion => {
+                    new_schema_index.insert(schema_index, num_fields + 2);
+                    ScalarExpr::ConstantExpr(ConstantExpr {
+                        span: None,
+                        value: table_version.into(),
+                    })
+                }
+                StreamColumnType::OriginBlockId => {
+                    new_schema_index.insert(schema_index, num_fields + 3);
+                    ScalarExpr::BoundColumnRef(BoundColumnRef {
+                        span: None,
+                        column: ColumnBindingBuilder::new(
+                            CURRENT_BLOCK_ID_COL_NAME.to_string(),
+                            num_fields + 1,
+                            Box::new(stream_column.data_type()),
+                            Visibility::Visible,
+                        )
+                        .build(),
+                    })
+                }
+                StreamColumnType::OriginRowNum => {
+                    new_schema_index.insert(schema_index, num_fields + 4);
+                    ScalarExpr::BoundColumnRef(BoundColumnRef {
+                        span: None,
+                        column: ColumnBindingBuilder::new(
+                            CURRENT_BLOCK_ROW_NUM_COL_NAME.to_string(),
+                            num_fields,
+                            Box::new(stream_column.data_type()),
+                            Visibility::Visible,
+                        )
+                        .build(),
+                    })
+                }
+                StreamColumnType::RowVersion => unreachable!(),
+            };
+
+            let new_stream_column_scalar_expr = ScalarExpr::FunctionCall(FunctionCall {
+                span: None,
+                func_name: "if".to_string(),
+                params: vec![],
+                arguments: vec![
+                    ScalarExpr::FunctionCall(FunctionCall {
+                        span: None,
+                        func_name: "is_not_null".to_string(),
+                        params: vec![],
+                        arguments: vec![origin_stream_column_scalar_expr.clone()],
+                    }),
+                    origin_stream_column_scalar_expr,
+                    current_stream_column_scalar_expr,
+                ],
+            });
+
+            exprs.push(
+                new_stream_column_scalar_expr
+                    .as_expr()?
+                    .project_column_ref(|col| col.index),
+            );
+        }
+
+        // Add projection to keep input schema.
+        let mut projections = Vec::with_capacity(num_fields);
+        for i in 0..num_fields {
+            if let Some(index) = new_schema_index.get(&i) {
+                projections.push(*index);
             } else {
                 projections.push(i);
             }
@@ -219,11 +158,15 @@ impl StreamContext {
                 projection: projections,
             },
         ];
+
         let stream_columns = if is_delete {
-            vec![origin_block_id_col]
+            // ORIGIN_BLOCK_ID.
+            vec![stream_columns[1].clone()]
         } else {
-            vec![origin_row_num_col, origin_block_id_col]
+            // ORIGIN_BLOCK_ROW_NUM, ORIGIN_BLOCK_ID.
+            vec![stream_columns[2].clone(), stream_columns[1].clone()]
         };
+
         Ok(StreamContext {
             stream_columns,
             operators,
