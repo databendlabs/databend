12b540389af473e27dc04f6e5c7060f7ac7ff6a4
xudong963 <wxd963996380@gmail.com>
2024-07-24 10:41:12 +0800

    update

--- src/query/settings/src/settings_getter_setter.rs
+++ src/query/settings/src/settings_getter_setter.rs
@@ -488,6 +488,10 @@ impl Settings {
         Ok(self.try_get_u64("enable_distributed_compact")? != 0)
     }
 
+    pub fn get_enable_analyze_histogram(&self) -> Result<bool> {
+        Ok(self.try_get_u64("enable_analyze_histogram")? != 0)
+    }
+
     pub fn get_enable_aggregating_index_scan(&self) -> Result<bool> {
         Ok(self.try_get_u64("enable_aggregating_index_scan")? != 0)
     }
