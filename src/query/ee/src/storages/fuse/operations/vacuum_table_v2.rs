3351efe0fc5dd218344662e32ee1f553a51beebf
sky <3374614481@qq.com>
2024-07-29 11:18:14 +0800

    add more log

--- src/query/ee/src/storages/fuse/operations/vacuum_table_v2.rs
+++ src/query/ee/src/storages/fuse/operations/vacuum_table_v2.rs
@@ -38,10 +38,18 @@ use uuid::Version;
 
 #[async_backtrace::framed]
 pub async fn do_vacuum2(fuse_table: &FuseTable, ctx: Arc<dyn TableContext>) -> Result<Vec<String>> {
+    let start = std::time::Instant::now();
     let Some(lvt) = set_lvt(fuse_table, ctx.as_ref()).await? else {
         return Ok(vec![]);
     };
+    info!(
+        "set lvt for table {} takes {:?}, lvt: {:?}",
+        fuse_table.get_table_info().desc,
+        start.elapsed(),
+        lvt
+    );
 
+    let start = std::time::Instant::now();
     let snapshots_before_lvt = list_until_timestamp(
         fuse_table,
         &fuse_table.meta_location_generator().snapshot_dir(),
@@ -49,14 +57,31 @@ pub async fn do_vacuum2(fuse_table: &FuseTable, ctx: Arc<dyn TableContext>) -> R
         true,
     )
     .await?;
+    info!(
+        "list snapshots before lvt for table {} takes {:?}, snapshots_dir: {:?}, lvt: {:?}, snapshots: {:?}",
+        fuse_table.get_table_info().desc,
+        start.elapsed(),
+        fuse_table.meta_location_generator().snapshot_dir(),
+        lvt,
+        slice_summary(&snapshots_before_lvt)
+    );
 
+    let start = std::time::Instant::now();
     let Some((gc_root, snapshots_to_gc)) =
         select_gc_root(fuse_table, &snapshots_before_lvt).await?
     else {
         return Ok(vec![]);
     };
-    let least_visible_timestamp = gc_root.least_visible_timestamp.unwrap();
+    info!(
+        "select gc_root for table {} takes {:?}, gc_root: {:?}, snapshots_to_gc: {:?}",
+        fuse_table.get_table_info().desc,
+        start.elapsed(),
+        gc_root,
+        slice_summary(snapshots_to_gc)
+    );
 
+    let start = std::time::Instant::now();
+    let least_visible_timestamp = gc_root.least_visible_timestamp.unwrap();
     let gc_root_segments = gc_root
         .segments
         .iter()
@@ -69,11 +94,28 @@ pub async fn do_vacuum2(fuse_table: &FuseTable, ctx: Arc<dyn TableContext>) -> R
         false,
     )
     .await?;
+    info!(
+        "list segments before gc_root for table {} takes {:?}, segment_dir: {:?}, least_visible_timestamp: {:?}, segments: {:?}",
+        fuse_table.get_table_info().desc,
+        start.elapsed(),
+        fuse_table.meta_location_generator().segment_dir(),
+        least_visible_timestamp,
+        slice_summary(&segments_before_gc_root)
+    );
+
+    let start = std::time::Instant::now();
     let segments_to_gc: Vec<String> = segments_before_gc_root
         .into_iter()
         .filter(|s| !gc_root_segments.contains(s))
         .collect();
+    info!(
+        "Filter segments to gc for table {} takes {:?}, segments_to_gc: {:?}",
+        fuse_table.get_table_info().desc,
+        start.elapsed(),
+        slice_summary(&segments_to_gc)
+    );
 
+    let start = std::time::Instant::now();
     let segments_io =
         SegmentsIO::create(ctx.clone(), fuse_table.get_operator(), fuse_table.schema());
     let segments = segments_io
@@ -83,6 +125,13 @@ pub async fn do_vacuum2(fuse_table: &FuseTable, ctx: Arc<dyn TableContext>) -> R
     for segment in segments {
         gc_root_blocks.extend(segment?.blocks.iter().map(|b| b.location.0.clone()));
     }
+    info!(
+        "read segments for table {} takes {:?}",
+        fuse_table.get_table_info().desc,
+        start.elapsed(),
+    );
+
+    let start = std::time::Instant::now();
     let blocks_before_gc_root = list_until_timestamp(
         fuse_table,
         &fuse_table.meta_location_generator().block_dir(),
@@ -90,10 +139,28 @@ pub async fn do_vacuum2(fuse_table: &FuseTable, ctx: Arc<dyn TableContext>) -> R
         false,
     )
     .await?;
+    info!(
+        "list blocks before gc_root for table {} takes {:?}, block_dir: {:?}, least_visible_timestamp: {:?}, blocks: {:?}",
+        fuse_table.get_table_info().desc,
+        start.elapsed(),
+        fuse_table.meta_location_generator().block_dir(),
+        least_visible_timestamp,
+        slice_summary(&blocks_before_gc_root)
+    );
+
+    let start = std::time::Instant::now();
     let blocks_to_gc: Vec<String> = blocks_before_gc_root
         .into_iter()
         .filter(|b| !gc_root_blocks.contains(b))
         .collect();
+    info!(
+        "Filter blocks to gc for table {} takes {:?}, blocks_to_gc: {:?}",
+        fuse_table.get_table_info().desc,
+        start.elapsed(),
+        slice_summary(&blocks_to_gc)
+    );
+
+    let start = std::time::Instant::now();
     let files_to_gc = snapshots_to_gc
         .iter()
         .chain(segments_to_gc.iter())
@@ -102,6 +169,12 @@ pub async fn do_vacuum2(fuse_table: &FuseTable, ctx: Arc<dyn TableContext>) -> R
         .collect::<Vec<_>>();
     let op = Files::create(ctx, fuse_table.get_operator());
     op.remove_file_in_batch(&files_to_gc).await?;
+    info!(
+        "remove files for table {} takes {:?}, files_to_gc: {:?}",
+        fuse_table.get_table_info().desc,
+        start.elapsed(),
+        slice_summary(&files_to_gc)
+    );
     Ok(files_to_gc)
 }
 
@@ -229,4 +302,19 @@ async fn select_gc_root<'a>(
         snapshots_before_lvt
     );
     Ok(None)
+}
+
+fn slice_summary<T: std::fmt::Debug>(s: &[T]) -> String {
+    if s.len() > 10 {
+        let first_five = &s[..5];
+        let last_five = &s[s.len() - 5..];
+        format!(
+            "First five: {:?}, Last five: {:?},Len: {}",
+            first_five,
+            last_five,
+            s.len()
+        )
+    } else {
+        format!("{:?}", s)
+    }
 }
