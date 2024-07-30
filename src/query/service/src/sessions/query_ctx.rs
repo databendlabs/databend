27b0ed3734672cc3b4451b751e4cdadec48ba1e1
sky <3374614481@qq.com>
2024-07-30 16:37:51 +0800

    fix missing header

--- src/query/service/src/sessions/query_ctx.rs
+++ src/query/service/src/sessions/query_ctx.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use std::any::Any;
 use std::cmp::min;
 use std::collections::hash_map::Entry;
