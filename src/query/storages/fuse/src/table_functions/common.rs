373f24e6b885bdaaf824593782cd9943fbaf2f81
dantengsky <dantengsky@gmail.com>
2024-07-31 10:03:10 +0800

    wip

--- src/query/storages/fuse/src/table_functions/common.rs
+++ src/query/storages/fuse/src/table_functions/common.rs
@@ -14,13 +14,21 @@
 
 use std::sync::Arc;
 
+use databend_common_catalog::catalog_kind::CATALOG_DEFAULT;
+use databend_common_catalog::plan::DataSourcePlan;
 use databend_common_catalog::table_args::TableArgs;
+use databend_common_catalog::table_context::TableContext;
+use databend_common_expression::DataBlock;
+use databend_common_expression::TableSchemaRef;
 use databend_storages_common_table_meta::meta::TableSnapshot;
 use futures_util::TryStreamExt;
 
 use crate::io::MetaReaders;
 use crate::io::SnapshotHistoryReader;
+use crate::table_functions::parse_db_tb_opt_args;
 use crate::table_functions::string_literal;
+use crate::table_functions::FuseColumn;
+use crate::table_functions::SimpleTableFunc;
 use crate::FuseTable;
 
 pub struct CommonArgs {
@@ -73,4 +81,65 @@ pub async fn location_snapshot(
     }
 
     Ok(None)
+}
+
+#[async_trait::async_trait]
+pub trait CommonArgFunction {
+    async fn apply(
+        &self,
+        ctx: &Arc<dyn TableContext>,
+        tbl: &FuseTable,
+        snapshot: Arc<TableSnapshot>,
+        limit: Option<usize>,
+    ) -> databend_common_exception::Result<DataBlock>;
+}
+
+impl<T> SimpleTableFunc for T
+where T: CommonArgFunction + Send + Sync
+{
+    fn table_args(&self) -> Option<TableArgs> {
+        // Some((&self.args).into())
+        todo!()
+    }
+    fn schema(&self) -> TableSchemaRef {
+        // Self::schema()
+        todo!()
+    }
+
+    async fn apply(
+        &self,
+        ctx: &Arc<dyn TableContext>,
+        plan: &DataSourcePlan,
+    ) -> databend_common_exception::Result<Option<DataBlock>> {
+        todo!()
+        // let tenant_id = ctx.get_tenant();
+        // let tbl = ctx
+        //    .get_catalog(CATALOG_DEFAULT)
+        //    .await?
+        //    .get_table(
+        //        &tenant_id,
+        //        self.args.arg_database_name.as_str(),
+        //        self.args.arg_table_name.as_str(),
+        //    )
+        //    .await?;
+        // let limit = plan.push_downs.as_ref().and_then(|x| x.limit);
+        // let tbl = FuseTable::try_from_table(tbl.as_ref())?;
+        // Ok(Some(self.get_blocks(ctx, tbl, limit).await?))
+    }
+
+    fn create(table_args: TableArgs) -> databend_common_exception::Result<Self>
+    where Self: Sized {
+        todo!()
+        // let (arg_database_name, arg_table_name, arg_snapshot_id) = parse_db_tb_opt_args(
+        //    &table_args,
+        //    crate::table_functions::fuse_columns::fuse_column::FUSE_FUNC_COLUMN,
+        //)?;
+        // Ok(Self {
+        //    args: CommonArgs {
+        //        arg_database_name,
+        //        arg_table_name,
+        //        arg_snapshot_id,
+        //    },
+        //})
+    }
 }
