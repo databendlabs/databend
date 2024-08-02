63916880a5a18cdac367d9cd14c47f6a9fa36355
Winter Zhang <coswde@gmail.com>
2024-08-02 15:14:54 +0800

    Revert "refactor: add retry logic for flight service (#16097)"
    This reverts commit 8c38b973de774522ae17d00ced449f7f79aef3bb.

--- src/common/exception/src/exception_into.rs
+++ src/common/exception/src/exception_into.rs
@@ -325,13 +325,6 @@ impl From<tonic::Status> for ErrorCode {
             tonic::Code::Unknown => {
                 let details = status.details();
                 if details.is_empty() {
-                    if status.source().map_or(false, |e| e.is::<hyper::Error>()) {
-                        return ErrorCode::CannotConnectNode(format!(
-                            "{}, source: {:?}",
-                            status.message(),
-                            status.source()
-                        ));
-                    }
                     return ErrorCode::UnknownException(format!(
                         "{}, source: {:?}",
                         status.message(),
