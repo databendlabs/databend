09835abc2bf23785e5d6291b7efa7391bd2a3872
coldWater <forsaken628@gmail.com>
2024-08-04 15:28:21 +0800

    readme
    Signed-off-by: coldWater <forsaken628@gmail.com>

--- src/common/geobuf/src/geo_adapter.rs
+++ src/common/geobuf/src/geo_adapter.rs
@@ -1,94 +1,20 @@
 use flatbuffers::Vector;
 use geozero::error::GeozeroError;
+use geozero::GeozeroGeometry;
 use geozero::ToGeo;
 
 use super::geo_buf;
 use super::geo_buf::InnerObject;
 use super::geo_buf::Object;
-use super::Element;
 use super::Geometry;
 use super::GeometryBuilder;
-use super::Visitor;
-
-impl<V: Visitor> Element<V> for geo::Geometry {
-    fn accept(&self, visitor: &mut V) -> Result<(), GeozeroError> {
-        fn visit_points<V: Visitor>(
-            points: &[geo::Coord],
-            visitor: &mut V,
-            multi: bool,
-        ) -> Result<(), GeozeroError> {
-            visitor.visit_points_start(points.len())?;
-            for p in points.iter() {
-                visitor.visit_point(p.x, p.y, true)?;
-            }
-            visitor.visit_points_end(multi)
-        }
-        match self {
-            geo::Geometry::Point(point) => {
-                visitor.visit_point(point.0.x, point.0.y, false)?;
-                visitor.finish(geo_buf::ObjectKind::Point)
-            }
-            geo::Geometry::MultiPoint(points) => {
-                visitor.visit_points_start(points.len())?;
-                for p in points.iter() {
-                    visitor.visit_point(p.0.x, p.0.y, true)?;
-                }
-                visitor.visit_points_end(false)?;
-                visitor.finish(geo_buf::ObjectKind::MultiPoint)
-            }
-            geo::Geometry::LineString(line) => {
-                visit_points(&line.0, visitor, false)?;
-                visitor.finish(geo_buf::ObjectKind::LineString)
-            }
-            geo::Geometry::MultiLineString(lines) => {
-                visitor.visit_lines_start(lines.0.len())?;
-                for line in lines.0.iter() {
-                    visit_points(&line.0, visitor, true)?;
-                }
-                visitor.visit_lines_end()?;
-                visitor.finish(geo_buf::ObjectKind::MultiLineString)
-            }
-            geo::Geometry::Polygon(polygon) => {
-                visitor.visit_polygon_start(polygon.interiors().len() + 1)?;
-                visit_points(&polygon.exterior().0, visitor, true)?;
-                for ring in polygon.interiors() {
-                    visit_points(&ring.0, visitor, true)?;
-                }
-                visitor.visit_polygon_end(false)?;
-                visitor.finish(geo_buf::ObjectKind::Polygon)
-            }
-            geo::Geometry::MultiPolygon(polygons) => {
-                visitor.visit_polygons_start(polygons.0.len())?;
-                for polygon in polygons {
-                    visitor.visit_polygon_start(polygon.interiors().len() + 1)?;
-                    visit_points(&polygon.exterior().0, visitor, true)?;
-                    for ring in polygon.interiors() {
-                        visit_points(&ring.0, visitor, true)?;
-                    }
-                    visitor.visit_polygon_end(true)?;
-                }
-                visitor.visit_polygons_end()?;
-                visitor.finish(geo_buf::ObjectKind::MultiPolygon)
-            }
-            geo::Geometry::GeometryCollection(collection) => {
-                visitor.visit_collection_start(collection.len())?;
-                for geom in collection {
-                    geom.accept(visitor)?;
-                }
-                visitor.visit_collection_end()?;
-                visitor.finish(geo_buf::ObjectKind::Collection)
-            }
-            _ => unreachable!(),
-        }
-    }
-}
 
 impl TryFrom<&geo::Geometry<f64>> for Geometry {
     type Error = GeozeroError;
 
     fn try_from(geo: &geo::Geometry<f64>) -> Result<Self, Self::Error> {
         let mut builder = GeometryBuilder::new();
-        geo.accept(&mut builder)?;
+        geo.process_geom(&mut builder)?;
         Ok(builder.build())
     }
 }
