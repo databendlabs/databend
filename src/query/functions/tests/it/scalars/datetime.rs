86942fcc713f58c5ae65e869c89572c6ca26506f
Reclaimers <floran.nicoletti38@gmail.com>
2024-08-04 18:22:31 +0200

    Correction unti test

--- src/query/functions/tests/it/scalars/datetime.rs
+++ src/query/functions/tests/it/scalars/datetime.rs
@@ -67,8 +67,8 @@ fn test_convert_timezone(file: &mut impl Write) {
     run_ast(file, "convert_timezone(a,b,c,d)", &[
         ("a", DateType::from_data(vec![-100, 0, 100])),
         ("b", TimestampType::from_data(vec![12321231])),
-        ("c", Null::from_data()),
-        ("d", Null::from_data()),
+        ("c", None::Datetype),
+        ("d", None::TimestampType),
     ]);
 }
 
