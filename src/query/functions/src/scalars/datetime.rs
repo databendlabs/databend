819c22518722a78a6c0d67ad0aad5a433f9235da
Reclaimers <floran.nicoletti38@gmail.com>
2024-08-05 22:19:18 +0200

    Correction eval function 10

--- src/query/functions/src/scalars/datetime.rs
+++ src/query/functions/src/scalars/datetime.rs
@@ -27,7 +27,7 @@ use chrono_tz::Tz;
 use databend_common_arrow::arrow::bitmap::Bitmap;
 use databend_common_arrow::arrow::temporal_conversions::EPOCH_DAYS_FROM_CE;
 use databend_common_exception::ErrorCode;
-use databend_common_expression::{error_to_null, Scalar};
+use databend_common_expression::error_to_null;
 use databend_common_expression::types::date::check_date;
 use databend_common_expression::types::date::date_to_string;
 use databend_common_expression::types::date::string_to_date;
