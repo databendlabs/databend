871cb8b73edfcf1bb9e453366b38e2b9244fb258
Reclaimers <floran.nicoletti38@gmail.com>
2024-08-06 16:24:43 +0200

    Adding unit test 2

--- src/query/functions/tests/it/scalars/datetime.rs
+++ src/query/functions/tests/it/scalars/datetime.rs
@@ -37,9 +37,12 @@ fn test_datetime() {
     test_rounder_functions(file);
 }
 
-fn test_convert_timezone(file: &mut impl Write)
-{
-    run_ast(file, "convert_timezone('2024-08-06T14:30:00+02:00','America/New_York')", &[]);
+fn test_convert_timezone(file: &mut impl Write) {
+    run_ast(
+        file,
+        "convert_timezone('2024-08-06T14:30:00+02:00','America/New_York')",
+        &[],
+    );
 }
 
 fn test_to_timestamp(file: &mut impl Write) {
