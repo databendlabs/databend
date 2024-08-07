26c808fdfcfb367cdad16ea571d7b9ec602333ff
Reclaimers <floran.nicoletti38@gmail.com>
2024-08-07 23:08:30 +0200

    Correction convert_timezone 5

--- src/query/functions/src/scalars/datetime.rs
+++ src/query/functions/src/scalars/datetime.rs
@@ -140,17 +140,17 @@ fn int64_domain_to_timestamp_domain<T: AsPrimitive<i64>>(
 
 fn register_convert_timezone(registry: &mut FunctionRegistry) {
     // 2 arguments function [target_timezone, src_timestamp]
-    registry.register_passthrough_nullable_2_arg::<StringType, StringType, StringType, _, _>(
+    registry.register_passthrough_nullable_2_arg::<StringType, StringType, TimestampType, _, _>(
         "convert_timezone",
         |_, _, _| FunctionDomain::MayThrow,
         eval_convert_timezone,
     );
 
     // 3 arguments function [target_timezone, src_timestamp, src_timezone]
-    registry.register_passthrough_nullable_3_arg::<StringType, StringType, StringType, StringType, _, _>(
+    registry.register_passthrough_nullable_3_arg::<StringType, StringType, StringType, TimestampType, _, _>(
         "convert_timezone",
         |_, _, _, _| FunctionDomain::MayThrow,
-        vectorize_with_builder_3_arg::<StringType, StringType, StringType, StringType>(
+        vectorize_with_builder_3_arg::<StringType, StringType, StringType, TimestampType>(
             |target_tz, src_timestamp, src_tz, output, ctx| {
                 // Parsing parameters
                 let t_tz: Tz = match target_tz.parse() {
@@ -187,8 +187,8 @@ fn register_convert_timezone(registry: &mut FunctionRegistry) {
         target_tz: ValueRef<StringType>,
         src_timestamp: ValueRef<StringType>,
         ctx: &mut EvalContext,
-    ) -> ValueRef<StringType> {
-        vectorize_with_builder_2_arg::<StringType, StringType, StringType>(
+    ) -> Value<TimestampType> {
+        vectorize_with_builder_2_arg::<StringType, StringType, TimestampType>(
             |target_tz, src_timestamp, output, ctx| {
                 // Parsing parameters
                 let t_tz: Tz = match target_tz.parse() {
