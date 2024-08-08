f6e13777d0c4057670450dae57a6389a53b0a2cc
sundyli <543950155@qq.com>
2024-08-08 17:27:43 +0800

    refactor(query): use bigint to handle fallback overflow

--- src/query/functions/src/scalars/decimal/arithmetic.rs
+++ src/query/functions/src/scalars/decimal/arithmetic.rs
@@ -89,8 +89,7 @@ macro_rules! binary_decimal {
             let scale_b = $right.scale();
 
             // Note: the result scale is always larger than the left scale
-            let scale_mul = scale_b + $size.scale - scale_a;
-            let multiplier = T::e(scale_mul as u32);
+            let scale_mul = (scale_b + $size.scale - scale_a) as u32;
             let func = |a: T, b: T, result: &mut Vec<T>, ctx: &mut EvalContext| {
                 // We are using round div here which follow snowflake's behavior: https://docs.snowflake.com/sql-reference/operators-arithmetic
                 // For example:
@@ -102,7 +101,7 @@ macro_rules! binary_decimal {
                     ctx.set_error(result.len(), "divided by zero");
                     result.push(one);
                 } else {
-                   match a.do_round_div(b, multiplier) {
+                   match a.do_round_div(b, scale_mul) {
                         Some(t) => result.push(t),
                         None => {
                             ctx.set_error(
