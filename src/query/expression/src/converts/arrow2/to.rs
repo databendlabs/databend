4b5546c812428f2e1d81d6ff18b72e099dad51f5
coldWater <forsaken628@gmail.com>
2024-08-09 23:34:50 +0800

    fix
    Signed-off-by: coldWater <forsaken628@gmail.com>

--- src/query/expression/src/converts/arrow2/to.rs
+++ src/query/expression/src/converts/arrow2/to.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use databend_common_arrow::arrow::array::Array;
 use databend_common_arrow::arrow::bitmap::Bitmap;
 use databend_common_arrow::arrow::buffer::Buffer;
 use databend_common_arrow::arrow::datatypes::DataType as ArrowDataType;
