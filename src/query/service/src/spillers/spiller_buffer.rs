5d4951b76e9c07614d8ec15f19fd813ce7940928
Dousir9 <736191200@qq.com>
2024-08-16 23:05:29 +0800

    chore(query): use 1024 instead of 1000 in SpillBuffer

--- src/query/service/src/spillers/spiller_buffer.rs
+++ src/query/service/src/spillers/spiller_buffer.rs
@@ -27,7 +27,7 @@ impl SpillBuffer {
     pub fn create(num_partitions: usize, buffer_threshold: usize) -> Self {
         // The threshold of each partition, we will spill the partition data if the
         // size exceeds the threshold, the minimum threshold is 1MB.
-        let partition_threshold = std::cmp::max(buffer_threshold / num_partitions, 1) * 1000 * 1000;
+        let partition_threshold = std::cmp::max(buffer_threshold / num_partitions, 1) * 1024 * 1024;
         SpillBuffer {
             partition_data: vec![Vec::new(); num_partitions],
             partition_size: vec![0; num_partitions],
