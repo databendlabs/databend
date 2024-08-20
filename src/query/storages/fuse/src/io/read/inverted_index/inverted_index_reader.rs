d14f7a5ed1b7ca521ec28aa2085fa1a028c2dec9
baishen <baishen2009@gmail.com>
2024-08-20 23:03:47 +0800

    feat(query): Inverted index search function support options (#16256)
    * feat(query): Inverted index search function support options
    
    * add tests
    
    * create query in pruner
    
    * add tests
    
    * fix

--- src/query/storages/fuse/src/io/read/inverted_index/inverted_index_reader.rs
+++ src/query/storages/fuse/src/io/read/inverted_index/inverted_index_reader.rs
@@ -21,46 +21,30 @@ use databend_storages_common_index::InvertedIndexDirectory;
 use opendal::Operator;
 use tantivy::collector::DocSetCollector;
 use tantivy::collector::TopDocs;
-use tantivy::query::QueryParser;
-use tantivy::schema::Field;
+use tantivy::query::Query;
 use tantivy::tokenizer::TokenizerManager;
 use tantivy::Index;
-use tantivy::Score;
 
 use crate::io::read::inverted_index::inverted_index_loader::load_inverted_index_directory;
 use crate::io::read::inverted_index::inverted_index_loader::InvertedIndexFileReader;
 
 #[derive(Clone)]
 pub struct InvertedIndexReader {
-    has_score: bool,
-    query_fields: Vec<Field>,
-    query_field_boosts: Vec<(Field, Score)>,
     directory: InvertedIndexDirectory,
-    tokenizer_manager: TokenizerManager,
 }
 
 impl InvertedIndexReader {
     pub async fn try_create(
         dal: Operator,
         field_nums: usize,
-        has_score: bool,
         need_position: bool,
-        query_fields: Vec<Field>,
-        query_field_boosts: Vec<(Field, Score)>,
-        tokenizer_manager: TokenizerManager,
         index_loc: &str,
     ) -> Result<Self> {
         let directory =
             load_inverted_index_directory(dal.clone(), need_position, field_nums, index_loc)
                 .await?;
 
-        Ok(Self {
-            has_score,
-            query_fields,
-            query_field_boosts,
-            directory,
-            tokenizer_manager,
-        })
+        Ok(Self { directory })
     }
 
     // Filter the rows and scores in the block that can match the query text,
@@ -68,25 +52,20 @@ impl InvertedIndexReader {
     #[allow(clippy::type_complexity)]
     pub fn do_filter(
         self,
-        query: &str,
+        has_score: bool,
+        query: &dyn Query,
+        tokenizer_manager: TokenizerManager,
         row_count: u64,
     ) -> Result<Option<Vec<(usize, Option<F32>)>>> {
         let start = Instant::now();
         let mut index = Index::open(self.directory)?;
-        index.set_tokenizers(self.tokenizer_manager);
+        index.set_tokenizers(tokenizer_manager);
         let reader = index.reader()?;
         let searcher = reader.searcher();
 
-        let mut query_parser = QueryParser::for_index(&index, self.query_fields);
-        // set optional boost value for the field
-        for (field, boost) in &self.query_field_boosts {
-            query_parser.set_field_boost(*field, *boost);
-        }
-        let query = query_parser.parse_query(query)?;
-
-        let matched_rows = if self.has_score {
+        let matched_rows = if has_score {
             let collector = TopDocs::with_limit(row_count as usize);
-            let docs = searcher.search(&query, &collector)?;
+            let docs = searcher.search(query, &collector)?;
 
             let mut matched_rows = Vec::with_capacity(docs.len());
             for (score, doc_addr) in docs {
@@ -97,7 +76,7 @@ impl InvertedIndexReader {
             matched_rows
         } else {
             let collector = DocSetCollector;
-            let docs = searcher.search(&query, &collector)?;
+            let docs = searcher.search(query, &collector)?;
 
             let mut matched_rows = Vec::with_capacity(docs.len());
             for doc_addr in docs {
