64010ca0ca97975b445c3a7882260fefa90dfb5c
coldWater <forsaken628@gmail.com>
2024-08-27 13:22:28 +0800

    BlockStream
    Signed-off-by: coldWater <forsaken628@gmail.com>

--- src/query/pipeline/transforms/src/processors/transforms/transform_k_way_merge_sort.rs
+++ src/query/pipeline/transforms/src/processors/transforms/transform_k_way_merge_sort.rs
@@ -32,19 +32,77 @@ use super::sort::algorithm::SortAlgorithm;
 use super::sort::Merger;
 use super::sort::SortedStream;
 
-struct TaskInput {
-    input: Arc<InputPort>,
-    buffer: Vec<DataBlock>,
-    ready: bool,
+pub struct KWayMergePartitionProcessor {}
+
+impl Processor for KWayMergePartitionProcessor {
+    fn name(&self) -> String {
+        "KWayMergePartition".to_string()
+    }
+
+    fn as_any(&mut self) -> &mut dyn Any {
+        self
+    }
 }
 
-impl TaskInput {
-    fn new(input: Arc<InputPort>) -> TaskInput {
-        TaskInput {
+fn get_u32(block: &DataBlock, i: usize) -> u32 {
+    let n = block.num_columns();
+    let cols = block.columns();
+    unwrap_u32(&cols[n - i])
+}
+
+fn unwrap_u32(entry: &BlockEntry) -> u32 {
+    *entry
+        .value
+        .as_scalar()
+        .unwrap()
+        .as_number()
+        .unwrap()
+        .as_u_int32()
+        .unwrap()
+}
+
+pub struct KWayMergeWorkerProcessor<A>
+where A: SortAlgorithm
+{
+    input: Arc<InputPort>,
+    output: Arc<OutputPort>,
+    stream_count: usize,
+    schema: DataSchemaRef,
+    block_size: usize,
+    sort_desc: Arc<Vec<SortColumnDescription>>,
+    remove_order_col: bool,
+
+    buffer: Vec<DataBlock>,
+    ready: bool,
+    merger: Option<Merger<A, BlockStream>>,
+    output_data: VecDeque<DataBlock>,
+}
+
+impl<A> KWayMergeWorkerProcessor<A>
+where A: SortAlgorithm
+{
+    pub fn create(
+        input: Arc<InputPort>,
+        stream_count: usize,
+        output: Arc<OutputPort>,
+        schema: DataSchemaRef,
+        block_size: usize,
+        sort_desc: Arc<Vec<SortColumnDescription>>,
+        remove_order_col: bool,
+    ) -> Result<Self> {
+        Ok(Self {
+            merger: None,
             input,
+            output,
+            output_data: VecDeque::new(),
+            stream_count,
+            schema,
+            block_size,
+            sort_desc,
+            remove_order_col,
             buffer: Vec::new(),
             ready: false,
-        }
+        })
     }
 
     fn pull(&mut self) -> Result<(bool, bool)> {
@@ -58,11 +116,14 @@ impl TaskInput {
 
             let before = &self.buffer[0];
 
-            let task_id = get_task_id(&before);
-            assert_eq!(task_id, get_task_id(&block));
+            const TASK_ID_POS: usize = 3;
+            const TASK_ROWS_POS: usize = 2;
 
-            let task_rows = get_task_rows(&before);
-            debug_assert_eq!(task_rows, get_task_rows(&block));
+            let task_id = get_u32(&before, TASK_ID_POS);
+            assert_eq!(task_id, get_u32(&block, TASK_ID_POS));
+
+            let task_rows = get_u32(&before, TASK_ROWS_POS);
+            debug_assert_eq!(task_rows, get_u32(&block, TASK_ROWS_POS));
 
             let rows = self.buffer.iter().map(|b| b.num_rows() as u32).sum::<u32>()
                 + block.num_rows() as u32;
@@ -85,129 +146,34 @@ impl TaskInput {
         self.input.set_need_data();
         Ok((false, true))
     }
-}
 
-fn get_task_id(block: &DataBlock) -> u32 {
-    let n = block.num_columns();
-    debug_assert!(n >= 4);
-    let cols = block.columns();
-    unwrap_u32(&cols[n - 3])
-}
+    fn streams(&mut self) -> Vec<BlockStream> {
+        const INPUT_ID_POS: usize = 1;
 
-fn get_task_rows(block: &DataBlock) -> u32 {
-    let n = block.num_columns();
-    debug_assert!(n >= 4);
-    let cols = block.columns();
-    unwrap_u32(&cols[n - 2])
-}
+        let mut streams = vec![VecDeque::new(); self.stream_count];
 
-fn get_input_id(block: &DataBlock) -> u32 {
-    let n = block.num_columns();
-    debug_assert!(n >= 4);
-    let cols = block.columns();
-    unwrap_u32(&cols[n - 1])
-}
-
-fn unwrap_u32(entry: &BlockEntry) -> u32 {
-    *entry
-        .value
-        .as_scalar()
-        .unwrap()
-        .as_number()
-        .unwrap()
-        .as_u_int32()
-        .unwrap()
-}
-
-struct TaskStream {
-    input: Arc<Mutex<TaskInput>>,
-    id: u32,
-    remove_order_col: bool,
-}
-
-impl TaskStream {
-    fn new(input: Arc<Mutex<TaskInput>>, id: usize, remove_order_col: bool) -> TaskStream {
-        TaskStream {
-            input,
-            id: id as u32,
-            remove_order_col,
-        }
-    }
-}
-
-impl SortedStream for TaskStream {
-    fn next(&mut self) -> Result<(Option<(DataBlock, Column)>, bool)> {
-        let mut input = self.input.lock().unwrap();
-        if !input.ready || input.buffer.is_empty() {
-            match input.pull()? {
-                (_, true) => return Ok((None, true)),
-                (false, false) => return Ok((None, false)),
-                (true, false) => (),
-            }
-        }
-
-        for (i, b) in input.buffer.iter().enumerate() {
-            if get_input_id(b) != self.id {
-                continue;
-            }
-            let mut block = input.buffer.remove(i);
+        for mut block in self.buffer.drain(..) {
+            let id = get_u32(&block, INPUT_ID_POS) as usize;
 
             let n = block.num_columns();
             let columns = block.columns();
             let task_rows = columns[n - 2].clone();
             let task_id = columns[n - 3].clone();
             let sort_col = columns[n - 4].value.as_column().unwrap().clone();
+
             if self.remove_order_col {
                 block.pop_columns(4);
             } else {
                 block.pop_columns(3);
             }
+
             block.add_column(task_id);
             block.add_column(task_rows);
 
-            return Ok((Some((block, sort_col)), false));
+            streams[id].push_back((block, sort_col));
         }
-        unreachable!()
-    }
-}
-
-pub struct KWayMergeWorkerProcessor<A>
-where A: SortAlgorithm
-{
-    merger: Merger<A, TaskStream>,
-
-    input: Arc<InputPort>,
-    output: Arc<OutputPort>,
-
-    output_data: VecDeque<DataBlock>,
-}
-
-impl<A> KWayMergeWorkerProcessor<A>
-where A: SortAlgorithm
-{
-    pub fn create(
-        input: Arc<InputPort>,
-        stream_count: usize,
-        output: Arc<OutputPort>,
-        schema: DataSchemaRef,
-        block_size: usize,
-        limit: Option<usize>,
-        sort_desc: Arc<Vec<SortColumnDescription>>,
-        remove_order_col: bool,
-    ) -> Result<Self> {
-        let task_input = Arc::new(Mutex::new(TaskInput::new(input.clone())));
-
-        let streams = (0..stream_count)
-            .map(|id| TaskStream::new(task_input.clone(), id, remove_order_col))
-            .collect::<Vec<_>>();
-
-        let merger = Merger::<A, TaskStream>::create(schema, streams, sort_desc, block_size, limit);
-        Ok(Self {
-            merger,
-            input,
-            output,
-            output_data: VecDeque::new(),
-        })
+        self.ready = false;
+        streams
     }
 }
 
@@ -237,25 +203,146 @@ where A: SortAlgorithm + Send + 'static
             return Ok(Event::NeedConsume);
         }
 
-        if self.merger.is_finished() {
-            self.output.finish();
-            self.input.finish();
-            return Ok(Event::Finished);
-        }
-
-        self.merger.poll_pending_stream()?;
-
-        if self.merger.has_pending_stream() {
-            Ok(Event::NeedData)
-        } else {
-            Ok(Event::Sync)
+        match self.pull()? {
+            (_, true) => return Ok(Event::NeedData),
+            (false, false) => return Ok(Event::Finished),
+            (true, false) => Ok(Event::Sync),
         }
     }
 
     fn process(&mut self) -> Result<()> {
-        while let Some(block) = self.merger.next_block()? {
+        debug_assert!(self.ready);
+        let mut merger = Merger::<A, BlockStream>::create(
+            self.schema.clone(),
+            self.streams(),
+            self.sort_desc.clone(),
+            self.block_size,
+            None,
+        );
+        while let Some(block) = merger.next_block()? {
             self.output_data.push_back(block);
         }
+        debug_assert!(merger.is_finished());
         Ok(())
+    }
+}
+
+pub struct KWayMergeCombineProcessor {
+    inputs: Vec<Arc<InputPort>>,
+    output: Arc<OutputPort>,
+
+    cur: usize,
+    buffer: VecDeque<DataBlock>,
+}
+
+impl KWayMergeCombineProcessor {
+    pub fn create(
+        inputs: Vec<Arc<InputPort>>,
+        output: Arc<OutputPort>,
+        limit: Option<usize>,
+    ) -> Result<Self> {
+        Ok(Self {
+            inputs,
+            output,
+            cur: 0,
+            buffer: VecDeque::new(),
+        })
+    }
+
+    fn pull(&mut self) -> Result<(bool, bool)> {
+        let input = &self.inputs[self.cur];
+
+        while input.has_data() {
+            let block = input.pull_data().unwrap()?;
+
+            if self.buffer.is_empty() {
+                self.buffer.push_back(block);
+                continue;
+            }
+
+            let before = &self.buffer[0];
+
+            const TASK_ID_POS: usize = 2;
+            const TASK_ROWS_POS: usize = 1;
+
+            let task_id = get_u32(&before, TASK_ID_POS);
+            assert_eq!(task_id, get_u32(&block, TASK_ID_POS));
+
+            let task_rows = get_u32(&before, TASK_ROWS_POS);
+            debug_assert_eq!(task_rows, get_u32(&block, TASK_ROWS_POS));
+
+            let rows = self.buffer.iter().map(|b| b.num_rows() as u32).sum::<u32>()
+                + block.num_rows() as u32;
+
+            if task_rows > rows {
+                self.buffer.push_back(block);
+                continue;
+            } else if task_rows == rows {
+                input.set_not_need_data();
+                self.cur += 1;
+                return Ok((true, false));
+            } else {
+                unreachable!()
+            }
+        }
+        if input.is_finished() {
+            return Ok((false, false));
+        }
+        input.set_need_data();
+        Ok((false, true))
+    }
+}
+
+impl Processor for KWayMergeCombineProcessor {
+    fn name(&self) -> String {
+        "KWayMergeCombine".to_string()
+    }
+
+    fn as_any(&mut self) -> &mut dyn Any {
+        self
+    }
+
+    fn event(&mut self) -> Result<Event> {
+        if self.output.is_finished() {
+            for input in self.inputs.iter() {
+                input.finish();
+            }
+            return Ok(Event::Finished);
+        }
+
+        if !self.output.can_push() {
+            return Ok(Event::NeedConsume);
+        }
+
+        if !self.buffer.is_empty() {
+            while let Some(block) = self.buffer.pop_front() {
+                self.output.push_data(Ok(block));
+            }
+            return Ok(Event::NeedConsume);
+        }
+
+        let (data, pending) = self.pull()?;
+
+        match self.pull()? {
+            (_, true) => return Ok(Event::NeedData),
+            (false, false) => {
+                return Ok(Event::Finished);
+                // todo
+            }
+            (true, false) => {
+                while let Some(block) = self.buffer.pop_front() {
+                    self.output.push_data(Ok(block));
+                }
+                return Ok(Event::NeedConsume);
+            }
+        }
+    }
+}
+
+type BlockStream = VecDeque<(DataBlock, Column)>;
+
+impl SortedStream for BlockStream {
+    fn next(&mut self) -> Result<(Option<(DataBlock, Column)>, bool)> {
+        Ok((self.pop_front(), false))
     }
 }
