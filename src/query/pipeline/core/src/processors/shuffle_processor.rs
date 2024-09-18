20298afebc6f6529086754311b8f55a531c986b5
Dousir9 <736191200@qq.com>
2024-09-18 12:00:32 +0800

    chore(query): new shuffle processor

--- src/query/pipeline/core/src/processors/shuffle_processor.rs
+++ src/query/pipeline/core/src/processors/shuffle_processor.rs
@@ -18,34 +18,49 @@ use std::sync::Arc;
 use databend_common_exception::Result;
 
 use crate::processors::Event;
+use crate::processors::EventCause;
 use crate::processors::InputPort;
 use crate::processors::OutputPort;
 use crate::processors::Processor;
 
 pub struct ShuffleProcessor {
-    channel: Vec<(Arc<InputPort>, Arc<OutputPort>)>,
+    input2output: Vec<usize>,
+    output2input: Vec<usize>,
+
+    finished_port: usize,
+    inputs: Vec<(bool, Arc<InputPort>)>,
+    outputs: Vec<(bool, Arc<OutputPort>)>,
 }
 
 impl ShuffleProcessor {
     pub fn create(
         inputs: Vec<Arc<InputPort>>,
         outputs: Vec<Arc<OutputPort>>,
-        rule: Vec<usize>,
+        edges: Vec<usize>,
     ) -> Self {
-        let len = rule.len();
+        let len = edges.len();
         debug_assert!({
-            let mut sorted = rule.clone();
+            let mut sorted = edges.clone();
             sorted.sort();
             let expected = (0..len).collect::<Vec<_>>();
             sorted == expected
         });
 
-        let mut channel = Vec::with_capacity(len);
-        for (i, input) in inputs.into_iter().enumerate() {
-            let output = outputs[rule[i]].clone();
-            channel.push((input, output));
+        let mut input2output = vec![0_usize; edges.len()];
+        let mut output2input = vec![0_usize; edges.len()];
+
+        for (input, output) in edges.into_iter().enumerate() {
+            input2output[input] = output;
+            output2input[output] = input;
         }
-        ShuffleProcessor { channel }
+
+        ShuffleProcessor {
+            input2output,
+            output2input,
+            finished_port: 0,
+            inputs: inputs.into_iter().map(|x| (false, x)).collect(),
+            outputs: outputs.into_iter().map(|x| (false, x)).collect(),
+        }
     }
 }
 
@@ -59,23 +74,68 @@ impl Processor for ShuffleProcessor {
         self
     }
 
-    fn event(&mut self) -> Result<Event> {
-        let mut finished = true;
-        for (input, output) in self.channel.iter() {
-            if output.is_finished() || input.is_finished() {
-                input.finish();
-                output.finish();
-                continue;
+    fn event_with_cause(&mut self, cause: EventCause) -> Result<Event> {
+        let ((input_finished, input), (output_finished, output)) = match cause {
+            EventCause::Other => unreachable!(),
+            EventCause::Input(index) => (
+                &mut self.inputs[index],
+                &mut self.outputs[self.input2output[index]],
+            ),
+            EventCause::Output(index) => (
+                &mut self.inputs[self.output2input[index]],
+                &mut self.outputs[index],
+            ),
+        };
+
+        if output.is_finished() {
+            input.finish();
+
+            if !*input_finished {
+                *input_finished = true;
+                self.finished_port += 1;
             }
-            finished = false;
-            input.set_need_data();
-            if output.can_push() && input.has_data() {
-                output.push_data(input.pull_data().unwrap());
+
+            if !*output_finished {
+                *output_finished = true;
+                self.finished_port += 1;
             }
+
+            return match self.finished_port == (self.inputs.len() + self.outputs.len()) {
+                true => Ok(Event::Finished),
+                false => Ok(Event::NeedConsume),
+            };
         }
-        if finished {
-            return Ok(Event::Finished);
+
+        if !output.can_push() {
+            input.set_not_need_data();
+            return Ok(Event::NeedConsume);
         }
+
+        if input.has_data() {
+            output.push_data(input.pull_data().unwrap());
+            return Ok(Event::NeedConsume);
+        }
+
+        if input.is_finished() {
+            output.finish();
+
+            if !*input_finished {
+                *input_finished = true;
+                self.finished_port += 1;
+            }
+
+            if !*output_finished {
+                *output_finished = true;
+                self.finished_port += 1;
+            }
+
+            return match self.finished_port == (self.inputs.len() + self.outputs.len()) {
+                true => Ok(Event::Finished),
+                false => Ok(Event::NeedConsume),
+            };
+        }
+
+        input.set_need_data();
         Ok(Event::NeedData)
     }
 }
