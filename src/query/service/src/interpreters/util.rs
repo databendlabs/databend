56efa5470655a059192b4ac06ff9730663080a6d
TCeason <33082201+TCeason@users.noreply.github.com>
2024-09-19 09:48:12 +0800

    feat(query): Procedure Part2 support arguments (#16453)
    * feat(query): Procedure Part2 support arguments
    
    * fix conversation
    
    * refactor var_to_ast with parse_expr

--- src/query/service/src/interpreters/util.rs
+++ src/query/service/src/interpreters/util.rs
@@ -12,9 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::sync::Arc;
+
+use databend_common_ast::ast::Expr;
+use databend_common_ast::parser::parse_expr;
+use databend_common_ast::parser::tokenize_sql;
+use databend_common_ast::parser::Dialect;
+use databend_common_exception::ErrorCode;
 use databend_common_expression::ComputedExpr;
+use databend_common_expression::DataBlock;
+use databend_common_expression::DataSchemaRef;
 use databend_common_expression::Scalar;
 use databend_common_expression::TableSchemaRef;
+use databend_common_script::ir::ColumnAccess;
+use databend_common_script::Client;
+use databend_common_sql::Planner;
+use futures_util::TryStreamExt;
+use itertools::Itertools;
+
+use crate::interpreters::InterpreterFactory;
+use crate::sessions::QueryContext;
 
 #[allow(clippy::type_complexity)]
 pub fn generate_desc_schema(
@@ -60,4 +77,110 @@ pub fn generate_desc_schema(
         extras.push(extra);
     }
     (names, types, nulls, default_exprs, extras)
+}
+
+#[derive(Debug, Clone)]
+pub struct QueryResult {
+    pub(crate) schema: DataSchemaRef,
+    pub(crate) block: DataBlock,
+}
+
+pub struct ScriptClient {
+    pub(crate) ctx: Arc<QueryContext>,
+}
+
+impl Client for ScriptClient {
+    type Var = Scalar;
+    type Set = QueryResult;
+
+    async fn query(&self, query: &str) -> databend_common_exception::Result<Self::Set> {
+        let ctx = self
+            .ctx
+            .get_current_session()
+            .create_query_context()
+            .await?;
+
+        let mut planner = Planner::new(ctx.clone());
+        let (plan, _) = planner.plan_sql(query).await?;
+        let interpreter = InterpreterFactory::get(ctx.clone(), &plan).await?;
+        let stream = interpreter.execute(ctx.clone()).await?;
+        let blocks = stream.try_collect::<Vec<_>>().await?;
+        let schema = plan.schema();
+
+        let block = match blocks.len() {
+            0 => DataBlock::empty_with_schema(schema.clone()),
+            1 => blocks[0].clone(),
+            _ => DataBlock::concat(&blocks)?,
+        };
+
+        Ok(QueryResult { schema, block })
+    }
+
+    fn var_to_ast(&self, scalar: &Self::Var) -> databend_common_exception::Result<Expr> {
+        let scalar = scalar.to_string();
+        let ast = parse_expr(&tokenize_sql(&scalar)?, Dialect::PostgreSQL)?;
+
+        Ok(ast)
+    }
+
+    fn read_from_set(
+        &self,
+        set: &Self::Set,
+        row: usize,
+        col: &ColumnAccess,
+    ) -> databend_common_exception::Result<Self::Var> {
+        let offset = match col {
+            ColumnAccess::Position(offset) => *offset,
+            // TODO(andylokandy): name resolution
+            ColumnAccess::Name(name) => set
+                .schema
+                .fields()
+                .iter()
+                .position(|f| f.name() == name)
+                .ok_or_else(|| {
+                ErrorCode::ScriptExecutionError(format!(
+                    "cannot find column with name {} in block, available columns: {}",
+                    name,
+                    set.schema
+                        .fields()
+                        .iter()
+                        .map(|f| format!("'{}'", f.name()))
+                        .join(", ")
+                ))
+            })?,
+        };
+        let col = set.block.columns().get(offset).ok_or_else(|| {
+            ErrorCode::ScriptExecutionError(format!(
+                "cannot read column at offset {} from block with {} columns",
+                offset,
+                set.block.num_columns()
+            ))
+        })?;
+        let cell = col
+            .value
+            .index(row)
+            .ok_or_else(|| {
+                ErrorCode::ScriptExecutionError(format!(
+                    "cannot read value at row {} from column with {} rows",
+                    row,
+                    set.block.num_rows(),
+                ))
+            })?
+            .to_owned();
+
+        Ok(cell)
+    }
+
+    fn num_rows(&self, set: &Self::Set) -> usize {
+        set.block.num_rows()
+    }
+
+    fn is_true(&self, scalar: &Self::Var) -> databend_common_exception::Result<bool> {
+        match scalar {
+            Scalar::Boolean(v) => Ok(*v),
+            _ => Err(ErrorCode::ScriptExecutionError(format!(
+                "`is_true` called on non-boolean value {scalar}",
+            ))),
+        }
+    }
 }
