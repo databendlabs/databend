84d583b45b50de21cd9cbc95a8c3d11626ae9aec
sky <3374614481@qq.com>
2024-09-25 14:24:39 +0800

    fix missing header

--- src/query/storages/fuse/src/io/write/meta_writer.rs
+++ src/query/storages/fuse/src/io/write/meta_writer.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use databend_common_exception::Result;
 use databend_storages_common_cache::CacheAccessor;
 use databend_storages_common_cache::CachedObject;
