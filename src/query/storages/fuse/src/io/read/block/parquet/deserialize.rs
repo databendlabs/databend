a1e472751157606c901a5be95651fa45dcf56ea7
dantengsky <dantengsky@gmail.com>
2024-10-11 10:46:37 +0800

    fix: wrong flags

--- src/query/storages/fuse/src/io/read/block/parquet/deserialize.rs
+++ src/query/storages/fuse/src/io/read/block/parquet/deserialize.rs
@@ -40,7 +40,7 @@ pub fn column_chunks_to_record_batch(
     compression: &Compression,
     block_path: &str,
 ) -> databend_common_exception::Result<RecordBatch> {
-    let use_v1 = !is_possible_non_standard_decimal_block(block_path)?
+    let use_v1 = is_possible_non_standard_decimal_block(block_path)?
         && original_schema.fields.iter().any(|f| {
             matches!(
                 f.data_type.remove_nullable(),
