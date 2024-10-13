01bc7ed672e689af15982ab511be60ddd0fc2531
sundyli <543950155@qq.com>
2024-10-13 22:33:14 +0800

    chore(query): improve variable parser (#16599)
    * chore(query): improve variable parser
    
    * chore(query): improve variable parser
    
    * chore(query): improve variable parser

--- src/query/ast/tests/it/parser.rs
+++ src/query/ast/tests/it/parser.rs
@@ -788,7 +788,7 @@ fn test_statement() {
         r#"DROP FUNCTION binary_reverse;"#,
         r#"DROP FUNCTION isnotempty;"#,
         r#"
-            EXECUTE IMMEDIATE 
+            EXECUTE IMMEDIATE
             $$
             BEGIN
                 LOOP
@@ -1239,6 +1239,7 @@ fn test_expr_error() {
         r#"1 a"#,
         r#"CAST(col1)"#,
         r#"a.add(b)"#,
+        r#"$ abc + 3"#,
         r#"[ x * 100 FOR x in [1,2,3] if x % 2 = 0 ]"#,
         r#"
             G.E.B IS NOT NULL
