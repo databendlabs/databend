82ae34158c75a091e1e95c0d7b1192c5684bf260
sundy-li <543950155@qq.com>
2024-10-28 15:02:34 +0800

    feat: add table function udf_echo && expose more metrics of udf client

--- src/query/service/src/table_functions/table_function_factory.rs
+++ src/query/service/src/table_functions/table_function_factory.rs
@@ -33,6 +33,7 @@ use databend_storages_common_table_meta::table_id_ranges::SYS_TBL_FUNC_ID_BEGIN;
 use itertools::Itertools;
 use parking_lot::RwLock;
 
+use super::others::UdfEchoTable;
 use super::ExecuteBackgroundJobTable;
 use super::LicenseInfoTable;
 use super::SuggestedBackgroundTasksTable;
@@ -287,6 +288,11 @@ impl TableFunctionFactory {
         creators.insert(
             "show_variables".to_string(),
             (next_id(), Arc::new(ShowVariables::create)),
+        );
+
+        creators.insert(
+            "udf_echo".to_string(),
+            (next_id(), Arc::new(UdfEchoTable::create)),
         );
 
         TableFunctionFactory {
