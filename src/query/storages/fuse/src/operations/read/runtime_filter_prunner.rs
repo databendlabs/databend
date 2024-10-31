2f5af018a206e0c2f59faff30879ac8c3e2e723e
Dousir9 <736191200@qq.com>
2024-10-31 09:28:22 +0800

    feat(query): implement partition scan

--- src/query/storages/fuse/src/operations/read/runtime_filter_prunner.rs
+++ src/query/storages/fuse/src/operations/read/runtime_filter_prunner.rs
@@ -15,12 +15,16 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 
+use databend_common_arrow::arrow::bitmap::Bitmap;
 use databend_common_arrow::arrow::bitmap::MutableBitmap;
+use databend_common_arrow::arrow::buffer::Buffer;
 use databend_common_base::runtime::profile::Profile;
 use databend_common_base::runtime::profile::ProfileStatisticsName;
 use databend_common_catalog::plan::PartInfoPtr;
 use databend_common_exception::Result;
+use databend_common_expression::types::BinaryColumn;
 use databend_common_expression::types::NumberColumn;
+use databend_common_expression::types::StringColumn;
 use databend_common_expression::Column;
 use databend_common_expression::ConstantFolder;
 use databend_common_expression::DataBlock;
@@ -34,6 +38,7 @@ use databend_common_expression::KeysState::U256;
 use databend_common_expression::Scalar;
 use databend_common_expression::TableSchema;
 use databend_common_functions::BUILTIN_FUNCTIONS;
+use databend_common_hashtable::DictionaryKeys;
 use databend_common_hashtable::FastHash;
 use databend_storages_common_index::statistics_to_domain;
 use log::debug;
@@ -46,31 +51,30 @@ use crate::FuseBlockPartInfo;
 pub fn runtime_filter_pruner(
     table_schema: Arc<TableSchema>,
     part: &PartInfoPtr,
-    filters: &[Expr<String>],
+    filters: &[(String, Expr<String>)],
     func_ctx: &FunctionContext,
 ) -> Result<bool> {
     if filters.is_empty() {
         return Ok(false);
     }
     let part = FuseBlockPartInfo::from_part(part)?;
-    let pruned = filters.iter().any(|filter| {
-        let column_refs = filter.column_refs();
-        // Currently only support filter with one column(probe key).
-        debug_assert!(column_refs.len() == 1);
-        let ty = column_refs.values().last().unwrap();
-        let name = column_refs.keys().last().unwrap();
+    let pruned = filters.iter().any(|(_, filter)| {
+        let Some(column_id) = Expr::<String>::column_id(&filter) else {
+            return false;
+        };
+        let data_type = filter.data_type();
         if let Some(stats) = &part.columns_stat {
-            let column_ids = table_schema.leaf_columns_of(name);
+            let column_ids = table_schema.leaf_columns_of(&column_id);
             if column_ids.len() != 1 {
                 return false;
             }
-            debug_assert!(column_ids.len() == 1);
             if let Some(stat) = stats.get(&column_ids[0]) {
                 let stats = vec![stat];
-                let domain = statistics_to_domain(stats, ty);
+                let domain = statistics_to_domain(stats, data_type);
+                dbg!("domain = {:?}", &domain);
 
                 let mut input_domains = HashMap::new();
-                input_domains.insert(name.to_string(), domain.clone());
+                input_domains.insert(column_id, domain.clone());
 
                 let (new_expr, _) = ConstantFolder::fold_with_domain(
                     filter,
@@ -102,75 +106,31 @@ pub fn runtime_filter_pruner(
 
 pub(crate) fn update_bitmap_with_bloom_filter(
     column: Column,
-    filter: &BinaryFuse16,
+    bloom_filter: &BinaryFuse16,
     bitmap: &mut MutableBitmap,
 ) -> Result<()> {
-    let data_type = column.data_type();
+    let data_type = column.data_type().remove_nullable();
     let num_rows = column.len();
     let method = DataBlock::choose_hash_method_with_types(&[data_type.clone()], false)?;
+
+    let (column, validity) = if let Column::Nullable(inner_column) = column {
+        if inner_column.validity.unset_bits() == 0 {
+            (inner_column.column, None)
+        } else {
+            (inner_column.column, Some(inner_column.validity))
+        }
+    } else {
+        (column, None)
+    };
+
     let columns = &[column];
     let group_columns = columns.into();
-    let mut idx = 0;
     match method {
-        HashMethodKind::Serializer(method) => {
-            let key_state = method.build_keys_state(group_columns, num_rows)?;
-            match key_state {
-                KeysState::Column(Column::Binary(col)) => col.iter().for_each(|key| {
-                    let hash = key.fast_hash();
-                    if filter.contains(&hash) {
-                        bitmap.set(idx, true);
-                    }
-                    idx += 1;
-                }),
-                _ => unreachable!(),
-            }
-        }
-        HashMethodKind::DictionarySerializer(method) => {
-            let key_state = method.build_keys_state(group_columns, num_rows)?;
-            match key_state {
-                KeysState::Dictionary { dictionaries, .. } => dictionaries.iter().for_each(|key| {
-                    let hash = key.fast_hash();
-                    if filter.contains(&hash) {
-                        bitmap.set(idx, true);
-                    }
-                    idx += 1;
-                }),
-                _ => unreachable!(),
-            }
-        }
-        HashMethodKind::SingleBinary(method) => {
-            let key_state = method.build_keys_state(group_columns, num_rows)?;
-            match key_state {
-                KeysState::Column(Column::Binary(col))
-                | KeysState::Column(Column::Variant(col))
-                | KeysState::Column(Column::Bitmap(col)) => col.iter().for_each(|key| {
-                    let hash = key.fast_hash();
-                    if filter.contains(&hash) {
-                        bitmap.set(idx, true);
-                    }
-                    idx += 1;
-                }),
-                KeysState::Column(Column::String(col)) => col.iter_binary().for_each(|key| {
-                    let hash = key.fast_hash();
-                    if filter.contains(&hash) {
-                        bitmap.set(idx, true);
-                    }
-                    idx += 1;
-                }),
-                _ => unreachable!(),
-            }
-        }
         HashMethodKind::KeysU8(hash_method) => {
             let key_state = hash_method.build_keys_state(group_columns, num_rows)?;
             match key_state {
-                KeysState::Column(Column::Number(NumberColumn::UInt8(c))) => {
-                    c.iter().for_each(|key| {
-                        let hash = key.fast_hash();
-                        if filter.contains(&hash) {
-                            bitmap.set(idx, true);
-                        }
-                        idx += 1;
-                    })
+                KeysState::Column(Column::Number(NumberColumn::UInt8(keys))) => {
+                    update_bitmap_primitive_types(keys, bitmap, bloom_filter, &validity)
                 }
                 _ => unreachable!(),
             }
@@ -178,14 +138,8 @@ pub(crate) fn update_bitmap_with_bloom_filter(
         HashMethodKind::KeysU16(hash_method) => {
             let key_state = hash_method.build_keys_state(group_columns, num_rows)?;
             match key_state {
-                KeysState::Column(Column::Number(NumberColumn::UInt16(c))) => {
-                    c.iter().for_each(|key| {
-                        let hash = key.fast_hash();
-                        if filter.contains(&hash) {
-                            bitmap.set(idx, true);
-                        }
-                        idx += 1;
-                    })
+                KeysState::Column(Column::Number(NumberColumn::UInt16(keys))) => {
+                    update_bitmap_primitive_types(keys, bitmap, bloom_filter, &validity)
                 }
                 _ => unreachable!(),
             }
@@ -193,14 +147,8 @@ pub(crate) fn update_bitmap_with_bloom_filter(
         HashMethodKind::KeysU32(hash_method) => {
             let key_state = hash_method.build_keys_state(group_columns, num_rows)?;
             match key_state {
-                KeysState::Column(Column::Number(NumberColumn::UInt32(c))) => {
-                    c.iter().for_each(|key| {
-                        let hash = key.fast_hash();
-                        if filter.contains(&hash) {
-                            bitmap.set(idx, true);
-                        }
-                        idx += 1;
-                    })
+                KeysState::Column(Column::Number(NumberColumn::UInt32(keys))) => {
+                    update_bitmap_primitive_types(keys, bitmap, bloom_filter, &validity)
                 }
                 _ => unreachable!(),
             }
@@ -208,14 +156,8 @@ pub(crate) fn update_bitmap_with_bloom_filter(
         HashMethodKind::KeysU64(hash_method) => {
             let key_state = hash_method.build_keys_state(group_columns, num_rows)?;
             match key_state {
-                KeysState::Column(Column::Number(NumberColumn::UInt64(c))) => {
-                    c.iter().for_each(|key| {
-                        let hash = key.fast_hash();
-                        if filter.contains(&hash) {
-                            bitmap.set(idx, true);
-                        }
-                        idx += 1;
-                    })
+                KeysState::Column(Column::Number(NumberColumn::UInt64(keys))) => {
+                    update_bitmap_primitive_types(keys, bitmap, bloom_filter, &validity)
                 }
                 _ => unreachable!(),
             }
@@ -223,29 +165,150 @@ pub(crate) fn update_bitmap_with_bloom_filter(
         HashMethodKind::KeysU128(hash_method) => {
             let key_state = hash_method.build_keys_state(group_columns, num_rows)?;
             match key_state {
-                U128(c) => c.iter().for_each(|key| {
-                    let hash = key.fast_hash();
-                    if filter.contains(&hash) {
-                        bitmap.set(idx, true);
-                    }
-                    idx += 1;
-                }),
+                U128(keys) => update_bitmap_primitive_types(keys, bitmap, bloom_filter, &validity),
                 _ => unreachable!(),
             }
         }
         HashMethodKind::KeysU256(hash_method) => {
             let key_state = hash_method.build_keys_state(group_columns, num_rows)?;
             match key_state {
-                U256(c) => c.iter().for_each(|key| {
-                    let hash = key.fast_hash();
-                    if filter.contains(&hash) {
-                        bitmap.set(idx, true);
-                    }
-                    idx += 1;
-                }),
+                U256(keys) => update_bitmap_primitive_types(keys, bitmap, bloom_filter, &validity),
+                _ => unreachable!(),
+            }
+        }
+        HashMethodKind::SingleBinary(method) => {
+            let key_state = method.build_keys_state(group_columns, num_rows)?;
+            match key_state {
+                KeysState::Column(Column::Binary(keys))
+                | KeysState::Column(Column::Variant(keys))
+                | KeysState::Column(Column::Bitmap(keys)) => {
+                    update_bitmap_binary(keys, bitmap, bloom_filter, &validity)
+                }
+                KeysState::Column(Column::String(keys)) => {
+                    update_bitmap_string(keys, bitmap, bloom_filter, &validity)
+                }
+                _ => unreachable!(),
+            }
+        }
+        HashMethodKind::Serializer(method) => {
+            let key_state = method.build_keys_state(group_columns, num_rows)?;
+            match key_state {
+                KeysState::Column(Column::Binary(keys)) => {
+                    update_bitmap_binary(keys, bitmap, bloom_filter, &validity)
+                }
+                _ => unreachable!(),
+            }
+        }
+        HashMethodKind::DictionarySerializer(method) => {
+            let key_state = method.build_keys_state(group_columns, num_rows)?;
+            match key_state {
+                KeysState::Dictionary { dictionaries, .. } => {
+                    update_bitmap_dictionary(dictionaries, bitmap, bloom_filter, &validity)
+                }
                 _ => unreachable!(),
             }
         }
     }
+
     Ok(())
+}
+
+fn update_bitmap_primitive_types<T: FastHash>(
+    keys: Buffer<T>,
+    bitmap: &mut MutableBitmap,
+    bloom_filter: &BinaryFuse16,
+    validity: &Option<Bitmap>,
+) {
+    if let Some(validity) = validity {
+        for (index, key) in keys.iter().enumerate() {
+            if validity.get_bit(index) {
+                let hash = key.fast_hash();
+                if bloom_filter.contains(&hash) {
+                    bitmap.set(index, true);
+                }
+            }
+        }
+    } else {
+        for (index, key) in keys.iter().enumerate() {
+            let hash = key.fast_hash();
+            if bloom_filter.contains(&hash) {
+                bitmap.set(index, true);
+            }
+        }
+    }
+}
+
+fn update_bitmap_string(
+    keys: StringColumn,
+    bitmap: &mut MutableBitmap,
+    bloom_filter: &BinaryFuse16,
+    validity: &Option<Bitmap>,
+) {
+    if let Some(validity) = validity {
+        for (index, key) in keys.iter().enumerate() {
+            if validity.get_bit(index) {
+                let hash = key.fast_hash();
+                if bloom_filter.contains(&hash) {
+                    bitmap.set(index, true);
+                }
+            }
+        }
+    } else {
+        for (index, key) in keys.iter().enumerate() {
+            let hash = key.fast_hash();
+            if bloom_filter.contains(&hash) {
+                bitmap.set(index, true);
+            }
+        }
+    }
+}
+
+fn update_bitmap_binary(
+    keys: BinaryColumn,
+    bitmap: &mut MutableBitmap,
+    bloom_filter: &BinaryFuse16,
+    validity: &Option<Bitmap>,
+) {
+    if let Some(validity) = validity {
+        for (index, key) in keys.iter().enumerate() {
+            if validity.get_bit(index) {
+                let hash = key.fast_hash();
+                if bloom_filter.contains(&hash) {
+                    bitmap.set(index, true);
+                }
+            }
+        }
+    } else {
+        for (index, key) in keys.iter().enumerate() {
+            let hash = key.fast_hash();
+            if bloom_filter.contains(&hash) {
+                bitmap.set(index, true);
+            }
+        }
+    }
+}
+
+fn update_bitmap_dictionary(
+    keys: Vec<DictionaryKeys>,
+    bitmap: &mut MutableBitmap,
+    bloom_filter: &BinaryFuse16,
+    validity: &Option<Bitmap>,
+) {
+    if let Some(validity) = validity {
+        for (index, key) in keys.iter().enumerate() {
+            if validity.get_bit(index) {
+                let hash = key.fast_hash();
+                if bloom_filter.contains(&hash) {
+                    bitmap.set(index, true);
+                }
+            }
+        }
+    } else {
+        for (index, key) in keys.iter().enumerate() {
+            let hash = key.fast_hash();
+            if bloom_filter.contains(&hash) {
+                bitmap.set(index, true);
+            }
+        }
+    }
 }
---
+++ src/query/storages/fuse/src/operations/read/source_reader.rs
@@ -0,0 +1,104 @@
+// Copyright 2021 Datafuse Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+use std::sync::Arc;
+
+use xorf::BinaryFuse16;
+use crate::io::BlockReader;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum ReaderState {
+    Uninitialized,
+    All,
+    Filter(usize),
+    Remaining,
+    Finish,
+}
+
+impl ReaderState {
+    pub fn next_reader_state(&self, source_reader: &SourceReader) -> ReaderState {
+        match self {
+            ReaderState::Uninitialized => match source_reader {
+                SourceReader::Parquet { filter_readers, .. } => {
+                    if filter_readers.is_empty() {
+                        ReaderState::All
+                    } else {
+                        ReaderState::Filter(0)
+                    }
+                }
+            },
+            ReaderState::Filter(index) => match source_reader {
+                SourceReader::Parquet {
+                    filter_readers,
+                    remaining_reader,
+                    ..
+                } => {
+                    if index + 1 < filter_readers.len() {
+                        ReaderState::Filter(*index + 1)
+                    } else if remaining_reader.is_some() {
+                        ReaderState::Remaining
+                    } else {
+                        ReaderState::Finish
+                    }
+                }
+            },
+            ReaderState::All | ReaderState::Remaining | ReaderState::Finish => ReaderState::Finish,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub enum SourceReader {
+    Parquet {
+        block_reader: Arc<BlockReader>,
+        filter_readers: Vec<(Arc<BlockReader>, Arc<BinaryFuse16>)>,
+        remaining_reader: Option<Arc<BlockReader>>,
+        column_positions: Vec<usize>,
+    },
+}
+
+impl SourceReader {
+    pub fn source_block_reader(&self, reader_state: &ReaderState) -> SourceBlockReader {
+        match self {
+            SourceReader::Parquet {
+                block_reader,
+                filter_readers,
+                remaining_reader,
+                ..
+            } => {
+                let (source_block_reader, bloom_filter) = match reader_state {
+                    ReaderState::All => (block_reader.clone(), None),
+                    ReaderState::Filter(index) => (
+                        filter_readers[*index].0.clone(),
+                        Some(filter_readers[*index].1.clone()),
+                    ),
+                    ReaderState::Remaining => (remaining_reader.as_ref().unwrap().clone(), None),
+                    _ => unreachable!(),
+                };
+                SourceBlockReader::Parquet {
+                    block_reader: source_block_reader,
+                    bloom_filter,
+                }
+            }
+        }
+    }
+}
+
+pub enum SourceBlockReader {
+    Parquet {
+        block_reader: Arc<BlockReader>,
+        bloom_filter: Option<Arc<BinaryFuse16>>,
+    },
+}
