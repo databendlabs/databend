19680cdcf6f97a21c3037c48cec2347d72d0f310
zhang2014 <coswde@gmail.com>
2024-11-06 17:03:37 +0800

    refactor(base): lock free backtrace capture

--- src/common/exception/src/elf/dwarf_subprogram.rs
+++ src/common/exception/src/elf/dwarf_subprogram.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::slice::range;
-
 use gimli::Attribute;
 use gimli::AttributeValue;
 use gimli::EntriesTreeNode;
