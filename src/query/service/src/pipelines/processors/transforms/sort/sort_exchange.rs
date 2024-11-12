e04d0f246019a3217731337f715e364928f2c5fb
coldWater <forsaken628@gmail.com>
2024-11-12 16:54:54 +0800

    SortRangeExchange
    Signed-off-by: coldWater <forsaken628@gmail.com>

--- src/query/service/src/pipelines/processors/transforms/sort/sort_exchange.rs
+++ src/query/service/src/pipelines/processors/transforms/sort/sort_exchange.rs
@@ -11,3 +11,27 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use std::sync::Arc;
+
+use databend_common_exception::Result;
+use databend_common_expression::group_hash_columns_slice;
+use databend_common_expression::ColumnBuilder;
+use databend_common_expression::DataBlock;
+use databend_common_expression::Value;
+use databend_common_pipeline_core::processors::Exchange;
+
+pub struct SortRangeExchange {
+    num_partitions: usize,
+    init: bool,
+}
+
+impl Exchange for SortRangeExchange {
+    fn partition(&self, mut block: DataBlock, n: usize) -> Result<Vec<DataBlock>> {
+        match block.take_meta() {
+            Some(_) => {}
+            None => {}
+        };
+        todo!()
+    }
+}
