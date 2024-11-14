f39e192f6a709d5df6ff2740bfdadc6214cfab99
Rowlandev <chase.grainger2134@gmail.com>
2024-11-14 08:20:21 -0500

    changed test to use glue as a catalog option

--- src/meta/proto-conv/tests/it/v111_add_glue_as_iceberg_catalog_option.rs
+++ src/meta/proto-conv/tests/it/v111_add_glue_as_iceberg_catalog_option.rs
@@ -14,9 +14,10 @@
 
 use chrono::TimeZone;
 use chrono::Utc;
-use databend_common_meta_app::schema as mt;
+use databend_common_meta_app::schema::CatalogOption;
+use databend_common_meta_app::schema::IcebergCatalogOption;
+use databend_common_meta_app::schema::IcebergGlueCatalogOption;
 use fastrace::func_name;
-use maplit::btreemap;
 
 use crate::common;
 
@@ -32,29 +33,27 @@ use crate::common;
 // The message bytes are built from the output of `test_pb_from_to()`
 #[test]
 fn test_v111_add_glue_as_iceberg_catalog_option() -> anyhow::Result<()> {
-    let database_meta_v111 = vec![
-        34, 10, 10, 3, 120, 121, 122, 18, 3, 102, 111, 111, 42, 2, 52, 52, 50, 10, 10, 3, 97, 98,
-        99, 18, 3, 100, 101, 102, 162, 1, 23, 50, 48, 49, 52, 45, 49, 49, 45, 50, 56, 32, 49, 50,
-        58, 48, 48, 58, 48, 57, 32, 85, 84, 67, 170, 1, 23, 50, 48, 49, 52, 45, 49, 49, 45, 50, 57,
-        32, 49, 50, 58, 48, 48, 58, 48, 57, 32, 85, 84, 67, 178, 1, 7, 102, 111, 111, 32, 98, 97,
-        114, 232, 1, 1, 160, 6, 110, 168, 6, 24,
+    let catalog_meta_v111 = vec![
+        18, 55, 26, 53, 18, 45, 10, 21, 104, 116, 116, 112, 58, 47, 47, 49, 50, 55, 46, 48, 46, 48,
+        46, 49, 58, 57, 57, 48, 48, 18, 14, 115, 51, 58, 47, 47, 109, 121, 95, 98, 117, 99, 107,
+        101, 116, 160, 6, 98, 168, 6, 24, 160, 6, 98, 168, 6, 24, 162, 1, 23, 50, 48, 49, 52, 45,
+        49, 49, 45, 50, 56, 32, 49, 50, 58, 48, 48, 58, 48, 57, 32, 85, 84, 67, 160, 6, 98, 168, 6,
+        24,
     ];
 
-    let want = || mt::DatabaseMeta {
-        engine: "44".to_string(),
-        engine_options: btreemap! {s("abc") => s("def")},
-        options: btreemap! {s("xyz") => s("foo")},
+    let want = || databend_common_meta_app::schema::CatalogMeta {
+        catalog_option: CatalogOption::Iceberg(IcebergGlueCatalogOption::Rest(
+            IcebergGlueCatalogOption {
+                address: "http://127.0.0.1:9900".to_string(),
+                warehouse: "s3://my_bucket".to_string(),
+                props: Default::default(),
+            },
+        )),
         created_on: Utc.with_ymd_and_hms(2014, 11, 28, 12, 0, 9).unwrap(),
-        updated_on: Utc.with_ymd_and_hms(2014, 11, 29, 12, 0, 9).unwrap(),
-        comment: "foo bar".to_string(),
-        drop_on: None,
-        gc_in_progress: true,
     };
 
     common::test_pb_from_to(func_name!(), want())?;
-    common::test_load_old(func_name!(), database_meta_v111.as_slice(), 111, want())
-}
+    common::test_load_old(func_name!(), catalog_meta_v111.as_slice(), 111, want())?;
 
-fn s(ss: impl ToString) -> String {
-    ss.to_string()
+    Ok(())
 }
