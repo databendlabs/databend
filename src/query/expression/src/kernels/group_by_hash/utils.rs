8bffe9c516bddcd96d4e1da5f1d5a34a57be9ff6
sundy-li <543950155@qq.com>
2024-11-20 22:01:22 +0800

    update

--- src/query/expression/src/kernels/group_by_hash/utils.rs
+++ src/query/expression/src/kernels/group_by_hash/utils.rs
@@ -40,7 +40,8 @@ pub fn serialize_group_columns(
         }
         builder.commit_row();
     }
-    debug_assert_eq!(builder.data.len(), serialize_size);
+    // For nulllable column it will only serialize valid row data
+    debug_assert!(builder.data.len() <= serialize_size);
     builder.build()
 }
 
