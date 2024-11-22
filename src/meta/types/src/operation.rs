a62a0a440f50574e120e681d02d1bb12cab56fb5
张炎泼 <drdr.xp@gmail.com>
2024-11-22 15:35:46 +0800

    chore: fix deprecated

--- src/meta/types/src/operation.rs
+++ src/meta/types/src/operation.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(deprecated)]
+
 //! This crate defines data types used in meta data storage service.
 
 use std::fmt::Debug;
