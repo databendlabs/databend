d119bf56b368b008ad7d296984953f1f85f66532
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-11-26 08:14:47 +0800

    ci: fix flaky test in cluster mode

--- tests/suites/1_stateful/09_http_handler/09_0001_json_response.sh
+++ tests/suites/1_stateful/09_http_handler/09_0001_json_response.sh
@@ -3,9 +3,7 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../../../shell_env.sh
 
-echo "drop table if exists t1;" | $BENDSQL_CLIENT_CONNECT
-
-curl -s --header 'Content-Type: application/json'  --request POST '127.0.0.1:8000/v1/query/'  --data-raw '{"sql": "select * from t1", "pagination": { "wait_time_secs": 2}}' -u root: | jq -c ".error"
+curl -s --header 'Content-Type: application/json'  --request POST '127.0.0.1:8000/v1/query/'  --data-raw '{"sql": "select * from system.t1", "pagination": { "wait_time_secs": 5}}' -u root: | jq -c ".error"
 
 curl -s --header 'Content-Type: application/json'  --request POST '127.0.0.1:8000/v1/query/'  --data-raw '{sql": "select * from tx", "pagination": { "wait_time_secs": 2}}' -u root:
 echo ""
