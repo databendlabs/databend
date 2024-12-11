cc0c69fc9e2993efaa056479cedcd8b080cc29e8
sundy-li <543950155@qq.com>
2024-12-11 16:04:57 +0800

    chore(query): collect bool

--- src/query/expression/src/lib.rs
+++ src/query/expression/src/lib.rs
@@ -75,6 +75,7 @@ pub use crate::input_columns::*;
 pub use crate::kernels::*;
 pub use crate::property::*;
 pub use crate::register::*;
+pub use crate::register_comparision::*;
 pub use crate::row::*;
 pub use crate::schema::*;
 pub use crate::utils::block_thresholds::BlockThresholds;
