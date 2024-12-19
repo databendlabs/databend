8e600a3557f6faba6547d0e848cc78371c4a2ba8
everpcpc <git@everpcpc.com>
2024-12-19 16:50:12 +0800

    z

--- src/query/users/src/jwt/jwk.rs
+++ src/query/users/src/jwt/jwk.rs
@@ -184,7 +184,7 @@ impl JwkKeyStore {
         let new_keys = match self.load_keys().await {
             Ok(new_keys) => new_keys,
             Err(err) => {
-                warn!("Failed to refresh JWKS: {}", err);
+                warn!("Failed to load JWKS: {}", err);
                 if !old_keys.is_empty() {
                     return Ok(old_keys);
                 }
