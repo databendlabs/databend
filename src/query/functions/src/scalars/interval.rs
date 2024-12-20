a5a86e7e592a3ebdd3084c9909c4d22722e23910
TCeason <tai_chong@foxmail.com>
2024-12-20 16:53:56 +0800

    feat(query): support interval function

--- src/query/functions/src/scalars/interval.rs
+++ src/query/functions/src/scalars/interval.rs
@@ -13,13 +13,17 @@
 // limitations under the License.
 
 use databend_common_column::types::months_days_micros;
+use databend_common_expression::date_helper::EvalMonthsImpl;
 use databend_common_expression::error_to_null;
 use databend_common_expression::types::interval::interval_to_string;
 use databend_common_expression::types::interval::string_to_interval;
+use databend_common_expression::types::Int64Type;
 use databend_common_expression::types::IntervalType;
 use databend_common_expression::types::NullableType;
 use databend_common_expression::types::StringType;
+use databend_common_expression::types::TimestampType;
 use databend_common_expression::vectorize_with_builder_1_arg;
+use databend_common_expression::vectorize_with_builder_2_arg;
 use databend_common_expression::EvalContext;
 use databend_common_expression::FunctionDomain;
 use databend_common_expression::FunctionRegistry;
@@ -30,6 +34,9 @@ pub fn register(registry: &mut FunctionRegistry) {
     // to_interval(xx)
     register_string_to_interval(registry);
     register_interval_to_string(registry);
+    // data/timestamp/interval +/- interval
+    register_interval_add_sub(registry);
+    register_number_to_interval(registry);
 }
 
 fn register_string_to_interval(registry: &mut FunctionRegistry) {
@@ -77,5 +84,229 @@ fn register_interval_to_string(registry: &mut FunctionRegistry) {
                 output.push(&res);
             },
         ),
+    );
+}
+
+fn register_interval_add_sub(registry: &mut FunctionRegistry) {
+    registry.register_passthrough_nullable_2_arg::<IntervalType, IntervalType, IntervalType, _, _>(
+        "plus",
+        |_, _, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_2_arg::<IntervalType, IntervalType, IntervalType>(
+            |a, b, output, _| {
+                output.push(months_days_micros::new(
+                    a.months() + b.months(),
+                    a.days() + b.days(),
+                    a.microseconds() + b.microseconds(),
+                ))
+            },
+        ),
+    );
+
+    registry
+        .register_passthrough_nullable_2_arg::<TimestampType, IntervalType, TimestampType, _, _>(
+            "plus",
+            |_, _, _| FunctionDomain::MayThrow,
+            vectorize_with_builder_2_arg::<TimestampType, IntervalType, TimestampType>(
+                |a, b, output, ctx| {
+                    // plus microseconds and days
+                    let ts = a
+                        .wrapping_add(b.microseconds())
+                        .wrapping_add((b.days() as i64).wrapping_mul(86_400_000_000));
+                    match EvalMonthsImpl::eval_timestamp(
+                        ts,
+                        ctx.func_ctx.jiff_tz.clone(),
+                        b.months(),
+                    ) {
+                        Ok(t) => output.push(t),
+                        Err(e) => {
+                            ctx.set_error(output.len(), e);
+                            output.push(0);
+                        }
+                    }
+                },
+            ),
+        );
+
+    registry
+        .register_passthrough_nullable_2_arg::<IntervalType, TimestampType, TimestampType, _, _>(
+            "plus",
+            |_, _, _| FunctionDomain::MayThrow,
+            vectorize_with_builder_2_arg::<IntervalType, TimestampType, TimestampType>(
+                |b, a, output, ctx| {
+                    // plus microseconds and days
+                    let ts = a
+                        .wrapping_add(b.microseconds())
+                        .wrapping_add((b.days() as i64).wrapping_mul(86_400_000_000));
+                    match EvalMonthsImpl::eval_timestamp(
+                        ts,
+                        ctx.func_ctx.jiff_tz.clone(),
+                        b.months(),
+                    ) {
+                        Ok(t) => output.push(t),
+                        Err(e) => {
+                            ctx.set_error(output.len(), e);
+                            output.push(0);
+                        }
+                    }
+                },
+            ),
+        );
+
+    registry.register_passthrough_nullable_2_arg::<IntervalType, IntervalType, IntervalType, _, _>(
+        "minus",
+        |_, _, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_2_arg::<IntervalType, IntervalType, IntervalType>(
+            |a, b, output, _| {
+                output.push(months_days_micros::new(
+                    a.months() - b.months(),
+                    a.days() - b.days(),
+                    a.microseconds() - b.microseconds(),
+                ));
+            },
+        ),
+    );
+
+    registry
+        .register_passthrough_nullable_2_arg::<TimestampType, IntervalType, TimestampType, _, _>(
+            "minus",
+            |_, _, _| FunctionDomain::MayThrow,
+            vectorize_with_builder_2_arg::<TimestampType, IntervalType, TimestampType>(
+                |a, b, output, ctx| {
+                    // plus microseconds and days
+                    let ts = a
+                        .wrapping_sub(b.microseconds())
+                        .wrapping_sub((b.days() as i64).wrapping_mul(86_400_000_000));
+                    match EvalMonthsImpl::eval_timestamp(
+                        ts,
+                        ctx.func_ctx.jiff_tz.clone(),
+                        -b.months(),
+                    ) {
+                        Ok(t) => output.push(t),
+                        Err(e) => {
+                            ctx.set_error(output.len(), e);
+                            output.push(0);
+                        }
+                    }
+                },
+            ),
+        );
+}
+
+fn register_number_to_interval(registry: &mut FunctionRegistry) {
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "epoch",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new(0, 0, val * 1_000_000);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_centuries",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new((val * 100 * 12) as i32, 0, 0);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_days",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new(0, val as i32, 0);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_weeks",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new(0, (val * 7) as i32, 0);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_decades",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new((val * 10 * 12) as i32, 0, 0);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_hours",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new(0, 0, val * 3600 * 1_000_000);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_microseconds",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new(0, 0, val);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_millennia",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new((val * 1000 * 12) as i32, 0, 0);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_milliseconds",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new(0, 0, val * 1000);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_minutes",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new(0, 0, val * 60 * 1_000_000);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_months",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new(val as i32, 0, 0);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_seconds",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new(0, 0, val * 1_000_000);
+            output.push(res);
+        }),
+    );
+
+    registry.register_passthrough_nullable_1_arg::<Int64Type, IntervalType, _, _>(
+        "to_years",
+        |_, _| FunctionDomain::MayThrow,
+        vectorize_with_builder_1_arg::<Int64Type, IntervalType>(|val, output, _| {
+            let res = months_days_micros::new((val * 12) as i32, 0, 0);
+            output.push(res);
+        }),
     );
 }
