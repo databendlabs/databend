9a8784e5100aac08a07b1c4bb611c805a8c12767
sundyli <543950155@qq.com>
2024-12-27 11:10:43 +0800

    chore(query): improve hook vacuum temp after query (#17125)
    * chore(query): improve ctx lock
    
    * chore(query): improve vacuum
    
    * send spill nums via progress in cluster
    
    * send spill nums via progress in cluster

--- src/query/service/src/sessions/query_ctx_shared.rs
+++ src/query/service/src/sessions/query_ctx_shared.rs
@@ -143,6 +143,7 @@ pub struct QueryContextShared {
 
     pub(in crate::sessions) query_queued_duration: Arc<RwLock<Duration>>,
 
+    pub(in crate::sessions) cluster_spill_file_nums: Arc<RwLock<HashMap<String, usize>>>,
     pub(in crate::sessions) spilled_files:
         Arc<RwLock<HashMap<crate::spillers::Location, crate::spillers::Layout>>>,
 }
@@ -201,6 +202,8 @@ impl QueryContextShared {
             merge_into_join: Default::default(),
             multi_table_insert_status: Default::default(),
             query_queued_duration: Arc::new(RwLock::new(Duration::from_secs(0))),
+
+            cluster_spill_file_nums: Default::default(),
             spilled_files: Default::default(),
         }))
     }
