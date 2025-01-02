5de45020d85e7bb3c792a325fa5fa523849571b0
coldWater <forsaken628@gmail.com>
2025-01-02 20:37:08 +0800

    fix

--- scripts/ci/deploy/databend-query-standalone.sh
+++ scripts/ci/deploy/databend-query-standalone.sh
@@ -27,6 +27,8 @@ nohup target/${BUILD_PROFILE}/databend-meta --single --log-level=INFO &
 echo "Waiting on databend-meta 10 seconds..."
 python3 scripts/ci/wait_tcp.py --timeout 30 --port 9191
 
+mkdir -p .databend/temp/_query_spill
+
 echo 'Start databend-query...'
 nohup target/${BUILD_PROFILE}/databend-query -c scripts/ci/deploy/config/databend-query-node-1.toml --internal-enable-sandbox-tenant &
 
