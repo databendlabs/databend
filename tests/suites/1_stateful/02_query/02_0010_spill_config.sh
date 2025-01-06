4c7da9b1a421b1feb914c2682177e383abb47e3e
coldWater <forsaken628@gmail.com>
2025-01-06 12:13:21 +0800

    fix

--- tests/suites/1_stateful/02_query/02_0010_spill_config.sh
+++ tests/suites/1_stateful/02_query/02_0010_spill_config.sh
@@ -3,6 +3,10 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../../../shell_env.sh
 
+echo "set global aggregate_spilling_bytes_threshold_per_proc=1;" | $BENDSQL_CLIENT_CONNECT
+
 echo "SELECT COUNT() FROM (SELECT number::string, count() FROM numbers_mt(100000) group by number::string);" | $BENDSQL_CLIENT_CONNECT
 
 echo "select count(*)>0 from system.temp_files;" | $BENDSQL_CLIENT_CONNECT
+
+echo "unset global aggregate_spilling_bytes_threshold_per_proc;" | $BENDSQL_CLIENT_CONNECT
