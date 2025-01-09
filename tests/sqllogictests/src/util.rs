618c5e2cff1c38d0e17421545b5e7aadf0f13734
baishen <baishen2009@gmail.com>
2025-01-09 19:10:56 +0800

    fix lazy

--- tests/sqllogictests/src/util.rs
+++ tests/sqllogictests/src/util.rs
@@ -13,19 +13,33 @@
 // limitations under the License.
 
 use std::collections::BTreeMap;
+use std::collections::HashSet;
 use std::path::Path;
 use std::path::PathBuf;
+use std::time::Duration;
 
+use bollard::Docker;
 use clap::Parser;
+use redis::Commands;
 use serde::Deserialize;
 use serde::Serialize;
 use serde_json::Value;
+use testcontainers::core::IntoContainerPort;
+use testcontainers::core::WaitFor;
+use testcontainers::runners::AsyncRunner;
+use testcontainers::ContainerAsync;
+use testcontainers::GenericImage;
+use testcontainers::ImageExt;
+use testcontainers_modules::mysql::Mysql;
+use testcontainers_modules::redis::Redis;
+use testcontainers_modules::redis::REDIS_PORT;
 use walkdir::DirEntry;
 use walkdir::WalkDir;
 
 use crate::arg::SqlLogicTestArgs;
 use crate::error::DSqlLogicTestError;
 use crate::error::Result;
+
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 pub struct ServerInfo {
     pub id: String,
@@ -134,29 +148,75 @@ static PREPARE_STAGE: std::sync::Once = std::sync::Once::new();
 static PREPARE_SPILL: std::sync::Once = std::sync::Once::new();
 static PREPARE_WASM: std::sync::Once = std::sync::Once::new();
 
-pub fn lazy_prepare_data(file_path: &Path) -> Result<()> {
+#[derive(Eq, Hash, PartialEq)]
+pub enum LazyDir {
+    Tpch,
+    Tpcds,
+    Stage,
+    UdfNative,
+    Spill,
+    Dictionaries,
+}
+
+pub fn collect_lazy_dir(file_path: &Path, lazy_dirs: &mut HashSet<LazyDir>) -> Result<()> {
     let file_path = file_path.to_str().unwrap_or_default();
     if file_path.contains("tpch/") {
-        PREPARE_TPCH.call_once(|| {
-            println!("Calling the script prepare_tpch_data.sh ...");
-            run_script("prepare_tpch_data.sh").unwrap();
-        });
+        if !lazy_dirs.contains(&LazyDir::Tpch) {
+            lazy_dirs.insert(LazyDir::Tpch);
+        }
     } else if file_path.contains("tpcds/") {
-        PREPARE_TPCDS.call_once(|| {
-            println!("Calling the script prepare_tpcds_data.sh ...");
-            run_script("prepare_tpcds_data.sh").unwrap();
-        });
+        if !lazy_dirs.contains(&LazyDir::Tpcds) {
+            lazy_dirs.insert(LazyDir::Tpcds);
+        }
     } else if file_path.contains("stage/") || file_path.contains("stage_parquet/") {
-        PREPARE_STAGE.call_once(|| {
-            println!("Calling the script prepare_stage.sh ...");
-            run_script("prepare_stage.sh").unwrap();
-        });
+        if !lazy_dirs.contains(&LazyDir::Stage) {
+            lazy_dirs.insert(LazyDir::Stage);
+        }
     } else if file_path.contains("udf_native/") {
-        println!("wasm context Calling the script prepare_stage.sh ...");
-        PREPARE_WASM.call_once(|| run_script("prepare_stage.sh").unwrap())
+        if !lazy_dirs.contains(&LazyDir::UdfNative) {
+            lazy_dirs.insert(LazyDir::UdfNative);
+        }
     } else if file_path.contains("spill/") {
-        println!("Calling the script prepare_spill_data.sh ...");
-        PREPARE_SPILL.call_once(|| run_script("prepare_spill_data.sh").unwrap())
+        if !lazy_dirs.contains(&LazyDir::Spill) {
+            lazy_dirs.insert(LazyDir::Spill);
+        }
+    } else if file_path.contains("dictionaries/") && !lazy_dirs.contains(&LazyDir::Dictionaries) {
+        lazy_dirs.insert(LazyDir::Dictionaries);
+    }
+    Ok(())
+}
+
+pub fn lazy_prepare_data(lazy_dirs: &HashSet<LazyDir>) -> Result<()> {
+    for lazy_dir in lazy_dirs {
+        match lazy_dir {
+            LazyDir::Tpch => {
+                PREPARE_TPCH.call_once(|| {
+                    println!("Calling the script prepare_tpch_data.sh ...");
+                    run_script("prepare_tpch_data.sh").unwrap();
+                });
+            }
+            LazyDir::Tpcds => {
+                PREPARE_TPCDS.call_once(|| {
+                    println!("Calling the script prepare_tpcds_data.sh ...");
+                    run_script("prepare_tpcds_data.sh").unwrap();
+                });
+            }
+            LazyDir::Stage => {
+                PREPARE_STAGE.call_once(|| {
+                    println!("Calling the script prepare_stage.sh ...");
+                    run_script("prepare_stage.sh").unwrap();
+                });
+            }
+            LazyDir::UdfNative => {
+                println!("wasm context Calling the script prepare_stage.sh ...");
+                PREPARE_WASM.call_once(|| run_script("prepare_stage.sh").unwrap())
+            }
+            LazyDir::Spill => {
+                println!("Calling the script prepare_spill_data.sh ...");
+                PREPARE_SPILL.call_once(|| run_script("prepare_spill_data.sh").unwrap())
+            }
+            _ => {}
+        }
     }
     Ok(())
 }
@@ -175,4 +235,141 @@ fn run_script(name: &str) -> Result<()> {
         )));
     }
     Ok(())
+}
+
+pub async fn run_ttc_container(
+    docker: &Docker,
+    image: &str,
+    port: u16,
+    cs: &mut Vec<ContainerAsync<GenericImage>>,
+) -> Result<()> {
+    let mut images = image.split(":");
+    let image = images.next().unwrap();
+    let tag = images.next().unwrap_or("latest");
+
+    let container_name = format!("databend-ttc-{}", port);
+    println!("Start {container_name}");
+
+    // Stop the container
+    let _ = docker.stop_container(&container_name, None).await;
+    let _ = docker.remove_container(&container_name, None).await;
+
+    let container_res = GenericImage::new(image, tag)
+        .with_exposed_port(port.tcp())
+        .with_wait_for(WaitFor::Duration {
+            length: Duration::from_secs(300),
+        })
+        .with_network("host")
+        .with_env_var(
+            "DATABEND_DSN",
+            "databend://root:@127.0.0.1:8000?sslmode=disable",
+        )
+        .with_env_var("TTC_PORT", format!("{port}"))
+        .with_container_name(&container_name)
+        .start()
+        .await;
+    match container_res {
+        Ok(container) => {
+            println!("Started container: {}", container.id());
+            cs.push(container);
+            Ok(())
+        }
+        Err(e) => Err(format!("Start {container_name} failed: {e}").into()),
+    }
+}
+
+#[allow(dead_code)]
+pub struct DictionaryContainer {
+    pub redis: ContainerAsync<Redis>,
+    pub mysql: ContainerAsync<Mysql>,
+}
+
+pub async fn lazy_run_dictionary_containers(
+    lazy_dirs: &HashSet<LazyDir>,
+) -> Result<Option<DictionaryContainer>> {
+    if !lazy_dirs.contains(&LazyDir::Dictionaries) {
+        return Ok(None);
+    }
+    let docker = Docker::connect_with_local_defaults().unwrap();
+    println!("run dictionary source server container");
+    let redis = run_redis_server(&docker).await?;
+    let mysql = run_mysql_server(&docker).await?;
+    let dict_container = DictionaryContainer { redis, mysql };
+
+    Ok(Some(dict_container))
+}
+
+async fn run_redis_server(docker: &Docker) -> Result<ContainerAsync<Redis>> {
+    let container_name = "redis".to_string();
+
+    // Stop the container
+    let _ = docker.stop_container(&container_name, None).await;
+    let _ = docker.remove_container(&container_name, None).await;
+
+    let redis_res = Redis::default()
+        .with_network("host")
+        .with_startup_timeout(Duration::from_secs(300))
+        .with_container_name(&container_name)
+        .start()
+        .await;
+
+    match redis_res {
+        Ok(redis) => {
+            let host_ip = redis.get_host().await.unwrap();
+            let url = format!("redis://{}:{}", host_ip, REDIS_PORT);
+            let client = redis::Client::open(url.as_ref()).unwrap();
+            let mut con = client.get_connection().unwrap();
+
+            // Add some key values for test.
+            let keys = vec!["a", "b", "c", "1", "2"];
+            for key in keys {
+                let val = format!("{}_value", key);
+                con.set::<_, _, ()>(key, val).unwrap();
+            }
+            Ok(redis)
+        }
+        Err(e) => Err(format!("Start {container_name} failed: {e}").into()),
+    }
+}
+
+async fn run_mysql_server(docker: &Docker) -> Result<ContainerAsync<Mysql>> {
+    let container_name = "mysqld".to_string();
+
+    // Stop the container
+    let _ = docker.stop_container(&container_name, None).await;
+    let _ = docker.remove_container(&container_name, None).await;
+
+    // Add a table for test.
+    // CREATE TABLE test.user(
+    //   id INT,
+    //   name VARCHAR(100),
+    //   age SMALLINT UNSIGNED,
+    //   salary DOUBLE,
+    //   active BOOL
+    // );
+    //
+    // +------+-------+------+---------+--------+
+    // | id   | name  | age  | salary  | active |
+    // +------+-------+------+---------+--------+
+    // |    1 | Alice |   24 |     100 |      1 |
+    // |    2 | Bob   |   35 |   200.1 |      0 |
+    // |    3 | Lily  |   41 |  1000.2 |      1 |
+    // |    4 | Tom   |   55 | 3000.55 |      0 |
+    // |    5 | NULL  | NULL |    NULL |   NULL |
+    // +------+-------+------+---------+--------+
+    let mysql_res = Mysql::default()
+        .with_init_sql(
+"CREATE TABLE test.user(id INT, name VARCHAR(100), age SMALLINT UNSIGNED, salary DOUBLE, active BOOL); INSERT INTO test.user VALUES(1, 'Alice', 24, 100, true), (2, 'Bob', 35, 200.1, false), (3, 'Lily', 41, 1000.2, true), (4, 'Tom', 55, 3000.55, false), (5, NULL, NULL, NULL, NULL);"
+        .to_string()
+        .into_bytes(),
+)
+        .with_network("host")
+        .with_startup_timeout(Duration::from_secs(300))
+        .with_container_name(&container_name)
+        .start().await;
+
+    match mysql_res {
+        Ok(mysql) => Ok(mysql),
+        Err(e) => Err(format!("Start {container_name} failed: {e}").into()),
+    }
 }
