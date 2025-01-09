6a5d2598a0e2b946c6aad205de131b62ba442b1a
zhyass <mytesla@live.com>
2025-01-09 22:33:40 +0800

    enable sort spill in recluster
    fix: hilbert clustering in cluster mode
    
    fix quoted cluster key

--- src/query/pipeline/transforms/src/processors/transforms/transform_compact_no_split_builder.rs
+++ src/query/pipeline/transforms/src/processors/transforms/transform_compact_no_split_builder.rs
@@ -79,7 +79,7 @@ impl AccumulatingTransform for BlockCompactNoSplitBuilder {
 
     fn transform(&mut self, data: DataBlock) -> Result<Vec<DataBlock>> {
         self.accumulated_rows += data.num_rows();
-        self.accumulated_bytes += data.memory_size();
+        self.accumulated_bytes += crate::processors::memory_size(&data);
         if !self
             .thresholds
             .check_large_enough(self.accumulated_rows, self.accumulated_bytes)
