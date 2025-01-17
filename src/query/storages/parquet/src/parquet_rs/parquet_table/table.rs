31c243bbfb5c049d5f31836ed44a6a087f19a50c
Yang Xiufeng <yangxiufeng.c@gmail.com>
2025-01-17 09:18:02 +0800

    fix: set query_kind earlier to ensure it takes effect. (#17302)
    maybe related to the performance regression reported in issue 17284.

--- src/query/storages/parquet/src/parquet_rs/parquet_table/table.rs
+++ src/query/storages/parquet/src/parquet_rs/parquet_table/table.rs
@@ -34,6 +34,7 @@ use databend_common_catalog::table::DummyColumnStatisticsProvider;
 use databend_common_catalog::table::Table;
 use databend_common_catalog::table::TableStatistics;
 use databend_common_catalog::table_context::TableContext;
+use databend_common_exception::ErrorCode;
 use databend_common_exception::Result;
 use databend_common_expression::TableField;
 use databend_common_expression::TableSchema;
@@ -141,10 +142,17 @@ impl ParquetRSTable {
 
         // If the query is `COPY`, we don't need to collect column statistics.
         // It's because the only transform could be contained in `COPY` command is projection.
-        let need_stats_provider = !matches!(
-            query_kind,
-            QueryKind::CopyIntoTable | QueryKind::CopyIntoLocation
-        );
+        let need_stats_provider = match query_kind {
+            QueryKind::CopyIntoTable | QueryKind::CopyIntoLocation => true,
+            QueryKind::Unknown => {
+                // add this branch to ensure query_kind is set
+                return Err(ErrorCode::Internal(
+                    "Unexpected QueryKind::Unknown: query_kind was not properly set before calling ParquetRSTable::create.",
+                ));
+            }
+            _ => false,
+        };
+
         let max_threads = settings.get_max_threads()? as usize;
         let max_memory_usage = settings.get_max_memory_usage()?;
 
