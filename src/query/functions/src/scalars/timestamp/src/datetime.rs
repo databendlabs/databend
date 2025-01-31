840c58bfe0c3bc30ee2e99c8014e50a76bdf3dd0
TCeason <tai_chong@foxmail.com>
2025-02-01 06:51:08 +0800

    function rename to epoch

--- src/query/functions/src/scalars/timestamp/src/datetime.rs
+++ src/query/functions/src/scalars/timestamp/src/datetime.rs
@@ -1525,7 +1525,7 @@ fn register_to_number_functions(registry: &mut FunctionRegistry) {
     );
 
     registry.register_passthrough_nullable_1_arg::<TimestampType, Float64Type, _, _>(
-        "epoch_microsecond",
+        "epoch",
         |_, domain| {
             FunctionDomain::Domain(SimpleDomain::<F64> {
                 min: (domain.min as f64 / 1_000_000f64).into(),
