6bb6f7929f6d940aca837a23ab4e1496e62ff695
coldWater <forsaken628@gmail.com>
2025-02-14 15:42:13 +0800

    fix

--- src/query/service/src/pipelines/processors/transforms/window/partition/window_partition_partial_top_n_exchange.rs
+++ src/query/service/src/pipelines/processors/transforms/window/partition/window_partition_partial_top_n_exchange.rs
@@ -68,6 +68,10 @@ impl WindowPartitionTopNExchange {
 impl Exchange for WindowPartitionTopNExchange {
     const NAME: &'static str = "WindowTopN";
     fn partition(&self, block: DataBlock, n: usize) -> Result<Vec<DataBlock>> {
+        if block.is_empty() {
+            return Ok(vec![]);
+        }
+
         let partition_permutation = self.partition_permutation(&block);
 
         // Partition the data blocks to different processors.
