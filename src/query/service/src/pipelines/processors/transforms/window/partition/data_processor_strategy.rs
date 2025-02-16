d1a26135977c897415184ebcc4dc1ddfbe1e541e
zhyass <mytesla@live.com>
2025-02-17 02:41:51 +0800

    fix

--- src/query/service/src/pipelines/processors/transforms/window/partition/data_processor_strategy.rs
+++ src/query/service/src/pipelines/processors/transforms/window/partition/data_processor_strategy.rs
@@ -34,17 +34,26 @@ impl CompactStrategy {
     pub fn new(thresholds: BlockThresholds) -> Self {
         Self { thresholds }
     }
+
+    fn concat_blocks(blocks: Vec<DataBlock>) -> Result<DataBlock> {
+        DataBlock::concat(&blocks)
+    }
 }
 
 impl DataProcessorStrategy for CompactStrategy {
     const NAME: &'static str = "Compact";
 
     fn process_data_blocks(&self, data_blocks: Vec<DataBlock>) -> Result<Vec<DataBlock>> {
+        let blocks_num = data_blocks.len();
+        if blocks_num < 2 {
+            return Ok(data_blocks);
+        }
+
         let mut accumulated_rows = 0;
         let mut accumulated_bytes = 0;
-        let mut pending_blocks = vec![];
-        let mut staged_blocks = vec![];
-        let mut result = vec![];
+        let mut pending_blocks = Vec::with_capacity(blocks_num);
+        let mut staged_blocks = Vec::with_capacity(blocks_num);
+        let mut result = Vec::with_capacity(blocks_num);
         for block in data_blocks {
             accumulated_rows += block.num_rows();
             accumulated_bytes += memory_size(&block);
@@ -57,8 +66,7 @@ impl DataProcessorStrategy for CompactStrategy {
             }
 
             if !staged_blocks.is_empty() {
-                result.push(DataBlock::concat(&staged_blocks)?);
-                staged_blocks.clear();
+                result.push(Self::concat_blocks(std::mem::take(&mut staged_blocks))?);
             }
 
             if pending_blocks.is_empty()
@@ -68,8 +76,7 @@ impl DataProcessorStrategy for CompactStrategy {
             {
                 std::mem::swap(&mut staged_blocks, &mut pending_blocks);
             } else {
-                result.push(DataBlock::concat(&pending_blocks)?);
-                pending_blocks.clear();
+                result.push(Self::concat_blocks(std::mem::take(&mut pending_blocks))?);
             }
 
             staged_blocks.push(block);
@@ -79,7 +86,7 @@ impl DataProcessorStrategy for CompactStrategy {
 
         staged_blocks.append(&mut pending_blocks);
         if !staged_blocks.is_empty() {
-            result.push(DataBlock::concat(&staged_blocks)?);
+            result.push(Self::concat_blocks(std::mem::take(&mut staged_blocks))?);
         }
 
         Ok(result)
