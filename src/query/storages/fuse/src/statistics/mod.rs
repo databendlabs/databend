a323dee98462fdc1a982fb4950c4702a809384a3
sky <3374614481@qq.com>
2025-02-19 00:22:33 +0800

    make lint

--- src/query/storages/fuse/src/statistics/mod.rs
+++ src/query/storages/fuse/src/statistics/mod.rs
@@ -18,8 +18,6 @@ mod column_statistic;
 pub mod reducers;
 
 pub use accumulator::RowOrientedSegmentBuilder;
-pub use block_statistics::BlockStatistics;
-pub use accumulator::StatisticsAccumulator;
 pub use cluster_statistics::sort_by_cluster_stats;
 pub use cluster_statistics::ClusterStatsGenerator;
 pub use column_statistic::calc_column_distinct_of_values;
