7287f98008879ed292c07c44350bd182b6863294
baishen <baishen2009@gmail.com>
2025-02-20 13:24:51 +0800

    feat(sqlsmith): fuzz test support more dbs as seed (#17489)
    * feat(sqlsmith): fuzz test support more dbs as seed
    
    * fix

--- src/tests/sqlsmith/src/util.rs
+++ src/tests/sqlsmith/src/util.rs
@@ -25,6 +25,11 @@ enum TestType {
     Databend,
     Duckdb,
     Sqlite,
+    Tidb,
+    Postgres,
+    Cockroach,
+    Doris,
+    Starrocks,
 }
 
 // Walk through the sqllogic tests folder and read the test SQL in it as seed to generate fuzzed SQL.
@@ -33,38 +38,62 @@ pub(crate) fn read_sql_from_test_dirs(fuzz_path: &str) -> Result<Vec<String>> {
         TestType::Sqlite
     } else if fuzz_path.contains("duckdb") {
         TestType::Duckdb
+    } else if fuzz_path.contains("tidb") {
+        TestType::Tidb
+    } else if fuzz_path.contains("postgres") {
+        TestType::Postgres
+    } else if fuzz_path.contains("cockroach") {
+        TestType::Cockroach
+    } else if fuzz_path.contains("doris") {
+        TestType::Doris
+    } else if fuzz_path.contains("starrocks") {
+        TestType::Starrocks
     } else {
         TestType::Databend
     };
 
     let path = Path::new(fuzz_path);
     let mut paths = vec![];
-    collect_paths(path, &mut paths)?;
+    collect_paths(&test_type, path, &mut paths)?;
 
     let mut sqls = vec![];
     for path in paths.into_iter() {
         let content = read_to_string(path)?;
         let lines: Vec<&str> = content.lines().collect();
         match test_type {
-            TestType::Databend | TestType::Duckdb => {
+            TestType::Databend | TestType::Duckdb | TestType::Cockroach => {
                 read_sqllogict_test_sqls(lines, &mut sqls);
             }
             TestType::Sqlite => {
                 read_sqlite_test_sqls(lines, &mut sqls);
+            }
+            TestType::Tidb | TestType::Postgres | TestType::Doris | TestType::Starrocks => {
+                read_test_sqls(lines, &mut sqls);
             }
         }
     }
     Ok(sqls)
 }
 
-fn collect_paths(path: &Path, paths: &mut Vec<PathBuf>) -> Result<()> {
+fn collect_paths(test_type: &TestType, path: &Path, paths: &mut Vec<PathBuf>) -> Result<()> {
+    let file_ext = match test_type {
+        TestType::Postgres | TestType::Doris => "sql",
+        TestType::Cockroach | TestType::Starrocks => "",
+        _ => "test",
+    };
     if path.is_dir() {
         for entry in (path.read_dir()?).flatten() {
             let sub_path = entry.path();
-            collect_paths(&sub_path, paths)?;
+            collect_paths(test_type, &sub_path, paths)?;
         }
-    } else if path.is_file() && path.extension().is_some_and(|ext| ext == "test") {
-        paths.push(path.to_path_buf());
+    } else if path.is_file() {
+        let is_test_file = match test_type {
+            TestType::Cockroach | TestType::Starrocks => path.extension().is_none(),
+            _ => path.extension().is_some_and(|ext| ext == file_ext),
+        };
+        if is_test_file {
+            paths.push(path.to_path_buf());
+        }
     }
     Ok(())
 }
@@ -136,5 +165,51 @@ fn read_sqlite_test_sqls(lines: Vec<&str>, sqls: &mut Vec<String>) {
                 current_sql.clear();
             }
         }
+    }
+}
+
+// Read and parse Postgres/Tidb/Doris/Starrocks test files.
+fn read_test_sqls(lines: Vec<&str>, sqls: &mut Vec<String>) {
+    let mut current_sql = String::new();
+    let mut in_properties = false;
+
+    for line in lines {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        if line.starts_with("#") || line.starts_with("--") || line.starts_with("set") {
+            in_properties = false;
+            if !current_sql.is_empty() {
+                sqls.push(current_sql.clone());
+                current_sql.clear();
+            }
+            continue;
+        } else {
+            let line = line.replace("primary key", "");
+            let line = line.replace("ENGINE=OLAP", "");
+            if line.starts_with("PROPERTIES") {
+                in_properties = true;
+            }
+            if !line.starts_with("DUPLICATE KEY")
+                && !line.starts_with("AGGREGATE KEY")
+                && !line.starts_with("PARTITION BY")
+                && !line.starts_with("DISTRIBUTED BY")
+                && !in_properties
+            {
+                if !current_sql.is_empty() {
+                    current_sql.push(' ');
+                }
+                current_sql.push_str(&line);
+            }
+            if line.ends_with(";") {
+                in_properties = false;
+                sqls.push(current_sql.clone());
+                current_sql.clear();
+            }
+        }
+    }
+    if !current_sql.is_empty() {
+        sqls.push(current_sql);
     }
 }
