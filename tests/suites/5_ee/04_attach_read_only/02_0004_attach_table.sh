8897264f7819e2b7ccb4f3a5d0d6e1d8a18a78eb
dantengsky <dantengsky@gmail.com>
2025-02-24 18:05:41 +0800

    tweak stateless test

--- tests/suites/5_ee/04_attach_read_only/02_0004_attach_table.sh
+++ tests/suites/5_ee/04_attach_read_only/02_0004_attach_table.sh
@@ -97,7 +97,6 @@ stmt "drop table if exists attach_tbl2"
 echo "attach table attach_tbl2 's3://testbucket/admin/data/$base_storage_prefix' connection=(connection_name ='my_conn')" | $BENDSQL_CLIENT_CONNECT
 query "desc attach_tbl2"
 
-
 stmt "insert into base values('c1', 'c2_new', 'c3', 'c4')"
 
 echo "select all should work"
@@ -105,8 +104,6 @@ query "select * from attach_tbl order by c2_new"
 
 echo "select c2_new should work"
 query "select c2_new from attach_tbl order by c2_new"
-
-
 
 echo "##################################"
 echo "## drop column from base table ###"
