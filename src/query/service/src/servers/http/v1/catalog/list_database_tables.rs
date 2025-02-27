de3dd58316b685d7a3792082e1de6ef976773b34
everpcpc <git@everpcpc.com>
2025-02-27 09:39:07 +0800

    z

--- src/query/service/src/servers/http/v1/catalog/list_database_tables.rs
+++ src/query/service/src/servers/http/v1/catalog/list_database_tables.rs
@@ -14,6 +14,8 @@
 
 use chrono::DateTime;
 use chrono::Utc;
+use databend_common_catalog::catalog::CatalogManager;
+use databend_common_exception::Result;
 use poem::error::Result as PoemResult;
 use poem::web::Json;
 use poem::web::Path;
@@ -44,14 +46,68 @@ pub struct TableInfo {
     pub index_size: u64,
 }
 
+#[async_backtrace::framed]
+async fn handle(ctx: &HttpQueryContext, database: String) -> Result<ListDatabaseTablesResponse> {
+    let tenant = ctx.session.get_current_tenant();
+    let user = ctx.session.get_current_user()?;
+    let visibility_checker = ctx.session.get_visibility_checker(false).await?;
+
+    let catalog = CatalogManager::instance().get_default_catalog(Default::default())?;
+    let database = catalog.get_database(&tenant, &database).await?;
+
+    if !visibility_checker.check_database_visibility(
+        catalog.name().as_str(),
+        database.name(),
+        database.get_db_info().database_id.db_id,
+    ) {
+        return Ok(ListDatabaseTablesResponse {
+            tables: vec![],
+            warnings: vec![],
+        });
+    }
+
+    let warnings = vec![];
+    let tables = database
+        .list_tables()
+        .await?
+        .into_iter()
+        .filter(|tbl| {
+            visibility_checker.check_table_visibility(
+                catalog.name().as_str(),
+                database.name(),
+                tbl.name(),
+                tbl.get_table_info().database_id.db_id,
+                tbl.get_table_info().table_id,
+            )
+        })
+        .map(|tbl| {
+            let stats = tbl.get_table_info().meta.statistics;
+            TableInfo {
+                name: tbl.name().to_string(),
+                table_type: tbl.table_type().to_string(),
+                database: database.name().to_string(),
+                catalog: catalog.name().to_string(),
+                owner: user.name().to_string(),
+                engine: tbl.engine().to_string(),
+                cluster_by: tbl.cluster_by().to_string(),
+                create_time: tbl.create_time(),
+                num_rows: stats.num_rows,
+                data_size: stats.data_size,
+                data_compressed_size: stats.data_compressed_size,
+                index_size: stats.index_size,
+            }
+        })
+        .collect::<Vec<_>>();
+
+    Ok(ListDatabaseTablesResponse { tables, warnings })
+}
+
 #[poem::handler]
 #[async_backtrace::framed]
 pub async fn list_database_tables_handler(
     ctx: &HttpQueryContext,
     Path(database): Path<String>,
 ) -> PoemResult<impl IntoResponse> {
-    // TODO:
-    let tables = vec![];
-    let warnings = vec![];
-    Ok(Json(ListDatabaseTablesResponse { tables, warnings }))
+    let response = handle(ctx, database).await.map_err(InternalServerError)?;
+    Ok(Json(response))
 }
