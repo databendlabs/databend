7f20259c7e3ede4531f221903023925d2ef732e5
everpcpc <git@everpcpc.com>
2025-02-27 11:53:30 +0800

    z

--- src/query/service/src/servers/http/v1/catalog/get_database_table.rs
+++ src/query/service/src/servers/http/v1/catalog/get_database_table.rs
@@ -141,8 +141,7 @@ pub async fn get_database_table_handler(
     Path((database, table)): Path<(String, String)>,
 ) -> PoemResult<impl IntoResponse> {
     let resp = handle(ctx, database, table).await.map_err(|e| match e {
-        ErrorCode::UNKNOWN_DATABASE => NotFound(e),
-        ErrorCode::UNKNOWN_TABLE => NotFound(e),
+        ErrorCode::UNKNOWN_DATABASE | ErrorCode::UNKNOWN_TABLE => NotFound(e),
         _ => InternalServerError(e),
     })?;
     Ok(Json(resp))
