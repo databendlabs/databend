4219aa145ab45ead37a0cac37c078b1527d40944
Bohu <overred.shuttler@gmail.com>
2025-03-12 08:49:31 +0800

    chore(optimizer): deduplicate_join_condition unit tests (#17585)
    * optimizer/filter/deduplicate_join_condition: add unit test
    
    * add more tests
    
    * add a bug but the query works as expected
    
    * improve test with table_id and column_id
    
    * refine deduplicate_join_condition.rs
    
    * setup_test/action: add pip cache purge
    
    * try to alternative the python mirror
    
    * revert

--- src/query/sql/src/planner/optimizer/mod.rs
+++ src/query/sql/src/planner/optimizer/mod.rs
@@ -39,6 +39,7 @@ pub use cascades::CascadesOptimizer;
 pub use decorrelate::FlattenInfo;
 pub use decorrelate::SubqueryRewriter;
 pub use extract::PatternExtractor;
+pub use filter::DeduplicateJoinConditionOptimizer;
 pub use filter::InferFilterOptimizer;
 pub use hyper_dp::DPhpy;
 pub use m_expr::MExpr;
