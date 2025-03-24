d9d7cdeb818e34d2e1bbaf538c9ea06713a37230
dantengsky <dantengsky@gmail.com>
2025-03-24 14:53:14 +0800

    refactor: optimize file reading for disk-based cache on Unix (#17638)
    * refactor: Optimize file reading for disk-based cache on Unix
    
    For Unix platforms (`#[cfg(unix)]`):
    - Open files with the `O_NOATIME` flag to avoid updating access time.
    - Leverage known file size to streamline reading.
    - Bypass `stat` and `seek` operations used by `std::fs::File::read_to_end`.
    
    * fix: comment typo
    
    * fix: add licenses header
    
    * fix: cargo fmt

--- src/query/storages/common/cache/src/providers/mod.rs
+++ src/query/storages/common/cache/src/providers/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod disk_cache;
+pub mod disk_cache;
 mod memory_cache;
 mod table_data_cache;
 
