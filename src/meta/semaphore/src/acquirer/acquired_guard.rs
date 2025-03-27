451e1760648800f45b099a0e53528c90f7f30cd4
Zhang Yanpo <drdr.xp@gmail.com>
2025-03-27 15:37:42 +0800

    chore: fix type changes

--- src/meta/semaphore/src/acquirer/acquired_guard.rs
+++ src/meta/semaphore/src/acquirer/acquired_guard.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::fmt::Debug;
 use std::future::Future;
 
 use futures::future::BoxFuture;
