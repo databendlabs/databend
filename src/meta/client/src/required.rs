d2906bda20c1b012fbba8bda1fd935bed45eab6b
Zhang Yanpo <drdr.xp@gmail.com>
2025-04-15 16:07:45 +0800

    refactor: upgrade minimum meta-service version to 1.2.677
    1. **Upgrade Minimum Meta-Service Version**:
    
       - The meta-service version has been upgraded to `1.2.677`, which adds
         support for the `initial_flush` feature in `WatchRequest`.
    
       - This feature is required by `meta-Semaphore` and `meta-Cache`.
    
       **Deployment Note**: Before deploying this commit, the databend-meta
       service must be upgraded to version `1.2.677` or higher.
    
    2. **Fine-Grained Feature Control in `meta-client`**:
    
       - The `meta-client` now supports fine-grained control over feature
         requirements by defining each databend-meta feature with its
         minimum supported version.
    
       - Features are defined as follows:
    
         ```rust
         pub const KV_API:              FeatureSpec = ("kv_api",              (1, 2, 259));
         pub const KV_READ_V1:          FeatureSpec = ("kv_read_v1",          (1, 2, 259));
         pub const TRANSACTION:         FeatureSpec = ("transaction",         (1, 2, 259));
         pub const EXPORT:              FeatureSpec = ("export",              (1, 2, 259));
         pub const EXPORT_V1:           FeatureSpec = ("export_v1",           (1, 2, 315));
         pub const WATCH:               FeatureSpec = ("watch",               (1, 2, 259));
         pub const WATCH_INITIAL_FLUSH: FeatureSpec = ("watch/initial_flush", (1, 2, 677));
         pub const MEMBER_LIST:         FeatureSpec = ("member_list",         (1, 2, 259));
         pub const GET_CLUSTER_STATUS:  FeatureSpec = ("get_cluster_status",  (1, 2, 259));
         pub const GET_CLIENT_INFO:     FeatureSpec = ("get_client_info",     (1, 2, 259));
         ```
    
    3. **New API for Meta Client**:
    
       - Introduced `MetaGrpcClient::try_create_with_features()` to create a
         `MetaGrpcClient` with limited feature requirements.
    
       - During the handshake with the databend-meta service, the client
         verifies the minimum version of each specified feature against the
         server version.
    
    - This approach ensures maximum compatibility and flexibility, allowing
      clients to work seamlessly with the meta-service while leveraging new
      features when supported.

---
+++ src/meta/client/src/required.rs
@@ -0,0 +1,130 @@
+// Copyright 2021 Datafuse Labs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Defines the feature this client requires from the databend-meta server.
+
+use std::collections::BTreeMap;
+
+pub type VersionTuple = (u64, u64, u64);
+
+pub type FeatureSpec = (&'static str, VersionTuple);
+
+pub type Features = BTreeMap<&'static str, VersionTuple>;
+
+#[rustfmt::skip]
+pub mod features {
+    use crate::FeatureSpec;
+
+    pub const KV_API:              FeatureSpec = ("kv_api",              (1, 2, 259));
+    pub const KV_READ_V1:          FeatureSpec = ("kv_read_v1",          (1, 2, 259));
+    pub const TRANSACTION:         FeatureSpec = ("transaction",         (1, 2, 259));
+    pub const EXPORT:              FeatureSpec = ("export",              (1, 2, 259));
+    // `export_v1` is not a required feature because the client fallback to `export` if `export_v1` is not available.
+    pub const EXPORT_V1:           FeatureSpec = ("export_v1",           (1, 2, 315));
+    pub const WATCH:               FeatureSpec = ("watch",               (1, 2, 259));
+    pub const WATCH_INITIAL_FLUSH: FeatureSpec = ("watch/initial_flush", (1, 2, 677));
+    pub const MEMBER_LIST:         FeatureSpec = ("member_list",         (1, 2, 259));
+    pub const GET_CLUSTER_STATUS:  FeatureSpec = ("get_cluster_status",  (1, 2, 259));
+    pub const GET_CLIENT_INFO:     FeatureSpec = ("get_client_info",     (1, 2, 259));
+}
+
+/// All features that are ever defined and their least required server version.
+pub fn all() -> &'static [FeatureSpec] {
+    #[rustfmt::skip]
+    pub const REQUIRES: &[FeatureSpec] = &[
+        features::KV_API,
+        features::KV_READ_V1,
+        features::TRANSACTION,
+        features::EXPORT,
+        features::EXPORT_V1,
+        features::WATCH,
+        features::WATCH_INITIAL_FLUSH,
+        features::MEMBER_LIST,
+        features::GET_CLUSTER_STATUS,
+        features::GET_CLIENT_INFO,
+    ];
+
+    REQUIRES
+}
+
+/// Filter out supported features by the given version.
+pub fn supported_features(version: (u64, u64, u64)) -> BTreeMap<&'static str, VersionTuple> {
+    all()
+        .iter()
+        .filter(|(_, v)| version >= *v)
+        .copied()
+        .collect()
+}
+
+/// Features a standard meta-client requires.
+///
+/// `export_v1` is not a required feature because the client fallback to `export` if `export_v1` is not available.
+pub fn std() -> &'static [FeatureSpec] {
+    #[rustfmt::skip]
+    pub const REQUIRES: &[FeatureSpec] = &[
+        features::KV_API,
+        features::KV_READ_V1,
+        features::TRANSACTION,
+        features::EXPORT,
+        // `export_v1` is not a required feature
+        // features::EXPORT_V1,
+        features::WATCH,
+        features::WATCH_INITIAL_FLUSH,
+        features::MEMBER_LIST,
+        features::GET_CLUSTER_STATUS,
+        features::GET_CLIENT_INFO,
+    ];
+
+    REQUIRES
+}
+
+/// Features that are required by business logic, such as read, write and transaction
+pub fn business() -> &'static [FeatureSpec] {
+    #[rustfmt::skip]
+    pub const REQUIRES: &[FeatureSpec] = &[
+        features::KV_API,
+        features::KV_READ_V1,
+        features::TRANSACTION,
+    ];
+
+    REQUIRES
+}
+
+/// Features that are required by business logic, such as read, write and transaction and watch
+pub fn business_with_watch() -> &'static [FeatureSpec] {
+    #[rustfmt::skip]
+    pub const REQUIRES: &[FeatureSpec] = &[
+        features::KV_API,
+        features::KV_READ_V1,
+        features::TRANSACTION,
+        features::WATCH,
+        features::WATCH_INITIAL_FLUSH,
+    ];
+
+    REQUIRES
+}
+
+/// Features that are required by export api
+///
+/// A client that only needs to use the export api specifies these features.
+pub fn export() -> &'static [FeatureSpec] {
+    #[rustfmt::skip]
+    pub const REQUIRES: &[FeatureSpec] = &[
+        features::EXPORT,
+        // `export_v1` is not a required feature
+        // features::EXPORT_V1,
+    ];
+
+    REQUIRES
+}
