ae0ebeb275370bf02b3c7085a25fdf170bb5bc55
zhyass <mytesla@live.com>
2025-05-09 02:45:14 +0800

    partial restore

--- src/query/storages/fuse/src/io/write/stream/block_builder.rs
+++ src/query/storages/fuse/src/io/write/stream/block_builder.rs
@@ -242,7 +242,7 @@ impl StreamBlockBuilder {
         self.row_count >= self.properties.block_thresholds.min_rows_per_block
             || self.block_size >= self.properties.block_thresholds.max_bytes_per_block
             || (file_size >= self.properties.block_thresholds.min_compressed_per_block
-            && self.block_size >= self.properties.block_thresholds.min_bytes_per_block)
+                && self.block_size >= self.properties.block_thresholds.min_bytes_per_block)
     }
 
     pub fn write(&mut self, block: DataBlock) -> Result<()> {
