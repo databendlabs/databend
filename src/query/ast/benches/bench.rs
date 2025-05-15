1406e057ced01560d05dc569dc27ed5858263724
KKould <kould2333@gmail.com>
2025-05-15 18:24:44 +0800

    perf: Simplify the matching pattern when parse function, avoid exponential backtracking

--- src/query/ast/benches/bench.rs
+++ src/query/ast/benches/bench.rs
@@ -16,12 +16,14 @@ fn main() {
     divan::main()
 }
 
-// bench                  fastest       │ slowest       │ median        │ mean          │ samples │ iters
-// ╰─ dummy                             │               │               │               │         │
-//    ├─ deep_query       122 µs        │ 324.3 µs      │ 127.2 µs      │ 130.1 µs      │ 100     │ 100
-//    ├─ large_query      1.366 ms      │ 1.686 ms      │ 1.409 ms      │ 1.417 ms      │ 100     │ 100
-//    ├─ large_statement  1.336 ms      │ 1.441 ms      │ 1.391 ms      │ 1.39 ms       │ 100     │ 100
-//    ╰─ wide_expr        556 µs        │ 697.2 µs      │ 578.3 µs      │ 580.5 µs      │ 100     │ 100
+// bench                     fastest       │ slowest       │ median        │ mean          │ samples │ iters
+// ╰─ dummy                                │               │               │               │         │
+//    ├─ deep_function_call  732.9 ms      │ 732.9 ms      │ 732.9 ms      │ 732.9 ms      │ 1       │ 1
+//    ├─ deep_query          319.4 µs      │ 515.6 µs      │ 333.4 µs      │ 335.3 µs      │ 100     │ 100
+//    ├─ large_query         1.998 ms      │ 2.177 ms      │ 2.032 ms      │ 2.038 ms      │ 100     │ 100
+//    ├─ large_statement     1.952 ms      │ 2.079 ms      │ 2.016 ms      │ 2.011 ms      │ 100     │ 100
+//    ╰─ wide_expr           620.4 µs      │ 783.7 µs      │ 646 µs        │ 646.4 µs      │ 100     │ 100
+
 #[divan::bench_group(max_time = 0.5)]
 mod dummy {
     use databend_common_ast::parser::parse_expr;
@@ -56,6 +58,14 @@ mod dummy {
     #[divan::bench]
     fn wide_expr() {
         let case = r#"a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a AND a"#;
+        let tokens = tokenize_sql(case).unwrap();
+        let expr = parse_expr(&tokens, Dialect::PostgreSQL).unwrap();
+        divan::black_box(expr);
+    }
+
+    #[divan::bench]
+    fn deep_function_call() {
+        let case = r#"ROUND(6378.138 * 2 * ASIN(SQRT(POW(SIN(RADIANS(CASE WHEN MOD(EXTRACT(SECOND FROM CURRENT_TIMESTAMP), 2) = 0 THEN 45.6789 ELSE 30.1234 END - IFNULL(NULLIF((SELECT 37.7749), 0), 15.4321))), 2) + POW(SIN(RADIANS((SELECT -122.4194) / 2)), 2) * COS(RADIANS(LEAST(60, GREATEST(20, (SELECT 25.5))))))) * (1000 + (RAND() * 500 - 250)), 2)"#;
         let tokens = tokenize_sql(case).unwrap();
         let expr = parse_expr(&tokens, Dialect::PostgreSQL).unwrap();
         divan::black_box(expr);
