39fd7306791aa2bb7666ff13db86196bd6eacd1b
Winter Zhang <coswde@gmail.com>
2025-05-19 15:24:28 +0800

    chore(query): add system history enterprise feature (#17961)
    feat(query): add system history enterprise feature

--- src/query/service/src/sessions/query_ctx.rs
+++ src/query/service/src/sessions/query_ctx.rs
@@ -40,6 +40,7 @@ use databend_common_base::runtime::profile::Profile;
 use databend_common_base::runtime::profile::ProfileStatisticsName;
 use databend_common_base::runtime::GlobalIORuntime;
 use databend_common_base::runtime::MemStat;
+use databend_common_base::runtime::ThreadTracker;
 use databend_common_base::runtime::TrySpawn;
 use databend_common_base::JoinHandle;
 use databend_common_catalog::catalog::CATALOG_DEFAULT;
@@ -73,6 +74,8 @@ use databend_common_expression::TableDataType;
 use databend_common_expression::TableField;
 use databend_common_expression::TableSchema;
 use databend_common_io::prelude::FormatSettings;
+use databend_common_license::license::Feature;
+use databend_common_license::license_manager::LicenseManagerSwitch;
 use databend_common_meta_app::principal::FileFormatParams;
 use databend_common_meta_app::principal::GrantObject;
 use databend_common_meta_app::principal::OnErrorMode;
@@ -1160,6 +1163,18 @@ impl TableContext for QueryContext {
         database: &str,
         table: &str,
     ) -> Result<Arc<dyn Table>> {
+        // Queries to non-internal system_history databases require license checks to be enabled.
+        if database.eq_ignore_ascii_case("system_history") && ThreadTracker::should_log() {
+            LicenseManagerSwitch::instance().check_enterprise_enabled(
+                unsafe {
+                    self.get_settings()
+                        .get_enterprise_license()
+                        .unwrap_or_default()
+                },
+                Feature::SystemHistory,
+            )?;
+        }
+
         let batch_size = self.get_settings().get_stream_consume_batch_size_hint()?;
         self.get_table_from_shared(catalog, database, table, batch_size)
             .await
