968771a87b5fc3a9fba9fafb3375c3e37bda0992
Yang Xiufeng <yangxiufeng.c@gmail.com>
2025-05-20 15:46:18 +0800

    add cast from variant to binary/interval.

--- src/query/functions/src/scalars/binary.rs
+++ src/query/functions/src/scalars/binary.rs
@@ -33,7 +33,6 @@ use databend_common_expression::types::NumberDataType;
 use databend_common_expression::types::NumberType;
 use databend_common_expression::types::StringType;
 use databend_common_expression::types::UInt8Type;
-use databend_common_expression::types::VariantType;
 use databend_common_expression::vectorize_1_arg;
 use databend_common_expression::Column;
 use databend_common_expression::EvalContext;
@@ -66,27 +65,6 @@ pub fn register(registry: &mut FunctionRegistry) {
         "try_to_string",
         |_, _| FunctionDomain::Full,
         error_to_null(eval_binary_to_string),
-    );
-
-    registry.register_passthrough_nullable_1_arg::<VariantType, BinaryType, _, _>(
-        "to_binary",
-        |_, _| FunctionDomain::Full,
-        |val, _| match val {
-            Value::Scalar(val) => Value::Scalar(val.to_vec()),
-            Value::Column(col) => Value::Column(col),
-        },
-    );
-
-    registry.register_combine_nullable_1_arg::<VariantType, BinaryType, _, _>(
-        "try_to_binary",
-        |_, _| FunctionDomain::Full,
-        |val, _| match val {
-            Value::Scalar(val) => Value::Scalar(Some(val.to_vec())),
-            Value::Column(col) => {
-                let validity = Bitmap::new_constant(true, col.len());
-                Value::Column(NullableColumn::new(col, validity))
-            }
-        },
     );
 
     registry.register_passthrough_nullable_1_arg::<BitmapType, BinaryType, _, _>(
