dc25c759998d4184e17a149b17fa70527b0af0bf
sundyli <543950155@qq.com>
2025-05-25 18:19:08 +0800

    chore(query): refactor parquet part to make reader work on parallel (#17987)
    * chore(query): refactor parquet part to make reader work on parallel
    
    * chore(query): refactor parquet part to make reader work on parallel
    
    * chore(query): refactor parquet part to make reader work on parallel
    
    * increase timeout
    
    * update
    
    * update
    
    * update
    
    * update

--- src/query/storages/iceberg/src/partition.rs
+++ src/query/storages/iceberg/src/partition.rs
@@ -33,6 +33,7 @@ pub(crate) fn convert_file_scan_task(task: iceberg::scan::FileScanTask) -> Box<d
                 compressed_size: task.length,
                 estimated_uncompressed_size: task.length * 5,
                 dedup_key: format!("{}_{}", task.data_file_path, task.length),
+                bucket_option: None,
             };
 
             if !task.deletes.is_empty() {
