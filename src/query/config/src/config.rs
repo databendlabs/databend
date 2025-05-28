0134b368fe6ad66e8834a6508e2afaecbc330ca2
Liuqing Yue <dqhl76@gmail.com>
2025-05-28 15:35:24 +0800

    benchmark

--- src/query/config/src/config.rs
+++ src/query/config/src/config.rs
@@ -2297,6 +2297,22 @@ impl TryInto<InnerLogConfig> for LogConfig {
             history.level = file.level.clone();
         }
 
+        history.on = true;
+        history.tables = vec![
+            InnerHistoryTableConfig {
+                table_name: "query_history".to_string(),
+                ..Default::default()
+            },
+            InnerHistoryTableConfig {
+                table_name: "profile_history".to_string(),
+                ..Default::default()
+            },
+            InnerHistoryTableConfig {
+                table_name: "login_history".to_string(),
+                ..Default::default()
+            },
+        ];
+
         Ok(InnerLogConfig {
             file,
             stderr: self.stderr.try_into()?,
