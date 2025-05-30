69dba169b0c012f7300f86a8ae6a2a5b8d73fbf4
Liuqing Yue <dqhl76@gmail.com>
2025-05-30 20:37:14 +0800

    fix: `explain analyze` missing partition information (#18040)
    * refactor: clean code, we don't need `dry_run` flag actually
    
    * refactor: enhance pruning statistics handling with plan_id support
    
    * fix: `explain analyze` missing partition information
    
    * chore: add test for `explain analyze` partitions information checking
    
    * fixup
    
    * fixup

--- src/query/service/src/sessions/query_ctx.rs
+++ src/query/service/src/sessions/query_ctx.rs
@@ -1918,12 +1918,12 @@ impl TableContext for QueryContext {
         self.shared.get_warehouse_clusters().await
     }
 
-    fn get_pruned_partitions_stats(&self) -> Option<PartStatistics> {
+    fn get_pruned_partitions_stats(&self) -> HashMap<u32, PartStatistics> {
         self.shared.get_pruned_partitions_stats()
     }
 
-    fn set_pruned_partitions_stats(&self, partitions: PartStatistics) {
-        self.shared.set_pruned_partitions_stats(partitions);
+    fn set_pruned_partitions_stats(&self, plan_id: u32, stats: PartStatistics) {
+        self.shared.set_pruned_partitions_stats(plan_id, stats);
     }
 
     fn get_next_broadcast_id(&self) -> u32 {
