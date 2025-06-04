33e614a0b414f296f91903a53cd7b35173436e75
dantengsky <dantengsky@gmail.com>
2025-06-04 14:47:33 +0800

    chore: tweak license validation logging (#18066)
    chore: Tweak license validation logging
    
    Also log at warning level to ensure the message could be
    propagated to client applications, e.g., BendSQL and MySQL
    interactive sessions

--- src/query/service/src/interpreters/interpreter.rs
+++ src/query/service/src/interpreters/interpreter.rs
@@ -109,10 +109,14 @@ pub trait Interpreter: Sync + Send {
         {
             Ok(_) => true,
             Err(e) => {
-                log::error!(
-                        "[INTERPRETER] CRITICAL ALERT: License validation FAILED - enterprise features DISABLED, System may operate in DEGRADED MODE with LIMITED CAPABILITIES and REDUCED PERFORMANCE. Please contact us at https://www.databend.com/contact-us/ or email hi@databend.com to restore full functionality: {}",
-                        e
-                    );
+                let msg =
+                    format!("[INTERPRETER] CRITICAL ALERT: License validation FAILED - enterprise features DISABLED, System may operate in DEGRADED MODE with LIMITED CAPABILITIES and REDUCED PERFORMANCE. Please contact us at https://www.databend.com/contact-us/ or email hi@databend.com to restore full functionality: {}",
+                e);
+                log::error!("{}", msg);
+
+                // Also log at warning level to ensure the message could be propagated to client applications
+                // (e.g., BendSQL and MySQL interactive sessions)
+                log::warn!("{}", msg);
                 false
             }
         };
