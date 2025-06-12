679665895be8845d2a901ea69b0593e3fe2c803c
Yang Xiufeng <yangxiufeng.c@gmail.com>
2025-06-12 18:44:25 +0800

    Update streaming_load.rs

--- src/query/storages/stage/src/streaming_load.rs
+++ src/query/storages/stage/src/streaming_load.rs
@@ -75,7 +75,7 @@ pub fn build_streaming_load_pipeline(
     let max_threads = settings.get_max_threads()? as usize;
 
     // since there are only one source, a few processor is fast enough and avoid both OOM and small DataBlocks.
-    let max_threads = max_threads.max(4);
+    let max_threads = max_threads.min(4);
 
     let file_format_options_ext = FileFormatOptionsExt::create_from_settings(&settings, false)?;
 
