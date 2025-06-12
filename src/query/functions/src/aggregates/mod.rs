e58eeeb87db1e434453fed176dfdfcc9e64dcee5
sundyli <543950155@qq.com>
2025-06-12 12:18:04 +0800

    feat(query): add bool_and and bool_or agg functions (#18132)
    * feat(query): add bool_and and bool_or agg functions
    
    * feat(query): add bool_and and bool_or agg functions
    
    * feat(query): add bool_and and bool_or agg functions
    
    * feat(query): add bool_and and bool_or agg functions
    
    * feat(query): fix

--- src/query/functions/src/aggregates/mod.rs
+++ src/query/functions/src/aggregates/mod.rs
@@ -23,6 +23,7 @@ mod aggregate_array_agg;
 mod aggregate_array_moving;
 mod aggregate_avg;
 mod aggregate_bitmap;
+mod aggregate_boolean;
 mod aggregate_combinator_distinct;
 mod aggregate_combinator_if;
 mod aggregate_combinator_state;
