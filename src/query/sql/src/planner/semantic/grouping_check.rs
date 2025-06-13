ddc5c24ab0cc507cc33f58fc71fbcab675afe8fd
sundyli <543950155@qq.com>
2025-06-13 09:56:59 +0800

    feat(query): fix window binder work with group by expr alias (#18148)
    * feat(query): fix
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- src/query/sql/src/planner/semantic/grouping_check.rs
+++ src/query/sql/src/planner/semantic/grouping_check.rs
@@ -204,6 +204,16 @@ impl VisitorMut<'_> for GroupingChecker<'_> {
         if self
             .bind_context
             .aggregate_info
+            .group_items
+            .iter()
+            .any(|item| item.index == column.column.index)
+        {
+            return Ok(());
+        }
+
+        if self
+            .bind_context
+            .aggregate_info
             .get_aggregate_function(&column.column.column_name)
             .is_some()
         {
