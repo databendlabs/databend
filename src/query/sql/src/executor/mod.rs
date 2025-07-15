5e6bedf197aba162874370083319a9e2e508b4bd
zhang2014 <coswde@gmail.com>
2025-07-15 16:33:26 +0800

    refactor(query): use trait to refactor physical plan

--- src/query/sql/src/executor/mod.rs
+++ src/query/sql/src/executor/mod.rs
@@ -12,21 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod format;
 pub mod physical_plans;
 mod util;
 
 pub mod table_read_plan;
 
-// pub use physical_plan::DeriveHandle;
-// pub use physical_plan::IPhysicalPlan;
-// pub use physical_plan::PhysicalPlan;
-// pub use physical_plan::PhysicalPlanDynExt;
-// pub use physical_plan::PhysicalPlanMeta;
-// pub use databend_query::physical_plan::physical_plan_builder::MutationBuildInfo;
-// pub use databend_query::physical_plan::physical_plan_builder::PhysicalPlanBuilder;
-// pub use physical_plans::build_broadcast_plans;
-// pub use physical_plans::PhysicalRuntimeFilter;
-// pub use physical_plans::PhysicalRuntimeFilters;
-// pub use physical_plan::PhysicalPlanVisitor;
 pub use util::*;
