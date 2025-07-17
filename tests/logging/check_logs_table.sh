f78130de65710b4e3e417473fc18fc09e1916572
TCeason <tai_chong@foxmail.com>
2025-07-17 19:55:01 +0800

    fix(query): the stage utilized by the history table needs to be restricted
    The history table uses a stage named log_1f93b76af0bd4b1d8e018667865fbc65 to store intermediate raw log files, and access to this stage need also be controlled by the privilege system.

--- tests/logging/check_logs_table.sh
+++ tests/logging/check_logs_table.sh
@@ -57,47 +57,6 @@ response=$(curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-
 # Wait for the query to be logged
 sleep 13
 
-# Test 1
-check_query_log "basic-1" "$query_id" "SELECT count(*) FROM system_history.log_history WHERE target = 'databend::log::profile' and" "1"
-
-# Test 2
-check_query_log "basic-2" "$query_id" "SELECT count(*) FROM system_history.profile_history WHERE" "1"
-
-# Test 3
-check_query_log "basic-3" "$query_id" "SELECT count(*) FROM system_history.log_history WHERE target = 'databend::log::query' and" "2"
-
-# Test 4
-check_query_log "basic-4" "$query_id" "SELECT count(*) FROM system_history.query_history WHERE" "1"
-
-
-# Test 6
-check_query_log "basic-6" "$select_query_id" "SELECT count(*) FROM system_history.access_history WHERE" "1"
-
-# Test 7
-check_query_log "basic-7" "$create_query_id" "SELECT object_modified_by_ddl[0]['object_name'] FROM system_history.access_history WHERE" "default.default.t"
-
-# Test 8
-check_query_log "basic-8" "$insert_query_id" "SELECT objects_modified[0]['object_name'] FROM system_history.access_history WHERE" "default.default.t"
-
-# Test 9
-check_query_log "basic-9" "$select_query_id" "SELECT base_objects_accessed[0]['object_name'] FROM system_history.access_history WHERE" "default.default.t"
-
-# Test 10
-check_query_log "basic-10" $create_view_query_id "select query_text from system_history.query_history where" "CREATE VIEW v AS SELECT a FROM t"
-
-# Test 11
-check_query_log "basic-11" null "SELECT count(*) FROM system_history.login_history WHERE session_id = '$select_session_id' " "1"
-
-# Check timezone, regression test for https://github.com/databendlabs/databend/pull/18059
-check_query_log "t-1" "$select_query_id" "settings (timezone='Asia/Shanghai') SELECT DATE_DIFF(hour, timestamp, now()) FROM system_history.log_history WHERE target = 'databend::log::profile' and" "0"
-
-check_query_log "t-2" "$select_query_id" "settings (timezone='Asia/Shanghai') SELECT DATE_DIFF(hour, event_time, now()) FROM system_history.query_history WHERE" "0"
-
-check_query_log "t-3" "$select_query_id" "settings (timezone='Asia/Shanghai') SELECT DATE_DIFF(hour, timestamp, now()) FROM system_history.profile_history WHERE" "0"
-
-check_query_log "t-4" null "settings (timezone='Asia/Shanghai') SELECT sum(DATE_DIFF(hour, event_time, now())) FROM system_history.login_history" "0"
-
-check_query_log "t-5" "$select_query_id" "settings (timezone='Asia/Shanghai') SELECT DATE_DIFF(hour, query_start, now()) FROM system_history.access_history WHERE" "0"
 
 response=$(curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-Type: application/json' -d '{"sql": "drop user if exists a"}')
 response=$(curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-Type: application/json' -d '{"sql": "drop role if exists ra"}')
@@ -106,6 +65,7 @@ response=$(curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-
 response=$(curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-Type: application/json' -d '{"sql": "grant select , drop on system_history.* to role ra"}')
 response=$(curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-Type: application/json' -d '{"sql": "grant alter on system_history.* to role ra"}')
 response=$(curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-Type: application/json' -d '{"sql": "grant role ra to a"}')
+response=$(curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-Type: application/json' -d '{"sql": "grant write, read on stage log_1f93b76af0bd4b1d8e018667865fbc65 to a"}')
 
 execute_and_verify() {
     local cmd_description="$1"
@@ -114,7 +74,7 @@ execute_and_verify() {
     local jq_expression="$4"
     local result
 
-    echo -n "Executing: $cmd_description ... "
+    echo "Executing: $cmd_description ... "
 
     result=$(curl -s -u "${user_cred}" -XPOST "http://localhost:8000/v1/query" -H 'Content-Type: application/json' -d "${json_payload}" | jq -r "${jq_expression}")
 
@@ -122,6 +82,8 @@ execute_and_verify() {
         echo "Failed! Expected result: true, actual result: $result."
         echo "$cmd_description failed."
         exit 1 # Exit script immediately if it fails
+    else
+        echo "Description: $cmd_description completed successfully."
     fi
 }
 
@@ -140,32 +102,67 @@ check_system_history_permissions() {
         '{"sql": "alter table system_history.log_history add column id int"}' \
         '.state == "Failed"'
 
-    # Command 3: User 'a:123' attempts to drop 'system_history.access_history' table (expected to succeed)
+    # Command 3: User 'a:123' drop 'system_history.access_history' table (expected to succeed)
     execute_and_verify \
-        "User 'a:123' attempts to drop 'system_history.access_history' table" \
+        "User 'a:123' drop 'system_history.access_history' table" \
         "a:123" \
         '{"sql": "drop table system_history.access_history"}' \
         '.state == "Succeeded"'
 
-    # Command 4: User 'root:' attempts to grant ownership on 'system_history.*' (expected to fail)
+    # Command 4: User 'root:' grant ownership on 'system_history.*' (expected to fail)
     execute_and_verify \
-        "User 'root:' attempts to grant ownership on 'system_history.*'" \
+        "User 'root:' grant ownership on 'system_history.*'" \
         "root:" \
         '{"sql": "grant ownership on system_history.* to role ra"}' \
         '.state == "Failed"'
 
-    # Command 5: User 'root:' attempts to grant ownership on 'system_history.query_history' (expected to fail)
+    # Command 5: User 'root:' grant ownership on 'system_history.query_history' (expected to fail)
     execute_and_verify \
-        "User 'root:' attempts to grant ownership on 'system_history.query_history'" \
+        "User 'root:' grant ownership on 'system_history.query_history'" \
         "root:" \
         '{"sql": "grant ownership on system_history.query_history to role ra"}' \
         '.state == "Failed"'
-    # Command 6: User 'a:123' attempts to select 'system_history.query_history' table (expected to succeed)
+    # Command 6: User 'a:123' select 'system_history.query_history' table (expected to succeed)
         execute_and_verify \
-            "User 'a:123' attempts to query 'system_history.query_history' table" \
+            "User 'a:123' query 'system_history.query_history' table" \
             "a:123" \
             '{"sql": "select count() from system_history.query_history"}' \
             '.state == "Succeeded"'
+
+    # Command 7: User 'a:123' drop stage 'log_1f93b76af0bd4b1d8e018667865fbc65' table (expected to failed)
+            execute_and_verify \
+                "User 'a:123' drop stage log_1f93b76af0bd4b1d8e018667865fbc65" \
+                "a:123" \
+                '{"sql": "drop stage log_1f93b76af0bd4b1d8e018667865fbc65"}' \
+                '.state == "Failed"'
+
+    # Command 8: User 'a:123' copy into @log_1f93b76af0bd4b1d8e018667865fbc65 from (select * from system.one) (expected to failed)
+                execute_and_verify \
+                    "User 'a:123' copy into @log_1f93b76af0bd4b1d8e018667865fbc65 from (select * from system.one);" \
+                    "a:123" \
+                    '{"sql": "copy into @log_1f93b76af0bd4b1d8e018667865fbc65 from (select * from system.one);"}' \
+                    '.state == "Failed"'
+
+    # Command 9: User 'a:123' copy into t from (select * from @log_1f93b76af0bd4b1d8e018667865fbc65) (expected to failed)
+                execute_and_verify \
+                    "User 'a:123' copy into t from (select * from @log_1f93b76af0bd4b1d8e018667865fbc65);" \
+                    "a:123" \
+                    '{"sql": "copy into t from (select * from @log_1f93b76af0bd4b1d8e018667865fbc65);"}' \
+                    '.state == "Failed"'
+
+    # Command 10: User 'root' grant stage 'log_1f93b76af0bd4b1d8e018667865fbc65' ownership (expected to failed)
+                execute_and_verify \
+                    "User 'root' grant ownership on stage log_1f93b76af0bd4b1d8e018667865fbc65 to role ra" \
+                    "root:" \
+                    '{"sql": "grant ownership on stage log_1f93b76af0bd4b1d8e018667865fbc65 to role ra"}' \
+                    '.state == "Failed"'
+
+    # Command 11: User 'root' drop stage 'log_1f93b76af0bd4b1d8e018667865fbc65' (expected to succeed)
+                execute_and_verify \
+                    "User 'root' drop stage log_1f93b76af0bd4b1d8e018667865fbc65" \
+                    "root:" \
+                    '{"sql": "drop stage log_1f93b76af0bd4b1d8e018667865fbc65"}' \
+                    '.state == "Succeeded"'
 }
 
 check_system_history_permissions
