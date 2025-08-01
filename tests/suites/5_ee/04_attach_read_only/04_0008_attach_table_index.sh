5df4c7ad4184f735ed8f546446bb7e6ee98bd3f8
kould <kould2333@gmail.com>
2025-08-01 18:22:36 +0800

    chore: fix test

--- tests/suites/5_ee/04_attach_read_only/04_0008_attach_table_index.sh
+++ tests/suites/5_ee/04_attach_read_only/04_0008_attach_table_index.sh
@@ -4,7 +4,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../../../shell_env.sh
 
 echo "drop table if exists phrases" | $BENDSQL_CLIENT_CONNECT
-echo "create table phrases (id INT, text STRING, NGRAM INDEX idx_text (text))" | $BENDSQL_CLIENT_CONNECT
+echo "create table phrases (id INT, text STRING, NGRAM INDEX idx_text (text)) 's3://testbucket/admin/data/' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}')" | $BENDSQL_CLIENT_CONNECT
 
 echo "init table"
 echo "INSERT INTO phrases VALUES(1, 'apple banana cherry'),(2, 'banana date fig'),(3, 'cherry elderberry fig'),(4, 'date grape kiwi')" | $BENDSQL_CLIENT_CONNECT
@@ -17,19 +17,19 @@ echo "attach table att_phrases 's3://testbucket/admin/$storage_prefix' connectio
 
 # check index
 echo "check index"
-echo "show indexes" | $BENDSQL_CLIENT_CONNECT
+echo "select name, type, database, table from system.indexes;" | $BENDSQL_CLIENT_CONNECT
 
 # refresh ngram index
-stmt "REFRESH NGRAM INDEX idx1 ON att_phrases;"
+stmt "REFRESH NGRAM INDEX idx_text ON phrases;"
 echo "drop table if exists att_phrases" | $BENDSQL_CLIENT_CONNECT
-echo "attach table att_phrases 's3://testbucket/admin/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
-echo "show indexes" | $BENDSQL_CLIENT_CONNECT
+echo "attach table att_phrases 's3://testbucket/admin/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
+echo "select name, type, database, table from system.indexes;" | $BENDSQL_CLIENT_CONNECT
 
 # drop ngram index
-stmt "DROP NGRAM INDEX idx1 ON att_phrases;"
+stmt "DROP NGRAM INDEX idx_text ON phrases;"
 echo "drop table if exists att_phrases" | $BENDSQL_CLIENT_CONNECT
-echo "attach table att_phrases 's3://testbucket/admin/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
-echo "show indexes" | $BENDSQL_CLIENT_CONNECT
+echo "attach table att_phrases 's3://testbucket/admin/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
+echo "select name, type, database, table from system.indexes;" | $BENDSQL_CLIENT_CONNECT
 
 echo "drop table if exists phrases" | $BENDSQL_CLIENT_CONNECT
 echo "drop table if exists att_phrases" | $BENDSQL_CLIENT_CONNECT
