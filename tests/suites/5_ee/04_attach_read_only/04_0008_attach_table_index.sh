38307981ebb6f92a25197fcc894323317f7e8901
kould <kould2333@gmail.com>
2025-08-04 14:25:40 +0800

    chore: fix test

--- tests/suites/5_ee/04_attach_read_only/04_0008_attach_table_index.sh
+++ tests/suites/5_ee/04_attach_read_only/04_0008_attach_table_index.sh
@@ -8,7 +8,7 @@ echo "create table phrases (id INT, text STRING,  text_0 STRING, embedding Vecto
 echo "CREATE INVERTED INDEX idx_inverted ON phrases(text_0);" | $BENDSQL_CLIENT_CONNECT
 
 echo "init table"
-echo "INSERT INTO phrases VALUES(1, 'apple banana cherry', 'apple banana cherry', 0.50515236),(2, 'banana date fig', 'banana date fig', 0.8561939),(3, 'cherry elderberry fig', 'cherry elderberry fig', 0.87169914),(4, 'date grape kiwi', 'date grape kiwi', 0.55843271)" | $BENDSQL_CLIENT_CONNECT
+echo "INSERT INTO phrases VALUES(1, 'apple banana cherry', 'apple banana cherry', [0.61418788, 0.34545306, 0.14638622, 0.53249639, 0.09139293, 0.84940919, 0.105433, 0.4156201]),(2, 'banana date fig', 'banana date fig', [0.21828953, 0.87852734, 0.64221122, 0.24536394, 0.81689593, 0.86341877, 0.7218334, 0.45028494]),(3, 'cherry elderberry fig', 'cherry elderberry fig', [0.43279006, 0.45523681, 0.76060274, 0.66284758, 0.19131476, 0.13564463, 0.88712212, 0.93279565]),(4, 'date grape kiwi', 'date grape kiwi', [0.79671359, 0.86079789, 0.94477631, 0.5116732, 0.29733205, 0.33645561, 0.41380333, 0.75909903])" | $BENDSQL_CLIENT_CONNECT
 
 storage_prefix=$(mysql -uroot -h127.0.0.1 -P3307  -e "set global hide_options_in_show_create_table=0;show create table phrases" | grep -i snapshot_location | awk -F'SNAPSHOT_LOCATION='"'"'|_ss' '{print $2}')
 
