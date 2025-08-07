7cbe13d96faa9e56260ee66a0bd1334b0b3804db
TCeason <tai_chong@foxmail.com>
2025-08-07 16:18:50 +0800

    Introduce Experimental Feature Gate
    enable_experimental_row_access_policy default disable

--- src/query/sql/src/planner/binder/binder.rs
+++ src/query/sql/src/planner/binder/binder.rs
@@ -612,9 +612,39 @@ impl Binder {
             }
 
             // RowAccessPolicy
-            Statement::CreateRowAccessPolicy(stmt) => self.bind_create_row_access(stmt).await?,
-            Statement::DropRowAccessPolicy(stmt) => self.bind_drop_row_access(stmt).await?,
-            Statement::DescRowAccessPolicy(stmt) => self.bind_desc_row_access(stmt).await?,
+            Statement::CreateRowAccessPolicy(stmt) => {
+                if self
+                    .ctx
+                    .get_settings()
+                    .get_enable_experimental_row_access_policy()?
+                {
+                    self.bind_create_row_access(stmt).await?
+                } else {
+                    return Err(ErrorCode::Unimplemented("Experimental Row Access Policy is unstable and may have compatibility issues. To use it, set enable_experimental_row_access_policy=1"));
+                }
+            }
+            Statement::DropRowAccessPolicy(stmt) => {
+                if self
+                    .ctx
+                    .get_settings()
+                    .get_enable_experimental_row_access_policy()?
+                {
+                    self.bind_drop_row_access(stmt).await?
+                } else {
+                    return Err(ErrorCode::Unimplemented("Experimental Row Access Policy is unstable and may have compatibility issues. To use it, set enable_experimental_row_access_policy=1"));
+                }
+            }
+            Statement::DescRowAccessPolicy(stmt) => {
+                if self
+                    .ctx
+                    .get_settings()
+                    .get_enable_experimental_row_access_policy()?
+                {
+                    self.bind_desc_row_access(stmt).await?
+                } else {
+                    return Err(ErrorCode::Unimplemented("Experimental Row Access Policy is unstable and may have compatibility issues. To use it, set enable_experimental_row_access_policy=1"));
+                }
+            }
             Statement::SetRole {
                 is_default,
                 role_name,
