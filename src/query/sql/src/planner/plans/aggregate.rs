61a5404759fc04cca5b72b855930273417af3ecc
sundyli <543950155@qq.com>
2025-08-09 23:35:40 +0800

    fix

--- src/query/sql/src/planner/plans/aggregate.rs
+++ src/query/sql/src/planner/plans/aggregate.rs
@@ -89,14 +89,6 @@ impl Aggregate {
             self.group_items
                 .iter()
                 .enumerate()
-                .filter(|(_, item)| {
-                    // skip _grouping_id column cause this column is generated in expansion
-                    if let ScalarExpr::BoundColumnRef(c) = &item.scalar {
-                        c.column.column_name != "_grouping_id"
-                    } else {
-                        true
-                    }
-                })
                 .map(|(index, item)| item.bound_column_expr(format!("_group_item_{}", index)))
                 .collect()
         } else {
@@ -347,7 +339,14 @@ impl Operator for Aggregate {
                     let settings = ctx.get_settings();
 
                     // Group aggregation, enforce `Hash` distribution
-                    match settings.get_group_by_shuffle_mode()?.as_str() {
+                    let mut group_by_shuffle_mode = settings.get_group_by_shuffle_mode()?;
+
+                    // Grouping set must be merged before_merge
+                    if self.grouping_sets.is_some() {
+                        group_by_shuffle_mode = "before_merge".to_string();
+                    }
+
+                    match group_by_shuffle_mode.as_str() {
                         "before_partial" => {
                             children_required.push(vec![RequiredProperty {
                                 distribution: Distribution::Hash(self.get_distribution_keys(true)?),
