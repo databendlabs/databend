d63e2315b5f80ad1ed2e11ff647183be069d452d
zhang2014 <coswde@gmail.com>
2025-08-11 01:02:30 +0800

    refactor(query): use trait to refactor physical plan

--- src/query/service/src/physical_plans/mod.rs
+++ src/query/service/src/physical_plans/mod.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 mod physical_add_stream_column;
 mod physical_aggregate_expand;
 mod physical_aggregate_final;
