1cc8a05a1c2685c600855fd9af9eee0a494c5455
kould <kould2333@gmail.com>
2025-08-12 19:11:22 +0800

    chore: codefmt

--- src/meta/proto-conv/tests/it/v142_udtf.rs
+++ src/meta/proto-conv/tests/it/v142_udtf.rs
@@ -14,6 +14,7 @@
 
 use chrono::DateTime;
 use chrono::Utc;
+use databend_common_expression::types::DataType;
 use databend_common_meta_app::principal::UDFDefinition;
 use databend_common_meta_app::principal::UserDefinedFunction;
 use databend_common_meta_app::principal::UDTF;
@@ -35,18 +36,20 @@ use crate::common;
 fn test_decode_v142_udtf() -> anyhow::Result<()> {
     let bytes = vec![
         10, 9, 116, 101, 115, 116, 95, 117, 100, 116, 102, 18, 21, 84, 104, 105, 115, 32, 105, 115,
-        32, 97, 32, 100, 101, 115, 99, 114, 105, 112, 116, 105, 111, 110, 66, 25, 26, 16, 115, 101,
-        108, 101, 99, 116, 32, 42, 32, 102, 114, 111, 109, 32, 116, 49, 160, 6, 142, 1, 168, 6, 24,
-        42, 23, 50, 48, 50, 51, 45, 49, 50, 45, 49, 53, 32, 48, 49, 58, 50, 54, 58, 48, 57, 32, 85,
-        84, 67, 160, 6, 142, 1, 168, 6, 24,
+        32, 97, 32, 100, 101, 115, 99, 114, 105, 112, 116, 105, 111, 110, 66, 79, 10, 16, 10, 2,
+        99, 49, 18, 10, 146, 2, 0, 160, 6, 142, 1, 168, 6, 24, 10, 16, 10, 2, 99, 50, 18, 10, 138,
+        2, 0, 160, 6, 142, 1, 168, 6, 24, 18, 16, 10, 2, 99, 51, 18, 10, 170, 2, 0, 160, 6, 142, 1,
+        168, 6, 24, 26, 16, 115, 101, 108, 101, 99, 116, 32, 42, 32, 102, 114, 111, 109, 32, 116,
+        49, 160, 6, 142, 1, 168, 6, 24, 42, 23, 50, 48, 50, 51, 45, 49, 50, 45, 49, 53, 32, 48, 49,
+        58, 50, 54, 58, 48, 57, 32, 85, 84, 67, 160, 6, 142, 1, 168, 6, 24,
     ];
 
     let want = || UserDefinedFunction {
         name: "test_udtf".to_string(),
         description: "This is a description".to_string(),
         definition: UDFDefinition::UDTF(UDTF {
-            arg_types: vec![],
-            return_types: vec![],
+            arg_types: vec![(s("c1"), DataType::String), (s("c2"), DataType::Boolean)],
+            return_types: vec![(s("c3"), DataType::Date)],
             sql: "select * from t1".to_string(),
         }),
         created_on: DateTime::<Utc>::from_timestamp(1702603569, 0).unwrap(),
@@ -54,4 +57,8 @@ fn test_decode_v142_udtf() -> anyhow::Result<()> {
 
     common::test_pb_from_to(func_name!(), want())?;
     common::test_load_old(func_name!(), bytes.as_slice(), 142, want())
+}
+
+fn s(ss: impl ToString) -> String {
+    ss.to_string()
 }
