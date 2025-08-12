8f15b6c4924782a114fac79027d5319b0cd2570d
coldWater <forsaken628@gmail.com>
2025-08-12 19:00:43 +0800

    logforth

--- src/common/tracing/src/loggers.rs
+++ src/common/tracing/src/loggers.rs
@@ -12,19 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::io::Write;
 use std::path::Path;
 
 use databend_common_base::runtime::LimitMemGuard;
 use databend_common_base::runtime::ThreadTracker;
 use log::Record;
-use logforth::append::rolling_file::NonBlockingBuilder;
 use logforth::append::rolling_file::RollingFileWriter;
 use logforth::append::rolling_file::Rotation;
 use logforth::append::RollingFile;
-use logforth::layout::collect_kvs;
-use logforth::layout::CustomLayout;
-use logforth::layout::KvDisplay;
+use logforth::non_blocking::NonBlockingBuilder;
 use logforth::Append;
+use logforth::Diagnostic;
 use logforth::Layout;
 use serde_json::Map;
 
@@ -42,9 +41,7 @@ pub(crate) fn new_rolling_file_appender(
         .max_file_size(max_file_size)
         .build(dir)
         .expect("failed to initialize rolling file appender");
-    let (non_blocking, guard) = NonBlockingBuilder::default()
-        .thread_name("log-file-appender")
-        .finish(rolling);
+    let (non_blocking, guard) = NonBlockingBuilder::new("log-file-appender", rolling).finish();
 
     (
         RollingFileWarp::new(RollingFile::new(non_blocking)),
@@ -63,70 +60,62 @@ impl RollingFileWarp {
     }
 
     /// Sets the layout used to format log records as bytes.
-    pub fn with_layout(self, layout: impl Into<Layout>) -> Self {
+    pub fn with_layout(self, layout: impl Layout) -> Self {
         Self::new(self.inner.with_layout(layout))
     }
 }
 
 impl Append for RollingFileWarp {
-    fn append(&self, record: &Record) -> anyhow::Result<()> {
+    fn append(&self, record: &Record, diagnostics: &[Diagnostic]) -> anyhow::Result<()> {
         let _guard = LimitMemGuard::enter_unlimited();
-        self.inner.append(record)
+        self.inner.append(record, diagnostics)
     }
 }
 
-pub fn get_layout(format: &str) -> Layout {
-    match format {
-        "text" => text_layout(),
-        "json" => json_layout(),
-        "identical" => identical_layout(),
-        _ => unimplemented!("file logging format {format} is not supported"),
+#[derive(Debug)]
+pub struct IdenticalLayout;
+
+impl Layout for IdenticalLayout {
+    fn format(&self, record: &Record, _diagnostics: &[Diagnostic]) -> anyhow::Result<Vec<u8>> {
+        Ok(format!("{}", record.args()).into_bytes())
     }
 }
 
-fn identical_layout() -> Layout {
-    CustomLayout::new(|record: &Record| Ok(format!("{}", record.args()).into_bytes())).into()
+#[derive(Debug)]
+
+pub struct TextLayout;
+
+impl Layout for TextLayout {
+    fn format(&self, record: &Record, _diagnostics: &[Diagnostic]) -> anyhow::Result<Vec<u8>> {
+        let mut buf = Vec::new();
+        if let Some(query_id) = ThreadTracker::query_id() {
+            write!(buf, "{query_id} ")?;
+        }
+
+        write!(
+            buf,
+            "{} {:>5} {}: {}:{} {}",
+            chrono::Local::now().to_rfc3339_opts(chrono::SecondsFormat::Micros, true),
+            record.level(),
+            record.module_path().unwrap_or(""),
+            Path::new(record.file().unwrap_or_default())
+                .file_name()
+                .and_then(|name| name.to_str())
+                .unwrap_or_default(),
+            record.line().unwrap_or(0),
+            record.args(),
+        )?;
+        record.key_values().visit(&mut KvWriter(&mut buf))?;
+
+        Ok(buf)
+    }
 }
 
-fn text_layout() -> Layout {
-    CustomLayout::new(|record: &Record| {
-        let s = match ThreadTracker::query_id() {
-            None => format!(
-                "{} {:>5} {}: {}:{} {}{}",
-                chrono::Local::now().to_rfc3339_opts(chrono::SecondsFormat::Micros, true),
-                record.level(),
-                record.module_path().unwrap_or(""),
-                Path::new(record.file().unwrap_or_default())
-                    .file_name()
-                    .and_then(|name| name.to_str())
-                    .unwrap_or_default(),
-                record.line().unwrap_or(0),
-                record.args(),
-                KvDisplay::new(record.key_values()),
-            ),
-            Some(query_id) => format!(
-                "{} {} {:>5} {}: {}:{} {}{}",
-                query_id,
-                chrono::Local::now().to_rfc3339_opts(chrono::SecondsFormat::Micros, true),
-                record.level(),
-                record.module_path().unwrap_or(""),
-                Path::new(record.file().unwrap_or_default())
-                    .file_name()
-                    .and_then(|name| name.to_str())
-                    .unwrap_or_default(),
-                record.line().unwrap_or(0),
-                record.args(),
-                KvDisplay::new(record.key_values()),
-            ),
-        };
+#[derive(Debug)]
+pub struct JsonLayout;
 
-        Ok(s.into_bytes())
-    })
-    .into()
-}
-
-fn json_layout() -> Layout {
-    CustomLayout::new(|record: &Record| {
+impl Layout for JsonLayout {
+    fn format(&self, record: &Record, _diagnostics: &[Diagnostic]) -> anyhow::Result<Vec<u8>> {
         let mut fields = Map::new();
         fields.insert("message".to_string(), format!("{}", record.args()).into());
         for (k, v) in collect_kvs(record.key_values()) {
@@ -154,6 +143,39 @@ fn json_layout() -> Layout {
         };
 
         Ok(s.into_bytes())
-    })
-    .into()
+    }
+}
+
+pub struct KvWriter<'a>(pub &'a mut Vec<u8>);
+
+impl<'kvs> log::kv::VisitSource<'kvs> for KvWriter<'_> {
+    fn visit_pair(
+        &mut self,
+        key: log::kv::Key<'kvs>,
+        value: log::kv::Value<'kvs>,
+    ) -> Result<(), log::kv::Error> {
+        write!(self.0, " {key}={value}")?;
+        Ok(())
+    }
+}
+
+pub fn collect_kvs(kv: &dyn log::kv::Source) -> Vec<(String, String)> {
+    let mut collector = KvCollector { kv: Vec::new() };
+    kv.visit(&mut collector).ok();
+    collector.kv
+}
+
+struct KvCollector {
+    kv: Vec<(String, String)>,
+}
+
+impl<'kvs> log::kv::VisitSource<'kvs> for KvCollector {
+    fn visit_pair(
+        &mut self,
+        key: log::kv::Key<'kvs>,
+        value: log::kv::Value<'kvs>,
+    ) -> Result<(), log::kv::Error> {
+        self.kv.push((key.to_string(), value.to_string()));
+        Ok(())
+    }
 }
