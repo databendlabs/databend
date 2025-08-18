d320f33493a5dd5fed6628d72b1d0386701cdd50
TCeason <tai_chong@foxmail.com>
2025-08-18 12:09:08 +0800

    fmt code

--- src/query/service/src/interpreters/interpreter_table_row_access_add.rs
+++ src/query/service/src/interpreters/interpreter_table_row_access_add.rs
@@ -23,8 +23,8 @@ use databend_common_meta_app::schema::DatabaseType;
 use databend_common_meta_app::schema::SetTableRowAccessPolicyAction;
 use databend_common_meta_app::schema::SetTableRowAccessPolicyReq;
 use databend_common_sql::plans::AddTableRowAccessPolicyPlan;
-use databend_common_storages_stream::stream_table::STREAM_ENGINE;
 use databend_common_storages_basic::view_table::VIEW_ENGINE;
+use databend_common_storages_stream::stream_table::STREAM_ENGINE;
 use databend_common_users::UserApiProvider;
 use databend_enterprise_row_access_policy_feature::get_row_access_policy_handler;
 
