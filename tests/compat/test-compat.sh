1a52991a6d848cf0c738b40de02dd1fa4e944900
everpcpc <git@everpcpc.com>
2025-08-27 21:06:38 +0800

    z

--- tests/compat/test-compat.sh
+++ tests/compat/test-compat.sh
@@ -252,8 +252,8 @@ run_test() {
 	echo " === Run metasrv related test: 05_ddl"
 
 	if [ "$query_ver" = "current" ]; then
-		# Only run test on mysql handler
-		$sqllogictests --handlers mysql --run_dir 05_ddl
+		# Only run test on http handler
+		$sqllogictests --handlers http --run_dir 05_ddl
 	else
 		(
 			# download suites into ./testsuite/$query_ver/{bin/,suites/}
@@ -264,7 +264,7 @@ run_test() {
 		rm -rf "tests/sqllogictests/suites"
 		mv "./testsuite/$query_ver/suites" "tests/sqllogictests/suites"
 
-    ./testsuite/$query_ver/bin/databend-sqllogictests --handlers mysql --run_dir 05_ddl
+		./testsuite/$query_ver/bin/databend-sqllogictests --handlers http --run_dir 05_ddl
 		cd -
 	fi
 }
