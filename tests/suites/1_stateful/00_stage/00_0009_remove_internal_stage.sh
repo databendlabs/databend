133c97cdbe5e8d83e3b2600736d59c32fbb18bb0
Yang Xiufeng <yangxiufeng.c@gmail.com>
2025-08-31 16:27:49 +0800

    ci: unify path of minio test data path.

--- tests/suites/1_stateful/00_stage/00_0009_remove_internal_stage.sh
+++ tests/suites/1_stateful/00_stage/00_0009_remove_internal_stage.sh
@@ -9,13 +9,13 @@ echo "drop stage if exists s1" | $BENDSQL_CLIENT_CONNECT
 ## Copy from internal stage
 echo "CREATE STAGE s1;" | $BENDSQL_CLIENT_CONNECT
 
-aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/admin/data/ontime_200.csv s3://testbucket/admin/stage/internal/s1/ontime_200.csv >/dev/null 2>&1
-aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/admin/data/ontime_200.csv.gz s3://testbucket/admin/stage/internal/s1/ontime_200.csv.gz >/dev/null 2>&1
-aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/admin/data/ontime_200.csv.zst s3://testbucket/admin/stage/internal/s1/ontime_200.csv.zst >/dev/null 2>&1
+aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/data/ontime_200.csv s3://testbucket/admin/stage/internal/s1/ontime_200.csv >/dev/null 2>&1
+aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/data/ontime_200.csv.gz s3://testbucket/admin/stage/internal/s1/ontime_200.csv.gz >/dev/null 2>&1
+aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/data/ontime_200.csv.zst s3://testbucket/admin/stage/internal/s1/ontime_200.csv.zst >/dev/null 2>&1
 
-aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/admin/data/ontime_200.csv s3://testbucket/admin/stage/internal/s1/dir/ontime_200.csv >/dev/null 2>&1
-aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/admin/data/ontime_200.csv.gz s3://testbucket/admin/stage/internal/s1/dir/ontime_200.csv.gz >/dev/null 2>&1
-aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/admin/data/ontime_200.csv.zst s3://testbucket/admin/stage/internal/s1/dir/ontime_200.csv.zst >/dev/null 2>&1
+aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/data/ontime_200.csv s3://testbucket/admin/stage/internal/s1/dir/ontime_200.csv >/dev/null 2>&1
+aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/data/ontime_200.csv.gz s3://testbucket/admin/stage/internal/s1/dir/ontime_200.csv.gz >/dev/null 2>&1
+aws --endpoint-url http://127.0.0.1:9900/ s3 cp s3://testbucket/data/ontime_200.csv.zst s3://testbucket/admin/stage/internal/s1/dir/ontime_200.csv.zst >/dev/null 2>&1
 
 ## List files in internal stage
 echo "=== List files in internal stage ==="
--- tests/suites/5_ee/04_attach_read_only/04_0008_attach_table_index.sh
+++ tests/suites/5_ee/04_attach_read_only/04_0008_attach_table_index.sh
@@ -4,7 +4,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../../../shell_env.sh
 
 echo "drop table if exists phrases" | $BENDSQL_CLIENT_CONNECT
-echo "create table phrases (id INT, text STRING,  text_0 STRING, embedding Vector(8), NGRAM INDEX idx_text (text), VECTOR INDEX idx_vector (embedding) m=10 ef_construct=40 distance='cosine') 's3://testbucket/admin/data/' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}')" | $BENDSQL_CLIENT_CONNECT
+echo "create table phrases (id INT, text STRING,  text_0 STRING, embedding Vector(8), NGRAM INDEX idx_text (text), VECTOR INDEX idx_vector (embedding) m=10 ef_construct=40 distance='cosine') 's3://testbucket/data/' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}')" | $BENDSQL_CLIENT_CONNECT
 echo "CREATE INVERTED INDEX idx_inverted ON phrases(text_0);" | $BENDSQL_CLIENT_CONNECT
 
 echo "init table"
@@ -14,7 +14,7 @@ storage_prefix=$(mysql -uroot -h127.0.0.1 -P3307  -e "set global hide_options_in
 
 # attach table
 echo "drop table if exists att_phrases" | $BENDSQL_CLIENT_CONNECT
-echo "attach table att_phrases 's3://testbucket/admin/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
+echo "attach table att_phrases 's3://testbucket/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
 
 # check index
 echo "check index"
@@ -23,31 +23,31 @@ echo "select name, type, database, table from system.indexes where database = 'd
 # refresh ngram index
 stmt "REFRESH NGRAM INDEX idx_text ON phrases;"
 echo "drop table if exists att_phrases" | $BENDSQL_CLIENT_CONNECT
-echo "attach table att_phrases 's3://testbucket/admin/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
+echo "attach table att_phrases 's3://testbucket/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
 echo "select name, type, database, table from system.indexes where database = 'default' and table = 'att_phrases';" | $BENDSQL_CLIENT_CONNECT
 
 # drop ngram index
 stmt "DROP NGRAM INDEX idx_text ON phrases;"
 echo "drop table if exists att_phrases" | $BENDSQL_CLIENT_CONNECT
-echo "attach table att_phrases 's3://testbucket/admin/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
+echo "attach table att_phrases 's3://testbucket/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
 echo "select name, type, database, table from system.indexes where database = 'default' and table = 'att_phrases';" | $BENDSQL_CLIENT_CONNECT
 
 # refresh inverted index
 stmt "REFRESH INVERTED INDEX idx_inverted ON phrases;"
 echo "drop table if exists att_phrases" | $BENDSQL_CLIENT_CONNECT
-echo "attach table att_phrases 's3://testbucket/admin/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
+echo "attach table att_phrases 's3://testbucket/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
 echo "select name, type, database, table from system.indexes where database = 'default' and table = 'att_phrases';" | $BENDSQL_CLIENT_CONNECT
 
 # drop inverted index
 stmt "DROP INVERTED INDEX idx_inverted ON phrases;"
 echo "drop table if exists att_phrases" | $BENDSQL_CLIENT_CONNECT
-echo "attach table att_phrases 's3://testbucket/admin/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
+echo "attach table att_phrases 's3://testbucket/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
 echo "select name, type, database, table from system.indexes where database = 'default' and table = 'att_phrases';" | $BENDSQL_CLIENT_CONNECT
 
 # drop vector index
 stmt "DROP VECTOR INDEX idx_vector ON phrases;"
 echo "drop table if exists att_phrases" | $BENDSQL_CLIENT_CONNECT
-echo "attach table att_phrases 's3://testbucket/admin/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
+echo "attach table att_phrases 's3://testbucket/data/$storage_prefix' connection=(access_key_id ='minioadmin' secret_access_key ='minioadmin' endpoint_url='${STORAGE_S3_ENDPOINT_URL}');" | $BENDSQL_CLIENT_CONNECT
 echo "select name, type, database, table from system.indexes where database = 'default' and table = 'att_phrases';" | $BENDSQL_CLIENT_CONNECT
 
 echo "drop table if exists phrases" | $BENDSQL_CLIENT_CONNECT
