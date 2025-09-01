f04ffa7686910369602c4826c0b1a28bddf7fda9
Yang Xiufeng <yangxiufeng.c@gmail.com>
2025-09-01 15:37:22 +0800

    ci: ttc test with nginx and minio.

---
+++ scripts/ci/deploy/databend-query-cluster-3-nodes-nginx.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Copyright 2022 The Databend Authors.
+# SPDX-License-Identifier: Apache-2.0.
+
+set -ex
+
+SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+cd "$SCRIPT_PATH/../../.." || exit
+BUILD_PROFILE=${BUILD_PROFILE:-debug}
+
+# Caveat: has to kill query first.
+# `query` tries to remove its liveness record from meta before shutting down.
+# If meta is stopped, `query` will receive an error that hangs graceful
+# shutdown.
+killall databend-query || true
+sleep 3
+
+killall databend-meta || true
+sleep 3
+
+for bin in databend-query databend-meta; do
+	if test -n "$(pgrep $bin)"; then
+		echo "The $bin is not killed. force killing."
+		killall -9 $bin || true
+	fi
+done
+
+# Wait for killed process to cleanup resources
+sleep 1
+
+echo 'Start Meta service HA cluster(3 nodes)...'
+
+mkdir -p ./.databend/
+
+nohup ./target/${BUILD_PROFILE}/databend-meta -c scripts/ci/deploy/config/databend-meta-node-1.toml >./.databend/meta-1.out 2>&1 &
+python3 scripts/ci/wait_tcp.py --timeout 30 --port 9191
+
+# wait for cluster formation to complete.
+sleep 1
+
+nohup ./target/${BUILD_PROFILE}/databend-meta -c scripts/ci/deploy/config/databend-meta-node-2.toml >./.databend/meta-2.out 2>&1 &
+python3 scripts/ci/wait_tcp.py --timeout 30 --port 28202
+
+# wait for cluster formation to complete.
+sleep 1
+
+nohup ./target/${BUILD_PROFILE}/databend-meta -c scripts/ci/deploy/config/databend-meta-node-3.toml >./.databend/meta-3.out 2>&1 &
+python3 scripts/ci/wait_tcp.py --timeout 30 --port 28302
+
+# wait for cluster formation to complete.
+sleep 1
+
+echo 'Start databend-query node-1'
+nohup env RUST_BACKTRACE=1 QUERY_HTTP_HANDLER_PORT=8081 target/${BUILD_PROFILE}/databend-query -c scripts/ci/deploy/config/databend-query-node-1.toml --internal-enable-sandbox-tenant >./.databend/query-1.out 2>&1 &
+
+echo "Waiting on node-1..."
+python3 scripts/ci/wait_tcp.py --timeout 30 --port 9091
+
+echo 'Start databend-query node-2'
+env "RUST_BACKTRACE=1" nohup target/${BUILD_PROFILE}/databend-query -c scripts/ci/deploy/config/databend-query-node-2.toml --internal-enable-sandbox-tenant >./.databend/query-2.out 2>&1 &
+
+echo "Waiting on node-2..."
+python3 scripts/ci/wait_tcp.py --timeout 30 --port 9092
+
+echo 'Start databend-query node-3'
+env "RUST_BACKTRACE=1" nohup target/${BUILD_PROFILE}/databend-query -c scripts/ci/deploy/config/databend-query-node-3.toml --internal-enable-sandbox-tenant >./.databend/query-3.out 2>&1 &
+
+echo "Waiting on node-3..."
+python3 scripts/ci/wait_tcp.py --timeout 30 --port 9093
+
+echo 'Start Nginx'
+if docker inspect nginx_db >/dev/null 2>&1; then
+  docker rm -f nginx_db
+fi
+docker run -d --network host --name nginx_db -v "$(pwd)/scripts/ci/deploy/config/nginx.conf":/etc/nginx/nginx.conf:ro nginx
+
+docker logs nginx_db
+
+curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-Type: application/json' -d '{"sql": "select * from system.clusters"}'
+
+echo
+
+curl -s -u root: -XPOST "http://localhost:8000/v1/query" -H 'Content-Type: application/json' -d "{\"sql\": \"create user databend IDENTIFIED BY 'databend'\"}"
+
+echo
+
+echo "All done..."
