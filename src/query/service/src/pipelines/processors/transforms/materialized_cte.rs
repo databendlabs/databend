40966a88c721e1cd395ec7fd241a2e7c7a27d80f
sky <3374614481@qq.com>
2025-09-03 16:12:11 +0800

    feat: support backpressure in MaterializedCteSink

--- src/query/service/src/pipelines/processors/transforms/materialized_cte.rs
+++ src/query/service/src/pipelines/processors/transforms/materialized_cte.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::any::Any;
 use std::sync::Arc;
 
 use async_channel::Receiver;
@@ -19,43 +20,68 @@ use async_channel::Sender;
 use databend_common_exception::ErrorCode;
 use databend_common_exception::Result;
 use databend_common_expression::DataBlock;
+use databend_common_pipeline_core::processors::Event;
 use databend_common_pipeline_core::processors::InputPort;
 use databend_common_pipeline_core::processors::OutputPort;
+use databend_common_pipeline_core::processors::Processor;
 use databend_common_pipeline_core::processors::ProcessorPtr;
-use databend_common_pipeline_sinks::AsyncSink;
-use databend_common_pipeline_sinks::AsyncSinker;
 use databend_common_pipeline_sources::AsyncSource;
 use databend_common_pipeline_sources::AsyncSourcer;
 use databend_common_storages_fuse::TableContext;
 
 pub struct MaterializedCteSink {
+    input: Arc<InputPort>,
     senders: Vec<Sender<DataBlock>>,
 }
 
 impl MaterializedCteSink {
     pub fn create(input: Arc<InputPort>, senders: Vec<Sender<DataBlock>>) -> Result<ProcessorPtr> {
-        Ok(ProcessorPtr::create(AsyncSinker::create(input, Self {
-            senders,
-        })))
+        Ok(ProcessorPtr::create(Box::new(Self { input, senders })))
+    }
+
+    fn should_apply_backpressure(&self) -> bool {
+        self.senders.iter().all(|sender| !sender.is_empty())
     }
 }
 
 #[async_trait::async_trait]
-impl AsyncSink for MaterializedCteSink {
-    const NAME: &'static str = "MaterializedCteSink";
+impl Processor for MaterializedCteSink {
+    fn name(&self) -> String {
+        "MaterializedCteSink".to_string()
+    }
 
-    async fn consume(&mut self, data_block: DataBlock) -> Result<bool> {
+    fn as_any(&mut self) -> &mut dyn Any {
+        self
+    }
+
+    fn event(&mut self) -> Result<Event> {
+        if self.input.is_finished() {
+            for sender in self.senders.iter() {
+                sender.close();
+            }
+            self.input.finish();
+            return Ok(Event::Finished);
+        }
+
+        if self.input.has_data() {
+            if self.should_apply_backpressure() {
+                return Ok(Event::NeedConsume);
+            }
+            return Ok(Event::Async);
+        }
+
+        self.input.set_need_data();
+        Ok(Event::NeedData)
+    }
+
+    async fn async_process(&mut self) -> Result<()> {
+        let data_block = self.input.pull_data().ok_or_else(|| {
+            ErrorCode::Internal("Failed to pull data from input port in materialized cte sink")
+        })??;
         for sender in self.senders.iter() {
             sender.send(data_block.clone()).await.map_err(|_| {
                 ErrorCode::Internal("Failed to send blocks to materialized cte consumer")
             })?;
-        }
-        Ok(false)
-    }
-
-    async fn on_finish(&mut self) -> Result<()> {
-        for sender in self.senders.iter() {
-            sender.close();
         }
         Ok(())
     }
