3136b6675bd8937e0c9348c48a82846ec3f2d888
everpcpc <git@everpcpc.com>
2025-09-09 15:33:05 +0800

    z

--- tests/nox/noxfile.py
+++ tests/nox/noxfile.py
@@ -17,9 +17,9 @@ def python_client(session, driver_version):
         env = {
             "DRIVER_VERSION": driver_version,
         }
-        session.run("behave","tests/asyncio", env=env)
-        session.run("behave","tests/blocking", env=env)
-        session.run("behave","tests/cursor", env=env)
+        session.run("behave", "tests/asyncio", env=env)
+        session.run("behave", "tests/blocking", env=env)
+        session.run("behave", "tests/cursor", env=env)
 
 
 JDBC_DRIVER = ["0.4.0", "main"]
--- tests/suites/1_stateful/09_http_handler/09_0007_session.py
+++ tests/suites/1_stateful/09_http_handler/09_0007_session.py
@@ -168,8 +168,12 @@ HEADER_SESSION_ID_V = "101010"
 
 
 def do_query_from_worksheet(client, sql, sid=HEADER_SESSION_ID_V, new_session=False):
-    internal = None if new_session else "{}";
-    payload = {"sql": sql, "pagination": {"max_rows_per_page": 2, "wait_time_secs": 10}, "session": {"internal": internal}}
+    internal = None if new_session else "{}"
+    payload = {
+        "sql": sql,
+        "pagination": {"max_rows_per_page": 2, "wait_time_secs": 10},
+        "session": {"internal": internal},
+    }
     resp = client.post(
         query_url,
         auth=auth,
@@ -208,8 +212,11 @@ def test_worksheet_session():
     resp = do_query_from_worksheet(client, "select * from t09_0007", new_session=True)
     assert "Unknown table" in resp["error"]["message"], resp
 
-    resp = do_query_from_worksheet(client, "select * from numbers(10) ", new_session=True)
+    resp = do_query_from_worksheet(
+        client, "select * from numbers(10) ", new_session=True
+    )
     assert len(resp["data"]) == 2, resp
+
 
 def main():
     test_no_session()
