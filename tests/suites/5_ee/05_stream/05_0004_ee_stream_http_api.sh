4bc2fbea2215b47def37f581c3e16ffdf9a94eea
everpcpc <git@everpcpc.com>
2025-09-10 10:57:41 +0800

    z

--- tests/suites/5_ee/05_stream/05_0004_ee_stream_http_api.sh
+++ tests/suites/5_ee/05_stream/05_0004_ee_stream_http_api.sh
@@ -10,7 +10,7 @@ echo "create table t1(a int)" | $BENDSQL_CLIENT_CONNECT
 echo "create stream s1 on table t1" | $BENDSQL_CLIENT_CONNECT
 
 echo "# Test catalog streams API - existing database"
-curl -s -u root: -XGET "http://localhost:8000/v1/catalog/databases/default/streams" | jq '. | .streams[0] |= del(.created_on, .updated_on)'
+curl -s -u root: -XGET "http://localhost:8000/v1/catalog/databases/default/streams" | jq '. | .streams[]? |= del(.created_on, .updated_on)'
 
 echo "# Test catalog streams API - non-existent database"
 curl -s -u root: -XGET "http://localhost:8000/v1/catalog/databases/nonexistent/streams" -w "%{http_code}\n"
