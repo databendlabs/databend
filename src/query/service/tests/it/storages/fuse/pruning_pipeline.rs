0afd99f2feb493ee87fe3a8b5357653c1c5959a7
kould <kould2333@gmail.com>
2025-09-12 14:17:23 +0800

    chore: codefmt

--- src/query/service/tests/it/storages/fuse/pruning_pipeline.rs
+++ src/query/service/tests/it/storages/fuse/pruning_pipeline.rs
@@ -176,6 +176,7 @@ async fn test_snapshot_pruner() -> Result<()> {
         cluster_key: None,
         table_indexes: None,
         table_constraints: None,
+        auto_increments: vec![],
         attached_columns: None,
         table_partition: None,
         table_properties: None,
