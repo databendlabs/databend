af39e74a82d7e49d5bfaa67dfed7638135162a07
coldWater <forsaken628@gmail.com>
2025-09-17 20:06:46 +0800

    test_cur_batch

--- src/query/expression/src/aggregate/elastic_hash.rs
+++ src/query/expression/src/aggregate/elastic_hash.rs
@@ -4,16 +4,22 @@ struct Elastic {
     count: usize,
     delta: f64,
     c: f64,
+    i: usize,
+    batch_limit: usize,
+    load_limit: usize,
 }
 
 impl Elastic {
-    pub fn with_capacity(n: usize) -> Self {
+    pub fn with_capacity(n: usize, delta: f64) -> Self {
         assert!(n.is_power_of_two());
         Self {
             entries: vec![None; n],
             c: 1000.0,
             count: 0,
-            delta: 0.75,
+            delta,
+            i: 0,
+            batch_limit: n / 8 * 3,
+            load_limit: n - (n as f64 * delta) as usize,
         }
     }
 
@@ -22,10 +28,10 @@ impl Elastic {
         self.entries.len().trailing_zeros() as usize + 1
     }
 
-    fn ai_range(&self, i: usize) -> std::ops::Range<usize> {
-        assert!(i >= 1);
-        assert!(i <= self.levels());
-        // A_i 大小 = n / 2^(i-1)
+    fn sub_range(&self, i: usize) -> std::ops::Range<usize> {
+        debug_assert!(i >= 1);
+        debug_assert!(i <= self.levels());
+
         let n = self.entries.len();
         let size = n >> (i - 1);
         let start = n - size;
@@ -33,51 +39,49 @@ impl Elastic {
         start..end
     }
 
-    pub fn ai(&self, i: usize) -> &[Option<usize>] {
-        let r = self.ai_range(i);
+    fn sub_size(&self, i: usize) -> usize {
+        self.entries.len() >> i
+    }
+
+    pub fn sub(&self, i: usize) -> &[Option<usize>] {
+        let r = self.sub_range(i);
         &self.entries[r]
     }
 
-    pub fn ai_mut(&mut self, i: usize) -> &mut [Option<usize>] {
-        let r = self.ai_range(i);
+    pub fn sub_mut(&mut self, i: usize) -> &mut [Option<usize>] {
+        let r = self.sub_range(i);
         &mut self.entries[r]
     }
 
     pub fn epsilon_i(&self, i: usize) -> f64 {
-        let r = self.ai_range(i);
+        let r = self.sub_range(i);
         let sz = r.len();
         let empty = self.entries[r].iter().filter(|x| x.is_none()).count();
         empty as f64 / sz as f64
     }
 
-    pub fn next_batch(&self) -> Option<usize> {
-        let mut count = self.count;
+    pub fn cur_batch(&mut self) -> Option<usize> {
+        let count = self.count;
 
-        for i in 0..self.levels() {
-            let b = self.batch_size(i);
-            if count > b {
-                count -= b
-            } else {
-                return Some(i);
-            }
+        if self.i > self.levels() {
+            return None;
         }
 
-        None
+        if count < self.batch_limit {
+            Some(self.i)
+        } else {
+            self.i += 1;
+            self.batch_limit += self.batch_size(self.i);
+            Some(self.i)
+        }
     }
 
-    pub fn batch_size(&self, i: usize) -> usize {
+    fn batch_size(&self, i: usize) -> usize {
+        let n = self.entries.len();
         if i == 0 {
-            return (self.ai_range(1).len() as f64 * 0.75) as usize;
+            return n / 8 * 3;
         }
-
-        let ai = self.ai_range(i).len();
-        let ai1 = self.ai_range(i + 1).len();
-
-        // 计划批量：Bi (i>=1) = |Ai| - floor(δ|Ai|/2) - ceil(0.75|Ai|) + ceil(0.75|A_{i+1}|)
-        let term4 = (0.75_f64 * ai1 as f64).ceil() as usize; // ceil(0.75|A_{i+1}|)
-        let term2 = ((self.delta * ai as f64) / 2.0).floor() as usize; // floor(δ|Ai|/2)
-
-        (0.25_f64 * ai as f64).ceil() as usize + term4 - term2
+        1.max(((1.0 - self.delta) / 2.0 * (n >> i) as f64) as usize + (n >> (i + 3)))
     }
 
     pub fn f_value(&self, i: usize) -> f64 {
@@ -87,12 +91,15 @@ impl Elastic {
     }
 
     pub fn probe_sub(&self, i: usize, mut j: usize, max: Option<usize>) -> Option<usize> {
-        let sub = self.ai(i);
+        let sub = self.sub(i);
         let end = max.map(|max| (j + max) % sub.len());
 
         let mut c = 0;
         loop {
             c += 1;
+            if c > sub.len() {
+                panic!()
+            }
             if sub[j].is_none() {
                 println!("    probe count {c}");
                 return Some(j);
@@ -112,7 +119,7 @@ impl Elastic {
 
     pub fn probe(&self, i: usize, value: usize) -> (u8, usize, usize) {
         if i == 0 {
-            let j = value % self.ai_range(1).len();
+            let j = value % self.sub_size(1);
             return (0, 1, self.probe_sub(1, j, None).unwrap());
         }
 
@@ -120,22 +127,22 @@ impl Elastic {
 
         // case 3
         if self.epsilon_i(i + 1) <= 0.25 {
-            let j = value % self.ai_range(i).len();
+            let j = value % self.sub_size(i);
             return (30, i, self.probe_sub(i, j, None).unwrap());
         }
 
         // case 1
         if self.epsilon_i(i) > 0.5 * self.delta {
-            let j = value % self.ai_range(i).len();
+            let j = value % self.sub_size(i);
             if let Some(j) = self.probe_sub(i, j, Some(self.f_value(i) as usize)) {
                 return (10, i, j);
             }
-            let j = value % self.ai_range(i + 1).len();
+            let j = value % self.sub_size(i + 1);
             return (11, i + 1, self.probe_sub(i + 1, j, None).unwrap());
         }
 
         // case 2
-        let j = value % self.ai_range(i + 1).len();
+        let j = value % self.sub_size(i + 1);
         (20, i + 1, self.probe_sub(i + 1, j, None).unwrap())
     }
 }
@@ -145,15 +152,15 @@ mod tests {
 
     #[test]
     fn test_main() {
-        let mut elastic = Elastic::with_capacity(128);
+        let mut elastic = Elastic::with_capacity(128, 0.75);
 
-        for value in 200..284 {
-            let i = elastic.next_batch().unwrap();
+        for value in 200..280 {
+            let i = elastic.cur_batch().unwrap();
             println!("start {value} {i}");
 
             let (case, i, j) = elastic.probe(i, value);
 
-            let x = elastic.ai_mut(i)[j].replace(value);
+            let x = elastic.sub_mut(i)[j].replace(value);
             elastic.count += 1;
             assert_eq!(x, None);
 
@@ -165,5 +172,31 @@ mod tests {
         }
 
         println!("{elastic:?}")
+    }
+
+    #[test]
+    fn test_cur_batch() {
+        // when i>=1,  B(i).len = A(i).len - A(i).len * delta/2 - 0.75 * A(i).len + 0.75 * A( i+1 ).len
+        {
+            let mut index = Elastic::with_capacity(128, 0.25);
+            let mut ls = vec![];
+            while index.cur_batch().is_some() {
+                ls.push(index.batch_limit);
+                index.count = index.batch_limit;
+            }
+            assert_eq!(&ls, &[48, 80, 96, 104, 108, 109, 110, 111, 112, 113]);
+        }
+
+        {
+            let mut index = Elastic::with_capacity(2048, 0.1);
+            let mut ls = vec![];
+            while index.cur_batch().is_some() {
+                ls.push(index.batch_limit);
+                index.count = index.batch_limit;
+            }
+            assert_eq!(&ls, &[
+                768, 1356, 1650, 1797, 1870, 1906, 1924, 1933, 1937, 1938, 1939, 1940, 1941, 1942
+            ]);
+        }
     }
 }
