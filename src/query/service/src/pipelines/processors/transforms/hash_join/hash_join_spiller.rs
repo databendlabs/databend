445517fd42fc266b3a01cad59b6b9d4484163b29
zhang2014 <coswde@gmail.com>
2025-09-20 12:20:09 +0800

    refactor(query): stream style block writer for hash join spill

--- src/query/service/src/pipelines/processors/transforms/hash_join/hash_join_spiller.rs
+++ src/query/service/src/pipelines/processors/transforms/hash_join/hash_join_spiller.rs
@@ -90,11 +90,7 @@ impl HashJoinSpiller {
         // if the partition memory size exceeds the threshold.
         let partition_threshold = partition_buffer_threshold * 1024 * 1024 / num_partitions;
 
-        let mut block_bytes = ctx.get_settings().get_max_block_bytes()? as usize;
-
-        if ctx.get_settings().get_force_join_data_spill()? {
-            block_bytes = 1;
-        }
+        let block_bytes = ctx.get_settings().get_max_block_bytes()? as usize;
 
         // Create a PartitionBuffer to buffer data before spilling.
         let partition_buffer = PartitionBuffer::create(num_partitions);
