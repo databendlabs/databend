7531beeb57b5cfded769780404271da27481d915
dqhl76 <dqhl76@gmail.com>
2025-10-22 18:07:58 +0800

    used for debug

--- src/query/service/src/pipelines/processors/transforms/aggregator/new_final_aggregate/new_transform_final_aggregate.rs
+++ src/query/service/src/pipelines/processors/transforms/aggregator/new_final_aggregate/new_transform_final_aggregate.rs
@@ -265,13 +265,13 @@ impl NewFinalAggregateTransform {
         // merge new produced repartitioned queues into local repartitioned queues
         self.repartitioned_queues.merge_queues(new_produced);
 
-        if self.spiller.memory_settings.check_spill() {
-            info!(
-                "NewFinalAggregateTransform[{}] trigger spill due to memory limit",
-                self.id
-            );
-            self.shared_state.lock().is_spilled = true;
-        }
+        // if self.spiller.memory_settings.check_spill() {
+        //     info!(
+        //         "NewFinalAggregateTransform[{}] trigger spill due to memory limit",
+        //         self.id
+        //     );
+        //     self.shared_state.lock().is_spilled = true;
+        // }
 
         // if other processor or itself trigger spill, this processor will need spill its local repartitioned queue out
         if self.shared_state.lock().is_spilled && !self.is_spilled && !self.working_queue.is_empty()
