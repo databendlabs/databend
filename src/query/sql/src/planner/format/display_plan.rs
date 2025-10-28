199214ae0b1b333058dbc1ed4bb6a114ca9a3dd8
dantengsky <dantengsky@gmail.com>
2025-10-28 19:26:52 +0800

    Implements Plan::AlterDatabase indentity format

--- src/query/sql/src/planner/format/display_plan.rs
+++ src/query/sql/src/planner/format/display_plan.rs
@@ -282,9 +282,7 @@ impl Plan {
             Plan::SetWorkloadGroupQuotas(_) => Ok("SetWorkloadGroupQuotas".to_string()),
             Plan::UnsetWorkloadGroupQuotas(_) => Ok("UnsetWorkloadGroupQuotas".to_string()),
             Plan::AlterRole(_) => Ok("AlterRole".to_string()),
-            Plan::AlterDatabase(_) => {
-                todo!()
-            }
+            Plan::AlterDatabase(_) => Ok("AlterDatabase".to_string()),
         }
     }
 }
