f8103c69678278d3439439ccdc8e486929192e39
zhang2014 <coswde@gmail.com>
2025-11-07 21:26:48 +0800

    refactor(query): merge pipeline core, sources and sinks crate

--- src/query/pipeline/src/core/mod.rs
+++ src/query/pipeline/src/core/mod.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 mod finished_chain;
 mod input_error;
 mod lock_guard;
