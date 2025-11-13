b4f0fd32fcf32b740a5fa3d08308e71ded0e530a
TCeason <33082201+TCeason@users.noreply.github.com>
2025-11-13 08:41:02 +0800

    refactor(query): prevent masking/row access policy name conflicts (#18937)
    * feat(query): prevent masking/row access policy name conflicts
    
    * extract check and fetch_id from loop
    
    * delete loop
    
    * fix
    
    * remove extra get_pb
    
    * use ?? replace double unwrap
    
    * fix flaky test

--- src/query/ee_features/data_mask/src/data_mask_handler.rs
+++ src/query/ee_features/data_mask/src/data_mask_handler.rs
@@ -30,11 +30,15 @@ use std::sync::Arc;
 
 use databend_common_base::base::GlobalInstance;
 use databend_common_exception::Result;
+use databend_common_meta_app::data_mask::data_mask_name_ident::Resource;
+use databend_common_meta_app::data_mask::CreateDatamaskReply;
 use databend_common_meta_app::data_mask::CreateDatamaskReq;
 use databend_common_meta_app::data_mask::DatamaskMeta;
 use databend_common_meta_app::data_mask::DropDatamaskReq;
 use databend_common_meta_app::tenant::Tenant;
+use databend_common_meta_app::tenant_key::errors::ExistError;
 use databend_common_meta_store::MetaStore;
+use databend_common_meta_types::MetaError;
 use databend_common_meta_types::SeqV;
 
 #[async_trait::async_trait]
@@ -43,7 +47,10 @@ pub trait DatamaskHandler: Sync + Send {
         &self,
         meta_api: Arc<MetaStore>,
         req: CreateDatamaskReq,
-    ) -> Result<()>;
+    ) -> std::result::Result<
+        std::result::Result<CreateDatamaskReply, ExistError<Resource>>,
+        MetaError,
+    >;
 
     async fn drop_data_mask(&self, meta_api: Arc<MetaStore>, req: DropDatamaskReq) -> Result<()>;
 
@@ -75,7 +82,10 @@ impl DatamaskHandlerWrapper {
         &self,
         meta_api: Arc<MetaStore>,
         req: CreateDatamaskReq,
-    ) -> Result<()> {
+    ) -> std::result::Result<
+        std::result::Result<CreateDatamaskReply, ExistError<Resource>>,
+        MetaError,
+    > {
         self.handler.create_data_mask(meta_api, req).await
     }
 
