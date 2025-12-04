b861aca8f2bb5c48de8bed30b05848b4b81a66df
sky <3374614481@qq.com>
2025-12-04 11:58:14 +0800

    fix

--- src/query/service/src/pipelines/processors/transforms/new_hash_join/memory/inner_join.rs
+++ src/query/service/src/pipelines/processors/transforms/new_hash_join/memory/inner_join.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use std::ops::Deref;
 use std::sync::Arc;
 use std::sync::PoisonError;
