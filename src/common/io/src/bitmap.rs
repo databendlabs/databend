f1b4ac8d0fb182eff83d0d8802af6bfdb686954e
kould <kould2333@gmail.com>
2025-12-09 14:52:14 +0800

    chore: codefmt

--- src/common/io/src/bitmap.rs
+++ src/common/io/src/bitmap.rs
@@ -29,12 +29,12 @@ use roaring::RoaringTreemap;
 use smallvec::SmallVec;
 
 // https://github.com/ClickHouse/ClickHouse/blob/516a6ed6f8bd8c5f6eed3a10e9037580b2fb6152/src/AggregateFunctions/AggregateFunctionGroupBitmapData.h#L914
-const LARGE_THRESHOLD: usize = 32;
-const HYBRID_MAGIC: [u8; 2] = *b"HB";
-const HYBRID_VERSION: u8 = 1;
-const HYBRID_KIND_SMALL: u8 = 0;
-const HYBRID_KIND_LARGE: u8 = 1;
-const HYBRID_HEADER_LEN: usize = 4;
+pub const LARGE_THRESHOLD: usize = 32;
+pub const HYBRID_MAGIC: [u8; 2] = *b"HB";
+pub const HYBRID_VERSION: u8 = 1;
+pub const HYBRID_KIND_SMALL: u8 = 0;
+pub const HYBRID_KIND_LARGE: u8 = 1;
+pub const HYBRID_HEADER_LEN: usize = 4;
 
 type SmallBitmap = SmallVec<[u64; LARGE_THRESHOLD]>;
 
