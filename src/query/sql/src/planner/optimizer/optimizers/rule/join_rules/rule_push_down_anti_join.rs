1eda119bcd3b8a49e5eae50c6d405f93761e1218
zhang2014 <coswde@gmail.com>
2025-12-11 18:59:57 +0800

    chore(query): refactor the cardinality of anti-join

--- src/query/sql/src/planner/optimizer/optimizers/rule/join_rules/rule_push_down_anti_join.rs
+++ src/query/sql/src/planner/optimizer/optimizers/rule/join_rules/rule_push_down_anti_join.rs
@@ -12,7 +12,224 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::sync::Arc;
 
+use databend_common_exception::Result;
+
+use crate::binder::JoinPredicate;
+use crate::optimizer::ir::Matcher;
+use crate::optimizer::ir::RelExpr;
+use crate::optimizer::ir::SExpr;
+use crate::optimizer::optimizers::rule::Rule;
+use crate::optimizer::optimizers::rule::RuleID;
+use crate::optimizer::optimizers::rule::TransformResult;
+use crate::plans::Join;
+use crate::plans::JoinType;
+use crate::plans::RelOp;
+use crate::plans::RelOperator;
+use crate::ColumnSet;
+
+/// Push `Left/Right Semi|Anti` join closer to the base table that participates
+/// in the predicate so that fewer rows stay in the join tree.
 pub struct RulePushdownAntiJoin {
+    id: RuleID,
+    matchers: Vec<Matcher>,
+}
 
+impl RulePushdownAntiJoin {
+    pub fn new() -> Self {
+        Self {
+            id: RuleID::PushDownAntiJoin,
+            matchers: vec![Matcher::MatchOp {
+                op_type: RelOp::Join,
+                children: vec![Matcher::Leaf, Matcher::Leaf],
+            }],
+        }
+    }
+
+    fn try_push_down(&self, left: &SExpr, right: &SExpr, join: Join) -> Result<Option<SExpr>> {
+        let right_rel_expr = RelExpr::with_s_expr(right);
+
+        if let Some(inner_join) = extract_inner_join(left)? {
+            let inner_join_rel_expr = RelExpr::with_s_expr(&inner_join);
+            let inner_join_left_prop = inner_join_rel_expr.derive_relational_prop_child(0)?;
+            let inner_join_right_prop = inner_join_rel_expr.derive_relational_prop_child(1)?;
+
+            let equi_conditions = join
+                .equi_conditions
+                .iter()
+                .map(|condition| {
+                    JoinPredicate::new(
+                        &condition.left,
+                        &inner_join_left_prop,
+                        &inner_join_right_prop,
+                    )
+                })
+                .collect::<Vec<_>>();
+
+            if equi_conditions.iter().all(left_predicate) {
+                // let mut new_equi_conditions = Vec::with_capacity(equi_conditions.len());
+
+                // for (idx, (inferred, predicate)) in equi_conditions.into_iter().enumerate() {
+                //     if !inferred || matches!(predicate, JoinPredicate::Left(_)) {
+                //         new_equi_conditions.push(join.equi_conditions[idx].clone());
+                //     }
+                // }
+
+                // join.equi_conditions = new_equi_conditions;
+                let right_prop = right_rel_expr.derive_relational_prop()?;
+                let mut union_output_columns = ColumnSet::new();
+                union_output_columns.extend(right_prop.output_columns.clone());
+                union_output_columns.extend(inner_join_left_prop.output_columns.clone());
+
+                if join
+                    .non_equi_conditions
+                    .iter()
+                    .all(|x| x.used_columns().is_subset(&union_output_columns))
+                {
+                    let new_inner_join = inner_join.replace_children([
+                        Arc::new(SExpr::create_binary(
+                            RelOperator::Join(join.clone()),
+                            inner_join.child(0)?.clone(),
+                            right.clone(),
+                        )),
+                        Arc::new(inner_join.child(1)?.clone()),
+                    ]);
+
+                    return replace_inner_join(left, new_inner_join);
+                }
+            } else if equi_conditions.iter().all(right_predicate) {
+                // let mut new_equi_conditions = Vec::with_capacity(equi_conditions.len());
+
+                // for (idx, (inferred, predicate)) in equi_conditions.into_iter().enumerate() {
+                //     if !inferred || matches!(predicate, JoinPredicate::Left(_)) {
+                //         new_equi_conditions.push(join.equi_conditions[idx].clone());
+                //     }
+                // }
+
+                // join.equi_conditions = new_equi_conditions;
+                let right_prop = right_rel_expr.derive_relational_prop()?;
+                let mut union_output_columns = ColumnSet::new();
+                union_output_columns.extend(right_prop.output_columns.clone());
+                union_output_columns.extend(inner_join_right_prop.output_columns.clone());
+
+                if join
+                    .non_equi_conditions
+                    .iter()
+                    .all(|x| x.used_columns().is_subset(&union_output_columns))
+                {
+                    let new_inner_join = inner_join.replace_children([
+                        Arc::new(inner_join.child(0)?.clone()),
+                        Arc::new(SExpr::create_binary(
+                            RelOperator::Join(join.clone()),
+                            inner_join.child(1)?.clone(),
+                            right.clone(),
+                        )),
+                    ]);
+
+                    return replace_inner_join(left, new_inner_join);
+                }
+            }
+        }
+
+        Ok(None)
+    }
+}
+
+impl Rule for RulePushdownAntiJoin {
+    fn id(&self) -> RuleID {
+        self.id
+    }
+
+    fn apply(&self, s_expr: &SExpr, state: &mut TransformResult) -> Result<()> {
+        let join: Join = s_expr.plan().clone().try_into()?;
+
+        if matches!(join.join_type, JoinType::LeftAnti | JoinType::LeftSemi) {
+            if let Some(mut result) =
+                self.try_push_down(s_expr.child(0)?, s_expr.child(1)?, join)?
+            {
+                result.set_applied_rule(&self.id);
+                state.add_result(result);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn matchers(&self) -> &[Matcher] {
+        &self.matchers
+    }
+}
+
+impl Default for RulePushdownAntiJoin {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+fn replace_inner_join(expr: &SExpr, new_join: SExpr) -> Result<Option<SExpr>> {
+    match expr.plan() {
+        RelOperator::Join(join) if join.join_type == JoinType::Inner => Ok(Some(new_join)),
+        RelOperator::Filter(_) => match replace_inner_join(expr.child(0)?, new_join)? {
+            None => Ok(None),
+            Some(new_child) => Ok(Some(expr.replace_children([Arc::new(new_child)]))),
+        },
+        _ => Ok(None),
+    }
+}
+
+fn extract_inner_join(expr: &SExpr) -> Result<Option<SExpr>> {
+    match expr.plan() {
+        RelOperator::Join(join) if join.join_type == JoinType::Inner => Ok(Some(expr.clone())),
+        RelOperator::Filter(_) => extract_inner_join(expr.child(0)?),
+        _ => Ok(None),
+    }
+}
+
+// struct ColumnMappingRewriter<'a> {
+//     mapping: &'a HashMap<usize, ColumnBinding>,
+// }
+//
+// impl VisitorMut<'_> for ColumnMappingRewriter {
+//     fn visit_bound_column_ref(&mut self, col: &mut BoundColumnRef) -> Result<()> {
+//         if let Some(&new_index) = self.mapping.get(&col.column.index) {
+//             col.column = new_index.clone();
+//         }
+//         Ok(())
+//     }
+// }
+//
+// fn replace_by_equivalence(
+//     expr: &ScalarExpr,
+//     mapping: &HashMap<IndexType, ColumnBinding>,
+// ) -> Result<ScalarExpr> {
+//     if mapping.is_empty() {
+//         return Ok(expr.clone());
+//     }
+//
+//     let mut new_expr = expr.clone();
+//     let mut rewriter = ColumnMappingRewriter { mapping };
+//     rewriter.visit(&mut new_expr)?;
+//     Ok(new_expr)
+// }
+//
+// fn collect_mapping(join: &Join) -> Result<Vec<(ColumnBinding, ColumnBinding)>> {
+//     for equi_condition in &join.equi_conditions {
+//         match equi_condition.left
+//     }
+// }
+//
+// fn all_left(
+//     conditions: &[JoinEquiCondition],
+//     inner_join: Join,
+//     left: Arc<RelationalProperty>,
+// ) -> Result<bool> {
+// }
+
+fn left_predicate(tuple: &JoinPredicate) -> bool {
+    matches!(&tuple, JoinPredicate::Left(_))
+}
+
+fn right_predicate(tuple: &JoinPredicate) -> bool {
+    matches!(&tuple, JoinPredicate::Right(_))
 }
