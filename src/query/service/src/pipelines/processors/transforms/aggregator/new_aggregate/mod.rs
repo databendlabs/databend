df270a0f10a2a4390df0060ceea9bdeca9730f76
dqhl76 <dqhl76@gmail.com>
2025-12-25 13:14:15 +0800

    license check

--- src/query/service/src/pipelines/processors/transforms/aggregator/new_aggregate/mod.rs
+++ src/query/service/src/pipelines/processors/transforms/aggregator/new_aggregate/mod.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 mod new_aggregate_spiller;
 mod new_transform_aggregate_partial;
 mod new_transform_final_aggregate;
