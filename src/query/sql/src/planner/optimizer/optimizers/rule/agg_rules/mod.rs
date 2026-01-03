fc0b3b16f25a766f1f6324d5206508f8e0413dcc
zhang2014 <coswde@gmail.com>
2026-01-03 13:23:48 +0800

    z

--- src/query/sql/src/planner/optimizer/optimizers/rule/agg_rules/mod.rs
+++ src/query/sql/src/planner/optimizer/optimizers/rule/agg_rules/mod.rs
@@ -19,7 +19,7 @@ mod rule_grouping_sets_to_union;
 mod rule_hierarchical_grouping_sets;
 mod rule_push_down_filter_aggregate;
 mod rule_push_down_limit_aggregate;
-mod rule_shrink_group_by_type;
+mod rule_shrink_type;
 mod rule_split_aggregate;
 mod rule_try_apply_agg_index;
 
@@ -29,6 +29,6 @@ pub use rule_grouping_sets_to_union::RuleGroupingSetsToUnion;
 pub use rule_hierarchical_grouping_sets::RuleHierarchicalGroupingSetsToUnion;
 pub use rule_push_down_filter_aggregate::RulePushDownFilterAggregate;
 pub use rule_push_down_limit_aggregate::RulePushDownRankLimitAggregate;
-pub use rule_shrink_group_by_type::RuleShrinkGroupByType;
+pub use rule_shrink_type::RuleShrinkType;
 pub use rule_split_aggregate::RuleSplitAggregate;
 pub use rule_try_apply_agg_index::RuleTryApplyAggIndex;
