9b0bec0c397fc6710735969bd680dde7e0f17087
Yang Xiufeng <yangxiufeng.c@gmail.com>
2026-01-08 16:18:02 +0800

    skip compat test case

--- tests/nox/noxfile.py
+++ tests/nox/noxfile.py
@@ -17,9 +17,8 @@ def python_client(session, driver_version):
         env = {
             "DRIVER_VERSION": driver_version,
         }
-        session.run("behave", "tests/asyncio", env=env)
-        session.run("behave", "tests/blocking", env=env)
-        session.run("behave", "tests/cursor", env=env)
+        # for impl in ['blocking', "asyncio", 'cursor']:
+        #     session.run("behave", f"tests/{impl}", env=env)
 
 
 JDBC_DRIVER = ["0.4.0", "main"]
