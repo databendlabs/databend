4703f32588e426ea5540ac0d17958e0acb20fa77
dantengsky <dantengsky@gmail.com>
2026-01-16 16:37:59 +0800

    split mods

--- src/query/storages/common/blocks/src/lib.rs
+++ src/query/storages/common/blocks/src/lib.rs
@@ -14,6 +14,12 @@
 
 #![allow(clippy::collapsible_if, clippy::uninlined_format_args)]
 
-mod parquet_rs;
-pub use parquet_rs::*;
+mod encoding_rules;
 pub mod memory;
+mod parquet_rs;
+
+pub use encoding_rules::EncodingStatsProvider;
+pub use encoding_rules::NdvProvider;
+pub use encoding_rules::page_limit::MAX_BATCH_MEMORY_SIZE;
+pub use encoding_rules::page_limit::write_batch_with_page_limit;
+pub use parquet_rs::*;
