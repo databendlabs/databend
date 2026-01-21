6afec18ebd9aa317ad3046ef4c6092cd9ae1d7f1
coldWater <forsaken628@gmail.com>
2026-01-21 17:35:39 +0800

    fix

--- src/query/service/src/pipelines/processors/transforms/new_hash_join/memory/nested_loop.rs
+++ src/query/service/src/pipelines/processors/transforms/new_hash_join/memory/nested_loop.rs
@@ -88,6 +88,10 @@ impl<T: Join> Join for NestedLoopJoin<T> {
         self.inner.final_build()
     }
 
+    fn add_runtime_filter_packet(&self, packet: JoinRuntimeFilterPacket) {
+        self.inner.add_runtime_filter_packet(packet);
+    }
+
     fn build_runtime_filter(&self) -> Result<JoinRuntimeFilterPacket> {
         self.inner.build_runtime_filter()
     }
