e154a0a455404b590da1d57041fac8e346b0f905
zhang2014 <coswde@gmail.com>
2026-01-22 20:00:57 +0800

    refactor(query): refactor hybrid hash join for new join

--- src/query/settings/src/settings_default.rs
+++ src/query/settings/src/settings_default.rs
@@ -1505,7 +1505,7 @@ impl DefaultSettings {
                     range: Some(SettingRange::Numeric(0..=1)),
                 }),
                 ("enable_experimental_new_join", DefaultSettingValue {
-                    value: UserSettingValue::UInt64(1),
+                    value: UserSettingValue::UInt64(0),
                     desc: "Enables the experimental new join implement",
                     mode: SettingMode::Both,
                     scope: SettingScope::Both,
