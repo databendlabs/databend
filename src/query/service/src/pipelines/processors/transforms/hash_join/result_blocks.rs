0a2143398c247ce9f6284904b74cb2ecee59c21d
zhang2014 <coswde@gmail.com>
2026-03-09 13:29:29 +0800

    z

--- src/query/service/src/pipelines/processors/transforms/hash_join/result_blocks.rs
+++ src/query/service/src/pipelines/processors/transforms/hash_join/result_blocks.rs
@@ -53,13 +53,14 @@ impl HashJoinProbeState {
             self.hash_join_state.hash_join_desc.join_type,
             JoinType::InnerAny | JoinType::RightAny
         ) {
-            let build_num_rows = unsafe {
-                (*self.hash_join_state.build_state.get())
-                    .generation_state
-                    .build_num_rows
-            };
+            unsafe {
+                let build_state = *self.hash_join_state.build_state.get();
 
-            probe_state.used_once = Some(MutableBitmap::from_len_zeroed(build_num_rows))
+                let block_num = build_state.generation_state.chunks.len();
+                probe_state.used_once = Some(MutableBitmap::from_len_zeroed(
+                    block_num * probe_state.max_block_size,
+                ));
+            }
         }
         let no_other_predicate = self
             .hash_join_state
