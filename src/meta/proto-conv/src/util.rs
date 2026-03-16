41ac6203dfa5b1c80fe25e5e812ad1f040599c7e
zhyass <mytesla@live.com>
2026-03-16 10:42:34 +0800

    fix test

--- src/meta/proto-conv/src/util.rs
+++ src/meta/proto-conv/src/util.rs
@@ -197,7 +197,7 @@ const META_CHANGE_LOG: &[(u64, &str)] = &[
     (165, "2026-01-24: Add: add cluster_key_v2 on TableMeta"),
     (166, "2026-02-13: Add: UserOption::default_warehouse"),
     (167, "2026-02-28: Add: file_format.proto Lance file format"),
-    (168, "2026-03-11: Remove: share_by in TableMeta/DatabaseMeta and SnapshotRef in TableMeta")
+    (168, "2026-03-11: Remove: share_by in TableMeta/DatabaseMeta and refs in TableMeta, Remove SnapshotRef, Add TableTag")
     // Dear developer:
     //      If you're gonna add a new metadata version, you'll have to add a test for it.
     //      You could just copy an existing test file(e.g., `../tests/it/v024_table_meta.rs`)
