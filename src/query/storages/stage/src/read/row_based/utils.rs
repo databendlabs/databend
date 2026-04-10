d7f8ee2b1bf1eb0ae597cf515c0b6b8810bbcbaa
Yang Xiufeng <yangxiufeng.c@gmail.com>
2026-04-10 08:56:38 +0800

    refactor: use trim_ascii.

--- src/query/storages/stage/src/read/row_based/utils.rs
+++ src/query/storages/stage/src/read/row_based/utils.rs
@@ -23,24 +23,9 @@ pub fn truncate_column_data(s: String) -> String {
     }
 }
 
-pub fn trim_ascii_space(mut data: &[u8]) -> &[u8] {
-    while let Some((first, rest)) = data.split_first() {
-        if is_trim_space_byte(*first) {
-            data = rest;
-        } else {
-            break;
-        }
-    }
-
-    while let Some((last, rest)) = data.split_last() {
-        if is_trim_space_byte(*last) {
-            data = rest;
-        } else {
-            break;
-        }
-    }
-
-    data
+#[inline]
+pub fn trim_ascii_space(data: &[u8]) -> &[u8] {
+    data.trim_ascii()
 }
 
 #[inline]
