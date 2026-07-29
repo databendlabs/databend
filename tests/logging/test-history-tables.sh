31e009bcb291895b71a220c5cf5b84b1c541df78
Yang Xiufeng <yangxiufeng.c@gmail.com>
2026-07-29 11:37:13 +0800

    test(logging): extend history startup timeout

--- tests/logging/test-history-tables.sh
+++ tests/logging/test-history-tables.sh
@@ -102,13 +102,13 @@ echo 'Start databend-query node-1'
 nohup env RUST_BACKTRACE=1 target/${BUILD_PROFILE}/databend-query -c scripts/ci/deploy/config/databend-query-node-1.toml --internal-enable-sandbox-tenant >./.databend/query-1.out 2>&1 &
 
 echo "Waiting on node-1..."
-python3 scripts/ci/wait_tcp.py --timeout 30 --port 9091
+python3 scripts/ci/wait_tcp.py --timeout 50 --port 9091
 
 echo 'Start databend-query node-2'
 env "RUST_BACKTRACE=1" nohup target/${BUILD_PROFILE}/databend-query -c scripts/ci/deploy/config/databend-query-node-2.toml --internal-enable-sandbox-tenant >./.databend/query-2.out 2>&1 &
 
 echo "Waiting on node-2..."
-python3 scripts/ci/wait_tcp.py --timeout 30 --port 9092
+python3 scripts/ci/wait_tcp.py --timeout 50 --port 9092
 
 echo "Started 2-node cluster with history tables enabled..."
 
