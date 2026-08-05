7bb3a533c662ab8326872c9b11f8cd162e999d1c
coldWater <forsaken628@gmail.com>
2026-08-05 12:21:05 +0800

    fix

--- src/query/service/src/schedulers/fragments/plan_fragment.rs
+++ src/query/service/src/schedulers/fragments/plan_fragment.rs
@@ -23,6 +23,7 @@ use databend_common_exception::ErrorCode;
 use databend_common_exception::Result;
 use databend_common_expression::BlockEntry;
 use databend_common_expression::Column;
+use databend_common_expression::ColumnBuilder;
 use databend_common_expression::DataBlock;
 use databend_common_settings::ReplaceIntoShuffleStrategy;
 use databend_storages_common_table_meta::meta::BlockSlotDescription;
@@ -36,6 +37,7 @@ use crate::physical_plans::IPhysicalPlan;
 use crate::physical_plans::MutationSource;
 use crate::physical_plans::PhysicalPlan;
 use crate::physical_plans::PhysicalPlanCast;
+use crate::physical_plans::PhysicalPlanMeta;
 use crate::physical_plans::PhysicalPlanVisitor;
 use crate::physical_plans::Recluster;
 use crate::physical_plans::ReplaceDeduplicate;
@@ -105,13 +107,45 @@ impl PlanFragment {
             }
             FragmentType::Intermediate => {
                 if self.has_merge_input {
-                    // If this is a intermediate fragment with merge input,
-                    // we will only send it to coordinator node.
-                    let action = QueryFragmentAction::create(
-                        Fragmenter::get_local_executor(ctx),
-                        self.plan.clone(),
-                    );
+                    // Only the coordinator can consume the merge input. Other shuffle
+                    // destinations still need this fragment to receive remote data.
+                    let local_executor = Fragmenter::get_local_executor(ctx);
+                    let action =
+                        QueryFragmentAction::create(local_executor.clone(), self.plan.clone());
                     fragment_actions.add_action(action);
+
+                    if let Some(exchange) = &self.exchange {
+                        let mut empty_plan = self.plan.clone();
+                        let Some(exchange_sink) =
+                            ExchangeSink::from_mut_physical_plan(&mut empty_plan)
+                        else {
+                            return Err(ErrorCode::Internal(
+                                "Intermediate fragment exchange plan has no ExchangeSink",
+                            ));
+                        };
+                        exchange_sink.input = PhysicalPlan::new(ConstantTableScan {
+                            meta: PhysicalPlanMeta::new("ConstantTableScan"),
+                            values: exchange_sink
+                                .schema
+                                .fields()
+                                .iter()
+                                .map(|field| {
+                                    ColumnBuilder::with_capacity(field.data_type(), 0).build()
+                                })
+                                .collect(),
+                            num_rows: 0,
+                            output_schema: exchange_sink.schema.clone(),
+                        });
+
+                        for executor in exchange.get_destinations() {
+                            if executor != local_executor {
+                                fragment_actions.add_action(QueryFragmentAction::create(
+                                    executor,
+                                    empty_plan.clone(),
+                                ));
+                            }
+                        }
+                    }
                 } else {
                     // Otherwise distribute the fragment to all the executors.
                     for executor in Fragmenter::get_executors(ctx) {
