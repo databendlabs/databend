03587ace02b4fdd91cd273f847c7f4a5cd032624
Liuqing Yue <dqhl76@gmail.com>
2026-08-19 02:11:28 +0000

    fix(query): avoid oversized row output buffer allocations (#20325)
    fix(formats): avoid oversized row output buffers

--- src/query/formats/src/output_format/tsv.rs
+++ src/query/formats/src/output_format/tsv.rs
@@ -66,7 +66,7 @@ impl TEXTOutputFormat {
 impl OutputFormat for TEXTOutputFormat {
     fn serialize_block(&mut self, block: &DataBlock) -> Result<Vec<u8>> {
         let rows_size = block.num_rows();
-        let mut buf = Vec::with_capacity(block.memory_size());
+        let mut buf = Vec::with_capacity(block.estimate_block_size(block.num_columns()));
 
         let fd = self.field_delimiter;
         let rd = &self.record_delimiter;
