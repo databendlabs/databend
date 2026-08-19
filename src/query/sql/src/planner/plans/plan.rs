0586e434322503a2a26e422b82b4cc01e7e08463
TCeason <tai_chong@foxmail.com>
2026-08-19 15:58:52 +0800

    feat(query): add system.materialized_views

--- src/query/sql/src/planner/plans/plan.rs
+++ src/query/sql/src/planner/plans/plan.rs
@@ -535,6 +535,7 @@ pub enum RewriteKind {
     ShowDatabases,
     ShowDropDatabases,
     ShowTables(String, String),
+    ShowMaterializedViews,
     ShowColumns(String, String, String),
     ShowTablesStatus,
     ShowVirtualColumns,
