7540c119e91b29657a803e7ee3eb440d5f3128cf
zhyass <mytesla@live.com>
2026-08-31 11:16:02 +0800

    test(meta): add segment claim compatibility fixture

--- src/meta/proto-conv/src/util.rs
+++ src/meta/proto-conv/src/util.rs
@@ -212,6 +212,7 @@ const META_CHANGE_LOG: &[(u64, &str)] = &[
     (180, "2026-07-10: Add: catalog.proto/PaimonCatalogOption"),
     (181, "2026-07-21: Add: materialized_view.proto/{MVDefinition,MVSourceBinding,MVSourceBindingVersion}"),
     (182, "2026-08-05: Add: datatype.proto/AggregateState"),
+    (183, "2026-08-31: Add: segment_claim.proto/SegmentClaimMeta"),
     // Dear developer:
     //      If you're gonna add a new metadata version, you'll have to add a test for it.
     //      You could just copy an existing test file(e.g., `../tests/it/v024_table_meta.rs`)
