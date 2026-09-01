45761c15f3c235425deed6e4f62d10ef1d95cabf
dantengsky <dantengsky@gmail.com>
2026-09-01 21:51:56 +0800

    test(storage): update vacuum2 fake-time assertions

--- tests/suites/9_faked_time/00_dummy_cases/00_0003_vacuum2_respect_time_travel.sh
+++ tests/suites/9_faked_time/00_dummy_cases/00_0003_vacuum2_respect_time_travel.sh
@@ -3,47 +3,19 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../../../shell_env.sh
 
-SEGMENTS=$(echo "select file_location from fuse_segment('default','test_vacuum2_respect_time_travel');" | bendsql_connect_root)
-BLOCKS=$(echo "select block_location from fuse_block('default','test_vacuum2_respect_time_travel');" | bendsql_connect_root)
-
 stmt "insert into test_vacuum2_respect_time_travel values(2);"
 
-IFS=$'\n' read -d '' -r -a segments <<< "$SEGMENTS"
-IFS=$'\n' read -d '' -r -a blocks <<< "$BLOCKS"
-blooms=()
-for block in "${blocks[@]}"; do
-    bloom=$(echo "$block" | sed -E 's|(1/[0-9]+)/_b/g([0-9a-f]{32})_v2\.parquet|\1/_i_b_v2/\2_v4.parquet|')
-    blooms+=("$bloom")
-done
-
 stmt "set data_retention_time_in_days = 2;truncate table test_vacuum2_respect_time_travel;"
-
-SNAPSHOTS=$(echo "select snapshot_location from fuse_snapshot('default','test_vacuum2_respect_time_travel');" | bendsql_connect_root)
-IFS=$'\n' read -d '' -r -a snapshots <<< "$SNAPSHOTS"
-to_be_vacuumed=("${snapshots[@]}" "${segments[@]}" "${blocks[@]}" "${blooms[@]}")
 
 # gc root
 stmt "insert into test_vacuum2_respect_time_travel values(3);"
 
-
 # should have 4 snapshots
 query "select count(*) from fuse_snapshot('default','test_vacuum2_respect_time_travel')"
 
-RESULTS=$(echo "set data_retention_time_in_days = 0;select * from fuse_vacuum2('default','test_vacuum2_respect_time_travel',true);" | bendsql_connect_root)
-IFS=$'\n' read -d '' -r -a results <<< "$RESULTS"
+stmt "set data_retention_time_in_days = 0;call system\$fuse_vacuum2('default','test_vacuum2_respect_time_travel',true);"
 
-# verify the vacuum result
-sorted_results=($(printf "%s\n" "${results[@]}" | sort))
-sorted_to_be_vacuumed=($(printf "%s\n" "${to_be_vacuumed[@]}" | sort))
-
-if [ "$(printf "%s" "${sorted_results[@]}")" != "$(printf "%s" "${sorted_to_be_vacuumed[@]}")" ]; then
-    echo "Vacuum failed"
-    echo "Results array: ${sorted_results[@]}"
-    echo "To be vacuumed array: ${sorted_to_be_vacuumed[@]}"
-    exit 1
-fi
-
-# remain two snapshots
+# only the current snapshot remains
 query "select count(*) from fuse_snapshot('default','test_vacuum2_respect_time_travel')"
 
 # verify the data
